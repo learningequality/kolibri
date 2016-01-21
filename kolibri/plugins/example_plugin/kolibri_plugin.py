84e3d3295c6367242d44f813767d452d24141274
Benjamin Bach <benjaoming@gmail.com>
2016-01-21 03:46:19 +0100

    rm print stm

--- kolibri/plugins/example_plugin/kolibri_plugin.py
+++ kolibri/plugins/example_plugin/kolibri_plugin.py
@@ -10,7 +10,11 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+
+from django.utils.translation import ugettext as _
+
 from kolibri.plugins.base import KolibriPluginBase
+from kolibri.plugins.hooks import NAVIGATION_POPULATE
 
 
 class ExamplePlugin(KolibriPluginBase):
@@ -26,3 +30,14 @@ class ExamplePlugin(KolibriPluginBase):
         print("Deactivating example plugin")
         # Make this automatic and use __name__ ?
         config["INSTALLED_APPS"].remove("kolibri.plugins.example_plugin")
+
+    def main_navigation(self):
+        return [{
+            'menu_name': _("Google"),
+            'menu_url': 'http://google.com',
+        }]
+
+    def hooks(self):
+        return {
+            NAVIGATION_POPULATE: self.main_navigation
+        }
