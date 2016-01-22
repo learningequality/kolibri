2683c38eb3d0af0c4d6200a4967ec8ed34f8fb0c
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-01-21 16:25:19 -0800

    Demonstrate `hooks` as property & plugin inheritance
    Also adds quite a few docstrings.

--- kolibri/plugins/example_plugin/kolibri_plugin.py
+++ kolibri/plugins/example_plugin/kolibri_plugin.py
@@ -17,27 +17,80 @@ from kolibri.plugins.base import KolibriPluginBase
 from kolibri.plugins.hooks import NAVIGATION_POPULATE
 
 
-class ExamplePlugin(KolibriPluginBase):
+class NavMenuPlugin(KolibriPluginBase):
+    """
+    An example plugin that demonstrates how the Kolibri plugin system works. Adds items to the nav menu by registering
+    a callback to the NAVIGATION_POPULATE hook.
+    """
 
     @classmethod
     def enable(cls, config):
+        """
+        A plugin must implement this abstract method. It's called by the command ``kolibri plugin <NAME> enable`` to
+        modify the kolibri settings file.
+
+        :param config: A dictionary of settings, like the one at ``django.conf.settings``.
+        :return: Nothing, though it may modify the mutable config parameter
+        """
         print("Activating example plugin")
         # Make this automatic and use __name__ ?
         config["INSTALLED_APPS"].append("kolibri.plugins.example_plugin")
 
     @classmethod
     def disable(cls, config):
+        """
+        A plugin must implement this abstract method. It's called by the command ``kolibri plugin <NAME> disable`` to
+        modify the kolibri settings file.
+
+        :param config: A dictionary of settings, like the one at ``django.conf.settings``.
+        :return: Nothing, though it may modify the mutable config parameter
+        """
         print("Deactivating example plugin")
         # Make this automatic and use __name__ ?
         config["INSTALLED_APPS"].remove("kolibri.plugins.example_plugin")
 
-    def main_navigation(self):
+    @staticmethod
+    def main_navigation():
+        """
+        A callback registered in the ``hooks`` property of this class. It's defined as a method instead of a
+        module-level function in order to allow other plugins to inherit this class and change the behavior.
+        """
         return [{
             'menu_name': _("Google"),
             'menu_url': 'http://google.com',
         }]
 
+    @property
     def hooks(self):
+        """
+        Normally ``hooks`` should be an attribute -- here we use the ``property`` decorator to simulate that.
+        This allows the callback to be changed by subclasses without changing the ``hooks`` attribute.
+        """
         return {
             NAVIGATION_POPULATE: self.main_navigation
         }
+
+
+class ExtendedPlugin(NavMenuPlugin):
+    """
+    Demonstrates plugin inheritance. Notice that the ``hooks`` attribute need not be changed -- only the callback.
+    """
+    @classmethod
+    def enable(cls, config):
+        pass
+
+    @classmethod
+    def disable(cls, config):
+        pass
+
+    def main_navigation(self):
+        return [
+            {
+                'menu_name': _('Disney'),
+                'menu_url': 'http://disney.com',
+            },
+            {
+                'menu_name': _('Yahoo'),
+                'menu_url': 'http://yahoo.com',
+            }
+        ]
--- kolibri/plugins/hooks.py
+++ kolibri/plugins/hooks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Hooks are used for your plugin's ``hooks()`` method, returning a dictionary
-with hooks you wish to use. For instance::
+Hooks are used for your plugin's ``hooks`` attribute, a dictionary
+with hooks you wish to use. The keys are pre-defined constants in this module or arbitrary strings. For instance::
 
     from kolibri.plugins.base import KolibriPluginBase
     from kolibri.plugins.hooks import NAVIGATION_POPULATE
@@ -26,6 +26,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 __hook_index = -1
+
+
 def __enumerate_hook():
     global __hook_index
     __hook_index += 1
@@ -38,6 +40,7 @@ REGISTERED_HOOKS = {
     x: [] for x in range(__hook_index + 1)
 }
 
+
 def register_hook(hook, callback):
     """
     Takes a hook identifier and adds it to the registry
@@ -49,4 +52,8 @@ def register_hook(hook, callback):
 
 
 def get_callables(hook):
+    """
+    :param hook: The hook identifier, either a constant from this module or an arbitrary string.
+    :return: An iterable of callbacks.
+    """
     return REGISTERED_HOOKS[hook]
