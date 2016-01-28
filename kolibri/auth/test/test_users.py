d8aecd13d42eaffce081ad514c3f7f770a21952e
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-01-27 20:32:22 -0800

    Remove circular import

--- kolibri/auth/test/test_users.py
+++ kolibri/auth/test/test_users.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from kolibri.auth.models import BaseUser, FacilityUser, DeviceOwner
+# Importing user models here results in a circular import... you should use get_user_model, but then there's no
+# way to get the proxy models as well. So just import at runtime.
 
 
 class UserSanityTestCase(TestCase):
@@ -9,19 +10,24 @@ class UserSanityTestCase(TestCase):
     """
 
     def test_facility_user(self):
+        from kolibri.auth.models import BaseUser
         with self.assertRaises(NotImplementedError):
             BaseUser().is_device_owner()
 
     def test_base_user(self):
+        from kolibri.auth.models import FacilityUser
         self.assertFalse(FacilityUser(username="foo").is_device_owner())
 
     def test_device_admin(self):
+        from kolibri.auth.models import DeviceOwner
         self.assertTrue(DeviceOwner(username="bar").is_device_owner())
 
     def test_short_name(self):
+        from kolibri.auth.models import FacilityUser
         self.assertEqual(FacilityUser(username="mike", first_name="Mike", last_name="Gallaspy").get_short_name(),
                          "Mike")
 
     def test_full_name(self):
+        from kolibri.auth.models import FacilityUser
         self.assertEqual(FacilityUser(username="mike", first_name="Mike", last_name="Gallaspy").get_full_name(),
                          "Mike Gallaspy")
