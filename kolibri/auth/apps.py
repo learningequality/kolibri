3aa82869db61c41db27c11cb7f2ea5b0cd685b67
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-01-27 17:40:05 -0800

    Create auth app skel

---
+++ kolibri/auth/apps.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class KolibriAuthConfig(AppConfig):
+    name = 'kolibri.auth'
+    label = 'kolibriauth'
+    verbose_name = 'Kolibri Auth'
--- kolibri/deployment/default/settings/base.py
+++ kolibri/deployment/default/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kolibri.auth.apps.KolibriAuthConfig',
 ] + conf.config['INSTALLED_APPS']
 
 MIDDLEWARE_CLASSES = (
