77be00d5f30ab0264a355b9f9773871ae4beb899
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-01-28 17:07:26 -0800

    Add DeviceBackend authentication

---
+++ kolibri/auth/backends.py
@@ -0,0 +1,38 @@
+"""
+Implements a custom auth backend as described in the Django docs. Notably authentication could be provided by the
+default Django backend, but authorization (i.e. permissions checking) must be handled by our custom backends since we
+do not use the regular Django permissions. See handling authorization docs. Should then be listed in the
+AUTHENTICATION_BACKENDS. Note that authentication backends are checked in the order they're listed.
+"""
+from models import BaseUser, DeviceOwner
+
+
+class BaseBackend(object):
+    """
+    Provides the authentication parts that are common to both backends.
+    """
+    def _authenticate(self, username=None, password=None):
+        try:
+            user = BaseUser.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except BaseUser.DoesNotExist:
+            return None
+
+
+class DeviceBackend(BaseBackend):
+    """
+    A very simple backend that, when passed in a user for permissions checking, returns True if the user is a
+    DeviceAdmin, or False otherwise.
+    """
+
+    def authenticate(self, username=None, password=None):
+        user = self._authenticate(username, password)
+        return DeviceOwner.objects.get(pk=user.pk) if user._is_device_owner else None
+
+    def get_user(self, user_id):
+        try:
+            user = DeviceOwner.objects.get(pk=user_id)
+            return user if user._is_device_owner else None
+        except DeviceOwner.DoesNotExist:
+            return None
---
+++ kolibri/auth/test/test_backend.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+# Importing user models here results in a circular import... you should use get_user_model, but then there's no
+# way to get the proxy models as well. So just import at runtime.
+from kolibri.auth.backends import DeviceBackend
+
+
+class DeviceBackendTestCase(TestCase):
+    def setUp(self):
+        from kolibri.auth.models import FacilityUser, DeviceOwner
+        user = self.user = FacilityUser(username="Mike")
+        user.set_password("foo")
+        user.save()
+
+        do = self.do = DeviceOwner(username="Chuck")
+        do.set_password("foobar")
+        do.save()
+
+    def test_facility_user_not_authenticated(self):
+        self.assertIsNone(DeviceBackend().authenticate(username="Mike", password="foo"))
+
+    def test_device_owner_authenticated(self):
+        self.assertEqual(self.do, DeviceBackend().authenticate(username="Chuck", password="foobar"))
