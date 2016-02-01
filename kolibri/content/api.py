20d0100b117e1ed47f1131f05d620fa9608787d5
66eli77 <66eli77@gmail.com>
2016-02-01 15:10:25 -0800

    never catch all exceptions!

--- kolibri/content/api.py
+++ kolibri/content/api.py
@@ -33,7 +33,7 @@ def is_valid_uuid(uuid_to_test, version=4):
     """
     try:
         uuid_obj = UUID(uuid_to_test, version=version)
-    except:
+    except ValueError:
         return False
 
     return str(uuid_obj) == uuid_to_test
