527fe5acb592bdeaf07748fc4dad514b125468d8
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-01 18:51:14 -0800

    Stub out Role and Collection models

--- kolibri/deployment/default/settings/base.py
+++ kolibri/deployment/default/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kolibri.auth.apps.KolibriAuthConfig',
+    'mptt',
 ] + conf.config['INSTALLED_APPS']
 
 MIDDLEWARE_CLASSES = (
