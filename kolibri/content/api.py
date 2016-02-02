4f9ab85b0eeb1ad6f599d9cd6f6fecf67dde9f02
66eli77 <66eli77@gmail.com>
2016-02-01 17:36:05 -0800

    add properly formated docstrings

--- kolibri/content/api.py
+++ kolibri/content/api.py
@@ -15,21 +15,9 @@ def is_valid_uuid(uuid_to_test, version=4):
     """
     Check if uuid_to_test is a valid UUID.
 
-    Parameters
-    ----------
-    uuid_to_test : str
-    version : {1, 2, 3, 4}
-
-    Returns
-    -------
-    `True` if uuid_to_test is a valid UUID, otherwise `False`.
-
-    Examples
-    --------
-    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
-    True
-    >>> is_valid_uuid('c9bf9e58')
-    False
+    :param uuid_to_test: str
+    :param version: int {1, 2, 3, 4}
+    :return: True if uuid_to_test is from a valid UUID
     """
     try:
         uuid_obj = UUID(uuid_to_test, version=version)
@@ -43,8 +31,8 @@ def is_valid_uuid(uuid_to_test, version=4):
 
 def can_get_content_with_id(func):
     """
-    decorator function for returning ContentMetadata object when giving content id
-    it can take keyword argument/s "content" or "content1" and "content2"
+    decorator function for returning ContentMetadata object when giving content id.
+    it can take keyword argument/s "content" or "content1" and "content2".
     """
     @wraps(func)
     def wrapper(channel_id=None, **kwargs):
@@ -65,7 +53,13 @@ def can_get_content_with_id(func):
     return wrapper
 
 def get_content_with_id(content_id, channel_id=None):
-    """Return arbitrary sets of Content Metadata objects based on content id(s)"""
+    """
+    Get arbitrary sets of ContentMetadata objects based on content id(s).
+
+    :param channel_id: str
+    :param content_id: list or str or uuid
+    :return: QuerySet of ContentMetadata
+    """
     if isinstance(content_id, list):
         return KolibriContent.ContentMetadata.objects.using(channel_id).filter(content_id__in=content_id)
     else:
@@ -73,88 +67,161 @@ def get_content_with_id(content_id, channel_id=None):
 
 @can_get_content_with_id
 def get_ancestor_topics(channel_id=None, content=None, **kwargs):
+    """"
+    Get all ancestors that the their kind are topics 
+
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of ContentMetadata
+    """
     return content.get_ancestors().filter(kind="topic").using(channel_id)
 
 @can_get_content_with_id
 def immediate_children(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """Return a set of ContentMetadatas that has this ContentMetadata as the immediate parent."""
+    """
+    Get a set of ContentMetadatas that have this ContentMetadata as the immediate parent.
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of ContentMetadata
+    """
     return content.get_children().using(channel_id)
 
 @can_get_content_with_id
 def leaves(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all ContentMetadatas that are the terminal nodes and also the descendants of the this ContentMetadata"""
+    """
+    Get all ContentMetadatas that are the terminal nodes and also the descendants of the this ContentMetadata.
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of ContentMetadata
+    """
     return content.get_leafnodes().using(channel_id)
 
 @can_get_content_with_id
 def get_all_formats(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all possible formats for a particular content including its descendants' formats"""
+    """
+    Get all possible formats for a particular content including its descendants' formats.
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of Format
+    """
     all_end_nodes = leaves(channel_id=channel_id, content=content)
     return KolibriContent.Format.objects.using(channel_id).filter(contentmetadata__in=all_end_nodes)
 
 @can_get_content_with_id
 def get_available_formats(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all available formats for a particular content excluding its descendants' formats
-    if the pass-in content is a topic, this function will return null"""
+    """
+    Get all available formats for a particular content excluding its descendants' formats.
+    if the pass-in content is a topic, this function will return null.
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of Format
+    """
     return KolibriContent.Format.objects.using(channel_id).filter(contentmetadata=content, available=True)
 
 @can_get_content_with_id
 def get_possible_formats(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all possible formats for a particular content excluding its descendants' formats
-    if the pass-in content is a topic, this function will return null"""
+    """
+    Get all possible formats for a particular content excluding its descendants' formats.
+    if the pass-in content is a topic, this function will return null.
+
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of Format
+    """
     return KolibriContent.Format.objects.using(channel_id).filter(contentmetadata=content)
 
 @can_get_content_with_id
 def get_files_for_quality(channel_id=None, content=None, format_quality=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all files for a particular content in particular quality. 
-    For the first argument, please pass in a string like "high" or "low" or "normal"
-    For the second argument, You can pass in either a ContentMetadata object with keyword 'content' or a uuid4 with keyword "content_id"
-    topic content will return null."""
+    """
+    Get all files for a particular content in particular quality. 
+    For format_quality argument, please pass in a string like "high" or "low" or "normal".
+    topic content will return null.
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :param format_quality: str
+    :return: QuerySet of File
+    """
     the_formats = get_possible_formats(channel_id=channel_id, content=content).filter(quality=format_quality)
     return KolibriContent.File.objects.using(channel_id).filter(format__in=the_formats)
 
 @can_get_content_with_id
 def get_missing_files(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all missing files under the content"""
+    """
+    Get all missing files of the content.
+
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of File
+    """
     all_end_nodes = leaves(channel_id=channel_id, content=content)
     return KolibriContent.File.objects.using(channel_id).filter(available = False, format__contentmetadata__in=all_end_nodes)
 
 @can_get_content_with_id
 def get_all_prerequisites(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return cotents that are the prerequisites of this content"""
+    """
+    Get cotents that are the prerequisites of this content.
+
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of ContentMetadata
+    """
     return KolibriContent.ContentMetadata.objects.using(channel_id).filter(prerequisite=content)
 
 @can_get_content_with_id
 def get_all_related(channel_id=None, content=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return cotents that are the related to this content"""
+    """
+    Get cotents that are related to this content.
+
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :return: QuerySet of ContentMetadata
+    """
     return KolibriContent.ContentMetadata.objects.using(channel_id).filter(relate_to=content)
 
 @can_get_content_with_id
 def set_prerequisite(channel_id=None, content1=None, content2=None, **kwargs):
-    """set prerequisite relationship between content1 and content2"""
+    """
+    Set prerequisite relationship between content1 and content2.
+
+    :param channel_id: str
+    :param content1: ContentMetadata or str
+    :param content2: ContentMetadata or str
+    """
     KolibriContent.PrerequisiteContentRelationship.objects.using(channel_id).create(relationship_type='prerequisite', contentmetadata_1=content1, contentmetadata_2=content2)
 
 @can_get_content_with_id
 def set_is_related(channel_id=None, content1=None, content2=None, **kwargs):
-    """set related relationship between content1 and content2"""
+    """
+    Set is related relationship between content1 and content2.
+
+    :param channel_id: str
+    :param content1: ContentMetadata or str
+    :param content2: ContentMetadata or str
+    """
     KolibriContent.RelatedContentRelationship.objects.using(channel_id).create(relationship_type='related', contentmetadata_1=content1, contentmetadata_2=content2)
 
 @can_get_content_with_id
 def children_of_kind(channel_id=None, content=None, kind=None, **kwargs):
-    """The content can be a ContentMetadatas object or a content_id string."""
-    """return all ContentMetadatas of a particular kind under the given ContentMetadata"""
+    """
+    Get all ContentMetadatas of a particular kind under the given ContentMetadata.
+    For kind argument, please pass in a string like "topic" or "video" or "exercise".
+    
+    :param channel_id: str
+    :param content: ContentMetadata or str
+    :param kind: str
+    :return: QuerySet of ContentMetadata
+    """
     return content.get_descendants(include_self=False).filter(kind=kind).using(channel_id)
 
 # def scan_and_update_file_availability(channel_id=None):
-#     """this is a safty check to ensure that the File model truthfully reflects what's actually in and not in the content_files folder"""
+#     """
+#     this is a safty check to ensure that the File model truthfully reflects what's actually in and not in the content_files folder.
+#     """
 #     all_files = KolibriContent.File.objects.using(channel_id).all().only("available", 'checksum', 'extension')
 #     for f in all_files:
 #         the_file_path = settings.CONTENT_COPY_DIR + '/' + f.checksum + '.' + f.extension
@@ -165,7 +232,13 @@ def children_of_kind(channel_id=None, content=None, kind=None, **kwargs):
 #     bulk_update(all_files, update_fields=['available'], using=channel_id)
 
 def update_content_copy(file_object=None, content_copy=None):
-    """you can pass None on content_copy to remove the associated file on disk"""
+    """
+    Update the File object you pass in with the content copy
+    You can pass None on content_copy to remove the associated file on disk.
+    
+    :param file_object: File
+    :param content_copy: str
+    """
     if not file_object:
         raise TypeError( "must provide a File object to update content copy")
     if content_copy:
@@ -180,7 +253,12 @@ def update_content_copy(file_object=None, content_copy=None):
 """channel API methods"""
 
 def process_channel_identifier(channel_identifier):
-    """this func will take a channel id or channel name and return a ChannelMetadata object"""
+    """
+    This func will take a channel id or channel name and return a ChannelMetadata object.
+
+    :param channel_identifier: str
+    :return: ChannelMetadata
+    """
     if is_valid_uuid(channel_identifier):
         try:
             the_channel = KolibriContent.ChannelMetadata.objects.get(channel_id=channel_identifier)
@@ -202,12 +280,30 @@ def process_channel_identifier(channel_identifier):
         raise ValueError('the channel_identifier must be either a UUID String or a normal String')
 
 def get_available_channels():
+    """
+    Get all existing ChannelMetadata objects.
+
+    :return: QuerySet of ChannelMetadata
+    """
     return KolibriContent.ChannelMetadata.objects.all()
 
 def get_channel(channel_identifier):
+    """
+    Get a ChannelMetadata object by channel id or channel name.
+
+    :param channel_identifier: str
+    :return: ChannelMetadata
+    """
     return process_channel_identifier(channel_identifier)
 
 def get_channel_property(channel_identifier, property_name):
+    """
+    Get the ChannelMetadata property according to property_name argument
+
+    :param channel_identifier: str
+    :param property_name: str
+    :return: str or boolean
+    """
     if isinstance(property_name, str):
         try:
             if property_name == 'channel_id':
--- kolibri/content/management/commands/populate_contentdb.py
+++ kolibri/content/management/commands/populate_contentdb.py
@@ -1,13 +1,12 @@
 '''
-the data structure of this dummy database
+the data structure
      root
     /    \
    c1     c2
         /  |  \
     c2c1 c2c2 c2c3
 
-to collect data for fixture, run the commandline $ kolibri manage dumpdata content
-run manage.py flush to clean the database afterwards
+to run the command: <kolibri manage populate_contentdb>
 '''
 import uuid
 from django.core.management.base import BaseCommand
@@ -16,12 +15,15 @@ from django.core.files import File as DjFile
 
 
 class Command(BaseCommand):
-    help = ' To collect data for fixture, run the commandline $ kolibri manage dumpdata content'
+    help = ' To collect data for fixture, run the commandline <kolibri manage dumpdata content>. You may want to run <kolibri manage flush> to clean the database after you have created the fixture.'
 
     def populate_content_db(self):
+        """
+        create artifical objects in the main database for testig
+        """
         db_name = "default"
 
-        '''populate MimeType'''
+        # populate MimeType
         video_L = models.MimeType.objects.using(db_name).create(readable_name = 'video_lowres', machine_name = 'Wall-E')
         video_N = models.MimeType.objects.using(db_name).create(readable_name = 'video_normal', machine_name = 'Eva')
         video_H = models.MimeType.objects.using(db_name).create(readable_name = 'video_high', machine_name = 'Doraemon')
@@ -35,13 +37,13 @@ class Command(BaseCommand):
         ex_N = models.MimeType.objects.using(db_name).create(readable_name = 'exercise_normal', machine_name = 'Terminator')
         ex_H = models.MimeType.objects.using(db_name).create(readable_name = 'exercise_highres', machine_name = 'Ed-209')
 
-        '''populate License'''
+        # populate License
         MIT = models.License.objects.using(db_name).create(license_name = 'MIT')
         WTFPL = models.License.objects.using(db_name).create(license_name = 'WTFPL')
         GNU = models.License.objects.using(db_name).create(license_name = 'GNU')
         CC = models.License.objects.using(db_name).create(license_name = 'CC')
 
-        '''populate ContentMetadata'''
+        # populate ContentMetadata
         root = models.ContentMetadata.objects.using(db_name).create(title='root', parent=None, license=WTFPL, kind='topic', description='balbla1', slug='slut_1', total_file_size=21, available=False, content_id=uuid.uuid4())
         c1 = models.ContentMetadata.objects.using(db_name).create(title='c1', parent=root, license=WTFPL, kind='video', description='balbla2', slug='slut_2', total_file_size=22, available=False, content_id=uuid.uuid4())
         c2 = models.ContentMetadata.objects.using(db_name).create(title='c2', parent=root, license=GNU, kind='topic', description='balbla3', slug='slut_3', total_file_size=23, available=False, content_id=uuid.uuid4())
@@ -49,18 +51,18 @@ class Command(BaseCommand):
         c2c2 = models.ContentMetadata.objects.using(db_name).create(title='c2c2', parent=c2, license=GNU, kind='topic', description='balbla5', slug='slut_5', total_file_size=23, available=False, content_id=uuid.uuid4())
         c2c3 = models.ContentMetadata.objects.using(db_name).create(title='c2c3', parent=c2, license=GNU, kind='topic', description='balbla5', slug='slut_6', total_file_size=24, available=False, content_id=uuid.uuid4())
 
-        '''populate Format'''
+        # populate Format
         format_1 = models.Format.objects.using(db_name).create(contentmetadata=c1, mimetype=video_H, available=False, format_size=102, quality="high")
         format_2 = models.Format.objects.using(db_name).create(contentmetadata=c1, mimetype=video_L, available=False, format_size=51, quality="low")
         format_3 = models.Format.objects.using(db_name).create(contentmetadata=c2c1, mimetype=ex_L, available=False, format_size=46, quality="high")
 
-        '''populate File'''
+        # populate File
         file_0 = models.File.objects.using(db_name).create(format=format_1)
         file_1 = models.File.objects.using(db_name).create(format=format_2)
         file_2 = models.File.objects.using(db_name).create(format=format_3)
         file_3 = models.File.objects.using(db_name).create(format=format_3)
 
-        '''populate Content Relationship'''
+        # populate Content Relationship
         cr1 = models.PrerequisiteContentRelationship.objects.using(db_name).create(relationship_type='prerequisite', contentmetadata_1=root, contentmetadata_2=c1)
         cr2 = models.RelatedContentRelationship.objects.using(db_name).create(relationship_type='related', contentmetadata_1=c1, contentmetadata_2=c2)
 
