9e358ca662e0eecc2abf007bdf657247e5517145
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-01 16:59:52 -0800

    Add proxy model queryset tests

--- kolibri/auth/test/test_users.py
+++ kolibri/auth/test/test_users.py
@@ -6,6 +6,45 @@ from django.test import TestCase
 # way to get the proxy models as well. So just import at runtime.
 
 
+class UserProxyManagerTestCase(TestCase):
+    """
+    Checks that the BaseUser proxy models' default manager returns appropriate instances.
+    """
+    def setUp(self):
+        from kolibri.auth.models import FacilityUser, DeviceOwner
+        self.user = FacilityUser.objects.create(username="mike", first_name="Mike", last_name="Gallaspy")
+        self.user = FacilityUser.objects.create(username="brian", first_name="Brian", last_name="Gallaspy")
+        self.fu_usernames = ["mike", "brian"]
+        self.do = DeviceOwner.objects.create(username="bar")
+        self.do_usernames = ["bar"]
+
+    def test_facility_user_manager_returns_facility_users(self):
+        from kolibri.auth.models import FacilityUser, BaseUser
+        base_users = BaseUser.objects.filter(username__in=self.fu_usernames)
+        facility_users = FacilityUser.objects.all()
+        self.assertEqual(len(base_users), len(facility_users))
+
+    def test_fu_set_correct(self):
+        """ continuation of test_facility_user_manager_returns_facility_users """
+        from kolibri.auth.models import FacilityUser, BaseUser
+        base_users = BaseUser.objects.filter(username__in=self.fu_usernames)
+        facility_users = FacilityUser.objects.all()
+        self.assertListEqual(sorted([u.id for u in base_users]), sorted([u.id for u in facility_users]))
+
+    def test_device_owner_manager_returns_device_owners(self):
+        from kolibri.auth.models import DeviceOwner, BaseUser
+        base_users = BaseUser.objects.filter(username__in=self.do_usernames)
+        device_owners = DeviceOwner.objects.all()
+        self.assertEqual(len(base_users), len(device_owners))
+
+    def test_do_set_correct(self):
+        """ continuation of test_device_owner_manager_returns_device_owners """
+        from kolibri.auth.models import DeviceOwner, BaseUser
+        base_users = BaseUser.objects.filter(username__in=self.do_usernames)
+        device_owners = DeviceOwner.objects.all()
+        self.assertListEqual(sorted([u.id for u in base_users]), sorted([u.id for u in device_owners]))
+
+
 class UserSanityTestCase(TestCase):
     """
     Sanity checks basic functionality of user models.
