26ffd1c9c3f36a1ddcd11119222abe7e46dd3ad2
66eli77 <66eli77@gmail.com>
2016-02-03 10:15:20 -0800

    reformat the code to comply with flake8

--- kolibri/content/management/commands/populate_contentdb.py
+++ kolibri/content/management/commands/populate_contentdb.py
@@ -9,13 +9,14 @@ the data structure
 to run the command: <kolibri manage populate_contentdb>
 '''
 import uuid
+
 from django.core.management.base import BaseCommand
 from kolibri.content import models
-from django.core.files import File as DjFile
 
 
 class Command(BaseCommand):
-    help = ' To collect data for fixture, run the commandline <kolibri manage dumpdata content>. You may want to run <kolibri manage flush> to clean the database after you have created the fixture.'
+    help = ' To collect data for fixture, run the commandline <kolibri manage dumpdata content>.'
+    +' You may want to run <kolibri manage flush> to clean the database after you have created the fixture.'
 
     def populate_content_db(self):
         """
@@ -24,32 +25,44 @@ class Command(BaseCommand):
         db_name = "default"
 
         # populate MimeType
-        video_L = models.MimeType.objects.using(db_name).create(readable_name = 'video_lowres', machine_name = 'Wall-E')
-        video_N = models.MimeType.objects.using(db_name).create(readable_name = 'video_normal', machine_name = 'Eva')
-        video_H = models.MimeType.objects.using(db_name).create(readable_name = 'video_high', machine_name = 'Doraemon')
-        audio_L = models.MimeType.objects.using(db_name).create(readable_name = 'audio_lowres', machine_name = 'Asimo')
-        audio_N = models.MimeType.objects.using(db_name).create(readable_name = 'audio_normal', machine_name = 'Roomba')
-        audio_H = models.MimeType.objects.using(db_name).create(readable_name = 'audio_high', machine_name = 'Astro')
-        image_L = models.MimeType.objects.using(db_name).create(readable_name = 'image_lowres', machine_name = 'Hal-2000')
-        image_N = models.MimeType.objects.using(db_name).create(readable_name = 'image_normal', machine_name = 'Hal-2000')
-        image_H = models.MimeType.objects.using(db_name).create(readable_name = 'image_highres', machine_name = 'Android')
-        ex_L = models.MimeType.objects.using(db_name).create(readable_name = 'exercise_lowres', machine_name = 'Optimus')
-        ex_N = models.MimeType.objects.using(db_name).create(readable_name = 'exercise_normal', machine_name = 'Terminator')
-        ex_H = models.MimeType.objects.using(db_name).create(readable_name = 'exercise_highres', machine_name = 'Ed-209')
+        video_L = models.MimeType.objects.using(db_name).create(readable_name='video_lowres', machine_name='Wall-E')
+        video_H = models.MimeType.objects.using(db_name).create(readable_name='video_high', machine_name='Doraemon')
+        ex_L = models.MimeType.objects.using(db_name).create(readable_name='exercise_lowres', machine_name='Optimus')
+        models.MimeType.objects.using(db_name).create(readable_name='audio_lowres', machine_name='Asimo')
+        models.MimeType.objects.using(db_name).create(readable_name='audio_normal', machine_name='Roomba')
+        models.MimeType.objects.using(db_name).create(readable_name='audio_high', machine_name='Astro')
+        models.MimeType.objects.using(db_name).create(readable_name='image_lowres', machine_name='Hal-2000')
+        models.MimeType.objects.using(db_name).create(readable_name='image_normal', machine_name='Hal-2000')
+        models.MimeType.objects.using(db_name).create(readable_name='image_highres', machine_name='Android')
+        models.MimeType.objects.using(db_name).create(readable_name='video_normal', machine_name='Eva')
+        models.MimeType.objects.using(db_name).create(readable_name='exercise_normal', machine_name='Terminator')
+        models.MimeType.objects.using(db_name).create(readable_name='exercise_highres', machine_name='Ed-209')
 
         # populate License
-        MIT = models.License.objects.using(db_name).create(license_name = 'MIT')
-        WTFPL = models.License.objects.using(db_name).create(license_name = 'WTFPL')
-        GNU = models.License.objects.using(db_name).create(license_name = 'GNU')
-        CC = models.License.objects.using(db_name).create(license_name = 'CC')
+        WTFPL = models.License.objects.using(db_name).create(license_name='WTFPL')
+        GNU = models.License.objects.using(db_name).create(license_name='GNU')
+        CC = models.License.objects.using(db_name).create(license_name='CC')
+        models.License.objects.using(db_name).create(license_name='MIT')
 
         # populate ContentMetadata
-        root = models.ContentMetadata.objects.using(db_name).create(title='root', parent=None, license=WTFPL, kind='topic', description='balbla1', slug='slut_1', total_file_size=21, available=False, content_id=uuid.uuid4())
-        c1 = models.ContentMetadata.objects.using(db_name).create(title='c1', parent=root, license=WTFPL, kind='video', description='balbla2', slug='slut_2', total_file_size=22, available=False, content_id=uuid.uuid4())
-        c2 = models.ContentMetadata.objects.using(db_name).create(title='c2', parent=root, license=GNU, kind='topic', description='balbla3', slug='slut_3', total_file_size=23, available=False, content_id=uuid.uuid4())
-        c2c1 = models.ContentMetadata.objects.using(db_name).create(title='c2c1', parent=c2, license=CC, kind='exercise', description='balbla4', slug='slut_4', total_file_size=23, available=False, content_id=uuid.uuid4())
-        c2c2 = models.ContentMetadata.objects.using(db_name).create(title='c2c2', parent=c2, license=GNU, kind='topic', description='balbla5', slug='slut_5', total_file_size=23, available=False, content_id=uuid.uuid4())
-        c2c3 = models.ContentMetadata.objects.using(db_name).create(title='c2c3', parent=c2, license=GNU, kind='topic', description='balbla5', slug='slut_6', total_file_size=24, available=False, content_id=uuid.uuid4())
+        root = models.ContentMetadata.objects.using(db_name).create(
+            title='root', parent=None, license=WTFPL, kind='topic',
+            description='balbla1', slug='slut_1', total_file_size=21, available=False, content_id=uuid.uuid4())
+        c1 = models.ContentMetadata.objects.using(db_name).create(
+            title='c1', parent=root, license=WTFPL, kind='video',
+            description='balbla2', slug='slut_2', total_file_size=22, available=False, content_id=uuid.uuid4())
+        c2 = models.ContentMetadata.objects.using(db_name).create(
+            title='c2', parent=root, license=GNU, kind='topic',
+            description='balbla3', slug='slut_3', total_file_size=23, available=False, content_id=uuid.uuid4())
+        c2c1 = models.ContentMetadata.objects.using(db_name).create(
+            title='c2c1', parent=c2, license=CC, kind='exercise',
+            description='balbla4', slug='slut_4', total_file_size=23, available=False, content_id=uuid.uuid4())
+        models.ContentMetadata.objects.using(db_name).create(
+            title='c2c2', parent=c2, license=GNU, kind='topic',
+            description='balbla5', slug='slut_5', total_file_size=23, available=False, content_id=uuid.uuid4())
+        models.ContentMetadata.objects.using(db_name).create(
+            title='c2c3', parent=c2, license=GNU, kind='topic',
+            description='balbla5', slug='slut_6', total_file_size=24, available=False, content_id=uuid.uuid4())
 
         # populate Format
         format_1 = models.Format.objects.using(db_name).create(contentmetadata=c1, mimetype=video_H, available=False, format_size=102, quality="high")
