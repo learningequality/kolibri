fa4b3784bc10f920db22dca7a356eb65d64d9762
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-03 18:09:40 -0800

    Add Classroom.delete tests & fix goofy imports
    Previously I had thought I encounterd a circular import error,
    but then I realized that tox wasn't properly setting up Django
    with a DJANGO_SETTINGS_MODULE. Fixed that in a separate commit,
    then rebased this branch and changed the imports.

--- kolibri/auth/test/test_backend.py
+++ kolibri/auth/test/test_backend.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 
-# Importing user models and backend models here results in a circular import... you should use get_user_model, but
-# then there's no way to get the proxy models as well. So just import at runtime.
+from kolibri.auth.models import FacilityUser, DeviceOwner
+from kolibri.auth.backends import DeviceBackend, FacilityBackend
 
 
 class DeviceBackendTestCase(TestCase):
     def setUp(self):
-        from kolibri.auth.models import FacilityUser, DeviceOwner
         user = self.user = FacilityUser(username="Mike")
         user.set_password("foo")
         user.save()
@@ -16,30 +15,24 @@ class DeviceBackendTestCase(TestCase):
         do.save()
 
     def test_facility_user_not_authenticated(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertIsNone(DeviceBackend().authenticate(username="Mike", password="foo"))
 
     def test_device_owner_authenticated(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertEqual(self.do, DeviceBackend().authenticate(username="Chuck", password="foobar"))
 
     def test_get_facility_user(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertIsNone(DeviceBackend().get_user(self.user.id))
 
     def test_get_device_owner(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertEqual(self.do, DeviceBackend().get_user(self.do.id))
 
     def test_nonexistent_user_returns_none(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertIsNone(DeviceBackend().get_user(4756))
 
     def test_perms_sanity(self):
         """
         DeviceBackend has very simple permissions -- True for DeviceOwners, otherwise False!
         """
-        from kolibri.auth.backends import DeviceBackend
         db = DeviceBackend()
         self.assertTrue(db.has_perm(self.do, "foo"))
         self.assertTrue(db.has_module_perms(self.do, "foo"))
@@ -47,17 +40,14 @@ class DeviceBackendTestCase(TestCase):
         self.assertFalse(db.has_module_perms(self.user, "foo"))
 
     def test_authenticate_nonexistent_user_returns_none(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertIsNone(DeviceBackend().authenticate("foo", "bar"))
 
     def test_authenticate_with_wrong_password_returns_none(self):
-        from kolibri.auth.backends import DeviceBackend
         self.assertIsNone(DeviceBackend().authenticate("Chuck", "goo"))
 
 
 class FacilityBackendTestCase(TestCase):
     def setUp(self):
-        from kolibri.auth.models import FacilityUser, DeviceOwner
         user = self.user = FacilityUser(username="Mike")
         user.set_password("foo")
         user.save()
@@ -67,29 +57,22 @@ class FacilityBackendTestCase(TestCase):
         do.save()
 
     def test_facility_user_authenticated(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertEqual(self.user, FacilityBackend().authenticate(username="Mike", password="foo"))
 
     def test_device_owner_not_authenticated(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertIsNone(FacilityBackend().authenticate(username="Chuck", password="foobar"))
 
     def test_get_facility_user(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertEqual(self.user, FacilityBackend().get_user(self.user.id))
 
     def test_get_device_owner(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertIsNone(FacilityBackend().get_user(self.do.id))
 
     def test_nonexistent_user_returns_none(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertIsNone(FacilityBackend().get_user(4756))
 
     def test_authenticate_nonexistent_user_returns_none(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertIsNone(FacilityBackend().authenticate("foo", "bar"))
 
     def test_authenticate_with_wrong_password_returns_none(self):
-        from kolibri.auth.backends import FacilityBackend
         self.assertIsNone(FacilityBackend().authenticate("Mike", "goo"))
--- kolibri/auth/test/test_collections_and_roles.py
+++ kolibri/auth/test/test_collections_and_roles.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from kolibri.auth import get_user_models, get_hierarchy_models, get_collection_proxies, get_role_proxies
+from kolibri.auth.models import FacilityUser, Facility, Classroom, LearnerGroup, FacilityAdmin, Coach, Learner, \
+    Role, Collection, HierarchyNode
 
 
 class CollectionRemovalTestCase(TestCase):
@@ -8,72 +9,84 @@ class CollectionRemovalTestCase(TestCase):
     Tests that removing Roles and sub-Collections from a Collection does not create orhpans.
     """
     def setUp(self):
-        FacilityAdmin, Coach, Learner = get_role_proxies()
-        Facility, Classroom, LearnerGroup = get_collection_proxies()
-        _, FacilityUser, _ = get_user_models()
-
-        user = self.user = FacilityUser.objects.create(username='foo')
+        user1, user2, user3 = self.user1, self.user2, self.user3 = (
+            FacilityUser.objects.create(username='foo'),
+            FacilityUser.objects.create(username='bar'),
+            FacilityUser.objects.create(username='baz'),
+        )
         self.lg = LearnerGroup.objects.create()
-        self.lg.add_learner(user)
+        self.lg.add_learner(user1)
 
         self.cr = Classroom.objects.create()
-        self.cr.add_coach(user)
+        self.cr.add_coach(user2)
 
         self.f = Facility.objects.create()
-        self.f.add_admin(user)
+        self.f.add_admin(user3)
 
     def test_remove_learner(self):
-        _, _, Learner = get_role_proxies()
         self.assertEqual(Learner.objects.count(), 1)
-        self.lg.remove_learner(self.user)
+        self.lg.remove_learner(self.user1)
         self.assertEqual(Learner.objects.count(), 0)
 
     def test_remove_coach(self):
-        _, Coach, _ = get_role_proxies()
         self.assertEqual(Coach.objects.count(), 1)
-        self.cr.remove_coach(self.user)
+        self.cr.remove_coach(self.user2)
         self.assertEqual(Coach.objects.count(), 0)
 
     def test_remove_admin(self):
-        FacilityAdmin, _, _ = get_role_proxies()
         self.assertEqual(FacilityAdmin.objects.count(), 1)
-        self.f.remove_admin(self.user)
+        self.f.remove_admin(self.user3)
         self.assertEqual(FacilityAdmin.objects.count(), 0)
+
+    def test_delete_learner_group(self):
+        """ Deleting a LearnerGroup should delete its associated Learners as well """
+        self.lg.delete()
+        self.assertEqual(Learner.objects.count(), 0)
+
+    def test_delete_classroom_pt1(self):
+        """ Deleting a Classroom should delete its associated Learners """
+        self.cr.delete()
+        self.assertEqual(Learner.objects.count(), 0)
+
+    def test_delete_classroom_pt2(self):
+        """ Deleting a Classroom should delete its associated LearnerGroups """
+        self.cr.delete()
+        self.assertEqual(LearnerGroup.objects.count(), 0)
+
+    def test_delete_classroom_pt3(self):
+        """ Deleting a Classroom should delete its associated Coaches """
+        self.cr.delete()
+        self.assertEqual(Coach.objects.count(), 0)
 
 
 class CollectionsTestCase(TestCase):
-    def setUp(self):
-        self.Facility, self.Classroom, self.LearnerGroup = get_collection_proxies()
-        self.FacilityAdmin, self.Coach, self.Learner = get_role_proxies()
-        _, self.FacilityUser, _ = get_user_models()
-
     def test_add_admin(self):
-        user = self.FacilityUser.objects.create(username='foo')
-        facility = self.Facility.objects.create()
+        user = FacilityUser.objects.create(username='foo')
+        facility = Facility.objects.create()
         facility.add_admin(user)
-        self.assertEqual(self.FacilityAdmin.objects.filter(user=user).count(), 1)
+        self.assertEqual(FacilityAdmin.objects.filter(user=user).count(), 1)
 
     def test_add_classroom(self):
-        facility = self.Facility.objects.create()
-        facility.add_classroom(self.Classroom.objects.create())
-        self.assertEqual(self.Classroom.objects.count(), 1)
+        facility = Facility.objects.create()
+        facility.add_classroom(Classroom.objects.create())
+        self.assertEqual(Classroom.objects.count(), 1)
 
     def test_add_coach(self):
-        user = self.FacilityUser.objects.create(username='foo')
-        classroom = self.Classroom.objects.create()
+        user = FacilityUser.objects.create(username='foo')
+        classroom = Classroom.objects.create()
         classroom.add_coach(user)
-        self.assertEqual(self.Coach.objects.filter(user=user).count(), 1)
+        self.assertEqual(Coach.objects.filter(user=user).count(), 1)
 
     def test_add_learner_group(self):
-        classroom = self.Classroom.objects.create()
-        classroom.add_learner_group(self.LearnerGroup.objects.create())
-        self.assertEqual(self.LearnerGroup.objects.count(), 1)
+        classroom = Classroom.objects.create()
+        classroom.add_learner_group(LearnerGroup.objects.create())
+        self.assertEqual(LearnerGroup.objects.count(), 1)
 
     def test_learner(self):
-        user = self.FacilityUser.objects.create(username='foo')
-        learner_group = self.LearnerGroup.objects.create()
+        user = FacilityUser.objects.create(username='foo')
+        learner_group = LearnerGroup.objects.create()
         learner_group.add_learner(user)
-        self.assertEqual(self.Learner.objects.filter(user=user).count(), 1)
+        self.assertEqual(Learner.objects.filter(user=user).count(), 1)
 
 
 class HierarchyNodeSanityTestCase(TestCase):
@@ -82,19 +95,17 @@ class HierarchyNodeSanityTestCase(TestCase):
     we separate these tests from the rest.
     """
     def setUp(self):
-        self.Role, self.Collection = get_hierarchy_models()
-        BaseUser, FacilityUser, DeviceOwner = get_user_models()
         self.user = FacilityUser.objects.create(username='mike')
 
     def test_hierarchy_nodes_created_for_role(self):
         from kolibri.auth.models import HierarchyNode
         self.assertEqual(HierarchyNode.objects.count(), 0)
-        self.Role.objects.create(user=self.user, kind='Coach')
+        Role.objects.create(user=self.user, kind='Coach')
         self.assertEqual(HierarchyNode.objects.count(), 1)
 
     def test_hierarchy_nodes_not_orphaned_when_role_deleted(self):
         from kolibri.auth.models import HierarchyNode
-        role = self.Role.objects.create(user=self.user, kind='Coach')
+        role = Role.objects.create(user=self.user, kind='Coach')
         self.assertEqual(HierarchyNode.objects.count(), 1)
         role.delete()
         self.assertEqual(HierarchyNode.objects.count(), 0)
@@ -102,12 +113,12 @@ class HierarchyNodeSanityTestCase(TestCase):
     def test_hierarchy_nodes_created_for_collection(self):
         from kolibri.auth.models import HierarchyNode
         self.assertEqual(HierarchyNode.objects.count(), 0)
-        self.Collection.objects.create(kind='Facility')
+        Collection.objects.create(kind='Facility')
         self.assertEqual(HierarchyNode.objects.count(), 1)
 
     def test_hierarchy_nodes_not_orphaned_when_collection_deleted(self):
         from kolibri.auth.models import HierarchyNode
-        coll = self.Collection.objects.create(kind='Facility')
+        coll = Collection.objects.create(kind='Facility')
         self.assertEqual(HierarchyNode.objects.count(), 1)
         coll.delete()
         self.assertEqual(HierarchyNode.objects.count(), 0)
@@ -123,7 +134,6 @@ class HierarchyNodeStructureTestCase(TestCase):
     Collection 2  |  Role 4
     """
     def setUp(self):
-        from kolibri.auth.models import HierarchyNode
         collections = self.collections = [HierarchyNode.objects.create(kind='Collection') for _ in range(0, 3)]
         roles = self.roles = [HierarchyNode.objects.create(kind='Role') for _ in range(0, 5)]
         roles[0].parent = roles[1].parent = collections[1].parent = collections[2].parent = collections[0]
@@ -133,7 +143,6 @@ class HierarchyNodeStructureTestCase(TestCase):
             n.save()
 
     def test_assert_setup_sanity(self):
-        from kolibri.auth.models import HierarchyNode
         self.assertEqual(self.collections[0].children.count(), 4)
         self.assertEqual(self.collections[0].get_descendants().count(), 7)
         self.assertEqual(HierarchyNode.objects.all().count(), 8)
@@ -141,13 +150,11 @@ class HierarchyNodeStructureTestCase(TestCase):
         self.assertEqual(HierarchyNode.objects.filter(kind='Collection').count(), 3)
 
     def test_insert_collection_node(self):
-        from kolibri.auth.models import HierarchyNode
         tracer_node = HierarchyNode.objects.create(kind='Tracer')
         self.collections[0].insert_collection_node(tracer_node)
         self.assertEqual(self.collections[0].children.filter(kind='Tracer').count(), 1)
 
     def test_insert_role_node(self):
-        from kolibri.auth.models import HierarchyNode
         tracer_node = HierarchyNode.objects.create(kind='Tracer')
         self.collections[0].insert_role_node(tracer_node)
         self.assertEqual(self.collections[0].children.filter(kind='Tracer').count(), 1)
