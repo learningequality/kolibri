41ba2053eaa6691c19d3e04775b0ecbb207b5d87
Richard Tibbles <richard.tibbles@gmail.com>
2016-02-11 00:28:51 -0800

    Update webpack config to build webpack stats into each plugin folder.

--- webpack.config.js
+++ webpack.config.js
@@ -16,25 +16,25 @@ var parseBundleIni = function(iniFile) {
     var data = iniParser.parse(fs.readFileSync(iniFile, 'utf-8'));
     var bundle_data = {};
     for (key in data) {
-        logging.info(path.join(path.dirname(iniFile), "webpack-stats.json"));
         if (typeof data[key]["entry_file"] !== "undefined") {
             bundle_data[key] = path.join(path.dirname(iniFile), data[key]["entry_file"]);
-
         } else {
             logging.error(iniFile + " file is misconfigured, missing 'entry_file' parameter for bundle " + key);
         }
     }
+
+    var module_name = path.relative(__dirname, path.dirname(iniFile)).replace(/\//g, ".");
 
     if (Object.keys(bundle_data).length > 0) {
         bundles.push({
             context: __dirname,
             entry: bundle_data,
             output: {
-                path: path.join(path.dirname(iniFile), "static"),
+                path: path.join(path.dirname(iniFile), "static", module_name),
                 filename: "[name]-[hash].js"
             },
             plugins: [
-                new BundleTracker({path: path.dirname(iniFile), filename: "webpack-stats.json"})
+                new BundleTracker({path: path.dirname(iniFile), filename: "bundle_catalog.json"})
             ]
         });
     }
