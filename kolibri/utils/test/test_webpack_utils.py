85cb60d9d304fd3a2ef900fa59b50b441cc05985
Richard Tibbles <richard.tibbles@gmail.com>
2016-02-12 22:18:39 -0800

    Add mock to test requirements. Add tests for all webpack util functions.

--- kolibri/utils/test/test_webpack_utils.py
+++ kolibri/utils/test/test_webpack_utils.py
@@ -4,7 +4,11 @@ from django.test import TestCase
 
 from kolibri.utils import webpack
 
+from mock import patch
+
 class WebpackConfigTestCase(TestCase):
+
+    files = [{"name": "this.css"}]
 
     def test_get_actual_plugin(self):
         webpack.PLUGIN_CACHE = {"test": {}}
@@ -15,3 +19,19 @@ class WebpackConfigTestCase(TestCase):
         webpack.PLUGIN_CACHE = {}
         webpack.__initialized = True
         self.assertRaises(webpack.NoFrontEndPlugin, webpack.get_plugin, "test")
+
+    @patch('kolibri.utils.webpack.get_plugin', return_value="test")
+    @patch('kolibri.utils.webpack.get_bundle', return_value=files)
+    def test_get_webpack_bundle(self, mocked_get_bundle, mocked_get_plugin):
+        webpack.PLUGIN_CACHE = {}
+        webpack.__initialized = True
+        output = webpack.get_webpack_bundle("test", None, "test")
+        self.assertEqual(output[0]["name"], "this.css")
+
+    @patch('kolibri.utils.webpack.get_plugin', return_value="test")
+    @patch('kolibri.utils.webpack.get_bundle', return_value=files)
+    def test_get_webpack_bundle_filter(self, mocked_get_bundle, mocked_get_plugin):
+        webpack.PLUGIN_CACHE = {}
+        webpack.__initialized = True
+        output = webpack.get_webpack_bundle("test", "js", "test")
+        self.assertEqual(len(list(output)), 0)
