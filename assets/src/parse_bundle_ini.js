929caa37bc0d19475b2744236517d71ff3501771
Richard Tibbles <richard.tibbles@gmail.com>
2016-02-17 10:08:23 -0800

    Makes kolibri_core object accessible on global namespace for plugins to register against.

--- assets/src/parse_bundle_ini.js
+++ assets/src/parse_bundle_ini.js
@@ -6,6 +6,7 @@ var logging = require('./logging');
 
 var parseBundleIni = function(iniFile, bundles, base_dir, externals) {
     var data = iniParser.parse(fs.readFileSync(iniFile, 'utf-8'));
+    var module_name = path.relative(base_dir, path.dirname(iniFile)).replace(/\//g, ".");
     var bundle_data = {};
     var library;
     for (var key in data) {
@@ -21,6 +22,25 @@ var parseBundleIni = function(iniFile, bundles, base_dir, externals) {
                         logging.warn(iniFile + " file is misconfigured, duplicate key " + key + " set as external module");
                     }
                 }
+                bundles.push({
+                    context: base_dir,
+                    entry: bundle_data,
+                    output: {
+                        path: path.relative(base_dir, path.join(path.dirname(iniFile), "static", module_name)),
+                        filename: "[name]-[hash].js",
+                        publicPath: path.join(module_name, "/"),
+                        library: key
+                    },
+                    plugins: [
+                        new BundleTracker({
+                            path: path.dirname(iniFile),
+                            filename: "bundle_catalog.json"
+                        })
+                    ],
+                    resolve: {
+                        root: base_dir
+                    }
+                });
             } else {
                 logging.warn(iniFile + " file is misconfigured, duplicate key " + key + " set for plugins");
             }
@@ -29,28 +49,8 @@ var parseBundleIni = function(iniFile, bundles, base_dir, externals) {
         }
     }
 
-    var module_name = path.relative(base_dir, path.dirname(iniFile)).replace(/\//g, ".");
-
     if (Object.keys(bundle_data).length > 0) {
-        bundles.push({
-            context: base_dir,
-            entry: bundle_data,
-            output: {
-                path: path.relative(base_dir, path.join(path.dirname(iniFile), "static", module_name)),
-                filename: "[name]-[hash].js",
-                publicPath: path.join(module_name, "/"),
-                library: library
-            },
-            plugins: [
-                new BundleTracker({
-                    path: path.dirname(iniFile),
-                    filename: "bundle_catalog.json"
-                })
-            ],
-            resolve: {
-                root: base_dir
-            }
-        });
+
     }
 
     return [bundles, externals];
