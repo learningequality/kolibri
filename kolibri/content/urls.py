25d0b282d6be5d904ef811cc77f1d726fbd709ec
66eli77 <66eli77@gmail.com>
2016-03-03 17:18:26 -0800

    add endpoints. they all start with 'content_api/'

--- kolibri/content/urls.py
+++ kolibri/content/urls.py
@@ -3,8 +3,9 @@
 Most of the api endpoints here use django_rest_framework to expose the content app APIs,
 except some set methods that do not return anything.
 """
+from django.conf.urls import include, url
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import routers, viewsets
 
 from . import api, models, serializers
 
@@ -124,3 +125,24 @@ def set_prerequisite_view(request, channel_id=None, content1=None, content2=None
 def set_is_related_view(request, channel_id=None, content1=None, content2=None):
     api.set_is_related(channel_id=channel_id, content1=content1, content2=content2)
     return HttpResponse(status=200)
+
+
+router = routers.DefaultRouter()
+router.register(r'get_content_with_id', GetContentWithIdViewSet, base_name="contentmetadata-list")
+router.register(r'get_ancestor_topics', AncestorTopicsViewSet, base_name="contentmetadata-list")
+router.register(r'immediate_children', ImmediateChildrenViewSet, base_name="contentmetadata-list")
+router.register(r'leaves', LeavesViewSet, base_name="contentmetadata-list")
+router.register(r'get_all_formats', GetAllFormatsViewSet, base_name="format-list")
+router.register(r'get_available_formats', GetAvailableFormatsViewSet, base_name="format-list")
+router.register(r'get_possible_formats', GetPossibleFormatsViewSet, base_name="format-list")
+router.register(r'get_files_for_quality', GetFilesForQualityViewSet, base_name="file-list")
+router.register(r'get_missing_files', GetMissingFilesViewSet, base_name="file-list")
+router.register(r'get_all_prerequisites', GetAllPrerequisitesViewSet, base_name="contentmetadata-list")
+router.register(r'get_all_related', GetAllRelatedViewSet, base_name="contentmetadata-list")
+router.register(r'children_of_kind', ChildrenOfKindViewSet, base_name="contentmetadata-list")
+
+urlpatterns = [
+    url(r'^content_api/', include(router.urls)),
+    url(r'^content_api/set_prerequisite/(?P<channel_id>.*)/(?P<content1>.*)/(?P<content2>.*)/$', set_prerequisite_view),
+    url(r'^content_api/set_is_related/(?P<channel_id>.*)/(?P<content1>.*)/(?P<content2>.*)/$', set_is_related_view),
+]
