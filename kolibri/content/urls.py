cba5d0698e034759eb364c89c4fe0a72a4da1dc8
66eli77 <66eli77@gmail.com>
2016-03-03 17:30:08 -0800

    add docstrings

--- kolibri/content/urls.py
+++ kolibri/content/urls.py
@@ -47,82 +47,138 @@ class AbstractFileViewSet(viewsets.ModelViewSet):
 
 
 class GetContentWithIdViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(GetContentWithIdViewSet, self).get_queryset()
         return api.get_content_with_id(self.request.GET['content'], channel_id=self.ch_id)
 
 
 class AncestorTopicsViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(AncestorTopicsViewSet, self).get_queryset()
         return api.get_ancestor_topics(channel_id=self.ch_id, content=self.cn)
 
 
 class ImmediateChildrenViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(ImmediateChildrenViewSet, self).get_queryset()
         return api.immediate_children(channel_id=self.ch_id, content=self.cn)
 
 
 class LeavesViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(LeavesViewSet, self).get_queryset()
         return api.leaves(channel_id=self.ch_id, content=self.cn)
 
 
 class GetAllFormatsViewSet(AbstractFormatViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of Format objects
+    """
     def get_queryset(self):
         super(GetAllFormatsViewSet, self).get_queryset()
         return api.get_all_formats(channel_id=self.ch_id, content=self.cn)
 
 
 class GetAvailableFormatsViewSet(AbstractFormatViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of Format objects
+    """
     def get_queryset(self):
         super(GetAvailableFormatsViewSet, self).get_queryset()
         return api.get_available_formats(channel_id=self.ch_id, content=self.cn)
 
 
 class GetPossibleFormatsViewSet(AbstractFormatViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of Format objects
+    """
     def get_queryset(self):
         super(GetPossibleFormatsViewSet, self).get_queryset()
         return api.get_possible_formats(channel_id=self.ch_id, content=self.cn)
 
 
 class GetFilesForQualityViewSet(AbstractFileViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>, format_quality: <string>}
+    :return: QuerySet of File obejcts
+    """
     def get_queryset(self):
         super(GetFilesForQualityViewSet, self).get_queryset()
         return api.get_files_for_quality(channel_id=self.ch_id, content=self.cn, format_quality=self.request.GET['format_quality'])
 
 
 class GetMissingFilesViewSet(AbstractFileViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of File objects
+    """
     def get_queryset(self):
         super(GetMissingFilesViewSet, self).get_queryset()
         return api.get_missing_files(channel_id=self.ch_id, content=self.cn)
 
 
 class GetAllPrerequisitesViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(GetAllPrerequisitesViewSet, self).get_queryset()
         return api.get_all_prerequisites(channel_id=self.ch_id, content=self.cn)
 
 
 class GetAllRelatedViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(GetAllRelatedViewSet, self).get_queryset()
         return api.get_all_related(channel_id=self.ch_id, content=self.cn)
 
 
 class ChildrenOfKindViewSet(AbstractContentViewSet):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content: <UUID string>, kind: <string>}
+    :return: QuerySet of ContentMetadata objects
+    """
     def get_queryset(self):
         super(ChildrenOfKindViewSet, self).get_queryset()
         return api.children_of_kind(channel_id=self.ch_id, content=self.cn, kind=self.request.GET['kind'])
 
 
 def set_prerequisite_view(request, channel_id=None, content1=None, content2=None):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content1: <UUID string>, content2: <UUID string>}
+    :return: html status
+    """
     api.set_prerequisite(channel_id=channel_id, content1=content1, content2=content2)
     return HttpResponse(status=200)
 
 def set_is_related_view(request, channel_id=None, content1=None, content2=None):
+    """
+    front-end call need to supply data: {channel_id: <UUID string>, content1: <UUID string>, content2: <UUID string>}
+    :return: html status
+    """
     api.set_is_related(channel_id=channel_id, content1=content1, content2=content2)
     return HttpResponse(status=200)
 
