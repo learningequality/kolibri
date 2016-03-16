ca91ec42f49603d17c137809e2159ba22ed664ce
66eli77 <66eli77@gmail.com>
2016-03-16 16:44:53 -0700

    add url endpoint for accessing all the contents under each channel

--- kolibri/content/serializers.py
+++ kolibri/content/serializers.py
@@ -4,9 +4,12 @@ from rest_framework import serializers
 
 class ChannelMetadataSerializer(serializers.HyperlinkedModelSerializer):
 
+    contentmetadatas = serializers.HyperlinkedIdentityField(
+        lookup_field='channel_id', view_name='contentmetadata-list', lookup_url_kwarg='channelmetadata_channel_id')
+
     class Meta:
         model = ChannelMetadata
-        fields = ('url', 'channel_id', 'name', 'description', 'author', 'theme', 'subscribed')
+        fields = ('url', 'channel_id', 'name', 'description', 'author', 'theme', 'subscribed', 'contentmetadatas')
         extra_kwargs = {
             'url': {'lookup_field': 'channel_id', 'view_name': 'channelmetadata-detail'}
         }
