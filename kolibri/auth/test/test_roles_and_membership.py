e53fb332bfe117b8a1c24bfe9a0f133839009e63
Jamie Alexandre <jamalex@gmail.com>
2016-03-21 09:06:11 -0700

    Rename remaining "relations" to "roles".

--- kolibri/auth/test/test_roles_and_membership.py
+++ kolibri/auth/test/test_roles_and_membership.py
@@ -1,5 +1,5 @@
 """
-Tests of role and relation calculations.
+Tests of role and membership calculations.
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
@@ -24,31 +24,31 @@ class RolesWithinFacilityTestCase(TestCase):
     def setUp(self):
         self.data = create_dummy_facility_data()
 
-    def test_admin_has_admin_relation_to_own_facility(self):
+    def test_admin_has_admin_role_for_own_facility(self):
         admin = self.data["facility_admin"]
         facility = self.data["facility"]
         self.assertTrue(admin.has_role(role_kinds.ADMIN, facility))
         self.assertIn(role_kinds.ADMIN, admin.get_roles_for(facility))
 
-    def test_coach_has_coach_relation_to_own_classroom(self):
+    def test_coach_has_coach_role_for_own_classroom(self):
         coach0 = self.data["classroom_coaches"][0]
         classroom0 = self.data["classrooms"][0]
         self.assertTrue(coach0.has_role(role_kinds.COACH, classroom0))
         self.assertIn(role_kinds.COACH, coach0.get_roles_for(classroom0))
 
-    def test_coach_has_no_coach_relation_to_other_classroom(self):
+    def test_coach_has_no_coach_role_for_other_classroom(self):
         coach0 = self.data["classroom_coaches"][0]
         classroom1 = self.data["classrooms"][1]
         self.assertFalse(coach0.has_role(role_kinds.COACH, classroom1))
         self.assertNotIn(role_kinds.COACH, coach0.get_roles_for(classroom1))
 
-    def test_coach_has_coach_relation_to_learner_from_own_classroom(self):
+    def test_coach_has_coach_role_for_learner_from_own_classroom(self):
         coach0 = self.data["classroom_coaches"][0]
         learner0 = self.data["learners_one_group"][0][0][0]
         self.assertTrue(coach0.has_role(role_kinds.COACH, learner0))
         self.assertIn(role_kinds.COACH, coach0.get_roles_for(learner0))
 
-    def test_coach_has_no_coach_relation_to_learner_from_other_classroom(self):
+    def test_coach_has_no_coach_role_for_learner_from_other_classroom(self):
         coach0 = self.data["classroom_coaches"][0]
         learner1 = self.data["learners_one_group"][1][0][0]
         self.assertFalse(coach0.has_role(role_kinds.COACH, learner1))
@@ -61,14 +61,14 @@ class RolesAcrossFacilitiesTestCase(TestCase):
         self.data1 = create_dummy_facility_data()
         self.data2 = create_dummy_facility_data()
 
-    def test_no_relations_between_users_across_facilities(self):
+    def test_no_roles_between_users_across_facilities(self):
         users1 = self.data1["all_users"]
         users2 = self.data2["all_users"]
         for user1 in users1:
             for user2 in users2:
                 self.assertEqual(len(user1.get_roles_for(user2)), 0)
 
-    def test_no_relations_to_collections_across_facilities(self):
+    def test_no_roles_for_collections_across_facilities(self):
         users1 = self.data1["classroom_coaches"] + [self.data1["facility_admin"]] + self.data1["facility"].get_members()
         collections2 = [self.data2["facility"]] + self.data2["classrooms"] + flatten(self.data2["learnergroups"])
         for user1 in users1:
@@ -141,11 +141,11 @@ class DeviceOwnerRolesTestCase(TestCase):
         self.device_owner = DeviceOwner.objects.create(username="blooh", password="#")
         self.device_owner2 = DeviceOwner.objects.create(username="blaah", password="#")
 
-    def test_device_owner_has_admin_relation_for_everyone(self):
+    def test_device_owner_has_admin_role_for_everyone(self):
         for user in self.data["all_users"]:
             self.assertTrue(self.device_owner.has_role(role_kinds.ADMIN, user))
 
-    def test_device_owner_has_admin_relation_for_all_collections(self):
+    def test_device_owner_has_admin_role_for_all_collections(self):
         for coll in self.data["all_collections"]:
             self.assertTrue(self.device_owner.has_role(role_kinds.ADMIN, coll))
 
@@ -153,8 +153,8 @@ class DeviceOwnerRolesTestCase(TestCase):
         for user in self.data["all_users"]:
             self.assertEqual(len(user.get_roles_for(self.device_owner)), 0)
 
-    def test_device_owner_has_admin_relation_to_itself(self):
+    def test_device_owner_has_admin_role_for_itself(self):
         self.assertTrue(self.device_owner.has_role(role_kinds.ADMIN, self.device_owner))
 
-    def test_device_owner_has_admin_relation_to_other_device_owner(self):
+    def test_device_owner_has_admin_role_for_other_device_owner(self):
         self.assertTrue(self.device_owner.has_role(role_kinds.ADMIN, self.device_owner2))
