7c2766d67dfa128b9a6ded80fc060cd095ed2e9a
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-04-06 19:07:00 -0700

    Stub out TextInputWithTagDisplay view
    But there's a bug... Marionette.Deferred is undefined.

--- kolibri/core/assets/src/components_demo.js
+++ kolibri/core/assets/src/components_demo.js
@@ -2,10 +2,18 @@
 var components = require('components');
 var logging = require('loglevel');
 var KolibriModule = require('kolibri_module');
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+var Mn = require('backbone.marionette');
+var _ = require('lodash');
 
 logging.setDefaultLevel(2);
 
 logging.info('Component demo loaded!');
+
+
+var app = new Mn.Application();
 
 
 var ComponentDemoPlugin = KolibriModule.extend({
@@ -16,9 +24,31 @@ var ComponentDemoPlugin = KolibriModule.extend({
 
     initialize: function() {
         logging.info('Demo initialized!');
-    },
+        app.start();
+    }
 });
 
+
+var TextInputWithTagDisplay = Mn.LayoutView.extend({
+    template: function(serialized_model) {
+        return _.template(this.template_html)({});
+    },
+
+    template_html: '<div>Foo!</div>'
+});
+
+
+app.on('start', function(){
+    app.addRegions({
+        content: '#content'
+    });
+
+    var tiwtd = new TextInputWithTagDisplay();
+    app.getRegion('content').show(tiwtd);
+});
+
+
 var cpd = new ComponentDemoPlugin();
+
 
 module.exports = {};
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,13 +13,14 @@ var config = {
         loaders: [
             // This is required in order to prevent Backbone from importing jQuery.
             // See here for more details: https://github.com/jashkenas/backbone/wiki/Using-Backbone-without-jQuery
-            { test: /backbone\.js$/, loader: 'imports?define=>false' },
+            //{ test: /backbone\.js$/, loader: 'imports?define=>false' },
             { test: /fg-loadcss\/src\/onloadCSS/, loader: 'exports?onloadCSS'}
         ]
     },
     plugins: [
         // This is required in order to prevent Backbone from importing jQuery.
-        new webpack.IgnorePlugin(/^jquery$/)
+        // FIXME: But Marionette requires Backbone.$.Deferred after all... :-(
+        // new webpack.IgnorePlugin(/^jquery$/)
     ],
     resolve: {
         // This allows us to specify some 'shortcuts' to avoid typing out very long relative paths for frequently
