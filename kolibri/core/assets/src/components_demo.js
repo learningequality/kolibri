9a9f2fb5cdeae11e1f2318bf83863eaa47488682
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-04-08 16:26:14 -0700

    Annotate component_demos

--- kolibri/core/assets/src/components_demo.js
+++ kolibri/core/assets/src/components_demo.js
@@ -1,7 +1,23 @@
 'use strict';
+/*
+What is this?
+-------------
+
+A proof-of-concept Kolibri GUI widget gallery. Based on the docs here: https://docs.google.com/document/d/1siba2Yv4iJRPbLwvtWDDjrp_32a_MSb_3Dx27BmZ7p4/edit#
+
+Defines a KolibriModule, which is our point of entry into the code.
+In this case, it's a wrapper around a Marionette Application object -- but in principle it could provide a unified
+point of entry for using *other* frameworks as well.
+
+Demonstrates composition of "base components" from the components module also in this directory.
+
+Read the inline comments for more explanation.
+ */
 var components = require('components');
 var logging = require('loglevel');
 var KolibriModule = require('kolibri_module');
+
+// Set Backbone.$ explicitly, as it's required for View DOM manipulation in general and Marionette specifically.
 var $ = require('jquery');
 var Backbone = require('backbone');
 Backbone.$ = $;
@@ -21,28 +37,44 @@ var ComponentDemoPlugin = KolibriModule.extend({
 
     once: {},
 
+    // KolibriModules must define an initialize function, which is called when they are instantiated.
     initialize: function() {
         logging.info('Demo initialized!');
         app.start();
     }
 });
 
-
+/*
+LayoutViews are containers for subviews.
+ */
 var TextInputWithTagDisplay = Mn.LayoutView.extend({
-    template: function(serialized_model) {
-        var template_html = '<div class="textinput"></div>' +
-                            '<div class="taglist"></div>';
-        return _.template(template_html)({});
-    },
+    /*
+    The template and regions attributes define the DOM.
+    Regions are a Marionette abstraction which handle View loading/destroying -- they are essentially named DOM
+    elements.
+     */
+    template: _.template('<div class="textinput"></div>' +
+                         '<div class="taglist"></div>'),
 
     regions: {
-        textinput: '.textinput',
+        textinput: '.textinput',  // Th selector refers to DOM elements in the View's template only.
         taglist: '.taglist'
     },
 
+    /*
+    A good ol' fashioned initialize function. Setup the child views, but *don't* display them. That's
+    the responsibility of the framework presently, and in the future our virtual dom(?)
+     */
     initialize: function() {
+        // A view should pass on it's own model or some element in it. Then events triggered on the model can
+        // be handled by the Application.
         this.textInputField = new components.TextInputField({model: this.model});
         this.tagList = new components.TagList({collection: this.model.get('tags')});
+
+        // The list of events of base elements emit (and the arguments passed with those events)
+        // should be enumerated in the documentation.
+        // In this case, I'm assuming that TagList base component emits a 'tag_clicked' event, namespaced by 'tag_list'
+        // We may choose a different convention.
         this.listenTo(this.tagList, 'tag_list:tag_clicked', function(tag_name) {
             console.log(tag_name + ' was clicked!');
             // Don't try to redraw -- just change the underlying model and let the app handle redrawing.
@@ -54,23 +86,32 @@ var TextInputWithTagDisplay = Mn.LayoutView.extend({
         });
     },
 
+    // This convention is recommended by Marionette for efficient repaints.
+    // See: http://marionettejs.com/docs/v2.4.5/marionette.layoutview.html#efficient-nested-view-structures
     onBeforeShow: function() {
         this.showChildView('taglist', this.tagList);
         this.showChildView('textinput', this.textInputField);
     }
 });
 
-
+// The Application object is Marionette's container object, and integrates with their debugging tool.
 app.on('start', function(){
+    // Applications have methods for managing Regions in the DOM -- these are the same Regions used by LayoutViews.
+    // addRegions instantiates a Region identified by a selector and attaches it to the app with the given name.
     app.addRegions({
-        content: '#content'
+        content: '#content'  // This element already exists in the DOM.
     });
 
+    // Just bootstrapping some data for the demo. In practice, this might be fetched from the server.
     var tags = new Backbone.Collection([
         new Backbone.Model({name: 'foo_tag'}),
         new Backbone.Model({name: 'bar_tag'}),
         new Backbone.Model({name: 'baz_tag'})
     ]);
+
+    // The entire Application state should be externalized. In this case, it's encapsulated in a Model, but in practice
+    // you could use several Models/Collections/etc, any object which uses the Backbone.Events framework should
+    // suffice.
     var appModel = new Backbone.Model({
         enabled: true,
         tags: tags
