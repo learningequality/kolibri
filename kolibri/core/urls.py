379db56954e0afe960e1caf309c2b0bd65603651
Richard Tibbles <richard.tibbles@gmail.com>
2016-04-14 14:57:34 -0700

    Useful enhancements to enable Plugins to extend the base <head> tag. Enable plugins to extend the core urls.py.

--- kolibri/core/urls.py
+++ kolibri/core/urls.py
@@ -4,12 +4,22 @@ enters the docs)
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf.urls import url
+from kolibri.plugins import hooks
 
 from . import views
 
 app_name = 'kolibri'
 
 urlpatterns = [
-    url('^', views.IndexView.as_view(), name='index'),
+    url('^/$', views.IndexView.as_view(), name='index'),
     # url('.* ', views.TODOView.as_view())
 ]
+
+def retrieve_plugin_urls():
+    for callback in hooks.get_callables(hooks.IMPORT_URLS):
+            for item in callback():
+                if 'url_base' in item and 'urls' in item:
+                    yield url(item.get('url_base'), item.get('urls'))
+
+
+urlpatterns += (item for item in retrieve_plugin_urls())
