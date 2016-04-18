a9272d77929b80b7b864063d9d357f07f0a5f2dd
indirectlylit <devon@indirectlylit.com>
2016-04-18 16:44:52 -0700

    break out HTML and CSS into individual components

--- kolibri/core/assets/src/riot_demo.js
+++ kolibri/core/assets/src/riot_demo.js
@@ -7,7 +7,13 @@ logging.setDefaultLevel(2);
 logging.info('Riot demo loaded!');
 
 require('normalize-css');
+
 require('./riot-tags/app.tag.html');
+require('./riot-tags/usermgmt.tag.html');
+require('./riot-tags/user.tag.html');
+require('./riot-tags/rostermgmt.tag.html');
+require('./riot-tags/class.tag.html');
+require('./riot-tags/classmember.tag.html');
 
 riot.mount('app');
 
