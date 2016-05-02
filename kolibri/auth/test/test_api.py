5cd768ee9f5a85a8091fdc8cee67f80aa28eb6d6
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-02 15:57:28 -0700

    Stub out Facility api test case
    Starts to make some assertions about the API form/response.
    
    Conflicts:
    	kolibri/deployment/default/urls.py

--- kolibri/auth/test/test_api.py
+++ kolibri/auth/test/test_api.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import factory
 
-from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+from rest_framework.test import APITestCase
 
 from .. import models
 
@@ -22,15 +24,28 @@ class FacilityUserFactory(factory.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user%d' % n)
 
 
-class FacilityApiTestCase(TestCase):
+class FacilityApiTestCase(APITestCase):
 
     def setUp(self):
-        self.password = "abc123"
+        self.password = 'abc123'
         self.facility = FacilityFactory.create()
         self.user = FacilityUserFactory.create(facility=self.facility)
         self.user.set_password(self.password)
         self.user.save()
-        self.client = Client()
 
-    def test(self):
+    def test_sanity(self):
         self.assertTrue(self.client.login(username=self.user.username, password=self.password, facility=self.facility))
+
+    def test_facility_user_can_get_detail(self):
+        response = self.client.get(reverse('rest_framework:facility-detail', kwargs={'pk': self.facility.pk}),
+                                   format='json')
+        # .assertDictContainsSubset checks that the first argument is a subset of the second argument
+        self.assertDictContainsSubset({
+            'name': self.facility.name,
+            'kind': self.facility.kind,
+            'parent': ''
+        }, response.data)
+
+    def test_facility_user_cant_get_list(self):
+        response = self.client.get(reverse('rest_framework:facility-list'), format='json')
+        self.assertEqual(response.status_code, 403)
--- kolibri/deployment/default/urls.py
+++ kolibri/deployment/default/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('kolibri.core.urls')),
     url(r'', include('kolibri.content.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
