b5c67188d8c83eab9cd6d541e0f41297d7a98456
Benjamin Bach <benjaoming@gmail.com>
2016-05-05 13:55:34 +0200

    Remove non-ascii char

--- kolibri/plugins/hooks.py
+++ kolibri/plugins/hooks.py
@@ -177,11 +177,11 @@ class KolibriHookMeta(type):
     This is the base meta class of all hooks in Kolibri.
 
         Metaclasses are deeper magic than 99% of users should ever worry about.
-        If you wonder whether you need them, you don’t (the people who actually
-        need them know with certainty that they need them, and don’t need an
+        If you wonder whether you need them, you don't (the people who actually
+        need them know with certainty that they need them, and don't need an
         explanation about why).
 
-        – Tim Peters
+        - Tim Peters
 
     """
 
