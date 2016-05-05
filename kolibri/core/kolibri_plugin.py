2ab5480e5c401e603f9105fa57b2a9e64c61d6ec
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-05 11:59:57 -0700

    Remove old navigation stuff

--- kolibri/core/kolibri_plugin.py
+++ kolibri/core/kolibri_plugin.py
@@ -3,33 +3,8 @@ The core app of Kolibri also uses the plugin API <3
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
-from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext as _
-from kolibri.plugins.base import KolibriFrontEndPluginBase, KolibriPluginBase
-from kolibri.plugins.hooks import FRONTEND_PLUGINS, NAVIGATION_POPULATE
-
-
-def main_navigation():
-    """
-    Callback for the plugin below
-    :return: A list of nav menu items used in the NAVIGATION_POPULATE hook.
-    """
-    return [{
-        'menu_name': _("Start page"),
-        'menu_url': reverse('kolibri:index'),
-    }]
-
-
-class KolibriCore(KolibriPluginBase):
-    """
-    The most minimal plugin possible. Because it's in the core, it doesn't define ``enable`` or ``disable``. Those
-    methods should never be called for this plugin.
-    """
-    def hooks(self):
-
-        return {
-            NAVIGATION_POPULATE: main_navigation
-        }
+from kolibri.plugins.base import KolibriFrontEndPluginBase
+from kolibri.plugins.hooks import FRONTEND_PLUGINS
 
 
 class KolibriCoreFrontEnd(KolibriFrontEndPluginBase):
@@ -123,7 +98,6 @@ class NavigationModule(KolibriFrontEndPluginBase):
 
 
 PLUGINS = (
-    KolibriCore,
     KolibriCoreFrontEnd,
     ManagementModule,
     LearnModule,
