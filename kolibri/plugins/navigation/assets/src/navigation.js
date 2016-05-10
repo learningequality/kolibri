a4f8497faf7e46f25bcf87cfa002ba3f3bfedcec
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-10 14:08:02 -0700

    Create Vue hello-world for navigation
    Actually it's quite simple.

---
+++ kolibri/plugins/navigation/assets/src/navigation.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Vue = require('vue');
+
+window.onload = function(){
+    new Vue({
+      el: '#navigation-module',
+      data: {
+        message: 'Hello Vue.js!'
+      }
+    });
+};
