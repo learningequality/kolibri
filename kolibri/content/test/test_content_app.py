3b60fb29fc315ab13f4a699aa1bb89ac11a21aed
66eli77 <66eli77@gmail.com>
2016-05-10 15:35:14 -0700

    fix the test

--- kolibri/content/test/test_content_app.py
+++ kolibri/content/test/test_content_app.py
@@ -33,8 +33,8 @@ class ContentMetadataTestCase(TestCase):
         # Create a temporary directory
         self.test_dir = tempfile.mkdtemp()
         # Create files in the temporary directory
-        self.temp_f_1 = open(os.path.join(self.test_dir, 'test_1.pdf'), 'w')
-        self.temp_f_2 = open(os.path.join(self.test_dir, 'test_2.mp4'), 'w')
+        self.temp_f_1 = open(os.path.join(self.test_dir, 'test_1.pdf'), 'wb')
+        self.temp_f_2 = open(os.path.join(self.test_dir, 'test_2.mp4'), 'wb')
         # Write something to it
         self.temp_f_1.write('The owls are not what they seem')
         self.temp_f_2.write('The owl are not what they seem')
@@ -66,50 +66,50 @@ class ContentMetadataTestCase(TestCase):
         """
         # add same content copy twise, there should be no duplication
         fpath_1 = self.temp_f_1.name
-        fm_1 = content.Format.objects.using(self.the_channel_id).get(format_size=102)
-        fm_3 = content.Format.objects.using(self.the_channel_id).get(format_size=46)
-        file_1 = content.File.objects.using(self.the_channel_id).get(format=fm_1)
-        api.update_content_copy(file_1, fpath_1)
-        file_3 = content.File.objects.using(self.the_channel_id).filter(format=fm_3)[1]
-        api.update_content_copy(file_3, fpath_1)
+        fm_1 = content.FileFormat.objects.using(self.the_channel_id).get(extension='avi')
+        fm_3 = content.FileFormat.objects.using(self.the_channel_id).get(extension='mp4')
+        file_1 = content.File.objects.using(self.the_channel_id).filter(file_format=fm_1)[0]
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_1, content_copy_path=fpath_1)
+        file_3 = content.File.objects.using(self.the_channel_id).get(file_format=fm_3)
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_3, content_copy_path=fpath_1)
         self.assertEqual(1, len(os.listdir(settings.CONTENT_COPY_DIR+'/0/9/')))
 
         # swap the content copy in file_3
         fpath_2 = self.temp_f_2.name
-        self.assertEqual(file_3.extension, '.pdf')
-        api.update_content_copy(file_3, fpath_2)
-        self.assertEqual(file_3.extension, '.mp4')
+        self.assertEqual(file_1.file_format.extension, 'pdf')
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_1, content_copy_path=fpath_2)
+        self.assertEqual(file_1.file_format.extension, 'mp4')
 
         # because file_3 and file_2 all have reference pointing to this content copy,
         # erase the reference from file_2 won't delete the content copy
-        fm_2 = content.Format.objects.using(self.the_channel_id).get(format_size=51)
-        file_2 = content.File.objects.using(self.the_channel_id).get(format=fm_2)
-        api.update_content_copy(file_2, fpath_2)
+        fm_2 = content.FileFormat.objects.using(self.the_channel_id).get(extension='png')
+        file_2 = content.File.objects.using(self.the_channel_id).get(file_format=fm_2)
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_2, content_copy_path=fpath_2)
         self.assertTrue(file_2.content_copy)
-        api.update_content_copy(file_2, None)
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_2, content_copy_path=None)
         self.assertFalse(file_2.content_copy)
         content_copy_path = settings.CONTENT_COPY_DIR+'/3/3/335782204c8215e0061516c6b3b80271.mp4'
         self.assertTrue(os.path.isfile(content_copy_path))
 
         # all reference pointing to this content copy is gone,
         # the content copy should be deleted
-        api.update_content_copy(file_3, None)
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_1, content_copy_path=None)
         self.assertFalse(os.path.isfile(content_copy_path))
         self.assertFalse(file_2.content_copy)
         self.assertFalse(file_2.checksum)
 
         # update None content copy on empty File object should be silent and have no effect
-        api.update_content_copy(file_2, None)
+        api.update_content_copy(channel_id=self.the_channel_id, file_object=file_2, content_copy_path=None)
 
         # test File __str__ method
-        self.assertEqual(file_1.__str__(), '09293abba61d4fcfa4e3bd804bcaba43.pdf')
+        self.assertEqual(file_3.__str__(), '09293abba61d4fcfa4e3bd804bcaba43.pdf')
 
         # test MimeType __str__ method
-        self.assertEqual(fm_1.mimetype.__str__(), 'video_high')
+        self.assertEqual(file_3.preset.name, 'high_res_video')
 
         # test for non File object exception
         with self.assertRaises(TypeError):
-            api.update_content_copy(None, None)
+            api.update_content_copy(channel_id=self.the_channel_id, file_object=None, content_copy_path=None)
 
     def test_get_content_with_id(self):
         # test for single content_id
@@ -148,29 +148,16 @@ class ContentMetadataTestCase(TestCase):
         actual_output = api.leaves(channel_id=self.the_channel_id, content=p)
         self.assertEqual(set(expected_output), set(actual_output))
 
-    def test_get_all_formats(self):
-        p = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c2")
-        expected_output = content.Format.objects.using(self.the_channel_id).filter(format_size=46)
-        actual_output = api.get_all_formats(channel_id=self.the_channel_id, content=p)
-        self.assertEqual(set(expected_output), set(actual_output))
-
-    def test_get_available_formats(self):
-        p = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c2")
-        expected_output = []
-        actual_output = api.get_available_formats(channel_id=self.the_channel_id, content=p)
-        self.assertEqual(set(expected_output), set(actual_output))
-
-    def test_get_possible_formats(self):
+    def test_get_all_presets(self):
         p = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1")
-        expected_output = content.Format.objects.using(self.the_channel_id).filter(format_size__in=[51, 102])
-        actual_output = api.get_possible_formats(channel_id=self.the_channel_id, content=p)
+        expected_output = content.FormatPreset.objects.using(self.the_channel_id).filter(kind__kind='video')
+        actual_output = api.get_all_presets(channel_id=self.the_channel_id, content=p)
         self.assertEqual(set(expected_output), set(actual_output))
 
     def test_get_files_for_quality(self):
         p = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1")
-        fm = content.Format.objects.using(self.the_channel_id).get(format_size=102)
-        expected_output = content.File.objects.using(self.the_channel_id).filter(format=fm)
-        actual_output = api.get_files_for_quality(channel_id=self.the_channel_id, content=p, format_quality="high")
+        expected_output = content.File.objects.using(self.the_channel_id).filter(id=2)
+        actual_output = api.get_files_for_quality(channel_id=self.the_channel_id, content=p, preset_quality="low_res_video")
         self.assertEqual(set(expected_output), set(actual_output))
 
     def test_get_missing_files(self):
@@ -297,9 +284,6 @@ class ContentMetadataTestCase(TestCase):
             pass
 
 
-@override_settings(
-    CONTENT_COPY_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/test_content_copy"
-)
 class ContentMetadataAPITestCase(APITestCase):
     """
     Testcase for content API methods
@@ -311,14 +295,6 @@ class ContentMetadataAPITestCase(APITestCase):
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
     }
-
-    def setUp(self):
-        # Create a temporary directory
-        self.test_dir = tempfile.mkdtemp()
-        # Create file in the temporary directory
-        self.temp_f_1 = open(os.path.join(self.test_dir, 'test_1.pdf'), 'w')
-        # Write something to it
-        self.temp_f_1.write('The owls are not what they seem')
 
     def _reverse_channel_url(self, pattern_name, extra_kwargs):
         """Helper method to reverse a URL using the current channel ID"""
@@ -355,75 +331,14 @@ class ContentMetadataAPITestCase(APITestCase):
         response = self.client.get(self._reverse_channel_url("contentmetadata-all-related", {"content_id": c1_id}))
         self.assertEqual(response.data[0]['title'], 'c2')
 
-    def test_all_formats_endpoint(self):
-        c2_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c2").content_id
-        response = self.client.get(self._reverse_channel_url("contentmetadata-all-formats", {"content_id": c2_id}))
-        self.assertEqual(response.data[0]['format_size'], 46)
-
-    def test_available_formats_endpoint(self):
-        c2_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c2").content_id
-        response = self.client.get(self._reverse_channel_url("contentmetadata-available-formats", {"content_id": c2_id}))
-        self.assertEqual(len(response.data), 0)
-
-    def test_possible_formats_endpoint(self):
-        c1_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1").content_id
-        response = self.client.get(self._reverse_channel_url("contentmetadata-possible-formats", {"content_id": c1_id}))
-        self.assertEqual(response.data[0]['format_size'], 102)
-        self.assertEqual(response.data[1]['format_size'], 51)
-
     def test_missing_files_endpoint(self):
         c1_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1").content_id
         response = self.client.get(self._reverse_channel_url("contentmetadata-missing-files", {"content_id": c1_id}))
         expected_output = content.File.objects.using(self.the_channel_id).filter(id__in=[1, 2])
-        self.assertEqual(response.data[0]['format'], expected_output[0].format.id)
-        self.assertEqual(response.data[1]['format'], expected_output[1].format.id)
+        self.assertEqual(response.data[0]['file_format']['extension'], expected_output[0].file_format.extension)
+        self.assertEqual(response.data[1]['file_format']['extension'], expected_output[1].file_format.extension)
 
-    def test_files_for_quality_endpoint(self):
+    def test_all_presets_endpoint(self):
         c1_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1").content_id
-        response = self.client.get(self._reverse_channel_url("contentmetadata_files_for_quality", {"content_id": c1_id, "quality": "high"}))
-        fm = content.Format.objects.using(self.the_channel_id).get(format_size=102)
-        self.assertEqual(response.data[0]['format'], fm.id)
-
-    def test_set_prerequisite_endpoint(self):
-        root = content.ContentMetadata.objects.using(self.the_channel_id).get(title="root")
-        c2 = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c2")
-        self.assertFalse(api.get_all_prerequisites(channel_id=self.the_channel_id, content=root))
-        self.client.put(self._reverse_channel_url("contentmetadata_set_prerequisite", {"content_id": c2.content_id, "prerequisite": root.content_id}))
-        self.assertTrue(api.get_all_prerequisites(channel_id=self.the_channel_id, content=root))
-
-    def test_set_is_related_endpoint(self):
-        root = content.ContentMetadata.objects.using(self.the_channel_id).get(title="root")
-        c1 = content.ContentMetadata.objects.using(self.the_channel_id).get(title="c1")
-        self.assertFalse(root in api.get_all_related(channel_id=self.the_channel_id, content=c1))
-        self.client.put(self._reverse_channel_url("contentmetadata_set_is_related", {"content_id": c1.content_id, "related": root.content_id}))
-        self.assertTrue(root in api.get_all_related(channel_id=self.the_channel_id, content=c1))
-
-    def test_children_of_kind_endpoint(self):
-        root_id = content.ContentMetadata.objects.using(self.the_channel_id).get(title="root").content_id
-        response = self.client.get(self._reverse_channel_url("contentmetadata_children_of_kind", {"content_id": root_id, "kind": "topic"}))
-        cn_titles = [k['title'] for k in response.data]
-        self.assertEqual(cn_titles[0], 'c2')
-        self.assertEqual(cn_titles[1], 'c2c2')
-        self.assertEqual(cn_titles[2], 'c2c3')
-
-    def test_update_content_copy_endpoint(self):
-        # add same content copy twice, there should be no duplication
-        fpath_1 = self.temp_f_1.name
-        fm_1 = content.Format.objects.using(self.the_channel_id).get(format_size=102)
-        fm_3 = content.Format.objects.using(self.the_channel_id).get(format_size=46)
-        file_1 = content.File.objects.using(self.the_channel_id).get(format=fm_1)
-        self.client.put(self._reverse_channel_url("file_update_content_copy", {"pk": file_1.pk, "content_copy": fpath_1}))
-        file_3 = content.File.objects.using(self.the_channel_id).filter(format=fm_3)[1]
-        self.client.put(self._reverse_channel_url("file_update_content_copy", {"pk": file_3.pk, "content_copy": fpath_1}))
-        self.assertEqual(1, len(os.listdir(settings.CONTENT_COPY_DIR+'/d/4/')))
-
-    @classmethod
-    def tearDownClass(self):
-        """
-        clean up files/folders created during the test
-        """
-        try:
-            shutil.rmtree(settings.CONTENT_COPY_DIR)
-            shutil.rmtree(self.test_dir)
-        except:
-            pass
+        response = self.client.get(self._reverse_channel_url("contentmetadata-all-presets", {"content_id": c1_id}))
+        self.assertEqual(response.data[0]['name'], 'high_res_video')
