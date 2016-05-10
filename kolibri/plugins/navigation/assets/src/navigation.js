dc62cd09071def33ed4ccbeb1dde5f27dbd0de0c
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-10 15:05:10 -0700

    Stub out Navigation app

--- kolibri/plugins/navigation/assets/src/navigation.js
+++ kolibri/plugins/navigation/assets/src/navigation.js
@@ -2,11 +2,21 @@
 
 var Vue = require('vue');
 
+var Navigation = Vue.extend({
+
+});
+
 window.onload = function(){
-    new Vue({
-      el: '#navigation-module',
-      data: {
-        message: 'Hello Vue.js!'
-      }
+    var nav = new Navigation({
+        el: '#navigation-module',
+        data: {
+            nav_items: global.kolibri_reserved.nav_items,
+            user_nav_items: global.kolibri_reserved.user_nav_items,
+            user: {
+                username: 'foobar',
+                first_name: 'Foo',
+                last_name: 'Bar'
+            }
+        }
     });
 };
