5d4452dcd9445eb1fdce5cf7a336ef3013d742ec
indirectlylit <devon@indirectlylit.com>
2016-05-11 01:19:36 -0700

    Repurposing `kolibri_main_navigation`
    `kolibri_main_navigation` renamed to `initialized_data`, a general way of pushing global data into the web app on load. Now adds the data to an existing `kolibriGlobal` object, which is an instance of `core_app`.

--- kolibri/core/templatetags/kolibri_tags.py
+++ kolibri/core/templatetags/kolibri_tags.py
@@ -1,19 +1,6 @@
 """
 Kolibri template tags
 =====================
-
-To use
-
-.. code-block:: html
-
-    {% load kolibri_tags %}
-
-    <ul>
-    {% for navigation in kolibri_main_navigation %}
-        <li><a href="{{ navigation.menu_url }}">{{ navigation.menu_name }}</a></li>
-    {% endfor %}
-    </ul>
-
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
