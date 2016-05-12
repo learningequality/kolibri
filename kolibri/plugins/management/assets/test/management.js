39ba10fa51b4019e66ae677e1fba31dd1d58ccbc
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-12 16:19:50 -0700

    Assert existence of some components in management app

--- kolibri/plugins/management/assets/test/management.js
+++ kolibri/plugins/management/assets/test/management.js
@@ -1,10 +1,38 @@
 /* eslint-env mocha */
-const Management = require('vue!../src/main.vue');
-const assert = require('assert');
+import Vue from 'vue';
+import Management from 'vue!../src/main.vue';
+import assert from 'assert';
 
 describe('The management module', () => {
   it('defines a Management vue', () => {
     // A sanity check
     assert(Management !== undefined);
+  });
+
+  describe('has the following components:', () => {
+    const vm = new Vue({
+      components: { Management },
+    }).$mount();
+
+    it('a classroom selector', () => {
+      Vue.nextTick(() => {
+        const el = vm.$el.querySelector('.classroom-selector');
+        assert.notStrictEqual(el, null);
+      });
+    });
+
+    it('a learner group selector', () => {
+      Vue.nextTick(() => {
+        const el = vm.$el.querySelector('.learner-group-selector');
+        assert.notStrictEqual(el, null);
+      });
+    });
+
+    it('a learner roster', () => {
+      Vue.nextTick(() => {
+        const el = vm.$el.querySelector('.learner-roster');
+        assert.notStrictEqual(el, null);
+      });
+    });
   });
 });
