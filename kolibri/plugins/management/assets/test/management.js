9e189d38731dd9d7ac4c37f8905c76eff42d5061
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-12 16:55:54 -0700

    Write classroom selector tests

--- kolibri/plugins/management/assets/test/management.js
+++ kolibri/plugins/management/assets/test/management.js
@@ -35,4 +35,68 @@ describe('The management module', () => {
       });
     });
   });
+
+  describe('changes the list of students in the roster when you select a classroom.', () => {
+    const johnDuck = {
+      id: 2,
+      first_name: 'John',
+      last_name: 'Duck',
+      username: 'jduck',
+    };
+    const learners = [
+      {
+        id: 1,
+        first_name: 'Mike',
+        last_name: 'G',
+        username: 'mike',
+      },
+      johnDuck,
+      {
+        id: 3,
+        first_name: 'Abe',
+        last_name: 'Lincoln',
+        username: 'abe',
+      },
+    ];
+    const vm = new Vue({
+      components: { Management },
+      data: {
+        classrooms: [
+          {
+            id: 1,
+            name: 'Classroom A',
+            learners: [1, 3],
+          },
+          {
+            id: 2,
+            name: 'Classroom B',
+            learners: [3],
+          },
+          {
+            id: 3,
+            name: 'Classroom C',
+            learners: [2],
+          },
+        ],
+        learners,
+      },
+    });
+
+    it('The roster shows all learners when you select "All classrooms".', () => {
+      const roster = vm.$refs.roster;
+      const classroomSelector = vm.$refs.classroomSelector;
+      classroomSelector.selection = 'All classrooms';
+      Vue.nextTick(() => {
+        assert.equal(roster.displayedLearners, learners);
+      });
+    });
+    it('The roster shows only John Duck when you select "Classroom 2".', () => {
+      const roster = vm.$refs.roster;
+      const classroomSelector = vm.$refs.classroomSelector;
+      classroomSelector.selection = 'Classroom 2';
+      Vue.nextTick(() => {
+        assert.equal(roster.displayedLearners, [johnDuck]);
+      });
+    });
+  });
 });
