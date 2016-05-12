adcdb0201d993367258e05cbb45148ec0ad1e468
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-11 18:18:35 -0700

    Stub out management module, vues

---
+++ kolibri/plugins/management/assets/src/main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <drop-down :list="classrooms"></drop-down>
+    <button>Create</button>
+    <button>Delete</button>
+  </div>
+  <div>
+    <drop-down :list="learnerGroups"></drop-down>
+    <button>Create</button>
+    <button>Delete</button>
+  </div>
+  <learner-list></learner-list>
+</template>
+
+
+<script>
+import learnerList from './learner-list.vue';
+import dropDown from './drop-down.vue';
+
+export default {
+  components: {
+    'learner-list': learnerList,
+    'drop-down': dropDown,
+  },
+  data: () => ({
+    classrooms: [
+      'All classrooms',
+      'Classroom 1',
+    ],
+    learnerGroups: [
+      'All groups',
+      'Group A',
+    ],
+  }),
+};
+</script>
+
+
+<style lang="stylus" scoped>
+</style>
