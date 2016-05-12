49649e63832bfebcf9feacee59bd5accd48abc43
indirectlylit <devon@indirectlylit.com>
2016-05-12 14:03:52 -0700

    update comments based on https://github.com/learningequality/kolibri/pull/78#discussion_r62923830

--- assets/src/read_bundle_plugin.js
+++ assets/src/read_bundle_plugin.js
@@ -47,14 +47,10 @@ var readBundlePlugin = function(plugin_file, base_dir) {
                     if (typeof externals[output[1]] === "undefined") {
                         // The second part of the output is any global variables that will be available to all other
                         // plugins. For the moment, this is only the `kolibriGlobal` variable.
-
-                        ////////////////
-                        // [NOTE - seems to often be undefined here, which is probably wrong]
+                        // Often be undefined, if there are no external variables to export
                         if (output[1] !== undefined) {
                             externals[output[1]] = output[1];
                         }
-                        ////////////////
-
                     } else {
                         logging.warn("Two plugins setting with same external flag " + output[1] + " in plugin file: " + plugin_file);
                     }
