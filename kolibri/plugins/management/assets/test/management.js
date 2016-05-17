0930d6d1aa7e2a6befff36f184d86c16324ec416
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-17 16:14:12 -0700

    Re-write classroom selector tests

--- kolibri/plugins/management/assets/test/management.js
+++ kolibri/plugins/management/assets/test/management.js
@@ -6,8 +6,9 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import assert from 'assert';
 
-import { store, mutations } from '../src/vuex/store.js';
-import Management from 'vue!../src/main.vue';
+import { store, mutations, constants } from '../src/vuex/store.js';
+import Management from '../src/main.vue';
+import fixture1 from './fixtures/fixture1.js';
 
 
 describe('The management module', () => {
@@ -19,31 +20,38 @@ describe('The management module', () => {
   describe('has the following components:', function () {
     before(function () {
       const container = new Vue({
-        template: '<div><management v-ref:main></management></div>',
+        template: '<div class="foo"><management v-ref:main></management></div>',
         components: { Management },
         store,
       }).$mount();
       this.vm = container.$refs.main;
     });
 
-    it('a classroom selector', function () {
+    after(function () {
+      this.vm.$destroy();
+    });
+
+    it('a classroom selector', function (done) {
       Vue.nextTick(() => {
         const child = this.vm.$refs.classroomSelector;
         assert.notStrictEqual(child, undefined);
+        done();
       });
     });
 
-    it('a learner group selector', function () {
+    it('a learner group selector', function (done) {
       Vue.nextTick(() => {
         const child = this.vm.$refs.learnerGroupSelector;
         assert.notStrictEqual(child, undefined);
+        done();
       });
     });
 
-    it('a learner roster', function () {
+    it('a learner roster', function (done) {
       Vue.nextTick(() => {
         const child = this.vm.$refs.learnerRoster;
         assert.notStrictEqual(child, undefined);
+        done();
       });
     });
   });
@@ -51,7 +59,7 @@ describe('The management module', () => {
   describe('changes the list of students in the roster when you select a classroom.', function () {
     beforeEach(function () {
       const testStore = new Vuex.Store({
-        state: require('fixture1.js'),
+        state: fixture1,
         mutations,
       });
       const container = new Vue({
@@ -60,9 +68,37 @@ describe('The management module', () => {
         store: testStore,
       });
       container.$mount();
+      this.vm = container.$refs.main;
     });
 
-    it('The roster shows all learners when you select "All classrooms".');
-    it('The roster shows only John Duck when you select "Classroom 2".');
+    afterEach(function () {
+      this.vm.$destroy();
+    });
+
+    it('The roster shows all learners when you select "All classrooms".', function (done) {
+      Vue.nextTick(() => {
+        this.vm.selectedClassroom = {
+          id: constants.ALL_CLASSROOMS_ID,
+        };
+        assert.strictEqual(this.vm.$refs.learnerRoster.learners, fixture1.learners);
+        done();
+      });
+    });
+
+    it('The roster shows only John Duck when you select "Classroom C".', function (done) {
+      // Look at the fixture file for the magic numbers here.
+      Vue.nextTick(() => {
+        this.vm.selectedClassroom = {
+          id: 2,
+        };
+        assert.strictEqual(this.vm.$refs.learnerRoster.learners, [{
+          id: 2,
+          first_name: 'John',
+          last_name: 'Duck',
+          username: 'jduck',
+        }]);
+        done();
+      });
+    });
   });
 });
