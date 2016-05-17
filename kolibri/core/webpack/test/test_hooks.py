1e058203fd68863248e20eaf4db10029f966c8fa
Benjamin Bach <benjaoming@gmail.com>
2016-05-17 21:11:32 +0200

    Create a Mixin for testing WebpackBundleHooks without NPM building

--- kolibri/core/webpack/test/test_hooks.py
+++ kolibri/core/webpack/test/test_hooks.py
@@ -5,30 +5,45 @@ emulated side-effects.
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-
-from django.test import TestCase
+from django.test.testcases import TestCase
 
 from .. import hooks
+from .base import TestHookMixin
 
 
 class _WebpackBundleHookSwappedOut(hooks.WebpackBundleHook):
-    unique_slug = "i get swapped out"
+    unique_slug = "i_get_swapped_out"
 
 
 class _WebpackBundleHookInheritor(_WebpackBundleHookSwappedOut):
 
-    unique_slug = "i get swapped in"
+    unique_slug = "i_get_swapped_in"
 
     class Meta:
         replace_parent = True
+
+
+class _FrontEndCoreHook(hooks.FrontEndCoreHook):
+    unique_slug = "im_a_core_hook"
+
+    class Meta:
+        replace_parent = True
+
+
+class _FrontEndASyncHook(TestHookMixin, hooks.FrontEndASyncHook):
+    unique_slug = "im_an_async_hook"
+
+    events = {
+        'some_weird_event_we_are_going_to_look_for': 'value'
+    }
 
 
 class WebpackBundleHookTestCase(TestCase):
 
     def test_replacement(self):
         """
-        Test that the parent of ``__WebpackBundleHook`` is no longer in the
-        registry
+        Test that the parent of ``_WebpackBundleHookInheritor`` is no longer in
+        the registry. Other tests depend on this.
         """
         registered_types = [
             type(hook) for hook in hooks.WebpackBundleHook().registered_hooks
@@ -43,3 +58,13 @@ class WebpackBundleHookTestCase(TestCase):
             _WebpackBundleHookInheritor,
             registered_types
         )
+
+    def test_sync_hook(self):
+
+        for hook in hooks.FrontEndAssetHook().registered_hooks:
+            if type(hook) is _FrontEndASyncHook:
+                for event_key in _FrontEndASyncHook.events.keys():
+                    self.assertIn(
+                        event_key,
+                        hook.render_to_html(),
+                    )
--- kolibri/core/webpack/test/test_webpack_tags.py
+++ kolibri/core/webpack/test/test_webpack_tags.py
@@ -1,56 +1,21 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django.template import Template, Context
-
-import json
-import tempfile
-
-from django.test import TestCase
-
+from django.test.testcases import TestCase
 
 from ..hooks import WebpackBundleHook
+from .base import TestHookMixin
 
 
-TEST_STATS_FILE = None
-
-TEST_STATS_FILE_DATA = {
-    "status": "done",
-    "chunks": {
-        "non_default_frontend": [
-            {
-                "name": "non_default_frontend-2c4fb3d6a29238b06f84.js",
-                "publicPath": "non_default_frontend/non_default_frontend-2c4fb3d6a29238b06f84.js",
-                "path": "kolibri/core/static/non_default_frontend/non_default_frontend-2c4fb3d6a29238b06f84.js"
-            }
-        ]
-    },
-    "publicPath": "default_frontend/"
-}
-
-class TestHook(WebpackBundleHook):
-    """
-    This hook is a blank test.
-    """
-    unique_slug = "non_default_frontend"
-    src_file = "assets/src/kolibri_core_app.js"
-
-    @property
-    def stats_file(self):
-        return TEST_STATS_FILE.name
+class TestHook(TestHookMixin, WebpackBundleHook):
+    pass
 
 
 class KolibriTagNavigationTestCase(TestCase):
 
     def setUp(self):
-        global TEST_STATS_FILE
-        TestCase.setUp(self)
-        TEST_STATS_FILE = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        super(KolibriTagNavigationTestCase, self).setUp()
         self.test_hook = TestHook()
-        json.dump(
-            TEST_STATS_FILE_DATA,
-            TEST_STATS_FILE
-        )
-        TEST_STATS_FILE.close()
 
     def test_frontend_tag(self):
         self.assertIn(
@@ -63,6 +28,6 @@ class KolibriTagNavigationTestCase(TestCase):
             """{% load webpack_tags %}\n{% webpack_assets 'non_default_frontend' %}""")
         c = Context({})
         self.assertIn(
-            TEST_STATS_FILE_DATA['chunks'][TestHook.unique_slug][0]['name'],
+            self.test_hook.TEST_STATS_FILE_DATA['chunks'][TestHook.unique_slug][0]['name'],
             t.render(c)
         )
