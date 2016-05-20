92c2b6444f86a3e99b0dadf732d49e7ec10924e7
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-20 15:22:12 -0700

    Change nav items template tag for use with navigation module
    Instead of injecting html directly into the django template, it
    now just provides some data for use by the navigation js.

--- kolibri/core/templatetags/kolibri_tags.py
+++ kolibri/core/templatetags/kolibri_tags.py
@@ -4,7 +4,10 @@ Kolibri template tags
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
+import json
+
 from django import template
+from django.utils.html import mark_safe
 from kolibri.core.hooks import NavigationHook
 
 register = template.Library()
@@ -12,6 +15,22 @@ register = template.Library()
 
 @register.simple_tag()
 def kolibri_main_navigation():
+    """
+    A tag to include an initial JS-object to bootstrap data into the app.
+    :return: An html string
+    """
+    init_data = {
+        'nav_items': [],
+        'user_nav_items': [],
+    }
 
     for hook in NavigationHook().registered_hooks:
-        yield hook
+        init_data['nav_items'].append({
+            'text': str(hook.label),
+            'url': str(hook.url),
+        })
+
+    html = ("<script type='text/javascript'>"
+            "window._nav={0};"
+            "</script>".format(json.dumps(init_data)))
+    return mark_safe(html)
