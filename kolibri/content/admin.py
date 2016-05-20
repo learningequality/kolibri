cbe4f5470fec966538f63ca9beb04838bfbf3aa3
66eli77 <66eli77@gmail.com>
2016-05-19 23:03:12 -0700

    change to contentnode_1 and contentnode_2 for content relationship

--- kolibri/content/admin.py
+++ kolibri/content/admin.py
@@ -5,27 +5,27 @@ from .models import PrerequisiteContentRelationship, RelatedContentRelationship
 
 class PrerequisiteRelationshipInline1(admin.TabularInline):
     model = PrerequisiteContentRelationship
-    fk_name = 'contentmetadata_1'
+    fk_name = 'contentnode_1'
     max = 20
     extra = 0
 
 class PrerequisiteRelationshipInline2(admin.TabularInline):
     model = PrerequisiteContentRelationship
-    fk_name = 'contentmetadata_2'
+    fk_name = 'contentnode_2'
     max = 20
     extra = 0
 
 class RelatedRelationshipInline1(admin.TabularInline):
     model = RelatedContentRelationship
-    fk_name = 'contentmetadata_1'
+    fk_name = 'contentnode_1'
     max = 20
     extra = 0
 
 class RelatedRelationshipInline2(admin.TabularInline):
     model = RelatedContentRelationship
-    fk_name = 'contentmetadata_2'
+    fk_name = 'contentnode_2'
     max = 20
     extra = 0
 
-class ContentMetadataAdmin(admin.ModelAdmin):
+class ContentNodeAdmin(admin.ModelAdmin):
     inlines = (PrerequisiteRelationshipInline1, PrerequisiteRelationshipInline2, RelatedRelationshipInline1, RelatedRelationshipInline2)
