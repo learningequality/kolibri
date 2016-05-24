a6205a5ca5d742a41aa2db225e46771506c74527
David Canas <davidxcanas@gmail.com>
2016-05-24 14:58:39 -0700

    Fixing up the navigation vue file, setting up for its inclusion in kolibri with new Vue-based frontend

--- kolibri/core/assets/src/core-base.vue
+++ kolibri/core/assets/src/core-base.vue
@@ -1,50 +1,26 @@
 <template>
-
-  <header>
-    <h3>Kolibri</h3>
-    <nav>
-      <ul>
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-      <ul>
-    </nav>
-  </header>
-  <main>
-    <slot></slot>
-  </main>
-  <footer>
-    Call a podiatrist – I'm in a footer!
-  </footer>
-
+  <kolibri-nav></kolibri-nav>
 </template>
 
+<script>
+const Vue = require('vue');
+import navOptions from './navigation.vue.html';
+const VueNav = Vue.extend(navOptions);
 
-<script></script>
-
-
-<style lang="stylus" scoped>
-
-  $bg-color = #E3E3F3
-
-  header
-    background-color: $bg-color
-    display: flex
-    justify-content: space-between
-    padding: 10px
-
-  footer
-    bottom: 1vh
-    position: fixed
-
-
-</style>
+export default {
+  components: {
+    'kolibri-nav': VueNav,
+  },
+};
+</script>
 
 
 <style lang="stylus">
-
   /* These global styles apply to everything */
 
   html, body
     font-family: sans-serif
-
+    margin:0
+    padding:0
+    box-sizing: border-box
 </style>
