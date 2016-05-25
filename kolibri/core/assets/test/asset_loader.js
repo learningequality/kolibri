f072156b6b82d8ede8cceece95b76e2a5303a64d
indirectlylit <devon@indirectlylit.com>
2016-05-25 10:38:35 -0700

    migrate `asset_loader` test to es6

--- kolibri/core/assets/test/asset_loader.js
+++ kolibri/core/assets/test/asset_loader.js
@@ -1,97 +1,99 @@
+/* global define, it, describe, beforeEach */
+
 'use strict';
 
-var assert = require('assert');
-var rewire = require('rewire');
+const assert = require('assert');
+const rewire = require('rewire');
 
-var assetLoader = rewire('../src/asset_loader.js');
+const assetLoader = rewire('../src/asset_loader.js');
 
-describe('Asset loader', function() {
-    describe('input is invalid', function() {
-        it('should raise an error', function (done) {
-            assetLoader('nothing', function(error) {
-                assert(error instanceof Error);
-                done();
-            });
-        });
+describe('Asset loader', () => {
+  describe('input is invalid', () => {
+    it('should raise an error', (done) => {
+      assetLoader('nothing', (error) => {
+        assert(error instanceof Error);
+        done();
+      });
     });
-    describe('js input is valid, file fetches', function() {
-        it('should callback null', function (done) {
-            assetLoader.__set__('scriptjs', function(file, success, fail) {
-                success();
-            });
-            assetLoader(['nothing.js'], function(error, missing) {
-                assert.equal(error, null);
-                done();
-            });
-        });
+  });
+  describe('js input is valid, file fetches', () => {
+    it('should callback null', (done) => {
+      assetLoader.__set__('scriptjs', (file, success) => {
+        success();
+      });
+      assetLoader(['nothing.js'], (error) => {
+        assert.equal(error, null);
+        done();
+      });
     });
-    describe('css input is valid, file fetches', function() {
-        it('should callback null', function (done) {
-            assetLoader.__set__('onloadcss', function(file, success) {
-                success();
-            });
-            assetLoader(['nothing.css'], function(error, missing) {
-                assert.equal(error, null);
-                done();
-            });
-        });
+  });
+  describe('css input is valid, file fetches', () => {
+    it('should callback null', (done) => {
+      assetLoader.__set__('onloadcss', (file, success) => {
+        success();
+      });
+      assetLoader(['nothing.css'], (error) => {
+        assert.equal(error, null);
+        done();
+      });
     });
-    describe('js input is valid, file fails to fetch', function() {
-        beforeEach(function(){
-            assetLoader.__set__('scriptjs', function(file, success, fail) {
-                fail();
-            });
-        });
-        it('should raise an error', function (done) {
-            assetLoader(['nothing.js'], function(error, missing) {
-                assert(error instanceof Error);
-                done();
-            });
-        });
-        it('should have the file in the missing array', function (done) {
-            assetLoader(['nothing.js'], function(error, missing) {
-                assert.deepEqual(['nothing.js'], missing);
-                done();
-            });
-        });
+  });
+  describe('js input is valid, file fails to fetch', () => {
+    beforeEach(() => {
+      assetLoader.__set__('scriptjs', (file, success, fail) => {
+        fail();
+      });
     });
-    describe('css and js input is valid, files fetch', function() {
-        beforeEach(function(){
-            assetLoader.__set__('scriptjs', function(file, success, fail) {
-                success();
-            });
-            assetLoader.__set__('onloadcss', function(file, success) {
-                success();
-            });
-        });
-        it('should callback null', function (done) {
-            assetLoader(['nothing.js', 'nothing.css'], function(error, missing) {
-                assert.equal(error, null);
-                done();
-            });
-        });
-        it('should have a null missing file list', function (done) {
-            assetLoader(['nothing.js', 'nothing.css'], function(error, missing) {
-                assert.equal(missing, null);
-                done();
-            });
-        });
+    it('should raise an error', (done) => {
+      assetLoader(['nothing.js'], (error) => {
+        assert(error instanceof Error);
+        done();
+      });
     });
-    describe('css input is valid, file fails to fetch', function() {
-        beforeEach(function(){
-            assetLoader.__set__('onloadcss', function(file, success) {});
-        });
-        it('should raise an error', function (done) {
-            assetLoader(['nothing.css'], function(error, missing) {
-                assert(error instanceof Error);
-                done();
-            }, 1);
-        });
-        it('should have the file in the missing array', function (done) {
-            assetLoader(['nothing.css'], function(error, missing) {
-                assert.deepEqual(['nothing.css'], missing);
-                done();
-            }, 1);
-        });
+    it('should have the file in the missing array', (done) => {
+      assetLoader(['nothing.js'], (error, missing) => {
+        assert.deepEqual(['nothing.js'], missing);
+        done();
+      });
     });
+  });
+  describe('css and js input is valid, files fetch', () => {
+    beforeEach(() => {
+      assetLoader.__set__('scriptjs', (file, success) => {
+        success();
+      });
+      assetLoader.__set__('onloadcss', (file, success) => {
+        success();
+      });
+    });
+    it('should callback null', (done) => {
+      assetLoader(['nothing.js', 'nothing.css'], (error) => {
+        assert.equal(error, null);
+        done();
+      });
+    });
+    it('should have a null missing file list', (done) => {
+      assetLoader(['nothing.js', 'nothing.css'], (error, missing) => {
+        assert.equal(missing, null);
+        done();
+      });
+    });
+  });
+  describe('css input is valid, file fails to fetch', () => {
+    beforeEach(() => {
+      assetLoader.__set__('onloadcss', () => {});
+    });
+    it('should raise an error', (done) => {
+      assetLoader(['nothing.css'], (error) => {
+        assert(error instanceof Error);
+        done();
+      }, 1);
+    });
+    it('should have the file in the missing array', (done) => {
+      assetLoader(['nothing.css'], (error, missing) => {
+        assert.deepEqual(['nothing.css'], missing);
+        done();
+      }, 1);
+    });
+  });
 });
