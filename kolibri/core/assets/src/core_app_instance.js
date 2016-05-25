79e30e24875e92978c2e5e8c9848edf72fc452c7
indirectlylit <devon@indirectlylit.com>
2016-05-24 17:53:14 -0700

    add comment

--- kolibri/core/assets/src/core_app_instance.js
+++ kolibri/core/assets/src/core_app_instance.js
@@ -7,6 +7,10 @@ require('./global.styl');
 const logging = require('loglevel');
 logging.setDefaultLevel(process.env.NODE_ENV === 'production' ? 2 : 0);
 
-// create an instance of the global app object
+// Create an instance of the global app object.
+// This is automatically attached to the kolibriGlobal object ... somehow?
+//
+// TODO - clarify how setting the `src_file` attribute in the core
+// kolibri_plugin.py attaches this object to the global variable.
 const CoreAppConstructor = require('./core_app_constructor');
 module.exports = new CoreAppConstructor();
