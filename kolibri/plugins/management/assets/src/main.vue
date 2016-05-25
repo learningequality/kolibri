5a07117276484b1397b7eda26c11de1d245d2408
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-05-25 13:04:04 -0700

    Fix more main.vue logic errors
    Again seems to have been introduced in conflict resolution,
    pesky duplicated sections and too many brackets to line up. :(

--- kolibri/plugins/management/assets/src/main.vue
+++ kolibri/plugins/management/assets/src/main.vue
@@ -35,6 +35,7 @@
       'learner-roster': require('./learner-roster.vue'),
       'drop-down': require('./drop-down.vue'),
     },
+
     computed: {
       classrooms() {
         const _classrooms = [{
@@ -45,65 +46,57 @@
         _classrooms.push(...this.getClassrooms);
         return _classrooms;
       },
-      computed: {
-        classrooms() {
-          const _classrooms = [{
-            id: constants.ALL_CLASSROOMS_ID,
-            name: 'All classrooms',
-            learnerGroups: [],
-          }];
-          _classrooms.push(...this.getClassrooms);
-          return _classrooms;
-        },
-        selectedClassroom: {
-          get() {
-            for (const classroom of this.classrooms) {
-              if (classroom.id === this.getSelectedClassroomId) {
-                return classroom;
-              }
+
+      selectedClassroom: {
+        get() {
+          for (const classroom of this.classrooms) {
+            if (classroom.id === this.getSelectedClassroomId) {
+              return classroom;
             }
-            // Default value in case getSelectedClassroom is inconsistent
-            return this.classrooms[0];
-          },
-          set({ id }) {
-            this.setSelectedClassroomId(id);
-          },
+          }
+          // Default value in case getSelectedClassroom is inconsistent
+          return this.classrooms[0];
         },
-        filteredLearners() {
-          const learners = this.$store.state.learners;
-          let _learners = learners;
-          if (this.selectedClassroom.id !== constants.ALL_CLASSROOMS_ID) {
-            const learnerGroupIds = this.selectedClassroom.learnerGroups;
-            const learnerIds = new Set();
-            for (const group of this.$store.state.learnerGroups) {
-              if (learnerGroupIds.indexOf(group.id) !== -1) {
-                for (const learnerId of group.learners) {
-                  if (!learnerIds.has(learnerId)) {
-                    learnerIds.add(learnerId);
-                  }
+        set({ id }) {
+          this.setSelectedClassroomId(id);
+        },
+      },
+
+      filteredLearners() {
+        const learners = this.$store.state.learners;
+        let _learners = learners;
+        if (this.selectedClassroom.id !== constants.ALL_CLASSROOMS_ID) {
+          const learnerGroupIds = this.selectedClassroom.learnerGroups;
+          const learnerIds = new Set();
+          for (const group of this.$store.state.learnerGroups) {
+            if (learnerGroupIds.indexOf(group.id) !== -1) {
+              for (const learnerId of group.learners) {
+                if (!learnerIds.has(learnerId)) {
+                  learnerIds.add(learnerId);
                 }
               }
             }
+          }
 
-            _learners = [];
-            for (const learner of learners) {
-              if (learnerIds.has(learner.id)) {
-                _learners.push(learner);
-              }
+          _learners = [];
+          for (const learner of learners) {
+            if (learnerIds.has(learner.id)) {
+              _learners.push(learner);
             }
           }
-          return _learners;
-        },
+        }
+        return _learners;
       },
-      vuex: {
-        getters: {
-          getClassrooms,
-          getSelectedClassroomId,
-        },
-        actions: {
-          addClassroom,
-          setSelectedClassroomId,
-        },
+    },
+
+    vuex: {
+      getters: {
+        getClassrooms,
+        getSelectedClassroomId,
+      },
+      actions: {
+        addClassroom,
+        setSelectedClassroomId,
       },
     },
   };
