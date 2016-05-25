0b000c20fa9a246cd6f0862d6261a2a0c5878620
66eli77 <66eli77@gmail.com>
2016-05-25 13:32:37 -0700

    add util function sync_contentdb_dir() to ensure the channelmetadata objects in default db are the same as channelmetadata objects in content dbs

--- kolibri/content/utils/validate.py
+++ kolibri/content/utils/validate.py
@@ -1,4 +1,9 @@
+import fnmatch
+import os
 from uuid import UUID
+
+from django.conf import settings
+from kolibri.content.models import ChannelMetadata
 
 
 def is_valid_uuid(uuid_to_test, version=4):
@@ -15,3 +20,22 @@ def is_valid_uuid(uuid_to_test, version=4):
         return False
 
     return str(uuid_obj) == uuid_to_test
+
+def scan_contentdb_dir():
+    db_list = fnmatch.filter(os.listdir(settings.CONTENT_DB_DIR), '*.sqlite3')
+    db_names = [db.split('.sqlite3', 1)[0] for db in db_list]
+    return db_names
+
+def sync_channelmetadata():
+    """
+    Everytime kolibri runs, scan through the settings.CONTENT_DB_DIR folder to collect the names for all ContentDB,
+    and use the name to query each ContentDB to get the ChannelMetadata object,
+    and use them to update the ChannelMetadata object in the default database to ensure they are insync
+    """
+    db_names = scan_contentdb_dir()
+    # delete channelmetadata obejcts in default db that cannot be found in CONTENT_DB_DIR
+    ChannelMetadata.objects.exclude(channel_id__in=db_names)
+    # sync the channelmetadata objects in default db with channelmetadata objects in CONTENT_DB_DIR
+    for db_name in db_names:
+        update_values = ChannelMetadata.objects.using(db_name).values()[0]
+        ChannelMetadata.objects.update_or_create(channel_id=db_name, defaults=update_values)
