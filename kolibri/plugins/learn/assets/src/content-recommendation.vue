69c4b0ca92ac3b8ce7c81ee98f72055be1bafd12
indirectlylit <devon@indirectlylit.com>
2016-06-10 16:55:55 -0700

    additional files

---
+++ kolibri/plugins/learn/assets/src/content-recommendation.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-for="(title,block) in content">
+    <rec-block :title="title" :block="block"></rec-block>
+  </template>
+</template>
+
+<script>
+
+  const content = require('./demo/content_recommendation_data.json');
+  module.exports = {
+    components: {
+      'rec-block': require('./rec-block'),
+    },
+    data: () => ({
+      content,
+    }),
+  };
+</script>
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+</style>
---
+++ kolibri/plugins/learn/assets/src/rec-block.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ title }}</h1>
+  <content-cards :contentcards="block"></content-cards>
+
+
+<!--   <template v-for="b in block">
+    <h2>{{ b.title }}</h2>
+    <ul v-for="(key,val) in b">
+      <li v-if="key=='thumbnail'"><img :src="val"></li>
+      <li>{{ key }}: {{ val }}</li>
+    </ul>
+</template> -->
+</template>
+
+
+<script>
+
+module.exports = {
+  props: ['block', 'title'],
+  components: {
+    'contentcard': require('./content-cards'),
+  },
+};
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+</style>
