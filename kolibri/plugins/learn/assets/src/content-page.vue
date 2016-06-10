b8a32c6fa5cbfe9f2daee0c1bf1e49bcbb91cd92
indirectlylit <devon@indirectlylit.com>
2016-06-10 16:55:17 -0700

    add content-page

---
+++ kolibri/plugins/learn/assets/src/content-page.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <h2>CONTENT!!!</h2>
+  <viewerslot kind="" url="">
+    <div class="obama">
+    </div>
+  </viewerslot>
+  <h3>
+    {{ title }}
+  </h3>
+  <p>
+    {{ description }}
+  </p>
+  <div class="rec-grid">
+    <content-card v-for="content in recommended">
+      <img src="{{ content.thumbnail }}"/>
+    </content-card>
+  </div>
+</template>
+
+
+<script>
+
+  module.exports = {
+    props: ['title', 'description', 'all-contents', 'recommended'],
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+  .rec-grid content-card img
+    max-width: 250px
+    max-height: 250px
+    margin: 0 15px 0 0
+
+  .rec-grid
+    display: inline-block
+
+  .obama
+    background: pink
+    width: 80%
+    height: 400px
+    text-align: center
+
+</style>
