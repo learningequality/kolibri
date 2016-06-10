d81ab2b9f04da0c0b0b4f008164a1303cd9209ad
indirectlylit <devon@indirectlylit.com>
2016-06-10 16:11:58 -0700

    vue.js component whitespace conventions

--- webpack_config/htmlhint-custom.js
+++ webpack_config/htmlhint-custom.js
@@ -43,3 +43,110 @@ HTMLHint.addRule({
         });
     }
 });
+
+/*
+  Vue whitespace conventions.
+  Based on the existing `tag-pair` rule
+
+  This code attempts to enforce:
+  - two blank lines between top-level tags
+  - one blank line of padding within a top-level tag
+  - one level of indent for the contents of all top-level tags
+
+*/
+HTMLHint.addRule({
+    id: '--vue-component-conventions',
+    description: 'Internal vue file conventions.',
+    init: function(parser, reporter) {
+        var self = this;
+        var stack=[];
+        var mapEmptyTags = parser.makeMap("area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed,track,command,source,keygen,wbr");//HTML 4.01 + HTML 5
+        var inTopLevelTag = false;
+        parser.addListener('text', function(event) {
+            var last = event.lastEvent;
+            if (last.type === 'tagstart') {
+                if (stack.length === 1 && last.tagName === "template") {
+                    var match = event.raw.match(/^(\n*)( *)/);
+                    if (match && match[1].length !== 2) {
+                        reporter.error('Top-level content should be surrounded by one empty line.', event.line, event.col, self, event.raw);
+                    }
+                    if (match && match[2].length !== 2) {
+                        reporter.error('Top-level content should be indented two spaces.', event.line, event.col, self, event.raw);
+                    }
+                }
+            }
+        });
+        // handle script and style tags
+        parser.addListener('cdata', function(event){
+            if (stack.length === 1) {
+                if (event.raw && !event.raw.trim()) {
+                    if (event.raw.indexOf('\n') !== -1) {
+                        reporter.error('Empty top-level tags should be on a single line.', event.line, event.col, self, event.raw);
+                    }
+                    else {
+                        reporter.error('Empty top-level tags should not contain spaces.', event.line, event.col, self, event.raw);
+                      }
+                }
+                else {
+                    // note - [^] is like . except it matches newlines
+                    // http://stackoverflow.com/questions/1068280/javascript-regex-multiline-flag-doesnt-work
+                    var match = event.raw.match(/^(\n*)( *)[^]+?(\n*)$/);
+                    if (match) {
+                        if (match && match[1].length !== 2) {
+                            reporter.error('Top-level content should be surrounded by one empty line.', event.line, event.col, self, event.raw);
+                        }
+                        if (match && match[2].length !== 2) {
+                            reporter.error('Top-level content should be indented two spaces.', event.line, event.col, self, event.raw);
+                        }
+                        if (match && match[3].length !== 2) {
+                            var offset = (event.raw.match(/\n/g) || []).length;
+                            reporter.error('Top-level content should be surrounded by one empty line.', event.line+offset, 1, self, event.raw);
+                        }
+                    }
+                }
+            }
+        });
+        parser.addListener('tagstart', function(event) {
+            if (!stack.length) {
+                if (event.lastEvent.type === "start") {
+                    if (event.line !== 1) {
+                        reporter.error('Content should start on the first line of the file.', event.line, event.col, self, event.raw);
+                    }
+                }
+                else if (event.lastEvent.raw !== "\n\n\n") {
+                    reporter.error('Need two endlines between top-level tags.', event.line, event.col, self, event.raw);
+                }
+            }
+            var tagName = event.tagName.toLowerCase();
+            if (mapEmptyTags[tagName] === undefined && !event.close) {
+                stack.push({
+                    tagName: tagName,
+                    line: event.line,
+                    raw: event.raw
+                });
+            }
+        });
+        parser.addListener('tagend', function(event) {
+            var last = event.lastEvent;
+            if (last.type === 'text') {
+                if (stack.length === 1 && event.tagName === "template") {
+                    var match = last.raw.match(/(\n*)$/);
+                    if (match && match[1].length !== 2) {
+                        reporter.error('Top-level content should be surrounded by one empty line.', event.line, event.col, self, event.raw);
+                    }
+                }
+            }
+            var tagName = event.tagName.toLowerCase();
+            for (var pos = stack.length-1;pos >= 0; pos--) {
+                if(stack[pos].tagName === tagName) {
+                    break;
+                }
+            }
+            var arrTags = [];
+            for (var i=stack.length-1;i>pos;i--) {
+                arrTags.push('</'+stack[i].tagName+'>');
+            }
+            stack.length=pos;
+        });
+    }
+});
