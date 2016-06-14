90d4e0cd6d591c748b533985562bfffbdcd2aeab
indirectlylit <devon@indirectlylit.com>
2016-06-13 18:25:59 -0700

    WIP - adding in vue-router

--- kolibri/plugins/learn/assets/src/app-root.vue
+++ kolibri/plugins/learn/assets/src/app-root.vue
@@ -1,7 +1,17 @@
 <template>
 
   <core-base>
+
+    <p>
+      <!-- use v-link directive for navigation. -->
+      <a v-link="{ path: '/foo' }">Go to Foo</a>
+      <a v-link="{ path: '/bar' }">Go to Bar</a>
+    </p>
+
+    <router-view></router-view>
+
     <topic-page></topic-page>
+
   </core-base>
 
 </template>
--- kolibri/plugins/learn/assets/src/app.js
+++ kolibri/plugins/learn/assets/src/app.js
@@ -1,15 +1,36 @@
 const KolibriModule = require('kolibri_module');
 const Vue = require('vue');
+const VueRouter = require('vue-router');
+Vue.use(VueRouter);
 
 class LearnModule extends KolibriModule {
   ready() {
+    const Foo = Vue.extend({
+      template: '<p>This is foo!</p>',
+    });
+
+    const Bar = Vue.extend({
+      template: '<p>This is bar!</p>',
+    });
+
     // attaches the root learn module to the `app-root` tag
-    this.vm = new Vue({
-      el: 'body',
+    const main = Vue.extend({
       components: {
         'app-root': require('./app-root'),
       },
     });
+
+    this.router = new VueRouter();
+    this.router.map({
+      '/foo': {
+        component: Foo,
+      },
+      '/bar': {
+        component: Bar,
+      },
+    });
+
+    this.router.start(main, 'body');
   }
 }
 
