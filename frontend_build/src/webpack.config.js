d9138773a6fe054b8e7a7dc68b9388399644a881
indirectlylit <devon@indirectlylit.com>
2016-06-15 10:13:54 -0700

    move `libs` function inside `read_bundle_plugins`

--- frontend_build/src/webpack.config.js
+++ frontend_build/src/webpack.config.js
@@ -6,27 +6,9 @@
  * configurations and Webpack documentation for details on what the configuration bundles do.
  */
 
-
 var path = require('path');
 var webpack = require('webpack');
 var logging = require('./logging');
 var readBundlePlugins = require('./read_bundle_plugins');
 
-
-// Mappings for libraries that we bundle in the Kolibri core app.
-// * the keys are names exposed by webpack to use in `require` statements, across apps
-// * the values are references to the packages, already inserted into kolibriGlobal
-//
-// kolibri_name is always == kolibriGlobal (this is defined in the base settings - base.py)
-var libs = function(kolibri_name) {
-  return {
-    'loglevel': kolibri_name + '.lib.loglevel',
-    'vue': kolibri_name + '.lib.vue',
-    'kolibri': kolibri_name,
-    'core-base': kolibri_name + '.lib.coreBase',
-  };
-};
-
-var bundles = readBundlePlugins(path.join(__dirname, '..', '..'), libs);
-
-module.exports = bundles;
+module.exports = readBundlePlugins(path.join(__dirname, '..', '..'));
