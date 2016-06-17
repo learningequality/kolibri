c72da56bd8cb467dc370754afe3f4b2411ec3a1e
indirectlylit <devon@indirectlylit.com>
2016-06-17 07:58:35 -0700

    add router-sync

--- kolibri/plugins/learn/assets/src/app.js
+++ kolibri/plugins/learn/assets/src/app.js
@@ -1,9 +1,12 @@
 const KolibriModule = require('kolibri_module');
 const rootvue = require('./vue');
 const router = require('./router');
+const store = require('./store');
+const sync = require('vuex-router-sync').sync;
 
 class LearnModule extends KolibriModule {
   ready() {
+    sync(store, router);
     router.start(rootvue, 'rootvue');
   }
 }
