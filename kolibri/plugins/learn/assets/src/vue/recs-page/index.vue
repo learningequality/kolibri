3c6a3c91bd7d063c7420987014286ea6af0ca492
indirectlylit <devon@indirectlylit.com>
2016-06-17 15:33:11 -0700

    break up component

---
+++ kolibri/plugins/learn/assets/src/vue/recs-page/index.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>
+    <block v-for="(title, block) in contents"
+      :titleid="title"
+      :block="block"
+    ></block>
+    <input @click="clicked"></input>
+  </div>
+
+</template>
+
+
+<script>
+
+  module.exports = {
+    components: {
+      block: require('./block'),
+    },
+    vuex: {
+      getters: {
+        // better practice would be to define vuex getter functions globally
+        contents: state => state.recommended,
+      },
+    },
+    methods: {
+      clicked() {
+        alert('hi');
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+</style>
