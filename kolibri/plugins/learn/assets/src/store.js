4498addc2ec7c4204e8ab2e2ea6b21391a34f3e3
indirectlylit <devon@indirectlylit.com>
2016-06-17 12:34:35 -0700

    comment out refs to demo files which broke the build

--- kolibri/plugins/learn/assets/src/store.js
+++ kolibri/plugins/learn/assets/src/store.js
@@ -6,11 +6,11 @@ Vue.use(Vuex);
 
 function initialState() {
   return {
-    breadcrumbs: require('./demo/breadcrumbs.json'),
-    topics: require('./demo/graphing__topics_only.json'),
-    contents: require('./demo/inequalities__content_only.json'),
-    recommended: require('./demo/content_recommendation_data.json'),
-    full: require('./demo/video__full_metadata.json'),
+    // breadcrumbs: require('./demo/breadcrumbs.json'),
+    // topics: require('./demo/graphing__topics_only.json'),
+    // contents: require('./demo/inequalities__content_only.json'),
+    // recommended: require('./demo/content_recommendation_data.json'),
+    // full: require('./demo/video__full_metadata.json'),
   };
 }
 
