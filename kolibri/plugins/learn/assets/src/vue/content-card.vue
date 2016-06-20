44cbe0bbe275e13878ff4d44b5d451e1b7603320
whitzhu <whitzhu@gmail.com>
2016-06-20 13:32:55 -0700

    Updated card text font size and font weight. Also updated card dimension

--- kolibri/plugins/learn/assets/src/vue/content-card.vue
+++ kolibri/plugins/learn/assets/src/vue/content-card.vue
@@ -32,24 +32,24 @@
   @require '~core-theme.styl'
 
   .content-card-container
-    width: 14em
-    height: 11.5em
+    width: 13.6em
+    height: 11em
     background-color: $core-bg-light
-    text-align: left
     border-radius: 4px
 
   .thumbnail
-    width: 14em
+    width: 13.6em
     height: 120px
     border-radius: 4px 4px 0 0
 
   .title
-    max-width: 14em
+    max-width: 13.6em
     max-height: 3em
-    margin: 0.4em 0.5em
+    margin: 0.4em
     overflow: hidden
     line-height: 1.5em
-    font-size: 1em
+    font-size: 0.9em
+    font-weight: 700
     color: $core-text-default
 
 </style>
