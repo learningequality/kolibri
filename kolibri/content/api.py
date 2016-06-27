ac8f0c9bf5aa5cdc4d37f485699c7b01ec9e7685
66eli77 <66eli77@gmail.com>
2016-06-27 15:38:26 -0700

    replace can_get_content_with_id() decorator with get_instance_with_pk_or_uuid() method

--- kolibri/content/api.py
+++ kolibri/content/api.py
@@ -2,8 +2,6 @@
 This module acts as the only interface point between other apps and the database backend for the content.
 It exposes several convenience functions for accessing content
 """
-from functools import wraps
-
 from django.db.models import Q
 from kolibri.content import models as KolibriContent
 from kolibri.content.utils import validate
@@ -11,50 +9,31 @@ from .constants import content_kinds
 
 """ContentDB API methods"""
 
-def can_get_content_with_id(func):
-    """
-    decorator function for returning ContentNode object when giving content id.
-    it can take keyword argument/s "content" or "content1" and "content2".
-    """
-    @wraps(func)
-    def wrapper(channel_id, **kwargs):
-        content = kwargs.get('content')
-        content1 = kwargs.get('content1')
-        content2 = kwargs.get('content2')
-        target_node = kwargs.get('target_node')
-        prerequisite = kwargs.get('prerequisite')
+def get_instance_with_pk_or_uuid(channel_id, content):
+    if validate.is_valid_uuid(content):
+        return KolibriContent.ContentNode.objects.using(channel_id).get(content_id=content)
+    elif isinstance(content, KolibriContent.ContentNode):
+        return content
+    else:
+        try:
+            pk = int(content)
+        except (ValueError, TypeError):
+            raise TypeError("Must provide a pk or a ContentNode object or a UUID content_id.")
+        return KolibriContent.ContentNode.objects.using(channel_id).get(pk=pk)
 
-        if isinstance(content, KolibriContent.ContentNode) or \
-                (isinstance(content1, KolibriContent.ContentNode) and isinstance(content2, KolibriContent.ContentNode)) or \
-                (isinstance(target_node, KolibriContent.ContentNode) and isinstance(prerequisite, KolibriContent.ContentNode)):
-            pass
-        elif validate.is_valid_uuid(content):
-            kwargs['content'] = KolibriContent.ContentNode.objects.using(channel_id).get(content_id=content)
-        elif validate.is_valid_uuid(content1) and validate.is_valid_uuid(content2):
-            kwargs['content1'] = KolibriContent.ContentNode.objects.using(channel_id).get(content_id=content1)
-            kwargs['content2'] = KolibriContent.ContentNode.objects.using(channel_id).get(content_id=content2)
-        elif validate.is_valid_uuid(target_node) and validate.is_valid_uuid(prerequisite):
-            kwargs['target_node'] = KolibriContent.ContentNode.objects.using(channel_id).get(content_id=target_node)
-            kwargs['prerequisite'] = KolibriContent.ContentNode.objects.using(channel_id).get(content_id=prerequisite)
-        else:
-            raise TypeError("must provide a ContentNode object or a UUID content_id")
-        return func(channel_id, **kwargs)
-    return wrapper
-
-def get_content_with_id(channel_id, content):
+def get_content_with_id_list(channel_id, content):
     """
-    Get arbitrary sets of ContentNode objects based on content id(s).
+    Get arbitrary sets of ContentNode objects based on content ids.
 
     :param channel_id: str
-    :param content_id: list or str or uuid
+    :param content_id: list of uuid
     :return: QuerySet of ContentNode
     """
     if isinstance(content, list):
         return KolibriContent.ContentNode.objects.using(channel_id).filter(content_id__in=content)
     else:
-        return KolibriContent.ContentNode.objects.using(channel_id).filter(content_id=content)
+        raise TypeError("Must provide a list of UUID content_id in order to use this method.")
 
-@can_get_content_with_id
 def get_ancestor_topics(channel_id, content, **kwargs):
     """"
     Get all ancestors that the their kind are topics
@@ -63,9 +42,9 @@ def get_ancestor_topics(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of ContentNode
     """
-    return content.get_ancestors().filter(kind=content_kinds.TOPIC).using(channel_id)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return content_instance.get_ancestors().filter(kind=content_kinds.TOPIC).using(channel_id)
 
-@can_get_content_with_id
 def immediate_children(channel_id, content, **kwargs):
     """
     Get a set of ContentNodes that have this ContentNode as the immediate parent.
@@ -74,9 +53,9 @@ def immediate_children(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of ContentNode
     """
-    return content.get_children().using(channel_id)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return content_instance.get_children().using(channel_id)
 
-@can_get_content_with_id
 def leaves(channel_id, content, **kwargs):
     """
     Get all ContentNodes that are the terminal nodes and also the descendants of the this ContentNode.
@@ -85,9 +64,9 @@ def leaves(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of ContentNode
     """
-    return content.get_leafnodes().using(channel_id)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return content_instance.get_leafnodes().using(channel_id)
 
-@can_get_content_with_id
 def get_missing_files(channel_id, content, **kwargs):
     """
     Get all missing files of the content.
@@ -96,13 +75,13 @@ def get_missing_files(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of File
     """
-    if content.kind == content_kinds.TOPIC:
-        all_end_nodes = leaves(channel_id=channel_id, content=content)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    if content_instance.kind == content_kinds.TOPIC:
+        all_end_nodes = leaves(channel_id=channel_id, content=content_instance)
         return KolibriContent.File.objects.using(channel_id).filter(available=False, contentnode__in=all_end_nodes)
     else:
         return KolibriContent.File.objects.using(channel_id).filter(available=False, contentnode=content)
 
-@can_get_content_with_id
 def get_all_prerequisites(channel_id, content, **kwargs):
     """
     Get cotents that are the prerequisites of this content.
@@ -111,9 +90,9 @@ def get_all_prerequisites(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of ContentNode
     """
-    return KolibriContent.ContentNode.objects.using(channel_id).filter(is_prerequisite_of=content)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return KolibriContent.ContentNode.objects.using(channel_id).filter(is_prerequisite_of=content_instance)
 
-@can_get_content_with_id
 def get_all_related(channel_id, content, **kwargs):
     """
     Get cotents that are related to this content.
@@ -122,9 +101,9 @@ def get_all_related(channel_id, content, **kwargs):
     :param content: ContentNode or str
     :return: QuerySet of ContentNode
     """
-    return KolibriContent.ContentNode.objects.using(channel_id).filter(Q(relate_to=content) | Q(is_related=content))
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return KolibriContent.ContentNode.objects.using(channel_id).filter(Q(relate_to=content_instance) | Q(is_related=content_instance))
 
-@can_get_content_with_id
 def set_prerequisite(channel_id, target_node, prerequisite, **kwargs):
     """
     Set prerequisite relationship between content1 and content2.
@@ -133,10 +112,11 @@ def set_prerequisite(channel_id, target_node, prerequisite, **kwargs):
     :param content1: ContentNode or str
     :param content2: ContentNode or str
     """
+    target_instance = get_instance_with_pk_or_uuid(channel_id, target_node)
+    prerequisite_instance = get_instance_with_pk_or_uuid(channel_id, prerequisite)
     KolibriContent.PrerequisiteContentRelationship.objects.using(channel_id).create(
-        target_node=target_node, prerequisite=prerequisite)
+        target_node=target_instance, prerequisite=prerequisite_instance)
 
-@can_get_content_with_id
 def set_is_related(channel_id, content1, content2, **kwargs):
     """
     Set is related relationship between content1 and content2.
@@ -145,10 +125,11 @@ def set_is_related(channel_id, content1, content2, **kwargs):
     :param content1: ContentNode or str
     :param content2: ContentNode or str
     """
+    content1_instance = get_instance_with_pk_or_uuid(channel_id, content1)
+    content2_instance = get_instance_with_pk_or_uuid(channel_id, content2)
     KolibriContent.RelatedContentRelationship.objects.using(channel_id).create(
-        contentnode_1=content1, contentnode_2=content2)
+        contentnode_1=content1_instance, contentnode_2=content2_instance)
 
-@can_get_content_with_id
 def children_of_kind(channel_id, content, kind, **kwargs):
     """
     Get all ContentNodes of a particular kind under the given ContentNode.
@@ -159,4 +140,5 @@ def children_of_kind(channel_id, content, kind, **kwargs):
     :param kind: str
     :return: QuerySet of ContentNode
     """
-    return content.get_descendants(include_self=False).filter(kind=kind).using(channel_id)
+    content_instance = get_instance_with_pk_or_uuid(channel_id, content)
+    return content_instance.get_descendants(include_self=False).filter(kind=kind).using(channel_id)
