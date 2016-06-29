c97f4fb9c67da0e8df0246749f922645c74a1254
Rafael Aguayo <ralphaguayo22@gmail.com>
2016-06-29 15:49:11 -0700

    Updated field options for some model attributes.

--- kolibri/logger/models.py
+++ kolibri/logger/models.py
@@ -12,13 +12,13 @@ class ContentInteractionLog(models.Model):
     """
     This Model provides a record of an interaction with a content item.
     """
-    user = models.ForeignKey(FacilityUser)
-    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    start_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
-    completion_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
+    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    start_timestamp = models.DateTimeField(blank=False, null=False)
+    completion_timestamp = models.DateTimeField(blank=True, null=True)
     item_session = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    kind = ""  # indicates how extra_fields should be interpreted
+    kind = models.CharField(max_length=200, blank=True)  # indicates how extra_fields should be interpreted
     extra_fields = models.TextField(blank=True)
 
 
@@ -26,13 +26,14 @@ class ContentSummaryLog(models.Model):
     """
     This Model provides a summary of all interactions of a user with a content item.
     """
-    user = models.ForeignKey(FacilityUser)
-    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    last_channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    start_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
-    last_activity_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
-    completion_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
+    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    last_channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    start_timestamp = models.DateTimeField(blank=False, null=False)
+    last_activity_timestamp = models.DateTimeField(blank=True, null=True)
+    completion_timestamp = models.DateTimeField(blank=True, null=True)
     progress = models.DecimalField(default=0.0, max_digits=2, decimal_places=1)
+    kind = models.CharField(max_length=200, blank=True)  # indicates how extra_fields should be interpreted
     extra_fields = models.TextField(blank=True)
 
 
@@ -40,12 +41,12 @@ class ContentRatingLog(models.Model):
     """
     This Model provides a record of user feedback on content.
     """
-    user = models.ForeignKey(FacilityUser)
-    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    quality = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    ease = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    learning = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
+    content_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    channel_id = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, db_index=True)
+    quality = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    ease = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    learning = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     feedback = models.TextField(blank=True)
 
 
@@ -53,8 +54,8 @@ class UserSessionLog(models.Model):
     """
     This Model provides a record of a user session in Kolibri.
     """
-    user = models.ForeignKey(FacilityUser)
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
     channels = models.TextField(blank=True)
-    start_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
-    completion_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_timestamp = models.DateTimeField(blank=False, null=False)
+    completion_timestamp = models.DateTimeField(blank=True, null=True)
     pages = models.TextField(blank=True)
