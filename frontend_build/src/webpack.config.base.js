9dde7ca98d517e1f2ab274ace549a6bf095dd456
Richard Tibbles <richard.tibbles@gmail.com>
2016-06-30 12:24:46 -0700

    Force babel transpilation for IE9 compatibility.

--- frontend_build/src/webpack.config.base.js
+++ frontend_build/src/webpack.config.base.js
@@ -41,7 +41,11 @@ var config = {
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015-ie'],
+          plugins: ['transform-runtime']
+        },
       },
       {
         test: /\.json$/,
