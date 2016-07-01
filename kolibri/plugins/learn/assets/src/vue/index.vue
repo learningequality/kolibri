afd61b10020fa334689abb5584eff02ad084ecbb
whitzhu <whitzhu@gmail.com>
2016-06-30 17:04:48 -0700

    Updated hover svg color

--- kolibri/plugins/learn/assets/src/vue/index.vue
+++ kolibri/plugins/learn/assets/src/vue/index.vue
@@ -4,19 +4,23 @@
 
     <nav id="learn-nav">
       <ul>
-        <a v-link="{ path: '/learn', exact: true }"><li><span>
+        <a v-link="{ path: '/learn', exact: true }">
+          <li><span>
           <svg fill="#000000" height="40" viewbox="0 0 24 24" width="40" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path>
-              <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path>
+            <path d="M0 0h24v24H0z" fill="none"></path>
           </svg>
-          Learn</span></li>
+          Learn
+          </span></li>
         </a>
-        <a v-link="{ path: '/explore', exact: true }"><li><span>
+        <a v-link="{ path: '/explore', exact: true }">
+          <li><span>
           <svg fill="#000000" height="40" viewbox="0 0 24 24" width="40" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z"></path>
-              <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z"></path>
+            <path d="M0 0h24v24H0z" fill="none"></path>
           </svg>
-          Explore</span></li>
+          Explore
+          </span></li>
         </a>
         <a v-link="{ path: '/learn/content', exact: true }"><li><span>Learn Content</span></li></a>
         <a v-link="{ path: '/explore/content', exact: true }"><li><span>
@@ -24,8 +28,10 @@
       </ul>
     </nav>
 
-    <!-- see router.js -->
-    <router-view></router-view>
+    <div class="page-wrapper">
+      <!-- see router.js -->
+      <router-view></router-view>
+    </div>
 
   </core-base>
 
@@ -49,6 +55,7 @@
 
   @require '~core-theme.styl'
 
+  // Navbar styling
   $nav-bar-width = 80
   $nav-element-height = 150
   $font-size = 1em
@@ -71,30 +78,42 @@
     padding: 0
     list-style-type: none
 
-    a
-      display: block
-      margin: 0
-      padding: 0
+  a:hover
+    svg
+      fill: $core-action-dark
 
-      li
-        display: table
-        height: $nav-element-height
+  a
+    display: block
+    margin: 0
+    padding: 0
 
-      span
-        display: table-cell
-        vertical-align: middle
+    li
+      display: table
+      height: $nav-element-height
 
-      svg
-        fill: $core-action-normal
+    span
+      display: table-cell
+      vertical-align: middle
+
+    svg
+      fill: $core-action-normal
 
   // this class is automatically added to links with the v-link directive
-  .v-link-active
+  a.v-link-active
     color: $core-bg-light
     background: $core-action-normal
     cursor: default
 
     svg
       fill: $core-bg-light
+
+  a.v-link-active:hover
+    svg
+      fill: $core-bg-light
+
+  // Page wrapper styling
+  .page-wrapper
+    margin-left: $nav-bar-width
 
 </style>
 
