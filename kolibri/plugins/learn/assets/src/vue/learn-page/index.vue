c3df688a8758b9b95d690273dad03e1ecef1f56a
indirectlylit <devon@indirectlylit.com>
2016-07-06 11:37:35 -0700

    remove learn-page placeholder content

--- kolibri/plugins/learn/assets/src/vue/learn-page/index.vue
+++ kolibri/plugins/learn/assets/src/vue/learn-page/index.vue
@@ -1,21 +1,7 @@
 <template>
 
   <div>
-    <div v-for="(title, block) in contents">
-      <h1>{{ title }}</h1>
-      <div class="card-list">
-        <content-card
-          v-for="content in block"
-          class="card"
-          linkhref="#"
-          :title="content.title"
-          :thumbsrc="content.thumbnail"
-          :kind="content.kind"
-          :progress="content.progress"
-          :pk="content.id">
-        </content-card>
-      </div>
-    </div>
+    TODO
   </div>
 
 </template>
@@ -27,19 +13,9 @@
     components: {
       'content-card': require('../content-card'),
     },
-    vuex: {
-      getters: {
-        // better practice would be to define vuex getter functions globally
-        contents: state => state.recommended,
-      },
-    },
   };
 
 </script>
 
 
-<style lang="stylus" scoped>
-
-  @require '~core-theme.styl'
-
-</style>
+<style lang="stylus" scoped></style>
