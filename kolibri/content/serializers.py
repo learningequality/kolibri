73800cc0b5b1a86b587a58938fecff950222e60e
Richard Tibbles <richard.tibbles@gmail.com>
2016-07-08 00:50:26 -0700

    Adds thumbnail computed field to ContentNode Serializer.

--- kolibri/content/serializers.py
+++ kolibri/content/serializers.py
@@ -28,6 +28,7 @@ class ContentNodeSerializer(serializers.ModelSerializer):
     # extension and checksum field along with the setting.STORAGE_ROOT, which can be passed to front end at template boostrapping.
     files = FileSerializer(many=True, read_only=True)
     ancestors = serializers.SerializerMethodField()
+    thumbnail = serializers.SerializerMethodField()
 
     def __init__(self, *args, **kwargs):
         # Instantiate the superclass normally
@@ -48,9 +49,15 @@ class ContentNodeSerializer(serializers.ModelSerializer):
         """
         return target_node.get_ancestors().values('pk', 'title')
 
+    def get_thumbnail(self, target_node):
+        try:
+            return target_node.files.get(thumbnail=True).get_url()
+        except File.DoesNotExist:
+            return None
+
     class Meta:
         model = ContentNode
         fields = (
             'pk', 'content_id', 'title', 'description', 'kind', 'available', 'tags', 'sort_order', 'license_owner',
-            'license', 'files', 'ancestors', 'parent'
+            'license', 'files', 'ancestors', 'parent', 'thumbnail'
         )
