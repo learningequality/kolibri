12528d9b4e58f3aaa867bfdd19fedb9b58b30a63
indirectlylit <devon@indirectlylit.com>
2016-07-11 17:28:19 -0700

    `content_id`s were coming back as null - switched with `pk`s

--- kolibri/content/api.py
+++ kolibri/content/api.py
@@ -40,11 +40,11 @@ class ContentNodeFilter(filters.FilterSet):
 
     def filter_recommendations(self, queryset, value):
         # if ContentInteractionLog.objects.count() == 0:
-        content_ids = queryset.values_list('content_id', flat=True).exclude(kind='topic')
+        pks = queryset.values_list('pk', flat=True).exclude(kind='topic')
         count = queryset.count()
         if count > 100:
-            count = 100
-        return queryset.filter(content_id__in=sample(list(content_ids), count))  # return 100 random content nodes
+            count = 25
+        return queryset.filter(pk__in=sample(list(pks), count))  # return 100 random content nodes
         #     content_counts_sorted = ContentInteractionLog.objects.values('content_id').annotate(Count('content_id')).order_by('-content_id__count')
         #     return queryset.filter(
         #         content_id__in=[content['content_id'] for content in content_counts_sorted][:10])  # return the 10 most frequently accessed pieces of content
