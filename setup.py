a3a85694898139b2ae8f97c8e00e45ffaadbb76a
Aron Fyodor Asor <aronasorman@gmail.com>
2016-07-13 14:55:16 -0700

    Move requirements/base.txt to install_requires.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import os
 import shutil
 import sys
 
-from pip.req import parse_requirements
 from setuptools import setup
 from setuptools.command.install_scripts import install_scripts
 
@@ -54,14 +53,29 @@ is_building_dist = any(
 static_requirements = []
 static_dir = os.path.dirname(os.path.realpath(kolibri_dist.__file__))
 
-install_requires = [str(ir.req) for ir in parse_requirements('./requirements.txt', session=False)]
+install_requires = [
+    'cherrypy==6.0.2',
+    'django-filter==0.13.0',
+    'django-js-reverse==0.7.2',
+    'six==1.10.0',
+    'djangorestframework==3.3.3',
+    'django==1.9.7',
+    'colorlog==2.6.0',
+    'docopt==0.6.2',
+    'django-mptt>=0.8.4',              # apparently you also need to have the dependency_links packages be included in install_requires
+]
+
+dependency_links = [
+    'http://github.com/django-mptt/django-mptt/tarball/9e131f91#egg=django-mptt-0.8.4'
+]
 
 # Check if user supplied the special '--static' option
 if '--static' in sys.argv:
     sys.argv.remove('--static')
     dist_name = 'kolibri-static'
     description += " This static version bundles all dependencies."
-    install_requires, static_requirements = [], install_requires
+    install_requires, static_requirements = [], (install_requires + dependency_links)
+    dependency_links = []
     static_build = True
 
 
@@ -195,7 +209,6 @@ elif is_building_dist:
             )
         )
 
-
 setup(
     name=dist_name,
     version=kolibri.__version__,
@@ -218,6 +231,7 @@ setup(
     package_dir={'kolibri': 'kolibri'},
     include_package_data=True,
     install_requires=install_requires,
+    dependency_links=dependency_links,
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'tox', 'flake8'],
     license='MIT',
