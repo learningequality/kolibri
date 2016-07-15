9f15afb8b7040723199a56fb17c0b4799344dc86
indirectlylit <devon@indirectlylit.com>
2016-07-15 14:25:44 -0700

    positioning of search button

--- kolibri/plugins/learn/assets/src/vue/index.vue
+++ kolibri/plugins/learn/assets/src/vue/index.vue
@@ -8,14 +8,16 @@
 
       <side-nav class='nav'></side-nav>
 
-      <button class='search-btn' :class="{ active: searchOpen }" @click='toggleSearch'>
-        <svg height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-          <path d="M0 0h24v24H0z" fill="none"></path>
-        </svg>
-      </button>
-
       <main role="main" class="page-content" v-if='!loading'>
+
+
+        <button class='search-btn' :class="{ active: searchOpen }" @click='toggleSearch'>
+          <svg height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+            <path d="M0 0h24v24H0z" fill="none"></path>
+          </svg>
+        </button>
+
         <explore-page v-if='showExplorePage'></explore-page>
         <content-page v-if='showContentPage'></content-page>
         <learn-page v-if='showLearnPage'></learn-page>
@@ -110,11 +112,19 @@
     z-index: 2
 
   .search-btn
+    // position search button to always be in the right-hand margin
+    $offset = $nav-bar-width + $nav-bar-padding + ($right-margin / 3)
+    left: $card-width + $offset
+    for $n-cols in $n-cols-array
+      $grid-width = grid-width($n-cols)
+      @media (min-width: breakpoint($grid-width))
+        left: $grid-width + $offset
+
     position: fixed
     top: 1em
-    right: 2em
     z-index: 1
     border: none
+
     svg
       fill: $core-action-normal
     &.active
