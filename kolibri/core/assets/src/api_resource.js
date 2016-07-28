07d0c9647ea201ab527b76f09bad8c9e37cd6556
66eli77 <66eli77@gmail.com>
2016-07-28 10:51:34 -0700

    add a function to retrieve cookies according to cookie name

--- kolibri/core/assets/src/api_resource.js
+++ kolibri/core/assets/src/api_resource.js
@@ -35,6 +35,23 @@ class Model {
     this.promises = [];
   }
 
+  getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name.concat('='))) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+
   /**
    * Method to fetch data from the server for this particular model.
    * @param {object} params - an object of parameters to be parsed into GET parameters on the
