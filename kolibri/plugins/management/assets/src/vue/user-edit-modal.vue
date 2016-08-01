eab6a3695c5dfdffdbb7619c67d6bece0a1eb9cf
indirectlylit <devon@indirectlylit.com>
2016-08-01 14:34:07 -0700

    delete more names

--- kolibri/plugins/management/assets/src/vue/user-edit-modal.vue
+++ kolibri/plugins/management/assets/src/vue/user-edit-modal.vue
@@ -9,8 +9,8 @@
         <br>First name: <input type="text" v-model="firstName" placeholder="Please type in your first name."><br>
         <br>Last name: <input type="text" v-model="lastName" placeholder="Please type in your last name."><br>
         <!-- radio buttons for selecting role -->
-        <br>Learner <input type="radio" name="picked" value="learner" v-model="role"><br>
-        <br>Admin <input type="radio" name="picked" value="admin" v-model="role"><br>
+        <br>Learner <input type="radio" value="learner" v-model="role"><br>
+        <br>Admin <input type="radio" value="admin" v-model="role"><br>
       </div>
       <div slot="footer">
         <button class="confirm-btn" type="button" @click="editUser">Confirm</button>
