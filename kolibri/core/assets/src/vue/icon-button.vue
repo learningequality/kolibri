5812050d08d5ec8df164ed1a6d087ee34a3a363a
indirectlylit <devon@indirectlylit.com>
2016-08-02 16:26:52 -0700

    add component

---
+++ kolibri/core/assets/src/vue/icon-button.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <button class="icon-button">
+    <slot></slot>
+    {{ text }}
+  </button>
+
+</template>
+
+
+<script>
+
+  module.exports = {
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus">
+
+  @require '~core-theme.styl'
+
+  .icon-button
+
+    svg
+      vertical-align: middle
+      fill: $core-action-normal
+
+    &:hover svg
+      fill: $core-action-dark
+
+</style>
+
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+  .icon-button
+    padding-right: 8px
+
+</style>
