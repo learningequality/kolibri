f30dc76b73dcf6275c0b6b4c119081c8b8537316
Jamie Alexandre <jamalex@gmail.com>
2016-08-02 14:03:14 -0700

    Extended CSV export viewset for ContentSummaryLogs, using parent class.

--- kolibri/logger/csv.py
+++ kolibri/logger/csv.py
@@ -1,41 +1,52 @@
 from kolibri.auth.api import KolibriAuthPermissions, KolibriAuthPermissionsFilter
-from rest_framework import serializers, viewsets
-from rest_framework_csv.renderers import CSVRenderer
+from kolibri.content.content_db_router import using_content_database
+from kolibri.content.models import ChannelMetadataCache, ContentNode
+from kolibri.core.api import CSVModelViewSet
+from rest_framework import serializers
 
 from .models import ContentSummaryLog
-
-
-class CSVModelViewSet(viewsets.ModelViewSet):
-
-    csv_export_filename = 'export'
-
-    def __init__(self, *args, **kwargs):
-        self.renderer_classes = self.generate_csv_renderer()
-
-    def generate_csv_renderer(self):
-        class_name = self.serializer_class.__name__.replace("Serializer", "Renderer")
-        parents = (CSVRenderer,)
-        header = self.serializer_class.Meta.fields
-        renderer_class = type(class_name, parents, {"header": header})
-        return (renderer_class,)
-
-    def finalize_response(self, request, response, *args, **kwargs):
-        response = super(CSVModelViewSet, self).finalize_response(request, response, *args, **kwargs)
-        response['content-disposition'] = 'attachment; filename=%s.csv' % self.csv_export_filename
-        return response
 
 
 class ContentSummaryLogCSVSerializer(serializers.ModelSerializer):
 
     username = serializers.SerializerMethodField()
+    facility_name = serializers.SerializerMethodField()
+    channel_name = serializers.SerializerMethodField()
+    content_title = serializers.SerializerMethodField()
 
     class Meta:
         model = ContentSummaryLog
-        fields = ('user', 'username', 'content_id', 'channel_id', 'start_timestamp',
-                  'last_activity_timestamp', 'completion_timestamp', 'progress', 'kind', 'extra_fields')
+        fields = ('username', 'facility_name', 'content_id', 'content_title', 'channel_id', 'channel_name', 'start_timestamp',
+                  'last_activity_timestamp', 'completion_timestamp', 'progress', 'kind')
+        labels = {
+            "start_timestamp": "Time of first interaction",
+            "last_activity_timestamp": "Time of last interaction",
+            "completion_timestamp": "Time of completion",
+        }
 
     def get_username(self, obj):
         return obj.user.username
+
+    def get_facility_name(self, obj):
+        return obj.user.facility.name
+
+    def get_channel_name(self, obj):
+        try:
+            channel = ChannelMetadataCache.objects.get(id=obj.channel_id)
+        except ChannelMetadataCache.DoesNotExist:
+            return ""
+        return channel.name
+
+    def get_content_title(self, obj):
+        try:
+            with using_content_database(obj.channel_id):
+                node = ContentNode.objects.filter(content_id=obj.content_id).first()
+                if node:
+                    return node.title
+                else:
+                    return ""
+        except KeyError:  # content DB doesn't exist
+            return ""
 
 
 class ContentSummaryLogCSVExportViewSet(CSVModelViewSet):
