218e014964f6233462fe580242cfc3031eb532f1
indirectlylit <devon@indirectlylit.com>
2016-08-04 15:29:04 -0700

    adding some session info to the core vuex store

--- kolibri/core/assets/src/core-state.js
+++ kolibri/core/assets/src/core-state.js
@@ -1,14 +1,35 @@
 
 
+const UserKinds = require('./constants').UserKinds;
+
 // core state is namespaced, and merged with a particular app's state
 const initialState = {
   core: {
     error: '',
     loading: true,
+    session: {
+      kind: UserKinds.ADMIN,
+      facility_id: '1',
+      user_id: '2',
+      username: 'starchy52',
+      fullname: 'Mr. Potato Head',
+    },
   },
 };
 
 const mutations = {
+  CORE_SET_SESSION(state, value) {
+    state.core.session = value;
+  },
+  CORE_CLEAR_SESSION(state) {
+    state.core.session = {
+      kind: UserKinds.ANONYMOUS,
+      facility_id: undefined,
+      user_id: undefined,
+      username: undefined,
+      fullname: undefined,
+    };
+  },
   CORE_SET_PAGE_LOADING(state, value) {
     state.core.loading = value;
   },
