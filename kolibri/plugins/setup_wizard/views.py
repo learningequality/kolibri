6f93fe8f66ff660cc6ce0df555d6722e6ccbfe20
66eli77 <66eli77@gmail.com>
2016-08-04 13:59:50 -0700

    fix the password setting in DeviceOwner creation

--- kolibri/plugins/setup_wizard/views.py
+++ kolibri/plugins/setup_wizard/views.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from kolibri.auth.models import DeviceOwner
@@ -14,6 +15,8 @@ class DeviceOwnerCreateViewSet(viewsets.ViewSet):
     def list(self, request):
         username = request.GET['username']
         password = request.GET['password']
-        DeviceOwner.objects.create(username=username, password=password)
-        redirect_path = 'http://' + request.META['HTTP_HOST'] + '/learn/'
+        d = DeviceOwner(username=username)
+        d.set_password(password)
+        d.save()
+        redirect_path = request.get_host() + reverse("kolibri:learnplugin:learn")
         return HttpResponse(redirect_path)
