03f8793bbed2e6a4c9d2055412d7a899258dd2cc
Jamie Alexandre <jamalex@gmail.com>
2016-08-05 12:55:07 -0700

    Rename sync_channelmetadata and generalize channel enumeration into util

--- kolibri/content/utils/annotation.py
+++ kolibri/content/utils/annotation.py
@@ -1,25 +1,17 @@
-import fnmatch
-import os
-
 from django.conf import settings
 
 from ..content_db_router import using_content_database
 from ..models import ChannelMetadata, ChannelMetadataCache
+from .channels import get_channel_id_list_from_scanning_content_database_dir
 
 
-def _get_channel_list_from_scanning_contentdb_dir():
-    db_list = fnmatch.filter(os.listdir(settings.CONTENT_DATABASE_DIR), '*.sqlite3')
-    db_names = [db.split('.sqlite3', 1)[0] for db in db_list]
-    return db_names
-
-
-def sync_channelmetadata():
+def update_channel_metadata_cache():
     """
     Scan through the settings.CONTENT_DATABASE_DIR folder for all channel content databases,
     and pull the data from each database's ChannelMetadata object to update the ChannelMetadataCache
     object in the default database to ensure they are in sync.
     """
-    db_names = _get_channel_list_from_scanning_contentdb_dir()
+    db_names = get_channel_id_list_from_scanning_content_database_dir(settings.CONTENT_DATABASE_DIR)
     # delete channelmetadata obejcts in default db that cannot be found in CONTENT_DATABASE_DIR
     ChannelMetadataCache.objects.exclude(id__in=db_names).delete()
     # sync the channelmetadata objects in default db with channelmetadata objects in CONTENT_DATABASE_DIR
