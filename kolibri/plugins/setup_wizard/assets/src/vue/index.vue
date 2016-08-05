0f18ca3ae55ff57b68a9195b86a11aaaf8259b8d
66eli77 <66eli77@gmail.com>
2016-08-05 14:42:19 -0700

    styled setup_wizard component

--- kolibri/plugins/setup_wizard/assets/src/vue/index.vue
+++ kolibri/plugins/setup_wizard/assets/src/vue/index.vue
@@ -2,17 +2,23 @@
 
   <div class="device-owner-creation">
     <div class="wrapper">
+      <img class="logo" src="./icons/logo-min.png">
       <div class="container">
-        <div class="title">Device Owner Creation</div>
+        <div class="title">Device Owner</div>
+        <br>
+        <div class="description">To use Kolibri, there are need to be at least one device owner. This account will have the highest privilege that can configure all settings about this installation.</div>
         <div class="creation-form">
-          <br>Username: <input type="text" v-model="username" placeholder="Example: Dylan Barth"><br>
-          <br>Password: <input type="text" v-model="password" placeholder="Example: 1212"><br>
+          <br><input :class="{ 'input-error': username_error }" type="text" v-model="username" placeholder="Username"><br>
+          <br><input :class="{ 'input-error': password_error }" type="text" v-model="password" placeholder="Password"><br>
         </div>
         <br>
-        <div class="title">Facility Creation</div>
-        <br>Facility: <input type="text" v-model="facility" placeholder="Example: San Diego High School"><br>
+        <div class="title">Facility</div>
         <br>
-        <button class="create-btn" type="button" @click="createBoth">Create</button>
+        <div class="description">To use Kolibri, there are need to be at least one facility. This facility can represent your school or trainning center, and classrooms and learnning groups are orgnized under this facility.</div>
+        <br><input :class="{ 'input-error': facility_error }" type="text" v-model="facility" placeholder="Facility name"><br>
+        <br>
+        <br>
+        <button class="create-btn" type="button" @click="createBoth">Create and get started</button>
       </div>
     </div>
   </div>
@@ -28,20 +34,41 @@
     data() {
       return {
         username: '',
+        username_error: false,
         password: '',
+        password_error: false,
         facility: '',
+        facility_error: false,
       };
     },
     methods: {
       createBoth() {
-        const deviceOwnerPayload = {
-          password: this.password,
-          username: this.username,
-        };
-        const facilityPayload = {
-          name: this.facility,
-        };
-        this.createDeviceOwnerAndFacility(deviceOwnerPayload, facilityPayload);
+        if (this.username && this.password && this.facility) {
+          const deviceOwnerPayload = {
+            password: this.password,
+            username: this.username,
+          };
+          const facilityPayload = {
+            name: this.facility,
+          };
+          this.createDeviceOwnerAndFacility(deviceOwnerPayload, facilityPayload);
+        } else {
+          if (!this.username) {
+            this.username_error = true;
+          } else {
+            this.username_error = false;
+          }
+          if (!this.password) {
+            this.password_error = true;
+          } else {
+            this.password_error = false;
+          }
+          if (!this.facility) {
+            this.facility_error = true;
+          } else {
+            this.facility_error = false;
+          }
+        }
       },
     },
     vuex: {
@@ -65,16 +92,44 @@
     width: 100%
     height: 100%
     display: table
-  
   .wrapper
     display: table-cell
     vertical-align: middle
-
   .container
     background: #fff
-    width: 450px
+    width: 100%
+    max-width: 430px
+    min-width: 320px
     border-radius: 4px
     margin: 0 auto
     padding: 20px 30px
+  .title
+    font-weight: bold
+  .description
+    font-size: 12px
+    color: $core-text-annotation
+  .create-btn
+    background-color: $core-action-normal
+    color: white
+  input
+    width: 100%
+    border-width: 2px
+    border-style: solid
+    border-color: $core-bg-canvas
+    border-radius: 4px
+    padding: 6px
+    background-color: $core-bg-canvas
+  .input-error
+    border-width: 2px
+    border-color: $core-text-alert
+    background-color: $core-text-alert-bg
+    
+  .logo
+    height: 20%
+    max-height: 160px
+    min-height: 100px
+    display: block
+    margin-left: auto
+    margin-right: auto
 
 </style>
