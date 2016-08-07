8d15160d03a7671bace9586ea436b2e863c06bea
66eli77 <66eli77@gmail.com>
2016-08-06 21:52:12 -0700

    better checking procedure for better performance

--- kolibri/plugins/setup_wizard/middleware.py
+++ kolibri/plugins/setup_wizard/middleware.py
@@ -7,14 +7,18 @@ class SetupWizardMiddleware():
     """
     display the superuser creation app if no superuser exists.
     """
-    has_superuser = False
+    deviceowner_already_created = False
 
     def process_request(self, request):
-        if request.path.startswith(reverse("facility-list")) or request.path.startswith(reverse("deviceowner-list")):
-            pass  # the api endpoint to create DeviceOwner
-        elif not self.has_superuser and not request.path.startswith(reverse("kolibri:setupwizardplugin:setupwizard")):
-            if DeviceOwner.objects.count() < 1:
-                # no superuser exists, redirect to the DeviceOwner creation UI
-                return redirect(reverse("kolibri:setupwizardplugin:setupwizard"))
-            else:
-                self.has_superuser = True
+        # If a DeviceOwner has already been created, no need to do anything here
+        self.deviceowner_already_created = self.deviceowner_already_created or DeviceOwner.objects.exists()
+        if self.deviceowner_already_created:
+            return
+
+        # Don't redirect for URLs that are required for the setup wizard
+        allowed_paths = [reverse(name) for name in ["facility-list", "deviceowner-list", "kolibri:setupwizardplugin:setupwizard"]]
+        if any(request.path.startswith(path_prefix) for path_prefix in allowed_paths):
+            return
+
+        # If we've gotten this far, we want to redirect to the setup wizard
+        return redirect(reverse("kolibri:setupwizardplugin:setupwizard"))
