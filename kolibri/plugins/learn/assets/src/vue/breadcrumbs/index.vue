b58c9af2d8faa366012de550ad8e962421ff44a1
whitzhu <whitzhu@gmail.com>
2016-08-11 13:02:53 -0700

    add in role="presentation" and replaced with externalized svg

--- kolibri/plugins/learn/assets/src/vue/breadcrumbs/index.vue
+++ kolibri/plugins/learn/assets/src/vue/breadcrumbs/index.vue
@@ -8,10 +8,7 @@
       <span v-if="pageName === thePageNames.LEARN_CONTENT">
         <a v-link="learnRoot">
           <span class='sep'>
-            <svg height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-            </svg>
+            <svg role="presentation" src="../icons/back.svg"></svg>
           Learn
           </span>
         </a>
@@ -19,10 +16,7 @@
       <span v-if="pageName === thePageNames.EXPLORE_CONTENT">
         <a v-link="parentLink">
           <span class='sep'>
-            <svg height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-            </svg>
+            <svg role="presentation" src="../icons/back.svg"></svg>
           Back
           </span>
         </a>
@@ -33,10 +27,7 @@
       <span class="back" v-if="!isRoot && pageMode === thePageMode.EXPLORE && pageName !== thePageNames.EXPLORE_CONTENT">
         <a v-link="backLink">
           <span class='sep'>
-            <svg height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-            </svg>
+            <svg role="presentation" src="../icons/back.svg"></svg>
           Back
           </span>
         </a>
