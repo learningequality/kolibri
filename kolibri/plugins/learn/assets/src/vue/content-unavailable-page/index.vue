7b9c28028ba855c36dd493e502b3c6cd93b75881
Christian Memije <christianmemije@gmail.com>
2016-08-11 18:08:48 -0700

    Add content-unavailable page.

---
+++ kolibri/plugins/learn/assets/src/vue/content-unavailable-page/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+    <h1>Content Unavailable</h1>
+    <p>Get yourself some channels!</p>
+  </div>
+
+</template>
+
+
+<script>
+
+  module.exports = {
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~core-theme.styl'
+
+</style>
