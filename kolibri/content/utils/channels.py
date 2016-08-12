fbb9aa8721f4ef039a9f110ce75a4801e690a38f
Jamie Alexandre <jamalex@gmail.com>
2016-08-11 20:48:39 -0700

    Moved UUID validation to kolibri.utils.uuids file.

--- kolibri/content/utils/channels.py
+++ kolibri/content/utils/channels.py
@@ -2,10 +2,10 @@ import fnmatch
 import logging as logger
 import os
 import psutil
-import uuid
 import sqlite3
 from collections import namedtuple
 from django.conf import settings
+from kolibri.utils.uuids import is_valid_uuid
 
 logging = logger.getLogger(__name__)
 
@@ -26,21 +26,13 @@ DriveData = namedtuple(
     ]
 )
 
-
-def _is_valid_hex_uuid(uuid_to_test):
-    try:
-        uuid_obj = uuid.UUID(uuid_to_test)
-    except ValueError:
-        return False
-    return uuid_to_test == uuid_obj.hex
-
 def get_channel_id_list_from_scanning_content_database_dir(content_database_dir, return_full_dir=False):
     """
     Returns a list of channel IDs for the channel databases that exist in a content database directory.
     """
     db_list = fnmatch.filter(os.listdir(content_database_dir), '*.sqlite3')
     db_names = [db.split('.sqlite3', 1)[0] for db in db_list]
-    valid_db_names = [name for name in db_names if _is_valid_hex_uuid(name)]
+    valid_db_names = [name for name in db_names if is_valid_uuid(name)]
     invalid_db_names = set(db_names) - set(valid_db_names)
 
     if invalid_db_names:
