84fb73d03758ff7ea45e1c90718ad5e70da8bbda
Christian Memije <christianmemije@gmail.com>
2016-08-11 19:37:00 -0700

    More descriptive

--- kolibri/plugins/learn/assets/src/vue/content-unavailable-page/index.vue
+++ kolibri/plugins/learn/assets/src/vue/content-unavailable-page/index.vue
@@ -1,8 +1,9 @@
 <template>
 
   <div>
-    <h1>Content Unavailable</h1>
-    <p>Get yourself some channels!</p>
+    <h1>No Content Channels Available</h1>
+    <p v-if="isAdminOrSuperuser">Download content channels from the <a href="/management/">Manage Content Page.</a></p>
+    <p v-else>You need to log in as an administrator to manage your content channels.</p>
   </div>
 
 </template>
@@ -11,7 +12,12 @@
 <script>
 
   module.exports = {
-  };
+    vuex: {
+      getters: {
+        isAdminOrSuperuser: state => state.core.is_admin_or_superuser,
+      },
+    },
+};
 
 </script>
 
