ffb20315927600713f2c5c299389268eb4314271
Christian Memije <christianmemije@gmail.com>
2016-08-17 11:49:37 -0700

    Videoplayer resize and buttons fix

--- kolibri/plugins/video_mp4_render/assets/src/vue/index.vue
+++ kolibri/plugins/video_mp4_render/assets/src/vue/index.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div>
-    <div v-el:videowrapperwrapper class="videowrapperwrapper">
+    <div v-el:videowrapperwrapper class="videowrapperwrapper hidden">
       <div v-el:videowrapper class="videowrapper">
         <video v-el:video class="video-js vjs-default-skin" @seeking="handleSeek" @timeupdate="updateTime">
           <template v-for="video in videoSources">
@@ -96,6 +96,8 @@
         this.videoWidth = this.videoPlayer.videoWidth();
         this.videoHeight = this.videoPlayer.videoHeight();
         this.resizeVideo();
+        this.videoPlayerIsReady();
+        this.$els.videowrapperwrapper.classList.remove('hidden');
       },
 
       resizeVideo() {
@@ -123,62 +125,11 @@
         return debounce(this.resizeVideo, 300);
       },
 
-      updateTime() {
-        this.dummyTime = this.videoPlayer.currentTime();
-        if (this.dummyTime - this.lastUpdateTime >= 5) {
-          this.recordProgress();
-          this.lastUpdateTime = this.dummyTime;
-        }
-      },
-      /* Catches when a user jumps around/skips while playing the video */
-      handleSeek() {
-        /* Record any progress up to this point */
-        this.recordProgress();
-        /* Set last check to be where player is at now */
-        this.dummyTime = this.videoPlayer.currentTime();
-        this.lastUpdateTime = this.dummyTime;
-      },
+      videoPlayerIsReady() {
+        this.videoPlayer.addChild('ReplayButton');
+        this.videoPlayer.addChild('ForwardButton');
+        this.videoPlayer.addChild('TogglePlayButton');
 
-      recordProgress() {
-        this.$emit('progressUpdate', Math.max(0,
-          (this.dummyTime - this.progressStartingPoint) /
-          Math.floor(this.videoPlayer.duration())));
-        this.progressStartingPoint = this.videoPlayer.currentTime();
-      },
-    },
-
-    ready() {
-      this.videoPlayer = videojs(this.$els.video, {
-        fluid: true,
-        inactivityTimeout: 1000,
-        controls: true,
-        autoplay: false,
-        preload: 'auto',
-        poster: this.posterSource,
-        playbackRates: [0.5, 1.0, 1.25, 1.5, 2.0],
-        textTrackDisplay: true,
-        ReplayButton: true,
-        ForwardButton: true,
-        TogglePlayButton: true,
-        bigPlayButton: false,
-        controlBar: {
-          children: [
-            { name: 'currentTimeDisplay' },
-            { name: 'timeDivider' },
-            { name: 'progressControl' },
-            { name: 'durationDisplay' },
-            { name: 'remainingTimeDisplay' },
-            { name: 'muteToggle' },
-            { name: 'VolumeBar' },
-            { name: 'playbackRateMenuButton' },
-            { name: 'captionsButton' },
-            { name: 'fullscreenToggle' },
-          ],
-        },
-      },
-
-
-      () => {
         const centerButtons = this.$els.videowrapper.childNodes[1];
         const toggleButton = centerButtons
           .getElementsByClassName('videotoggle')[0];
@@ -210,10 +161,60 @@
         videojs(this.$els.video).on('ended', () => {
           this.setPlayState(false);
         });
+      },
+
+      updateTime() {
+        this.dummyTime = this.videoPlayer.currentTime();
+        if (this.dummyTime - this.lastUpdateTime >= 5) {
+          this.recordProgress();
+          this.lastUpdateTime = this.dummyTime;
+        }
+      },
+      /* Catches when a user jumps around/skips while playing the video */
+      handleSeek() {
+        /* Record any progress up to this point */
+        this.recordProgress();
+        /* Set last check to be where player is at now */
+        this.dummyTime = this.videoPlayer.currentTime();
+        this.lastUpdateTime = this.dummyTime;
+      },
+
+      recordProgress() {
+        this.$emit('progressUpdate', Math.max(0,
+          (this.dummyTime - this.progressStartingPoint) /
+          Math.floor(this.videoPlayer.duration())));
+        this.progressStartingPoint = this.videoPlayer.currentTime();
+      },
+    },
+
+    ready() {
+      this.videoPlayer = videojs(this.$els.video, {
+        fluid: true,
+        autoplay: false,
+        controls: true,
+        textTrackDisplay: true,
+        bigPlayButton: false,
+        inactivityTimeout: 1000,
+        preload: 'auto',
+        poster: this.posterSource,
+        playbackRates: [0.5, 1.0, 1.25, 1.5, 2.0],
+        controlBar: {
+          children: [
+            { name: 'currentTimeDisplay' },
+            { name: 'timeDivider' },
+            { name: 'progressControl' },
+            { name: 'durationDisplay' },
+            { name: 'remainingTimeDisplay' },
+            { name: 'muteToggle' },
+            { name: 'VolumeBar' },
+            { name: 'playbackRateMenuButton' },
+            { name: 'captionsButton' },
+            { name: 'fullscreenToggle' },
+          ],
+        },
       });
 
       this.videoPlayer.on('loadedmetadata', this.loadedMetaData);
-
       global.addEventListener('resize', this.debouncedResizeVideo);
     },
     beforeDestroy() {
@@ -267,6 +268,9 @@
     height: 100%
     background-color: rgba(0, 0, 0, 0)
     position: relative
+
+  .hidden
+    visibility: hidden
 
   .video-js .vjs-menu
     font-family: 'NotoSans', 'sans-serif'
@@ -327,7 +331,6 @@
     background-size: contain
     background-color: rgba(0, 0, 0, 0.3)
 
-  .video-js .display,
   .video-js .display
     display: block
 
