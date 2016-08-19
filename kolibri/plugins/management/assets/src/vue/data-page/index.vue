a51c21aa1dc7864bda3d2f82d3570eb833d9ef38
Jamie Alexandre <jamalex@gmail.com>
2016-08-18 21:49:09 -0700

    Add additional CSV export option in the UI, for ContentSessionLogs

--- kolibri/plugins/management/assets/src/vue/data-page/index.vue
+++ kolibri/plugins/management/assets/src/vue/data-page/index.vue
@@ -1,16 +1,40 @@
 <template>
 
-  <div>
+  <div class="wrapper">
+
     <h1>Export Usage Data</h1>
 
     <p>
-      Download a comma-separated spreadsheet containing information about users and
+      Download comma-separated spreadsheets containing information about users and
       their interactions with the content on this device.
     </p>
 
-    <icon-button @click="downloadCSV" text="Download CSV">
-      <svg src="../icons/download.svg"></svg>
-    </icon-button>
+    <div class="exportblock">
+      <h2>Content Summary Logs</h2>
+      <p>
+        Each row in a content summary log represents a summary of a user's entire history of
+        interactions with a particular piece of content.
+      </p>
+      <a :href="summarylogurl">
+        <icon-button text="Download CSV">
+          <svg src="../icons/download.svg"></svg>
+        </icon-button>
+      </a>
+    </div>
+
+    <div class="exportblock">
+      <h2>Content Session Logs</h2>
+      <p>
+        Each row in a content session log represents a user's interactions with a particular
+        piece of content within the context of a single page load.
+      </p>
+      <a :href="sessionlogurl">
+        <icon-button text="Download CSV">
+          <svg src="../icons/download.svg"></svg>
+        </icon-button>
+      </a>
+    </div>
+
   </div>
 
 </template>
@@ -20,17 +44,31 @@
 
   const Kolibri = require('kolibri');
   module.exports = {
-    methods: {
-      downloadCSV() {
-        window.open(Kolibri.urls['contentsummarylogcsv-list'](), '_blank');
-      },
-    },
     components: {
       'icon-button': require('icon-button'),
+    },
+    computed: {
+      summarylogurl() {
+        return Kolibri.urls['contentsummarylogcsv-list']();
+      },
+      sessionlogurl() {
+        return Kolibri.urls['contentsessionlogcsv-list']();
+      },
     },
   };
 
 </script>
 
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+
+  @require 'jeet'
+
+  @media (min-width: 900px)
+    .exportblock
+      col(1/2)
+
+  .wrapper
+    cf()
+
+</style>
