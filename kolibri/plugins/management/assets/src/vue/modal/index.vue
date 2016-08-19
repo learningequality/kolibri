b4e68423c60440d47f87a5533880ab30c1c223a2
David Canas <davidxcanas@gmail.com>
2016-08-18 18:08:05 -0700

    HTML Accessibility done, working on modifying the focus order

--- kolibri/plugins/management/assets/src/vue/modal/index.vue
+++ kolibri/plugins/management/assets/src/vue/modal/index.vue
@@ -1,38 +1,36 @@
 <template>
 
-  <div class="modal-root" v-on:keyup.esc="closeModal">
-    <div class="modal" v-if="showModal" transition="modal">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <button @click="closeModal" class="close-btn">
-            <svg src="../icons/close.svg"></svg>
-            <span class="visuallyhidden">Close</span>
-          </button>
-          <div class="modal-header">
-            <slot name="header">
-              Kolibri
-            </slot>
-          </div>
-          <div class="modal-body">
-            <slot name="body">
-              disappear in oblivion..
-            </slot>
-          </div>
-          <div class="modal-footer">
-            <slot name="footer">
-              <button @click="closeModal" class="close-btn">OK</button>
-            </slot>
-          </div>
-        </div>
-      </div>
-    </div>
+  <!-- Accessibility properties for the overlay -->
+  <div class="modal-overlay"
+    v-if="showModal"
+    @keyup.esc="closeModal"
+    id="modal-window"
+    aria-hidden="{{(!showModal).toString()}}"
+    role="dialog"
+    aria-labelledby="modal-title">
 
-    <div @click="openModal">
-    <!-- wrap this named slot so that the openModal method logic is encapsulated inside this modal component, but the parent component can pass anything to this slot for styling purpose -->
-      <slot name="openbtn">
-        <button>{{ btntext }}</button>
+    <div class="modal" v-el:modal transition="modal">
+      <!-- Close Button -->
+      <button aria-label="close" @click="closeModal" class="btn-close">
+        <svg src="../icons/close.svg"></svg>
+      </button>
+
+      <!-- Modal Content -->
+      <slot name="body" class="modal-content" id="modal-holder" role="document">
+        <p>
+          To populate, add <code>slot="body"</code> to the HTML element you want to fill here.
+        </p>
       </slot>
+
     </div>
+  </div>
+
+  <!-- Think this might be more appropriate as a boolean, or case-by-case -->
+  <div v-if="btntext" @click="openModal">
+  <!-- wrap this named slot so that the openModal method logic is encapsulated inside this modal component, but the parent component can pass anything to this slot for styling purpose -->
+    <slot name="openbtn">
+      <button>{{ btntext }}</button>
+    </slot>
   </div>
 
 </template>
@@ -42,23 +40,46 @@
 
   module.exports = {
     props: {
+      // mostly keeping around for backwards compatibility
+      // TODO get rid of this prop
       btntext: {
         type: String,
-        default: 'Open Modal',
+      },
+      disableClose: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+    },
+    events: {
+      open() {
+        this.showModal = true;
+        this.lastFocus = document.activeElement;
+
+        /* eslint-disable*/
+        // Need to wait for DOM to update asynchronously, then get the modal element
+        vue.nextTick(() => console.log(this.$get('$els.modal')));
+        /* eslint-enable*/
+      },
+      close() {
+        this.showModal = false;
+        this.lastFocus.focus();
       },
     },
     data() {
       return {
         showModal: false,
+        lastFocus: '',
       };
     },
     methods: {
       openModal() {
-        this.showModal = true;
+        // propogate open event here and in parent
         this.$dispatch('open');
+        console.log(this.$els.modal);
       },
       closeModal() {
-        this.showModal = false;
+        // propogate close event here and in parent
         this.$dispatch('close');
       },
     },
@@ -71,22 +92,21 @@
 
   @require '~core-theme.styl'
 
-  .modal
+  .modal-overlay
     position: fixed
-    z-index: 1  // TODO: why is this necessary? fix search bar, don't add z-index
     top: 0
     left: 0
     width: 100%
     height: 100%
     background: rgba(0, 0, 0, 0.7)
-    display: table
-    transition: opacity 0.3s ease
+    // transition: opacity 0.3s ease
 
-  .modal-wrapper
-    display: table-cell
-    vertical-align: middle
-
-  .modal-container
+  .modal
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    width: 60%
     background: #fff
     max-width: 380px
     border-radius: $radius
@@ -95,19 +115,12 @@
     margin: 0 auto
     padding: 15px 30px
 
-  .modal-header
-    font-weight: bold
-    padding-bottom: 10px
-
-  .modal-footer
-    margin-top: 15px
-    padding-bottom: inherit
-
-  .close-btn
+  .btn-close
     float: right
     color: $core-text-default
     border: none
 
+  // Animation Specs
   .modal-enter, .modal-leave
     opacity: 0
 
