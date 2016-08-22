54b941c040865c4a33849527547892bed93b649c
Christian Memije <christianmemije@gmail.com>
2016-08-22 16:16:22 -0700

    Removed lines.

--- frontend_build/src/read_bundle_plugins.js
+++ frontend_build/src/read_bundle_plugins.js
@@ -25,9 +25,6 @@ var libs = function(kolibri_name) {
     'vue': kolibri_name + '.lib.vue',
     'kolibri': kolibri_name,
     'vuex': kolibri_name + '.lib.vuex',
-    'core-base': kolibri_name + '.lib.coreBase',
-    'content-renderer': kolibri_name + '.lib.contentRenderer',
-    'loading-spinner': kolibri_name + '.lib.loadingSpinner',
   };
 };
 
