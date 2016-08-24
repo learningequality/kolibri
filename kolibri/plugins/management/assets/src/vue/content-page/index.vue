460dabde3c8282b2274a7f37bbf22161205362d7
indirectlylit <devon@indirectlylit.com>
2016-08-24 09:58:29 -0700

    remove debug code

--- kolibri/plugins/management/assets/src/vue/content-page/index.vue
+++ kolibri/plugins/management/assets/src/vue/content-page/index.vue
@@ -9,10 +9,6 @@
       :id="pageState.taskList[0].id"
     ></task-status>
 
-    <button @click="incrementDebugTask">next task</button>
-
-    <hr>
-
     <icon-button v-if="!pageState.taskList.length" text="Add Channel" :primary="true" @click="startImportWizard">
       <svg src="../icons/add.svg"></svg>
     </icon-button>
@@ -31,38 +27,6 @@
 
 <script>
 
-  const debugTasks = [
-    {},
-    {
-      percentage: 0.5,
-      status: 'in_progress',
-      type: 'local_export',
-      id: '12345678901234567890',
-      metadata: {},
-    },
-    {
-      percentage: 0.0,
-      status: 'pending',
-      type: 'local_export',
-      id: '12345678901234567890',
-      metadata: {},
-    },
-    {
-      percentage: 0.3,
-      status: 'error',
-      type: 'local_export',
-      id: '12345678901234567890',
-      metadata: {},
-    },
-    {
-      percentage: 1.0,
-      status: 'success',
-      type: 'local_export',
-      id: '12345678901234567890',
-      metadata: {},
-    },
-  ];
-
   const actions = require('../../actions');
   const ContentWizardPages = require('../../state/constants').ContentWizardPages;
 
@@ -76,7 +40,6 @@
       'wizard-export': require('./wizard-export'),
     },
     data: () => ({
-      i: 0, // TODO - remove debuging
       intervalId: undefined,
     }),
     attached() {
@@ -106,15 +69,6 @@
         pageState: state => state.pageState,
       },
       actions: {
-        incrementDebugTask(state) { // TODO - remove debuging
-          this.i = (this.i + 1) % debugTasks.length;
-          if (this.i) {
-            state.dispatch('SET_CONTENT_PAGE_TASKS', [debugTasks[this.i]]);
-            state.dispatch('SET_CONTENT_PAGE_WIZARD_STATE', false, {});
-          } else {
-            state.dispatch('SET_CONTENT_PAGE_TASKS', []);
-          }
-        },
         startImportWizard: actions.startImportWizard,
         pollTasksAndChannels: actions.pollTasksAndChannels,
       },
