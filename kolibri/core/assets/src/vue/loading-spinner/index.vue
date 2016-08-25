35ab572c74c6056cd5b151dcb9aeb1abc3041b5c
Christian Memije <christianmemije@gmail.com>
2016-08-25 10:50:05 -0700

    Add 250ms delay to loading-spinner.

--- kolibri/core/assets/src/vue/loading-spinner/index.vue
+++ kolibri/core/assets/src/vue/loading-spinner/index.vue
@@ -1,6 +1,10 @@
 <template>
 
-  <div class="loading-spinner"></div>
+  <div>
+    <div class="loading-spinner-wrapper">
+      <div class="loading-spinner"></div>
+    </div>
+  </div>
 
 </template>
 
@@ -16,9 +20,30 @@
 
 <style lang="stylus" scoped>
 
-  .loading-spinner
+  .loading-spinner-wrapper
     width: 100%
     height: 100%
+    position: relative
+    opacity: 0
+    animation-duration: 0s
+    animation-delay: 250ms
+    animation-name: delaydisplay
+    animation-fill-mode: forwards
+
+  @keyframes delaydisplay
+    from
+      opacity: 0
+    to
+      opacity: 1
+
+  .loading-spinner
+    width: 50%
+    height: 50%
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
     background: url('loading-spinner.gif') no-repeat center
+    background-size: contain
 
 </style>
