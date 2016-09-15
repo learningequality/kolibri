b2a2c9e41d64b41a4ba6f07503a5bfc39656b621
Richard Tibbles <richard.tibbles@gmail.com>
2016-09-15 10:25:27 -0700

    Adds frontend and backend tests for core API functions.

---
+++ frontend_build/test/test_apiSpecExportTools.js
@@ -0,0 +1,107 @@
+var assert = require('assert');
+var rewire = require('rewire');
+
+var apiSpecExportTools = rewire('../src/apiSpecExportTools');
+
+var testSpec = {
+  module: 'test',
+  requireName: 'test'
+};
+
+var oneDeepSpec = {
+  test: testSpec
+};
+
+var twoDeepSpec = {
+  test: {
+    test: testSpec
+  }
+};
+
+describe('coreExternals', function() {
+
+  // Note: all externals objects will have at least one entry for the Kolibri object itself.
+  describe('top level with special keys', function() {
+    it('should have one entry', function (done) {
+      apiSpecExportTools.__set__("apiSpec", testSpec);
+      assert(Object.keys(apiSpecExportTools.coreExternals('test_global')).length === 1);
+      done();
+    });
+  });
+  describe('1 nested valid spec', function() {
+    it('should have two entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", oneDeepSpec);
+      assert(Object.keys(apiSpecExportTools.coreExternals('test_global')).length === 2);
+      done();
+    });
+  });
+  describe('2 nested valid spec', function() {
+    it('should have two entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", twoDeepSpec);
+      assert(Object.keys(apiSpecExportTools.coreExternals('test_global')).length === 2);
+      done();
+    });
+  });
+});
+
+describe('coreAliases', function() {
+
+  describe('top level with special keys no local import', function() {
+    it('should have no entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", testSpec);
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 0);
+      done();
+    });
+  });
+  describe('1 nested valid spec no local import', function() {
+    it('should have no entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", oneDeepSpec);
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 0);
+      done();
+    });
+  });
+  describe('2 nested valid spec no local import', function() {
+    it('should have no entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", twoDeepSpec);
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 0);
+      done();
+    });
+  });
+  describe('1 nested valid spec with local import but no requireName', function() {
+    it('should have no entries', function (done) {
+      apiSpecExportTools.__set__("apiSpec", {
+        test: {
+          module: './test'
+        }
+      });
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 0);
+      done();
+    });
+  });
+  describe('1 nested valid spec with local import', function() {
+    it('should have one entry', function (done) {
+      apiSpecExportTools.__set__("apiSpec", {
+        test: {
+          module: './test',
+          requireName: 'test'
+        }
+      });
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 1);
+      done();
+    });
+  });
+  describe('2 nested valid spec with local import', function() {
+    it('should have one entry', function (done) {
+      apiSpecExportTools.__set__("apiSpec", {
+        test: {
+          test: {
+            module: './test',
+            requireName: 'test'
+          }
+        }
+      });
+      assert(Object.keys(apiSpecExportTools.coreAliases()).length === 1);
+      done();
+    });
+  });
+});
