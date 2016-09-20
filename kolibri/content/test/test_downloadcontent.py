44c656b2a1389d21ede879013727ea5767908ff6
Christian Memije <christianmemije@gmail.com>
2016-09-20 16:55:30 -0700

    Fixed tests.

--- kolibri/content/test/test_downloadcontent.py
+++ kolibri/content/test/test_downloadcontent.py
@@ -1,11 +1,13 @@
 import os
 import tempfile
 import hashlib
+import mimetypes
 from django.test import TestCase, Client
 from django.test.utils import override_settings
 from kolibri.auth.models import DeviceOwner
 from kolibri.content.models import File, ContentNode
 from kolibri.content.utils.paths import get_content_storage_file_path
+from fle_utils import constants
 
 CONTENT_STORAGE_DIR_TEMP = tempfile.mkdtemp()
 
@@ -24,27 +26,41 @@ class DownloadContentTestCase(TestCase):
 
         self.client = Client()
         self.hash = hashlib.md5("DUMMYDATA".encode()).hexdigest()
-        self.extension = "pdf"
+        self.extension = dict(constants.format_choices).get("pdf")
         self.filename = "{}.{}".format(self.hash, self.extension)
         self.title = "abc123!@#$%^&*();'[],./?><"
         self.contentnode = ContentNode(title=self.title)
         self.available = True
-        self.preset = "doc"
+        self.preset = constants.FP_DOCUMENT
         self.file = File(checksum=self.hash, extension=self.extension, available=self.available,
                          contentnode=self.contentnode, preset=self.preset)
 
+        self.path = get_content_storage_file_path(self.filename)
+        path_dir = os.path.dirname(self.path)
+        if not os.path.exists(path_dir):
+            os.makedirs(path_dir)
+        tempfile = open(self.path, "w")
+        tempfile.write("test")
+        tempfile.close()
 
     def test_generate_download_filename(self):
-        self.assertEqual(self.file.get_download_filename(), "abc123._Document")
+        self.assertEqual(self.file.get_download_filename(), "abc123._Document.{}".format(self.extension))
 
     def test_generate_download_url(self):
         self.assertEqual(self.file.get_download_url(), "/downloadcontent/{}/{}".format(self.filename,
-                                                                                          self.file.get_download_filename()))
+                                                                                       self.file.get_download_filename()))
 
     def test_download_existing_file(self):
         response = self.client.get(self.file.get_download_url())
-        self.assertEqual(response.status_code, 304)
+        self.assertEqual(response.status_code, 200)
 
     def test_download_non_existing_file(self):
-        response = self.client.get(self.file.get_download_url())
+        bad_download_url = self.file.get_download_url().replace(self.file.get_download_url()[25:25], "aaaaa")
+        response = self.client.get(bad_download_url)
         self.assertEqual(response.status_code, 404)
+
+    def test_download_headers(self):
+        response = self.client.get(self.file.get_download_url())
+        self.assertEqual(response['Content-Type'], mimetypes.guess_type(self.filename)[0])
+        self.assertEqual(response['Content-Disposition'], 'attachment;')
+        self.assertEqual(response['Content-Length'], str(os.path.getsize(self.path)))
