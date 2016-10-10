8fefad33468cb39398cd0fc550cb2a9e8c040dce
66eli77 <66eli77@gmail.com>
2016-10-10 12:00:56 -0700

    only form field support required argument, so get rid of blank and null arguments instead to achieve the same

--- kolibri/logger/models.py
+++ kolibri/logger/models.py
@@ -112,7 +112,7 @@ class MasteryLog(BaseLogModel):
     # with an assessment.
     mastery_criterion = models.TextField()
     start_timestamp = models.DateTimeField()
-    end_timestamp = models.DateTimeField(blank=True, null=True, required=True)
+    end_timestamp = models.DateTimeField()
     completion_timestamp = models.DateTimeField(blank=True, null=True)
     # The integer mastery level that this log is tracking.
     mastery_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
@@ -133,7 +133,7 @@ class AttemptLog(BaseLogModel):
     # that this attemptlog is a record of an interaction with.
     item = models.CharField(max_length=200)
     start_timestamp = models.DateTimeField()
-    end_timestamp = models.DateTimeField(blank=True, null=True, required=True)
+    end_timestamp = models.DateTimeField()
     completion_timestamp = models.DateTimeField(blank=True, null=True)
     # Which mastery log was this attemptlog associated with?
     masterylog = models.ForeignKey(MasteryLog, related_name="attemptlogs")
