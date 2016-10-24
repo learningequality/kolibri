5ba50f0613a0cc1c070a44c2b6fb62bac2d62888
indirectlylit <devon@indirectlylit.com>
2016-10-24 16:40:09 -0700

    trying to fix tests under python3

--- kolibri/core/discovery/test/dummydata/windows_data.py
+++ kolibri/core/discovery/test/dummydata/windows_data.py
@@ -1,7 +1,7 @@
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "windows_wmic_output.csv")) as f:
-    wmic_csv = f.read()
+    wmic_csv = str(f.read())
 
 os_system_responses = {
     "wmic logicaldisk list full /format:csv": wmic_csv,
