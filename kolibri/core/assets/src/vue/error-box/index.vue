86a02b0118ece92d2611fa6fe95b928e8aa497a1
Christian Memije <christianmemije@gmail.com>
2016-11-02 12:49:24 -0700

    Changed the error-box.

--- kolibri/core/assets/src/vue/error-box/index.vue
+++ kolibri/core/assets/src/vue/error-box/index.vue
@@ -1,11 +1,12 @@
 <template>
 
-  <div>
+  <div class="error-box-wrapper" v-bind:class="{ hidden: isHidden }">
+    <button class="close-button" v-on:click="hideErrorbox">X</button>
     <h1>{{ $tr('errorHeader') }}</h1>
     <label for="error-textbox" aria-live="polite">{{ $tr('errorPrefixText') }}</label><br>
-    <textarea id="error-textbox">
+    <div id="error-textbox">
       {{ error }}
-    </textarea>
+    </div>
   </div>
 
 </template>
@@ -24,6 +25,14 @@
         error: state => state.core.error,
       },
     },
+    data: () => ({
+      isHidden: false,
+    }),
+    methods: {
+      hideErrorbox() {
+        this.isHidden = true;
+      },
+    },
   };
 
 </script>
@@ -31,13 +40,31 @@
 
 <style lang="stylus" scoped>
 
-  h1
+  .error-box-wrapper
+    padding: 10px
     margin-top: 50px
+    position: relative
+    background-color: #FFF6F6
+    border: 2px solid #e0b4b4
+    color: #9F3A38
 
-  textarea
-    width: 100%
-    height: 300px
+  .hidden
+    display: none
+
+  .close-button
+    position: absolute
+    right: 5px
+    top: 5px
+
+  #error-textbox
+    max-height: 300px
+    padding: 5px
+    margin-top: 10px
+    overflow: auto
     font-family: monospace
     font-size: 10px
+    border: 1px solid black
+    background-color: white
+    color: black
 
 </style>
