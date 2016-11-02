cbcb5ec70c0bd74ca219f8c316090fa675234992
indirectlylit <devon@indirectlylit.com>
2016-11-02 11:00:35 -0700

    animated learner attempt history with demo

---
+++ kolibri/plugins/learn/assets/src/vue/scratchpad-page/exercise-attempts/answer-icon.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <div>
+    <svg v-if="isRight" src="./icons/right.svg" class="yes" :class="{ yay: success }"></svg>
+    <svg v-if="isWrong" src="./icons/wrong.svg" class="no" :class="{ yay: success }"></svg>
+    <svg v-if="isHint" src="./icons/hint.svg" class="no" :class="{ yay: success }"></svg>
+  </div>
+
+</template>
+
+
+<script>
+
+  const AnswerTypes = require('./constants').AnswerTypes;
+
+  module.exports = {
+    props: {
+      // one of the AnswerTypes
+      answertype: {
+        type: Number,
+        required: true,
+      },
+      // Visually indicate that the user has succeeded
+      success: {
+        type: Boolean,
+        required: true,
+      },
+    },
+    computed: {
+      isRight() {
+        return this.answertype === AnswerTypes.RIGHT;
+      },
+      isWrong() {
+        return this.answertype === AnswerTypes.WRONG;
+      },
+      isHint() {
+        return this.answertype === AnswerTypes.HINT;
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~kolibri.styles.coreTheme'
+
+  svg
+    transition: transform $core-time ease-in
+
+  .yes
+    fill: $core-action-normal
+
+  .yes.yay
+    transform: scale(1.5)
+
+  .no
+    fill: $core-text-annotation
+
+  .no.yay
+    transform: scale(0.75)
+
+</style>
---
+++ kolibri/plugins/learn/assets/src/vue/scratchpad-page/exercise-attempts/index.vue
@@ -0,0 +1,131 @@
+<template>
+
+  <div class="wrapper">
+    <div
+      class="answer"
+      v-for="item in itemsToRender"
+      transition="fade"
+      track-by="originalIndex"
+      :style="styleForIndex($index, item.originalIndex)"
+    >
+      <answer-icon :answertype="item.answer" :success="success"></answer-icon>
+    </div>
+    <div
+      class="placeholder"
+      v-for="i in numspaces"
+      :class="{'placeholder-empty': i === 0 && waiting}"
+    ></div>
+  </div>
+
+</template>
+
+
+<script>
+
+  // const AnswerTypes = require('./constants').AnswerTypes;
+
+  module.exports = {
+    props: {
+      // Creates an empty space awaiting a new attempt
+      waiting: {
+        type: Boolean,
+        required: true,
+      },
+      // Visually indicate that the user has succeeded
+      success: {
+        type: Boolean,
+        required: true,
+      },
+      // Total number of answer spaces to show
+      numspaces: {
+        type: Number,
+        required: true,
+      },
+      // An array of AnswerTypes for all attempts (including those not shown).
+      // Ordered from first to most recent.
+      log: {
+        type: Array,
+        required: true,
+      },
+    },
+    components: {
+      'answer-icon': require('./answer-icon'),
+    },
+    computed: {
+      numItemsToRender() {
+        if (this.waiting) {
+          return this.numspaces;
+        }
+        return this.numspaces + 1;
+      },
+      // returns a list of items the items to be rendered in the DOM
+      itemsToRender() {
+        // save the original index of the item in the log and slice of the end
+        const visible = this.log
+          .map((answer, originalIndex) => ({ answer, originalIndex }))
+          .slice(-1 * this.numItemsToRender);
+        visible.reverse();
+        return visible;
+      },
+    },
+    methods: {
+      styleForIndex(visualIndex, originalIndex) {
+        const ANSWER_WIDTH = 4 + 30 + 4;  // margin + width + margin
+        let xPos = ANSWER_WIDTH * (this.log.length - 1 - originalIndex);
+        if (this.waiting) {
+          xPos += ANSWER_WIDTH;
+        }
+        const style = {};
+        // translateZ(0) is there to try and force GPU-acceleration.
+        // (see e.g. http://blog.teamtreehouse.com/increase-your-sites-performance-with-hardware-accelerated-css)
+        style.transform = `translate(${xPos}px) translateZ(0)`;
+        // hidden "slide-off" item
+        if (visualIndex === this.numItemsToRender - 1) {
+          style.opacity = 0;
+        }
+        return style;
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~kolibri.styles.coreTheme'
+
+  $size = 30px
+  $margin = 4px
+
+  .wrapper
+    white-space: nowrap
+
+  .answer
+    width: $size
+    height: 20px
+    display: inline-block
+    margin: $margin
+    text-align: center
+    position: absolute
+    transition: transform 0.5s ease-in-out, opacity 1s ease-in-out
+
+    // try to improve performance - http://stackoverflow.com/a/10133679
+    backface-visibility: hidden
+    perspective: 1000
+
+  .fade-enter, .fade-leave
+    opacity: 0
+
+  .placeholder
+    display: inline-block
+    height: $size
+    width: $size
+    margin: $margin
+    border-bottom: 1px solid $core-text-annotation
+    transition: border-bottom 0.1s linear
+
+  .placeholder-empty
+    border-bottom: 3px solid $core-text-annotation
+
+</style>
