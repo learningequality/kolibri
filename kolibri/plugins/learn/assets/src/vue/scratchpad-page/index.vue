20c4af8dd3e444aafeba089e561d691c1b96355c
66eli77 <66eli77@gmail.com>
2016-11-03 12:57:57 -0700

    restore scratchpad-page

--- kolibri/plugins/learn/assets/src/vue/scratchpad-page/index.vue
+++ kolibri/plugins/learn/assets/src/vue/scratchpad-page/index.vue
@@ -1,111 +1,19 @@
 <template>
 
   <div>
-    <h1>Exercise Attempt Visualization</h1>
-    <p>Demo of CSS transitions</p>
-    <ul>
-      <li>N: <input type="number" v-model="N"></li>
-      <li>
-        Log:
-        <button
-          class="answer"
-          @click="right"
-          :disabled="!waiting || success"> ✓
-        </button>
-        <button
-          class="answer"
-          @click="wrong"
-          :disabled="!waiting || success"> x
-        </button>
-        <button
-          class="answer"
-          @click="hint"
-          :disabled="!waiting || success"> ?
-        </button>
-        <button
-          @click="next"
-          :disabled="waiting || success"> next
-        </button>
-      </li>
-      <li>success: <input type="checkbox" v-model="success"></li>
-      <li><button @click="clear">clear</button></li>
-    </ul>
-    <hr>
-    <div>
-      <exercise-attempts
-        :waiting="waiting"
-        :success="success"
-        :numspaces="parseInt(N)"
-        :log="log"
-      >
-      </exercise-attempts>
-    </div>
+    <h1>Learn Scratchpad</h1>
+    <p>Use this page for in-progress component prototyping.</p>
   </div>
 
 </template>
 
 
 <script>
-
-  const AnswerTypes = {
-    HINT: -1,
-    WRONG: 0,
-    RIGHT: 1,
-  };
-
-
   module.exports = {
-    methods: {
-      clear() {
-        // http://stackoverflow.com/a/1232046
-        this.log.splice(0, this.log.length);
-        this.waiting = true;
-      },
-      right() {
-        this.log.push(AnswerTypes.RIGHT);
-        this.waiting = false;
-      },
-      wrong() {
-        this.log.push(AnswerTypes.WRONG);
-        this.waiting = false;
-      },
-      hint() {
-        this.log.push(AnswerTypes.HINT);
-        this.waiting = false;
-      },
-      next() {
-        this.waiting = true;
-      },
-    },
-    data() {
-      return {
-        waiting: true,
-        success: false,
-        // M: 4,
-        N: 6,
-        log: [
-          AnswerTypes.HINT,
-          AnswerTypes.RIGHT,
-          AnswerTypes.WRONG,
-        ],
-      };
-    },
   };
-
 </script>
 
 
 <style lang="stylus" scoped>
-
   @require '~kolibri.styles.coreTheme'
-
-  h1
-    margin-top: 80px
-
-  li
-    margin-bottom: 8px
-
-  .answer
-    width: 25px
-
 </style>
