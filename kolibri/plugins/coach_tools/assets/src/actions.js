a6aac3aa4763b0e3ea5ab96076f8564a6a102ccf
Christian Memije <christianmemije@gmail.com>
2016-11-04 18:44:14 -0700

    Moved reports options to constants.

--- kolibri/plugins/coach_tools/assets/src/actions.js
+++ kolibri/plugins/coach_tools/assets/src/actions.js
@@ -1,13 +1,7 @@
 const router = require('kolibri.coreVue.router');
-const PageNames = require('./state/constants').PageNames;
 const coreActions = require('kolibri.coreVue.vuex.actions');
-
-
-// Valid options
-const CONTENT_SCOPE_OPTIONS = ['root', 'topic', 'content'];
-const USER_SCOPE_OPTIONS = ['facility', 'classroom', 'learnergroup', 'user'];
-const ALL_OR_RECENT_OPTIONS = ['all', 'recent'];
-const VIEW_BY_CONTENT_OR_LEARNERS_OPTIONS = ['content_view', 'user_view'];
+const PageNames = require('./state/constants').PageNames;
+const ReportsOptions = require('./state/constants').ReportsOptions;
 
 
 function showCoachRoot(store) {
@@ -20,23 +14,24 @@ function redirectToDefaultReports(store, params) {
   store.dispatch('CORE_SET_PAGE_LOADING', true);
   store.dispatch('SET_PAGE_NAME', 'REPORTS_NO_QUERY');
 
+  // if necessary query server root topic PK
   // TODO: Get channel id, root id, and facility id.
   const channelId = 'channel_id';
   const contentScopeId = 'root_id';
   const userScopeId = 'facility_id';
 
-  // if necessary query server root topic PK
-
   router.replace({
     name: PageNames.REPORTS,
     params: {
       channel_id: channelId,
-      content_scope: CONTENT_SCOPE_OPTIONS[0],
+      content_scope: ReportsOptions.CONTENT_SCOPE_OPTIONS[0],
       content_scope_id: contentScopeId,
-      user_scope: USER_SCOPE_OPTIONS[0],
+      user_scope: ReportsOptions.USER_SCOPE_OPTIONS[0],
       user_scope_id: userScopeId,
-      all_or_recent: ALL_OR_RECENT_OPTIONS[0],
-      view_by_content_or_learners: VIEW_BY_CONTENT_OR_LEARNERS_OPTIONS[0],
+      all_or_recent: ReportsOptions.ALL_OR_RECENT_OPTIONS[0],
+      view_by_content_or_learners: ReportsOptions.VIEW_BY_CONTENT_OR_LEARNERS_OPTIONS[0],
+      sort_column: ReportsOptions.SORT_COLUMN_OPTIONS[0],
+      sort_order: ReportsOptions.SORT_ORDER_OPTIONS[0],
     },
   });
 }
@@ -54,17 +49,19 @@ function showReports(store, params) {
   // const userScopeId = params.user_scope_id;
   const allOrRecent = params.all_or_recent;
   const viewByContentOrLearners = params.view_by_content_or_learners;
+  const sortColumn = params.sort_column;
+  const sortOrder = params.sort_order;
 
 
   // Check if params are valid.
-  if (!(CONTENT_SCOPE_OPTIONS.includes(contentScope)
-    && USER_SCOPE_OPTIONS.includes(userScope)
-    && ALL_OR_RECENT_OPTIONS.includes(allOrRecent)
-    && VIEW_BY_CONTENT_OR_LEARNERS_OPTIONS.includes(viewByContentOrLearners))) {
-    // If incorrect redirect to default reports.
+  if (!(ReportsOptions.CONTENT_SCOPE_OPTIONS.includes(contentScope)
+    && ReportsOptions.USER_SCOPE_OPTIONS.includes(userScope)
+    && ReportsOptions.ALL_OR_RECENT_OPTIONS.includes(allOrRecent)
+    && ReportsOptions.VIEW_BY_CONTENT_OR_LEARNERS_OPTIONS.includes(viewByContentOrLearners)
+    && ReportsOptions.SORT_COLUMN_OPTIONS.includes(sortColumn)
+    && ReportsOptions.SORT_ORDER_OPTIONS.includes(sortOrder))) {
+    // If invalid query, just throw error.
     coreActions.handleError(store, 'Invalid query. Redirected to a valid query.');
-    console.log('Invalid query.');
-    redirectToDefaultReports(store, params);
   } else {
     console.log('Valid query.');
   }
