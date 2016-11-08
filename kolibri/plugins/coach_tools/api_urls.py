8677422562e7e37021eef0a56660c2593a8d7c53
Rafael Aguayo <ralphaguayo22@gmail.com>
2016-11-08 15:05:17 -0800

    Added 404 errors for incorrect api urls.

--- kolibri/plugins/coach_tools/api_urls.py
+++ kolibri/plugins/coach_tools/api_urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.http import Http404
 from rest_framework import routers
 
 from .api import ContentReportViewSet, ContentSummaryViewSet, RecentReportViewSet, UserReportViewSet, UserSummaryViewSet
@@ -15,9 +16,12 @@ content_summary_router.register(r'contentsummary', ContentSummaryViewSet, base_n
 user_summary_router = routers.SimpleRouter()
 user_summary_router.register(r'usersummary', UserSummaryViewSet, base_name='usersummary')
 
+def return_404(*args, **kwargs):
+    raise Http404
 
 urlpatterns = [
     url(r'^(?P<channel_id>[^/.]+)/(?P<content_node_id>[^/.]+)/(?P<collection_kind>[^/.]+)/(?P<collection_id>[^/.]+)/', include(reports_router.urls)),
     url(r'^(?P<channel_id>[^/.]+)/(?P<collection_kind>[^/.]+)/(?P<collection_id>[^/.]+)/', include(content_summary_router.urls)),
     url(r'^(?P<channel_id>[^/.]+)/(?P<content_node_id>[^/.]+)/', include(user_summary_router.urls)),
+    url(r'^', return_404)
 ]
