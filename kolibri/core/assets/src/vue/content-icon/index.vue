ba05daa46eb28dbd8fddc7103a540a15fd144de6
indirectlylit <devon@indirectlylit.com>
2016-11-09 10:56:12 -0800

    add a color option to content icon

--- kolibri/core/assets/src/vue/content-icon/index.vue
+++ kolibri/core/assets/src/vue/content-icon/index.vue
@@ -1,11 +1,11 @@
 <template>
 
   <div>
-    <svg v-if="kind=='topic'" class="content-icon" src="./content-icons/topic.svg"></svg>
-    <svg v-if="kind=='video'" class="content-icon" src="./content-icons/video.svg"></svg>
-    <svg v-if="kind=='audio'" class="content-icon" src="./content-icons/audio.svg"></svg>
-    <svg v-if="kind=='document'" class="content-icon" src="./content-icons/document.svg"></svg>
-    <svg v-if="kind=='exercise'" class="content-icon" src="./content-icons/exercise.svg"></svg>
+    <svg v-if="kind=='topic'" :class="['content-icon', colorClass]" src="./content-icons/topic.svg"></svg>
+    <svg v-if="kind=='video'" :class="['content-icon', colorClass]" src="./content-icons/video.svg"></svg>
+    <svg v-if="kind=='audio'" :class="['content-icon', colorClass]" src="./content-icons/audio.svg"></svg>
+    <svg v-if="kind=='document'" :class="['content-icon', colorClass]" src="./content-icons/document.svg"></svg>
+    <svg v-if="kind=='exercise'" :class="['content-icon', colorClass]" src="./content-icons/exercise.svg"></svg>
   </div>
 
 </template>
@@ -37,6 +37,15 @@
           return false;
         },
       },
+      colorstyle: {
+        type: String,
+        default: 'action',
+      },
+    },
+    computed: {
+      colorClass() {
+        return `color-${this.colorStyle}`;
+      },
     },
   };
 
@@ -50,6 +59,11 @@
   .content-icon
     width: 100%
     height: 100%
+
+  .color-action
     fill: $core-action-normal
+
+  .color-text-default
+    fill: $core-text-default
 
 </style>
