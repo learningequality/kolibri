3edeb49c7d1fd6e794077fad007d031ed4b14d58
Christian Memije <christianmemije@gmail.com>
2016-11-10 14:54:33 -0800

    Made the tabs into their own all-recent-tabs component.

---
+++ kolibri/plugins/coach_tools/assets/src/vue/reports/all-recent-tabs.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <div class="tabs">
+    <a v-link="recentviewlink" :class="{ active: isrecentview }">Recent</a>
+    <a v-link="allviewlink" :class="{ active: !isrecentview }">Topics</a>
+  </div>
+
+</template>
+
+
+<script>
+
+  module.exports = {
+    props: {
+      recentviewlink: {
+        type: Object,
+        required: true,
+      },
+      allviewlink: {
+        type: Object,
+        required: true,
+      },
+      isrecentview: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~kolibri.styles.coreTheme'
+
+  .tabs a
+    background-color: $core-bg-canvas
+    color: $core-action-normal
+    border: none
+    padding: 5px 15px
+    font-size: 1em
+    border-radius: 0
+    border-bottom: solid 3px white
+    margin-top: 5px
+    &:hover
+      color: $core-action-dark
+
+  .tabs a.active
+    background-color: $core-bg-light
+    border-bottom: solid 3px $core-action-normal
+
+  .tabcontents
+    background-color: $core-bg-light
+
+</style>
--- kolibri/plugins/coach_tools/assets/src/vue/reports/index.vue
+++ kolibri/plugins/coach_tools/assets/src/vue/reports/index.vue
@@ -7,10 +7,12 @@
     <breadcrumbs :list="userBreadcrumbs"></breadcrumbs>
 
     <!--TABS-->
-    <div class="tabs">
-      <a v-link="recentViewLink" :class="{ active: isRecentView }">Recent</a>
-      <a v-link="allViewLink" :class="{ active: !isRecentView }">Topics</a>
-    </div>
+    <all-recent-tabs
+      :recentviewlink="recentViewLink"
+      :allviewlink="allViewLink"
+      :isrecentview="isRecentView"
+    ></all-recent-tabs>
+
 
     <div class="tabcontents">
       <!--CONTENT BREADCRUMBS-->
@@ -49,6 +51,7 @@
   module.exports = {
     components: {
       'breadcrumbs': require('./breadcrumbs'),
+      'all-recent-tabs': require('./all-recent-tabs'),
       'report-header': require('./report-header'),
       'summary-section': require('./summary-section'),
       'view-by-switch': require('./view-by-switch'),
@@ -147,24 +150,5 @@
 <style lang="stylus" scoped>
 
   @require '~kolibri.styles.coreTheme'
-
-  .tabs a
-    background-color: $core-bg-canvas
-    color: $core-action-normal
-    border: none
-    padding: 5px 15px
-    font-size: 1em
-    border-radius: 0
-    border-bottom: solid 3px white
-    margin-top: 5px
-    &:hover
-      color: $core-action-dark
-
-  .tabs a.active
-    background-color: $core-bg-light
-    border-bottom: solid 3px $core-action-normal
-
-  .tabcontents
-    background-color: $core-bg-light
 
 </style>
