0a4202e801ae80f8304d9ec19b553de38692eca5
indirectlylit <devon@indirectlylit.com>
2016-11-10 14:15:33 -0800

    move computed properties out of summary up to report

--- kolibri/plugins/coach_tools/assets/src/vue/reports/summary-section/index.vue
+++ kolibri/plugins/coach_tools/assets/src/vue/reports/summary-section/index.vue
@@ -3,24 +3,24 @@
   <div class="summary-section">
 
     <!--TOPICS-->
-    <div v-if="contentSummary.kind === 'topic'">
-      <p>{{ exerciseCount }} Exercises - {{ contentCount }} Content Items</p>
+    <div v-if="kind === 'topic'">
+      <p>{{ exercisecount }} Exercises - {{ contentcount }} Content Items</p>
       <p>Last Active: {{ lastActiveText }}</p>
       <div>
         <p>Exercises</p>
-        <progress-bar :progress="exerciseProgress"></progress-bar>
+        <progress-bar :progress="exerciseprogress"></progress-bar>
       </div>
       <div>
         <p>Content</p>
-        <progress-bar :progress="contentProgress"></progress-bar>
+        <progress-bar :progress="contentprogress"></progress-bar>
       </div>
     </div>
 
     <!--EXERCISE-->
-    <div v-if="contentSummary.kind === 'exercise'">
+    <div v-if="kind === 'exercise'">
       <p>{{ questions_count }} Questions - Mastery Model: {{ mastery_model }}</p>
       <p>Last Active: {{ last_active }}</p>
-      <div v-if="userSummary.full_name">
+      <div v-if="singleuser">
         <p>{{ questions_answered }} Questions Answered - {{ attempts }} Attempts - {{ time_spent }} -
           {{ date_mastered }}</p>
       </div>
@@ -31,10 +31,10 @@
     </div>
 
     <!--VIDEO/AUDIO-->
-    <div v-if="contentSummary.kind === 'video' ">
+    <div v-if="kind === 'video' ">
       <p>{{ duration }}</p>
       <p>Last Active: {{ last_active }}</p>
-      <div v-if="userSummary.full_name">
+      <div v-if="singleuser">
         <p>{{ time_spent }}</p>
         <progress-bar :progress="video_progress"></progress-bar>
       </div>
@@ -44,10 +44,10 @@
     </div>
 
     <!--DOCUMENT-->
-    <div v-if="contentSummary.kind === ('document' || 'audio')">
+    <div v-if="kind === ('document' || 'audio')">
       <p>{{ pages }} Pages</p>
       <p>Last Active: {{ last_active }}</p>
-      <div v-if="userSummary.full_name">
+      <div v-if="singleuser">
         <p>
           <span v-if="document_progress === 1.000">Viewed - {{ time_spent }}</span>
           <span v-else>Not Viewed</span>
@@ -64,61 +64,46 @@
 
 <script>
 
-  /* given an array of objects sum the keys on those that pass the filter */
-  function sumOfKeys(array, key, filter = () => true) {
-    return array
-      .filter(filter)
-      .reduce((prev, item) => prev + item[key], 0);
-  }
-
   module.exports = {
     $trNameSpace: 'report-summary',
     $trs: {
       lastActiveText: '{0, date, medium}',
     },
     computed: {
-      exerciseCount() {
-        return sumOfKeys(
-          this.contentSummary.progress,
-          'node_count',
-          item => item.kind === 'exercise'
-        );
-      },
-      exerciseProgress() {
-        const totalProgress = sumOfKeys(
-          this.contentSummary.progress,
-          'total_progress',
-          item => item.kind === 'exercise'
-        );
-        return totalProgress / this.exerciseCount;
-      },
-      contentCount() {
-        return sumOfKeys(
-          this.contentSummary.progress,
-          'node_count',
-          item => item.kind !== 'exercise'
-        );
-      },
-      contentProgress() {
-        const totalProgress = sumOfKeys(
-          this.contentSummary.progress,
-          'total_progress',
-          item => item.kind !== 'exercise'
-        );
-        return totalProgress / this.contentCount;
-      },
       lastActiveText() {
-        if (this.contentSummary.last_active) {
-          return this.$tr('lastActiveText', new Date(this.contentSummary.last_active));
+        if (this.lastactive) {
+          return this.$tr('lastActiveText', new Date(this.lastactive));
         }
         return '–';
       },
     },
-    vuex: {
-      getters: {
-        loading: state => state.core.loading,
-        contentSummary: state => state.pageState.content_scope_summary,
-        userSummary: state => state.pageState.user_scope_summary,
+    props: {
+      kind: {
+        type: String,
+        required: true,
+      },
+      exercisecount: {
+        type: Number,
+        required: true,
+      },
+      exerciseprogress: {
+        type: Number,
+        required: true,
+      },
+      contentcount: {
+        type: Number,
+        required: true,
+      },
+      contentprogress: {
+        type: Number,
+        required: true,
+      },
+      lastactive: {
+        type: String,
+      },
+      singleuser: {
+        type: Boolean,
+        required: true,
       },
     },
     data: () => ({
