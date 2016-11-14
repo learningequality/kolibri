5c3fb4cf3cfd285101f039bcdd96d25ac50ca513
indirectlylit <devon@indirectlylit.com>
2016-11-14 09:34:17 -0800

    allow ‘undefined’ as input to progress cells

--- kolibri/plugins/coach_tools/assets/src/vue/reports/data-cells/progress-cell.vue
+++ kolibri/plugins/coach_tools/assets/src/vue/reports/data-cells/progress-cell.vue
@@ -1,11 +1,14 @@
 <template>
 
   <div>
-    <progress-bar :progress="num"></progress-bar>
-    <div v-if="extraText" class="extra-text">
-      <svg src="../../icons/user.svg" class="person-icon"></svg>
-      {{extraText}}
-    </div>
+    <template v-if="num !== undefined">
+      <progress-bar :progress="num"></progress-bar>
+      <div v-if="extraText" class="extra-text">
+        <svg src="../../icons/user.svg" class="person-icon"></svg>
+        {{extraText}}
+      </div>
+    </template>
+    <template v-else>–</template>
   </div>
 
 </template>
@@ -23,7 +26,6 @@
     props: {
       num: {
         type: Number,
-        required: true,
         validator(value) {
           return value >= 0 && value <= 1;
         },
