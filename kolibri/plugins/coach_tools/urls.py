238790713c16fb3a33d5c86526d9df224567cd39
indirectlylit <devon@indirectlylit.com>
2016-11-21 11:41:07 -0800

    fix - only explicit relative imports

--- kolibri/plugins/coach_tools/urls.py
+++ kolibri/plugins/coach_tools/urls.py
@@ -1,7 +1,7 @@
-from api_urls import urlpatterns
 from django.conf.urls import include, url
 
 from . import views
+from .api_urls import urlpatterns
 
 urlpatterns = [
     url('^api/', include(urlpatterns)),
