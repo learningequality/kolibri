e416a9b38f49a6c4f44a593ec3fce46819ce8037
Christian Memije <christianmemije@gmail.com>
2016-11-22 15:32:17 -0800

    More accessible dropdown menu!

--- kolibri/core/assets/src/vue/content-renderer/download-button.vue
+++ kolibri/core/assets/src/vue/content-renderer/download-button.vue
@@ -2,16 +2,17 @@
 
   <div class="dropdown">
     <icon-button
+      v-el:dropdownbutton
       :text="$tr('downloadContent')"
       :primary="false"
       :icononright="true"
       class="dropdown-button"
       @click="toggleDropdown"
-      aria-haspopup="true"
-    >
+      aria-haspopup="true">
       <svg src="./expand.svg"></svg>
     </icon-button>
     <ul
+      v-el:dropdownitems
       class="dropdown-items"
       :class="{ dropdownopen: dropdownopen }"
       :aria-hidden="dropDownOpenText"
@@ -55,26 +56,80 @@
     data() {
       return {
         dropdownopen: false,
+        itemSelected: 0,
       };
     },
     computed: {
       dropDownOpenText() {
         return String(this.dropdownopen);
       },
+      dropDownItems() {
+        let listItems = this.$els.dropdownitems.children;
+        listItems = [...listItems];
+        const anchorItems = [];
+        listItems.forEach((li) => {
+          anchorItems.push(li.children[0]);
+        });
+        return anchorItems;
+      },
+      focusableItems() {
+        let focusableItems = [];
+        focusableItems.push(this.$els.dropdownbutton);
+        focusableItems = focusableItems.concat(this.dropDownItems);
+        return focusableItems;
+      },
     },
     methods: {
-      toggleDropdown() {
-        this.dropdownopen = !this.dropdownopen;
-      },
       prettifyFileSize(bytes) {
         return filesize(bytes);
       },
+      toggleDropdown() {
+        this.dropdownopen = !this.dropdownopen;
+        this.itemSelected = 0;
+      },
       handleKeys(e) {
         if (this.dropdownopen) {
-          if (e.keyCode === 27) {
-            this.toggleDropdown();
+          switch (e.keyCode) {
+            case 40: // down
+              e.stopPropagation();
+              e.preventDefault();
+              this.itemSelected++;
+              this.focusOnItem();
+              return;
+
+            case 38: // up
+              e.stopPropagation();
+              e.preventDefault();
+              this.itemSelected--;
+              this.focusOnItem();
+              return;
+
+            case 9: // tab
+              e.stopPropagation();
+              e.preventDefault();
+              if (this.itemSelected === (this.focusableItems.length - 1)) {
+                this.toggleDropdown();
+                return;
+              }
+              this.itemSelected++;
+              this.focusOnItem();
+              return;
+
+            case 27: // esc
+              e.stopPropagation();
+              e.preventDefault();
+              this.toggleDropdown();
+              return;
+
+            default:
+              return;
           }
         }
+      },
+      focusOnItem() {
+        this.itemSelected =
+          Math.min(Math.max(this.itemSelected, 0), (this.focusableItems.length - 1));
+        this.focusableItems[this.itemSelected].focus();
       },
     },
     ready() {
@@ -106,24 +161,30 @@
     list-style: none
     padding: 0
     margin: 0
-    margin-top: 1px
+    margin-top: 2px
     display: none
     position: absolute
 
   .dropdown-item
-    padding: 0.5em
+    padding: 0
     margin: 0
     width: 100%
     position: relative
     display: block
-    &:hover
-      background-color: $core-action-light
 
   .dropdown-item-link
+    padding: 0.5em
+    margin: 0
+    width: 100%
     display: block
     text-decoration: none
     white-space: nowrap
     font-size: smaller
+    &:focus
+      background-color: $core-action-light
+    &:hover
+      background-color: $core-action-light
+      outline: $core-action-light 2px solid
 
   .dropdownopen
     display: block
