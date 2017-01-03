967728da6e9b946b1b7064dcc9bb2c703353810d
indirectlylit <devon@indirectlylit.com>
2017-01-02 18:22:06 -0800

    simplify attempt-log representation, add validation, and get animations to work again
    reverts some changes from eee745af24b40ae6040c744e5798cb6a83f17136

--- kolibri/core/assets/src/vue/exercise-attempts/index.vue
+++ kolibri/core/assets/src/vue/exercise-attempts/index.vue
@@ -4,11 +4,11 @@
     <transition-group name="fade">
       <div
         class="answer"
-        v-for="(item, index) in log"
-        :style="styleForIndex(index)"
-        key="index"
+        v-for="(item, index) in itemsToRender"
+        :style="styleForIndex(index, item.originalIndex)"
+        :key="item.originalIndex"
       >
-        <answer-icon :answer="item" :success="success"/>
+        <answer-icon :answer="item.answer" :success="success"/>
       </div>
     </transition-group>
     <div
@@ -40,10 +40,13 @@
         type: Number,
         required: true,
       },
-      // An array of AnswerTypes for all attempts (including those not shown).
-      // Ordered from first to most recent.
+      // Array of answers - strings that are 'right', 'wrong', or 'hint'
+      // ordered from first to last
       log: {
         type: Array,
+        validator(arr) {
+          return arr.every(val => ['right', 'wrong', 'hint'].includes(val));
+        },
       },
     },
     components: {
@@ -56,11 +59,19 @@
         }
         return this.numSpaces + 1;
       },
+      // returns a list of items the items to be rendered in the DOM
+      itemsToRender() {
+        // save the original index of the item in the log and slice of the end
+        return this.log
+          .map((answer, originalIndex) => ({ answer, originalIndex }))
+          .slice(-1 * this.numItemsToRender)
+          .reverse();
+      },
     },
     methods: {
-      styleForIndex(visualIndex) {
+      styleForIndex(visualIndex, originalIndex) {
         const ANSWER_WIDTH = 4 + 30 + 4;  // margin + width + margin
-        let xPos = ANSWER_WIDTH * visualIndex;
+        let xPos = ANSWER_WIDTH * (this.log.length - 1 - originalIndex);
         if (this.waiting) {
           xPos += ANSWER_WIDTH;
         }
@@ -97,14 +108,11 @@
     margin: $margin
     text-align: center
     position: absolute
-    transition: transform 0.5s ease-in-out, opacity 1s ease-in-out
+    transition: all 0.5s ease-in-out
 
     // try to improve performance - http://stackoverflow.com/a/10133679
     backface-visibility: hidden
     perspective: 1000
-
-  .fade-enter-active, .fade-leave-active
-    transition: all 0.3s ease
 
   .fade-enter, .fade-leave-active
     opacity: 0
