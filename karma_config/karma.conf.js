e1cac3266322bf16d4e11b75dea30fd9453587c2
Richard Tibbles <richard.tibbles@gmail.com>
2017-01-10 17:31:58 -0800

    Use Karma es6-shim to let tests run ES6 code.

--- karma_config/karma.conf.js
+++ karma_config/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'es6-shim'],
 
 
     // list of files / patterns to load
