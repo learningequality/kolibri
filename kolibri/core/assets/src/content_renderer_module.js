811f3beedfd24b19a61ac3538d485cc03130aa6c
Richard Tibbles <richard.tibbles@gmail.com>
2017-02-02 10:42:57 -0800

    Allow content renderer modules to use a kind: [extensions...] object to define the content types they can render.

--- kolibri/core/assets/src/content_renderer_module.js
+++ kolibri/core/assets/src/content_renderer_module.js
@@ -2,7 +2,14 @@ const KolibriModule = require('kolibri_module');
 
 module.exports = class ContentRenderer extends KolibriModule {
   render(contentType) {
-    if ((Array.isArray(this.contentType) && this.contentType.includes(contentType)) ||
+    const kind = contentType.split('/')[0];
+    const extension = contentType.split('/')[1];
+    // Check if it is an object and not null
+    if ((this.contentType === new Object(this.contentType) &&
+      // Check if it has kind as a key.
+      this.contentType[kind] &&
+      // Check if that kind has the extension in its array
+      this.contentType[kind].includes(extension)) ||
       this.contentType === contentType) {
       this.broadcastComponent(contentType);
     }
