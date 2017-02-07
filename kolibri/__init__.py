445dc4e60bba2283b8faab479ccadfd1507e7f63
Aron Fyodor Asor <aronasorman@gmail.com>
2017-02-07 15:07:02 -0800

    Make flake8 happy by removing unnecessary imports.

--- kolibri/__init__.py
+++ kolibri/__init__.py
@@ -2,10 +2,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 # NB! This is not necessarily the version scheme we want, however having a good
 # tracking of releases once we start doing lots of pre-releases is essential.
-from .utils.version import derive_version_from_git_tag, derive_version_from_version_file, get_version
+from .utils.version import get_version
 
 __author__ = 'Learning Equality'
 __email__ = 'info@learningequality.org'
-__version__ = str(
-    derive_version_from_git_tag() or derive_version_from_version_file()
-)
+__version__ = str(get_version())
