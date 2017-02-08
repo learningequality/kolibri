0724eb3a9673e4da384c9a2ec1b3c6983b058c5d
David <davidxcanas@gmail.com>
2017-02-07 20:59:47 -0800

    Bootstrapping user plugin. Running into some issues with the way Vue is rendering everything

---
+++ kolibri/plugins/user/assets/src/state/constants.js
@@ -0,0 +1,12 @@
+
+// a name for every URL pattern
+const PageNames = {
+  LOGIN: 'LOGIN',
+  SIGN_UP: 'SIGN_UP',
+  EDIT: 'EDIT',
+  SCRATCHPAD: 'SCRATCHPAD',
+};
+
+module.exports = {
+  PageNames,
+};
