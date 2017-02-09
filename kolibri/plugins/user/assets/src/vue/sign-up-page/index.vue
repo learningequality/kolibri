7eabc54e5d8fbc4679493b23d215a3e71879c13e
Christian Memije <christianmemije@gmail.com>
2017-02-09 12:56:48 -0800

    Adding forms to sign in and sign up pages.

--- kolibri/plugins/user/assets/src/vue/sign-up-page/index.vue
+++ kolibri/plugins/user/assets/src/vue/sign-up-page/index.vue
@@ -1,8 +1,16 @@
 <template>
 
   <div>
-    <h1>User Sign Up</h1>
-    <p>Use this page for in-progress component prototyping.</p>
+    <h1>Create an Account</h1>
+
+    <form ref="form">
+      <label>Name <input type="text" placeholder="Enter Name" required v-model="name"></label>
+      <label>Username <input type="text" placeholder="Enter A Username" required v-model="username"></label>
+      <label>Password <input type="password" placeholder="Enter A Password" required v-model="password"></label>
+      <label>Confirm Password <input type="password" placeholder="Confirm Password" required v-model="confirmed_password"></label>
+      <icon-button :primary="true" text="Finish" type="submit" @click="signUp"></icon-button>
+    </form>
+
   </div>
 
 </template>
@@ -11,6 +19,20 @@
 <script>
 
   module.exports = {
+    data: () => ({
+      name: '',
+      username: '',
+      password: '',
+      confirmed_password: '',
+    }),
+    methods: {
+      signUp() {
+        if (this.$refs.form.checkValidity()) {
+          // call sign up action
+          console.log('sign up');
+        }
+      },
+    },
   };
 
 </script>
@@ -19,5 +41,8 @@
 <style lang="stylus" scoped>
 
   @require '~kolibri.styles.coreTheme'
+
+  label
+    display: block
 
 </style>
