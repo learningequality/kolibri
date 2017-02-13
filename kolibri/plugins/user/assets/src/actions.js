98d1a47cb47993884831f99b821f82971693b73e
Christian Memije <christianmemije@gmail.com>
2017-02-13 10:04:26 -0800

    Signup working.

--- kolibri/plugins/user/assets/src/actions.js
+++ kolibri/plugins/user/assets/src/actions.js
@@ -9,7 +9,9 @@
 const constants = require('./state/constants');
 const PageNames = constants.PageNames;
 // const samePageCheckGenerator = require('kolibri.coreVue.vuex.actions').samePageCheckGenerator;
-
+const SignUpResource = require('kolibri').resources.SignUpResource;
+const coreActions = require('kolibri.coreVue.vuex.actions');
+// const router = require('kolibri.coreVue.router');
 
 // ================================
 // USER ACTIONS
@@ -43,8 +45,26 @@ function showProfile(store) {
   store.dispatch('CORE_SET_TITLE', 'User Profile');
 }
 
+
+function signUp(store, signUpCreds) {
+  const signUpModel = SignUpResource.createModel(signUpCreds);
+  const signUpPromise = signUpModel.save(signUpCreds);
+  signUpPromise.then(() => {
+    store.dispatch('CORE_SET_SIGN_UP_ERROR', null);
+    // TODO: Use router.
+    window.location.href = 'http://127.0.0.1:8000';
+  }).catch(error => {
+    if (error.status.code === 400) {
+      store.dispatch('CORE_SET_SIGN_UP_ERROR', 400);
+    } else {
+      coreActions.handleApiError(store, error);
+    }
+  });
+}
+
 module.exports = {
   showSignIn,
   showSignUp,
   showProfile,
+  signUp,
 };
