6cd1daa1627af3f09fde6e3e52b50b8599fdf831
Christian Memije <christianmemije@gmail.com>
2017-02-14 15:23:37 -0800

    Create app-bar component

---
+++ kolibri/core/assets/src/vue/app-bar/index.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div>
+    <ui-toolbar
+      :title="title"
+      type="colored"
+      textColor="white"
+      class="app-bar">
+    </ui-toolbar>
+  </div>
+
+</template>
+
+
+<script>
+
+  module.exports = {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+    components: {
+      'ui-toolbar': require('keen-ui/src/UiToolbar'),
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  .app-bar
+    z-index: 1000
+
+</style>
