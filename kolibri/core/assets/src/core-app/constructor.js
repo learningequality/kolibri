de0d584765399fd1052489bd964e183fd936a7e5
indirectlylit <indirectlylit@users.noreply.github.com>
2017-02-15 18:42:33 -0800

    fix lint

--- kolibri/core/assets/src/core-app/constructor.js
+++ kolibri/core/assets/src/core-app/constructor.js
@@ -146,12 +146,12 @@ module.exports = class CoreApp {
 
   get client() {
     return (options) => {
-      if (!options.method || options.method === "GET") {
+      if (!options.method || options.method === 'GET') {
         const cacheBust = new Date().getTime();
         options.params[cacheBust] = cacheBust;
       }
       return rest.wrap(mime, { mime: 'application/json' }).wrap(csrf, { name: 'X-CSRFToken',
         token: cookiejs.get('csrftoken') }).wrap(errorCode)(options);
-    }
+    };
   }
 };
