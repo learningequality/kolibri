b70d95e9cc2207ca7d25f9d2e3031bb80b2bcac7
indirectlylit <indirectlylit@users.noreply.github.com>
2017-02-16 16:42:15 -0800

    Update core-base.vue
    whitespace

--- kolibri/core/assets/src/vue/core-base.vue
+++ kolibri/core/assets/src/vue/core-base.vue
@@ -104,9 +104,7 @@
       },
       navOpenStyle() {
         if (this.navShown) {
-          return {
-            marginLeft: `${this.paddingForNav}px`,
-          };
+          return { marginLeft: `${this.paddingForNav}px` };
         }
         return '';
       },
