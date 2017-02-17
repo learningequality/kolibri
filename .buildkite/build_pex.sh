ac34a885f527cfcf054df5eb2c5b9327ee63da16
Aron Fyodor Asor <aronasorman@gmail.com>
2017-02-17 10:41:48 -0800

    Make sure the pex package exists on whoever is building our pex file.

--- .buildkite/build_pex.sh
+++ .buildkite/build_pex.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+pip install pex                 # pex is really the only thing we need here.
 buildkite-agent artifact download 'dist/*.whl' dist/
 make pex
 buildkite-agent artifact upload 'dist/*.pex'
