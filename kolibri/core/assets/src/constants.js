b226e14c8e722d7f2ebcfe06a6df20ed3be0b7f3
Christian Memije <christianmemije@gmail.com>
2017-02-17 19:01:59 -0800

    FIx Learn title.

--- kolibri/core/assets/src/constants.js
+++ kolibri/core/assets/src/constants.js
@@ -64,8 +64,7 @@ widgets they supply to core and in what order, this
 is a work-around.
 */
 const TopLevelPageNames = {
-  LEARN_LEARN: 'LEARN_LEARN',
-  LEARN_EXPLORE: 'LEARN_EXPLORE',
+  LEARN: 'LEARN',
   COACH: 'COACH',
   MANAGE: 'MANAGE',
   USER: 'USER',
