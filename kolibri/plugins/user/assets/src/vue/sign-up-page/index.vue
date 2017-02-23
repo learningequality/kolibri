8722c496300f3dd04c7d132951b10b75b3387a09
Richard Tibbles <richard.tibbles@gmail.com>
2017-02-23 15:33:21 -0800

    Update signin page to latest and genericize/materialize text and branding.

--- kolibri/plugins/user/assets/src/vue/sign-up-page/index.vue
+++ kolibri/plugins/user/assets/src/vue/sign-up-page/index.vue
@@ -1,47 +1,74 @@
 <template>
 
-  <div>
-    <h1>{{ $tr('createAccount') }}</h1>
+  <div id="signup-page">
 
-    <form ref="form" @submit.prevent="signUp">
-      <label for="name">{{ $tr('name') }}</label>
-      <input
-        id="name"
-        type="text"
+    <ui-toolbar type="colored" textColor="white">
+      <template slot="icon">
+        <img id="logo" src="../img/instant-school-logo.png" />
+      </template>
+      <template slot="brand">
+        {{ $tr('kolibri') }}
+      </template>
+      <div slot="actions">
+        <router-link id="login" :to="signInPage">
+          <span>{{ $tr('logIn') }}</span>
+        </router-link>
+      </div>
+    </ui-toolbar>
+
+    <form class="signup-form" ref="form" @submit.prevent="signUp">
+      <ui-alert type="error" @dismiss="resetSignUpState" v-if="errorCode">
+        {{errorMessage}}
+      </ui-alert>
+
+      <h1 class="signup-title">{{ $tr('createAccount') }}</h1>
+
+      <ui-textbox
         :placeholder="$tr('enterName')"
+        :label="$tr('name')"
         :aria-label="$tr('name')"
         v-model="name"
         autocomplete="name"
+        autofocus
         required
-        autofocus>
-      <label for="username">{{ $tr('username') }}</label>
-      <input
-        id="username"
-        type="text"
+        id="name"
+        type="text" />
+
+      <ui-textbox
         :placeholder="$tr('enterUsername')"
+        :label="$tr('username')"
         :aria-label="$tr('username')"
+        :invalid="usernameError"
         v-model="username"
-        required>
-      <label for="password">{{ $tr('password') }}</label>
-      <input
+        autocomplete="username"
+        required
+        id="username"
+        type="text" />
+
+      <ui-textbox
         id="password"
         type="password"
         :placeholder="$tr('enterPassword')"
         :aria-label="$tr('password')"
+        :label="$tr('password')"
         v-model="password"
         autocomplete="new-password"
-        required>
-      <label for="confirmed-password">{{ $tr('confirmPassword') }}</label>
-      <input
+        required />
+
+      <ui-textbox
         id="confirmed-password"
         type="password"
         :placeholder="$tr('confirmPassword')"
         :aria-label="$tr('confirmPassword')"
+        :label="$tr('confirmPassword')"
+        :invalid="!passwordsMatch"
+        :error="passwordError "
         v-model="confirmed_password"
-        required>
-      <icon-button :primary="true" text="Finish" type="submit"></icon-button>
+        autocomplete="new-password"
+        required />
 
-      <p v-if="signUpError" class="sign-up-error">{{ $tr('signUpError') }}</p>
+      <icon-button :disabled="canSubmit" id="submit" :primary="true" text="Finish" type="submit" />
+
     </form>
 
   </div>
@@ -52,29 +79,69 @@
 <script>
 
   const actions = require('../../actions');
+  const PageNames = require('../../state/constants').PageNames;
 
   module.exports = {
+    name: 'Sign-Up-Page',
     $trNameSpace: 'signUpPage',
     $trs: {
-      createAccount: 'Create an Account',
+      createAccount: 'Create an account',
       name: 'Name',
-      enterName: 'Enter Name',
+      enterName: 'Enter name',
       username: 'Username',
-      enterUsername: 'Enter Username',
+      enterUsername: 'Enter username',
       password: 'Password',
-      enterPassword: 'Enter Password',
-      confirmPassword: 'Confirm Password',
-      signUpError: 'That username already exists. Try a different username.',
+      enterPassword: 'Enter password',
+      confirmPassword: 'Confirm password',
+      passwordMatchError: 'Passwords do not match',
+      genericError: 'Something went wrong during sign up!',
+      logIn: 'Log in',
+      kolibri: 'Kolibri',
     },
     components: {
       'icon-button': require('kolibri.coreVue.components.iconButton'),
+      'ui-alert': require('keen-ui/src/UiAlert'),
+      'ui-textbox': require('keen-ui/src/UiTextbox'),
+      'ui-toolbar': require('keen-ui/src/UiToolbar'),
+      'ui-checkbox': require('keen-ui/src/UiCheckbox'),
     },
     data: () => ({
       name: '',
       username: '',
       password: '',
       confirmed_password: '',
+      termsAgreement: false,
     }),
+    computed: {
+      signInPage() {
+        return { name: PageNames.SIGN_IN };
+      },
+      passwordsMatch() {
+        // make sure both fields are populated
+        if (this.password && this.confirmed_password) {
+          return this.password === this.confirmed_password;
+        }
+        return true;
+      },
+      passwordError() {
+        if (this.passwordsMatch) {
+          return '';
+        }
+        return this.$tr('passwordMatchError');
+      },
+      usernameError() {
+        return this.errorCode === 400;
+      },
+      allFieldsPopulated() {
+        return !(this.name && this.username && this.password && this.confirmed_password);
+      },
+      canSubmit() {
+        return !this.termsAgreement || this.allFieldsPopulated || !this.passwordsMatch || this.busy;
+      },
+      errorMessage() {
+        return this.backendErrorMessage || this.$tr('genericError');
+      },
+    },
     methods: {
       signUp() {
         this.signUpAction({
@@ -86,10 +153,14 @@
     },
     vuex: {
       getters: {
-        signUpError: state => state.core.signUpError === 400,
+        session: state => state.core.session,
+        errorCode: state => state.pageState.errorCode,
+        busy: state => state.pageState.busy,
+        backendErrorMessage: state => state.pageState.errorMessage,
       },
       actions: {
         signUpAction: actions.signUp,
+        resetSignUpState: actions.resetSignUpState,
       },
     },
   };
@@ -100,17 +171,56 @@
 <style lang="stylus" scoped>
 
   @require '~kolibri.styles.definitions'
+  $iphone-5-width = 320px
+  $vertical-page-margin = 100px
+  $logo-size = (1.64 * 1.125)rem
+  $logo-margin = (0.38 * $logo-size)rem
 
-  input
+  // component, highest level
+  #signup-page
+    width: 100%
+    height: 100%
+    overflow-y: auto
+
+  // Action Bar
+  #logo
+    // 1.63 * font height
+    height: $logo-size
+    display: inline-block
+    margin-left: $logo-margin
+
+  #login
+    margin-right: 1em
+    color: white
+    text-decoration: none
+
+  // Form
+  .signup-title
+    text-align: center
+
+  .signup-form
+    margin-top: $vertical-page-margin
+    margin-left: auto
+    margin-right: auto
+    width: ($iphone-5-width - 20)px
+
+  .terms
+    background-color: $core-bg-light
+    color: $core-text-annotation
+    height: 6em
+    overflow-y: scroll
+    padding: 0.5em
+    margin-bottom: 1em
+    p
+      margin-top: 0
+
+  #submit
+    width: 90%
     display: block
+    margin-left: auto
+    margin-right: auto
 
-  .sign-up-error
-    color: red
-
-  input
-    display: block
-
-  .sign-up-error
-    color: red
+    margin-top: $vertical-page-margin
+    margin-bottom: $vertical-page-margin
 
 </style>
