c85ce94669394ecbc91b4975b5a62d5a2e79ab0a
Aron Fyodor Asor <aronasorman@gmail.com>
2017-02-27 10:38:12 -0800

    Only device owners can create tasks now.

---
+++ kolibri/tasks/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsDeviceOwnerOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_superuser
