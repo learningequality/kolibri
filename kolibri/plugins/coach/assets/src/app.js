1d7b1c2cef4216354f098231f41dcc57f97fd978
66eli77 <66eli77@gmail.com>
2017-03-08 13:21:40 -0800

    get rid of unused code

--- kolibri/plugins/coach/assets/src/app.js
+++ kolibri/plugins/coach/assets/src/app.js
@@ -10,20 +10,6 @@ const store = require('./state/store');
 const PageNames = require('./state/constants').PageNames;
 
 
-// const REPORT_URL_PATTERN = [
-//   '/reports',
-//   ':channel_id',
-//   ':content_scope',
-//   ':content_scope_id',
-//   ':user_scope',
-//   ':user_scope_id',
-//   ':all_or_recent',
-//   ':view_by_content_or_learners',
-//   ':sort_column',
-//   ':sort_order',
-// ].join('/');
-
-
 class CoachToolsModule extends KolibriModule {
   ready() {
     coreActions.getCurrentSession(store).then(() => {
