ba858efc21ee4fd16599fdbf570be10fae8d3690
Richard Tibbles <richard.tibbles@gmail.com>
2017-03-08 10:04:56 -0800

    Rename all classes to Coach instead of CoachTools.

--- kolibri/plugins/coach/views.py
+++ kolibri/plugins/coach/views.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import, print_function, unicode_literals
 from django.views.generic.base import TemplateView
 
 
-class CoachToolsView(TemplateView):
+class CoachView(TemplateView):
     template_name = "coach/coach.html"
