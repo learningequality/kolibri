0fc1525abc3b21f8faabc670b299a78beb73e8e9
66eli77 <66eli77@gmail.com>
2017-03-15 15:37:47 -0700

    update answer-history component to use real date from the server-side

--- kolibri/plugins/coach/assets/src/vue/coach-exercise-render-page/answer-history/index.vue
+++ kolibri/plugins/coach/assets/src/vue/coach-exercise-render-page/answer-history/index.vue
@@ -1,7 +1,47 @@
 <template>
 
   <div id="answer-history">
-    <h1>{{ $tr('header') }}</h1>
+    <h3>{{ $tr('header') }}</h3>
+
+    <ul id="history-list">
+      <template v-for="(attemptLog, index) in attemptLogs">
+        <li v-if="index == 0">
+          <p class="item">
+            {{dayElapse(attemptLog.end_timestamp)}}
+          </p>
+        </li>
+        <li v-else-if="dayElapse(attemptLogs[index -1].end_timestamp) != dayElapse(attemptLog.end_timestamp)">
+          <p class="item">
+            {{dayElapse(attemptLog.end_timestamp)}}
+          </p>
+        </li>
+        <li @click="setSelected(index)" :class="isSeleteced(index)" class="clickable">
+          <div>
+            <mat-svg
+              v-if="attemptLog.hinted"
+              class="item svg-item svg-hint"
+              category="action"
+              name="lightbulb_outline"
+            />
+            <mat-svg
+              v-else-if="attemptLog.correct === 0"
+              class="item svg-item svg-wrong"
+              category="navigation"
+              name="cancel"
+            />
+            <mat-svg
+              v-else
+              class="item svg-item svg-correct"
+              category="action"
+              name="check_circle"
+            />
+            <h3 class="item">
+              {{ $tr('question') }} {{index+1}}
+            <h3>
+          </div>
+        </li>
+      </template>
+    </ul>
   </div>
 
 </template>
@@ -9,26 +49,53 @@
 
 <script>
 
+  const today = new Date();
+  const actions = require('../../../actions');
+
   module.exports = {
     $trNameSpace: 'CoachExerciseAnswerHistory',
     $trs: {
       header: 'Answer History',
+      today: 'Today',
+      yesterday: 'Yesterday',
+      daysAgo: ' Days Ago',
+      question: 'Question',
     },
-    // components: {
-    //   'content-renderer': require('kolibri.coreVue.components.contentRenderer'),
-    //   'page-status': require('./page-status'),
-    //   'answer-history': require('./answer-history'),
-    //   'question-attempt': require('./question-attempt'),
-    // },
-    computed: {
-      channelId() {
-        return '78eed5c0b59b30c0a40c94c17c849af6';
+    data: () => ({
+      selectedIndex: 0,
+    }),
+    methods: {
+      dayElapse(time) {
+        const logDay = new Date(time);
+        // one day = 24*60*60*1000 = 86400000
+        const elapse = (Date.UTC(today.getYear(), today.getMonth(), today.getDate()) -
+          Date.UTC(logDay.getYear(), logDay.getMonth(), logDay.getDate())) / 86400000;
+
+        if (elapse > 1) {
+          return elapse + this.$tr('daysAgo');
+        }
+        else if(elapse === 1) {
+          return this.$tr('yesterday');
+        }
+        return this.$tr('today');
+      },
+      setSelected(index) {
+        this.selectedIndex = index;
+        this.setSelectedAttemptLog(this.attemptLogs[index]);
+      },
+      isSeleteced(index) {
+        if(this.selectedIndex === index) {
+          return 'selected';
+        }
       },
     },
     vuex: {
       getters: {
-        pageState: state => state.pageState,
+        attemptLogs: state => state.pageState.attemptLogs,
       },
+      actions: {
+        setSelectedAttemptLog: actions.setSelectedAttemptLog,
+      }
     },
   };
 
@@ -37,7 +104,44 @@
 
 <style lang="stylus" scoped>
 
+  @require '~kolibri.styles.definitions'
+
   #answer-history
-    background-color: green
+    background-color: $core-bg-light
+
+  #history-list
+    overflow-y: scroll
+    list-style-type: none
+    max-height: inherit
+
+  .item
+    display: inline-block
+    height: 24px
+
+  .svg-item
+    vertical-align: middle
+
+  .svg-hint
+    fill: grey
+
+  .svg-wrong
+    fill: red
+
+  .svg-correct
+    fill: green
+
+  ul
+    padding-left: 0
+
+  li
+    clear: both
+    min-width: 120px
+    border-bottom: 2px solid $core-text-disabled
+
+  .clickable
+    cursor: pointer
+
+  .selected
+    background-color: $core-text-disabled
 
 </style>
