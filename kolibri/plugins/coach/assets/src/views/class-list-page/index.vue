be72db89dd0bb19e1c07a8e30cc340b15f6e28ad
indirectlylit <devon@indirectlylit.com>
2017-04-04 22:56:25 -0700

    rename learners -> members in class list

--- kolibri/plugins/coach/assets/src/views/class-list-page/index.vue
+++ kolibri/plugins/coach/assets/src/views/class-list-page/index.vue
@@ -11,7 +11,7 @@
         <thead>
           <tr>
             <th scope="col" class="table-text">{{ $tr('className') }}</th>
-            <th scope="col" class="table-data">{{ $tr('learners') }}</th>
+            <th scope="col" class="table-data">{{ $tr('members') }}</th>
             <th scope="col" class="table-data">{{ $tr('groups') }}</th>
           </tr>
         </thead>
@@ -68,7 +68,7 @@
       pageDescription: 'View learner progress and performance',
       className: 'Class name',
       tableCaption: 'List of classes',
-      learners: 'Learners',
+      members: 'Members',
       groups: 'Groups',
       noClassesExist: 'No Classes Exist.',
     },
