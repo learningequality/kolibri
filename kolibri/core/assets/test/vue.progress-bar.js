5bf31fdc111303abe95668dec9f8476dfd4feb85
Jonathan Boiser <jboiser@gmail.com>
2017-04-06 11:44:07 -0400

    Make edge tests more interesting

--- kolibri/core/assets/test/vue.progress-bar.js
+++ kolibri/core/assets/test/vue.progress-bar.js
@@ -1,10 +1,9 @@
 /* eslint-env mocha */
-
 const Vue = require('vue-test');
+const assert = require('assert');
 const progressBar = require('../src/views/progress-bar');
 
 const ProgressBarComponent = Vue.extend(progressBar);
-const assert = require('assert');
 
 describe('progressBar Component', () => {
   describe('`percent` computed property', () => {
@@ -17,7 +16,7 @@ describe('progressBar Component', () => {
       }).$mount();
     });
     it('should give 0 percent for progress of < 0', () => {
-      vm.progress = -1.0;
+      vm.progress = -0.0000000001;
       assert.equal(vm.percent, 0);
     });
     it('should give 10 percent for progress of 0.1', () => {
@@ -29,7 +28,7 @@ describe('progressBar Component', () => {
       assert.equal(vm.percent, 100);
     });
     it('should give 100 percent for progress of > 1.0', () => {
-      vm.progress = 70.0;
+      vm.progress = 1.0000000001;
       assert.equal(vm.percent, 100);
     });
   });
