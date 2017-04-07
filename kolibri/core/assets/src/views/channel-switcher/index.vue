49831f0aa0263e2339af8fba50c7fb5305e5f0c7
Christian Memije <christianmemije@gmail.com>
2017-04-06 17:51:44 -0700

    Creates a dropdown-menu core component

--- kolibri/core/assets/src/views/channel-switcher/index.vue
+++ kolibri/core/assets/src/views/channel-switcher/index.vue
@@ -1,52 +1,20 @@
 <template>
 
-  <span>
-    <span v-if="windowSize.breakpoint > 0">
-      <ui-button
-        class="app-bar-button"
-        icon="view_module"
-        type="secondary"
-        color="white"
-        :ariaLabel="$tr('switchChannels')"
-        has-dropdown
-        ref="buttonLarge">
-        {{ currentChannelName }}
-        <ui-menu
-          class="channel-switcher-menu"
-          contain-focus
-          contains-icons
-          slot="dropdown"
-          :options="channelOptions"
-          @close="$refs.buttonLarge.closeDropdown()"
-          @select="channelSelected"/>
-      </ui-button>
-    </span>
-    <span v-else>
-      <ui-icon-button
-        icon="view_module"
-        type="secondary"
-        color="white"
-        :ariaLabel="$tr('switchChannels')"
-        has-dropdown
-        ref="button">
-        <ui-menu
-          class="channel-switcher-menu"
-          contain-focus
-          contains-icons
-          slot="dropdown"
-          :options="channelOptions"
-          @close="$refs.button.closeDropdown()"
-          @select="channelSelected"/>
-      </ui-icon-button>
-    </span>
-  </span>
+  <dropdown-menu
+    :name="currentChannelName"
+    :options="channelOptions"
+    :inAppBar="true"
+    :displayDisabledAsSelected="true"
+    type="primary"
+    color="white"
+    icon="view_module"
+    @select="emitSelection"
+  />
 
 </template>
 
 
 <script>
-
-  const responsiveWindow = require('kolibri.coreVue.mixins.responsiveWindow');
 
   module.exports = {
     $trNameSpace: 'channelSwitcher',
@@ -54,11 +22,8 @@
       switchChannels: 'Switch channels',
     },
     components: {
-      'ui-button': require('keen-ui/src/UiButton'),
-      'ui-icon-button': require('keen-ui/src/UiIconButton'),
-      'ui-menu': require('keen-ui/src/UiMenu'),
+      'dropdown-menu': require('kolibri.coreVue.components.dropdownMenu'),
     },
-    mixins: [responsiveWindow],
     computed: {
       channelOptions() {
         return this.channelList.map(channel => {
@@ -72,13 +37,16 @@
         });
       },
       currentChannelName() {
-        return (Object(this.channelList.find(channel =>
-          channel.id === this.globalCurrentChannel
-        ))).title;
+        const channelName = Object(this.channelList.find(
+            channel => channel.id === this.globalCurrentChannel)).title;
+        if (channelName) {
+          return channelName;
+        }
+        return '';
       },
     },
     methods: {
-      channelSelected(channel) {
+      emitSelection(channel) {
         this.$emit('switch', channel.id);
       },
     },
@@ -93,17 +61,4 @@
 </script>
 
 
-<style lang="stylus">
-
-  @require '~kolibri.styles.definitions'
-
-  .channel-switcher-menu
-    max-width: 210px
-    .ui-menu-option
-        &.is-disabled
-          color: $core-accent-color
-          font-weight: bold
-          opacity: 1
-          background-color: rgba(0, 0, 0, 0.05)
-
-</style>
+<style lang="stylus"></style>
---
+++ kolibri/core/assets/src/views/dropdown-menu/index.vue
@@ -0,0 +1,120 @@
+<template>
+
+  <span>
+    <span v-if="windowSize.breakpoint > 0">
+      <ui-button
+        :ariaLabel="name"
+        :type="type"
+        :color="color"
+        :icon="icon"
+        :has-dropdown="true"
+        :class="{ appbarbutton: inAppBar }"
+        ref="buttonLarge">
+        {{ name }}
+        <ui-menu
+          :options="options"
+          slot="dropdown"
+          class="dropdown-menu"
+          :class="{ disabledasselected: displayDisabledAsSelected }"
+          @close="$refs.buttonLarge.closeDropdown()"
+          @select="emitSelection"/>
+      </ui-button>
+    </span>
+    <span v-else>
+      <ui-icon-button
+        :ariaLabel="name"
+        :type="type"
+        :color="color"
+        :icon="icon"
+        :has-dropdown="true"
+        ref="button">
+        <ui-menu
+          :options="options"
+          slot="dropdown"
+          class="dropdown-menu"
+          :class="{ disabledasselected: displayDisabledAsSelected }"
+          @close="$refs.button.closeDropdown()"
+          @select="emitSelection"/>
+      </ui-icon-button>
+    </span>
+  </span>
+
+</template>
+
+
+<script>
+
+  const responsiveWindow = require('kolibri.coreVue.mixins.responsiveWindow');
+
+  module.exports = {
+    components: {
+      'ui-button': require('keen-ui/src/UiButton'),
+      'ui-icon-button': require('keen-ui/src/UiIconButton'),
+      'ui-menu': require('keen-ui/src/UiMenu'),
+    },
+    mixins: [responsiveWindow],
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      options: {
+        type: Array,
+        required: true,
+      },
+      icon: {
+        type: String,
+        default: '',
+      },
+      type: {
+        type: String,
+        default: 'secondary',
+      },
+      color: {
+        type: String,
+        default: 'primary'
+      },
+      inAppBar: {
+        type: Boolean,
+        default: false,
+      },
+      displayDisabledAsSelected: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      emitSelection(selection) {
+        this.$emit('select', selection);
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  span
+    display: inline-block
+
+</style>
+
+
+<style lang="stylus">
+
+  @require '~kolibri.styles.definitions'
+
+  .dropdown-menu
+    &.ui-menu
+      max-width: 210px
+
+    &.disabledasselected
+      .ui-menu-option
+        &.is-disabled
+          color: $core-accent-color
+          font-weight: bold
+          opacity: 1
+          background-color: rgba(0, 0, 0, 0.05)
+
+</style>
