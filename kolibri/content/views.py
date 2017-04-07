a3486cfc2eb64b5fd8f62b7f96fc3252f644e18c
Richard Tibbles <richard.tibbles@gmail.com>
2017-04-07 09:37:59 -0700

    Explicitly include a mime.types file to enforce mimetype checking.

--- kolibri/content/views.py
+++ kolibri/content/views.py
@@ -11,7 +11,7 @@ from .utils.paths import get_content_storage_file_path
 
 # Do this to prevent import of broken Windows filetype registry that makes guesstype not work.
 # https://www.thecodingforums.com/threads/mimetypes-guess_type-broken-in-windows-on-py2-7-and-python-3-x.952693/
-mimetypes.init([])
+mimetypes.init([os.path.join(os.path.dirname(__file__), 'constants', 'mime.types')])
 
 class ZipContentView(View):
 
