9bbc805cc302dd9622e4c9d30b05bc921b5b0c17
Richard Tibbles <richard.tibbles@gmail.com>
2017-04-10 14:24:05 -0700

    Actually use filter backend.

--- kolibri/content/api.py
+++ kolibri/content/api.py
@@ -220,7 +220,7 @@ class ExamViewset(viewsets.ModelViewSet):
     serializer_class = serializers.ExamSerializer
     pagination_class = OptionalPageNumberPagination
     permissions_classes = (KolibriAuthPermissions,)
-    filter_backends = (KolibriAuthPermissionsFilter,)
+    filter_backends = (KolibriAuthPermissionsFilter, ExamFilter)
 
     def get_queryset(self):
         return models.Exam.objects.all()
