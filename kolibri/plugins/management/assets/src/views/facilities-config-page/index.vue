5dd004337e8ecdbfdc856fe074378b44a9ee7ca5
Jonathan Boiser <jboiser@gmail.com>
2017-04-11 13:44:25 -0400

    Add markup for settings checklist

--- kolibri/plugins/management/assets/src/views/facilities-config-page/index.vue
+++ kolibri/plugins/management/assets/src/views/facilities-config-page/index.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div>
     <div>
       <h2>Facilities Configuration</h2>
@@ -14,14 +15,37 @@
 
     <div>
       <h2>Facility Settings</h2>
+      <div class="settings">
+        <ui-checkbox box-position="right">
+          Allow users to edit their username
+        </ui-checkbox>
+        <ui-checkbox box-position="right">
+          Allow users to edit their full name
+        </ui-checkbox>
+        <ui-checkbox box-position="right">
+          Allow users to change their password when logged in
+        </ui-checkbox>
+        <ui-checkbox box-position="right">
+          Allow users to delete their account
+        </ui-checkbox>
+        <ui-checkbox box-position="right">
+          Permit users to sign-up on this device
+        </ui-checkbox>
+        <ui-checkbox box-position="right">
+          Require users to log-in on this device
+        </ui-checkbox>
+      </div>
     </div>
   </div>
+
 </template>
 
+
 <script>
+
   module.exports = {
     components: {
-
+      'ui-checkbox': require('keen-ui/src/UiCheckbox'),
     },
     computed: {
 
@@ -38,8 +62,18 @@
       },
     },
   };
+
 </script>
 
+
 <style lang="stylus" scoped>
+
+  .settings
+    max-width: 35rem
+
+  .settings > label
+    cursor: pointer
+    font-weight: bold
+    margin: 1.5rem 0
 
 </style>
