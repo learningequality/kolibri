5913cbb4aeb4970da8632a8fb1f97a9e0050f315
Jonathan Boiser <jboiser@gmail.com>
2017-04-12 17:06:16 -0400

    Allow a facility_id query param at /facilitydataset

--- kolibri/core/assets/src/api-resources/facilityDataset.js
+++ kolibri/core/assets/src/api-resources/facilityDataset.js
@@ -1,5 +1,9 @@
 const { Resource } = require('../api-resource');
 
+/**
+ * @example <caption>Get Datasets for a given Facility</caption>
+ * FacilityDatasetResource.getCollection({ facility_id: 1 })
+ */
 class FacilityDatasetResource extends Resource {
   static resourceName() {
     return 'facilitydataset';
