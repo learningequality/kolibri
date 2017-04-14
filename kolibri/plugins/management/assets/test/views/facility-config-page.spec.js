c0469333ebdac77d1e436af87ac1f2fb3e3315ff
Jonathan Boiser <jboiser@gmail.com>
2017-04-14 01:01:07 -0400

    Add test for clicking checkbox

--- kolibri/plugins/management/assets/test/views/facility-config-page.spec.js
+++ kolibri/plugins/management/assets/test/views/facility-config-page.spec.js
@@ -1,30 +1,44 @@
 /* eslint-env mocha */
 const Vue = require('vue-test');
+const Vuex = require('vuex');
+const sinon = require('sinon');
 const simulant = require('simulant');
 const ConfigPage = require('../../src/views/facilities-config-page');
 
 function makeWrapper(propsData) {
+  const store = new Vuex.Store({
+    state: {
+      pageState: {
+        settings: {
+          learner_can_edit_username: false,
+        }
+      }
+    },
+    // TODO bring in real mutations
+    mutations: {
+      CONFIG_PAGE_MODIFY_SETTING() {},
+    }
+  });
   const Ctor = Vue.extend(ConfigPage);
-  return new Ctor({ propsData }).$mount();
+  return new Ctor({ propsData, store }).$mount();
 }
 
 function getElements(wrapper) {
   return {
-    checkbox: wrapper.$el.querySelector('input[name="edit_username"]'),
+    checkbox: wrapper.$el.querySelector('input[name="learner_can_edit_username"]'),
   };
 }
 
 describe.only('facility config page view', () => {
-  it('clicking checkboxes dispatches a modify action', (done) => {
+  it('clicking checkboxes dispatches a modify action', () => {
     const wrapper = makeWrapper({});
+    const dispatchSpy = sinon.spy(wrapper.$store, 'dispatch');
     const els = getElements(wrapper);
     simulant.fire(els.checkbox, 'change');
-    Vue.nextTick(() => {
-      simulant.fire(els.checkbox, 'change');
-      Vue.nextTick(() => {
-        simulant.fire(els.checkbox, 'change');
-        done();
-      });
+    sinon.assert.calledWith(dispatchSpy, 'CONFIG_PAGE_MODIFY_SETTING', {
+      name: 'learner_can_edit_username',
+      value: true,
     });
+    dispatchSpy.restore();
   });
 });
