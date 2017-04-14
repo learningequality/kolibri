a8c2f6c489a6bf2cd3a9bed628006502d01bea6c
Jonathan Boiser <jboiser@gmail.com>
2017-04-14 15:06:55 -0400

    Refactor

--- kolibri/plugins/management/assets/test/views/facility-config-page.spec.js
+++ kolibri/plugins/management/assets/test/views/facility-config-page.spec.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 const simulant = require('simulant');
 const ConfigPage = require('../../src/views/facilities-config-page');
 
-function makeWrapper(propsData) {
+function makeWrapper(propsData = {}) {
   const store = new Vuex.Store({
     state: {
       pageState: {
@@ -26,13 +26,14 @@ function makeWrapper(propsData) {
 
 function getElements(wrapper) {
   return {
-    checkbox: wrapper.$el.querySelector('input[name="learner_can_edit_username"]'),
-    resetButton: wrapper.$el.querySelector('button[name="reset-settings"]'),
+    checkbox: () => wrapper.$el.querySelector('input[name="learner_can_edit_username"]'),
+    resetButton: () => wrapper.$el.querySelector('button[name="reset-settings"]'),
     // this modal does not mount/unmount like in real browser; there is
     // weird behavior in general for child nodes in testing
     confirmResetModal: () => wrapper.$el.querySelector('#confirm-reset'),
     cancelResetButton: () => wrapper.$el.querySelector('button[name="cancel"]'),
     confirmResetButton: () => wrapper.$el.querySelector('button[name="reset"]'),
+    saveButton: () => wrapper.$el.querySelector('button[name="save-settings"]'),
   };
 }
 
@@ -44,10 +45,10 @@ function promisifyNextTick() {
 
 describe.only('facility config page view', () => {
   it('clicking checkboxes dispatches a modify action', () => {
-    const wrapper = makeWrapper({});
+    const wrapper = makeWrapper();
     const dispatchSpy = sinon.spy(wrapper.$store, 'dispatch');
-    const els = getElements(wrapper);
-    simulant.fire(els.checkbox, 'change');
+    const { checkbox } = getElements(wrapper);
+    simulant.fire(checkbox(), 'change');
     sinon.assert.calledWith(dispatchSpy, 'CONFIG_PAGE_MODIFY_SETTING', {
       name: 'learner_can_edit_username',
       value: true,
@@ -56,47 +57,53 @@ describe.only('facility config page view', () => {
   });
 
   it('clicking save dispatches a save action', () => {
-    const wrapper = makeWrapper({});
-    // wrapper.resetToDefaultSettings();
+    const wrapper = makeWrapper();
+    const saveActionStub = sinon.stub(wrapper, 'saveFacilityConfig');
+    const { saveButton } = getElements(wrapper);
+    simulant.fire(saveButton(), 'click');
+    return promisifyNextTick()
+    .then(() => {
+      sinon.assert.calledOnce(saveActionStub);
+      saveActionStub.restore();
+    });
   });
 
   it('clicking reset brings up the confirmation modal', () => {
-    const wrapper = makeWrapper({});
+    const wrapper = makeWrapper();
     const { resetButton } = getElements(wrapper);
     assert.equal(wrapper.showModal, false);
-    simulant.fire(resetButton, 'click');
+    simulant.fire(resetButton(), 'click');
     assert.equal(wrapper.showModal, true);
   });
 
   it('canceling reset tears down the modal', () => {
-    const wrapper = makeWrapper({});
+    const wrapper = makeWrapper();
     const { resetButton, cancelResetButton } = getElements(wrapper);
     assert.equal(wrapper.showModal, false);
-    simulant.fire(resetButton, 'click');
+    simulant.fire(resetButton(), 'click');
     return promisifyNextTick()
     .then(() => {
       assert.equal(wrapper.showModal, true);
-      const cancelButton = cancelResetButton();
-      simulant.fire(cancelButton, 'click');
+      simulant.fire(cancelResetButton(), 'click');
       assert.equal(wrapper.showModal, false);
     });
   });
 
   // TODO maybe DRY up the open-modal flow
   it('confirming reset calls the reset action', () => {
-    const wrapper = makeWrapper({});
+    const wrapper = makeWrapper();
     const resetActionStub = sinon.stub(wrapper, 'resetFacilityConfig');
     const { resetButton, confirmResetButton } = getElements(wrapper);
-    simulant.fire(resetButton, 'click');
+    simulant.fire(resetButton(), 'click');
     return promisifyNextTick()
     .then(() => {
-      const confirmButton = confirmResetButton();
-      simulant.fire(confirmButton, 'click');
+      simulant.fire(confirmResetButton(), 'click');
       return promisifyNextTick();
     })
     .then(() => {
       assert.equal(wrapper.showModal, false);
       sinon.assert.called(resetActionStub);
+      resetActionStub.restore();
     });
   });
 
