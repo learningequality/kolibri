1e3b1706e130b583b3dc4c9842b1fb8a5a31e83f
indirectlylit <devon@indirectlylit.com>
2017-04-19 16:30:54 -0700

    pare down item list view to basics

--- kolibri/plugins/coach/assets/src/views/reports/item-list-page.vue
+++ kolibri/plugins/coach/assets/src/views/reports/item-list-page.vue
@@ -1,98 +1,70 @@
 <template>
 
   <div>
-    <div class="tabcontents">
-      <div class="top-section">
 
-        <!--HEADER SECTION-->
-        <h2>
-          <!--
-          <span v-if="pageState.userScopeSummary.full_name">
-            <mat-svg category="social" name="person"/>
-            {{ pageState.userScopeSummary.full_name }} -
-          </span>
-          -->
-          <content-icon
-            :kind="pageState.contentScopeSummary.kind"
-            colorstyle="text-default"
-          />
-          {{ pageState.contentScopeSummary.title }}
-        </h2>
-
-        <!--SUMMARY SECTION-->
-        <summary-section
-          :kind="pageState.contentScopeSummary.kind"
-          :exerciseCount="exerciseCount"
-          :exerciseProgress="exerciseProgress"
-          :contentCount="contentCount"
-          :contentProgress="contentProgress"
-          :singleUser="isSingleUser"
-          :userCount="userCount"
-          :completionCount="completionCount"
-          :isRecentView="isRecentPage"
-        />
-
-        <!--CONTENT BREADCRUMBS-->
-        <breadcrumbs :list="contentBreadcrumbs"/>
-
-      </div>
-
-      <!-- TABLE SECTION -->
-      <div v-if="!isSingleUser || !isSingleItem" class="table-section">
-
-        <!--TABLE SECTION-->
-        <table class="data-table">
-          <thead>
-            <tr>
-              <th is="header-cell"
-                :text="$tr('name')"
-                :column="ReportConstants.TableColumns.NAME"
-                class="name-col coach-filter table-name"
-              ></th>
-              <th is="header-cell"
-                :text="$tr('avg-exercise-progress')"
-                :column="ReportConstants.TableColumns.EXERCISE"
-                class="progress-col coach-filter"
-              ></th>
-              <th is="header-cell"
-                :text="$tr('avg-content-progress')"
-                :column="ReportConstants.TableColumns.CONTENT"
-                class="progress-col coach-filter"
-              ></th>
-              <th is="header-cell"
-                :text="$tr('last-activity')"
-                :column="ReportConstants.TableColumns.DATE"
-                class="date-col coach-filter"
-              ></th>
-            </tr>
-          </thead>
-          <tbody is="transition-group" name="item">
-            <tr v-for="row in standardDataTable" :key="row.id">
-              <th scope="row" class="name-col">
-                <item-cell
-                  :kind="row.kind"
-                  :title="row.title"
-                  :id="row.id"
-                  :parent="row.parent"
-                  :exerciseCount="row.exerciseCount"
-                  :contentCount="row.contentCount"
-                />
-              </th>
-              <td class="progress-col">
-                <progress-cell :num="row.exerciseProgress" :isExercise="true"/>
-              </td>
-              <td class="progress-col">
-                <progress-cell :num="row.contentProgress" :isExercise="false"/>
-              </td>
-              <td class="date-col">
-                <elapsed-time :date="row.lastActive" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
+    <h1>
+      <content-icon
+        :kind="pageState.contentScopeSummary.kind"
+        colorstyle="text-default"
+      />
+      {{ pageState.contentScopeSummary.title }}
+    </h1>
+    <div>
+      <ul>
+        <li>{{ $tr('exerciseCountText', {count: exerciseCount}) }}</li>
+        <li>{{ $tr('contentCountText', {count: contentCount}) }}</li>
+      </ul>
     </div>
+    <breadcrumbs :list="contentBreadcrumbs"/>
+
+    <div class="table-section">
+      <table class="data-table">
+        <thead>
+          <tr>
+            <th is="header-cell"
+              :text="$tr('name')"
+              class="name-col coach-filter table-name"
+            ></th>
+            <th is="header-cell"
+              :text="$tr('avgExerciseProgress')"
+              class="progress-col coach-filter"
+            ></th>
+            <th is="header-cell"
+              :text="$tr('avgContentProgress')"
+              class="progress-col coach-filter"
+            ></th>
+            <th is="header-cell"
+              :text="$tr('lastActivity')"
+              class="date-col coach-filter"
+            ></th>
+          </tr>
+        </thead>
+        <tbody is="transition-group" name="item">
+          <tr v-for="row in standardDataTable" :key="row.id">
+            <th scope="row" class="name-col">
+              <item-cell
+                :kind="row.kind"
+                :title="row.title"
+                :id="row.id"
+                :parent="row.parent"
+                :exerciseCount="row.exerciseCount"
+                :contentCount="row.contentCount"
+              />
+            </th>
+            <td class="progress-col">
+              <progress-cell :num="row.exerciseProgress" :isExercise="true"/>
+            </td>
+            <td class="progress-col">
+              <progress-cell :num="row.contentProgress" :isExercise="false"/>
+            </td>
+            <td class="date-col">
+              <elapsed-time :date="row.lastActive" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 
 </template>
@@ -100,40 +72,28 @@
 
 <script>
 
-  const ReportConstants = require('../../reportConstants');
   const CoachConstants = require('../../constants');
   const reportGetters = require('../../state/getters/reports');
-  const coachGetters = require('../../state/getters/main');
 
   module.exports = {
     $trNameSpace: 'reportPage',
     $trs: {
-      'name': 'Name',
-      'avg-exercise-progress': 'Avg. exercise progress',
-      'avg-content-progress': 'Avg. resource progress',
-      'last-activity': 'Last active',
-      'all-learners': 'All learners ({0, number, integer})',
+      name: 'Name',
+      avgExerciseProgress: 'Avg. exercise progress',
+      avgContentProgress: 'Avg. resource progress',
+      lastActivity: 'Last activity',
+      exerciseCountText: '{count, number, integer} {count, plural, one {Exercise} other {Exercises}}',
+      contentCountText: '{count, number, integer} {count, plural, one {Resource} other {Resources}}',
     },
     components: {
       'content-icon': require('kolibri.coreVue.components.contentIcon'),
-      'summary-section': require('./summary-section'),
       'breadcrumbs': require('./breadcrumbs'),
       'header-cell': require('./header-cell'),
-      'user-cell': require('./data-cells/user-cell'),
       'progress-cell': require('./data-cells/progress-cell'),
       'item-cell': require('./data-cells/item-cell'),
       'elapsed-time': require('kolibri.coreVue.components.elapsedTime'),
     },
     computed: {
-      ReportConstants() {
-        return ReportConstants; // allow constants to be accessed inside templates
-      },
-      isSingleUser() {
-        return this.pageState.userScope === ReportConstants.UserScopes.USER;
-      },
-      isSingleItem() {
-        return this.pageState.contentScope === ReportConstants.ContentScopes.CONTENT;
-      },
       contentBreadcrumbs() {
         const list = this.pageState.contentScopeSummary.ancestors.map((item, index) => {
           const breadcrumb = {
@@ -161,13 +121,8 @@
       getters: {
         pageState: state => state.pageState,
         exerciseCount: reportGetters.exerciseCount,
-        exerciseProgress: reportGetters.exerciseProgress,
         contentCount: reportGetters.contentCount,
-        contentProgress: reportGetters.contentProgress,
         standardDataTable: reportGetters.standardDataTable,
-        userCount: reportGetters.userCount,
-        completionCount: reportGetters.completionCount,
-        isRecentPage: coachGetters.isRecentPage,
       },
     },
   };
@@ -206,7 +161,6 @@
   .item-move
     transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1)
 
-  .top-section,
   .table-section
     background-color: $core-bg-light
     margin-top: 1em
