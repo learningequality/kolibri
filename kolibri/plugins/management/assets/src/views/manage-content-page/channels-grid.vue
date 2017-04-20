f9b7a2b4e85f961b1f42625b952cfea58cbe322c
Jonathan Boiser <jboiser@gmail.com>
2017-04-20 16:15:18 -0400

    Add deleteChannel workflow

--- kolibri/plugins/management/assets/src/views/manage-content-page/channels-grid.vue
+++ kolibri/plugins/management/assets/src/views/manage-content-page/channels-grid.vue
@@ -1,37 +1,56 @@
 <template>
 
-  <table class="tal">
-    <thead>
-      <tr>
-        <th class="col-header col-channel" scope="col">{{ $tr('nameHeader') }}</th>
-        <th class="col-header col-export" scope="col">{{ $tr('numContentsHeader') }}</th>
-        <th class="col-header col-export" scope="col">{{ $tr('sizeHeader') }}</th>
-        <th class="col-header col-export" scope="col">{{ $tr('lastUpdatedHeader') }}</th>
-        <th class="col-header col-export" scope="col"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="channel in channelList">
-        <td>
-          <b>{{ channel.title }}<b>
-        </td>
-        <td>
-          {{ numberOfFilesInChannel(channel.id) }}
-        </td>
-        <td>
-          {{ totalSizeOfFilesInChannel(channel.id) }}
-        </td>
-        <td>
-          {{ lastUpdatedDate(channel.id) }}
-        </td>
-        <td>
-          <button class="delete-button">
-            {{ $tr('delete') }}
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table class="tal">
+      <thead>
+        <tr>
+          <th class="col-header col-channel" scope="col">{{ $tr('nameHeader') }}</th>
+          <th class="col-header col-export" scope="col">{{ $tr('numContentsHeader') }}</th>
+          <th class="col-header col-export" scope="col">{{ $tr('sizeHeader') }}</th>
+          <th class="col-header col-export" scope="col">{{ $tr('lastUpdatedHeader') }}</th>
+          <th class="col-header col-export" scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(channel, idx) in channelList">
+          <td>
+            <b>{{ channel.title }}<b>
+          </td>
+          <td>
+            {{ numberOfFilesInChannel(channel.id) }}
+          </td>
+          <td>
+            {{ totalSizeOfFilesInChannel(channel.id) }}
+          </td>
+          <td>
+            {{ lastUpdatedDate(channel.id) }}
+          </td>
+          <td>
+            <button @click="selectedChannelIdx=idx" class="delete-button">
+              {{ $tr('delete') }}
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <modal
+      title="Delete channel"
+      v-if="channelIsSelected"
+      @cancel="selectedChannelIdx=null"
+    >
+      <div>
+        <p>Are you sure you want to delete {{ selectedChannelTitle }} and its contents?</p>
+        <p>To restore this channel, you will need to re-import it from the internet or storage device.</p>
+      </div>
+
+      <div>
+        <button @click="selectedChannelIdx=null">Cancel</button>
+        <button @click="handleDeleteChannel()">Confirm</button>
+      </div>
+
+    </modal>
+  <div>
 
 </template>
 
@@ -39,9 +58,32 @@
 <script>
 
   const bytesForHumans = require('./bytesForHumans');
+  const actions = require('../../state/actions');
 
   module.exports = {
+    data: () => ({
+      selectedChannelIdx: null,
+    }),
+    computed: {
+      channelIsSelected() {
+        return this.selectedChannelIdx !== null;
+      },
+      selectedChannelTitle() {
+        if (this.channelIsSelected) {
+          return this.channelList[this.selectedChannelIdx].title;
+        }
+        return '';
+      }
+    },
+    components: {
+      modal: require('kolibri.coreVue.components.coreModal'),
+    },
     methods: {
+      handleDeleteChannel() {
+        if (this.selectedChannelIdx !== null) {
+          this.deleteChannel(this.channelList[this.selectedChannelIdx].id);
+        }
+      },
       numberOfFilesInChannel(channelId) {
         const channel = this.channelInfo[channelId];
         return channel ? channel.numberOfFiles : this.$tr('pleaseWait');
@@ -60,6 +102,9 @@
         channelList: state => state.core.channels.list,
         pageState: state => state.pageState,
         channelInfo: state => state.pageState.channelInfo
+      },
+      actions: {
+        deleteChannel: actions.deleteChannel,
       },
     },
     $trNameSpace: 'channelsGrid',
