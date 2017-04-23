573df1f055b647d9174e9be8a4b6e5c2968c6c20
Richard Tibbles <richard.tibbles@gmail.com>
2017-04-22 18:42:48 -0700

    Adds core points-icon component.

---
+++ kolibri/core/assets/src/views/points-icon/index.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div :style="dimensions">
+    <file-svg v-if="active" src="./icons/leaf.svg"/>
+    <file-svg v-else src="./icons/grey-leaf.svg"/>
+  </div>
+
+</template>
+
+
+<script>
+
+  function widthHeightValidate(units) {
+    return units.search(/[0-9]+(em|px|%)/) > -1;
+  }
+
+  module.exports = {
+    props: {
+      active: {
+        type: Boolean,
+        required: true,
+      },
+      height: {
+        type: String,
+        required: true,
+        validator: widthHeightValidate,
+      },
+      width: {
+        type: String,
+        required: true,
+        validator: widthHeightValidate,
+      },
+    },
+    computed: {
+      dimensions() {
+        return {
+          height: this.height,
+          width: this.width,
+        };
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~kolibri.styles.definitions'
+
+</style>
