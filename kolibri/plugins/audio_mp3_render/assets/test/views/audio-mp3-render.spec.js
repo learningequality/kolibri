63e8b9f0bbf41bd53ba802cbd50ce9dc5f2de214
Jonathan Boiser <jboiser@gmail.com>
2017-04-25 11:47:05 -0400

    Use simulant to do ‘timeupdate’ event; improve assertion

--- kolibri/plugins/audio_mp3_render/assets/test/views/audio-mp3-render.spec.js
+++ kolibri/plugins/audio_mp3_render/assets/test/views/audio-mp3-render.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 const Vue = require('vue-test');
 const sinon = require('sinon');
+const simulant = require('simulant');
 const audioRenderer = require('../../src/views/index.vue');
 
 function makeVm(propsData) {
@@ -10,18 +11,18 @@ function makeVm(propsData) {
 
 describe('audio mp3 render component', () => {
   describe('timeUpdate', () => {
-    it('does not error out if <audio> is undefined', () => {
+    it('does not error out if <audio> dispatches event after unmount', () => {
       // regression test for https://github.com/learningequality/kolibri/issues/1276
       const vm = makeVm({ defaultFile: '' });
       const audioEl = vm.$el.querySelector('#audio');
-      try {
-        // unmount the vm to simulate original situation
-        vm.$destroy();
-        // then dispatch an event
-        audioEl.dispatchEvent(new Event('timeupdate'));
-      } catch (err) {
-        sinon.assert.fail('No exceptions were expected');
-      }
+      const updateTimeSpy = sinon.spy(vm, 'updateTime');
+      // unmount the vm to simulate original situation
+      vm.$destroy();
+      // then dispatch timeupdate event immediately after
+      simulant.fire(audioEl, 'timeupdate');
+      Vue.nextTick(() => {
+        sinon.assert.called(updateTimeSpy);
+      });
     });
   });
 });
