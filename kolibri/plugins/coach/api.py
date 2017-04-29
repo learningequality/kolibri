a3a8fa2a5da825a8785d3d1f2792fccd4cec69cc
Richard Tibbles <richard.tibbles@gmail.com>
2017-04-28 23:33:29 -0400

    Uniqueify by content_id for ContentNodes in coach reports.

--- kolibri/plugins/coach/api.py
+++ kolibri/plugins/coach/api.py
@@ -1,7 +1,7 @@
 import datetime
 from dateutil.parser import parse
 
-from django.db.models import Q
+from django.db.models import Min, Q
 from django.utils import timezone
 
 from kolibri.auth.constants import role_kinds
@@ -98,4 +98,6 @@ class RecentReportViewSet(viewsets.ModelViewSet):
             Q(progress__gt=0) | Q(masterylogs__in=attempted_mastery_logs),
             user__in=get_members_or_user(self.kwargs['collection_kind'], self.kwargs['collection_id']),
             end_timestamp__gte=datetime_cutoff).values_list('content_id')
-        return ContentNode.objects.filter(content_id__in=recent_content_items)
+        return ContentNode.objects.filter(pk__in=ContentNode.objects.filter(
+            content_id__in=recent_content_items).values('content_id').annotate(pk=Min('pk')).values_list(
+            'pk', flat=True))
