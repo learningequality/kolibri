1bbf92d8a74cfe73f91764a51ae67647dd02c3b6
Jonathan Boiser <jboiser@gmail.com>
2017-05-02 17:46:10 -0400

    Implement guest user case

--- kolibri/plugins/learn/assets/src/state/prepareLearnApp.js
+++ kolibri/plugins/learn/assets/src/state/prepareLearnApp.js
@@ -1,4 +1,15 @@
+const { currentUserId } = require('kolibri.coreVue.vuex.getters');
+
+const LEARN_SET_MEMBERSHIPS = 'LEARN_SET_MEMBERSHIPS';
+
 function prepareLearnApp(store) {
+  const userId = currentUserId(store.state);
+  if (userId === null) {
+    return Promise.resolve()
+    .then(() => {
+      store.dispatch(LEARN_SET_MEMBERSHIPS, []);
+    });
+  }
   return Promise.resolve();
 }
 
