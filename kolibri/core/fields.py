3c50cff2f454779f8c80ab4ec3c9ef48e2ddc757
Richard Tibbles <richard.tibbles@gmail.com>
2017-05-09 10:12:59 -0700

    Turn all existing DateTimeFields into DateTimeTzFields. Add schema and data migrations.

--- kolibri/core/fields.py
+++ kolibri/core/fields.py
@@ -5,22 +5,27 @@ import pytz
 from django.db.models.fields import Field
 from django.utils import timezone
 
-# Encode with a Z at the end to ensure that Django parses it as
-# UTC.
-date_time_format = "%Y-%m-%d %H:%M:%S.%fZ"
+date_time_format = "%Y-%m-%d %H:%M:%S.%f"
 tz_format = "({tz})"
 tz_regex = re.compile("\(([^\)]+)\)")
 db_storage_string = "{date_time_string}{tz_string}"
 
 def parse_timezonestamp(value):
-    try:
+    if tz_regex.search(value):
         tz = pytz.timezone(tz_regex.search(value).groups()[0])
-    except IndexError:
+    else:
         tz = timezone.get_default_timezone()
     utc_value = tz_regex.sub('', value)
     value = datetime.datetime.strptime(utc_value, date_time_format)
     value = timezone.make_aware(value, pytz.utc)
     return value.astimezone(tz)
+
+def create_timezonestamp(value):
+    tz = value.tzinfo.zone
+    date_time_string = value.strftime(date_time_format)
+    tz_string = tz_format.format(tz=tz)
+    value = db_storage_string.format(date_time_string=date_time_string, tz_string=tz_string)
+    return value
 
 class DateTimeTzField(Field):
 
@@ -43,12 +48,9 @@ class DateTimeTzField(Field):
 
     def get_prep_value(self, value):
         # Casts datetimes into the format expected by the backend
-        # value = super(DateTimeTzField, self).get_prep_value(value)
-        tz = value.tzinfo.zone
-        date_time_string = value.strftime(date_time_format)
-        tz_string = tz_format.format(tz=tz)
-        value = db_storage_string.format(date_time_string=date_time_string, tz_string=tz_string)
-        return value
+        if value is None:
+            return value
+        return create_timezonestamp(value)
 
     def get_db_prep_value(self, value, connection, prepared=False):
         if not prepared:
