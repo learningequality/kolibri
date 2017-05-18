f3b6d4d10307ab9b3a56aeadb27286e5dfad51d1
Christian Memije <christianmemije@gmail.com>
2017-05-18 15:06:08 -0700

    :lipstick: :construction: Changes the appearance of the grid-item

--- kolibri/plugins/learn/assets/src/views/card-grid/grid-item/index.vue
+++ kolibri/plugins/learn/assets/src/views/card-grid/grid-item/index.vue
@@ -1,22 +1,21 @@
 <template>
 
-  <router-link :to="link" class="link-block">
-    <div class="wrapper">
-      <div class="card-thumbnail">
-        <slot/>
-        <div class="progress-icon-wrapper">
-          <progress-icon :progress="progress"/>
-        </div>
+  <router-link :to="link" class="card">
+
+    <div class="card-thumbnail" :style="backgroundImg">
+      <div class="card-progress-icon-wrapper">
+        <progress-icon :progress="progress"/>
       </div>
-      <div class="card-content">
-        <div class="text">
-          <content-icon
-          class="content-icon"
-          v-if="kind"
-          :kind="kind"/>
-          {{ title }}
-        </div>
+
+      <div class="card-content-icon-background" :style="contentIconBackground"></div>
+      <div class="card-content-icon-wrapper">
+        <content-icon :kind="kind" class="card-content-icon"/>
       </div>
+    </div>
+
+    <div class="card-content">
+      <h3 class="card-title">{{ title }}</h3>
+      <h4 class="card-subtitle"></h4>
     </div>
   </router-link>
 
@@ -28,14 +27,14 @@
   const validateLinkObject = require('kolibri.utils.validateLinkObject');
 
   module.exports = {
-    components: {
-      'content-icon': require('kolibri.coreVue.components.contentIcon'),
-      'progress-icon': require('kolibri.coreVue.components.progressIcon'),
-    },
     props: {
       title: {
         type: String,
         required: true,
+      },
+      subtitle: {
+        type: String,
+        required: false,
       },
       kind: {
         type: String,
@@ -50,6 +49,36 @@
         required: true,
         validator: validateLinkObject,
       },
+      thumbnail: {
+        type: String,
+        required: false,
+      },
+    },
+    computed: {
+      backgroundImg() {
+        return { backgroundImage: `url('${this.thumbnail}')` };
+      },
+      contentIconBackground() {
+        let color = '#262626';
+        if (this.kind === 'topics') {
+          color = '#ffca28';
+        } else if (this.kind === 'exercise') {
+          color = '#33A369';
+        } else if (this.kind === 'video') {
+          color = '#3938A5';
+        } else if (this.kind === 'audio') {
+          color = '#E65997';
+        } else if (this.kind === 'document') {
+          color = '#ED2828';
+        } else if (this.kind === 'html5') {
+          color = '#FF8B41';
+        }
+        return { borderColor: `${color} transparent transparent transparent` };
+      },
+    },
+    components: {
+      'content-icon': require('kolibri.coreVue.components.contentIcon'),
+      'progress-icon': require('kolibri.coreVue.components.progressIcon'),
     },
   };
 
@@ -59,57 +88,80 @@
 <style lang="stylus" scoped>
 
   @require '~kolibri.styles.definitions'
-  @require '../../learn.styl'
 
-  $thumb-width = 100px
-
-  .card
-    width: $card-width
-    height: $card-height
-    overflow: hidden
-    background-color: $core-bg-light
-    border-radius: $radius
-
-  .card-thumbnail
-    height: 122px
-    position: relative
-
-  .text
-    -webkit-line-clamp: 3 // Enhance Chrome, doesn't work on other browsers
-    -webkit-box-orient: vertical // Enhance Chrome, doesn't work on other browsers
-    width: auto
-    max-height: 68px
-    margin-left: 35px
-    overflow: hidden
-    font-size: 0.9rem
-    line-height: 1.2rem
-    text-overflow: ellipsis
-    color: $core-text-default
-
-  .content-icon
-    position: absolute
-    left: 10px
-    top: 10px
-    font-size: 1.5em
-
-  .card-content
-    padding: 10px
-    position: relative
+  $card-width = 210px
+  $card-height = $card-width
+  $card-thumbnail-ratio = (9 / 16)
+  $card-thumbnail-height = $card-width * $card-thumbnail-ratio
+  $card-content-height = $card-height - $card-thumbnail-height
+  $card-content-padding = ($card-width / (320 / 24))
+  $card-elevation-resting = 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)
+  $card-elevation-raised = 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2)
 
   a
     text-decoration: none
 
-  .progress-icon-wrapper
+  .card
+    display: inline-block
+    width: $card-width
+    height: $card-height
+    border-radius: 2px
+    background-color: $core-bg-light
+    box-shadow: $card-elevation-resting
+    &:hover, &:focus
+      box-shadow: $card-elevation-raised
+
+  .card-thumbnail
+    position: relative
+    width: $card-width
+    height: $card-thumbnail-height
+    background-size: cover
+    background-position: center
+    background-color: black
+
+  .card-progress-icon-wrapper
     position: absolute
     top: 0.25em
     right: 0.25em
     width: 1.5em
     height: 1.5em
 
-  .wrapper
-    background-color: $core-bg-light
+  .card-content-icon-background
+    position: absolute
+    top: 0
+    left: 0
+    width: 0
+    height: 0
+    border-style: solid
+    border-width: 3.5em 3.5em 0 0
 
-  .link-block
-    display: block
+  .card-content-icon-wrapper
+    position: absolute
+    top: 0
+    left: 0
+    padding: 0.25em
+    color: white
+
+  .card-content-icon
+    font-size: 1.25em
+
+  .card-content
+    padding: $card-content-padding
+    height: $card-content-height
+    color: $core-text-default
+
+  .card-title, .card-subtitle
+    margin: 0
+
+  .card-title
+    overflow: hidden
+    line-height: ($card-width / (320 / 32))
+    height: ($card-width / (320 / 64))
+    font-size: ($card-width / (320 / 24))
+
+  .card-subtitle
+    padding-top: ($card-width / (320 / 16))
+    font-size: ($card-width / (320 / 14))
+    color: $core-text-annotation
 
 </style>
