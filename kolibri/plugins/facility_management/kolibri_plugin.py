eb2d158a0bc7c719ea1f223c249eca35d9ed8b26
Benjamin Bach <benjamin@overtag.dk>
2017-05-30 19:00:37 +0200

    Do not load application stack of plugins in order to enable/disable

--- kolibri/plugins/facility_management/kolibri_plugin.py
+++ kolibri/plugins/facility_management/kolibri_plugin.py
@@ -1,14 +1,16 @@
 from __future__ import absolute_import, print_function, unicode_literals
+
 from django.utils.translation import ugettext_lazy as _
 from kolibri.core.hooks import UserNavigationHook
 from kolibri.core.webpack.hooks import WebpackBundleHook
 from kolibri.plugins.base import KolibriPluginBase
+
 from .hooks import FacilityManagementSyncHook
-from . import urls
 
 
 class FacilityManagementPlugin(KolibriPluginBase):
     def url_module(self):
+        from . import urls
         return urls
 
     def url_slug(self):
