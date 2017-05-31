0399b073023215b34ed7033f7c77d791adebd1a3
Jonathan Boiser <jboiser@gmail.com>
2017-05-31 15:12:18 -0700

    Use drive-list in wizard-import-local

--- kolibri/plugins/management/assets/src/views/manage-content-page/wizard-import-local.vue
+++ kolibri/plugins/management/assets/src/views/manage-content-page/wizard-import-local.vue
@@ -11,43 +11,13 @@
   >
     <div class="main">
       <template v-if="!drivesLoading">
-        <div class="modal-message">
-          <h2 class="core-text-alert" v-if="noDrives">
-            <mat-svg class="error-svg" category="alert" name="error_outline"/>
-            {{$tr('noDrivesDetected')}}
-          </h2>
-          <template v-else>
-            <h2>{{$tr('drivesFound')}}</h2>
-            <div class="drive-list">
-              <div class="enabled drive-names" v-for="(drive, index) in drivesWithData"
-                @click="selectDriveByID(drive.id)">
-                <input
-                  type="radio"
-                  :id="'drive-'+index"
-                  :value="drive.id"
-                  v-model="selectedDrive"
-                  name="drive-select"
-                >
-                <label :for="'drive-'+index">
-                  {{drive.name}}
-                  <br>
-                </label>
-              </div>
-              <div class="disabled drive-names" v-for="(drive, index) in drivesWithoutData">
-                <input
-                  type="radio"
-                  disabled
-                  :id="'disabled-drive-'+index"
-                >
-                <label :for="'disabled-drive-'+index">
-                  {{drive.name}}
-                  <br>
-                  <span class="drive-detail">{{$tr('incompatible')}}</span>
-                </label>
-              </div>
-            </div>
-          </template>
-        </div>
+        <drive-list
+          :value="selectedDrive"
+          :drives="wizardState.driveList"
+          :enabledDrivePred="driveIsEnabled"
+          :disabledMsg="$tr('incompatible')"
+          @change="(driveId) => selectedDrive = driveId"
+        />
         <div class="refresh-btn-wrapper">
           <icon-button
             :text="$tr('refresh')"
@@ -85,8 +55,6 @@
     $trNameSpace: 'wizardLocalImport',
     $trs: {
       title: 'Import from a Local Drive',
-      noDrivesDetected: 'No drives were detected',
-      drivesFound: 'Drives detected:',
       incompatible: 'No content available',
       refresh: 'Refresh',
       cancel: 'Cancel',
@@ -96,51 +64,32 @@
       'core-modal': require('kolibri.coreVue.components.coreModal'),
       'icon-button': require('kolibri.coreVue.components.iconButton'),
       'loading-spinner': require('kolibri.coreVue.components.loadingSpinner'),
+      'drive-list': require('./wizards/drive-list'),
     },
     data: () => ({
-      selectedDrive: undefined, // used when there's more than one option
+      selectedDrive: '',
     }),
     computed: {
-      noDrives() {
-        return !Array.isArray(this.wizardState.driveList);
-      },
-      driveToUse() {
-        if (this.drivesWithData.length === 1) {
-          return this.drivesWithData[0].id;
-        }
-        return this.selectedDrive;
-      },
       drivesLoading() {
         return this.wizardState.driveList === null;
       },
-      drivesWithData() {
-        return this.wizardState.driveList.filter(
-          (drive) => drive.metadata.channels.length
-        );
-      },
-      drivesWithoutData() {
-        return this.wizardState.driveList.filter(
-          (drive) => !drive.metadata.channels.length
-        );
-      },
       canSubmit() {
-        if (this.drivesLoading || this.wizardState.busy) {
-          return false;
-        }
-        return Boolean(this.driveToUse);
+        return (
+          !this.drivesLoading &&
+          this.selectedDrive !== '' &&
+          !this.wizardState.busy
+        );
       },
     },
     methods: {
+      driveIsEnabled: (drive) => drive.metadata.channels.length > 0,
       submit() {
-        this.triggerLocalContentImportTask(this.driveToUse);
+        this.triggerLocalContentImportTask(this.selectedDrive);
       },
       cancel() {
         if (!this.wizardState.busy) {
           this.cancelImportExportWizard();
         }
-      },
-      selectDriveByID(driveID) {
-        this.selectedDrive = driveID;
       },
     },
     vuex: {
@@ -179,28 +128,6 @@
   .error-svg
     margin-right: 0.2em
     margin-bottom: -6px
-
-  .drive-names
-    padding: 0.6em
-    border: 1px $core-bg-canvas solid
-    label
-      display: inline-table
-      font-size: 0.9em
-    &.disabled
-      color: $core-text-disabled
-    &.enabled
-      &:hover
-        background-color: $core-bg-canvas
-      &, label
-        cursor: pointer
-
-  .drive-list:not(first-child)
-    border-top: none
-
-  .drive-detail
-    color: $core-text-annotation
-    font-size: 0.7em
-
 
   .button-wrapper
     margin: 1em 0
