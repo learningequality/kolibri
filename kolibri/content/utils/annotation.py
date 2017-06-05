32affe235a509c0d3f0c7d9b4ae825f6284d30dd
Jonathan Boiser <jboiser@gmail.com>
2017-06-05 15:57:29 -0700

    last_updated is added to ContentMetadataCache only if new

--- kolibri/content/utils/annotation.py
+++ kolibri/content/utils/annotation.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from django.utils import timezone
 from django.conf import settings
 
 from ..content_db_router import using_content_database
@@ -14,15 +14,21 @@ def update_channel_metadata_cache():
     object in the default database to ensure they are in sync.
     """
     db_names = get_channel_ids_for_content_database_dir(settings.CONTENT_DATABASE_DIR)
-    # delete ChannelMetadata obejcts in default db that cannot be found in CONTENT_DATABASE_DIR
+    # Delete ChannelMetadataCache objects in default db that are not found in CONTENT_DATABASE_DIR
     ChannelMetadataCache.objects.exclude(id__in=db_names).delete()
-    # sync the ChannelMetadata objects in default db with ChannelMetadata objects in CONTENT_DATABASE_DIR
+    # sync the ChannelMetadataCache objects in default db with ChannelMetadata objects in CONTENT_DATABASE_DIR
     for db_name in db_names:
-        last_updated_datetime = datetime.now().isoformat()
         with using_content_database(db_name):
             update_values = ChannelMetadata.objects.values()[0]
-            update_values['last_updated'] = last_updated_datetime
-        ChannelMetadataCache.objects.update_or_create(
+
+        ch_metadata_obj, _ = ChannelMetadataCache.objects.update_or_create(
             id=db_name,
             defaults=update_values,
         )
+
+        # Records a new last_updated only if channel is brand new. Does not
+        # handle case where channel's version is upgraded, which is not
+        # yet supported on UI anyway
+        if ch_metadata_obj.last_updated is None:
+            ch_metadata_obj.last_updated = timezone.now()
+            ch_metadata_obj.save()
