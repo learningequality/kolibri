63bc43abe2c34267c122119a3300dd84da7c76bd
Jonathan Boiser <jboiser@gmail.com>
2017-06-06 15:21:12 -0700

    Stub deleteChannel action

---
+++ kolibri/plugins/management/assets/src/state/manageContentActions.js
@@ -0,0 +1,13 @@
+/**
+ * Delete a Channel from the device
+ * @param store - vuex store object
+ * @param {string} channelId - a valid channel UUID
+ * @returns {Promise}
+ */
+function deleteChannel(store, channelId) {
+  return Promise.resolve();
+}
+
+module.exports = {
+  deleteChannel,
+};
