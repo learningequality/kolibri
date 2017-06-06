c4938ad84aabdd3048f91ebc4637f72d6f260050
David <davidxcanas@gmail.com>
2017-06-05 19:16:29 -0700

    Making content-grid a bit more robust

--- kolibri/plugins/learn/assets/src/views/content-cards/content-grid.vue
+++ kolibri/plugins/learn/assets/src/views/content-cards/content-grid.vue
@@ -1,7 +1,14 @@
 <template>
 
-  <div class="pure-g">
-    <slot/>
+  <div>
+    <content-card
+      class="content-card"
+      v-for="content in contents"
+      :title="content.title"
+      :thumbnail="content.thumbnail"
+      :kind="content.kind || 'topic'"
+      :progress="content.progress"
+      :link="item.kind ? genContentLink(item.id) : genTopicLink(item.id)"/>
   </div>
 
 </template>
@@ -9,7 +16,38 @@
 
 <script>
 
-  module.exports = {};
+  const PageNames = require('../../constants').PageNames;
+
+  module.exports = {
+    props: {
+      contents: {
+        type: Array,
+        required: true,
+      },
+    },
+    components: {
+      'content-card': require('./content-card'),
+    },
+    methods: {
+      genContentLink(id) {
+        return {
+          name: PageNames.LEARN_CONTENT,
+          params: { channel_id: this.channelId, id },
+        };
+      },
+      genTopicLink(id) {
+        return {
+          name: PageNames.EXPLORE_TOPIC,
+          params: { channel_id: this.channelId, id },
+        };
+      },
+    },
+    vuex: {
+      getters: {
+        channelId: (state) => state.core.channels.currentId,
+      },
+    }
+  };
 
 </script>
 
@@ -17,5 +55,9 @@
 <style>
 
   @require '~kolibri.styles.definitions'
+
+  .content-card
+    margin-left: 10px
+    margin-right: 10px
 
 </style>
