47f34ec1da3275fd560090e41c24873cbf2b0b47
Aron Fyodor Asor <aronasorman@gmail.com>
2017-06-07 14:01:00 -0700

    deprioritize kolibri.dist from path.
    This is mainly to make sure that python3 uses the standard library's
    
    concurrent.futures package, while python2 will use kolibri.dist.concurrent.futures.

--- kolibri/utils/cli.py
+++ kolibri/utils/cli.py
@@ -14,9 +14,9 @@ import sys  # noqa
 
 import kolibri  # noqa
 from kolibri import dist as kolibri_dist  # noqa
-sys.path = [
+sys.path = sys.path + [
     os.path.realpath(os.path.dirname(kolibri_dist.__file__))
-] + sys.path
+]
 
 # Set default env
 os.environ.setdefault(
