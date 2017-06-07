de2a9c903270420282bf1a1052141bd06c098b85
chaowentan <chaowentan@gmail.com>
2017-06-07 12:37:37 -0700

    Implement a component for auto-generating a table of content

---
+++ kolibri/plugins/style_guide/assets/src/views/shell/table-of-content.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <ul>
+    <li v-for="anchor in anchors">
+      <a :href="route" @click="goToAnchor(anchor.hash)">
+        {{ anchor.label }}
+      </a>
+    </li>
+  </ul>
+
+</template>
+
+
+<script>
+
+  /**
+   * A component for auto-generating a table of content for a page. All elements
+   * with an [id] attribute defined will be shown in the TOC.
+   */
+
+  const map = require('lodash/map');
+
+  module.exports = {
+    name: 'TableOfContent',
+    props: {
+      /** The route of the current page. E.g. "#button" */
+      route: {
+        type: String,
+      },
+    },
+    data() {
+      return {
+        // These are all the anchors to show in the TOC.
+        // They are objects with the "hash" and "label" properties.
+        anchors: [],
+      };
+    },
+    mounted() {
+      this.anchors = map(
+        this.$parent.$el.querySelectorAll('[id]'),
+        (sectionHeadingEl) => ({
+          hash: `#${sectionHeadingEl.id}`,
+          label: sectionHeadingEl.innerText
+        }));
+    },
+    methods: {
+      // Uses the provided hash (e.g. "#heading") as the ID to find the element
+      // with anchor behavior, and scrolls it into view.
+      goToAnchor(hash) {
+        // Notes: scrollIntoView() may not be available in older browsers.
+        document.querySelector(hash).scrollIntoView();
+      }
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  ul
+    list-style-type: none
+
+</style>
