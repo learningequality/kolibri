15f6a993a4f85a6cbc4b2794e2b473efad3057ac
Jonathan Boiser <jboiser@gmail.com>
2017-06-07 14:36:44 -0700

    Move sorting and empty state into channel-grid

--- kolibri/plugins/management/assets/src/views/manage-content-page/index.vue
+++ kolibri/plugins/management/assets/src/views/manage-content-page/index.vue
@@ -35,7 +35,6 @@
           </div>
         </div>
         <hr>
-        <p class="core-text-alert" v-if="!sortedChannels.length">{{$tr('noChannels')}}</p>
 
         <channels-grid />
 
@@ -53,7 +52,6 @@
   const isSuperuser = require('kolibri.coreVue.vuex.getters').isSuperuser;
   const actions = require('../../state/actions');
   const ContentWizardPages = require('../../constants').ContentWizardPages;
-  const orderBy = require('lodash/orderBy');
 
   module.exports = {
     $trNameSpace: 'manageContentState',
@@ -61,7 +59,6 @@
       title: 'My channels',
       import: 'Import',
       export: 'Export',
-      noChannels: 'No channels installed',
       notAdminHeader: 'You need to sign in as the Device Owner to manage content',
       notAdminDetails: 'The Device Owner is the account originally created in the Setup Wizard',
     },
@@ -89,13 +86,6 @@
       }
     },
     computed: {
-      sortedChannels() {
-        return orderBy(
-          this.channelList,
-          [channel => channel.title.toUpperCase()],
-          ['asc']
-        );
-      },
       wizardComponent() {
         switch (this.pageState.wizardState.page) {
           case ContentWizardPages.CHOOSE_IMPORT_SOURCE:
@@ -114,7 +104,6 @@
     vuex: {
       getters: {
         isSuperuser,
-        channelList: state => state.core.channels.list,
         pageState: state => state.pageState,
       },
       actions: {
