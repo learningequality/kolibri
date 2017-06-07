24f8043a34487acbaf153dd33fbe499dca94408d
Jonathan Boiser <jboiser@gmail.com>
2017-06-07 10:57:47 -0700

    Turn actionnames to object

--- kolibri/plugins/management/assets/src/state/manageContentActions.js
+++ kolibri/plugins/management/assets/src/state/manageContentActions.js
@@ -1,5 +1,13 @@
 /* eslint-disable prefer-arrow-callback */
 const { ChannelResource, FileSummaryResource } = require('kolibri').resources;
+
+const namespace = 'MANAGE_CONTENT';
+
+const actionTypes = {
+  ADD_CHANNEL_FILE_SUMMARIES: `${namespace}/ADD_CHANNEL_FILE_SUMMARIES`,
+  REMOVE_CHANNEL: `${namespace}/REMOVE_CHANNEL`,
+};
+
 /**
  * Delete a Channel from the device
  * @param store - vuex store object
@@ -9,27 +17,29 @@ const { ChannelResource, FileSummaryResource } = require('kolibri').resources;
 function deleteChannel(store, channelId) {
   return ChannelResource.getModel(channelId).delete()
   .then(function onSuccess(msg) {
-    // force an update of the channel list
-    console.log('deleted ', msg);
+    store.dispatch(actionTypes.REMOVE_CHANNEL, channelId);
   });
 }
-
 
 /**
  * Hydrate the manage content pageState with channel file summary info
  *
  * @param store - vuex store object
  * @param {Array<String>} channelIds - an array of channelIds
- * @return {Promise}
+ * @return {Promise<Array<FileSummary>>}
  */
 function addChannelFileSummaries(store, channelIds) {
   const promises = channelIds.map(channelId =>
-    FileSummaryResource.getCollection({ channel_id: channelId })
+    FileSummaryResource.getCollection({ channel_id: channelId }).fetch()
   );
-  return Promise.all(promises);
+  return Promise.all(promises)
+  .then(function onSuccess(data) {
+    store.dispatch(actionTypes.ADD_CHANNEL_FILE_SUMMARIES, data);
+  });
 }
 
 module.exports = {
+  actionTypes,
   addChannelFileSummaries,
   deleteChannel,
 };
