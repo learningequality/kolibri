dce53711961a6b6342dd619dbb24e8f2c26b5692
Jonathan Boiser <jboiser@gmail.com>
2017-06-06 18:19:26 -0700

    Stub addChannelFileSummaries action

--- kolibri/plugins/management/assets/src/state/manageContentActions.js
+++ kolibri/plugins/management/assets/src/state/manageContentActions.js
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-arrow-callback */
-const { ChannelResource } = require('kolibri').resources;
+const { ChannelResource, FileSummaryResource } = require('kolibri').resources;
 /**
  * Delete a Channel from the device
  * @param store - vuex store object
@@ -8,12 +8,28 @@ const { ChannelResource } = require('kolibri').resources;
  */
 function deleteChannel(store, channelId) {
   return ChannelResource.getModel(channelId).delete()
-    .then(function onSuccess(msg) {
-      // force an update of the channel list
-      console.log('deleted ', msg);
-    });
+  .then(function onSuccess(msg) {
+    // force an update of the channel list
+    console.log('deleted ', msg);
+  });
+}
+
+
+/**
+ * Hydrate the manage content pageState with channel file summary info
+ *
+ * @param store - vuex store object
+ * @param {Array<String>} channelIds - an array of channelIds
+ * @return {Promise}
+ */
+function addChannelFileSummaries(store, channelIds) {
+  const promises = channelIds.map(channelId =>
+    FileSummaryResource.getCollection({ channel_id: channelId })
+  );
+  return Promise.all(promises);
 }
 
 module.exports = {
+  addChannelFileSummaries,
   deleteChannel,
 };
