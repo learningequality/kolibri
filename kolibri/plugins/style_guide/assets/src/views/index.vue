31fa673fd5dc6f2e874d698c8eabc79fb2ceebd9
Magali <magali.bzr@gmail.com>
2017-06-07 17:06:34 -0700

    Initial parser

--- kolibri/plugins/style_guide/assets/src/views/index.vue
+++ kolibri/plugins/style_guide/assets/src/views/index.vue
@@ -3,6 +3,11 @@
   <div>
 
     <h1>Style Guide</h1>
+    
+    <h2>Dropdown Menu</h2>
+    <h3>Path to file: kolibri/kolibri/core/assets/src/views/dropdown-menu/index.vue</h3>
+    <component-table class="component-table"></component-table>
+    <documentation-component></documentation-component>
 
   </div>
 
@@ -11,7 +16,12 @@
 
 <script>
 
-  module.exports = {};
+  module.exports = {
+    components: {
+      'component-table': require('./component_table'),
+      'documentation-component': require('./documentation_component'),
+    },
+  };
 
 </script>
 
@@ -19,5 +29,10 @@
 <style lang="stylus" scoped>
 
   @require '~kolibri.styles.definitions'
+
+  $component-table-width = 25em
+
+  .component-table
+    width: 100%
 
 </style>
