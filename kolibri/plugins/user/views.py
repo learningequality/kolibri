16a2b46a3bf65856b18cf0bff0c6beffc04ece8c
Richard Tibbles <richard.tibbles@gmail.com>
2017-06-09 17:06:20 -0700

    Bootstrap facility dataset data into user template. Update CHANGELOG.

--- kolibri/plugins/user/views.py
+++ kolibri/plugins/user/views.py
@@ -1,6 +1,14 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from django.views.generic.base import TemplateView
+from kolibri.auth.models import Facility
 
 
 class UserView(TemplateView):
     template_name = "user/user.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserView, self).get_context_data(**kwargs)
+
+        context['currentFacilityId'] = Facility.get_default_facility().id
+
+        return context
