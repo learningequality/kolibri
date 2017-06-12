c08364e58921fa15f5768fb6af692a7a1cc23904
chaowentan <chaowentan@gmail.com>
2017-06-12 16:56:11 -0700

    Fix z-index issue

--- kolibri/plugins/style_guide/assets/src/views/index.vue
+++ kolibri/plugins/style_guide/assets/src/views/index.vue
@@ -49,12 +49,13 @@
 
     height: $page-header-height
     line-height: $page-header-height
-
     padding-left: 2em
+
     position: fixed
     left: 0
     right: 0
     top: 0
+    z-index: 999
 
     h1
       margin: 0
