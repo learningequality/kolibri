ea40609ae41b830d16501cee37d1cc0de0f6a9a3
Jonathan Boiser <jboiser@gmail.com>
2017-06-13 15:13:54 -0700

    Use bytesForHumans module

--- kolibri/plugins/management/assets/src/views/manage-content-page/wizard-export.vue
+++ kolibri/plugins/management/assets/src/views/manage-content-page/wizard-export.vue
@@ -48,6 +48,7 @@
 <script>
 
   const actions = require('../../state/actions');
+  const bytesForHumans = require('./bytesForHumans');
 
   module.exports = {
     $trNameSpace: 'wizardExport',
@@ -82,7 +83,7 @@
     },
     methods: {
       formatEnabledMsg(drive) {
-        return `${this.$tr('available')} ${this.bytesForHumans(drive.freespace)}`;
+        return `${this.$tr('available')} ${bytesForHumans(drive.freespace)}`;
       },
       driveIsEnabled(drive) {
         return drive.writable;
@@ -96,37 +97,6 @@
         if (!this.wizardState.busy) {
           this.cancelImportExportWizard();
         }
-      },
-      bytesForHumans(bytes) {
-        // breaking down byte counts in terms of larger sizes
-        const kilobyte = 1024;
-        const megabyte = kilobyte ** 2;
-        const gigabyte = kilobyte ** 3;
-
-        function kilobyteCalc(byteCount) {
-          const kilos = Math.floor(byteCount / kilobyte);
-          return `${kilos} KB`;
-        }
-        function megabyteCalc(byteCount) {
-          const megs = Math.floor(byteCount / megabyte);
-          return `${megs} MB`;
-        }
-        function gigabyteCalc(byteCount) {
-          const gigs = Math.floor(byteCount / gigabyte);
-          return `${gigs} GB`;
-        }
-        function chooseSize(byteCount) {
-          if (byteCount > gigabyte) {
-            return gigabyteCalc(byteCount);
-          } else if (byteCount > megabyte) {
-            return megabyteCalc(byteCount);
-          } else if (byteCount > kilobyte) {
-            return kilobyteCalc(byteCount);
-          }
-          return `${bytes} B`;
-        }
-
-        return chooseSize(bytes);
       },
       selectDriveByID(driveID) {
         this.selectedDrive = driveID;
