7db746711123f4cf90cb26d96f0127aeaf6e5e41
Jamie Alexandre <jamalex@gmail.com>
2017-06-29 12:43:36 -0700

    Updated APK Buildkite scripts to use Kivy-based build system

--- .buildkite/build_apk.sh
+++ .buildkite/build_apk.sh
@@ -2,25 +2,18 @@
 
 set -euo pipefail
 
-# clone a fresh copy of the latest Kolibri Android repository
-yes y | rm -r kolibri-android-wrapper/ || true 2> /dev/null
-git clone https://github.com/learningequality/kolibri-android-wrapper.git
+# clone a fresh copy of the latest Kolibri Kivy repository
+yes y | rm -r kolibri-kivy/ || true 2> /dev/null
+git clone https://github.com/learningequality/kolibri-kivy.git
 
-# copy in the latest pex to make it available to the Docker build script
-buildkite-agent artifact download 'dist/*.pex' dist/
-cp dist/*.pex kolibri-android-wrapper/kolibri.pex
+# copy in the latest whl to make it available to the Docker build script
+buildkite-agent artifact download 'dist/*.whl' dist/
+cp dist/*.whl kolibri-kivy/
 
 # build the APK
-cd kolibri-android-wrapper
-docker build -t kolibriandroid .
+cd kolibri-kivy
+./build.sh
 cd ..
 
-# copy the APK out of the Docker image
-docker run -i --rm -v ${PWD}/dist:/mnt/dist kolibriandroid /bin/bash << COMMANDS
-cp /*.apk /mnt/dist
-echo Changing owner from \$(id -u):\$(id -g) to $(id -u):$(id -u)
-chown -R $(id -u):$(id -u) /mnt/dist
-COMMANDS
-
 # upload the APK to Buildkite
-buildkite-agent artifact upload 'dist/*.apk'
+buildkite-agent artifact upload 'kolibri-kivy/bin/*.apk'
