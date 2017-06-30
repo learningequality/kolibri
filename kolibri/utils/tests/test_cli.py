4e75ec7828d02211b25523642da97ce52c3ae8c9
Benjamin Bach <benjamin@overtag.dk>
2017-06-30 17:12:45 +0200

    Add further tests of different plugin disabling and pytest fixture for config

--- kolibri/utils/tests/test_cli.py
+++ kolibri/utils/tests/test_cli.py
@@ -3,28 +3,73 @@ Tests for `kolibri` module.
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
+import copy
 import logging
+import pytest
 
-from kolibri.utils import cli, conf
+from kolibri.utils import cli
 
 from .base import KolibriTestBase
 
 logger = logging.getLogger(__name__)
 
 
-def test_bogus_plugin_disable():
+@pytest.fixture
+def conf():
+    from kolibri.utils import conf
+    old_config = copy.deepcopy(conf.config)
+    yield conf
+    conf.update(old_config)
+    conf.save()
+
+
+def test_bogus_plugin_disable(conf):
     installed_apps_before = conf.config["INSTALLED_APPS"][:]
     cli.plugin("i_do_not_exist", disable=True)
     assert installed_apps_before == conf.config["INSTALLED_APPS"]
 
 
-def test_real_plugin_disable():
+def test_plugin_cannot_be_imported_disable(conf):
+    """
+    A plugin may be in conf.config['INSTALLED_APPS'] but broken or uninstalled
+    """
+    plugin_name = "giraffe.horse"
+    conf.config["INSTALLED_APPS"].append(plugin_name)
+    conf.save()
+    cli.plugin(plugin_name, disable=True)
+    assert plugin_name not in conf.config["INSTALLED_APPS"]
+
+
+def test_real_plugin_disable(conf):
     installed_apps_before = conf.config["INSTALLED_APPS"][:]
     test_plugin = "kolibri.plugins.audio_mp3_render"
     assert test_plugin in installed_apps_before
     # Because RIP example plugin
     cli.plugin(test_plugin, disable=True)
     assert test_plugin not in conf.config["INSTALLED_APPS"]
+
+
+def test_real_plugin_disable_twice(conf):
+    installed_apps_before = conf.config["INSTALLED_APPS"][:]
+    test_plugin = "kolibri.plugins.audio_mp3_render"
+    assert test_plugin in installed_apps_before
+    # Because RIP example plugin
+    cli.plugin(test_plugin, disable=True)
+    assert test_plugin not in conf.config["INSTALLED_APPS"]
+    installed_apps_before = conf.config["INSTALLED_APPS"][:]
+    cli.plugin(test_plugin, disable=True)
+    assert test_plugin not in conf.config["INSTALLED_APPS"]
+
+
+def test_plugin_with_no_plugin_class(conf):
+    """
+    Expected behavior is that nothing blows up with exceptions, user just gets
+    a warning and nothing is enabled or changed in the configuration.
+    """
+    # For fun, we pass in a system library
+    installed_apps_before = conf.config["INSTALLED_APPS"][:]
+    cli.plugin("os.path")
+    assert installed_apps_before == conf.config["INSTALLED_APPS"]
 
 
 class TestKolibriCLI(KolibriTestBase):
