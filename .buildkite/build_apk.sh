8c8767ca1b7e5886fa091e15c43715abe0893d8f
Jamie Alexandre <jamalex@gmail.com>
2017-06-29 18:33:05 -0700

    Don't delete other APKs, as they shouldn't exist (fresh environ)

--- .buildkite/build_apk.sh
+++ .buildkite/build_apk.sh
@@ -17,7 +17,6 @@ cd ..
 
 # extract the APK into the installer directory, and rename it to match the whl
 mkdir -p installer
-rm installer/*.apk
 cp kolibri-kivy/bin/*.apk installer/`basename -s .whl dist/*.whl`.apk
 
 # upload the APK to Buildkite
