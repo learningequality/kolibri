c23b0729eb2a609f1d7b017ed8d4addcabd57665
Aron Fyodor Asor <aronasorman@gmail.com>
2017-07-22 16:58:25 -0700

    Add WAL file mode.

--- kolibri/core/apps.py
+++ kolibri/core/apps.py
@@ -12,5 +12,16 @@ class KolibriCoreConfig(AppConfig):
         # Only run the PRAGMAs if we're using SQLite
         if connection.vendor == "sqlite":
             cursor = connection.cursor()
+
+            # Don't ensure that the OS has fully flushed
+            # our data to disk.
             cursor.execute("PRAGMA synchronous=OFF;")
+
+            # http://www.sqlite.org/wal.html
+            # WAL's main advantage allows simultaneous reads
+            # and writes (vs. the default exclusive write lock)
+            # at the cost of a slight penalty to all reads.
+            cursor.execute("PRAGMA journal_mode=WAL;")
+
+            # Store cross-database JOINs in memory.
             cursor.execute("PRAGMA temp_store=MEMORY;")
