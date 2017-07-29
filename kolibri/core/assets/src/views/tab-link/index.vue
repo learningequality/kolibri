d0329ad1d1d3d4cf51581b832fda08fd52c02fd1
indirectlylit <devon@indirectlylit.com>
2017-07-28 19:49:00 -0700

    manually copy over documentation changes
    from https://github.com/christianmemije/kolibri/commit/a8c0cc732ee6eac40dae585aa5b809b5c5871173

--- kolibri/core/assets/src/views/tab-link/index.vue
+++ kolibri/core/assets/src/views/tab-link/index.vue
@@ -21,7 +21,11 @@
   import ValidateLinkObject from 'kolibri.utils.validateLinkObject';
   import uiIcon from 'keen-ui/src/UiIcon';
   export default {
+    name: 'tab-link',
     props: {
+      /**
+        * The type of tab. title, icon, or icon-and-title.
+        */
       type: {
         type: String,
         validator(type) {
@@ -29,14 +33,23 @@
         },
         required: true,
       },
+      /**
+        * The text
+        */
       title: {
         type: String,
         required: false,
       },
+      /**
+        * A material icon name.
+        */
       icon: {
         type: String,
         required: false,
       },
+      /**
+        * A router link object
+        */
       link: {
         type: Object,
         required: true,
