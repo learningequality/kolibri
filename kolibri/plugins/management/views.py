6129b97dba14bdbfeb735a380c16174631adde9f
Jonathan Boiser <jboiser@gmail.com>
2017-08-11 16:13:03 -0700

    Set up device_management template

--- kolibri/plugins/management/views.py
+++ kolibri/plugins/management/views.py
@@ -6,3 +6,8 @@ from django.views.generic.base import TemplateView
 class ManagementView(TemplateView):
 
     template_name = "management/management.html"
+
+
+class DeviceManagementView(TemplateView):
+
+    template_name = "management/device_management.html"
