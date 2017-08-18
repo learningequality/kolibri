15661c32a0657418ca91df0408ebbeba42e2e824
Richard Tibbles <richard.tibbles@gmail.com>
2017-08-18 13:45:15 -0700

    Make AsyncCommands cancel aware. Add canceling to tasks import export tasks.

--- kolibri/tasks/api.py
+++ kolibri/tasks/api.py
@@ -1,6 +1,23 @@
 import logging as logger
+import os
 
+import requests
+from barbequeue.common.classes import State
+from barbequeue.exceptions import UserCancelledError
 from django.apps.registry import AppRegistryNotReady
+from django.core.management import call_command
+from django.db import connections
+from django.http import Http404
+from django.utils.translation import ugettext as _
+from kolibri.content.models import ChannelMetadataCache
+from kolibri.content.utils.channels import get_mounted_drives_with_channel_info
+from kolibri.content.utils.paths import get_content_database_file_path, get_content_database_file_url
+from rest_framework import serializers, viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
+from .client import get_client
+from .permissions import IsDeviceOwnerOnly
 
 try:
     from django.apps import apps
@@ -11,21 +28,6 @@ except AppRegistryNotReady:
 
     django.setup()
 
-import requests
-from django.core.management import call_command
-from django.http import Http404
-from django.db import connections
-from django.utils.translation import ugettext as _
-from kolibri.content.models import ChannelMetadataCache
-from kolibri.content.utils.channels import get_mounted_drives_with_channel_info
-from kolibri.content.utils.paths import get_content_database_file_url
-from rest_framework import serializers, viewsets
-from rest_framework.decorators import list_route
-from rest_framework.response import Response
-from barbequeue.common.classes import State
-
-from .permissions import IsDeviceOwnerOnly
-from .client import get_client
 
 logging = logger.getLogger(__name__)
 
@@ -38,6 +40,7 @@ class TasksViewSet(viewsets.ViewSet):
 
     def list(self, request):
         jobs_response = [_job_to_response(j) for j in get_client().all_jobs()]
+        get_client().clear(force=False)
         return Response(jobs_response)
 
     def create(self, request):
@@ -74,7 +77,7 @@ class TasksViewSet(viewsets.ViewSet):
             )
 
         task_id = get_client().schedule(
-            _networkimport, channel_id, track_progress=True)
+            _networkimport, channel_id, track_progress=True, cancellable=True)
 
         # attempt to get the created Task, otherwise return pending status
         resp = _job_to_response(get_client().status(task_id))
@@ -93,7 +96,7 @@ class TasksViewSet(viewsets.ViewSet):
                 "The 'drive_id' field is required.")
 
         job_id = get_client().schedule(
-            _localimport, request.data['drive_id'], track_progress=True)
+            _localimport, request.data['drive_id'], track_progress=True, cancellable=True)
 
         # attempt to get the created Task, otherwise return pending status
         resp = _job_to_response(get_client().status(job_id))
@@ -112,7 +115,7 @@ class TasksViewSet(viewsets.ViewSet):
                 "The 'drive_id' field is required.")
 
         job_id = get_client().schedule(
-            _localexport, request.data['drive_id'], track_progress=True)
+            _localexport, request.data['drive_id'], track_progress=True, cancellable=True)
 
         # attempt to get the created Task, otherwise return pending status
         resp = _job_to_response(get_client().status(job_id))
@@ -120,14 +123,23 @@ class TasksViewSet(viewsets.ViewSet):
         return Response(resp)
 
     @list_route(methods=['post'])
-    def cleartask(self, request):
+    def canceltask(self, request):
         '''
-        Clears a task with its task id given in the task_id parameter.
+        Cancel a task with its task id given in the task_id parameter.
         '''
 
         if 'task_id' not in request.data:
             raise serializers.ValidationError(
                 "The 'task_id' field is required.")
+
+        get_client().cancel(request.data['task_id'])
+        return Response({})
+
+    @list_route(methods=['post'])
+    def cleartasks(self, request):
+        '''
+        Cancels all running tasks.
+        '''
 
         get_client().clear(force=True)
         return Response({})
@@ -143,40 +155,65 @@ class TasksViewSet(viewsets.ViewSet):
         return Response(out)
 
 
-def _networkimport(channel_id, update_progress=None):
+def _networkimport(channel_id, update_progress=None, check_for_cancel=None):
     call_command(
         "importchannel",
         "network",
         channel_id,
-        update_progress=update_progress)
-    call_command(
-        "importcontent",
-        "network",
-        channel_id,
-        update_progress=update_progress)
-    connections.close_all()  # close all DB connections (FIX for #1818)
-
-def _localimport(drive_id, update_progress=None):
-    drives = get_mounted_drives_with_channel_info()
-    drive = drives[drive_id]
-    # copy channel's db file then copy all the content files from sorage dir
-    for channel in drive.metadata["channels"]:
-        call_command(
-            "importchannel",
-            "local",
-            channel["id"],
-            drive.datafolder,
-            update_progress=update_progress)
+        update_progress=update_progress,
+        check_for_cancel=check_for_cancel)
+    try:
         call_command(
             "importcontent",
-            "local",
-            channel["id"],
-            drive.datafolder,
-            update_progress=update_progress)
+            "network",
+            channel_id,
+            update_progress=update_progress,
+            check_for_cancel=check_for_cancel)
+    except UserCancelledError:
+        connections.close_all()  # close all DB connections (FIX for #1818)
+        try:
+            os.remove(get_content_database_file_path(channel_id))
+        except OSError:
+            pass
+        ChannelMetadataCache.objects.filter(id=channel_id).delete()
+        raise UserCancelledError
+    connections.close_all()  # close all DB connections (FIX for #1818)
+
+def _localimport(drive_id, update_progress=None, check_for_cancel=None):
+    drives = get_mounted_drives_with_channel_info()
+    drive = drives[drive_id]
+    # copy channel's db file then copy all the content files from storage dir
+    try:
+        for channel in drive.metadata["channels"]:
+            call_command(
+                "importchannel",
+                "local",
+                channel["id"],
+                drive.datafolder,
+                update_progress=update_progress,
+                check_for_cancel=check_for_cancel)
+            call_command(
+                "importcontent",
+                "local",
+                channel["id"],
+                drive.datafolder,
+                update_progress=update_progress,
+                check_for_cancel=check_for_cancel)
+    except UserCancelledError:
+        connections.close_all()  # close all DB connections (FIX for #1818)
+        for channel in drive.metadata["channels"]:
+            channel_id = channel["id"]
+            try:
+                os.remove(get_content_database_file_path(channel_id))
+            except OSError:
+                pass
+            ChannelMetadataCache.objects.filter(id=channel_id).delete()
+        connections.close_all()  # close all DB connections (FIX for #1818)s
+        raise UserCancelledError
     connections.close_all()  # close all DB connections (FIX for #1818)
 
 
-def _localexport(drive_id, update_progress=None):
+def _localexport(drive_id, update_progress=None, check_for_cancel=None):
     drives = get_mounted_drives_with_channel_info()
     drive = drives[drive_id]
     for channel in ChannelMetadataCache.objects.all():
@@ -184,12 +221,22 @@ def _localexport(drive_id, update_progress=None):
             "exportchannel",
             channel.id,
             drive.datafolder,
-            update_progress=update_progress)
-        call_command(
-            "exportcontent",
-            channel.id,
-            drive.datafolder,
-            update_progress=update_progress)
+            update_progress=update_progress,
+            check_for_cancel=check_for_cancel)
+        try:
+            call_command(
+                "exportcontent",
+                channel.id,
+                drive.datafolder,
+                update_progress=update_progress,
+                check_for_cancel=check_for_cancel)
+        except UserCancelledError:
+            try:
+                os.remove(get_content_database_file_path(channel.id, datafolder=drive.datafolder))
+            except OSError:
+                pass
+            connections.close_all()  # close all DB connections (FIX for #1818)
+            raise UserCancelledError
     connections.close_all()  # close all DB connections (FIX for #1818)
 
 
