2bcfa51da43ee9d6b9b35d1646619107942b4f75
Jonathan Boiser <jboiser@gmail.com>
2017-08-21 15:20:28 -0700

    Move showContentPage actions

--- kolibri/plugins/management/assets/src/device_management/state/actions/showPageActions.js
+++ kolibri/plugins/management/assets/src/device_management/state/actions/showPageActions.js
@@ -1,7 +1,41 @@
+import * as getters from 'kolibri.coreVue.vuex.getters';
+import * as coreActions from 'kolibri.coreVue.vuex.actions';
+import { TaskResource } from 'kolibri.resources';
+
+function _taskState(data) {
+  return {
+    id: data.id,
+    type: data.type,
+    status: data.status,
+    metadata: data.metadata,
+    percentage: data.percentage,
+  };
+}
+
 export function showContentPage(store) {
-  store.dispatch('SET_PAGE_STATE', {
-    contentJunk: true,
-  })
+  if (!getters.isSuperuser(store.state)) {
+    store.dispatch('CORE_SET_PAGE_LOADING', false);
+    return;
+  }
+
+  const taskCollectionPromise = TaskResource.getCollection().fetch();
+  taskCollectionPromise.only(
+    coreActions.samePageCheckGenerator(store),
+    taskList => {
+      const pageState = {
+        taskList: taskList.map(_taskState),
+        wizardState: { shown: false },
+        channelFileSummaries: {},
+      };
+      coreActions.setChannelInfo(store).then(() => {
+        store.dispatch('SET_PAGE_STATE', pageState);
+        store.dispatch('CORE_SET_PAGE_LOADING', false);
+      });
+    },
+    error => {
+      coreActions.handleApiError(store, error);
+    }
+  );
 }
 
 export function showPermissionsPage(store) {
--- kolibri/plugins/management/assets/src/device_management/views/index.vue
+++ kolibri/plugins/management/assets/src/device_management/views/index.vue
@@ -4,9 +4,7 @@
     <div>
       <div class="manage-content">
         <top-navigation />
-        <div class="page">
-          {{ pageName }}
-        </div>
+        <component class="page" :is="currentPage" />
       </div>
     </div>
   </core-base>
@@ -17,19 +15,30 @@
 <script>
 
   import { TopLevelPageNames } from 'kolibri.coreVue.vuex.constants';
+  import { PageNames } from '../constants';
   import coreBase from 'kolibri.coreVue.components.coreBase';
   import topNavigation from './device-top-nav';
   import store from '../state/store';
   import get from 'lodash/fp/get';
+  import manageContentPage from '../../views/manage-content-page';
+
+  const pageNameComponentMap = {
+    [PageNames.DEVICE_CONTENT_MGMT_PAGE]: 'manageContentPage',
+    [PageNames.DEVICE_PERMISSIONS_MGMT_PAGE]: 'managePermissionsPage',
+  };
 
   export default {
     name: 'deviceManagementRoot',
     components: {
       coreBase,
+      manageContentPage,
       topNavigation,
     },
     computed: {
       DEVICE: () => TopLevelPageNames.DEVICE,
+      currentPage() {
+        return pageNameComponentMap[this.pageName];
+      },
     },
     methods: {
 
