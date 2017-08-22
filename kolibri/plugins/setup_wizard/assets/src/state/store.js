08490a6a0baf923e8159997a0b117c23eb31ec30
David <davidxcanas@gmail.com>
2017-08-21 20:16:08 -0700

    Rethinking state and mutations, accounting for new permission presets

--- kolibri/plugins/setup_wizard/assets/src/state/store.js
+++ kolibri/plugins/setup_wizard/assets/src/state/store.js
@@ -5,23 +5,48 @@ import {
 } from 'kolibri.coreVue.vuex.store';
 
 const initialState = {
-  const initialState = {
-    onboardingData: {
-      language: '',
-      facilityName: '',
-      suName: '',
-      suUsername: '',
-      suPassword: '',
-      facilityPermissionOption: '',
-    },
-    submitted: false,
-    error: false,
-    onboardingStep: 1,
-  };
+  onboardingData: {
+    language: '',
+    facilityName: '',
+    suName: '',
+    suUsername: '',
+    suPassword: '',
+    facilityPreset: '',
+  },
+  submitted: false,
+  error: false,
+  onboardingStep: 1,
 };
+
 const mutations = {
-  SET_SUBMITTED_STATE(state, submittedFlag) {
-    state.pageState.submitted = submittedFlag;
+  SET_LANGUAGE(state, language) {
+    state.onboardingData.language = language;
+  },
+  SET_FACILITY_NAME(state, facilityName) {
+    state.onboardingData.facilityName = facilityName;
+  },
+  SET_SU_CREDENTIALS(state, credentials) {
+    state.onboardingData.suName = credentials.fullName;
+    state.onboardingData.suUsername = credentials.userName;
+    state.onboardingData.suPassword = credentials.password;
+  },
+  SET_FACILITY_PRESET(state, preset) {
+    state.onboardingData.facilityPreset = preset;
+  },
+  SET_SUBMITTED(state, submittedFlag) {
+    state.submitted = submittedFlag;
+  },
+  SET_ERROR(state, errorFlag) {
+    state.error = errorFlag;
+  },
+  INCREMENT_ONBOARDING_STEP(state) {
+    state.onboardingStep++;
+  },
+  DECREMENT_ONBOARDING_STEP(state) {
+    state.onboardingStep--;
+  },
+  SET_ONBOARDING_STEP(state, step) {
+    state.onboardingStep = step;
   },
 };
 
