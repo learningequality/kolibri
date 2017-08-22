d0606b3b53a906f838131293c41f9250f32d787c
David <davidxcanas@gmail.com>
2017-08-21 20:14:41 -0700

    Setting store at root vue creation within app.js

--- kolibri/plugins/setup_wizard/assets/src/app.js
+++ kolibri/plugins/setup_wizard/assets/src/app.js
@@ -1,14 +1,14 @@
 import KolibriModule from 'kolibri_module';
-
 import Vue from 'kolibri.lib.vue';
-
 import RootVue from './views';
+import store from './state/store'; // attaching store to the root element
 
 class SetupWizardModule extends KolibriModule {
   ready() {
     this.vm = new Vue({
       el: 'rootvue',
       render: createElement => createElement(RootVue),
+      store,
     });
   }
 }
