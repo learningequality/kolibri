bbf1f9b1b86e79b5a3edf70472e29fcfc6daee79
Jonathan Boiser <jboiser@gmail.com>
2017-08-23 11:54:52 -0700

    Fetch and transform device permissions

--- kolibri/plugins/management/assets/src/device_management/state/actions/permissionsActions.js
+++ kolibri/plugins/management/assets/src/device_management/state/actions/permissionsActions.js
@@ -1,15 +1,31 @@
-import { FacilityUserResource } from 'kolibri.resources';
+import { DevicePermissionsResource, FacilityUserResource } from 'kolibri.resources';
 import { handleApiError } from 'kolibri.coreVue.vuex.actions';
+import groupBy from 'lodash/groupBy';
+import mapValues from 'lodash/mapValues';
+import head from 'lodash/head';
 
 function fetchFacilityUsers() {
   return FacilityUserResource.getCollection().fetch();
 }
 
+function fetchDevicePermissions() {
+  return DevicePermissionsResource.getCollection().fetch({}, true)._promise
+    .then(function transform(permissions) {
+      // returns object, where userid is the key
+      return mapValues(groupBy(permissions, 'user'), head) ;
+    });
+}
+
 export function showPermissionsPage(store) {
-  return fetchFacilityUsers()
-    .then(function onSuccess(users) {
+  const promises = Promise.all([
+    fetchFacilityUsers(),
+    fetchDevicePermissions(),
+  ]);
+  return promises
+    .then(function onSuccess([users, permissions]) {
       store.dispatch('SET_PERMISSIONS_PAGE_STATE', {
         facilityUsers: users,
+        permissions,
       });
     })
     .catch(function onFailure(error) {
