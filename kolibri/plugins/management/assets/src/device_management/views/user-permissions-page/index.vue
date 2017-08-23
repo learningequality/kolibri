ddd77a5c5c1b4365ce7ee5d74d7277f538ddb28e
Jonathan Boiser <jboiser@gmail.com>
2017-08-23 10:10:22 -0700

    Stub user permissions page

--- kolibri/plugins/management/assets/src/device_management/views/user-permissions-page/index.vue
+++ kolibri/plugins/management/assets/src/device_management/views/user-permissions-page/index.vue
@@ -1,8 +1,39 @@
 <template>
 
-  <immersive-full-screen>
-    <div>
-      Yo
+  <immersive-full-screen
+    :backPageLink="{ name: 'DEVICE_PERMISSIONS_MGMT_PAGE' }"
+    backPageText="David Hu"
+  >
+    <div class="manage-content">
+      <div>
+        <h1>Full Name</h1>
+        <h2>Username</h2>
+      </div>
+
+      <div class="superuser-section">
+        <label>
+          <input type="checkbox" />
+          <h2>Make Superuser</h2>
+          <p>A superuser has all device permissions and is able to manage permissions of other users</p>
+        </label>
+      </div>
+
+      <div class="device-permissions-section">
+        <h2>Device Permissions</h2>
+        <label>
+          <input type="checkbox" />
+          <p>Can import and export content channels</p>
+        </label>
+      </div>
+
+      <div class="buttons">
+        <button>
+          Save Changes
+        </button>
+        <button>
+          Cancel
+        </button>
+      </div>
     </div>
 
   </immersive-full-screen>
@@ -38,5 +69,7 @@
 
 
 <style lang="stylus" scoped>
+
+  @require '../../../management-styles.styl'
 
 </style>
