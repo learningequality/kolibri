84856416f37fc725fe64d8ec8d72cb4cd780b330
David <davidxcanas@gmail.com>
2017-08-23 19:35:03 -0700

    Bootstrapping template for language selector, no styles. Removing unused style definition in progress-toolbar.

--- kolibri/plugins/setup_wizard/assets/src/views/progress-toolbar/index.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/progress-toolbar/index.vue
@@ -45,7 +45,4 @@
 
   @require '~kolibri.styles.definitions'
 
-  .progress-toolbar
-    // background-color: red
-
 </style>
--- kolibri/plugins/setup_wizard/assets/src/views/setup-forms/default-language-form/index.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/setup-forms/default-language-form/index.vue
@@ -1,8 +1,26 @@
 <template>
 
   <form>
-    Select default language
-    <button> hey </button>
+    <fieldset>
+      <legend>
+        <h1>
+          Select default language
+        </h1>
+      </legend>
+
+      <label>
+        <span> Selected: </span>
+        <span> {{ currentLanguage }} </span>
+      </label>
+
+      <k-button v-for="language in buttonLanguages" :raised="false" :text="language.name"/>
+      <select>
+        <option v-for="language in selectorLanguages" value="language.code">
+          {{ language.name }}
+        </option>
+      </select>
+
+    </fieldset>
   </form>
 
 </template>
@@ -10,8 +28,36 @@
 
 <script>
 
+  import { availableLanguages as allLanguages, currentLanguage } from 'kolibri.utils.i18n';
+  import kButton from 'kolibri.coreVue.components.kButton';
+  import omit from 'lodash/omit';
+
+  const numberOfLanguageButtons = 4;
+
+  const remainingLanguages = Object.values(omit(allLanguages, [currentLanguage]));
+  remainingLanguages.sort((lang1, lang2) => {
+    // puts words with foreign characters first in the array
+    return lang2.name.localeCompare(lang1.name);
+  });
+
+  const buttonLanguages = remainingLanguages.slice(0, numberOfLanguageButtons);
+  const selectorLanguages = remainingLanguages.slice(numberOfLanguageButtons);
+
   export default {
     name: 'defaultLanguageForm',
+    components: { kButton },
+    data() {
+      return {
+        selectedLanguage: currentLanguage,
+        buttonLanguages,
+        selectorLanguages,
+      };
+    },
+    computed: {
+      currentLanguage() {
+        return allLanguages[currentLanguage].name;
+      },
+    },
   };
 
 </script>
