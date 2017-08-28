b12992f9574431228f4ec6186b1d254486a15af9
David <davidxcanas@gmail.com>
2017-08-28 01:07:53 -0700

    First pass at Facility Name template

--- kolibri/plugins/setup_wizard/assets/src/views/setup-forms/facility-name-form/index.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/setup-forms/facility-name-form/index.vue
@@ -1,8 +1,18 @@
 <template>
 
   <form>
-    Set facility name
-    <button> hey </button>
+    <fieldset>
+      <legend>
+        <h1>
+          Set facility name
+        </h1>
+      </legend>
+
+      <k-textbox v-model="facilityName" label="Facility name"/>
+
+      <k-button type="submit" :text="submitText" />
+
+    </fieldset>
   </form>
 
 </template>
@@ -10,8 +20,26 @@
 
 <script>
 
+  import kTextbox from 'kolibri.coreVue.components.kTextbox';
+  import kButton from 'kolibri.coreVue.components.kButton';
+
   export default {
     name: 'facilityNameForm',
+    props: {
+      submitText: {
+        type: String,
+        required: true,
+      },
+    },
+    components: {
+      kTextbox,
+      kButton,
+    },
+    data() {
+      return {
+        facilityName: '',
+      };
+    },
   };
 
 </script>
