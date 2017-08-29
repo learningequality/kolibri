7d67282703f8cfae933278dd9cde55b8ed34829b
David <davidxcanas@gmail.com>
2017-08-29 00:13:13 -0700

    First pass user credential form

--- kolibri/plugins/setup_wizard/assets/src/views/setup-forms/superuser-credentials-form/index.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/setup-forms/superuser-credentials-form/index.vue
@@ -1,8 +1,21 @@
 <template>
 
   <form>
-    Set User credentials
-    <button> hey </button>
+    <fieldset>
+      <legend>
+        <h1>
+          Set user credentials
+        </h1>
+      </legend>
+
+      <k-textbox v-model="name" label="Full name"/>
+      <k-textbox v-model="username" label="Username"/>
+      <k-textbox v-model="password" label="Password"/>
+      <k-textbox v-model="passwordConfirm" label="Confirm Password"/>
+
+      <k-button type="submit" :text="submitText" />
+
+    </fieldset>
   </form>
 
 </template>
@@ -10,8 +23,29 @@
 
 <script>
 
+  import kTextbox from 'kolibri.coreVue.components.kTextbox';
+  import kButton from 'kolibri.coreVue.components.kButton';
+
   export default {
     name: 'superUserCredentialsForm',
+    props: {
+      submitText: {
+        type: String,
+        required: true,
+      },
+    },
+    components: {
+      kTextbox,
+      kButton,
+    },
+    data() {
+      return {
+        name: '',
+        username: '',
+        password: '',
+        passwordConfirm: '',
+      };
+    },
   };
 
 </script>
