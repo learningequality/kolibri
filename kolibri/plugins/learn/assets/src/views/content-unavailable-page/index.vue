1d99a898a77649bbf64e8ccab7c1ae786d6b5ba7
Jonathan Boiser <jboiser@gmail.com>
2017-08-29 15:50:13 -0700

    Fix no-content link

--- kolibri/plugins/learn/assets/src/views/content-unavailable-page/index.vue
+++ kolibri/plugins/learn/assets/src/views/content-unavailable-page/index.vue
@@ -18,7 +18,7 @@
     $trs: {
       header: 'No content channels available',
       adminLink:
-        'Download content channels from the <a href="/management/device/#/content">Content Management</a> page',
+        'Download content channels from the <a href="/management/device#/content">Content Management</a> page',
       notAdmin:
         'You need to sign in as the Device Owner to manage content. (This is the account originally created in the Setup Wizard.)',
     },
