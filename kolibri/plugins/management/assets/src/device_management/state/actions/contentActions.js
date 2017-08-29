65f4c8bc87e36d5f34531a4e622f613f6484486f
Jonathan Boiser <jboiser@gmail.com>
2017-08-28 17:39:12 -0700

    Delete unused actions

--- kolibri/plugins/management/assets/src/device_management/state/actions/contentActions.js
+++ kolibri/plugins/management/assets/src/device_management/state/actions/contentActions.js
@@ -1,4 +1,4 @@
-import { ChannelResource, FileSummaryResource } from 'kolibri.resources';
+import { ChannelResource } from 'kolibri.resources';
 import { canManageContent } from 'kolibri.coreVue.vuex.getters';
 import { handleApiError, setChannelInfo } from 'kolibri.coreVue.vuex.actions';
 import { fetchCurrentTasks } from './taskActions';
@@ -47,40 +47,4 @@ export function deleteChannel(store, channelId) {
   return ChannelResource.getModel(channelId)
     .delete()
     .then(() => refreshChannelList(store));
-}
-
-/**
- * Request and hydrate pageState with file summary info for single channel
- *
- * @param {Object} store - vuex store object
- * @param {string} channelId - channel UUID
- * @returns {Promise}
- */
-export function addChannelFileSummary(store, channelId) {
-  return (
-    FileSummaryResource.getCollection({ channel_id: channelId })
-      .fetch()
-      // FileSummary response is wrapped in an array as workaround on server side
-      .then(function onSuccess([data]) {
-        store.dispatch('ADD_CHANNEL_FILE_SUMMARY', data);
-      })
-      .catch(function onFailure(error) {
-        console.error(error); // eslint-disable-line
-      })
-  );
-}
-
-/**
- * Hydrate the manage content pageState with file summary info for all channels.
- * Requests for individual channels are non-blocking.
- *
- * @param {Object} store - vuex store object
- * @param {Array<String>} channelIds - an array of channelIds
- * @return {undefined}
- */
-export function addChannelFileSummaries(store, channelIds) {
-  channelIds.forEach(channelId => {
-    if (store.state.pageState.channelFileSummaries[channelId]) return;
-    addChannelFileSummary(store, channelId);
-  });
 }
--- kolibri/plugins/management/assets/src/device_management/state/mutations/contentMutations.js
+++ kolibri/plugins/management/assets/src/device_management/state/mutations/contentMutations.js
@@ -1,4 +1,3 @@
-import Vue from 'kolibri.lib.vue';
 import { PageNames } from '../../constants';
 
 function isManageContentPage(state) {
@@ -8,20 +7,5 @@ function isManageContentPage(state) {
 export function SET_CONTENT_PAGE_STATE(state, newPageState) {
   if (isManageContentPage(state)) {
     state.pageState = newPageState;
-  }
-}
-
-export function ADD_CHANNEL_FILE_SUMMARY(state, fileSummary) {
-  if (isManageContentPage(state)) {
-    state.pageState.channelFileSummaries[fileSummary.channel_id] = {
-      totalFileSizeInBytes: fileSummary.total_file_size,
-      numberOfFiles: fileSummary.total_files,
-    };
-  }
-}
-
-export function REMOVE_CHANNEL_FILE_SUMMARY(state, channelId) {
-  if (isManageContentPage(state)) {
-    Vue.delete(state.pageState.channelFileSummaries, channelId);
   }
 }
