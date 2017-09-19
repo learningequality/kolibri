90fe7661ea6f407e2e465af45d1f0632f6ec1783
David <davidxcanas@gmail.com>
2017-09-19 15:15:50 -0700

    Updating UA string tests according to Chrome dev docs

--- kolibri/core/assets/src/utils/browser.js
+++ kolibri/core/assets/src/utils/browser.js
@@ -10,28 +10,8 @@ export function isAndroidWebView() {
   const ua = window.navigator.userAgent;
   const isAndroid = /Android/.test(ua);
 
-  if (isAndroid) {
-    const androidVersion = parseFloat(ua.match(/Android\s([0-9\.]*)/)[1]);
-    const isChrome = /Chrome/.test(ua);
+  // First checks for 'wv' (Lolipop+), then for 'Version/x.x' (as specified in link above )
+  const isWebview = /wv/.test(ua) || /Version\/\d+\.\d+/.test(ua);
 
-    // WebView UA in Lollipop and Above
-    // Android >=5.0
-    if (androidVersion >= 5.0 && isChrome && /wv/.test(ua)) {
-      return true;
-    }
-
-    // WebView UA in KitKat to Lollipop
-    // Android >= 4.4
-    if (androidVersion >= 4.4 && androidVersion < 5.0 && isChrome && /Version\//.test(ua)) {
-      return true;
-    }
-
-    // Old WebView UA
-    // Android < 4.4
-    if (androidVersion < 4.4 && /Version\//.test(ua) && /\/534.30/.test(ua)) {
-      return true;
-    }
-  }
-
-  return false;
+  return isAndroid && isWebview;
 }
