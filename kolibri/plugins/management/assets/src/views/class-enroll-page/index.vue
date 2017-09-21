6ccdc830605299e7c52db451f7ec62993abdec19
Christian Memije <christianmemije@gmail.com>
2017-09-20 17:34:54 -0700

    Fix indentation

--- kolibri/plugins/management/assets/src/views/class-enroll-page/index.vue
+++ kolibri/plugins/management/assets/src/views/class-enroll-page/index.vue
@@ -91,7 +91,7 @@
                 :checked="isSelected(learner.id)"
                 @change="toggleSelection(learner.id)"
                 class="inline-block check"
-                 @click.native.stop
+                @click.native.stop
               />
             </td>
             <th class="col-username">{{ learner.username }}</th>
