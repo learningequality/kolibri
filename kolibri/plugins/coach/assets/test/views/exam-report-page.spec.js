c81707d4a2a940fc7839d203d07d9ff212a85703
Jonathan Boiser <jboiser@gmail.com>
2017-10-05 17:49:44 -0700

    DRY up exam report page spec

--- kolibri/plugins/coach/assets/test/views/exam-report-page.spec.js
+++ kolibri/plugins/coach/assets/test/views/exam-report-page.spec.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 import Vue from 'vue-test';
+import Vuex from 'vuex';
 import assert from 'assert';
 import ExamReportPage from '../../src/views/exam-report-page';
 import { shallow } from 'avoriaz';
@@ -24,60 +25,50 @@ function getTextInScoreColumn(tdEl) {
   return tdEl.find('td')[1].text();
 }
 
+const initialState = () => ({
+  classId: 'class_1',
+  pageState: {
+    channelId: 'channel_1',
+    examTakers: [],
+    exam: {
+      question_count: 6,
+    },
+  },
+});
+
 describe('exam report page', () => {
-  it('average score is not shown if no exams in progress', () => {
-    const wrapper = makeWrapper({
-      vuex: {
-        getters: {
-          examTakers: () => [
-            { progress: undefined, group: {}, score: undefined },
-            { progress: undefined, group: {}, score: undefined },
-          ],
-          classId: () => 'class_1',
-          exam: () => ({ question_count: 6 }),
-          channelId: () => 'channel_1',
-        },
-      },
-    });
+  it('average score is not shown if no exams are in progress', () => {
+    const state = initialState();
+    state.pageState.examTakers = [
+      { progress: undefined, group: {}, score: undefined },
+      { progress: undefined, group: {}, score: undefined },
+    ];
+    const wrapper = makeWrapper({ store: new Vuex.Store({ state }) });
     const { takenBy, averageScore } = getElements(wrapper);
     assert.equal(takenBy().text().trim(), 'Exam taken by: 0 learners');
     assert(averageScore() === undefined);
   });
 
   it('average score is shown if at least one exam in progress', () => {
-    const wrapper = makeWrapper({
-      vuex: {
-        getters: {
-          examTakers: () => [
-            { progress: 6, group: {}, score: 3 },
-            { progress: 6, group: {}, score: 3 },
-            { progress: undefined, group: {}, score: undefined },
-          ],
-          classId: () => 'class_1',
-          exam: () => ({ question_count: 6 }),
-          channelId: () => 'channel_1',
-        },
-      },
-    });
+    const state = initialState();
+    state.pageState.examTakers = [
+      { progress: 6, group: {}, score: 3 },
+      { progress: 6, group: {}, score: 3 },
+      { progress: undefined, group: {}, score: undefined },
+    ];
+    const wrapper = makeWrapper({ store: new Vuex.Store({ state }) });
     const { averageScore, takenBy } = getElements(wrapper);
     assert.equal(takenBy().text().trim(), 'Exam taken by: 2 learners');
     assert.equal(averageScore().text().trim(), 'Average Score: 50%');
   });
 
   it('shows correct scores for exam takers', () => {
-    const wrapper = makeWrapper({
-      vuex: {
-        getters: {
-          examTakers: () => [
-            { progress: 6, group: {}, score: 3 },
-            { progress: undefined, group: {}, score: undefined },
-          ],
-          classId: () => 'class_1',
-          exam: () => ({ question_count: 6 }),
-          channelId: () => 'channel_1',
-        },
-      },
-    });
+    const state = initialState();
+    state.pageState.examTakers = [
+      { progress: 6, group: {}, score: 3 },
+      { progress: undefined, group: {}, score: undefined },
+    ];
+    const wrapper = makeWrapper({ store: new Vuex.Store({ state }) });
     const { tableRows } = getElements(wrapper);
     // score is properly formatted
     assert.equal(getTextInScoreColumn(tableRows()[0]).trim(), '50%');
