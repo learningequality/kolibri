f15c8fbb04e22e978f7699c33cebc5b9b0b253ab
indirectlylit <devon@indirectlylit.com>
2017-10-07 23:35:53 -0700

    updating documentation for breadcrumbs

--- kolibri/plugins/style_guide/assets/src/views/shell/nav-menu.js
+++ kolibri/plugins/style_guide/assets/src/views/shell/nav-menu.js
@@ -36,7 +36,7 @@ const navMenu = [
         },
       },
       {
-        itemName: 'Breadcrumbs',
+        itemName: 'Channel breadcrumbs',
         itemRoute: {
           path: `/components/breadcrumbs`,
           component: breadcrumbsPage,
