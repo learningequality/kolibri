4db13248902feb9eb00be61bd158d32e22dcd2d8
Jonathan Boiser <jboiser@gmail.com>
2017-10-16 12:27:03 -0700

    Use translator to localize bytesForHumans

--- kolibri/plugins/management/assets/src/device_management/views/manage-content-page/bytesForHumans.js
+++ kolibri/plugins/management/assets/src/device_management/views/manage-content-page/bytesForHumans.js
@@ -1,4 +1,13 @@
-function bytesForHumans(bytes) {
+import { createTranslator } from 'kolibri.utils.i18n';
+
+const translator = createTranslator('bytesForHumansStrings', {
+  kbString: '{n, number, integer}KB',
+  mbString: '{n, number, integer}MB',
+  gbString: '{n, number, integer}GB',
+  bString: '{n, number, integer}B',
+});
+
+export default function bytesForHumans(bytes) {
   // breaking down byte counts in terms of larger sizes
   const kilobyte = 1024;
   const megabyte = kilobyte ** 2;
@@ -6,15 +15,15 @@ function bytesForHumans(bytes) {
 
   function kilobyteCalc(byteCount) {
     const kilos = Math.floor(byteCount / kilobyte);
-    return `${kilos} KB`;
+    return translator.$tr('kbString', { n: kilos });
   }
   function megabyteCalc(byteCount) {
     const megs = Math.floor(byteCount / megabyte);
-    return `${megs} MB`;
+    return translator.$tr('mbString', { n: megs });
   }
   function gigabyteCalc(byteCount) {
     const gigs = Math.floor(byteCount / gigabyte);
-    return `${gigs} GB`;
+    return translator.$tr('gbString', { n: gigs });
   }
   function chooseSize(byteCount) {
     if (byteCount > gigabyte) {
@@ -24,10 +33,8 @@ function bytesForHumans(bytes) {
     } else if (byteCount > kilobyte) {
       return kilobyteCalc(byteCount);
     }
-    return `${bytes} B`;
+    return translator.$tr('mbString', { n: bytes });
   }
 
   return chooseSize(bytes);
 }
-
-export { bytesForHumans as default };
