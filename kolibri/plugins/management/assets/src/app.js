9f34365b8b74fb27be32275754fa1a7814a67355
Richard Tibbles <richard@learningequality.org>
2017-10-26 18:20:12 -0700

    Update the facility management app to use the new kolibri_app as a proof of concept.

--- kolibri/plugins/management/assets/src/app.js
+++ kolibri/plugins/management/assets/src/app.js
@@ -1,70 +1,68 @@
-import KolibriModule from 'kolibri_module';
-import * as coreActions from 'kolibri.coreVue.vuex.actions';
-import router from 'kolibri.coreVue.router';
-import Vue from 'kolibri.lib.vue';
+import KolibriApp from 'kolibri_app';
 import RootVue from './views';
 import * as actions from './state/actions';
-import store from './state/store';
+import { initialState, mutations } from './state/store';
 import { PageNames } from './constants';
 
-class FacilityManagementModule extends KolibriModule {
-  ready() {
-    coreActions.getCurrentSession(store).then(() => {
-      const routes = [
-        {
-          name: PageNames.CLASS_MGMT_PAGE,
-          path: '/classes',
-          handler: () => {
-            actions.showClassesPage(store);
-          },
+class FacilityManagementModule extends KolibriApp {
+  get routes() {
+    return [
+      {
+        name: PageNames.CLASS_MGMT_PAGE,
+        path: '/classes',
+        handler: () => {
+          actions.showClassesPage(this.store);
         },
-        {
-          name: PageNames.CLASS_EDIT_MGMT_PAGE,
-          path: '/classes/:id',
-          handler: toRoute => {
-            actions.showClassEditPage(store, toRoute.params.id);
-          },
+      },
+      {
+        name: PageNames.CLASS_EDIT_MGMT_PAGE,
+        path: '/classes/:id',
+        handler: toRoute => {
+          actions.showClassEditPage(this.store, toRoute.params.id);
         },
-        {
-          name: PageNames.CLASS_ENROLL_MGMT_PAGE,
-          path: '/classes/:id/enroll',
-          handler: toRoute => {
-            actions.showClassEnrollPage(store, toRoute.params.id);
-          },
+      },
+      {
+        name: PageNames.CLASS_ENROLL_MGMT_PAGE,
+        path: '/classes/:id/enroll',
+        handler: toRoute => {
+          actions.showClassEnrollPage(this.store, toRoute.params.id);
         },
-        {
-          name: PageNames.USER_MGMT_PAGE,
-          path: '/users',
-          handler: () => {
-            actions.showUserPage(store);
-          },
+      },
+      {
+        name: PageNames.USER_MGMT_PAGE,
+        path: '/users',
+        handler: () => {
+          actions.showUserPage(this.store);
         },
-        {
-          name: PageNames.DATA_EXPORT_PAGE,
-          path: '/data',
-          handler: () => {
-            actions.showDataPage(store);
-          },
+      },
+      {
+        name: PageNames.DATA_EXPORT_PAGE,
+        path: '/data',
+        handler: () => {
+          actions.showDataPage(this.store);
         },
-        {
-          name: PageNames.FACILITY_CONFIG_PAGE,
-          path: '/configuration',
-          handler: () => {
-            actions.showFacilityConfigPage(store);
-          },
+      },
+      {
+        name: PageNames.FACILITY_CONFIG_PAGE,
+        path: '/configuration',
+        handler: () => {
+          actions.showFacilityConfigPage(this.store);
         },
-        {
-          path: '/',
-          redirect: '/classes',
-        },
-      ];
-
-      this.rootvue = new Vue({
-        el: 'rootvue',
-        render: createElement => createElement(RootVue),
-        router: router.init(routes),
-      });
-    });
+      },
+      {
+        path: '/',
+        redirect: '/classes',
+      },
+    ];
+  }
+  get RootVue() {
+    return RootVue;
+  }
+  get initialState() {
+    return initialState;
+  }
+  get mutations() {
+    return mutations;
   }
 }
 
