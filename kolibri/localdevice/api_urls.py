29ba540838b02e5717647aedf6eab1654b0f7a55
Lingyi Wang <lywang07@gmail.com>
2017-11-01 12:16:18 -0700

    move freespace endpoint to kolibri.core.device

--- kolibri/localdevice/api_urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from .api import LocalDeviceViewSet
-
-router = routers.SimpleRouter()
-router.register('localdevice', LocalDeviceViewSet, base_name='localdevice')
-
-urlpatterns = router.urls
