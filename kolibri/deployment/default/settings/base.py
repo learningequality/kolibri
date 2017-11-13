50b8ac671d193c2176a95a2724d08048fc772a57
Radina Matic <radina.matic@gmail.com>
2017-11-13 18:54:39 +0100

    Another IP

--- kolibri/deployment/default/settings/base.py
+++ kolibri/deployment/default/settings/base.py
@@ -138,7 +138,7 @@ if not os.path.exists(CONTENT_STORAGE_DIR):
     os.makedirs(CONTENT_STORAGE_DIR)
 
 # Base default URL for downloading content from an online server
-CENTRAL_CONTENT_DOWNLOAD_BASE_URL = "http://192.168.1.44:8080"
+CENTRAL_CONTENT_DOWNLOAD_BASE_URL = "http://192.168.1.45:8080"
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
