e9159f64fb669604ab0b77f6c72d9738a470b31e
Christian Memije <christianmemije@gmail.com>
2017-11-15 15:16:47 -0800

    Do not wait for loadedmetadata event

--- kolibri/plugins/media_player/assets/src/views/index.vue
+++ kolibri/plugins/media_player/assets/src/views/index.vue
@@ -212,8 +212,7 @@
         }
 
         this.$nextTick(() => {
-          this.player = videojs(this.$refs.player, videojsConfig);
-          this.player.on('loadedmetadata', this.handleReadyPlayer);
+          this.player = videojs(this.$refs.player, videojsConfig, this.handleReadyPlayer);
         });
       },
       handleReadyPlayer() {
