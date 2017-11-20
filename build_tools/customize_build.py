d1c06a15d57386a29f31f917ea377aa6ac10bfcf
Richard Tibbles <richard@learningequality.org>
2017-11-20 14:36:24 -0800

    Prevent empty env vars from breaking build. Add build logging for customization.

--- build_tools/customize_build.py
+++ build_tools/customize_build.py
@@ -31,6 +31,7 @@ def load_plugins_from_file(file_path):
     if file_path not in plugins_cache:
         # We have been passed a URL, not a local file path
         if file_path.startswith('http'):
+            print("Downloading plugins manifest from {file_path}".format(file_path=file_path))
             _, path = tempfile.mkstemp(suffix=".txt", text=True)
             with open(path, 'w') as f:
                 r = requests.get(file_path)
@@ -46,22 +47,26 @@ build_config_path = os.path.join(os.path.dirname(__file__), "../kolibri/utils/bu
 default_settings_template = "settings_path = '{path}'"
 
 def set_default_settings_module():
-    if "DEFAULT_SETTINGS_MODULE" in os.environ:
+    if "DEFAULT_SETTINGS_MODULE" in os.environ and os.environ["DEFAULT_SETTINGS_MODULE"]:
         default_settings_path = os.environ["DEFAULT_SETTINGS_MODULE"]
         with open(os.path.join(build_config_path, "default_settings.py"), 'w') as f:
             # Just write out settings_path = '<settings_path>'
+            print("Setting default settings module to {path}".format(path=default_settings_path))
             f.write(default_settings_template.format(path=default_settings_path))
 
 
 def set_build_time_plugins():
-    if "BUILD_TIME_PLUGINS" in os.environ:
+    if "BUILD_TIME_PLUGINS" in os.environ and os.environ["BUILD_TIME_PLUGINS"]:
         build_plugins = load_plugins_from_file(os.environ["BUILD_TIME_PLUGINS"])
         # For now, we still need to enable these plugins in order to build them
         # When the build system has been decoupled, this list of plugins can be used for that instead
         # Import CLI to set kolibri defaults
         from kolibri.utils import cli  # noqa
         from kolibri.utils import conf
-        # First disable all plugins, and then enable the plugins listed
+        print("Setting build time plugins to:")
+        for build_plugin in build_plugins:
+            print(build_plugin)
+        print("### End build plugins ###")
         conf.config['INSTALLED_APPS'] = build_plugins
         conf.save()
 
@@ -69,10 +74,14 @@ def set_build_time_plugins():
 run_time_plugin_template = "plugins = {plugins}\n"
 
 def set_run_time_plugins():
-    if "RUN_TIME_PLUGINS" in os.environ:
+    if "RUN_TIME_PLUGINS" in os.environ and os.environ["RUN_TIME_PLUGINS"]:
         runtime_plugins = load_plugins_from_file(os.environ["RUN_TIME_PLUGINS"])
         with open(os.path.join(build_config_path, "default_plugins.py"), 'w') as f:
             # Just write out 'plugins = [...]' <-- list of plugins
+            print("Setting run time plugins to:")
+            for runtime_plugin in runtime_plugins:
+                print(runtime_plugin)
+            print("### End run time plugins ###")
             f.write(run_time_plugin_template.format(plugins=runtime_plugins.__str__()))
 
 
--- build_tools/customize_requirements.py
+++ build_tools/customize_requirements.py
@@ -5,20 +5,24 @@ import requests
 
 
 def add_requirements_to_base():
-    if "EXTRA_REQUIREMENTS" in os.environ:
+    if "EXTRA_REQUIREMENTS" in os.environ and os.environ["EXTRA_REQUIREMENTS"]:
         file_path = os.environ["EXTRA_REQUIREMENTS"]
         # We have been passed a URL, not a local file path
         if file_path.startswith('http'):
+            print("Downloading extra requirements from {file_path}".format(file_path=file_path))
             _, path = tempfile.mkstemp(suffix=".txt", text=True)
             with open(path, 'w') as f:
                 r = requests.get(file_path)
                 f.write(r.content)
             file_path = path
-        with open(file_path, 'r') as f:
-            requirements = [requirement.strip() for requirement in f.readlines() if requirement.strip()]
-        if requirements:
-            with open(os.path.join(os.path.dirname(__file__), '../requirements/base.txt'), 'a') as f:
-                f.writelines(requirements)
+        try:
+            with open(file_path, 'r') as f:
+                requirements = [requirement.strip() for requirement in f.readlines() if requirement.strip()]
+            if requirements:
+                with open(os.path.join(os.path.dirname(__file__), '../requirements/base.txt'), 'a') as f:
+                    f.writelines(requirements)
+        except IOError:
+            pass
 
 
 if __name__ == "__main__":
