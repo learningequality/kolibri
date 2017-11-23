ba2ca45b94dfbd0f6bacf723d453796b745f0291
Aron Fyodor Asor <aronasorman@gmail.com>
2017-11-22 17:42:10 -0800

    add checks if the extra_metadata flag exists on jobs
    May happen if older iceqube jobs exist before the code upgrade

--- kolibri/tasks/api.py
+++ kolibri/tasks/api.py
@@ -411,8 +411,8 @@ def _job_to_response(job):
         }
     else:
         return {
-            "type": job.extra_metadata.get("type"),
-            "started_by": job.extra_metadata.get("started_by"),
+            "type": job.get("extra_metadata", {}).get("type"),
+            "started_by": job.get("extra_metadata", {}).get("started_by"),
             "status": job.state,
             "exception": str(job.exception),
             "traceback": str(job.traceback),
