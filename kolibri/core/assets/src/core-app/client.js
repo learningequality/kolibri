54d0369182c7773c24db1e775ddeaa757ff105ef
Richard Tibbles <richard@learningequality.org>
2017-11-29 18:37:02 -0800

    Add interceptors for core kolibri clients to handle disconnect states. Update connection snackbar component to handle countdown based on reconnectTime.

--- kolibri/core/assets/src/core-app/client.js
+++ kolibri/core/assets/src/core-app/client.js
@@ -2,11 +2,57 @@
  * Module for REST API client
  */
 
-import rest from 'rest';
+import interceptor from 'rest/interceptor';
 import mime from 'rest/interceptor/mime';
-import csrf from 'rest/interceptor/csrf';
-import errorCode from 'rest/interceptor/errorCode';
-import cookiejs from 'js-cookie';
+import baseClient from './baseClient';
+import errorCodes from '../disconnectionErrorCodes';
+
+import heartbeat from 'kolibri.heartbeat';
+import { connected } from 'kolibri.coreVue.vuex.getters';
+import store from 'kolibri.coreVue.vuex.store';
+
+const disconnectInterceptor = interceptor({
+  request: function(request) {
+    if (!connected(store.state)) {
+      if (request.cancel) {
+        request.cancel();
+      }
+      request.canceled = true;
+    }
+    return request;
+  },
+  response: function(response) {
+    if (response.request && response.request.canceled) {
+      return Promise.reject(response);
+    }
+    return response;
+  },
+});
+
+const loginTimeoutDetection = interceptor({
+  error: function(response) {
+    // If we receive a 403 response from the server, it is possible that the user
+    // is attempting to access information they are not allowed to see.
+    // However, more likely, it is because their login has timed out, but the frontend
+    // client code is still trying to access data that they would be allowed to see
+    // if they were logged in.
+    if (response.status && response.status.code === 403) {
+      // In this case, we should check right now if they are still logged in
+      heartbeat.beat();
+    }
+    return response;
+  },
+});
+
+const serverDisconnectDetection = interceptor({
+  response: function(response) {
+    if (response.status && errorCodes.includes(response.status.code)) {
+      heartbeat.monitorDisconnect();
+      return Promise.reject(response);
+    }
+    return response;
+  },
+});
 
 const client = options => {
   if (
@@ -21,22 +67,24 @@ const client = options => {
     const cacheBust = new Date().getTime();
     options.params[cacheBust] = cacheBust;
   }
-  return rest
+  return baseClient
+    .wrap(disconnectInterceptor)
     .wrap(mime, { mime: 'application/json' })
-    .wrap(csrf, {
-      name: 'X-CSRFToken',
-      token: cookiejs.get('csrftoken'),
-    })
-    .wrap(errorCode)(options);
+    .wrap(loginTimeoutDetection)
+    .wrap(serverDisconnectDetection)(options).then(response => {
+    if (response.request && response.request.canceled) {
+      return Promise.reject(response);
+    }
+    return response;
+  });
 };
 
 export default client;
 
-export const httpClient = rest
+export const httpClient = baseClient
+  .wrap(disconnectInterceptor)
   .wrap(mime, {
     mime: 'application/x-www-form-urlencoded',
   })
-  .wrap(csrf, {
-    name: 'X-CSRFToken',
-    token: cookiejs.get('csrftoken'),
-  });
+  .wrap(loginTimeoutDetection)
+  .wrap(serverDisconnectDetection);
