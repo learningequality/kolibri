423a965e38aaec61e71bf802ebe7b59f9667d179
mauhernandez <hernandezgmaureenp@gmail.com>
2017-11-30 18:13:42 -0500

    system crashing

--- kolibri/plugins/coach/assets/src/views/reports/report-subheading.vue
+++ kolibri/plugins/coach/assets/src/views/reports/report-subheading.vue
@@ -5,7 +5,6 @@
     <sub v-if="pageName == general && standardDataTable.length">{{ $tr('subHeading' , { threshold }) }}</sub>
     <sub v-if="pageName == learners && !standardDataTable.length">{{ $tr('noLearners') }}</sub>
     <sub v-if="pageName == groups">{{ $tr('noGroups') }}</sub>
-    <!-- <sub v-if="pageName == groups && !group.users.length">{{ $tr('noGroups') }}</sub> TOASK how to obtain the prop  -->
   </div>
 </template>
 
@@ -14,33 +13,28 @@
 
   import * as ReportConstants from '../../reportConstants';
   import * as reportGetters from '../../state/getters/reports';
-  // import {pageNames} from '../../constants';
+  import { PageNames } from '../../constants';
 
   export default {
     name: 'coachReportSubheading',
     $trs: {
-      noRecentProgress: 'No activity in past {threshold} days', //missing translation too ad manually to /kolibri/locale/es_ES/LC_FRONTEND_MESSAGES/coach_module-messages.json ?
-      noExams: 'You do not have any exams. Start by creating a new exam above',
+      noRecentProgress: 'No activity in past {threshold} days',
+      noExams: 'You do not have any exams. Start by creating a new exam below',
       noLearners: 'You do not have any learners registered yet',
-      noGroups: 'You do not have any groups created yet',
+      noGroups: 'You do not have any groups created yet. Start by creating a new one below',
       subHeading: 'Only showing activity in past {threshold} days',
     },
-    props: ['group'],
     data: () => ({
-      // recent: pageNames.RECENT_CHANNELS, TOASK how to include the constants /src/constants.js
-      recent: 'RECENT_CHANNELS',
-      exam: 'EXAMS',
-      learners: 'LEARNER_LIST',
-      groups: 'GROUPS',
-      general: 'TOPIC_CHANNELS',
+      recent: PageNames.RECENT_CHANNELS,
+      exam: PageNames.EXAMS,
+      learners: PageNames.LEARNER_LIST,
+      groups: PageNames.GROUPS,
+      general: PageNames.TOPIC_CHANNELS,
     }),
     computed: {
       threshold() {
         return ReportConstants.RECENCY_THRESHOLD_IN_DAYS;
       },
-      // recent(){ //TOASK as a methof for including the constants = not working
-      //   return pageNames.RECENT_CHANNELS;
-      // }
     },
     vuex: {
       getters: {
