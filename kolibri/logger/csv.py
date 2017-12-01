4a37ea16e8c25a60f33cee71839d4ea870b9895a
Richard Tibbles <richard@learningequality.org>
2017-11-30 22:36:55 -0800

    Use the actual content node's title, rather than the root node.

--- kolibri/logger/csv.py
+++ kolibri/logger/csv.py
@@ -36,11 +36,7 @@ class LogCSVSerializerBase(serializers.ModelSerializer):
 
     def get_content_title(self, obj):
         try:
-            channel = ChannelMetadata.objects.get(id=obj.channel_id)
-        except ChannelMetadata.DoesNotExist:
-            return ""
-        try:
-            node = ContentNode.objects.filter(tree_id=channel.root.tree_id).first()
+            node = ContentNode.objects.filter(content_id=obj.content_id).first()
             if node:
                 return node.title
             else:
