251a1e289f57635286d3d2e656e4550c9fc4b219
Richard Tibbles <richard@learningequality.org>
2017-12-06 12:29:09 -0800

    Import unicode_literals in all files that export strings for use in models. Update migrations.

--- kolibri/auth/constants/role_kinds.py
+++ kolibri/auth/constants/role_kinds.py
@@ -1,6 +1,8 @@
 """
 This module contains constants representing the kinds of "roles" that a user can have with respect to a Collection.
 """
+from __future__ import unicode_literals
+
 from django.utils.translation import ugettext_lazy as _
 
 ADMIN = "admin"
