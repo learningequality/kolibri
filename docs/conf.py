1af2e93ca68d4d98aa8f05ad39672abceec1c837
indirectlylit <devon@indirectlylit.com>
2017-12-14 17:52:09 -0800

    rearrange files and exclude partials

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ release = kolibri.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**/_*.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
