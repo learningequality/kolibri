ef737918332fe88e3593f191ea8ef12bd2748e11
Jonathan Boiser <jboiser@gmail.com>
2018-01-18 12:36:34 -0800

    Add django_filters to settings.INSTALLED_APPS

--- kolibri/deployment/default/settings/base.py
+++ kolibri/deployment/default/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'kolibri.auth.apps.KolibriAuthConfig',
     'kolibri.content',
     'kolibri.logger',
