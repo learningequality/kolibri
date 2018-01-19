401ba614d0a9a4be402daa064da4700bb0221cbf
Jonathan Boiser <jboiser@gmail.com>
2018-01-19 11:50:06 -0800

    Temporarily remove permissions

--- kolibri/core/lessons/viewsets.py
+++ kolibri/core/lessons/viewsets.py
@@ -1,14 +1,12 @@
 from .serializers import LessonSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from kolibri.auth.api import KolibriAuthPermissions, KolibriAuthPermissionsFilter
+from kolibri.auth.api import KolibriAuthPermissionsFilter
 from kolibri.core.lessons.models import Lesson
 from rest_framework.viewsets import ModelViewSet
 
+
 class LessonViewset(ModelViewSet):
     serializer_class = LessonSerializer
-    permissions_classes = (KolibriAuthPermissions,)
     filter_backends = (KolibriAuthPermissionsFilter, DjangoFilterBackend)
     filter_fields = ('collection',)
-
-    def get_queryset(self):
-        return Lesson.objects.all()
+    queryset = Lesson.objects.all()
