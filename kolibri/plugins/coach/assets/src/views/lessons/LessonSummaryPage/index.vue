2183ab076a9e82aaba068d276692a131cedc6203
David <davidxcanas@gmail.com>
2018-01-31 23:36:58 -0800

    First pass upper half of assessment page

--- kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/index.vue
+++ kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/index.vue
@@ -1,50 +1,71 @@
 <template>
 
   <div>
-    <dropdown-menu
-      :name="$tr('options')"
-      :options="lessonOptions"
-      @select="handleSelectOption"
-    />
-    <h1> {{ lessonTitle }} </h1>
-    <div> Options DD </div>
-    <p>
-      Status:
-    <!-- TODO include icon + string -->
-    </p>
-    <div v-if="lessonActive">
-      {{ $tr('active') }}
+    <div class="lesson-summary-header">
+      <h1 class="lesson-summary-header-title"> {{ lessonTitle }} </h1>
+      <div class="lesson-summary-header-options">
+        <dropdown-menu
+          :name="$tr('options')"
+          :options="lessonOptions"
+          @select="handleSelectOption"
+        />
+      </div>
     </div>
-    <div v-else>
-      {{ $tr('inactive') }}
-    </div>
-    <p> {{ lessonDescription }} </p>
-    <p> Visibility to: </p>
-
     <div>
-      <p>{{ $tr('resources') }}</p>
+      <dl>
+        <dt>
+          <!-- TODO wrapp strings -->
+          Status
+        </dt>
+        <dd>
+          {{ lessonActive ? $tr('active') : $tr('inactive') }}
+        </dd>
+        <dt>
+          <!-- TODO wrapp strings -->
+          Visibility
+        </dt>
+        <dd>
+          <ul class="group-list">
+            <li class="group-list-item" v-for="group in lessonGroups" :key="group.id">
+              <!-- TODO gonna have to call contentnode api for title -->
+              {{ group.id }}
+            </li>
+          </ul>
+        </dd>
+        <dt>
+          <!-- TODO wrapp strings -->
+          Description:
+        </dt>
+        <dd>
+          {{ lessonDescription }}
+        </dd>
+      </dl>
+
+      <!-- Maybe use the dt above?? -->
+      <h2>{{ $tr('resources') }}</h2>
       <table></table>
+
+      <delete-lesson-modal
+        v-if="currentAction==='deleteLesson'"
+        @cancel="currentAction=null"
+      />
+
+      <change-lesson-status-modal
+        v-if="currentAction==='changeLessonStatus'"
+        @cancel="currentAction=null"
+      />
+
+      <edit-lesson-details-modal
+        v-if="currentAction==='editLessonDetails'"
+        @cancel="currentAction=null"
+      />
+
+      <copy-lesson-modal
+        v-if="currentAction==='copyLesson'"
+        @cancel="currentAction=null"
+      />
     </div>
 
-    <delete-lesson-modal
-      v-if="currentAction==='deleteLesson'"
-      @cancel="currentAction=null"
-    />
-
-    <change-lesson-status-modal
-      v-if="currentAction==='changeLessonStatus'"
-      @cancel="currentAction=null"
-    />
-
-    <edit-lesson-details-modal
-      v-if="currentAction==='editLessonDetails'"
-      @cancel="currentAction=null"
-    />
-
-    <copy-lesson-modal
-      v-if="currentAction==='copyLesson'"
-      @cancel="currentAction=null"
-    />
   </div>
 
 </template>
@@ -124,4 +145,37 @@
 </script>
 
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+
+.lesson-summary-header
+  // maintaining a simple right/left alignment in a single text-line without floats. Simple RTL
+  display: table
+  width: 100%
+  &-title
+    display: table-cell
+    text-align: left
+  &-options
+    display: table-cell
+    text-align: right
+
+// TODO use classes
+dt
+  font-weight: bold
+  // TODO replace with verified values
+  margin-top: 1em
+  margin-bottom: 1em
+
+dd
+  margin: 0
+
+.group-list
+  margin: 0
+  padding: 0
+  &-item
+    margin: 0
+    list-style: none
+    &:not(:last-child)::after
+      // is this kosher?
+      content: ','
+
+</style>
