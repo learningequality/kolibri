80357c9f24461a91fafa48a0fd653de7272205e8
Akshay Mahajan <a1mahaja@ucsd.edu>
2018-02-04 16:32:33 -0800

    management command update

--- kolibri/core/webpack/management/commands/webpack_json.py
+++ kolibri/core/webpack/management/commands/webpack_json.py
@@ -13,16 +13,15 @@ class Command(BaseCommand):
     help = 'Creates a new schema'  # @ReservedAssignment
 
     def add_arguments(self, parser):
-        parser.add_argument('--outputfile', type=str, default=None, dest="output_file")
-
+        parser.add_argument('--output', type=str, default=None, dest="output_file")
+    
     def handle(self, *args, **options):
-
         logging.debug(args)
-
         result = [hook.webpack_bundle_data for hook in WebpackBundleHook().registered_hooks if hook.webpack_bundle_data]
 
         if options["output_file"]:
             logger.info("Writing webpack_json output to {}".format(options["output_file"]))
+            
             with open(options["output_file"], "w") as f:
                 json.dump(result, f)
         else:
