5f15cc284f1c98aa13c0d7177199f9a71aa12c6a
Jonathan Boiser <jboiser@gmail.com>
2018-02-07 13:18:23 -0800

    Data bootstrap for topic selection page

--- kolibri/plugins/coach/assets/src/state/actions/lessons.js
+++ kolibri/plugins/coach/assets/src/state/actions/lessons.js
@@ -1,7 +1,7 @@
 import { LessonsPageNames } from '../../lessonsConstants';
 import { getChannels } from 'kolibri.coreVue.vuex.getters';
 import { setClassState } from './main';
-import { LearnerGroupResource, LessonResource } from 'kolibri.resources';
+import { LearnerGroupResource, LessonResource, ContentNodeResource } from 'kolibri.resources';
 import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
 import { createTranslator } from 'kolibri.utils.i18n';
 
@@ -106,7 +106,53 @@ export function showLessonResourceSelectionRootPage(store, classId, lessonId) {
   );
 }
 
-export function showLessonSelectionTopicPage(store, classId, lessonId, topicId) {}
+function getThumbnailUrl(contentnode) {
+  const fileWithThumbnail = contentnode.files.find(file => file.thumbnail && file.available);
+  if (fileWithThumbnail) {
+    return fileWithThumbnail.storage_url;
+  }
+  return null;
+}
+
+export function showLessonSelectionTopicPage(store, classId, lessonId, topicId) {
+  store.dispatch('CORE_SET_PAGE_LOADING', true);
+  store.dispatch('SET_PAGE_STATE', {
+    currentLesson: lessonId,
+    contentList: [],
+  });
+  const loadRequirements = [
+    LearnerGroupResource.getCollection({ parent: classId }).fetch(),
+    ContentNodeResource.getModel(topicId).fetch(),
+    ContentNodeResource.getCollection({ parent: topicId }).fetch(),
+    ContentNodeResource.fetchAncestors(topicId),
+    updateLessons(store, classId),
+    setClassState(store, classId),
+  ];
+  return Promise.all(loadRequirements).then(
+    ([learnerGroups, topicNode, childNodes, ancestors]) => {
+      console.log(ancestors);
+      store.dispatch(
+        'SET_CONTENT_LIST',
+        childNodes.map(node => {
+          return {
+            id: node.pk,
+            description: node.description,
+            title: node.title,
+            thumbnail: getThumbnailUrl(node),
+            kind: node.kind,
+          };
+        })
+      );
+      store.dispatch('SET_LEARNER_GROUPS', learnerGroups);
+      store.dispatch('SET_PAGE_NAME', LessonsPageNames.SELECTION_ROOT);
+      store.dispatch('CORE_SET_TITLE', translator.$tr('selectResources'));
+      store.dispatch('CORE_SET_PAGE_LOADING', false);
+    },
+    () => {
+      store.dispatch('CORE_SET_PAGE_LOADING', false);
+    }
+  );
+}
 
 export function showLessonSelectionSearchPage(store, classId, lessonId, searchTerm) {}
 
