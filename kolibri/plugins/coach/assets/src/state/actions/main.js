7cfebc49fb78bb0cc9e1e2ad54549af0bb591fa7
Jonathan Boiser <jboiser@gmail.com>
2018-02-13 12:13:55 -0800

    Fix showExamPage test

--- kolibri/plugins/coach/assets/src/state/actions/main.js
+++ kolibri/plugins/coach/assets/src/state/actions/main.js
@@ -23,8 +23,7 @@ function _classState(classData) {
 }
 
 function setClassState(store, classId = null) {
-  const classCollection = ClassroomResource.getCollection();
-  return classCollection.fetch().then(classes => {
+  return ClassroomResource.getCollection().fetch().then(classes => {
     let className = null;
     if (classId) {
       className = classes.find(classroom => classroom.id === classId).name;
