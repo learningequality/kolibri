67cc36f9f0a6d946107e9b27bc6664796f88cea2
Jonathan Boiser <jboiser@gmail.com>
2018-02-14 10:58:11 -0800

    Provide option to hide content icon

--- kolibri/plugins/learn/assets/src/views/content-card/card-thumbnail.vue
+++ kolibri/plugins/learn/assets/src/views/content-card/card-thumbnail.vue
@@ -18,7 +18,10 @@
       :progress="progress"
     />
 
-    <div class="content-icon-wrapper">
+    <div
+      v-if="showContentIcon"
+      class="content-icon-wrapper"
+    >
       <svg
         height="64"
         width="64"
@@ -26,12 +29,21 @@
         class="content-icon-bg"
         :style="contentIconBgColor"
       >
-        <polygon stroke-width="0" :points="contentIconBgCoords" />
+        <polygon
+          stroke-width="0"
+          :points="contentIconBgCoords"
+        />
       </svg>
-      <content-icon :kind="kind" class="content-icon" />
+      <content-icon
+        :kind="kind"
+        class="content-icon"
+      />
     </div>
 
-    <div v-if="progress !== undefined" class="progress-bar-wrapper">
+    <div
+      v-if="progress!==undefined"
+      class="progress-bar-wrapper"
+    >
       <div
         class="progress-bar"
         :style="{ width: `${progress * 100}%` }"
@@ -68,6 +80,11 @@
         validator(value) {
           return values(ContentNodeKinds).includes(value);
         },
+      },
+      // If true, shows the content icon on the upper left of the thumbnail
+      showContentIcon: {
+        type: Boolean,
+        default: true,
       },
       progress: {
         type: Number,
