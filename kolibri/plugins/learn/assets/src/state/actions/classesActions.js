210ecf90a1ec2c87290ddd29ee8b79fdf11f8505
Jonathan Boiser <jboiser@gmail.com>
2018-02-14 10:54:27 -0800

    Add page titles

--- kolibri/plugins/learn/assets/src/state/actions/classesActions.js
+++ kolibri/plugins/learn/assets/src/state/actions/classesActions.js
@@ -1,9 +1,25 @@
 import { ClassesPageNames } from '../../constants';
 import { LearnerClassroomResource } from '../../apiResources';
+import { createTranslator } from 'kolibri.utils.i18n';
+
+const translator = createTranslator('classesPageTitles', {
+  allClasses: 'All classes',
+  classAssignments: 'Class assignments',
+});
+
+function preparePage(store, params) {
+  const { pageName, title } = params;
+  store.dispatch('SET_PAGE_NAME', pageName);
+  store.dispatch('CORE_SET_TITLE', title);
+  store.dispatch('CORE_SET_PAGE_LOADING', true);
+}
 
 // Shows a list of all the Classrooms a Learner is enrolled in
 export function showAllClassesPage(store) {
-  store.dispatch('SET_PAGE_NAME', ClassesPageNames.ALL_CLASSES);
+  preparePage(store, {
+    pageName: ClassesPageNames.ALL_CLASSES,
+    title: translator.$tr('allClasses'),
+  });
   return LearnerClassroomResource.getCollection({ no_assignments: true })
     .fetch()
     .then(classrooms => {
@@ -16,7 +32,10 @@ export function showAllClassesPage(store) {
 
 // For a given Classroom, shows a list of all Exams and Lessons assigned to the Learner
 export function showClassAssignmentsPage(store, classId) {
-  store.dispatch('SET_PAGE_NAME', ClassesPageNames.CLASS_ASSIGNMENTS);
+  preparePage(store, {
+    pageName: ClassesPageNames.CLASS_ASSIGNMENTS,
+    title: translator.$tr('classAssignments'),
+  });
   // Force fetch, so it doesn't re-use the assignments-less version in the cache
   return LearnerClassroomResource.getModel(classId)
     .fetch({}, true)
