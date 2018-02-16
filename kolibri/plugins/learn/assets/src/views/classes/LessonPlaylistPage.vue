9424b52f6f35d5d64fd829a9897501b701c36918
Jonathan Boiser <jboiser@gmail.com>
2018-02-15 17:20:03 -0800

    Fill in LessonPlaylistPage

--- kolibri/plugins/learn/assets/src/views/classes/LessonPlaylistPage.vue
+++ kolibri/plugins/learn/assets/src/views/classes/LessonPlaylistPage.vue
@@ -1,7 +1,32 @@
 <template>
 
   <div>
-    LessonPlaylistPage
+    <section class="lesson-details">
+      <div>
+        <h1 class="title">{{ currentLesson.name }}</h1>
+      </div>
+      <div v-if="currentLesson.description!==''">
+        <h3>{{ $tr('teacherNote') }}</h3>
+        <p> {{ currentLesson.description }}</p>
+      </div>
+    </section>
+
+    <section class="lesson-content-cards">
+      <content-card
+        v-for="c in contentNodes"
+        :key="c.pk"
+        class="content-card"
+        :isMobile="true"
+        :kind="c.kind"
+        :link="{}"
+        :progress="c.progress_fraction"
+        :thumbnail="getContentNodeThumbnail(c)"
+        :title="c.title"
+      />
+    </section>
+    <!-- <pre>
+      {{ JSON.stringify(contentNodes, null, 2) }}
+    </pre> -->
   </div>
 
 </template>
@@ -9,21 +34,50 @@
 
 <script>
 
+  import ContentCard from '../content-card';
+
+  // TODO Make this utility
+  function getContentNodeThumbnail(contentnode) {
+    const fileWithThumbnail = contentnode.files.find(file => file.thumbnail && file.available);
+    if (fileWithThumbnail) {
+      return fileWithThumbnail.storage_url;
+    }
+    return null;
+  }
+
   export default {
-    components: {},
-    props: {},
-    computed: {},
-    methods: {},
-    vuex: {
-      getters: {},
-      actions: {},
+    components: {
+      ContentCard,
     },
-    $trs: {},
+    methods: {
+      getContentNodeThumbnail,
+    },
+    vuex: {
+      getters: {
+        contentNodes: state => state.pageState.contentNodes,
+        currentLesson: state => state.pageState.currentLesson,
+      },
+    },
+    $trs: {
+      teacherNote: 'Teacher note:',
+    },
   };
 
 </script>
 
 
 <style lang="stylus" scoped>
+
+  .lesson-details
+    margin-bottom: 32px
+
+  .title
+    display: inline-block
+
+  .lesson-content-cards
+    max-width: 800px
+
+  .content-card
+    margin-bottom: 16px
 
 </style>
