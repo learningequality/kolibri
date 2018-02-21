f9e0c5ee00af31cd1aba941fba4a89822962739e
David <davidxcanas@gmail.com>
2018-02-21 15:14:06 -0800

    First pass resource preview layout.

--- kolibri/plugins/coach/assets/src/views/lessons/LessonContentPreviewPage/index.vue
+++ kolibri/plugins/coach/assets/src/views/lessons/LessonContentPreviewPage/index.vue
@@ -1,15 +1,20 @@
 <template>
 
-  <div>
-    Lesson Content Preview Page
-    <attempt-log-list
-      :attemptLogs="questions"
-      :selectedQuestionNumber="0"
+  <div class="content-preview-page">
+    <div class="description-area">
+      Lesson Content Preview Page
+    </div>
+    <!-- TODO consolidate this and attemptloglist? -->
+    <question-list
+      class="question-list left column"
+      @select="selectedQuestionIndex = $event"
+      :questions="questions"
+      :selectedIndex="selectedQuestionIndex"
     />
     <content-renderer
-      class="content-renderer"
+      class="content-area right column"
       :id="exercise.pk"
-      :itemId="questions[0]"
+      :itemId="selectedQuestion"
       :allowHints="false"
       :kind="exercise.kind"
       :files="exercise.files"
@@ -28,13 +33,22 @@
 <script>
 
   import contentRenderer from 'kolibri.coreVue.components.contentRenderer';
-  import attemptLogList from '../../attempt-log-list';
+  import questionList from '../../question-list';
   export default {
     components: {
       contentRenderer,
-      attemptLogList,
+      questionList,
     },
-    computed: {},
+    data() {
+      return {
+        selectedQuestionIndex: 0,
+      };
+    },
+    computed: {
+      selectedQuestion() {
+        return this.questions[this.selectedQuestionIndex];
+      },
+    },
     methods: {},
     vuex: {
       getters: {
@@ -49,4 +63,29 @@
 </script>
 
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+
+  $vertical-split = 30%
+  $horizontal-split = 30%
+
+  .content-preview-page
+    height: 100% // establish containing-blocks' height
+    position: relative // set the context for absolute elements within
+
+  .description-area
+    width: 100%
+    height: $horizontal-split
+
+  .column
+    position: absolute
+    bottom: 0
+    top: $horizontal-split
+    &.left
+      left: 0
+      right: 100% - $vertical-split
+    &.right
+      margin-left: 8px
+      right: 0
+      left: $vertical-split
+
+</style>
