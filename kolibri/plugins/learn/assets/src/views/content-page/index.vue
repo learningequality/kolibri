27b46f5a4974fc678e7106c4072cd3413ece2b82
Jonathan Boiser <jboiser@gmail.com>
2018-02-22 12:30:28 -0800

    Refactor content-page

--- kolibri/plugins/learn/assets/src/views/content-page/index.vue
+++ kolibri/plugins/learn/assets/src/views/content-page/index.vue
@@ -7,7 +7,7 @@
 
     <content-renderer
       v-if="!content.assessment"
-      v-show="!searchOpen"
+      class="content-renderer"
       @sessionInitialized="setWasIncomplete"
       @startTracking="startTracking"
       @stopTracking="stopTracking"
@@ -33,7 +33,7 @@
 
     <assessment-wrapper
       v-else
-      v-show="!searchOpen"
+      class="content-renderer"
       @sessionInitialized="setWasIncomplete"
       @startTracking="startTracking"
       @stopTracking="stopTracking"
@@ -63,13 +63,12 @@
     <p v-html="description" dir="auto" class="ta-l"></p>
 
 
-    <div class="metadata">
+    <section class="metadata" v-if="showMetadata">
       <!-- TODO: RTL - Do not interpolate strings -->
       <p v-if="content.author">
         {{ $tr('author', {author: content.author}) }}
       </p>
 
-      <!-- TODO: RTL - Do not interpolate strings -->
       <p v-if="content.license">
         {{ $tr('license', {license: content.license}) }}
 
@@ -81,29 +80,33 @@
             type="secondary"
             @click="licenceDescriptionIsVisible = !licenceDescriptionIsVisible"
           />
-          <!-- TODO: RTL - Do not interpolate strings -->
           <p v-if="licenceDescriptionIsVisible" dir="auto" class="ta-l">
             {{ content.license_description }}
           </p>
         </template>
-
       </p>
 
       <p v-if="content.license_owner">
         {{ $tr('copyrightHolder', {copyrightHolder: content.license_owner}) }}
       </p>
-    </div>
+    </section>
 
-    <download-button v-if="canDownload" :files="downloadableFiles" class="download-button" />
+    <download-button
+      v-if="canDownload"
+      :files="downloadableFiles"
+      class="download-button"
+    />
 
-    <template v-if="showRecommended">
-      <h2>{{ $tr('recommended') }}</h2>
-      <content-card-group-carousel
-        :genContentLink="genContentLink"
-        :header="recommendedText"
-        :contents="recommended"
-      />
-    </template>
+    <slot name="below_content">
+      <template v-if="showRecommended">
+        <h2>{{ $tr('recommended') }}</h2>
+        <content-card-group-carousel
+          :genContentLink="genContentLink"
+          :header="recommendedText"
+          :contents="recommended"
+        />
+      </template>
+    </slot>
 
     <template v-if="progress >= 1 && wasIncomplete">
       <points-popup
@@ -139,7 +142,7 @@
     startTrackingProgress as startTracking,
     stopTrackingProgress as stopTracking,
   } from 'kolibri.coreVue.vuex.actions';
-  import { PageNames, PageModes } from '../../constants';
+  import { PageNames, PageModes, ClassesPageNames } from '../../constants';
   import { pageMode } from '../../state/getters';
   import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
   import { isUserLoggedIn } from 'kolibri.coreVue.vuex.getters';
@@ -232,6 +235,10 @@
       downloadableFiles() {
         return this.content.files.filter(file => file.preset !== 'Thumbnail');
       },
+      showMetadata() {
+        // Hide metadata when viewing Resource in a Lesson
+        return !this.pageName === ClassesPageNames.LESSON_RESOURCE_VIEWER;
+      },
     },
     beforeDestroy() {
       this.stopTracking();
@@ -254,21 +261,20 @@
         this.wasIncomplete = false;
       },
       genContentLink(id, kind) {
-        if (kind === 'topic') {
-          return {
-            name: PageNames.TOPICS_TOPIC,
-            params: { channel_id: this.channelId, id },
-          };
+        let name;
+        if (kind === ContentNodeKinds.TOPIC) {
+          name = PageNames.TOPICS_TOPIC;
+        } else {
+          name = PageNames.RECOMMENDED_CONTENT;
         }
         return {
-          name: PageNames.RECOMMENDED_CONTENT,
+          name,
           params: { channel_id: this.channelId, id },
         };
       },
     },
     vuex: {
       getters: {
-        searchOpen: state => state.searchOpen,
         content: state => state.pageState.content,
         contentId: state => state.pageState.content.content_id,
         contentNodeId: state => state.pageState.content.id,
@@ -293,8 +299,6 @@
 
 
 <style lang="stylus" scoped>
-
-  @require '~kolibri.styles.definitions'
 
   .float
     float: right
