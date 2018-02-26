ceb77ba0236d54ce2b1976b85f58d6f0a46f5f01
Eugene Oliveros <eugene.oliveros@mrpau.com>
2018-02-26 09:39:12 +0800

    Clear UserExamResource cache after submission.

--- kolibri/plugins/learn/assets/src/state/actions/main.js
+++ kolibri/plugins/learn/assets/src/state/actions/main.js
@@ -463,7 +463,7 @@ function showExam(store, id, questionNumber) {
           router.getInstance().replace({ name: PageNames.EXAM_LIST });
           return;
         }
-        exam.closed = true;
+
         const currentChannel = getChannelObject(store.state, exam.channel_id);
         if (!currentChannel) {
           router.replace({ name: PageNames.CONTENT_UNAVAILABLE });
@@ -673,6 +673,7 @@ function closeExam(store) {
   examLog.closed = true;
   return ExamLogResource.getModel(examLog.id)
     .save(examLog)
+    .then(UserExamResource.clearCache())
     .catch(error => {
       handleApiError(store, error);
     });
