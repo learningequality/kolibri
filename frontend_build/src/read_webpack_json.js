28cd52f180ff757377e42463ee6dc05b2dcfec20
Akshay Mahajan <a1mahaja@ucsd.edu>
2018-03-07 11:32:48 -0800

    almost PR ready!!

--- frontend_build/src/read_webpack_json.js
+++ frontend_build/src/read_webpack_json.js
@@ -1,13 +1,16 @@
 var temp = require('temp').track();
 var fs = require('fs');
 var execSync = require('child_process').execSync;
+var path = require('path')
+var webpack_json = path.resolve(path.dirname(__filename), '../../build_tools/webpack_json.py')
+
 
 module.exports = function() {
   // the temporary path where the webpack_json json is stored
   var webpack_json_tempfile = temp.openSync({ suffix: '.json' }).path;
 
   // Run the script below to extract the relevant information about the plugin configuration from the Python code.
-  execSync('python cli.py --outputfile ' + webpack_json_tempfile);
+  execSync(`python ${webpack_json} --output_file  ${webpack_json_tempfile}`);
 
   var result = fs.readFileSync(webpack_json_tempfile);
 
