c58deda6f10a3bbd5e370a95ca743dcfe8450dc9
Lingyi Wang <lywang07@gmail.com>
2018-03-08 09:46:50 -0800

    add the script to check if nanobox needs to deploy the app

---
+++ .buildkite/check_deploy.sh
@@ -0,0 +1,25 @@
+set -eo pipefail
+
+if [ ! -z "$NANOBOX_APP_NAME" ] && [ "$BUILDKITE_PULL_REQUEST" = "false" ]; then
+
+    # Add the app name to the nanobox remote
+    REMOTEADD="$(nanobox remote add $NANOBOX_APP_NAME)"
+    REMOTEADDFAIL="Sorry"
+    REMOTEADDSUCCESS="linked"
+
+    if [ -z "${REMOTEADD##*$REMOTEADDSUCCESS*}" ]; then
+        echo "Added $NANOBOX_APP_NAME as a remote to Nanobox."
+        # Copy the generated pex file to deploy/kolibri.pex
+        buildkite-agent artifact download 'dist/*.pex' deploy/
+        mv deploy/dist/*.pex deploy/kolibri.pex
+
+        # Deploy the app
+        echo "Deploying $NANOBOX_APP_NAME to Nanobox..."
+        nanobox deploy "$NANOBOX_APP_NAME"
+    elif [ -z "${REMOTEADD##*$REMOTEADDFAIL*}" ]; then
+        echo "The remote $NANOBOX_APP_NAME does not exist on Nanobox."
+    else
+        echo "The outputs of the command `nanobox remote add` are changed."
+    fi
+
+fi
