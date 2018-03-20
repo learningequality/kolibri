cb5f7c2bd9ab9514d393e0460ef1d41bee7a1d07
Jonathan Boiser <jboiser@gmail.com>
2018-03-19 17:30:18 -0700

    Remove duplicated keys

--- kolibri/plugins/coach/assets/src/views/reports/channel-list-page.vue
+++ kolibri/plugins/coach/assets/src/views/reports/channel-list-page.vue
@@ -32,23 +32,17 @@
         </tr>
       </thead>
       <tbody slot="tbody">
-        <template v-for="channel in standardDataTable">
-          <tr :key="channel.id">
-            <td class="core-table-icon-col">
-              <content-icon :kind="CHANNEL" />
-            </td>
-            <name-cell
-              :kind="CHANNEL"
-              :title="channel.title"
-              :link="reportLink(channel.id)"
-              :key="channel.id"
-            />
-            <activity-cell
-              :date="channel.lastActive"
-              :key="channel.id"
-            />
-          </tr>
-        </template>
+        <tr v-for="channel in standardDataTable" :key="channel.id">
+          <td class="core-table-icon-col">
+            <content-icon :kind="CHANNEL" />
+          </td>
+          <name-cell
+            :kind="CHANNEL"
+            :title="channel.title"
+            :link="reportLink(channel.id)"
+          />
+          <activity-cell :date="channel.lastActive" />
+        </tr>
       </tbody>
     </core-table>
   </div>
