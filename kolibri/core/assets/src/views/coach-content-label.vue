a26ea63c17cb6625a411816a0bb75e2004db92d5
Jonathan Boiser <jboiser@gmail.com>
2018-03-27 16:00:12 -0700

    Add coach-content-label

---
+++ kolibri/core/assets/src/views/coach-content-label.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="coach-content-label">
+    <mat-svg
+      category="maps"
+      name="local_library"
+      class="coach-icon"
+    />
+    <span>{{ $tr('coachLabel') }}</span>
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'coachContentLabel',
+    $trs: {
+      coachLabel: 'Coach',
+    },
+  };
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+  @require '~kolibri.styles.theme'
+
+  .coach-icon
+    fill: $core-status-progress
+    vertical-align: text-bottom
+    margin-right: 4px
+
+</style>
