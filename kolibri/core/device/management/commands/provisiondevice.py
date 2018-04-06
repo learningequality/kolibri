294d0aa0a2e84a122abde297c0f395d404ee8eeb
Richard Tibbles <richard@learningequality.org>
2018-04-06 15:51:01 -0700

    Clean up provisiondevice management command. Add tests.

--- kolibri/core/device/management/commands/provisiondevice.py
+++ kolibri/core/device/management/commands/provisiondevice.py
@@ -8,10 +8,8 @@ from django.utils import six
 
 from kolibri.auth.constants.facility_presets import mappings
 from kolibri.auth.constants.facility_presets import presets
-from kolibri.auth.constants.role_kinds import ADMIN
 from kolibri.auth.models import Facility
 from kolibri.auth.models import FacilityUser
-from kolibri.core.device.models import DevicePermissions
 from kolibri.core.device.models import DeviceSettings
 
 logging = logger.getLogger(__name__)
@@ -26,34 +24,33 @@ def get_user_response(prompt, valid_answers=None):
 languages = dict(settings.LANGUAGES)
 
 
-def create_facility(options):
-    if options['facility'] is None and options['interactive']:
+def create_facility(facility_name=None, preset=None, interactive=False):
+    if facility_name is None and interactive:
         answer = get_user_response("Do you wish to create a facility? [yn] ", ["y", "n"])
         if answer == "y":
-            options['facility'] = get_user_response("What do you wish to name your facility? ")
+            facility = get_user_response("What do you wish to name your facility? ")
 
-    if options['facility']:
-        facility, created = Facility.objects.get_or_create(name=options['facility'])
+    if facility_name:
+        facility, created = Facility.objects.get_or_create(name=facility_name)
 
         if not created:
-            logging.warn("Facility with name {name} already exists, not modifying preset.".format(name=options['facility']))
+            logging.warn("Facility with name {name} already exists, not modifying preset.".format(name=facility_name))
             return facility
 
-        logging.info("Facility with name {name} created.".format(name=options['facility']))
+        logging.info("Facility with name {name} created.".format(name=facility_name))
 
-        if options['preset'] is None and options['interactive']:
-            options['preset'] = get_user_response(
+        if preset is None and interactive:
+            preset = get_user_response(
                 "Which preset do you wish to use? [{presets}]: ".format(presets=",".join(presets.keys())),
                 valid_answers=presets)
 
         # Only set preset data if we have created the facility, otherwise leave previous data intact
-        if options['preset']:
-            preset = options['preset']
+        if preset:
             dataset_data = mappings[preset]
             for key, value in dataset_data.items():
                 setattr(facility.dataset, key, value)
             facility.dataset.save()
-            logging.info("Facility preset changed to {preset}.".format(preset=options['preset']))
+            logging.info("Facility preset changed to {preset}.".format(preset=preset))
     else:
         facility = Facility.get_default_facility() or Facility.objects.first()
         if not facility:
@@ -61,37 +58,30 @@ def create_facility(options):
     return facility
 
 
-def create_superuser(options):
-    if options['superusername'] is None and options['interactive']:
-        options['superusername'] = get_user_response("Enter a username for the super user: ")
+def create_superuser(username=None, password=None, interactive=False):
+    if username is None and interactive:
+        username = get_user_response("Enter a username for the super user: ")
 
-    if options['superuserpassword'] is None and options['interactive']:
+    if password is None and interactive:
         confirm = ""
-        while options['superuserpassword'] != confirm:
-            options['superuserpassword'] = get_user_response("Enter a password for the super user: ")
+        while password != confirm:
+            password = get_user_response("Enter a password for the super user: ")
             confirm = get_user_response("Confirm password for the super user: ")
 
-    if options['superusername'] and options['superuserpassword']:
-        superuser, created = FacilityUser.objects.get_or_create(username=options['superusername'], facility=options['facility'])
-        if created:
-            logging.info("Superuser created with username {username}.".format(username=options['superusername']))
-            superuser.set_password(options["superuserpassword"])
-            superuser.save()
-        else:
-            logging.info("Setting user with username {username} as superuser.".format(username=options['superusername']))
-        options['facility'].add_role(superuser, ADMIN)
-        DevicePermissions.objects.create(user=superuser, is_superuser=True)
+    if username and password:
+        FacilityUser.objects.create_superuser(username, password)
+        logging.info("Superuser created with username {username}.".format(username=username))
 
 
-def create_device_settings(options):
-    if options['language_id'] is None and options['interactive']:
-        options['language_id'] = get_user_response(
+def create_device_settings(language_id=None, facility=None, interactive=False):
+    if language_id is None and interactive:
+        language_id = get_user_response(
             "Enter a default language code [{langs}]: ".format(langs=",".join(languages.keys())),
             valid_answers=languages)
     device_settings, created = DeviceSettings.objects.get_or_create()
     device_settings.is_provisioned = True
-    device_settings.language_id = options['language_id'] or device_settings.language_id
-    device_settings.default_facility = device_settings.default_facility or options['facility']
+    device_settings.language_id = language_id or device_settings.language_id
+    device_settings.default_facility = device_settings.default_facility or facility
     device_settings.save()
 
 
@@ -111,8 +101,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with transaction.atomic():
-            options['facility'] = create_facility(options)
+            facility = create_facility(
+                facility_name=options['facility'],
+                preset=options['preset'],
+                interactive=options['interactive'],
+            )
 
-            create_superuser(options)
+            create_device_settings(
+                language_id=options['language_id'],
+                facility=facility,
+                interactive=options['interactive'],
+            )
 
-            create_device_settings(options)
+            create_superuser(
+                username=options['superusername'],
+                password=options['superuserpassword'],
+                interactive=options['interactive'],
+            )
