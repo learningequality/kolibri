e7f7a6dc3c610a4efdee592b49491217fc237184
Richard Amodia <richard.amodia@mrpau.com>
2018-05-23 17:55:11 +0800

    Fix installer uploaded artifact filename.

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -5,4 +5,4 @@ rem After the installer successfully sign it will upload the signed installer at
 set current_path=%cd%
 
 buildkite-agent.exe artifact download "installer/*.exe" . --step "Build kolibri windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
-%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && buildkite-agent.exe artifact upload "%current_path%\installer\*.exe"
+%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && cd "%current_path%\installer\" && buildkite-agent.exe artifact upload "*.exe"
