acdacd1bf31788091aed85d907fc48a7929f7e31
Lingyi Wang <lywang07@gmail.com>
2018-05-23 21:38:56 -0700

    remove distutils in env.py and refactor code in install_cexts.py

--- build_tools/install_cexts.py
+++ build_tools/install_cexts.py
@@ -6,82 +6,90 @@ import shutil
 import subprocess
 import sys
 
+import requests
+
 DIST_CEXT = 'kolibri/dist/cext'
 PYPI_DOWNLOAD = 'https://pypi.python.org/simple/'
 PIWHEEL_DOWNLOAD = 'https://www.piwheels.hostedpi.com/simple/'
 
 
-"""
-Check if requests and bs4 modules are installed.
-"""
-def _install_requests_and_bs4_if_needed():
-    try:
-        imp.find_module('requests')
-        print('requests module exists')
-    except ImportError:
-        subprocess.call(['pip', 'install', 'requests'])
-
+def _install_bs4_if_needed():
+    """
+    Check if beautifulsoup4 module is installed.
+    """
     try:
         imp.find_module('bs4')
-        print('bs4 module exists')
     except ImportError:
-        subprocess.call(['pip', 'install', 'bs4'])
+        subprocess.call(['pip', 'install', 'beautifulsoup4'])
 
 
-def get_path_with_arch(platform, path):
+def get_path_with_arch(platform, path, abi, implementation, python_version):
     """
-    Distribute packages into folders according to the platform.
+    Calculate package path according to the platform.
     """
 
-    # Change the platform name into correct formats
-    platform = platform.replace('_', '-')
-    platform = platform.replace('x86-64', 'x86_64')
-    platform = platform.replace('manylinux1', 'linux')
+    # Split the platform into two parts.
+    # For example: manylinux1_x86_64 to Linux, x86_64
+    platform_split = platform.replace('manylinux1', 'Linux').split('_', 1)
 
-    path = os.path.join(path, platform)
+    # Windows 32-bit's machine name is x86.
+    if platform_split[0] == 'win32':
+        return os.path.join(path, 'Windows', 'x86')
 
-    return path
+    # Prior to CPython 3.3, there were two ABI-incompatible ways of building CPython
+    # There could be abi tag 'm' for narrow-unicode and abi tag 'mu' for wide-unicode
+    if implementation == 'cp' and int(python_version) < 33:
+        return os.path.join(path, platform_split[0], abi, platform_split[1])
+
+    return os.path.join(path, platform_split[0], platform_split[1])
 
 
-def download_package(path, platform, version, implementation, abi, name, pk_version):
+def download_package(path, platform, version, implementation, abi, name, pk_version, index_url):
     """
     Download the package according to platform, python version, implementation and abi.
     """
-    if 'arm' in platform:
-        index_url = PIWHEEL_DOWNLOAD
-    else:
-        index_url = PYPI_DOWNLOAD
-
     return_code = subprocess.call([
         'python', 'kolibripip.pex', 'download', '-q', '-d', path, '--platform', platform,
         '--python-version', version, '--implementation', implementation,
         '--abi', abi, '-i', index_url, '{}=={}'.format(name, pk_version)
     ])
+
+    # When downloaded as a tar.gz, convert to a wheel file first.
+    # This is specifically for pycparser package.
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith('tar.gz'):
+            subprocess.call([
+                'python', 'kolibripip.pex', 'wheel', '-q', '-w',
+                path, os.path.join(path, file), '--no-deps'])
+            os.remove(os.path.join(path, file))
+
     return return_code
 
 
-def install_package_by_wheel(path, name):
+def install_package_by_wheel(path):
     """
-    Install the package using the cached wheel files.
+    Install the package using the downloaded wheel files.
     """
-    return_code = subprocess.call([
-        'python', 'kolibripip.pex', 'install', '-q', '-t',
-        path, os.path.join(path, name)
-    ])
-    if return_code == 1:
-        sys.exit('\nInstallation failed for package {}.\n'.format(name))
-    else:
-        # Clean up all the whl files and dist-info folders in the directory
-        for item in os.listdir(path):
-            if item.endswith('.whl'):
-                os.remove(os.path.join(path, item))
-            elif item.endswith('dist-info'):
-                shutil.rmtree(os.path.join(path, item), ignore_errors=True)
+    files = os.listdir(path)
+    for file in files:
+        return_code = subprocess.call([
+            'python', 'kolibripip.pex', 'install', '-q', '-t',
+            path, os.path.join(path, file), '--no-deps'
+        ])
+        if return_code == 1:
+            sys.exit('\nInstallation failed for package {}.\n'.format(file))
+        else:
+            # Clean up the whl file and dist-info folder
+            os.remove(os.path.join(path, file))
+            shutil.rmtree(
+                os.path.join(
+                    path, '-'.join(file.split('-', 2)[:2])+'.dist-info'), ignore_errors=True)
 
 
-def parse_package_page(files, pk_version):
+def parse_package_page(files, pk_version, index_url):
     """
-    Parse the PYPI and Piwheel link for the package and install the desired wheel files.
+    Parse the PYPI and Piwheels link for the package and install the desired wheel files.
     """
 
     for file in files.find_all('a'):
@@ -91,7 +99,8 @@ def parse_package_page(files, pk_version):
         #   * not a whl file
         #   * not the version specified in requirements.txt
         #   * not python versions that kolibri supports
-        #   * for macosx or any 64-bit platforms, since the process of setup wizard has been fast enough
+        #   * not macosx or win_x64 platforms,
+        #     since the process of setup wizard has been fast enough
         if (
                 file_name[-1].split('.')[-1] != 'whl' or
                 file_name[1] != pk_version or
@@ -103,30 +112,21 @@ def parse_package_page(files, pk_version):
 
         print('Installing {}...'.format(file.string))
 
+        path = os.path.join(DIST_CEXT, file_name[2])
         implementation = file_name[2][:2]
         python_version = file_name[2][2:]
-
-        path = os.path.join(DIST_CEXT, file_name[2])
-
         abi = file_name[3]
         platform = file_name[4].split('.')[0]
 
-        #  Prior to CPython 3.3, there were two ABI-incompatible ways of building CPython
-        if 'linux' in platform and implementation == 'cp' and int(python_version) < 33:
-            # There could be abi tag 'm' for narrow-unicode and abi tag 'mu' for wide-unicode
-            path = os.path.join(os.path.join(path, 'linux'), abi)
-        path = get_path_with_arch(platform, path)
-        if path == '':
-            # Package is not supported in this platform
-            continue
+        path = get_path_with_arch(platform, path, abi, implementation, python_version)
 
         download_return = download_package(
             path, platform, python_version, implementation, abi, file_name[0],
-            pk_version)
+            pk_version, index_url)
 
-        # Successfully downloaded package
+        # Successfully download package
         if download_return == 0:
-            install_package_by_wheel(path, file.string)
+            install_package_by_wheel(path)
         # Download failed
         else:
             sys.exit('\nDownload failed for package {}.\n'.format(file.string))
@@ -134,11 +134,10 @@ def parse_package_page(files, pk_version):
 
 def install(name, pk_version):
     """
-    Start installing from the pypi page of the package.
+    Start installing from the pypi and piwheels pages of the package.
     """
-    _install_requests_and_bs4_if_needed()
+    _install_bs4_if_needed()
     try:
-        import requests
         from bs4 import BeautifulSoup
 
         # Parse everything in the package repo in both pypi and piwheel
@@ -147,7 +146,7 @@ def install(name, pk_version):
             r = requests.get(link + name)
             if r.status_code == 200:
                 files = BeautifulSoup(r.content, 'html.parser')
-                parse_package_page(files, pk_version)
+                parse_package_page(files, pk_version, link)
             else:
                 sys.exit('\nUnable to find package {} on {}.\n'.format(name, link))
 
