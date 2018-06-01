d2c33a66076b9136bbaa20be39b0e482fba9fcb9
Rafael Aguayo <ralphaguayo22@gmail.com>
2018-06-01 10:43:10 -0700

    Conditionally encode when reading stopword file

--- kolibri/content/utils/stopwords.py
+++ kolibri/content/utils/stopwords.py
@@ -1,10 +1,11 @@
 import json
 import os
 
+from kolibri.core.deviceadmin.utils import KWARGS_IO_READ
 
 # load stopwords file
 stopwords_path = os.path.join(os.path.dirname(__file__), os.path.pardir, 'constants', 'stopwords-all.json')
-with open(stopwords_path, 'r') as f:
+with open(stopwords_path, **KWARGS_IO_READ) as f:
     stopwords = json.load(f)
 
 # load into a set
