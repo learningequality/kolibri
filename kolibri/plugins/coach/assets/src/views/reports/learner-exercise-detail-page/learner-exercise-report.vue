49b219083736bca0202e0ba63b9cd9318fe38315
Christian Memije <christianmemije@gmail.com>
2018-06-13 12:33:02 -0700

    Fix attempt logs list

--- kolibri/plugins/coach/assets/src/views/reports/learner-exercise-detail-page/learner-exercise-report.vue
+++ kolibri/plugins/coach/assets/src/views/reports/learner-exercise-detail-page/learner-exercise-report.vue
@@ -101,7 +101,11 @@
       getters: {
         interactionIndex: state => state.pageState.interactionIndex,
         currentAttemptLog: state => state.pageState.currentAttemptLog,
-        attemptLogs: state => state.pageState.attemptLogs,
+        attemptLogs: state =>
+          state.pageState.attemptLogs.map(attempt => ({
+            ...attempt,
+            num_coach_contents: state.pageState.exercise.num_coach_contents,
+          })),
         currentInteraction: state => state.pageState.currentInteraction,
         currentInteractionHistory: state => state.pageState.currentInteractionHistory,
         classId: state => state.classId,
