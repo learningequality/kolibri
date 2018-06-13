8bb85a3b90cb9f66642b6f8b304e57d4bfe92a99
Richard Tibbles <richard@learningequality.org>
2018-06-13 16:34:21 -0700

    Don't try to download zip files that we don't make any more.

--- .buildkite/setup_and_upload_artifact.sh
+++ .buildkite/setup_and_upload_artifact.sh
@@ -35,7 +35,6 @@ mkdir -p dist
 mkdir -p installer
 buildkite-agent artifact download 'dist/*.pex' dist/
 buildkite-agent artifact download 'dist/*.whl' dist/
-buildkite-agent artifact download 'dist/*.zip' dist/
 buildkite-agent artifact download 'dist/*.tar.gz' dist/
 buildkite-agent artifact download 'installer/*.exe' installer/
 # buildkite-agent artifact download 'installer/*.apk' installer/
