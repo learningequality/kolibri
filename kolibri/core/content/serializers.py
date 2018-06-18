8039399f012325180dbb6b59c0e9b8f76cdf2bd6
bonidjukic <boni@djukic.com.hr>
2018-06-18 17:52:23 +0200

    Cache result of user.roles.exists() on serializer
    get_num_coach_contents method is called for each existing ContentNode
    object and consequently user.roles.exists() query is executed as well -
    this change simply caches the first query results on the serializer
    instance to avoid duplicating queries since user instance can't change
    within a single API call.

--- kolibri/core/content/serializers.py
+++ kolibri/core/content/serializers.py
@@ -366,8 +366,10 @@ class ContentNodeSerializer(serializers.ModelSerializer):
 
     def get_num_coach_contents(self, instance):
         user = self.context["request"].user
+        if not getattr(self, "user_roles_exist", None):
+            self.user_roles_exist = user.roles.exists()
         if user.is_facility_user:  # exclude anon users
-            if user.roles.exists() or user.is_superuser:  # must have coach role or higher
+            if self.user_roles_exist or user.is_superuser:  # must have coach role or higher
                 return get_num_coach_contents(instance)
         # all other conditions return 0
         return 0
