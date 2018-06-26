a03f73159f67bf5b77f24b1d80b1c83a598fb35e
Jonathan Boiser <jboiser@gmail.com>
2018-06-26 16:08:00 -0700

    Reorganize setup_wizard

--- kolibri/plugins/setup_wizard/assets/src/state/actions/forms.js
+++ kolibri/plugins/setup_wizard/assets/src/state/actions/forms.js
@@ -1,23 +1,18 @@
-function submitDefaultLanguage(store, language) {
+export function submitDefaultLanguage(store, language) {
   store.dispatch('SET_LANGUAGE', language);
 }
-function submitFacilityName(store, facilityName) {
+
+export function submitFacilityName(store, facilityName) {
   store.dispatch('SET_FACILITY_NAME', facilityName);
 }
-function submitSuperuserCredentials(store, name, username, password) {
+
+export function submitSuperuserCredentials(store, { name, username, password }) {
   store.dispatch('SET_SU', {
     name,
     username,
     password,
   });
 }
-function submitFacilityPermissions(store, facilityPreset) {
+export function submitFacilityPermissions(store, facilityPreset) {
   store.dispatch('SET_FACILITY_PRESET', facilityPreset);
 }
-
-export {
-  submitDefaultLanguage,
-  submitFacilityName,
-  submitSuperuserCredentials,
-  submitFacilityPermissions,
-};
