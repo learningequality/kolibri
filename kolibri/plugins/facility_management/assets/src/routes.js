ed8a848bf15cbe233a24c992d771f819562f3fa2
Jonathan Boiser <jboiser@gmail.com>
2018-06-26 15:24:13 -0700

    Move facility management routes to own module

---
+++ kolibri/plugins/facility_management/assets/src/routes.js
@@ -0,0 +1,67 @@
+import store from 'kolibri.coreVue.vuex.store';
+import { PageNames } from './constants';
+import {
+  showClassesPage,
+  showClassEditPage,
+  showLearnerClassEnrollmentPage,
+  showCoachClassAssignmentPage,
+} from './state/actions/class';
+import { showUserPage } from './state/actions/user';
+import { showDataPage } from './state/actions/data';
+import { showFacilityConfigPage } from './state/actions/facilityConfig';
+
+export default [
+  {
+    name: PageNames.CLASS_MGMT_PAGE,
+    path: '/classes',
+    handler: () => {
+      showClassesPage(store);
+    },
+  },
+  {
+    name: PageNames.CLASS_EDIT_MGMT_PAGE,
+    path: '/classes/:id',
+    handler: toRoute => {
+      showClassEditPage(store, toRoute.params.id);
+    },
+  },
+  {
+    name: PageNames.CLASS_ENROLL_LEARNER,
+    path: '/classes/:id/learner-enrollment/',
+    handler: toRoute => {
+      showLearnerClassEnrollmentPage(store, toRoute.params.id);
+    },
+  },
+  {
+    name: PageNames.CLASS_ASSIGN_COACH,
+    path: '/classes/:id/coach-assignment/',
+    handler: toRoute => {
+      showCoachClassAssignmentPage(store, toRoute.params.id);
+    },
+  },
+  {
+    name: PageNames.USER_MGMT_PAGE,
+    path: '/users',
+    handler: () => {
+      showUserPage(store);
+    },
+  },
+  {
+    name: PageNames.DATA_EXPORT_PAGE,
+    path: '/data',
+    handler: () => {
+      showDataPage(store);
+    },
+  },
+  {
+    name: PageNames.FACILITY_CONFIG_PAGE,
+    path: '/settings',
+    handler: () => {
+      showFacilityConfigPage(store);
+    },
+  },
+  {
+    path: '/',
+    redirect: '/classes',
+  },
+];
