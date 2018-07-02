83aa473ef952da6575b6c2024276dd35ea97193c
Jonathan Boiser <jboiser@gmail.com>
2018-07-02 14:41:29 -0700

    Comment coreStoreFactory

--- kolibri/core/assets/src/state/store.js
+++ kolibri/core/assets/src/state/store.js
@@ -5,12 +5,20 @@ import coreModule from './modules/core';
 
 Vue.use(Vuex);
 
+// Creates a new Vuex.Store instance with core components.
+// If a Vuex module (pluginModule) is provided, it will register its components as well.
+// This function is primarily used for to provide store fixtures in unit tests;
+// similar code is in kolibri_app.js for bootstrapping plugin single-page apps.
 export function coreStoreFactory(pluginModule) {
+  // Core state is organized under `state.core`, but actions, getters, and mutations
+  // are not namespaced.
   const store = new Vuex.Store({
     modules: {
       core: coreModule,
     },
   });
+  // Appends any pluginModule components to the store. `pluginModule.state`
+  // is added to `state`. Actions, getters, and mutations cannot be namespaced.
   if (pluginModule) {
     store.hotUpdate({
       actions: pluginModule.actions || {},
@@ -21,6 +29,8 @@ export function coreStoreFactory(pluginModule) {
       core: { ...coreModule.state },
       ...(pluginModule.state || {}),
     });
+    // Registers any modules defined in `pluginModule`. Their state objects will be organized
+    // under `state.submoduleState`. Actions, getters, and mutations can be namespaced.
     forEach(pluginModule.modules, (module, name) => {
       store.registerModule(name, module);
     });
