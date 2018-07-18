c88c32c2bb98e98d2f10f7a8d7c92e766a0d126e
José L. Redrejo Rodríguez <jredrejo@debian.org>
2018-07-18 21:23:14 +0200

    Created a thread to execute vacuum periodically (every 3 hours)

--- kolibri/core/sqlite/management/commands/vacuumsqlite.py
+++ kolibri/core/sqlite/management/commands/vacuumsqlite.py
@@ -1,4 +1,5 @@
 import logging as logger
+import time
 
 from django.core.management.base import BaseCommand
 from django.db import connections
@@ -15,22 +16,34 @@ class Command(BaseCommand):
             '--database', action='store', dest='database', default=DEFAULT_DB_ALIAS,
             help='Specifies the database to vacuum. Defaults to the "default" database.',
         )
+        parser.add_argument(
+            '--interval', action='store', dest='interval', default=0, type=int,
+            help='Specifies the interval (in minutes) to run the process continuosly. If 0, no repetition will happen',
+        )
 
     def handle(self, *args, **options):
         database = options['database']
         connection = connections[database]
+        interval = options['interval']
         if connection.vendor == "sqlite":
-            try:
-                cursor = connection.cursor()
-                cursor.execute('VACUUM;')
-                connection.close()
-            except Exception as e:
-                new_msg = (
-                    "Vacuum of database {db_name} couldn't be executed. Possible reasons:\n"
-                    "  * There is an open transaction in the db.\n"
-                    "  * There are one or more active SQL statements.\n"
-                    "  * The SQL was invalid.\n"
-                    "The full error: {error_msg}").format(db_name=connection.settings_dict['NAME'], error_msg=e)
-                logging.error(new_msg)
-            else:
-                logging.info("Sqlite database Vacuum finished.")
+            while True:
+                self.perform_vacuum(connection)
+                if not interval:
+                    break
+                logging.info("Next Vacuum in {interval} minutes.".format(interval=interval))
+                time.sleep(interval * 60)
+
+    def perform_vacuum(self, connection):
+        try:
+            cursor = connection.cursor()
+            cursor.execute('VACUUM;')
+            connection.close()
+        except Exception as e:
+            new_msg = (
+                "Vacuum of database {db_name} couldn't be executed. Possible reasons:\n"
+                "  * There is an open transaction in the db.\n"
+                "  * There are one or more active SQL statements.\n"
+                "The full error: {error_msg}").format(db_name=connection.settings_dict['NAME'], error_msg=e)
+            logging.error(new_msg)
+        else:
+            logging.info("Sqlite database Vacuum finished.")
