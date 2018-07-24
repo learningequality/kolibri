429832ff2296b6b1f790f2a8b1c62e3f2043220b
Rafael Aguayo <ralphaguayo22@gmail.com>
2018-07-24 15:44:56 -0700

    Same allow path list as setup wizard middleware

--- kolibri/plugins/user/middleware.py
+++ kolibri/plugins/user/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
@@ -5,9 +6,13 @@ from django.utils.deprecation import MiddlewareMixin
 from kolibri.core.auth.models import Facility
 from kolibri.core.auth.models import KolibriAnonymousUser
 
-
-SESSION_URL = reverse('session-list')
-USER_SIGNIN_URL = reverse("kolibri:user:user")
+ALLOWED_PATH_LIST = [
+    'deviceprovision',
+    'kolibri:setupwizardplugin:setupwizard',
+    'kolibri:set_language',
+    'session-list'
+    'admin:index',
+]
 
 
 class RedirectToSignInPageIfNoGuestAccessAndNoActiveSession(MiddlewareMixin):
@@ -17,8 +22,15 @@ class RedirectToSignInPageIfNoGuestAccessAndNoActiveSession(MiddlewareMixin):
     """
 
     def process_request(self, request):
+        # Don't redirect for URLs that are required for the setup wizard
+        allowed_paths = [reverse(name) for name in ALLOWED_PATH_LIST]
+        if settings.DEBUG:  # allow anonymous rest api browsing
+            allowed_paths.append('/api')
+        if any(request.path.startswith(path_prefix) for path_prefix in allowed_paths):
+            return
+
         if isinstance(request.user, KolibriAnonymousUser):
             dataset = getattr(Facility.get_default_facility(), 'dataset', None)
             if dataset and not dataset.allow_guest_access:
-                if not request.path.startswith(USER_SIGNIN_URL) and not request.path.startswith(SESSION_URL):
-                    return redirect(USER_SIGNIN_URL)
+                if not request.path.startswith(reverse("kolibri:user:user")):
+                    return redirect(reverse("kolibri:user:user"))
