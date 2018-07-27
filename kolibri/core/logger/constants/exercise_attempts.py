78baf987b14db41d73b59db48f7a059560dd03d4
Rafael Aguayo <ralphaguayo22@gmail.com>
2018-07-27 15:49:39 -0400

    return first x attemptlogs depending on exercise type

---
+++ kolibri/core/logger/constants/exercise_attempts.py
@@ -0,0 +1,8 @@
+from le_utils.constants import exercises
+
+MAPPING = {
+    exercises.NUM_CORRECT_IN_A_ROW_2: 2,
+    exercises.NUM_CORRECT_IN_A_ROW_3: 3,
+    exercises.NUM_CORRECT_IN_A_ROW_5: 5,
+    exercises.NUM_CORRECT_IN_A_ROW_10: 10,
+}
