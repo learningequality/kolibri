ca5341d2b70c7d685aef2ca294d820f8b8f1f343
Jonathan Boiser <jboiser@gmail.com>
2018-08-03 11:55:06 -0700

    Remove modal explaining different presets

--- kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/FacilityPermissionsForm.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/FacilityPermissionsForm.vue
@@ -1,64 +1,12 @@
 <template>
 
   <div>
-
-    <!-- Cannot match spec with current core modal -->
-    <!-- Should be its own component -->
-    <KModal
-      v-if="permissionPresetDetailsModalShown"
-      :title=" $tr('facilityPermissionsPresetDetailsHeader')"
-      :submitText="$tr('permissionsModalDismissText')"
-      @cancel="hideFacilityPermissionsDetails"
-      @submit="hideFacilityPermissionsDetails"
-    >
-
-      <dl class="permission-preset-human">
-        <dt class="permission-preset-human-title">
-          {{ $tr('selfManagedSetupTitle') }}
-        </dt>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('enabledUserSelfSignupPermissionDetail') }}
-        </dd>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('enabledAccountEditPermissionDetail') }}
-        </dd>
-      </dl>
-
-      <dl class="permission-preset-human">
-        <dt class="permission-preset-human-title">
-          {{ $tr('adminManagedSetupTitle') }}
-        </dt>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('disabledUserSelfSignupPermissionDetail') }}
-        </dd>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('enabledUserPasswordlessLoginPermissionDetail') }}
-        </dd>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('disabledAccountEditPermissionDetail') }}
-        </dd>
-      </dl>
-
-      <dl class="permission-preset-human">
-        <dt class="permission-preset-human-title">
-          {{ $tr('informalSetupTitle') }}
-        </dt>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('disabledUserSelfSignupPermissionDetail') }}
-        </dd>
-        <dd class="permission-preset-human-detail">
-          {{ $tr('enabledAccountEditPermissionDetail') }}
-        </dd>
-      </dl>
-    </KModal>
-
     <OnboardingForm
       :header="$tr('facilityPermissionsSetupFormHeader')"
       :description="$tr('facilityPermissionsSetupFormDescription')"
       :submitText="submitText"
       @submit="setPermissions"
     >
-
       <KRadioButton
         ref="first-button"
         class="permission-preset-radio-button"
@@ -90,10 +38,7 @@
         :text="$tr('facilityPermissionsPresetDetailsLink')"
         @click="showFacilityPermissionsDetails"
       />
-
-
     </OnboardingForm>
-
   </div>
 
 </template>
@@ -104,7 +49,6 @@
   import { mapMutations } from 'vuex';
   import KRadioButton from 'kolibri.coreVue.components.KRadioButton';
   import KButton from 'kolibri.coreVue.components.KButton';
-  import KModal from 'kolibri.coreVue.components.KModal';
   import OnboardingForm from './OnboardingForm';
 
   export default {
@@ -113,7 +57,6 @@
       OnboardingForm,
       KRadioButton,
       KButton,
-      KModal,
     },
     $trs: {
       facilityPermissionsSetupFormHeader: 'Choose a Facility setup',
@@ -129,15 +72,6 @@
       informalSetupTitle: 'Personal',
       informalSetupDescription:
         'Homeschooling, supplementary individual learning, and other informal use',
-      // IDEA This should be a dynamically generated component, based on mappings
-      permissionsModalDismissText: 'Close',
-      enabledUserAccountDeletionPermissionDetail: 'Guests can create their own accounts',
-      disabledUserAccountDeletionPermissionDetail: 'Admins must create all user accounts',
-      enabledAccountEditPermissionDetail: 'Users can edit their account information', //  QUESTION might be worth using select?
-      disabledAccountEditPermissionDetail: 'Users cannot edit their account information',
-      enabledUserPasswordlessLoginPermissionDetail: 'Users can sign in without their passwords',
-      enabledUserSelfSignupPermissionDetail: 'Guests can create their own accounts',
-      disabledUserSelfSignupPermissionDetail: 'Admins must create all user accounts',
     },
     props: {
       submitText: {
