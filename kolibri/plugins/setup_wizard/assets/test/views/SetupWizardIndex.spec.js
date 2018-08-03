6b22f3d0122fdbb20a21c422fd68da69cfe62661
Jonathan Boiser <jboiser@gmail.com>
2018-08-03 11:36:03 -0700

    Add test for current setup wizard workflow

---
+++ kolibri/plugins/setup_wizard/assets/test/views/SetupWizardIndex.spec.js
@@ -0,0 +1,49 @@
+import { mount } from '@vue/test-utils';
+import makeStore from '../makeStore';
+import SetupWizardIndex from '../../src/views/SetupWizardIndex';
+
+function makeWrapper() {
+  const store = makeStore();
+  const wrapper = mount(SetupWizardIndex, {
+    store,
+  })
+  jest.spyOn(wrapper.vm, 'provisionDevice').mockResolvedValue();
+  const els = {
+    DefaultLanguageForm: () => wrapper.find({ name: 'DefaultLanguageForm' }),
+    FacilityNameForm: () => wrapper.find({ name: 'FacilityNameForm' }),
+    FacilityPermissionsForm: () => wrapper.find({ name: 'FacilityPermissionsForm' }),
+    SuperuserCredentialsForm: () => wrapper.find({ name: 'SuperuserCredentialsForm' }),
+  };
+  const actions = {
+    simulateSubmit() {
+      wrapper.find('.body').element.$emit('submit')
+    }
+  }
+  return { wrapper, store, els, actions };
+}
+
+describe('SetupWizardIndex', () => {
+  it('clicking next takes you to the next step', () => {
+    const { els, wrapper } = makeWrapper();
+    // Step 1: DefaultLanguageForm
+    expect(els.DefaultLanguageForm().isVueComponent).toBe(true);
+    // Simulate clicking submit because clicking the button doesn't work
+    els.DefaultLanguageForm().vm.$emit('submit');
+    expect(els.DefaultLanguageForm().exists()).toBe(false);
+
+    // Step 2: Facility Name Form
+    expect(els.FacilityNameForm().isVueComponent).toBe(true);
+    els.FacilityNameForm().vm.$emit('submit');
+    expect(els.FacilityNameForm().exists()).toBe(false);
+
+    // Step 3: Facility Permissions Form
+    expect(els.FacilityPermissionsForm().isVueComponent).toBe(true);
+    els.FacilityPermissionsForm().vm.$emit('submit');
+    expect(els.FacilityPermissionsForm().exists()).toBe(false);
+
+    // Step 4: Superuser Credentials Form
+    expect(els.SuperuserCredentialsForm().isVueComponent).toBe(true);
+    els.SuperuserCredentialsForm().vm.$emit('submit');
+    expect(wrapper.vm.provisionDevice).toHaveBeenCalledTimes(1);
+  });
+});
