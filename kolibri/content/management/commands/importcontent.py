4ed07f553ca4bdc65e3b785be61cb93f659e2b1f
Lingyi Wang <lywang07@gmail.com>
2018-08-21 13:23:45 -0700

    only get the current progress when progresstracker exists

--- kolibri/content/management/commands/importcontent.py
+++ kolibri/content/management/commands/importcontent.py
@@ -183,9 +183,10 @@ class Command(AsyncCommand):
             * False, 0 - the transfer fails and needs to retry.
         """
         try:
-            # Save the current progress value
-            original_value = self.progresstrackers[0].progress
-            original_progress = self.progresstrackers[0].get_progress()
+            if self.progresstrackers:
+                # Save the current progress value
+                original_value = self.progresstrackers[0].progress
+                original_progress = self.progresstrackers[0].get_progress()
 
             with filetransfer, self.start_progress(total=filetransfer.total_size) as file_dl_progress_update:
                 # If size of the source file is smaller than the the size
