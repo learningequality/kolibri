203536fd93f1d8c992c47e8812e7010be628dc77
Jamie Alexandre <jamalex@gmail.com>
2018-08-23 13:58:20 -0700

    Restructure core public API endpoints into their own module

---
+++ kolibri/core/urls_public.py
@@ -0,0 +1,26 @@
+"""
+Kolibri public url patterns
+===========================
+
+This module defines the "public API" endpoints that we expect to be called externally by other
+instances of Kolibri, or by 3rd party applications or clients. For this reason, these endpoints
+need to be maintained with backwards compatibility to ensure ongoing support for older clients.
+
+If breaking changes need to be introduced to an endpoint, a new endpoint should be created
+instead, at a different URL (e.g. with version number v2 instead of v1), leaving the original
+endpoint in place and maintained to the best extent possible so older clients can still use it.
+
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from rest_framework import routers
+
+from .auth.api import PublicFacilityViewSet
+
+router = routers.SimpleRouter()
+
+router.register(r'v1/facility', PublicFacilityViewSet, base_name='publicfacility')
+
+urlpatterns = router.urls
