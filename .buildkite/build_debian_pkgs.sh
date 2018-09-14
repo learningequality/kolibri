2566cd860579fc60d04b4ca9a734780013fb8fba
Benjamin Bach <benjamin@learningequality.org>
2018-09-14 20:39:29 +0200

    Run Kolibri after installing

--- .buildkite/build_debian_pkgs.sh
+++ .buildkite/build_debian_pkgs.sh
@@ -18,7 +18,9 @@ buildkite-agent artifact download 'dist/*.tar.gz' dist/
 make docker-deb
 
 # Install the new .deb for testing purposes...
-docker run --env-file ./docker/env.list -v $PWD/dist:/kolibridist -e "DEBIAN_FRONTEND=noninteractive" "learningequality/kolibri-deb" bash -c "dpkg -i /kolibridist/*.deb"
+docker run --env-file ./docker/env.list -v $PWD/dist:/kolibridist \
+  -e "DEBIAN_FRONTEND=noninteractive" "learningequality/kolibri-deb" \
+  bash -c "dpkg -i /kolibridist/*.deb ; kolibri start ; kolibri stop"
 
 # Upload built kolibri windows installer at buildkite artifact.
 buildkite-agent artifact upload './dist/*.deb'
