3dc2f0a9940d753fb44b5af95b533cf2dde49fa4
Benjamin Bach <benjamin@learningequality.org>
2018-09-14 17:48:08 +0200

    Reuse docker image to try and install the .deb after building

--- .buildkite/build_debian_pkgs.sh
+++ .buildkite/build_debian_pkgs.sh
@@ -17,5 +17,8 @@ buildkite-agent artifact download 'dist/*.tar.gz' dist/
 
 make docker-deb
 
+# Install the new .deb for testing purposes...
+docker run --env-file ./docker/env.list -v $PWD/dist:/kolibridist -e "DEBIAN_FRONTEND=noninteractive" "learningequality/kolibri-deb" bash -c "dpkg -i /kolibridist/*.deb"
+
 # Upload built kolibri windows installer at buildkite artifact.
 buildkite-agent artifact upload './dist/*.deb'
