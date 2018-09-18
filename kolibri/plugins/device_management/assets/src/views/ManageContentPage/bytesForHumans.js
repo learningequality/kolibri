0aef7a35f2211bb642887cf00c86cf5ea34c6de5
bonidjukic <boni@djukic.com.hr>
2018-09-18 16:54:41 +0700

    Use powers of 10 instead of 2 for KB, MB and GB
    Free disk space size in the Device -> Info page is currently displaying
    values which correspond to Kibi, Mebi and Gibi prefixes (KiB, MiB, GiB
    respectively) rather than values which correspond to Kilo, Mega and Giga
    prefixes (KB, MB, GB respectively).
    
    Kibi, Mebi and Gibi prefixes denote powers of two, whereas Kilo, Mega
    and Giga denote powers of ten.

--- kolibri/plugins/device_management/assets/src/views/ManageContentPage/bytesForHumans.js
+++ kolibri/plugins/device_management/assets/src/views/ManageContentPage/bytesForHumans.js
@@ -8,9 +8,9 @@ const translator = createTranslator('BytesForHumansStrings', {
 });
 
 const ONE_B = 1;
-const ONE_KB = 1024;
-const ONE_MB = 1048576;
-const ONE_GB = 1073741824;
+const ONE_KB = 10 ** 3;
+const ONE_MB = 10 ** 6;
+const ONE_GB = 10 ** 9;
 
 const stringMap = {
   [ONE_B]: 'fileSizeInBytes',
