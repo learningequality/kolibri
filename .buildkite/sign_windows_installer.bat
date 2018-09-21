f0317364c167c9c4049c1d3666dc06674e36b1be
Richard Amodia <richard.amodia@mrpau.com>
2018-09-21 16:25:44 +0800

    Add a singed name at the filename of the installer, after it was successfully signed.

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -1,13 +1,13 @@
 @echo off
-rem This will download the Kolibri windows installer artifact at the windows-2016 buildkite agent.
-rem After the installer successfully sign it will upload the signed installer at the Sign Windows installer pipeline artifact.
-rem To sign the Windows installer set the `SIGN_WINDOWS_INSTALLER=true` environment variable.
+rem To sign the Windows installer, set the `SIGN_WINDOWS_INSTALLER=true` environment variable.
+rem This will download the Kolibri Windows installer artifact at the windows-2016 Buildkite agent.
+rem After the installer successfully sign it will upload the installer at the Sign Windows Installer pipeline artifact.
 
 IF NOT "%SIGN_WINDOWS_INSTALLER%" == "true" (GOTO DONT_SIGN)
 
 set current_path=%cd%
 buildkite-agent.exe artifact download "installer/*.exe" . --step "Build kolibri windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
-%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && cd "%current_path%\installer\" && buildkite-agent.exe artifact upload "*.exe"
+%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && cd "%current_path%\installer\" && ren *.exe *-signed.exe && buildkite-agent.exe artifact upload "*.exe"
 GOTO END
 
 :DONT_SIGN
