b609284bc1689bd7140f9fe4a8842cd40bcbd74a
Jonathan Boiser <jboiser@gmail.com>
2018-09-27 15:23:03 -0700

    Disable select-all in topic if all child nodes are not selectable

--- kolibri/plugins/device_management/assets/src/modules/wizard/handlers.js
+++ kolibri/plugins/device_management/assets/src/modules/wizard/handlers.js
@@ -4,7 +4,6 @@ import { createTranslator } from 'kolibri.utils.i18n';
 import ConditionalPromise from 'kolibri.lib.conditionalPromise';
 import samePageCheckGenerator from 'kolibri.utils.samePageCheckGenerator';
 import { ContentNodeGranularResource, RemoteChannelResource } from 'kolibri.resources';
-import ChannelResource from '../../apiResources/deviceChannel';
 import { ContentWizardPages, ContentWizardErrors, TransferTypes } from '../../constants';
 import { manageContentPageLink } from '../../views/ManageContentPage/manageContentLinks';
 import { getAvailableSpaceOnDrive, loadChannelMetadata } from './actions/selectContentActions';
