038a5fdd65824a386e28048a43634f1170e11d1e
Richard Amodia <richard.amodia@mrpau.com>
2018-09-27 19:09:17 +0800

    Add period to the signed name.

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -7,7 +7,7 @@ IF NOT "%SIGN_WINDOWS_INSTALLER%" == "true" (GOTO DONT_SIGN)
 
 set current_path=%cd%
 buildkite-agent.exe artifact download "installer/*.exe" . --step "Build kolibri windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
-%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && cd "%current_path%\installer\" && ren *.exe *-signed.exe && buildkite-agent.exe artifact upload "*.exe"
+%WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\installer" && cd "%current_path%\installer\" && ren *.exe *.-signed.exe && buildkite-agent.exe artifact upload "*.exe"
 GOTO END
 
 :DONT_SIGN
