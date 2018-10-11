9ea67fa6181144deecbd777699572cb2f9598206
Benjamin Bach <benjamin@learningequality.org>
2018-10-11 16:54:14 +0200

    Move docstring to top and add BSD License

--- kolibri/core/analytics/pskolibri/__init__.py
+++ kolibri/core/analytics/pskolibri/__init__.py
@@ -1,3 +1,47 @@
+"""
+This module is heavily based on the psutil module
+
+https://github.com/giampaolo/psutil
+Copyright (c) 2009, Jay Loden, Dave Daeschler, Giampaolo Rodola under BSD License
+
+Most of these functions are stripped versions of psutil
+simplified/adapted integrated with Kolibri.
+
+Linux part follows psutil in most of the code
+Windows adds some new ideas and has replaced the C code
+by Python code wrapped over ctypes
+
+
+psutil is distributed under BSD license reproduced below.
+
+Copyright (c) 2009, Jay Loden, Dave Daeschler, Giampaolo Rodola'
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the psutil authors nor the names of its contributors
+   may be used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
 from __future__ import absolute_import
 from __future__ import division
 
@@ -10,17 +54,6 @@ from kolibri.core.analytics.pskolibri.common import memoize_when_activated
 from kolibri.core.analytics.pskolibri.common import NoSuchProcess
 from kolibri.core.analytics.pskolibri.common import PY3
 from kolibri.core.analytics.pskolibri.common import WINDOWS
-# This module is heavily based on the psutil module
-#
-# https://github.com/giampaolo/psutil
-# Copyright (c) 2009, Jay Loden, Dave Daeschler, Giampaolo Rodola under BSD License
-#
-# Most of these functions are stripped versions of psutil
-# simplified/adapted integrated with Kolibri.
-#
-# Linux part follows psutil in most of the code
-# Windows adds some new ideas and has replaced the C code
-# by Python code wrapped over ctypes
 
 
 _TOTAL_PHYMEM = None
