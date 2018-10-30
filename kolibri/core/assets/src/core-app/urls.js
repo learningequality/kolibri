754dbd0f4522dc04abe21781aba14060838aeae9
Richard Tibbles <richard@learningequality.org>
2018-10-30 14:01:16 -0700

    Use a dynamically generated static path in frontend, rather than static.

--- kolibri/core/assets/src/core-app/urls.js
+++ kolibri/core/assets/src/core-app/urls.js
@@ -3,6 +3,15 @@
  * which has urls written into it at runtime by the Kolibri server.
  */
 
-const urls = {};
+const urls = {
+  static(url) {
+    if (!this.__staticURL) {
+      throw new ReferenceError('Static URL is not defined');
+    }
+    const base = new URL(this.__staticURL, window.location.origin);
+    const urlObject = new URL(url, base);
+    return urlObject.href;
+  },
+};
 
 export default urls;
