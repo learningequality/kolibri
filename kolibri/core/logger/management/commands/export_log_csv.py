86ac07773b498d7e701e5085eb9309aae2cf01f1
José L. Redrejo Rodríguez <jredrejo@debian.org>
2018-11-12 19:57:36 +0100

    export_log_csv command converted into async so it can be used with iceqube

--- kolibri/core/logger/management/commands/export_log_csv.py
+++ kolibri/core/logger/management/commands/export_log_csv.py
@@ -2,11 +2,29 @@ import logging
 import os
 import tempfile
 
+from rest_framework import serializers
+
 from kolibri.core.logger.csv import ContentSessionLogCSVExportViewSet
 from kolibri.core.logger.csv import ContentSummaryLogCSVExportViewSet
 from kolibri.core.tasks.management.commands.base import AsyncCommand
 
 logger = logging.getLogger(__name__)
+
+
+class AsyncListSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        """
+        List of object instances -> List of dicts of primitive datatypes.
+        """
+        for item in data:
+            yield self.child.to_representation(item)
+
+    @property
+    def data(self):
+        # We deliberately return the super of ListSerializer to avoid
+        # instantiating a ReturnList, which would force evaluating the generator
+        return super(serializers.ListSerializer, self).data
 
 
 class Command(AsyncCommand):
@@ -23,12 +41,28 @@ class Command(AsyncCommand):
 
     def _data(self, csv_set):
         queryset = csv_set.get_queryset()
+        self.total_rows = queryset.count()
         serializer_class = csv_set.get_serializer_class()
+        serializer_class.Meta.list_serializer_class = AsyncListSerializer
         serializer = serializer_class(queryset, many=True)
-        # The async update part must be implemented in the next sentence:
-        data = serializer.data
+
+        finished = False
+        while not finished:
+            finished = self._get_serialized_data(serializer)
+            if self.is_cancelled():
+                self.cancel()
+                break
         renderer = csv_set.renderer_classes[0]()
-        return renderer.render(data)
+        return renderer.render(self.data)
+
+    def _get_serialized_data(self, serializer):
+        self.data = []
+        with self.start_progress(total=self.total_rows) as progress_update:
+            for item in serializer.data:
+                progress_update(1)
+                self.data.append(item)
+
+        return True
 
     def _create_file(self, buffer, filename=None):
         if filename:
@@ -48,5 +82,8 @@ class Command(AsyncCommand):
             'session': ContentSummaryLogCSVExportViewSet,
         }
         csv_set = export_classes[options['log_type']]()
+        # Here is where 99% of the time is spent:
         buffer = self._data(csv_set)
+        # Considering insignificant (in relation to the time needed to serialize the table)
+        # the time spent in saving the file:
         self._create_file(buffer, options['output_file'])
