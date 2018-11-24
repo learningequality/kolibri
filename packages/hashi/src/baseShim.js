389fd8bda33582a8df59ae0afa8090783c5d6b28
Richard Tibbles <richard@learningequality.org>
2018-11-24 13:37:45 -0800

    Add cookie shim.

--- packages/hashi/src/baseShim.js
+++ packages/hashi/src/baseShim.js
@@ -1,6 +1,14 @@
+import { events } from './hashiBase';
+
 export default class BaseShim {
   constructor(mediator) {
     this.__mediator = mediator;
+    this.events = Object.assign({}, events);
+  }
+
+  setData(data) {
+    this.__setData(data);
+    this.stateUpdated();
   }
 
   sendMessage(event, data) {
@@ -19,5 +27,9 @@ export default class BaseShim {
       throw ReferenceError(`${event} is not a valid event name for ${this.nameSpace}`);
     }
     this.__mediator.removeMessageHandler({ nameSpace: this.nameSpace, event, callback });
+  }
+
+  stateUpdated() {
+    this.sendMessage(this.events.STATEUPDATE, this.data);
   }
 }
