d36181e67f450aa74da132bf87b66c00e5d4abd8
David C <david@learningequality.org>
2018-12-04 12:55:32 -0800

    Copying in environment variables and appropriate keystore files for signing.

--- platforms/android/scripts/rundocker.sh
+++ platforms/android/scripts/rundocker.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+CONTAINER_HOME=/home/kivy
+CONTAINER_NAME=android_container
+
 # create the container to be used throughout the script
-docker create -t --name android_container \
-  --mount type=bind,src=${PWD}/src,dst=/home/kivy/src \
-  --mount type=bind,src=${PWD}/scripts,dst=/home/kivy/scripts \
+docker create -t --name ${CONTAINER_NAME} \
+  --mount type=bind,src=${PWD}/src,dst=${CONTAINER_HOME}/src \
+  --mount type=bind,src=${PWD}/scripts,dst=${CONTAINER_HOME}/scripts \
+  --env P4A_RELEASE_KEYSTORE=${CONTAINER_HOME} \
+  --env P4A_RELEASE_KEYALIAS=${P4A_RELEASE_KEYALIAS} \
+  --env P4A_RELEASE_KEYSTORE_PASSWD=${P4A_RELEASE_KEYSTORE_PASSWD} \
+  --env P4A_RELEASE_KEYALIAS_PASSWD=${P4A_RELEASE_KEYALIAS_PASSWD} \
   android_kolibri
 
-# TODO copy the signing key and set appropriate environment variables for prod
+# make sure the environment variable is defined
+if [ "${P4A_RELEASE_KEYSTORE}" ]; then
+  # make sure the directory is valid
+  if [ -a ${P4A_RELEASE_KEYSTORE} ]; then
+    # copy keystore to same location on the container
+    docker cp ${P4A_RELEASE_KEYSTORE} ${CONTAINER_NAME}:${CONTAINER_HOME}}
+  fi
+fi
 
 # run the container, generating the apk
-docker start -a android_container
-docker wait android_container
+docker start -a ${CONTAINER_NAME} | grep -v "Copying /"
 
 # copy the apk to our host. Handles permissions.
-docker cp android_container:/home/kivy/dist/ .
+docker cp ${CONTAINER_NAME}:${CONTAINER_HOME}/dist/ .
 
 # manually remove the container afterward
-docker rm android_container
+docker rm ${CONTAINER_NAME}
