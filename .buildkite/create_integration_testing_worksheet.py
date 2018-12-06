ff4f52f7ef23db9766e18e299e82bd427b159041
Richard Amodia <richard.amodia@mrpau.com>
2018-12-06 19:38:38 +0800

    Display the worksheet link after it was created by the script. Fix the hardcoded column loop.

--- .buildkite/create_integration_testing_worksheet.py
+++ .buildkite/create_integration_testing_worksheet.py
@@ -6,39 +6,58 @@
 # Environment Variable/s:
     * GOOGLE_APPLICATION_CREDENTIALS = Your service account key.
 """
-
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+import itertools
 import os
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-WKS_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-WKS_TAG = os.getenv("BUILDKITE_TAG")
 
-WKS_TEMPLATE_TAG = 'release-v0.11.x'
-WKS_NAME = "Integration testing with Gherkin scenarios"
-WKS_INDEX_COPY = 0
-WKS_CELLS_RANGE = 100
-WKS_COLUMN = 'B'
+SPREADSHEET_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+SPREADSHEET_FILE_KEY = os.getenv("WKS_FILE_KEY")
+
+SHEET_TAG = os.getenv("BUILDKITE_TAG")
+SHEET_TEMPLATE_FEATURES_COLUMN = 'B'
+SHEET_TEMPLATE_INDEX = 0
+SHEET_MAX_EMPTY_CELL = 20
+SHEET_TEMPLATE_TAG = 'release-v0.11.x'
 
 SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
 
 def copy_worksheet():
-	google_account = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name(WKS_CREDENTIALS, SCOPE))
-	wks = google_account.open(WKS_NAME)
-	wks_id = wks.get_worksheet(WKS_INDEX_COPY).id
-	return wks.duplicate_sheet(wks_id, insert_sheet_index=1, new_sheet_id=None, new_sheet_name=WKS_TAG)
+    google_account = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name(SPREADSHEET_CREDENTIALS, SCOPE))
+    wks_open = google_account.open_by_key(SPREADSHEET_FILE_KEY)
+    wks_id = wks_open.get_worksheet(SHEET_TEMPLATE_INDEX).id
+    return wks_open.duplicate_sheet(wks_id, insert_sheet_index=1, new_sheet_id=None, new_sheet_name=SHEET_TAG)
+
+
+def get_worksheet_link(wks_id):
+    wks_domain = "https://docs.google.com/spreadsheets/d/"
+    link_arrg = "{0}{1}{2}{3}"
+    return link_arrg.format(wks_domain, SPREADSHEET_FILE_KEY, "/edit#gid=", wks_id)
+
 
 def edit_new_worksheet():
-	worksheet = copy_worksheet()
-	for count in range(WKS_CELLS_RANGE):
-		current_wks = WKS_COLUMN + str(count+1)
-		val = worksheet.acell(current_wks, value_render_option='FORMULA').value
-		final_value = val.replace(WKS_TEMPLATE_TAG, WKS_TAG)
-		if str(final_value):
-			worksheet.update_acell(current_wks, final_value)
+    worksheet = copy_worksheet()
+    print(worksheet.id)
+    count_cell_empty = 0
+    for count in itertools.count():
+        current_cell = SHEET_TEMPLATE_FEATURES_COLUMN + str(count + 1)
+        cell_value = worksheet.acell(current_cell, value_render_option='FORMULA').value
+        edited_cell_value = cell_value.replace(SHEET_TEMPLATE_TAG, SHEET_TAG)
+        if SHEET_TAG in edited_cell_value:
+            count_cell_empty = 0
+            worksheet.update_acell(current_cell, edited_cell_value)
+        else:
+            if not str(edited_cell_value):
+                count_cell_empty += 1
+            if count_cell_empty >= SHEET_MAX_EMPTY_CELL:
+                break
+    wks_link = get_worksheet_link(worksheet.id)
+    print("Here's the new integration testing worksheet link =====>", wks_link)
+
 
 def main():
     edit_new_worksheet()
