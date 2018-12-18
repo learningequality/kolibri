c57bce0ca1f5d474560a6dcc1a9a14b153e55667
Richard Tibbles <richard@learningequality.org>
2018-12-18 15:26:54 -0800

    Increase readability of packages.

--- jest.conf.js
+++ jest.conf.js
@@ -1,6 +1,7 @@
 const baseConfig = require('kolibri-tools/jest.conf');
 
 module.exports = Object.assign(baseConfig, {
+  // Make sure we transpile any raw vue or ES6 files
   transformIgnorePatterns: ['/node_modules/(?!(keen-ui|epubjs|kolibri-tools|kolibri)/).*/'],
   collectCoverageFrom: [
     'frontend_build/src/*.js',
