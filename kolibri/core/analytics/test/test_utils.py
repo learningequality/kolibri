8a811248436cd77c0667c765f4e71202b8da3624
Jamie Alexandre <jamalex@gmail.com>
2018-12-30 14:37:01 -0800

    Handle the case of there not being any timestamped logs at all in ping

--- kolibri/core/analytics/test/test_utils.py
+++ kolibri/core/analytics/test/test_utils.py
@@ -235,6 +235,15 @@ class FacilityStatisticsTestCase(BaseDeviceSetupMixin, TestCase):
         assert actual["f"] == "2018-10-11"
         assert actual["l"] == "2019-10-11"
 
+    def test_regression_4606_no_contentsessions_or_usersessions(self):
+        ContentSessionLog.objects.all().delete()
+        UserSessionLog.objects.all().delete()
+        facility = self.facilities[0]
+        # will raise an exception if we haven't addressed https://github.com/learningequality/kolibri/issues/4606
+        actual = extract_facility_statistics(facility)
+        assert actual["f"] is None
+        assert actual["l"] is None
+
 
 class ChannelStatisticsTestCase(BaseDeviceSetupMixin, TestCase):
 
--- kolibri/core/analytics/utils.py
+++ kolibri/core/analytics/utils.py
@@ -65,12 +65,13 @@ def extract_facility_statistics(facility):
                     .filter(start_timestamp__gt=datetime.datetime(2016, 1, 1))
                     .aggregate(first=Min("start_timestamp"), last=Max("end_timestamp")))
 
+    # extract the first and last times we've seen logs, ignoring any that are None
     first_times = [d["first"] for d in [usersess_agg, contsess_agg] if d["first"]]
     last_times = [d["last"] for d in [usersess_agg, contsess_agg] if d["last"]]
 
     # since newly provisioned devices won't have logs, we don't know whether we have an available datetime object
-    first_interaction_timestamp = getattr(min(first_times), 'strftime', None)
-    last_interaction_timestamp = getattr(max(last_times), 'strftime', None)
+    first_interaction_timestamp = getattr(min(first_times), 'strftime', None) if first_times else None
+    last_interaction_timestamp = getattr(max(last_times), 'strftime', None) if last_times else None
 
     sesslogs_by_kind = contsessions.order_by("kind").values("kind").annotate(count=Count("kind"))
     sesslogs_by_kind = {log["kind"]: log["count"] for log in sesslogs_by_kind}
