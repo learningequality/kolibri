fc1535fb78afbeff639dfa3fe66a7a5269ad19fc
David C <david@learningequality.org>
2019-01-09 12:28:02 -0800

    Correcting environment variable definition within docker.

--- platforms/android/scripts/rundocker.sh
+++ platforms/android/scripts/rundocker.sh
@@ -8,8 +8,9 @@ echo -ne "Creating container ${CONTAINER_NAME} \n\t id: "
 docker create -t --name ${CONTAINER_NAME} \
   --mount type=bind,src=${PWD}/whl,dst=${CONTAINER_HOME}/whl \
   --mount type=bind,src=${PWD}/scripts,dst=${CONTAINER_HOME}/scripts \
+  --mount type=bind,src=${PWD}/Makefile,dst=${CONTAINER_HOME}/Makefile \
   --mount type=bind,src=${PWD}/src/main.py,dst=${CONTAINER_HOME}/src/main.py \
-  --env P4A_RELEASE_KEYSTORE=${CONTAINER_HOME} \
+  --env P4A_RELEASE_KEYSTORE=${CONTAINER_HOME}/$(basename "${P4A_RELEASE_KEYSTORE}") \
   --env P4A_RELEASE_KEYALIAS=${P4A_RELEASE_KEYALIAS} \
   --env P4A_RELEASE_KEYSTORE_PASSWD=${P4A_RELEASE_KEYSTORE_PASSWD} \
   --env P4A_RELEASE_KEYALIAS_PASSWD=${P4A_RELEASE_KEYALIAS_PASSWD} \
