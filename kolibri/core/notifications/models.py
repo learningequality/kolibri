a0d1980093d21b8e89f44341c4cde9ef321e2109
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-01-14 09:44:00 +0100

    Remove duplicated line

--- kolibri/core/notifications/models.py
+++ kolibri/core/notifications/models.py
@@ -102,7 +102,6 @@ class LearnerProgressNotification(models.Model):
     user_id = UUIDField()
     classroom_id = UUIDField()
     contentnode_id = UUIDField(null=True)
-    contentnode_id = UUIDField(null=True)
     lesson_id = UUIDField(null=True)
     quiz_id = UUIDField(null=True)
     reason = models.CharField(max_length=200, choices=HelpReason.choices(), blank=True)
