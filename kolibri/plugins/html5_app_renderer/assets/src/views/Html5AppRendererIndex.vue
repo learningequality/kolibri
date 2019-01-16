6570e9c82f3779d09f07aa0e36e4c562a0cd59bd
Richard Tibbles <richard@learningequality.org>
2019-01-15 16:11:05 -0800

    Set new arbitrary time limit on HTML5 app progress.

--- kolibri/plugins/html5_app_renderer/assets/src/views/Html5AppRendererIndex.vue
+++ kolibri/plugins/html5_app_renderer/assets/src/views/Html5AppRendererIndex.vue
@@ -69,12 +69,26 @@
       });
       this.hashi.initialize((this.extraFields && this.extraFields.contentState) || {});
       this.$emit('startTracking');
+      this.startTime = now();
+      this.pollProgress();
     },
     beforeDestroy() {
       if (this.timeout) {
         clearTimeout(this.timeout);
       }
       this.$emit('stopTracking');
+    },
+    methods: {
+      recordProgress() {
+        const totalTime = now() - this.startTime;
+        this.$emit('updateProgress', Math.max(0, totalTime / 300000));
+        this.pollProgress();
+      },
+      pollProgress() {
+        this.timeout = setTimeout(() => {
+          this.recordProgress();
+        }, 15000);
+      },
     },
     $trs: {
       exitFullscreen: 'Exit fullscreen',
