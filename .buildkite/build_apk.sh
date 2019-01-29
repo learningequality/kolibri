7bd1e97bceaa5e3c56afd40802ad3daef4b6688c
David Canas <david@learningequality.org>
2019-01-29 11:22:01 -0800

    Adding automated Android builds to Buildkite pipeline.
    Update build script to include signatures.
    Includes on-demand encryption/decryption of release keys.
    
    Always build APK.
    Adding block to buildkite pipeline for releases.
    
    Updating paths.
    
    Cleaning up Bash syntax
    
    Fixing pathname issues.
    
    Using https due to ssh auth issue
    
    Restoring failure reporting to apk build script
    
    Cleaning up build script in favor of build hooks.
    
    Fixing pipeline syntax
    
    Adding logs to the build script and creating whl directory.
    
    DRYing up build entry in pipeline.
    
    Adding logs for debug
    
    Moving whl files out of dist/ dir
    
    Modifying Logs

--- .buildkite/build_apk.sh
+++ .buildkite/build_apk.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
+ANDROID_REPO_NAME="kolibri-installer-android"
+
 set -euo pipefail
 
-# clone a fresh copy of the latest Kolibri Kivy repository
-yes y | rm -r kolibri-kivy/ || true 2> /dev/null
-git clone https://github.com/learningequality/kolibri-kivy.git
-cd kolibri-kivy
-git checkout develop
-cd ..
+# clone the repo if folder does not exist
+if [ ! -d $ANDROID_REPO_NAME ]; then
+  echo '--- :robot_face: Cloning android repo'
+  git clone https://github.com/learningequality/${ANDROID_REPO_NAME}.git
+fi
 
+cd $ANDROID_REPO_NAME
+echo $PWD
+
+# Ensure that we're on the latest version of develop
+git fetch origin
+git checkout origin/develop
+
+echo '--- :ferris_wheel: Copying in latest Kolibri whl'
 # copy in the latest whl to make it available to the Docker build script
-buildkite-agent artifact download 'dist/*.whl' dist/
-cp dist/*.whl kolibri-kivy/src
+buildkite-agent artifact download 'dist/*.whl' .
+# Should probably already be there
+mkdir -p whl
+mv dist/*.whl whl/
 
-# build the APK
-cd kolibri-kivy
-make rundocker
-cd ..
-
-# extract the APK into the installer directory, and rename it to match the whl
-mkdir -p installer
-cp kolibri-kivy/bin/*.apk installer/`basename -s .whl dist/*.whl`.apk
+echo '--- :construction_worker: Building the APK'
+# build apk using docker
+make run_docker
 
 # upload the APK to Buildkite
-buildkite-agent artifact upload 'installer/*.apk'
+# TODO add last argument to change name on upload
+cd dist/android/
+
+echo '--- :arrow_up: Uploading to Buildkite'
+buildkite-agent artifact upload '*-release.apk'
