ac4e4b277e75745a0b5be04e6413d7934794f81b
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-02-04 20:31:39 +0100

    Don't exit if database is locked while vacuuming

--- kolibri/deployment/default/wsgi.py
+++ kolibri/deployment/default/wsgi.py
@@ -7,9 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 import os
+import time
 
 from django.core.wsgi import get_wsgi_application
+from django.db.utils import OperationalError
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kolibri.deployment.default.settings.base")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except OperationalError:  # It happens when sqlite vacuum is being executed. The db is locked
+    time.sleep(60)  # vacuum should not take longer than 1 minute
+    application = get_wsgi_application()  # try again one last time
