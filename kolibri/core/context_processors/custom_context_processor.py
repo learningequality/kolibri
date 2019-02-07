b94fb7d8e6c8fa3fbf99a006f49e5587e477eb94
Richard Tibbles <richard@learningequality.org>
2019-02-07 13:23:13 -0800

    Clean up unused code.

--- kolibri/core/context_processors/custom_context_processor.py
+++ kolibri/core/context_processors/custom_context_processor.py
@@ -3,12 +3,7 @@ import json
 import user_agents
 from django.conf import settings
 
-from kolibri.core.auth.api import SessionViewSet
 from kolibri.utils import conf
-
-
-def return_session(request):
-    return {'session': json.dumps(SessionViewSet().get_session(request)), 'kolibri': conf.KOLIBRI_CORE_JS_NAME}
 
 
 browser_requirements = [
--- kolibri/core/test/test_context_processor.py
+++ kolibri/core/test/test_context_processor.py
@@ -13,26 +13,6 @@ from rest_framework.test import APITestCase
 from kolibri.core.context_processors.custom_context_processor import pass_browser_entry
 
 
-class ContextProcessorTestCase(APITestCase):
-
-    def setUp(self):
-        self.request = mock.Mock()
-        self.request.session = {}
-        self.request.META = {}
-        self.template = Template("My name is...")
-        self.context = RequestContext(self.request)
-
-    def test_context_added_with_context_processor(self):
-        with mock.patch('kolibri.core.auth.api.SessionViewSet.get_session', return_value={"context": True}):
-            with self.context.bind_template(self.template):
-                self.assertEqual(json.loads(self.context['session'])['context'], True)
-
-    def tearDown(self):
-        self.request = None
-        self.template = None
-        self.context = None
-
-
 class BrowserEntryTestCase(TestCase):
     def test_pass_browser_entry_patch_fail(self):
         user_agent = mock.MagicMock()
