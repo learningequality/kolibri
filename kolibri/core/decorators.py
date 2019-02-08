dbcffcdef07f7170df1da3ae551bda4c17dfe5b1
Richard Tibbles <richard@learningequality.org>
2019-02-08 11:16:30 -0800

    Remove user specific data from templates. Add experimental no-user-data cache decorator.

--- kolibri/core/decorators.py
+++ kolibri/core/decorators.py
@@ -8,6 +8,8 @@ from __future__ import unicode_literals
 import sys
 from functools import wraps
 
+from django.utils.cache import patch_response_headers
+
 from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
 from six import string_types
@@ -320,3 +322,19 @@ def signin_redirect_exempt(view_func):
 
     wrapped_view.signin_redirect_exempt = True
     return wraps(view_func)(wrapped_view)
+
+
+def cache_no_user_data(view_func):
+    """
+    Set appropriate Vary on headers on a view that specify there is
+    no user specific data being rendered in the view.
+    """
+    def inner_func(*args, **kwargs):
+        request = args[0]
+        del request.session
+        response = view_func(*args, **kwargs)
+        headers = ['accept-language', 'accept-encoding', 'accept', 'user-agent']
+        patch_response_headers(response, cache_timeout=300)
+        response['Vary'] = ', '.join(headers)
+        return response
+    return inner_func
--- kolibri/plugins/user/views.py
+++ kolibri/plugins/user/views.py
@@ -3,7 +3,12 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+from kolibri.core.decorators import cache_no_user_data
 
+@method_decorator(cache_no_user_data, name='dispatch')
+@method_decorator(csrf_exempt, name='dispatch')
 class UserView(TemplateView):
     template_name = "user/user.html"
