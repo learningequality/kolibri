20673e05f269f82e5ee0d2420cd1a01ef7bd5229
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-02-08 17:40:04 +0100

    Keep the user reported while the server is trying to start

--- kolibri/deployment/default/wsgi.py
+++ kolibri/deployment/default/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+import logging
 import os
 import time
 
@@ -13,9 +14,20 @@ from django.core.wsgi import get_wsgi_application
 from django.db.utils import OperationalError
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kolibri.deployment.default.settings.base")
+logger = logging.getLogger(__name__)
 
-try:
-    application = get_wsgi_application()
-except OperationalError:  # It happens when sqlite vacuum is being executed. The db is locked
-    time.sleep(60)  # vacuum should not take longer than 1 minute
-    application = get_wsgi_application()  # try again one last time
+application = None
+tries_remaining = 6
+interval = 10
+while not application and tries_remaining:
+    try:
+        logger.info("Starting Kolibri")
+        application = get_wsgi_application()
+    except OperationalError:
+        # an OperationalError happens when sqlite vacuum is being executed. the db is locked
+        logger.info("DB busy. Trying again in {}s".format(interval))
+        tries_remaining -= 1
+        time.sleep(interval)
+        application = get_wsgi_application()  # try again one last time
+if not application:
+    logger.error("Could not start Kolibri")
