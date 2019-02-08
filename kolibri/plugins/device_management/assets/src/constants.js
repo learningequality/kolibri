5b6729bf321b8b448ea489239ba298402609ca1d
Rafael Aguayo <ralphaguayo22@gmail.com>
2019-02-07 18:15:47 -0800

    Add import from token for studio local import

--- kolibri/plugins/device_management/assets/src/constants.js
+++ kolibri/plugins/device_management/assets/src/constants.js
@@ -41,6 +41,11 @@ export const TransferTypes = {
   REMOTEIMPORT: 'remoteimport',
 };
 
+export const ApplicationTypes = {
+  KOLIBRI: 'kolibri',
+  STUDIO: 'studio',
+};
+
 export const ContentWizardErrors = {
   INVALID_PARAMETERS: 'INVALID_PARAMETERS',
   CHANNEL_NOT_FOUND_ON_SERVER: 'CHANNEL_NOT_FOUND_ON_SERVER',
