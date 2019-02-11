2845f58d1559c9edb7ee8931543471bce2a0f24c
Richard Tibbles <richard@learningequality.org>
2019-02-11 11:22:10 -0800

    Remove unnecessary router init from jest setup.

--- packages/kolibri-tools/jest.conf/setup.js
+++ packages/kolibri-tools/jest.conf/setup.js
@@ -4,10 +4,7 @@ import * as Aphrodite from 'aphrodite';
 import * as AphroditeNoImportant from 'aphrodite/no-important';
 
 import Vue from 'kolibri.lib.vue';
-import router from 'kolibri.coreVue.router';
 import { i18nSetup } from 'kolibri.utils.i18n';
-
-router.init([]);
 
 Aphrodite.StyleSheetTestUtils.suppressStyleInjection();
 AphroditeNoImportant.StyleSheetTestUtils.suppressStyleInjection();
