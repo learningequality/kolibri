fddfb58ac86d98372f2bc1418dd6f980b7efc482
Richard Tibbles <richard@learningequality.org>
2019-02-21 12:11:23 -0800

    Add in data for questions skipped by everyone on a quiz. As these are, presumably, difficult.

--- kolibri/plugins/coach/assets/src/modules/questionList/actions.js
+++ kolibri/plugins/coach/assets/src/modules/questionList/actions.js
@@ -1,5 +1,6 @@
 import { ContentNodeResource, ExamResource } from 'kolibri.resources';
 import { assessmentMetaDataState } from 'kolibri.coreVue.vuex.mappers';
+import { fetchNodeDataAndConvertExam } from 'kolibri.utils.exams';
 import ExerciseDifficulties from './../../apiResources/exerciseDifficulties';
 import QuizDifficulties from './../../apiResources/quizDifficulties';
 
@@ -15,7 +16,7 @@ export function setItemStats(store, { classId, exerciseId, quizId, lessonId, gro
     promises.push(
       ExamResource.fetchModel({
         id: quizId,
-      })
+      }).then(fetchNodeDataAndConvertExam)
     );
   } else {
     promises.push(
@@ -35,11 +36,20 @@ export function setItemStats(store, { classId, exerciseId, quizId, lessonId, gro
   return Promise.all(promises).then(([item, stats]) => {
     if (quizId) {
       store.commit('SET_STATE', { exam: item });
-      stats = stats.map(stat => ({
-        ...stat,
-        exercise_id: stat.content_id,
-        question_id: stat.item,
-      }));
+      // If no one attempted one of the questions, it could get missed out of the list
+      // of difficult questions, so use the exam data to fill in the blanks here.
+      stats = item.question_sources.map(source => {
+        const stat = stats.find(
+          stat => stat.item === source.question_id && stat.content_id === source.exercise_id
+        ) || {
+          correct: 0,
+          total: (stats[0] || {}).total || 0,
+        };
+        return {
+          ...stat,
+          ...source,
+        };
+      });
     } else {
       item.assessmentmetadata = assessmentMetaDataState(item);
       store.commit('SET_STATE', { exercise: item });
