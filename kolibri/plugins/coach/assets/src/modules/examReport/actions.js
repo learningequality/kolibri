03d26db8d3079db6ef87b5f1162b881bbc2b97e3
Jonathan Boiser <jboiser@gmail.com>
2019-02-26 12:09:32 -0800

    Delete unused examReport actions

--- kolibri/plugins/coach/assets/src/modules/examReport/actions.js
+++ kolibri/plugins/coach/assets/src/modules/examReport/actions.js
@@ -1,5 +1,4 @@
-import samePageCheckGenerator from 'kolibri.utils.samePageCheckGenerator';
-import { ExamResource, ExamLogResource, FacilityUserResource } from 'kolibri.resources';
+import { ExamResource } from 'kolibri.resources';
 import { createTranslator } from 'kolibri.utils.i18n';
 import router from 'kolibri.coreVue.router';
 import { PageNames } from '../../constants';
@@ -16,35 +15,6 @@ const snackbarTranslator = createTranslator('ExamReportSnackbarTexts', {
   examIsNowActive: 'Quiz is now active',
   examIsNowInactive: 'Quiz is now inactive',
 });
-
-function updateExamStatus(store, { examId, isActive }) {
-  return ExamResource.saveModel({
-    id: examId,
-    data: { active: isActive },
-  }).then(
-    () => {
-      store.commit('SET_EXAM_STATUS', { examId, isActive });
-      store.dispatch('setExamsModal', false);
-      store.dispatch(
-        'createSnackbar',
-        {
-          text: snackbarTranslator.$tr(isActive ? 'examIsNowActive' : 'examIsNowInactive'),
-          autoDismiss: true,
-        },
-        { root: true }
-      );
-    },
-    error => store.dispatch('handleError', error, { root: true })
-  );
-}
-
-export function activateExam(store, examId) {
-  return updateExamStatus(store, { examId, isActive: true });
-}
-
-export function deactivateExam(store, examId) {
-  return updateExamStatus(store, { examId, isActive: false });
-}
 
 export function copyExam(store, { exam, className }) {
   store.commit('CORE_SET_PAGE_LOADING', true, { root: true });
@@ -124,34 +94,4 @@ export function deleteExam(store, examId) {
     },
     error => store.dispatch('handleError', error, { root: true })
   );
-}
-
-export function setTableData(store, params) {
-  const { examId, classId } = params;
-  const { learnerGroups } = store.state;
-  const isSamePage = params.isSamePage || samePageCheckGenerator(store);
-  const promises = [
-    ExamLogResource.fetchCollection({
-      getParams: { exam: examId, collection: classId },
-      force: true,
-    }),
-    FacilityUserResource.fetchCollection({ getParams: { member_of: classId } }),
-  ];
-  return Promise.all(promises).then(([examLogs, facilityUsers]) => {
-    const examReportData = facilityUsers.map(user => {
-      const examTakenByUser = examLogs.find(examLog => String(examLog.user) === user.id) || {};
-      const learnerGroup = learnerGroups.find(group => group.user_ids.indexOf(user.id) > -1) || {};
-      return {
-        id: user.id,
-        name: user.full_name,
-        group: learnerGroup,
-        score: examTakenByUser.score,
-        progress: examTakenByUser.progress,
-        closed: examTakenByUser.closed,
-      };
-    });
-    if (isSamePage()) {
-      store.commit('SET_EXAM_REPORT_TABLE_DATA', examReportData);
-    }
-  });
 }
