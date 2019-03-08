001c007d656e00a74415968e03b20df5f7ce7ccb
Jonathan Boiser <jboiser@gmail.com>
2019-03-07 16:36:58 -0800

    Inject vuex store into DemoServer vue instance

--- kolibri/plugins/demo_server/assets/src/module.js
+++ kolibri/plugins/demo_server/assets/src/module.js
@@ -1,4 +1,5 @@
 import Vue from 'kolibri.lib.vue';
+import store from 'kolibri.coreVue.vuex.store';
 import DemoServerIndex from './views/DemoServerIndex';
 import KolibriModule from 'kolibri_module';
 
@@ -6,7 +7,7 @@ class DemoServerModule extends KolibriModule {
   ready() {
     const modalDiv = global.document.createElement('div');
     global.document.body.appendChild(modalDiv);
-    this.rootvue = new Vue(Object.assign({ el: modalDiv }, DemoServerIndex));
+    this.rootvue = new Vue(Object.assign({ el: modalDiv, store }, DemoServerIndex));
   }
 }
 
