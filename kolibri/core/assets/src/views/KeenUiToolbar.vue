886a6d77b4aa1f8ab3856ce7b2d5f90a4e5753cd
Michaela Robosova <robosova.michaela@seznam.cz>
2019-03-16 15:01:21 +0100

    Add customized KeenUI toolbar to coreVue

---
+++ kolibri/core/assets/src/views/KeenUiToolbar.vue
@@ -0,0 +1,20 @@
+<script>
+
+  import UiToolbar from 'keen-ui/src/UiToolbar';
+
+  export default {
+    name: 'KeenUiToolbar',
+    extends: UiToolbar,
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  /deep/ .ui-toolbar__nav-icon {
+    margin-right: 27px;
+    margin-left: -5px;
+  }
+
+</style>
