61b282a8c7bec7fbd5457b189b2da66e341cc66a
Kapya <kapsakala@gmail.com>
2019-04-16 13:07:26 +0200

    updated buildkite scripts

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -6,7 +6,7 @@ rem To sign the Windows installer set the `SIGN_WINDOWS_INSTALLER=true` environm
 IF NOT "%SIGN_WINDOWS_INSTALLER%" == "true" (GOTO DONT_SIGN)
 
 set current_path=%cd%
-buildkite-agent.exe artifact download "dist/*.exe" . --step "Build Windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
+buildkite-agent.exe artifact download "dist/*.exe" . --step "Build kolibri windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
 %WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\dist" && cd "%current_path%\installer\" && buildkite-agent.exe artifact upload "*.exe"
 GOTO END
 
