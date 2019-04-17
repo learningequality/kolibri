1c8f85ca1e9a1d10d0aa2fd4424adbbd32e7a345
Jonathan Boiser <jboiser@gmail.com>
2019-04-16 17:20:55 -0700

    Inline AssignmentSummary into LessonSummaryPage

--- kolibri/plugins/coach/assets/src/views/plan/LessonSummaryPage/index.vue
+++ kolibri/plugins/coach/assets/src/views/plan/LessonSummaryPage/index.vue
@@ -25,13 +25,32 @@
 
       <div class="lesson-summary">
 
-        <AssignmentSummary
-          :title="lessonTitle"
-          :active="lessonActive"
-          :description="lessonDescription"
-          :recipients="lessonAssignments"
-          :groups="learnerGroups"
-        />
+        <div>
+          <h1 dir="auto">
+            <KLabeledIcon>
+              <KIcon slot="icon" lesson />
+              {{ currentLesson.title }}
+            </KLabeledIcon>
+          </h1>
+
+          <HeaderTable>
+            <HeaderTableRow :keyText="coachStrings.$tr('statusLabel')">
+              <LessonActive slot="value" :active="currentLesson.is_active" />
+            </HeaderTableRow>
+            <HeaderTableRow :keyText="coachStrings.$tr('recipientsLabel')">
+              <template slot="value">
+                <template v-if="!currentLesson.lesson_assignments.length">
+                  {{ this.$tr('noOne') }}
+                </template>
+                <Recipients v-else :groupNames="groupNames" />
+              </template>
+            </HeaderTableRow>
+            <HeaderTableRow
+              :keyText="coachStrings.$tr('descriptionLabel')"
+              :valueText="currentLesson.description || $tr('noDescription')"
+            />
+          </HeaderTable>
+        </div>
 
         <div>
           <div class="resource-list">
@@ -76,9 +95,14 @@
   import samePageCheckGenerator from 'kolibri.utils.samePageCheckGenerator';
   import { crossComponentTranslator } from 'kolibri.utils.i18n';
   import KRouterLink from 'kolibri.coreVue.components.KRouterLink';
+  import KIcon from 'kolibri.coreVue.components.KIcon';
+  import KLabeledIcon from 'kolibri.coreVue.components.KLabeledIcon';
   import commonCoach from '../../common';
   import { selectionRootLink } from '../../../routes/planLessonsRouterUtils';
-  import AssignmentSummary from '../../plan/assignments/AssignmentSummary';
+  import HeaderTable from '../../common/HeaderTable';
+  import HeaderTableRow from '../../common/HeaderTable/HeaderTableRow';
+  import Recipients from '../../common/Recipients';
+  import LessonActive from '../../common/LessonActive';
   import ReportsLessonHeader from '../../reports/ReportsLessonHeader';
   import BackLinkWithOptions from '../../common/BackLinkWithOptions';
   import ManageLessonModals from './ManageLessonModals';
@@ -91,15 +115,20 @@
     name: 'LessonSummaryPage',
     metaInfo() {
       return {
-        title: this.lessonTitle,
+        title: this.currentLesson.title,
       };
     },
     components: {
+      KIcon,
+      KLabeledIcon,
+      HeaderTable,
+      HeaderTableRow,
+      Recipients,
+      LessonActive,
       BackLinkWithOptions,
       ResourceListTable,
       ManageLessonModals,
       KRouterLink,
-      AssignmentSummary,
       LessonOptionsDropdownMenu,
     },
     mixins: [commonCoach],
@@ -109,18 +138,22 @@
       },
       ...mapState(['reportRefreshInterval']),
       ...mapState('classSummary', { classId: 'id' }),
-      ...mapState('lessonSummary', {
-        // IDEA refactor, make actions get all this information themselves.
-        lessonId: state => state.currentLesson.id,
-        lessonTitle: state => state.currentLesson.title,
-        lessonActive: state => state.currentLesson.is_active,
-        lessonDescription: state => state.currentLesson.description,
-        lessonAssignments: state => state.currentLesson.lesson_assignments,
-        learnerGroups: state => state.learnerGroups,
-        workingResources: state => state.workingResources,
-      }),
+      ...mapState('lessonSummary', ['currentLesson', 'workingResources']),
+      lessonId() {
+        return this.$route.params.lessonId;
+      },
       lessonSelectionRootPage() {
         return selectionRootLink({ lessonId: this.lessonId, classId: this.classId });
+      },
+      groupNames() {
+        const names = [];
+        this.recipients.forEach(r => {
+          const match = this.groups.find(({ id }) => id === r.collection);
+          if (match) {
+            names.push(match.name);
+          }
+        });
+        return names;
       },
     },
     mounted() {
@@ -150,6 +183,8 @@
       noResourcesInLesson: 'No resources in this lesson',
       resources: 'Resources',
       manageResourcesButton: 'Manage resources',
+      noDescription: 'No description',
+      noOne: 'No one',
     },
   };
 
