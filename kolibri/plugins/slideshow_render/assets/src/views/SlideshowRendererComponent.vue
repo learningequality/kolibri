c38020c03ba19365f8c7b9b5b8e211a0952cab69
Jacob Pierce <w.jacobpierce@gmail.com>
2019-04-18 15:52:54 -0700

    Slideshow base renderer

---
+++ kolibri/plugins/slideshow_render/assets/src/views/SlideshowRendererComponent.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+    Slideshow
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'SlideshowRendererComponent',
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  /* TODO: Add Styles... */
+
+</style>
--- kolibri/utils/conf.py
+++ kolibri/utils/conf.py
@@ -77,6 +77,7 @@ except ImportError:
         "kolibri.plugins.style_guide",
         "kolibri.plugins.document_epub_render",
         "kolibri.plugins.default_theme",
+        "kolibri.plugins.slideshow_render",
     ]
 
 conf_file = os.path.join(KOLIBRI_HOME, "kolibri_settings.json")
