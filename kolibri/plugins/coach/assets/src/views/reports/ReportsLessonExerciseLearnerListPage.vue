7ce1f06c63f1a45d6ec743e1a0c650d346c193ab
Michaela Robosova <robosova.michaela@seznam.cz>
2019-05-05 13:19:17 +0200

    Switch to new exercise learners table component

--- kolibri/plugins/coach/assets/src/views/reports/ReportsLessonExerciseLearnerListPage.vue
+++ kolibri/plugins/coach/assets/src/views/reports/ReportsLessonExerciseLearnerListPage.vue
@@ -22,47 +22,8 @@
         <StatusSummary :tally="tally" />
       </p>
 
-      <CoreTable :emptyMessage="coachStrings.$tr('activityListEmptyState')">
-        <thead slot="thead">
-          <tr>
-            <th>{{ coachStrings.$tr('nameLabel') }}</th>
-            <th>{{ coachStrings.$tr('progressLabel') }}</th>
-            <th>{{ coachStrings.$tr('timeSpentLabel') }}</th>
-            <th>{{ coachStrings.$tr('groupsLabel') }}</th>
-            <th>{{ coachStrings.$tr('lastActivityLabel') }}</th>
-          </tr>
-        </thead>
-        <transition-group slot="tbody" tag="tbody" name="list">
-          <tr v-for="tableRow in table" :key="tableRow.id">
-            <td>
-              <KRouterLink
-                v-if="showLink(tableRow)"
-                :text="tableRow.name"
-                :to="link(tableRow.id)"
-              />
-              <template v-else>
-                {{ tableRow.name }}
-              </template>
-            </td>
-            <td>
-              <StatusSimple :status="tableRow.statusObj.status" />
-            </td>
-            <td>
-              <TimeDuration
-                :seconds="showTimeDuration(tableRow)"
-              />
-            </td>
-            <td>
-              <TruncatedItemList :items="tableRow.groups" />
-            </td>
-            <td>
-              <ElapsedTime
-                :date="showElapsedTime(tableRow)"
-              />
-            </td>
-          </tr>
-        </transition-group>
-      </CoreTable>
+      <ReportsExerciseLearners :entries="table" />
+
     </KPageContainer>
   </CoreBase>
 
@@ -74,11 +35,13 @@
   import commonCoach from '../common';
   import { PageNames } from '../../constants';
   import ReportsLessonExerciseHeader from './ReportsLessonExerciseHeader';
+  import ReportsExerciseLearners from './ReportsExerciseLearners';
 
   export default {
     name: 'ReportsLessonExerciseLearnerListPage',
     components: {
       ReportsLessonExerciseHeader,
+      ReportsExerciseLearners,
     },
     mixins: [commonCoach],
     computed: {
@@ -101,31 +64,14 @@
               this.$route.params.exerciseId,
               learner.id
             ),
+            exerciseLink: this.classRoute(PageNames.REPORTS_LESSON_EXERCISE_LEARNER_PAGE_ROOT, {
+              learnerId: learner.id,
+            }),
           };
           Object.assign(tableRow, learner);
           return tableRow;
         });
         return mapped;
-      },
-    },
-    methods: {
-      link(learnerId) {
-        return this.classRoute(PageNames.REPORTS_LESSON_EXERCISE_LEARNER_PAGE_ROOT, { learnerId });
-      },
-      showLink(tableRow) {
-        return tableRow.statusObj.status !== this.STATUSES.notStarted;
-      },
-      showTimeDuration(tableRow) {
-        if (tableRow.statusObj.status !== this.STATUSES.notStarted) {
-          return tableRow.statusObj.time_spent;
-        }
-        return undefined;
-      },
-      showElapsedTime(tableRow) {
-        if (tableRow.statusObj.status !== this.STATUSES.notStarted) {
-          return tableRow.statusObj.last_activity;
-        }
-        return undefined;
       },
     },
   };
