d17b4158cb25471639d686aa78963d35d84e1538
Richard Tibbles <richard@learningequality.org>
2019-05-10 17:02:35 -0700

    Use the path prefix option for the CSRF token cookie path.

--- kolibri/deployment/default/settings/base.py
+++ kolibri/deployment/default/settings/base.py
@@ -261,6 +261,11 @@ if path_prefix != '/':
 STATIC_URL = urljoin(path_prefix, 'static/')
 STATIC_ROOT = os.path.join(conf.KOLIBRI_HOME, "static")
 
+# https://docs.djangoproject.com/en/1.11/ref/settings/#csrf-cookie-path
+# Ensure that our CSRF cookie does not collide with other CSRF cookies
+# set by other Django apps served from the same domain.
+CSRF_COOKIE_PATH = path_prefix
+
 # https://docs.djangoproject.com/en/1.9/ref/settings/#std:setting-LOGGING
 # https://docs.djangoproject.com/en/1.9/topics/logging/
 
