525a410bda7df6c41284bd7b5b5528915c98917e
Blaine Jester <blainesworld@gmail.com>
2019-06-06 12:06:45 -0700

    Remove coalesce since completion_date isn't strictly tied to learner interactions

--- kolibri/plugins/coach/class_summary_api.py
+++ kolibri/plugins/coach/class_summary_api.py
@@ -1,7 +1,6 @@
 from django.db.models import Count
 from django.db.models import Max
 from django.db.models import Sum
-from django.db.models.functions import Coalesce
 from django.shortcuts import get_object_or_404
 from le_utils.constants import content_kinds
 from rest_framework import permissions
@@ -282,11 +281,7 @@ class ClassSummaryViewSet(viewsets.ViewSet):
         query_exams = Exam.objects.filter(collection=pk)
         query_exam_logs = logger_models.ExamLog.objects.filter(
             exam__in=query_exams
-        ).annotate(
-            last_activity=Coalesce(
-                Max("attemptlogs__end_timestamp"), "completion_timestamp"
-            )
-        )
+        ).annotate(last_activity=Max("attemptlogs__end_timestamp"))
 
         lesson_data = data(LessonSerializer, query_lesson)
         exam_data = data(ExamSerializer, query_exams)
