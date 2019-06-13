0e8468b721584dd02b777a3f13f1fd47504f6f55
Jacob Pierce <w.jacobpierce@gmail.com>
2019-06-13 06:36:23 -0700

    coach-plan-group-lesson-diffQs label=exerciseName

--- kolibri/plugins/coach/assets/src/views/reports/ReportsGroupReportLessonExerciseQuestionPage.vue
+++ kolibri/plugins/coach/assets/src/views/reports/ReportsGroupReportLessonExerciseQuestionPage.vue
@@ -5,7 +5,7 @@
     :immersivePagePrimary="true"
     immersivePageIcon="arrow_back"
     :immersivePageRoute="toolbarRoute"
-    :appBarTitle="title"
+    :appBarTitle="exercise.title"
     :pageTitle="title"
     :authorized="userIsAuthorized"
     authorizedRole="adminOrCoach"
@@ -32,6 +32,7 @@
     mixins: [commonCoach],
     computed: {
       ...mapState('questionDetail', ['title']),
+      ...mapState('exerciseDetail', ['exercise']),
       toolbarRoute() {
         return this.classRoute('ReportsGroupReportLessonExerciseQuestionListPage', {});
       },
