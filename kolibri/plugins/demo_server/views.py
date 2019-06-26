ea8be3f119009d1e770d735150a18864319d1e95
Richard Tibbles <richard@learningequality.org>
2019-06-25 17:54:44 -0700

    Clean up unused code left over from copypaste.

--- kolibri/plugins/demo_server/views.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
-
-from django.views.generic.base import TemplateView
-
-
-class StyleGuideView(TemplateView):
-    template_name = "style_guide/style_guide.html"
