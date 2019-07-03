faf67e2eb1d92b7c15caa098c7c8499f1927ea53
Richard Tibbles <richard@learningequality.org>
2019-07-02 17:00:16 -0700

    Simplify oidc provider configuration by just tweaking the URL config.

--- kolibri/plugins/oidc_provider_plugin/api_urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include
-from django.conf.urls import url
-from rest_framework import routers
-
-urlpatterns = [url(r"^", include("oidc_provider.urls", namespace="oidc_provider"))]
--- kolibri/plugins/oidc_provider_plugin/kolibri_plugin.py
+++ kolibri/plugins/oidc_provider_plugin/kolibri_plugin.py
@@ -1,22 +1,17 @@
-from __future__ import absolute_import, print_function, unicode_literals
-from kolibri.plugins.base import KolibriPluginBase
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
 from kolibri.core.oidc_provider_hook import OIDCProviderHook
+from kolibri.plugins.base import KolibriPluginBase
 
 
 class OIDCProvider(KolibriPluginBase):
-    untranslated_view_urls = "api_urls"
     root_view_urls = "root_urls"
     django_settings = "settings"
 
     def url_slug(self):
         return "^oidc_provider/"
-
-    # IMPORTANT: This should be added in case we want to have this feature:
-    # https://django-oidc-provider.readthedocs.io/en/latest/sections/sessionmanagement.html#sessionmanagement
-    # def __init__(self):
-    #     MIDDLEWARE += ["oidc_provider.middleware.SessionManagementMiddleware"]
-    #     setattr(settings, 'MIDDLEWARE', MIDDLEWARE)
 
 
 class EnableOIDC(OIDCProviderHook):
