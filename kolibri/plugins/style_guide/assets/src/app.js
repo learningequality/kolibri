8f713bcdd3dd9b450f853ca3936d7099c77aa342
Devon Rueckner <devon@learningequality.org>
2019-07-11 00:44:14 -0700

    get your scroll on

--- kolibri/plugins/style_guide/assets/src/app.js
+++ kolibri/plugins/style_guide/assets/src/app.js
@@ -1,6 +1,6 @@
 import router from 'kolibri.coreVue.router';
-import store from 'kolibri.coreVue.vuex.store';
 import urls from 'kolibri.urls';
+import debounce from 'lodash/debounce';
 import RootVue from './views/StyleGuideIndex';
 import { navMenuRoutes } from './routes';
 import pluginModule from './modules/pluginModule';
@@ -21,9 +21,20 @@ const scrollBehavior = function(to, from, savedPosition) {
     position.offset = { y: 128 };
     return position;
   }
+
+  // it seems we just cleared the hash due to scrolling, so don't scroll to the top
+  if (clearingHash) {
+    clearingHash = false;
+    return;
+  }
+
   // otherwise default to the top
   return { x: 0, y: 0 };
 };
+
+// two state variables to help with scrolling and anchor link positions
+let lastScrollTime = null;
+let clearingHash = false;
 
 class StyleGuideModule extends KolibriApp {
   get routes() {
@@ -36,14 +47,36 @@ class StyleGuideModule extends KolibriApp {
     return pluginModule;
   }
   ready() {
+    // make a router that lets us use anchor links (hash IDs)
     router.initRouter({
       mode: 'history',
       base: urls['style_guide'](),
       scrollBehavior,
     });
-    router.afterEach(() => {
-      store.dispatch('notLoading');
-    });
+
+    // clear the URL hash when you scroll away so you can click back to the section again!
+    window.addEventListener(
+      'scroll',
+      debounce(
+        () => {
+          // no hash, no worries
+          if (!window.location.hash) return;
+          // if it's been a while since we scrolled, nothing to do
+          const time = new Date().getTime();
+          if (lastScrollTime === null || time - lastScrollTime > 1000) {
+            lastScrollTime = time;
+            return;
+          }
+          // if we're actively scrolling, get rid of the hash
+          clearingHash = true;
+          router.replace('');
+        },
+        100,
+        { leading: true }
+      )
+    );
+
+    // people get ready
     return super.ready();
   }
 }
