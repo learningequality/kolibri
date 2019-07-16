9f6052a96aa53e58746def464b0697e74a299cff
Richard Tibbles <richard@learningequality.org>
2019-07-16 16:25:02 -0700

    Pass context into asset rendering.

--- python_packages/kolibri-sentry-plugin/kolibri_sentry_plugin/kolibri_plugin.py
+++ python_packages/kolibri-sentry-plugin/kolibri_sentry_plugin/kolibri_plugin.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from kolibri.core.webpack import hooks as webpack_hooks
 from kolibri.plugins.base import KolibriPluginBase
+from kolibri.utils import conf
 
 
 class SentryPlugin(KolibriPluginBase):
@@ -13,6 +14,10 @@ class SentryPlugin(KolibriPluginBase):
 class SentryPluginAsset(webpack_hooks.WebpackBundleHook):
     unique_slug = "kolibri_sentry_plugin_module"
     src_file = "assets/src/module.js"
+    context = {
+        "sentryDSN": conf.OPTIONS["Debug"]["SENTRY_FRONTEND_DSN"],
+        "sentryEnv": conf.OPTIONS["Debug"]["SENTRY_ENVIRONMENT"],
+    }
 
 
 class SentryPluginInclusionHook(webpack_hooks.FrontEndBaseSyncHook):
