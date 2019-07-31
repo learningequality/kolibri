9fc04ca7be11ad3a9ebbe9477fb53ba7ded8a2f3
Richard Tibbles <richard@learningequality.org>
2019-07-31 13:47:36 -0700

    Reinstate user sync inclusion hook machinery.

---
+++ kolibri/plugins/user/hooks.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from kolibri.core.webpack import hooks as webpack_hooks
+
+
+class UserSyncHook(webpack_hooks.WebpackInclusionHook):
+    """
+    Inherit a hook defining assets to be loaded synchronously in user/user.html
+    """
+
+    class Meta:
+        abstract = True
