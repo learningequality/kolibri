785ee737437a8e81927d3f8185ad830bb72f8649
Jonathan Boiser <jboiser@gmail.com>
2019-08-12 14:35:12 -0700

    Fix missing focus link on “View All” links

--- kolibri/plugins/coach/assets/src/views/home/HomePage/Block.vue
+++ kolibri/plugins/coach/assets/src/views/home/HomePage/Block.vue
@@ -61,6 +61,10 @@
     position: relative;
     top: 4px;
     right: -24px;
+    // Override inline-table rule to get focus outline to show.
+    // May not be safe to do everywhere, since it could cause
+    // vertical alignment issues like in #5606
+    display: inline-block !important;
   }
 
 </style>
