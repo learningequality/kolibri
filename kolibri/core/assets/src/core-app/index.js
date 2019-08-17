d8a41715e095a27645028f5b096a6a68687a6e80
Richard Tibbles <richard@learningequality.org>
2019-08-16 21:36:50 -0700

    Update corejs, remove redundant polyfills. Make index.js use es6 import/export.

--- kolibri/core/assets/src/core-app/index.js
+++ kolibri/core/assets/src/core-app/index.js
@@ -1,66 +1,52 @@
-// Shim Array includes for ES7 spec compliance
-require('array-includes').shim();
 // polyfill for older browsers
 // TODO: rtibbles whittle down these polyfills to only what is needed for the application
-require('core-js');
-require('url-polyfill');
-
+import 'core-js';
+// Required to setup Keen UI, should be imported only once in your project
+import 'keen-ui/src/bootstrap';
+import '../keen-config/font-stack.scss';
+import '../styles/main.scss';
+import urls from 'kolibri.urls';
+import * as theme from 'kolibri-components/src/styles/theme';
+import generateGlobalStyles from 'kolibri-components/src/styles/generateGlobalStyles';
+import trackInputModality from 'kolibri-components/src/styles/trackInputModality';
+import keenUiConfig from 'keen-ui/src/config';
+import branding from 'kolibri.utils.branding';
+import logging from 'kolibri.lib.logging';
+import keenOptions from '../keen-config/options.json';
+import CoreAppConstructor from './constructor';
+import monitorPageVisibility from './monitorPageVisibility';
+import getPluginData from 'kolibri.utils.getPluginData';
 // Do this before any async imports to ensure that public paths
 // are set correctly
-require('kolibri.urls').default.setUp();
+urls.setUp();
 
-const getPluginData = require('kolibri.utils.getPluginData').default; // eslint-disable-line
+// set up logging
+logging.setDefaultLevel(process.env.NODE_ENV === 'production' ? 2 : 0);
 
 // set up theme
-const theme = require('kolibri-components/src/styles/theme');
-
 const kolibriTheme = getPluginData().kolibriTheme;
 
 theme.setBrandColors(kolibriTheme.brandColors);
 theme.setTokenMapping(kolibriTheme.tokenMapping);
 // set up branding
-const branding = require('kolibri.utils.branding').default;
-
 branding.setBranding(kolibriTheme);
 
-// Required to setup Keen UI, should be imported only once in your project
-require('keen-ui/src/bootstrap');
-
 // configure Keen
-const KeenUiConfig = require('keen-ui/src/config').default;
-KeenUiConfig.set(require('../keen-config/options.json'));
-require('../keen-config/font-stack.scss');
+keenUiConfig.set(keenOptions);
 
 // global styles
-const generateGlobalStyles = require('kolibri-components/src/styles/generateGlobalStyles').default;
-
 generateGlobalStyles();
-require('../styles/main.scss');
 
 // monitor input modality
-const trackInputModality = require('kolibri-components/src/styles/trackInputModality').default;
-
 trackInputModality();
 
 // monitor page visibility
-require('./monitorPageVisibility');
-
-// set up logging
-const logging = require('kolibri.lib.logging').default;
-
-logging.setDefaultLevel(process.env.NODE_ENV === 'production' ? 2 : 0);
+monitorPageVisibility();
 
 // Create an instance of the global app object.
 // This is exported by webpack as the kolibriCoreAppGlobal object, due to the 'output.library' flag
 // which exports the coreApp at the bottom of this file as a named global variable:
 // https://webpack.github.io/docs/configuration.html#output-library
-//
-// This is achieved by setting the `src_file` attribute in the core
-// kolibri_plugin.py which tells the webpack build scripts to set the export of this file
-// to a global variable.
-const CoreAppConstructor = require('./constructor').default;
-
 const coreApp = new CoreAppConstructor();
 
-// Use a module.exports here to be compatible with webpack library output
-module.exports = coreApp;
+export default coreApp;
