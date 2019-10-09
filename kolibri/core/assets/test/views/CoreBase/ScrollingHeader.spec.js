369f8eaf575be6372f63b780c5b1d1f1ebe38595
Jonathan Boiser <jboiser@gmail.com>
2019-10-09 11:26:26 -0700

    Add ScrollingHeader tests

--- kolibri/core/assets/test/views/CoreBase/ScrollingHeader.spec.js
+++ kolibri/core/assets/test/views/CoreBase/ScrollingHeader.spec.js
@@ -3,20 +3,63 @@ import ScrollingHeader from '../../../src/views/CoreBase/ScrollingHeader';
 
 jest.useFakeTimers();
 
-const HEIGHT = 100;
-
-function createWrapper(scrollPosition = 0) {
+function makeWrapper(params = {}) {
+  const { scrollPosition = 0, isHidden } = params;
   return mount(ScrollingHeader, {
     propsData: {
-      height: HEIGHT,
+      mainWrapperScrollHeight: 1000,
       scrollPosition,
+      isHidden,
     },
   });
 }
 
-describe('Scrolling header movement logic', () => {
-  it('should mount', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.exists()).toBe(true);
+describe('ScrollingHeader component', () => {
+  it('should not do anything if it is hidden and user continues to go down', () => {
+    const wrapper = makeWrapper({ isHidden: true, scrollPosition: 10 });
+    wrapper.setProps({
+      scrollPosition: 20,
+    });
+    expect(wrapper.emitted('update:isHidden')).toBeUndefined();
+  });
+
+  it('should not do anything if it is shown and user continues to go up', () => {
+    const wrapper = makeWrapper({ isHidden: false, scrollPosition: 100 });
+    wrapper.setProps({
+      scrollPosition: 50,
+    });
+    expect(wrapper.emitted('update:isHidden')).toBeUndefined();
+  });
+
+  it('should not do anything if hidden and scrolling up under the distance threshold', () => {
+    const wrapper = makeWrapper({ isHidden: true, scrollPosition: 500 });
+    wrapper.setProps({
+      scrollPosition: 450,
+    });
+    expect(wrapper.emitted('update:isHidden')).toBeUndefined();
+  });
+
+  it('should not do anything if shown and scrolling down under the distance threshold', () => {
+    const wrapper = makeWrapper({ isHidden: false, scrollPosition: 500 });
+    wrapper.setProps({
+      scrollPosition: 520,
+    });
+    expect(wrapper.emitted('update:isHidden')).toBeUndefined();
+  });
+
+  it('should hide itself if going past the distance threshold', () => {
+    const wrapper = makeWrapper({ isHidden: false, scrollPosition: 100 });
+    wrapper.setProps({
+      scrollPosition: 200,
+    });
+    expect(wrapper.emitted('update:isHidden')[0]).toEqual([true]);
+  });
+
+  it('should unhide itself if going past the distance threshold', () => {
+    const wrapper = makeWrapper({ isHidden: true, scrollPosition: 500 });
+    wrapper.setProps({
+      scrollPosition: 200,
+    });
+    expect(wrapper.emitted('update:isHidden')[0]).toEqual([false]);
   });
 });
