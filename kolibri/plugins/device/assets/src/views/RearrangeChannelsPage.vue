95c761a49f1d5a7acdbcd57227c75f58fb075ffc
Jonathan Boiser <jboiser@gmail.com>
2019-10-09 13:12:35 -0700

    Start RearrangeChannelPage

---
+++ kolibri/plugins/device/assets/src/views/RearrangeChannelsPage.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'RearrangeChannelsPage',
+    components: {
+
+    },
+    props: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    $trs: {
+      instructions: 'Control the order channels are displayed to learners and coaches',
+      successNotification: 'Channel order saved',
+      failureNotification: 'There was a problem reordering the channels',
+    }
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
