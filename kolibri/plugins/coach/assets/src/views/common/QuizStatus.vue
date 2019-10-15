955ac7424a410acde61694aa4c33da459d7a6048
Jacob Pierce <w.jacobpierce@gmail.com>
2019-10-15 06:03:15 -0700

    Apply cancel/confirm modals for open/close quiz

--- kolibri/plugins/coach/assets/src/views/common/QuizStatus.vue
+++ kolibri/plugins/coach/assets/src/views/common/QuizStatus.vue
@@ -2,38 +2,57 @@
 
   <KPageContainer>
     <KButton
-      v-if="!exam.active"
+      v-if="!exam.active && !exam.archive"
       :primary="true"
-      text="Open Quiz"
-      type="submit"
+      :text="$tr('openQuizLabel')"
+      type="button"
       style="margin-left: 0; margin-top: 1rem; margin-bottom: 0;"
+      @click="showConfirmationModal = true"
     />
     <KButton
-      v-if="exam.active"
-      text="Close Quiz"
+      v-if="exam.active && !exam.archive"
+      :text="$tr('closeQuizLabel')"
       type="submit"
       style="margin-left: 0; margin-top: 1rem; margin-bottom: 0;"
+      :appearanceOverrides="cancelStyleOverrides"
+      @click="showCancellationModal = true"
     />
-    <div>
-      <b>{{ coachString('recipientsLabel') }}</b>
-    </div>
-    <div>
-      <Recipients
-        slot="value"
-        :groupNames="groupNames"
-        :hasAssignments="exam.assignments.length > 0"
-      />
-    </div>
-    <div>
-      <b>Average score</b>
-    </div>
-    <div>
-      <Score :value="avgScore" />
-    </div>
-    <div>
-      <b>Question order</b>
-    </div>
-    <div>{{ orderDescriptionString }}</div>
+    <dl>
+      <dt>
+        <b>{{ coachString('recipientsLabel') }}</b>
+      </dt>
+      <dd>
+        <Recipients
+          slot="value"
+          :groupNames="groupNames"
+          :hasAssignments="exam.assignments.length > 0"
+        />
+      </dd>
+      <dt>
+        <b>Average score</b>
+      </dt>
+      <dd>
+        <Score :value="avgScore" />
+      </dd>
+      <dt>
+        <b>Question order</b>
+      </dt>
+      <dd>{{ orderDescriptionString }}</dd>
+    </dl>
+    <QuizStatusModal
+      v-if="showConfirmationModal"
+      :modalHeader="$tr('openQuizLabel')"
+      :modalDetail="$tr('openQuizModalDetail')"
+      @cancel="showConfirmationModal = false"
+      @submit="openQuiz"
+    />
+    <QuizStatusModal
+      v-if="showCancellationModal"
+      :modalHeader="$tr('closeQuizLabel')"
+      :modalDetail="$tr('closeQuizModalDetail')"
+      @cancel="showCancellationModal = false"
+      @submit="closeQuiz"
+    />
   </KPageContainer>
 
 </template>
@@ -43,10 +62,11 @@
   import { coachStringsMixin } from './commonCoachStrings';
   import Score from './Score';
   import Recipients from './Recipients';
+  import QuizStatusModal from './QuizStatusModal';
 
   export default {
     name: 'QuizStatus',
-    components: { Score, Recipients },
+    components: { Score, Recipients, QuizStatusModal },
     mixins: [coachStringsMixin],
     props: {
       groupNames: {
@@ -62,11 +82,48 @@
         required: false,
       },
     },
+    data() {
+      return {
+        showConfirmationModal: false,
+        showCancellationModal: false,
+      };
+    },
     computed: {
       orderDescriptionString() {
         return this.exam.learners_see_fixed_order
           ? this.coachString('orderFixedLabel')
           : this.coachString('orderRandomLabel');
+      },
+      cancelStyleOverrides() {
+        return {
+          color: '#fff',
+          'background-color': '#f00',
+          ':hover': { 'background-color': '#f66' },
+        };
+      },
+    },
+    $trs: {
+      openQuizLabel: {
+        message: 'Open quiz',
+        context:
+          "Label for a button that, when clicked, will 'open' a quiz - making it active so that Learners may take the quiz.",
+      },
+      openQuizModalDetail: {
+        message:
+          'Opening the quiz will make it visible to learners and they will be able to answer questions',
+        context:
+          "Text shown on a modal pop-up window when the user clicks the 'Open Quiz' button. This explains what will happen when the user confirms the action of opening the quiz.",
+      },
+      closeQuizLabel: {
+        message: 'Close quiz',
+        context:
+          "Label for a button that, when clicked, will 'close' a quiz. This makes the quiz inactive and Learners will no longer be able to give answers.",
+      },
+      closeQuizModalDetail: {
+        message:
+          'All learners will be given a final score and a quiz report. Unfinished questions will be counted as incorrect.',
+        context:
+          "Text shown on a modal pop-up window when the user clicks the 'Close Quiz' button. This explains what will happen when the modal window is confirmed.",
       },
     },
   };
@@ -75,8 +132,12 @@
 
 <style scoped lang='scss'>
 
-  div:nth-child(2n) {
+  dt {
     margin-top: 1.5rem;
+    margin-left: 0;
+  }
+  dd {
+    margin: 0;
   }
   div {
     font-size: 0.925rem;
---
+++ kolibri/plugins/coach/assets/src/views/common/QuizStatusModal.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <KModal
+    :title="modalHeader"
+    :submitText="coreString('continueAction')"
+    :cancelText="coreString('cancelAction')"
+    @cancel="$emit('cancel')"
+    @submit="$emit('submit')"
+  >
+    <div>{{ modalDetail }}</div>
+  </KModal>
+
+</template>
+
+<script>
+
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+
+  export default {
+    name: 'QuizStatusModal',
+    mixins: [commonCoreStrings],
+    props: {
+      modalHeader: {
+        type: String,
+        required: false,
+        default: '',
+      },
+      modalDetail: {
+        type: String,
+        required: false,
+        default: '',
+      },
+    },
+  };
+
+</script>
