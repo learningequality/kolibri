e3a3780d6867d34089d718f8eb001309c58edf20
Richard Tibbles <richard@learningequality.org>
2019-10-22 16:34:32 -0700

    Black constants.

--- kolibri/core/content/constants/schema_versions.py
+++ kolibri/core/content/constants/schema_versions.py
@@ -12,7 +12,14 @@ VERSION_3 = "3"
 
 # List of the content db schema versions, ordered from most recent to least recent.
 # When a new schema version is generated, it should be added here, at the top of the list.
-CONTENT_DB_SCHEMA_VERSIONS = [VERSION_3, VERSION_2, VERSION_1, NO_VERSION, V040BETA3, V020BETA1]
+CONTENT_DB_SCHEMA_VERSIONS = [
+    VERSION_3,
+    VERSION_2,
+    VERSION_1,
+    NO_VERSION,
+    V040BETA3,
+    V020BETA1,
+]
 
 # The latest compatible exported schema version for this version of Kolibri
 CONTENT_SCHEMA_VERSION = VERSION_3
