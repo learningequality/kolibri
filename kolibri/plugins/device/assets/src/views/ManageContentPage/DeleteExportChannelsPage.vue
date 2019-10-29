b1c3f8eaac413da8f89d92b06caa43f8ce177a25
Jonathan Boiser <jboiser@gmail.com>
2019-10-28 17:08:22 -0700

    Stub new components

---
+++ kolibri/plugins/device/assets/src/views/ManageContentPage/DeleteExportChannelsPage.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div>
+
+
+    <SelectDriveModal v-if="exportMode && showModal" />
+    <DeleteChannelModal v-if="deleteMode && showModal" />
+    <SelectionBottomBar />
+  </div>
+
+</template>
+
+
+<script>
+
+  import SelectionBottomBar from './SelectionBottomBar';
+  import DeleteChannelModal from './DeleteChannelModal';
+
+  // Data requirements
+  // List of channels on device
+  export default {
+    name: 'DeleteExportChannelsPage',
+    components: {
+      SelectionBottomBar,
+      DeleteChannelModal,
+    },
+    props: {
+      actionType: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        showModal: false,
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    $trs: {
+      deleteAppBarTitle: 'Delete channels',
+      exportAppBarTitle: 'Export channels',
+    }
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ kolibri/plugins/device/assets/src/views/ManageContentPage/SelectionBottomBar.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <div>
+    <BottomAppBar>
+      <div></div>
+    </BottomAppBar>
+  </div>
+
+</template>
+
+
+<script>
+
+  import BottomAppBar from 'kolibri.coreVue.components.BottomAppBar';
+
+  export default {
+    name: 'SelectionBottomBar',
+    components: {
+      BottomAppBar,
+    },
+    props: {
+      numObjects: {
+        type: String,
+      },
+      objectType: {
+        type: String,
+      },
+      fileSize: {
+        type: Number,
+      },
+      actionType: {
+        type: String,
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    $trs: {
+      zeroChannelsSelected: '0 channels selected',
+      someChannelsSelected: '{count} {count, plural, one {channel} other {channels}} selected, {bytesText}',
+      zeroResourcesSelected: '0 resources selected',
+      someResourcesSelected: '{count} {count, plural, one {resource} other {resources}} selected, {bytesText}',
+    }
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
