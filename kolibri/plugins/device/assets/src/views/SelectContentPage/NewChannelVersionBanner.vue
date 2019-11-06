689d466b69f0560db6b0852041712a8cfa49ff91
Jonathan Boiser <jboiser@gmail.com>
2019-11-06 10:56:38 -0800

    Stub NewChannelVersionBanner

---
+++ kolibri/plugins/device/assets/src/views/SelectContentPage/NewChannelVersionBanner.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div>
+    {{ $tr('versionAvailable', { version }) }}
+    <KButton :text="$tr('seeWhatsNewAction')" />
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'NewChannelVersionBanner',
+    components: {},
+    props: {
+      version: {
+        type: Number,
+      },
+    },
+    $trs: {
+      versionAvailable: 'Version {version} is available!',
+      seeWhatsNewAction: `See what's new`,
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
