1457d66077a1c3eddafef8c99a56eb4a92d13389
David Canas <david@learningequality.org>
2019-11-07 00:12:58 -0800

    Building pkg rather than just .app

--- platforms/desktop-app/.buildkite/build.sh
+++ platforms/desktop-app/.buildkite/build.sh
@@ -3,21 +3,19 @@ set -euo pipefail
 
 mkdir -p whl
 
-echo "--- Downloading whl file"
-
 # Allows for building directly from pipeline or trigger
 if [[ $BUILDKITE_TRIGGERED_FROM_BUILD_ID ]]
 then
-  echo "Downloading from triggered build"
+  echo "--- Downloading from triggered build"
   buildkite-agent artifact download 'dist/*.whl' whl/ --build ${BUILDKITE_TRIGGERED_FROM_BUILD_ID}
 else
-  echo "Downloading from pip"
+  echo "--- Downloading from pip"
   pip3 download -d ./whl kolibri
 fi
 
-echo "--- Environment Prep"
-echo "Unpack whl"
+echo "--- Preparing Environment"
 
+echo "Unpacking whl"
 # Duped from Android installer's makefile
 # Only unpacks kolibri, ignores useless c extensions to reduce size
 unzip -q "whl/kolibri*.whl" "kolibri/*" -x "kolibri/dist/cext*" -d src/
@@ -26,7 +24,7 @@ unzip -q "whl/kolibri*.whl" "kolibri/*" -x "kolibri/dist/cext*" -d src/
 echo "Downloading deps"
 pipenv sync --dev 
 
-echo "--- :mac: Build app"
+echo "--- Build .app"
 
 # Sets the environment variable needed for the build to find packages in from whl
 echo "PYTHONPATH=$PWD/src/kolibri/dist" > .env
@@ -34,5 +32,10 @@ echo "PYTHONPATH=$PWD/src/kolibri/dist" > .env
 # Save log for uploading, but filter out very verbose build logs. Errors should still output.
 pipenv run pew build | tee full_build_log.txt > /dev/null
 
+echo "--- :mac: Build .pkg"
+
+pipenv run pew package
+
+ls ./package
 
 # TODO upload directly to google cloud
