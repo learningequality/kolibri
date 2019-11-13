f39e428bdc083a6612d7ca3c2611a5343f32304b
Jonathan Boiser <jboiser@gmail.com>
2019-11-12 17:45:39 -0800

    Strings for multiple resource deletion

--- kolibri/plugins/device/assets/src/views/ManageContentPage/ManageChannelContentsPage/index.vue
+++ kolibri/plugins/device/assets/src/views/ManageContentPage/ManageChannelContentsPage/index.vue
@@ -24,6 +24,7 @@
 
     <DeleteResourcesModal
       v-if="shownModal === 'DELETE'"
+      :numberOfResources="resourceCounts.count"
       @cancel="closeModal"
       @submit="handleDeleteSubmit"
     />
