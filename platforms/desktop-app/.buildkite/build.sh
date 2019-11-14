3a97d1a584b7e1720c45cfd3bdd2bbbe39815a2c
David Canas <david@learningequality.org>
2019-11-13 19:13:02 -0800

    Using dist dir rather than whl for simplification

--- platforms/desktop-app/.buildkite/build.sh
+++ platforms/desktop-app/.buildkite/build.sh
@@ -3,18 +3,15 @@
 # Setting for debug purposes
 set -exuo pipefail
 
-  mkdir -p whl
 
 # Allows for building directly from pipeline or trigger
 if [[ $BUILDKITE_TRIGGERED_FROM_BUILD_ID ]]
 then
   echo "--- Downloading from triggered build"
-
-  # Would download directly to whl/, but buildkite always download the parent folders of the specified artifact
   buildkite-agent artifact download "dist/*.whl" . --build $BUILDKITE_TRIGGERED_FROM_BUILD_ID
-  mv dist whl
 else
   echo "--- Downloading from pip"
+  mkdir -p dist
   pip3 download -d ./whl kolibri
 fi
 
@@ -23,7 +20,7 @@ echo "--- Preparing Environment"
 echo "Unpacking whl"
 # Duped from Android installer's makefile
 # Only unpacks kolibri, ignores useless c extensions to reduce size
-unzip -q "whl/kolibri*.whl" "kolibri/*" -x "kolibri/dist/cext*" -d src/
+unzip -q "dist/kolibri*.whl" "kolibri/*" -x "kolibri/dist/cext*" -d src/
 
 
 echo "Downloading deps"
