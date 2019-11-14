078213dfc896f15821d6f40deb769ae72aa74258
Devon Rueckner <devon@learningequality.org>
2019-11-14 17:20:25 -0500

    updates to privacy text

--- kolibri/core/assets/src/views/PrivacyInfoModal.vue
+++ kolibri/core/assets/src/views/PrivacyInfoModal.vue
@@ -31,6 +31,7 @@
       <p>{{ $tr('kolibriOwnersP1') }}</p>
       <p>{{ $tr('kolibriOwnersP2') }}</p>
       <p>{{ $tr('kolibriOwnersP3') }}</p>
+      <p>{{ $tr('kolibriOwnersP4') }}</p>
     </section>
     <section v-if="!hideAboutSection">
       <h2>{{ $tr('kolibriAboutTitle') }}</h2>
@@ -45,6 +46,7 @@
       <p>{{ $tr('kolibriAboutP2') }}</p>
       <p>{{ $tr('kolibriAboutP3') }}</p>
       <p>{{ $tr('kolibriAboutP4') }}</p>
+      <p>{{ $tr('kolibriAboutP5') }}</p>
     </section>
   </KModal>
 
@@ -82,7 +84,7 @@
       kolibriUsersP1:
         'You should use Kolibri in compliance with all applicable laws. This may mean obtaining permission from your parent, guardian, or teacher.',
       kolibriUsersP2:
-        'When you use Kolibri, please be aware that your personal information may be visible to others, depending on how the software has been configured and how you access the software.',
+        'Be aware that your personal information may be visible to others, depending on how the software has been configured and how you access the software.',
       kolibriUsersP3:
         'Please contact your local Kolibri administrator to understand what personal information of yours might be stored, who it’s visible to, how to update or delete it, or if you believe your account has been compromised.',
       kolibriUsersP4: 'You should not:',
@@ -94,7 +96,7 @@
       kolibriUsersL4:
         'Intentionally interfere with or damage operation of Kolibri or any user’s enjoyment of it, by any means',
       kolibriUsersP5:
-        'When you use Kolibri as a logged-in user, information such as your name, username, the content that you view, and your performance on assessments may be made available to administrators and certain coaches in your facility.',
+        'When you use Kolibri as a logged-in user, information such as your name, username, age, birth year, identification number, the content that you view, and your performance on assessments may be made available to administrators and coaches in your facility. Your information may also be used by the developers of Kolibri and shared with content creators to help improve the software and content for different learner types and needs.',
       kolibriUsersP6:
         'When you use Kolibri as a guest, aggregate information about the content you and other guest users view may be available to administrators and certain coaches.',
       kolibriOwnersTitle: 'Administrators',
@@ -103,6 +105,8 @@
       kolibriOwnersP2:
         'You should also follow best information security practices for protecting your users’ data. This includes keeping the device physically secure, encrypting the hard drive, using strong and unique passwords, keeping the operating system up-to-date, and having a properly-configured firewall.',
       kolibriOwnersP3:
+        'If you choose to sync your facility data to the Kolibri Data Portal, you are granting access to your data to organization admins on Kolibri Data Portal. It will be uploaded to cloud servers operated by Learning Equality, who will also have access to this data.',
+      kolibriOwnersP4:
         'Please ensure that your users have way of getting in touch with you if they have concerns about their accounts.',
       kolibriAboutTitle: 'About Kolibri',
       kolibriAboutP1:
@@ -112,7 +116,9 @@
       kolibriAboutP3:
         'Unlike many online web services that are similarly accessed through a web browser, there are thousands of independent Kolibri installations around the world – including this one. Each Kolibri installation is managed and controlled by the owner of the device that it is installed on.',
       kolibriAboutP4:
-        'In order to improve the quality of Kolibri and the content on it, Learning Equality might collect anonymized usage information when Kolibri has access to the internet. This may include IP addresses associated with the server, device details such as the operating system and time zone, and aggregate statistics about the users and content.',
+        'It is also possible for administrators to sync Kolibri data to the cloud-based Kolibri Data Portal service. If this occurs, all facility data will be accessible to organization admins on Kolibri Data Portal. It will be uploaded to cloud servers operated by Learning Equality, who will also have access to this data.',
+      kolibriAboutP5:
+        'In order to improve the quality of Kolibri and the content on it, Learning Equality collects anonymized usage information when Kolibri has access to the internet. This includes IP addresses associated with the server, and device details such as the operating system and time zone. We also collect aggregate statistics including: number of users and facilities, birth year and gender distribution, and content popularity. We make every effort to avoid collecting personally identifying information about Kolibri users.',
       openIdH1: 'Signing in to third-party applications using Kolibri',
       openIdP1:
         'It is possible to use Kolibri to register or sign in to third-party applications. If you do this, the other application will have access to your Kolibri username, unique user ID, and full name.',
