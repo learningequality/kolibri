98466c04d4bb514e7be9f729350fc6f112432a21
Devon Rueckner <devon@learningequality.org>
2019-11-15 17:11:18 -0500

    updates from Lauren

--- kolibri/core/assets/src/views/PrivacyInfoModal.vue
+++ kolibri/core/assets/src/views/PrivacyInfoModal.vue
@@ -94,7 +94,7 @@
       kolibriUsersL3:
         'Remove, circumvent, disable, damage or otherwise interfere with security-related features of Kolibri',
       kolibriUsersL4:
-        'Intentionally interfere with or damage operation of Kolibri or any user’s enjoyment of it, by any means',
+        'Intentionally interfere with or damage the operation of Kolibri or any user’s enjoyment of it, by any means',
       kolibriUsersP5:
         'When you use Kolibri as a logged-in user, information such as your name, username, age, birth year, identification number, the content that you view, and your performance on assessments may be made available to administrators and coaches in your facility. Your information may also be used by the developers of Kolibri and shared with content creators to help improve the software and content for different learner types and needs.',
       kolibriUsersP6:
@@ -105,14 +105,14 @@
       kolibriOwnersP2:
         'You should also follow best information security practices for protecting your users’ data. This includes keeping the device physically secure, encrypting the hard drive, using strong and unique passwords, keeping the operating system up-to-date, and having a properly-configured firewall.',
       kolibriOwnersP3:
-        'If you choose to sync your facility data to the Kolibri Data Portal, you are granting access to your data to organization admins on Kolibri Data Portal. It will be uploaded to cloud servers operated by Learning Equality, who will also have access to this data.',
+        'If you choose to sync your facility data to the Kolibri Data Portal, you are granting access to your data to organization administrators on Kolibri Data Portal. It will be uploaded to cloud servers operated by Learning Equality, who will also have access to this data.',
       kolibriOwnersP4:
         'Please ensure that your users have way of getting in touch with you if they have concerns about their accounts.',
       kolibriAboutTitle: 'About Kolibri',
       kolibriAboutP1:
         'The Kolibri software is built by Foundation for Learning Equality, Inc. More information, including Kolibri’s Terms of Service and Privacy Policy, can be found at:',
       kolibriAboutP2:
-        'Kolibri is a software application that can be installed on a wide variety devices without needing a connection to the internet.',
+        'Kolibri is a software application that can be installed on a wide variety of devices without needing a connection to the internet.',
       kolibriAboutP3:
         'Unlike many online web services that are similarly accessed through a web browser, there are thousands of independent Kolibri installations around the world – including this one. Each Kolibri installation is managed and controlled by the owner of the device that it is installed on.',
       kolibriAboutP4:
