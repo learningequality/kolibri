learningequality/kolibri

commit 6f5d35a6e1756e84620a4f34633e417c4f77457b

stub out some additional strings

kolibri/plugins/device/assets/src/views/ManageContentPage/index.vue
@@ -30,26 +30,43 @@
         </KGridItem>
       </KGrid>
 
-      <p>
-        <a href="#">
-          {{ $tr('taskManagerLink') }}
-        </a>
-      </p>
+      <!-- Stubbed out in case we need it -->
+      <template v-if="false">
+        <h2>
+          {{ $tr('tasksHeader') }}
+        </h2>
+        <p>
+          <a href="#">
+            {{ $tr('taskManagerLink') }}
+          </a>
+        </p>
+        <h2>
+          {{ $tr('resourcesHeader') }}
+        </h2>
+      </template>
 
       <p v-if="!channelsAreInstalled">
         {{ $tr('emptyChannelListMessage') }}
       </p>
 
-      <div class="channels-list">
-        <ChannelPanel
-          v-for="channel in installedChannelsWithResources"
-          :key="channel.id"
-          :channel="channel"
-          :disabled="channelIsBeingDeleted(channel.id)"
-          @select_delete="deleteChannelId = channel.id"
-          @select_manage="handleSelectManage(channel.id)"
-        />
-      </div>
+      <CoreTable>
+
+        <transition-group slot="tbody" tag="tbody" name="list">
+          <tr
+            v-for="channel in installedChannelsWithResources"
+            :key="channel.id"
+          >
+            <td>
+              <ChannelPanel
+                :channel="channel"
+                :disabled="channelIsBeingDeleted(channel.id)"
+                @select_delete="deleteChannelId = channel.id"
+                @select_manage="handleSelectManage(channel.id)"
+              />
+            </td>
+          </tr>
+        </transition-group>
+      </CoreTable>
 
       <SelectTransferSourceModal :pageName="pageName" />
 
@@ -165,6 +182,8 @@
       deleteChannels: 'Delete channels',
       editChannelOrder: 'Edit channel order',
       emptyChannelListMessage: 'No channels installed',
+      tasksHeader: 'Tasks',
+      resourcesHeader: 'Resources',
       taskManagerLink: 'View task manager',
     },
   };

kolibri/plugins/device/assets/src/views/ManageTasksPage/index.vue
@@ -1,13 +1,24 @@
 <template>
 
   <div>
-    <p v-if="!loading && taskList.length === 0" class="no-tasks">
+
+    <!-- Stubbed out in case we need it -->
+    <template v-if="false">
+      <h1>
+        {{ $tr('tasksHeader') }}
+      </h1>
+      <p>
+        <!-- Stubbed out in case we need it -->
+        <a href="#">{{ $tr('backToChannelsAction') }}</a>
+      </p>
+    </template>
+    <p v-if="!loading && taskList.length === 0">
       {{ $tr('emptyTasksMessage') }}
     </p>
 
     <KButton
-      v-if="showClearAllButton"
-      :text="$tr('clearAllAction')"
+      v-if="showClearCompletedButton"
+      :text="$tr('clearCompletedAction')"
       @click="handleClickClearAll"
     />
     <div class="task-panels">
@@ -56,7 +67,7 @@
       sortedTaskList() {
         return reverse(this.taskList);
       },
-      showClearAllButton() {
+      showClearCompletedButton() {
         return some(this.taskList, taskIsClearable);
       },
     },
@@ -95,9 +106,11 @@
       },
     },
     $trs: {
+      backToChannelsAction: 'Back to channels',
+      tasksHeader: 'Tasks',
       appBarTitle: 'Task manager',
-      emptyTasksMessage: 'Tasks you initiate will appear here',
-      clearAllAction: 'Clear all',
+      emptyTasksMessage: 'There are no tasks to display',
+      clearCompletedAction: 'Clear completed',
     },
   };
 
@@ -109,12 +122,6 @@
   .task-panels {
     max-width: 780px;
     margin-top: 32px;
-  }
-
-  .no-tasks {
-    padding: 128px 0;
-    font-size: 24px;
-    text-align: center;
   }
 
 </style>

commit 85cbf80b25e481ef5b543e3028d79618044bf284

revert breakage in 6f5d35a6e1756e84620a4f34633e417c4f77457b

kolibri/plugins/device/assets/src/views/ManageContentPage/index.vue
@@ -49,24 +49,16 @@
         {{ $tr('emptyChannelListMessage') }}
       </p>
 
-      <CoreTable>
-
-        <transition-group slot="tbody" tag="tbody" name="list">
-          <tr
-            v-for="channel in installedChannelsWithResources"
-            :key="channel.id"
-          >
-            <td>
-              <ChannelPanel
-                :channel="channel"
-                :disabled="channelIsBeingDeleted(channel.id)"
-                @select_delete="deleteChannelId = channel.id"
-                @select_manage="handleSelectManage(channel.id)"
-              />
-            </td>
-          </tr>
-        </transition-group>
-      </CoreTable>
+      <div class="channels-list">
+        <ChannelPanel
+          v-for="channel in installedChannelsWithResources"
+          :key="channel.id"
+          :channel="channel"
+          :disabled="channelIsBeingDeleted(channel.id)"
+          @select_delete="deleteChannelId = channel.id"
+          @select_manage="handleSelectManage(channel.id)"
+        />
+      </div>
 
       <SelectTransferSourceModal :pageName="pageName" />
 