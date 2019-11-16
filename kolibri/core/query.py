59b6d4b651b915fb1e0b88e8a2e507e5348d278d
Richard Tibbles <richard@learningequality.org>
2019-11-16 13:51:43 +0200

    Move SQCount to query module to allow reuse.

---
+++ kolibri/core/query.py
@@ -0,0 +1,8 @@
+from django.db.models import IntegerField
+from django.db.models import Subquery
+
+
+class SQCount(Subquery):
+    # Include ALIAS at the end to support Postgres
+    template = "(SELECT COUNT(%(field)s) FROM (%(subquery)s) AS %(field)s__sum)"
+    output_field = IntegerField()
