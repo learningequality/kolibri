bc9b3056fdc3593d8e496fef285be6bd48e80687
Blaine Jester <blainesworld@gmail.com>
2019-11-21 16:29:05 -0800

    Help out Firefox for Ctrl+P

--- packages/kolibri-components/src/styles/trackMediaType.js
+++ packages/kolibri-components/src/styles/trackMediaType.js
@@ -2,7 +2,11 @@ import globalThemeState from './globalThemeState';
 
 export default function trackMediaType() {
   const listener = isPrint => {
-    globalThemeState.mediaType = isPrint ? 'print' : null;
+    const mediaType = isPrint ? 'print' : null;
+
+    if (globalThemeState.mediaType !== mediaType) {
+      globalThemeState.mediaType = mediaType;
+    }
   };
 
   if (window.matchMedia) {
@@ -17,4 +21,16 @@ export default function trackMediaType() {
   // Fallback
   window.addEventListener('beforeprint', () => listener(true));
   window.addEventListener('afterprint', () => listener(false));
+
+  // Get a headstart on Ctrl+P to print mainly for Firefox
+  document.addEventListener('keydown', e => {
+    // IE and Edge don't support `code`, this won't help IE anyway
+    if (!('code' in e)) {
+      return;
+    }
+
+    if (e.ctrlKey && !e.altKey && e.code === 'KeyP') {
+      listener(true);
+    }
+  });
 }
