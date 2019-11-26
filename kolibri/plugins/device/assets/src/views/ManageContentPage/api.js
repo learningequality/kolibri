65bd8f4d3fe2357fd1f4e222bda7577bd4935364
Jonathan Boiser <jboiser@gmail.com>
2019-11-26 12:10:24 -0800

    Do params validation

--- kolibri/plugins/device/assets/src/views/ManageContentPage/api.js
+++ kolibri/plugins/device/assets/src/views/ManageContentPage/api.js
@@ -1,8 +1,55 @@
 import find from 'lodash/find';
-import { TaskResource } from 'kolibri.resources';
+import { TaskResource, ChannelResource } from 'kolibri.resources';
+import { NetworkLocationResource } from '../../apiResources';
 
 // Tries to find an existing CHANNELDIFFSTATS task, or create a new one.
 const kolibriStudioUrl = 'https://studio.learningequality.org';
+
+// Based on URL parameters from NewChannelVersionPage, fetches the channel
+// to be updated. Returns errors if params are invalid.
+export function fetchCurrentChannel(params) {
+  const { channelId, driveId, addressId } = params;
+  return ChannelResource.fetchModel({ id: channelId })
+    .then(() => {
+      if (driveId) {
+        // Check if drive is attached
+        return TaskResource.localDrives().then(drives => {
+          const driveMatch = find(drives, { id: driveId });
+          if (!driveMatch) {
+            // Re-use same error if drive not found
+            return Promise.reject('CHANNEL_NOT_ON_DRIVE');
+          }
+
+          const channelMatch = find(driveMatch.metadata.channels, { id: channelId });
+
+          if (!channelMatch) {
+            return Promise.reject('CHANNEL_NOT_ON_DRIVE');
+          }
+
+          return {
+            ...channelMatch,
+          };
+        });
+      } else if (addressId) {
+        // Check if the Address ID has a model
+        return NetworkLocationResource.fetchModel({ id: addressId })
+          .then(networkLocation => {
+            console.log(networkLocation);
+          })
+          .catch(() => {
+            // Re-use error if location not found
+            return Promise.reject('CHANNEL_NOT_ON_PEER');
+          });
+      } else {
+        //
+      }
+    })
+    .catch(error => {
+      if (error.status.code === 404) {
+        return Promise.reject('CHANNEL_NOT_INSTALLED');
+      }
+    });
+}
 
 export function fetchOrTriggerChannelDiffStatsTask(params) {
   const { channelId, driveId, baseurl } = params;
