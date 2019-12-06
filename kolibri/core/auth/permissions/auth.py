4885a45ff884a136f7e40b1ceabfc098789fe482
Jacob Pierce <w.jacobpierce@gmail.com>
2019-12-06 12:38:23 -0800

    Black formatting

--- kolibri/core/auth/permissions/auth.py
+++ kolibri/core/auth/permissions/auth.py
@@ -139,8 +139,10 @@ class AllCanReadFacilityDataset(BasePermissions):
 class CoachesCanManageGroupsForTheirClasses(BasePermissions):
     def _user_is_coach_for_classroom(self, user, obj):
         # make sure the target object is a group and user is a coach for the group's classroom
-        return obj.kind == LEARNERGROUP or obj.kind == ADHOCLEARNERSGROUP and user.has_role_for_collection(
-            COACH, obj.parent
+        return (
+            obj.kind == LEARNERGROUP
+            or obj.kind == ADHOCLEARNERSGROUP
+            and user.has_role_for_collection(COACH, obj.parent)
         )
 
     def user_can_create_object(self, user, obj):
@@ -162,7 +164,11 @@ class CoachesCanManageGroupsForTheirClasses(BasePermissions):
 class CoachesCanManageMembershipsForTheirGroups(BasePermissions):
     def _user_is_coach_for_group(self, user, group):
         # make sure the target object is a group and user is a coach for the group
-        return group.kind == LEARNERGROUP or group.kind == ADHOCLEARNERSGROUP and user.has_role_for_collection(COACH, group)
+        return (
+            group.kind == LEARNERGROUP
+            or group.kind == ADHOCLEARNERSGROUP
+            and user.has_role_for_collection(COACH, group)
+        )
 
     def _user_should_be_able_to_manage(self, user, obj):
         # Requesting user must be a coach for the group
