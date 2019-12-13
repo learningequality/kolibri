5b7f037f5b7b286216eeb84a8906adc13b21bb85
Micah Fitch <micah@learningequality.org>
2019-12-13 10:29:14 -0600

    tox startup/shutdown test: don't worry about leftover processes that aren't listening

--- test/ensure_no_kolibris_running_on_port.sh
+++ test/ensure_no_kolibris_running_on_port.sh
@@ -4,7 +4,7 @@ echo "Checking for processes running on $1/tcp..."
 running_kolibris=`lsof -i -P -U | grep $1 | grep kolibri`
 echo $running_kolibris
 
-if [ `lsof -i -P -U | grep $1 | grep kolibri | grep -c '^'` -gt "0" ]; then
+if [ `lsof -i -P -U | grep $1 | grep kolibri | grep LISTEN | grep -c '^'` -gt "0" ]; then
     # looks like kolibri is running!
     exit 1
 else
