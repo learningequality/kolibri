6cfa078d504f544d0604262097edd2d288d075fb
Jonathan Boiser <jboiser@gmail.com>
2019-12-26 14:39:31 -0800

    Combine test cases

--- kolibri/core/assets/test/views/side-nav.spec.js
+++ kolibri/core/assets/test/views/side-nav.spec.js
@@ -35,16 +35,6 @@ function setUserKind(store, userKind) {
   });
 }
 
-const filterableUserKinds = [
-  UserKinds.ANONYMOUS,
-  UserKinds.LEARNER,
-  UserKinds.COACH,
-  UserKinds.ADMIN,
-  UserKinds.CAN_MANAGE_CONTENT,
-  UserKinds.SUPERUSER,
-];
-
-//
 describe('side nav component', () => {
   it('should be hidden if navShown is false', () => {
     const wrapper = createWrapper({ navShown: false });
@@ -62,96 +52,96 @@ describe('side nav component', () => {
     await wrapper.vm.$nextTick();
     expect(wrapper.contains(logoutSideNavEntry)).toBe(true);
   });
+
   describe('SideNav components are shown/hidden depending on role', () => {
     afterEach(() => {
       // Clean up the registered component
       navComponents.pop();
     });
-    async function testSideNavVisibility(kind, otherKind, shouldShow) {
-      const component = {
+    const testCases = [
+      [UserKinds.ADMIN, UserKinds.ADMIN, true],
+      [UserKinds.ADMIN, UserKinds.CAN_MANAGE_CONTENT, false],
+      [UserKinds.ADMIN, UserKinds.COACH, true],
+      [UserKinds.ADMIN, UserKinds.LEARNER, true],
+      [UserKinds.ANONYMOUS, UserKinds.ADMIN, false],
+      [UserKinds.ANONYMOUS, UserKinds.ANONYMOUS, true],
+      [UserKinds.ANONYMOUS, UserKinds.CAN_MANAGE_CONTENT, false],
+      [UserKinds.ANONYMOUS, UserKinds.COACH, false],
+      [UserKinds.ANONYMOUS, UserKinds.LEARNER, false],
+      [UserKinds.CAN_MANAGE_CONTENT, UserKinds.CAN_MANAGE_CONTENT, true],
+      [UserKinds.COACH, UserKinds.ADMIN, false],
+      [UserKinds.COACH, UserKinds.CAN_MANAGE_CONTENT, false],
+      [UserKinds.COACH, UserKinds.COACH, true],
+      [UserKinds.COACH, UserKinds.LEARNER, true],
+      [UserKinds.LEARNER, UserKinds.ADMIN, false],
+      [UserKinds.LEARNER, UserKinds.CAN_MANAGE_CONTENT, false],
+      [UserKinds.LEARNER, UserKinds.COACH, false],
+      [UserKinds.LEARNER, UserKinds.LEARNER, true],
+      [UserKinds.SUPERUSER, UserKinds.ADMIN, true],
+      [UserKinds.SUPERUSER, UserKinds.COACH, true],
+      [UserKinds.SUPERUSER, UserKinds.LEARNER, true],
+      [UserKinds.SUPERUSER, UserKinds.SUPERUSER, true],
+    ];
+
+    it.each(testCases)(
+      'if user is %s, then %s component should show (%s)',
+      async (kind, otherKind, shouldShow) => {
+        const component = {
+          name: `${otherKind}SideNavEntry`,
+          render() {
+            return '';
+          },
+          role: otherKind,
+        };
+        navComponents.register(component);
+        expect(navComponents).toHaveLength(1);
+        const wrapper = createWrapper();
+        setUserKind(wrapper.vm.$store, kind);
+        await wrapper.vm.$nextTick();
+        expect(wrapper.contains(component)).toBe(shouldShow);
+      }
+    );
+  });
+
+  describe('with multiple components', () => {
+    afterEach(() => {
+      // Clean up the registered components
+      navComponents.pop();
+      navComponents.pop();
+    });
+    // All user kinds that can be copresented in the side nav.
+    const testCases = [
+      [UserKinds.LEARNER, UserKinds.COACH],
+      [UserKinds.LEARNER, UserKinds.ADMIN],
+      [UserKinds.LEARNER, UserKinds.CAN_MANAGE_CONTENT],
+      [UserKinds.COACH, UserKinds.ADMIN],
+      [UserKinds.COACH, UserKinds.CAN_MANAGE_CONTENT],
+      [UserKinds.ADMIN, UserKinds.CAN_MANAGE_CONTENT],
+    ];
+    it.each(testCases)('%s component should above %s component', async (kind, otherKind) => {
+      const component1 = {
+        name: `${kind}SideNavEntry`,
+        render() {
+          return '';
+        },
+        role: kind,
+      };
+      const component2 = {
         name: `${otherKind}SideNavEntry`,
         render() {
           return '';
         },
         role: otherKind,
       };
-      navComponents.register(component);
-      expect(navComponents).toHaveLength(1);
+      navComponents.register(component2);
+      navComponents.register(component1);
+      expect(navComponents).toHaveLength(2);
       const wrapper = createWrapper();
-      setUserKind(wrapper.vm.$store, kind);
+      setUserKind(wrapper.vm.$store, UserKinds.SUPERUSER);
       await wrapper.vm.$nextTick();
-      expect(wrapper.contains(component)).toBe(shouldShow);
-    }
-    filterableUserKinds.forEach(kind => {
-      it(`should show ${kind} component if added and user is ${kind}`, async () => {
-        await testSideNavVisibility(kind, kind, true);
-      });
-    });
-    // These UserKinds have monotonically escalating privileges.
-    const escalatingPrivileges = [
-      UserKinds.LEARNER,
-      UserKinds.COACH,
-      UserKinds.ADMIN,
-      UserKinds.SUPERUSER,
-    ];
-    escalatingPrivileges.forEach(kind => {
-      // This is slightly duplicative of the tests above, but not harmful.
-      escalatingPrivileges.slice(0, escalatingPrivileges.indexOf(kind)).forEach(otherKind => {
-        it(`should show ${otherKind} component if added and user is ${kind}`, async () => {
-          await testSideNavVisibility(kind, otherKind, true);
-        });
-      });
-    });
-    filterableUserKinds.forEach(kind => {
-      filterableUserKinds.slice(filterableUserKinds.indexOf(kind) + 1, -1).forEach(otherKind => {
-        it(`should not show ${otherKind} component if added and user is ${kind}`, async () => {
-          await testSideNavVisibility(kind, otherKind, false);
-        });
-      });
-    });
-  });
-
-  describe('with multiple components', () => {
-    // All user kinds that can be copresented in the side nav.
-    const userKinds = [
-      UserKinds.LEARNER,
-      UserKinds.COACH,
-      UserKinds.ADMIN,
-      UserKinds.CAN_MANAGE_CONTENT,
-      UserKinds.SUPERUSER,
-    ];
-    userKinds.forEach(kind => {
-      userKinds.slice(userKinds.indexOf(kind) + 1, -1).forEach(otherKind => {
-        afterEach(() => {
-          // Clean up the registered components
-          navComponents.pop();
-          navComponents.pop();
-        });
-        it(`should show ${otherKind} component below ${kind} component`, () => {
-          const component1 = {
-            name: `${kind}SideNavEntry`,
-            render() {
-              return '';
-            },
-            role: kind,
-          };
-          const component2 = {
-            name: `${otherKind}SideNavEntry`,
-            render() {
-              return '';
-            },
-            role: otherKind,
-          };
-          navComponents.register(component2);
-          navComponents.register(component1);
-          expect(navComponents).toHaveLength(2);
-          const wrapper = createWrapper();
-          setUserKind(wrapper.vm.$store, UserKinds.SUPERUSER);
-          expect(wrapper.vm.menuOptions[0]).toBe(component1);
-          expect(wrapper.vm.menuOptions[1]).toBe(component2);
-        });
-      });
-    });
+      expect(wrapper.vm.menuOptions[0]).toBe(component1);
+      expect(wrapper.vm.menuOptions[1]).toBe(component2);
+    })
   });
   describe('and the priority flag', () => {
     afterEach(() => {
