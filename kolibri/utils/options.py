be6f0fc9d1634475b3ff59975e4bbdf851618d26
Richard Amodia <richard.amodia@mrpau.com>
2020-02-12 19:25:54 +0800

    use with to create the ini file.

--- kolibri/utils/options.py
+++ kolibri/utils/options.py
@@ -356,20 +356,18 @@ def update_options_file(section, key, value, KOLIBRI_HOME, ini_filename="options
 
 def generate_empy_options_file(options_path, options_data):
 
-    exclude_key = ["PICKLE_PROTOCOL"]
+    exclude_key = ["Python"]
 
     # Generate an options.ini file inside the KOLIBRI_HOME as default placeholder config
-    file = open(options_path, "w")
-    for k, v in options_data.items():
-        file.write("# [{}] \n".format(k))
-        loop_count = 0
-        for k_child, v_child in v.items():
-            loop_count += 1
-            if k_child not in exclude_key:
-                if loop_count != len(v):
+    with open(options_path, "w") as file:
+        for k, v in options_data.items():
+            if k in exclude_key:
+                return
+            file.write("# [{}] \n".format(k))
+            loop_count = 0
+            for k_child, v_child in v.items():
+                loop_count += 1
+                if k_child not in exclude_key:
                     file.write("# {} = {} \n".format(k_child, v_child))
-                else:
-                    file.write("# {} = {} \n\n".format(k_child, v_child))
-            elif loop_count == len(v):
-                file.write("\n")
-    file.close()
+                if loop_count == len(v):
+                    file.write("\n")
