abc30de68be4510f25cd6d553f39262facebab61
José L. Redrejo Rodríguez <jredrejo@debian.org>
2020-02-14 20:28:29 +0100

    Always regenarate cache.db when the app starts

--- kolibri/deployment/default/cache.py
+++ kolibri/deployment/default/cache.py
@@ -2,7 +2,6 @@ import copy
 import os
 import shutil
 import sys
-from sqlite3 import DatabaseError
 
 from diskcache import Cache
 
@@ -14,12 +13,11 @@ cache_options = OPTIONS["Cache"]
 pickle_protocol = OPTIONS["Python"]["PICKLE_PROTOCOL"]
 
 diskcache_location = os.path.join(KOLIBRI_HOME, "process_cache")
-try:
-    diskcache_cache = Cache(diskcache_location, disk_pickle_protocol=pickle_protocol)
-except DatabaseError:
-    shutil.rmtree(diskcache_location, ignore_errors=True)
-    os.mkdir(diskcache_location)
-    diskcache_cache = Cache(diskcache_location, disk_pickle_protocol=pickle_protocol)
+# always delete previous cache whenever the app starts, to avoid deadlocks:
+shutil.rmtree(diskcache_location, ignore_errors=True)
+os.mkdir(diskcache_location)
+
+diskcache_cache = Cache(diskcache_location, disk_pickle_protocol=pickle_protocol)
 diskcache_cache.close()
 # Default to LocMemCache, as it has the simplest configuration
 default_cache = {
