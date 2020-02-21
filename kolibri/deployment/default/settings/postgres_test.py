d29209ac30fb9ed8f27a97f9c38106cbf576f81e
Rafael Aguayo <ralphaguayo22@gmail.com>
2020-02-20 18:37:43 -0800

    add serializable isolation db connection for postgres

--- kolibri/deployment/default/settings/postgres_test.py
+++ kolibri/deployment/default/settings/postgres_test.py
@@ -7,6 +7,15 @@ from __future__ import unicode_literals
 
 from .test import *  # noqa
 
+try:
+    isolation_level = None
+    import psycopg2  # noqa
+
+    isolation_level = psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE
+except ImportError:
+    pass
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -14,5 +23,12 @@ DATABASES = {
         "PASSWORD": "",
         "NAME": os.environ.get("POSTGRES_DB") or "default",  # noqa
         "TEST": {"NAME": "travis_ci_default"},
-    }
+    },
+    "default_serializable": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "NAME": os.environ.get("POSTGRES_DB") or "default",  # noqa
+        "OPTIONS": {"isolation_level": isolation_level},
+    },
 }
