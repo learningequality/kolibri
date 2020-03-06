0dbc895b59d4ebb7ef0581055ab8b32feee97f0a
Jonathan Boiser <jboiser@gmail.com>
2020-03-06 15:22:24 -0800

    Add special empty state message when only user kind filter is applied
    Fixes #6571

---
+++ kolibri/plugins/facility/assets/src/views/UserPage/__test__/UserPage.spec.js
@@ -0,0 +1,53 @@
+import { mount } from '@vue/test-utils';
+import makeStore from '../../../../test/makeStore';
+import UserPage from '../index';
+
+function makeWrapper() {
+  const store = makeStore();
+  const wrapper = mount(UserPage, {
+    store,
+  });
+  return { wrapper, store };
+}
+
+// Intentionally made to not match any 'kind' filter
+const unicornUser = { id: '1', kind: 'UNICORN', username: 'unicorn', full_name: 'unicorn' };
+const coachUser = { id: '1', kind: 'coach', username: 'coach', full_name: 'coach' };
+
+describe('UserPage component', () => {
+  describe('message in empty states', () => {
+    function getUserTableEmptyMessage(wrapper) {
+      return wrapper.find({ name: 'UserTable' }).props().emptyMessage;
+    }
+
+    it('when there are no users', () => {
+      const { wrapper } = makeWrapper();
+      expect(getUserTableEmptyMessage(wrapper)).toEqual('No users exist');
+    });
+
+    const testCases = [
+      ['learner', 'No learners exist'],
+      ['coach', 'No coaches exist'],
+      ['superuser', 'No super admins exist'],
+      ['coach', 'No coaches exist'],
+      ['admin', 'No admins exist'],
+    ];
+
+    test.each(testCases)('when filter is %s', async (kind, expected) => {
+      const { wrapper, store } = makeWrapper();
+      store.state.userManagement.facilityUsers = [{ ...unicornUser }];
+      wrapper.setData({ roleFilter: { value: kind } });
+      await wrapper.vm.$nextTick();
+      expect(getUserTableEmptyMessage(wrapper)).toEqual(expected);
+    });
+
+    it('if a keyword filter is applied, the empty message is "no users match..."', async () => {
+      const { wrapper, store } = makeWrapper();
+      store.state.userManagement.facilityUsers = [{ ...coachUser }];
+      wrapper.setData({ roleFilter: { value: 'coach' } });
+      wrapper.find({ name: 'PaginatedListContainer' }).setData({ filterInput: 'coachy' });
+      await wrapper.vm.$nextTick();
+      expect(getUserTableEmptyMessage(wrapper)).toEqual("No users match the filter: 'coachy'");
+    });
+  });
+});
