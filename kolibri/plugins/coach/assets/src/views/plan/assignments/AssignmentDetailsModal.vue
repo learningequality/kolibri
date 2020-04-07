8b089460d64e457e66657453cd0cabdc43e8e68b
Jacob Pierce <w.jacobpierce@gmail.com>
2020-04-07 15:51:00 -0700

    Add KButtonGroup to AssignmentDetailsModal

--- kolibri/plugins/coach/assets/src/views/plan/assignments/AssignmentDetailsModal.vue
+++ kolibri/plugins/coach/assets/src/views/plan/assignments/AssignmentDetailsModal.vue
@@ -53,19 +53,23 @@
     </form>
 
     <BottomAppBar>
-      <KButton
-        :text="coreString('cancelAction')"
-        appearance="flat-button"
-        :primary="false"
-        :disabled="disabled"
-        @click="$emit('cancel')"
-      />
-      <KButton
-        :text="coreString('saveChangesAction')"
-        :primary="true"
-        :disabled="disabled"
-        @click="submitData"
-      />
+      <KButtonGroup>
+        <KButton
+          :text="coreString('cancelAction')"
+          appearance="flat-button"
+          :primary="false"
+          :disabled="disabled"
+          style="margin-top: 8px; margin-bottom: 8px;"
+          @click="$emit('cancel')"
+        />
+        <KButton
+          :text="coreString('saveChangesAction')"
+          :primary="true"
+          :disabled="disabled"
+          style="margin-top: 8px; margin-bottom: 8px;"
+          @click="submitData"
+        />
+      </KButtonGroup>
     </BottomAppBar>
   </div>
 
