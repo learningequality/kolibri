f93610c0ca082ae92f0330ee7907389d9077384a
Jonathan Boiser <jboiser@gmail.com>
2020-04-10 12:39:35 -0700

    Stub FacilitiesTaskPAge

---
+++ kolibri/plugins/device/assets/src/views/FacilitiesPage/FacilitiesTasksPage.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div>
+
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'FacilitiesTasksPage',
+    components: {},
+    props: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {},
+    $trs: {},
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
