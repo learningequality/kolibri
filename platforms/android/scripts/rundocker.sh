9e46b729ffe9c8d0a935cb0e7ff0086e4cf883bd
Jamie Alexandre <jamalex@gmail.com>
2020-04-26 12:41:35 -0700

    Make docker P4A cache arch-specific

--- platforms/android/scripts/rundocker.sh
+++ platforms/android/scripts/rundocker.sh
@@ -5,7 +5,7 @@ set -e
 CONTAINER_HOME=/home/kivy
 
 # Specifies the name of the docker volume used to store p4a cache
-P4A_CACHE=p4a_cache
+P4A_CACHE=p4a_cache_$ARCH
 PEW_CACHE=pew_cache
 
 CID_FILE=kolibri-android-app-container-id.cid.txt
