463c9c6b51092d2f2b3ec703f5c4444f18107411
Dylan McCall <dylan@endlessm.com>
2020-04-29 17:50:55 -0700

    Fall back to stripping country code for localized files

--- platforms/flatpak-app/src/kolibri_gnome/desktop_launcher/utils.py
+++ platforms/flatpak-app/src/kolibri_gnome/desktop_launcher/utils.py
@@ -7,6 +7,13 @@ def get_localized_file(file_path_template, file_path_fallback):
     language = get_current_language()
     file_path = file_path_template.format(language)
     if not os.path.exists(file_path):
+        # TODO: Removing the country code like this isn't the same behaviour as
+        #       gettext. Ideally our translated asset files should either be
+        #       generated or should use the same language codes as the provided
+        #       translations.
+        language_base = language.split('_', 1)[0]
+        file_path = file_path_template.format(language_base)
+    if not os.path.exists(file_path):
         file_path = file_path_fallback
     return file_path
 
