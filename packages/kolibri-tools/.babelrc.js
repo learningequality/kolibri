96bb2dc5e3e67bc62bc33f984b9ea9267f9c6191
Jacob Pierce <w.jacobpierce@gmail.com>
2020-05-22 16:48:05 -0700

    lint fixes

--- packages/kolibri-tools/.babelrc.js
+++ packages/kolibri-tools/.babelrc.js
@@ -9,9 +9,7 @@ module.exports = {
           },
         ],
       ],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-      ],
+      plugins: ['@babel/plugin-transform-runtime'],
     },
   },
 };
