1c496131e574c8626a40b424254cbef188198c7e
Richard Tibbles <richard@learningequality.org>
2020-05-26 18:17:27 -0700

    Add device app key and endpoint for validating the app key.

--- kolibri/plugins/app/api.py
+++ kolibri/plugins/app/api.py
@@ -1,14 +1,20 @@
-from rest_framework import permissions
-from rest_framework import viewsets
+from django.http import HttpResponseRedirect
+from django.utils.http import is_safe_url
+from django.utils.http import urlunquote
 from rest_framework.decorators import action
 from rest_framework.exceptions import APIException
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 
+from kolibri.core.device.models import DeviceAppKey
 from kolibri.plugins.app.utils import interface
 from kolibri.plugins.app.utils import LAUNCH_INTENT
 
 
-class FromSameDevicePermissions(permissions.BasePermission):
+class FromSameDevicePermissions(BasePermission):
     """
     Allow only users on the same device as the server
     """
@@ -17,7 +23,7 @@ class FromSameDevicePermissions(permissions.BasePermission):
         return request.META.get("REMOTE_ADDR") == "127.0.0.1"
 
 
-class AppCommandsViewset(viewsets.ViewSet):
+class AppCommandsViewset(ViewSet):
 
     permission_classes = (FromSameDevicePermissions,)
 
@@ -33,3 +39,34 @@ class AppCommandsViewset(viewsets.ViewSet):
                 )
             interface.launch_intent(filename, message)
             return Response()
+
+
+APP_KEY_COOKIE_NAME = "app_key_cookie"
+
+
+class InitializeAppView(APIView):
+    permission_classes = (FromSameDevicePermissions,)
+
+    def get(self, request, token):
+        app_key = DeviceAppKey.get_app_key()
+        if app_key != token:
+            raise PermissionDenied("You have provided an invalid token")
+        redirect_url = request.GET.get("next", "/")
+        # Copied and modified from https://github.com/django/django/blob/stable/1.11.x/django/views/i18n.py#L40
+        if (redirect_url or not request.is_ajax()) and not is_safe_url(
+            url=redirect_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            redirect_url = request.META.get("HTTP_REFERER")
+            if redirect_url:
+                redirect_url = urlunquote(redirect_url)  # HTTP_REFERER may be encoded.
+            if not is_safe_url(
+                url=redirect_url,
+                allowed_hosts={request.get_host()},
+                require_https=request.is_secure(),
+            ):
+                redirect_url = "/"
+        response = HttpResponseRedirect(redirect_url)
+        response.set_cookie(APP_KEY_COOKIE_NAME, app_key)
+        return response
--- kolibri/plugins/app/api_urls.py
+++ kolibri/plugins/app/api_urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import url
 from rest_framework import routers
 
 from .api import AppCommandsViewset
+from .api import InitializeAppView
 
 router = routers.DefaultRouter()
 
 router.register(r"appcommands", AppCommandsViewset, base_name="appcommands")
 
-urlpatterns = [url(r"^", include(router.urls))]
+urlpatterns = [
+    url(r"^", include(router.urls)),
+    url(r"^initialize/([0-9a-f]{32})", InitializeAppView.as_view(), name="initialize"),
+]
