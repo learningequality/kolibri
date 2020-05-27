f95533eee1e64d6cf63c75cc7899b28f605297cb
José L. Redrejo Rodríguez <jredrejo@debian.org>
2020-05-27 16:44:54 +0200

    Remove tooltip

--- kolibri/plugins/facility/assets/src/views/FacilityConfigPage/index.vue
+++ kolibri/plugins/facility/assets/src/views/FacilityConfigPage/index.vue
@@ -51,16 +51,12 @@
                 @change="toggleSetting('learner_can_login_with_no_password')"
               />
               <span>
-                <KTooltip reference="editpassword" :refs="$refs">
-                    {{ $tr('explainingWhyPasswordCanBeChanged') }}
-                </KTooltip>
                 <KCheckbox
                   :key="learner_can_edit_password"
                   :disabled="enableChangePassword"
                   :label="$tr('learnerCanEditPassword')"
                   :checked="settings['learner_can_edit_password']"
                   @change="toggleSetting('learner_can_edit_password')"
-                  ref="editpassword"
                   class="checkbox-password"
                 />
               </span>
@@ -206,7 +202,6 @@
       pageHeader: 'Facility settings',
       resetToDefaultSettings: 'Reset to defaults',
       documentTitle: 'Configure Facility',
-      explainingWhyPasswordCanBeChanged: "Learners only can change their password if it's required to login",
     },
   };
 
