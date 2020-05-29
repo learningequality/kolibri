dc40f5a9046ed28e0a41c714f637c3faf7dd71dc
Richard Tibbles <richard@learningequality.org>
2020-05-29 12:27:57 -0700

    Coerce keys when generating channel_stats rather than when accessing it.

--- kolibri/core/content/serializers.py
+++ kolibri/core/content/serializers.py
@@ -11,7 +11,6 @@ from kolibri.core.content.models import ContentNode
 from kolibri.core.content.models import File
 from kolibri.core.content.models import Language
 from kolibri.core.fields import create_timezonestamp
-from uuid import UUID
 
 
 class DynamicFieldsModelSerializer(serializers.ModelSerializer):
@@ -438,18 +437,7 @@ class ContentNodeGranularSerializer(serializers.ModelSerializer):
 
     @property
     def channel_stats(self):
-        channel_stats = self.context["channel_stats"]
-        if channel_stats is not None:
-            # Convert ContentNode IDs to hex strings if they are UUID
-            # objects
-            stats_convert_id = {}
-            for key in channel_stats.keys():
-                if not isinstance(key, UUID):
-                    return channel_stats
-                contentnode_id = key.hex
-                stats_convert_id[contentnode_id] = channel_stats[key]
-            channel_stats = stats_convert_id
-        return channel_stats
+        return self.context["channel_stats"]
 
     def get_total_resources(self, instance):
         # channel_stats is None for export
