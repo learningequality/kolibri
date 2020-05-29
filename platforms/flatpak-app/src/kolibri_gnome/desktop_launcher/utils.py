9b115dd3ad459c7021953e7d50b388a9a0429951
Dylan McCall <dylan@endlessm.com>
2020-05-29 13:20:46 -0700

    Handle unknown languages in get_localized_file
    Closes #2

--- platforms/flatpak-app/src/kolibri_gnome/desktop_launcher/utils.py
+++ platforms/flatpak-app/src/kolibri_gnome/desktop_launcher/utils.py
@@ -5,7 +5,12 @@ from ..globals import get_current_language
 
 def get_localized_file(file_path_template, file_path_fallback):
     language = get_current_language()
+
+    if not language:
+        return file_path_fallback
+
     file_path = file_path_template.format(language)
+
     if not os.path.exists(file_path):
         # TODO: Removing the country code like this isn't the same behaviour as
         #       gettext. Ideally our translated asset files should either be
@@ -13,6 +18,8 @@ def get_localized_file(file_path_template, file_path_fallback):
         #       translations.
         language_base = language.split("_", 1)[0]
         file_path = file_path_template.format(language_base)
+
     if not os.path.exists(file_path):
         file_path = file_path_fallback
+
     return file_path
