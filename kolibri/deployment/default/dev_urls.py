9faf17dce9a8321d52e5a28ddd41ee7c90a48c44
Jacob Pierce <w.jacobpierce@gmail.com>
2020-06-04 18:16:38 -0700

    lint

--- kolibri/deployment/default/dev_urls.py
+++ kolibri/deployment/default/dev_urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls import url
 from django.http.response import HttpResponseRedirect
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from kolibri.deployment.default.urls import urlpatterns
 
@@ -18,21 +18,31 @@ def webpack_redirect_view(request):
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Kolibri API",
-      default_version='v0',
-      description="Kolibri Swagger API",
-      license=openapi.License(name="MIT"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Kolibri API",
+        default_version="v0",
+        description="Kolibri Swagger API",
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = urlpatterns + [
     url(r"^__open-in-editor/", webpack_redirect_view),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^api_explorer/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^api_explorer/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
 
 if getattr(settings, "DEBUG_PANEL_ACTIVE", False):
