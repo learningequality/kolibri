841841a0bd20e07b28075f86475c706291d4dd4c
David Canas <david@learningequality.org>
2020-06-09 12:00:47 -0700

    Consolidating docker build and docker run to single step Fixes for a share-nothing environment. Should only be necessary on 0.10

--- .buildkite/build_whl.sh
+++ .buildkite/build_whl.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+echo '--- Building environment'
+make dockerenvbuild
+
+echo '--- Building installers'
 make dockerenvdist
 buildkite-agent artifact upload 'dist/*.whl'
 buildkite-agent artifact upload 'dist/*.zip'
