8ac1782b053e4d50844237a709cf2bdc822f65ba
David Canas <david@learningequality.org>
2020-06-10 13:17:37 -0700

    Including version info in metadata for pipeline ease-of-use

--- .buildkite/build_whl.sh
+++ .buildkite/build_whl.sh
@@ -7,3 +7,5 @@ buildkite-agent artifact upload 'dist/*.whl'
 buildkite-agent artifact upload 'dist/*.zip'
 buildkite-agent artifact upload 'dist/*.tar.gz'
 buildkite-agent artifact upload 'dist/*.pex'
+
+cd kolibri && buildkite-agent artifact upload 'VERSION' && cd -
