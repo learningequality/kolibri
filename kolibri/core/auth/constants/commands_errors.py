ac304353ec53ada900cd4f1cbbac11438643659d
Radina Matic <radina.matic@gmail.com>
2020-06-10 11:28:13 -0700

    Invert message and context

--- kolibri/core/auth/constants/commands_errors.py
+++ kolibri/core/auth/constants/commands_errors.py
@@ -16,21 +16,21 @@ FILE_WRITE_ERROR = 9
 MESSAGES = {
     UNEXPECTED_EXCEPTION: _("Unexpected exception [{}]: {}"),
     TOO_LONG: pgettext_lazy(
-        "Content of cell '{}' is too long",
         "Error report message when the allowed number of digits has been exceeded.",
+        "Content of cell '{}' is too long",
     ),
     INVALID: pgettext_lazy(
-        "Content of cell '{}' is not valid",
         "Error report message when the cell contains an improper data format.",
+        "Content of cell '{}' is not valid",
     ),
-    DUPLICATED_USERNAME: _("Duplicated Username"),
+    DUPLICATED_USERNAME: _("Duplicated username"),
     INVALID_USERNAME: _(
         "Username only can contain characters, numbers and underscores"
     ),
     REQUIRED_COLUMN: _("The column '{}' is required"),
     INVALID_HEADER: _("Mix of valid and/or invalid header labels found in first row"),
     NO_FACILITY: _(
-        "No default facility exists, please make sure to provision this device before running this command"
+        "No default facility exists. Make sure to set up a facility on the device before running this command"
     ),
     FILE_READ_ERROR: _("Error trying to read csv file: {}"),
     FILE_WRITE_ERROR: _("Error trying to write csv file: {}"),
