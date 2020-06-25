19ee5ad5d1392cd7215aa03491dd41c0892a3c2d
Jonathan Boiser <jboiser@gmail.com>
2020-06-24 18:42:19 -0700

    Fix flake8 error

--- kolibri/core/auth/constants/collection_kinds.py
+++ kolibri/core/auth/constants/collection_kinds.py
@@ -3,8 +3,6 @@ This module contains constants representing the kinds of Collections.
 """
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext_lazy as _
-
 FACILITY = "facility"
 CLASSROOM = "classroom"
 LEARNERGROUP = "learnergroup"
