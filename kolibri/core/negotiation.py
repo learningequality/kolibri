9933818548c3e2aaff024a38964bc1530bff710c
Blaine Jester <blainesworld@gmail.com>
2020-06-30 14:53:17 -0700

    Allow default parsing

--- kolibri/core/negotiation.py
+++ kolibri/core/negotiation.py
@@ -1,22 +1,8 @@
 from rest_framework.negotiation import DefaultContentNegotiation
-from rest_framework.parsers import JSONParser
-from rest_framework.parsers import MultiPartParser
 from rest_framework.renderers import JSONRenderer
 
 
 class LimitContentNegotiation(DefaultContentNegotiation):
-    def select_parser(self, request, parsers):
-        """
-        Always return JSONParser unless a 'multipart/form-data' content type is sent
-        or this is for morango
-        """
-
-        if request.path.startswith("/api/morango"):
-            return super(LimitContentNegotiation, self).select_parser(request, parsers)
-        if "multipart" in request.content_type:
-            return MultiPartParser()
-        return JSONParser()
-
     def select_renderer(self, request, renderers, format_suffix=None):
         """
         Always return JSONRenderer unless for morango
