434cc5e9c1ccf3f8c5736a692d2664c1af4538c1
Blaine Jester <blainesworld@gmail.com>
2020-06-30 14:27:59 -0700

    Update content negotiation for morango operation

--- kolibri/core/negotiation.py
+++ kolibri/core/negotiation.py
@@ -1,21 +1,29 @@
-from rest_framework.negotiation import BaseContentNegotiation
+from rest_framework.negotiation import DefaultContentNegotiation
 from rest_framework.parsers import JSONParser
 from rest_framework.parsers import MultiPartParser
 from rest_framework.renderers import JSONRenderer
 
 
-class LimitContentNegotiation(BaseContentNegotiation):
+class LimitContentNegotiation(DefaultContentNegotiation):
     def select_parser(self, request, parsers):
         """
         Always return JSONParser unless a 'multipart/form-data' content type is sent
+        or this is for morango
         """
+
+        if request.path.startswith("/api/morango"):
+            return super(LimitContentNegotiation, self).select_parser(request, parsers)
         if "multipart" in request.content_type:
             return MultiPartParser()
         return JSONParser()
 
-    def select_renderer(self, request, renderers, format_suffix):
+    def select_renderer(self, request, renderers, format_suffix=None):
         """
-        Always return JSONRenderer
+        Always return JSONRenderer unless for morango
         """
+        if request.path.startswith("/api/morango"):
+            return super(LimitContentNegotiation, self).select_renderer(
+                request, renderers, format_suffix=None
+            )
         renderer = JSONRenderer()
         return (renderer, renderer.media_type)
