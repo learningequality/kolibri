94e282a3019179b827bf01118f272e0bc38b820c
lyw07 <lywang07@gmail.com>
2020-07-17 10:54:40 -0700

    use pip install --cache-dir to install packages. remove download step

--- build_tools/install_cexts.py
+++ build_tools/install_cexts.py
@@ -6,16 +6,12 @@ It is required to have pip version greater than 19.3.1 to run this script.
 Usage:
 > python build_tools/install_cexts.py --file "requirements/cext.txt" --cache-path "/cext_cache"
 
-It reads the package name and version from requirements/cext.txt file, downloads
-and installs the package and its dependency from PyPi for platforms such as
-manylinux, Windows and Piwheels for platforms such as ARM Linux. Please check
-the description of the function `parse_package_page` to see the platforms we skip
+It reads the package name and version from requirements/cext.txt file and
+installs the package and its dependencies using `pip install` with cache_path as
+the cache directory. It installs from PyPi for platforms such as manylinux,
+Windows and Piwheels for platforms such as ARM Linux. Please check the
+description of the function `parse_package_page` to see the platforms we skip
 downloading.
-
-The module downloads the wheel files of the package and its dependencies using
-`pip download` to the cache directory, copies the files from the cache directory
-to `kolibri/dist/cext` and runs `pip install` to install these wheel files one
-by one.
 
 When Kolibri starts, the function `prepend_cext_path` in `env.py` will calculate
 the c extension path based on system information and add it to sys.path so
@@ -45,7 +41,7 @@ PYPI_DOWNLOAD = "https://pypi.python.org/simple/"
 PIWHEEL_DOWNLOAD = "https://www.piwheels.org/simple/"
 
 
-def get_path_with_arch(platform, abi, implementation, python_version):
+def get_path_with_arch(platform, path, abi, implementation, python_version):
     """
     Calculate package path according to the platform.
     """
@@ -58,31 +54,41 @@ def get_path_with_arch(platform, abi, implementation, python_version):
 
     # Windows 32-bit's machine name is x86.
     if platform_split[0] == "win32":
-        return os.path.join("Windows", "x86")
+        return os.path.join(path, "Windows", "x86")
     # Windows 64-bit
     elif platform_split[0] == "win":
-        return os.path.join("Windows", "AMD64")
+        return os.path.join(path, "Windows", "AMD64")
 
     # Prior to CPython 3.3, there were two ABI-incompatible ways of building CPython
     # There could be abi tag 'm' for narrow-unicode and abi tag 'mu' for wide-unicode
     if implementation == "cp" and int(python_version) < 33:
-        return os.path.join(platform_split[0], abi, platform_split[1])
+        return os.path.join(path, platform_split[0], abi, platform_split[1])
 
-    return os.path.join(platform_split[0], platform_split[1])
+    return os.path.join(path, platform_split[0], platform_split[1])
 
 
-def download_package(
-    path, platform, version, implementation, abi, name, pk_version, index_url
+def run_pip_install(
+    path,
+    platform,
+    version,
+    implementation,
+    abi,
+    name,
+    pk_version,
+    index_url,
+    cache_path,
 ):
     """
-    Download the package according to platform, python version, implementation and abi.
+    Install the package and its dependencies according to platform,
+    python version, implementation and abi using `pip install` with cache_path as
+    the cache directory.
     """
     return_code = subprocess.call(
         [
             "pip",
-            "download",
+            "install",
             "-q",
-            "-d",
+            "-t",
             path,
             "--platform",
             platform,
@@ -94,6 +100,8 @@ def download_package(
             abi,
             "-i",
             index_url,
+            "--cache-dir",
+            cache_path,
             "--only-binary=:all:",
             "{}=={}".format(name, pk_version),
         ]
@@ -102,62 +110,9 @@ def download_package(
     return return_code
 
 
-def install_package_by_wheel(path):
+def install_package(package_name, package_version, index_url, info, cache_path):
     """
-    Install the package using the downloaded wheel files.
-    """
-    files = os.listdir(path)
-    for file in files:
-        # When the abi tag is abi3, the package has been installed, and a dist-info
-        # folder has been generated. Skip the installed package and remove the
-        # dist-info folder.
-        if os.path.isdir(os.path.join(path, file)):
-            if file.endswith(".dist-info"):
-                shutil.rmtree(os.path.join(path, file))
-            continue
-
-        # If the file is py2, py3 compatible, install it into kolibri/dist/cext
-        # instead of specific platform paths to reduce the size of installer
-        if "py2.py3-none-any" in file:
-            return_code = subprocess.call(
-                [
-                    "pip",
-                    "install",
-                    "-q",
-                    "-U",
-                    "-t",
-                    DIST_CEXT,
-                    os.path.join(path, file),
-                    "--no-deps",
-                ]
-            )
-        else:
-            return_code = subprocess.call(
-                [
-                    "pip",
-                    "install",
-                    "-q",
-                    "-t",
-                    path,
-                    os.path.join(path, file),
-                    "--no-deps",
-                ]
-            )
-
-        if return_code == 1:
-            sys.exit("\nInstallation failed for package {}.\n".format(file))
-        else:
-            # Clean up the whl file and dist-info folder
-            os.remove(os.path.join(path, file))
-            shutil.rmtree(
-                os.path.join(path, "-".join(file.split("-", 2)[:2]) + ".dist-info"),
-                ignore_errors=True,
-            )
-
-
-def download_and_install(package_name, package_version, index_url, info, cache_path):
-    """
-    Download and install packages based on the information we gather from the index_url page
+    Install packages based on the information we gather from the index_url page
     """
     for item in info:
         platform = item["platform"]
@@ -167,14 +122,15 @@ def download_and_install(package_name, package_version, index_url, info, cache_p
         filename = "-".join([package_name, package_version, abi, platform])
 
         # Calculate the path that the package will be installed into
-        version_path = os.path.join(cache_path, implementation + python_version)
-        package_path = get_path_with_arch(platform, abi, implementation, python_version)
-        volume_package_path = os.path.join(version_path, package_path)
+        version_path = os.path.join(DIST_CEXT, implementation + python_version)
+        package_path = get_path_with_arch(
+            platform, version_path, abi, implementation, python_version
+        )
 
-        print("Downloading package {}...".format(filename))
-        # Download the package to the cache folder
-        download_return = download_package(
-            volume_package_path,
+        print("Installing package {}...".format(filename))
+        # Install the package using pip with cache_path as the cache directory
+        install_return = run_pip_install(
+            package_path,
             platform,
             python_version,
             implementation,
@@ -182,29 +138,23 @@ def download_and_install(package_name, package_version, index_url, info, cache_p
             package_name,
             package_version,
             index_url,
+            cache_path,
         )
 
-        # Successfully download package
-        if download_return == 0:
-            # Copy the files downloaded in cache_path to DIST_CEXT
-            install_path = volume_package_path.replace(cache_path, DIST_CEXT)
-            shutil.copytree(volume_package_path, install_path)
-            print("Installing package {}...".format(filename))
-            install_package_by_wheel(install_path)
-        # Download failed
+        # Ignore Piwheels installation failure because the website is not always stable
+        if install_return == 1 and index_url == PYPI_DOWNLOAD:
+            sys.exit("\nInstallation failed for package {}.\n".format(filename))
         else:
-            # see https://github.com/learningequality/kolibri/issues/4656
-            print("\nDownload failed for package {}.\n".format(filename))
-
-            # We still need to have the program exit with error
-            # if something wrong with PyPi download.
-            if index_url == PYPI_DOWNLOAD:
-                sys.exit(1)
+            # Clean up .dist-info folders
+            dist_info_folders = os.listdir(package_path)
+            for folder in dist_info_folders:
+                if folder.endswith(".dist-info"):
+                    shutil.rmtree(os.path.join(package_path, folder))
 
 
 def parse_package_page(files, pk_version, index_url, cache_path):
     """
-    Parse the PYPI and Piwheels link for the package information.
+    Parse the PYPI and Piwheels links for the package information.
     We are not going to install the packages if they are:
         * not a whl file
         * not the version specified in requirements.txt
@@ -261,10 +211,10 @@ def parse_package_page(files, pk_version, index_url, cache_path):
             }
             result.append(info)
 
-    download_and_install(package_name, pk_version, index_url, result, cache_path)
+    install_package(package_name, pk_version, index_url, result, cache_path)
 
 
-def install(name, pk_version, cache_path):
+def parse_pypi_and_piwheels(name, pk_version, cache_path):
     """
     Start installing from the pypi and piwheels pages of the package.
     """
@@ -276,12 +226,6 @@ def install(name, pk_version, cache_path):
             parse_package_page(files, pk_version, link, cache_path)
         else:
             sys.exit("\nUnable to find package {} on {}.\n".format(name, link))
-
-    # Clean up .dist-info folders
-    files = os.listdir(DIST_CEXT)
-    for file in files:
-        if file.endswith(".dist-info"):
-            shutil.rmtree(os.path.join(DIST_CEXT, file))
 
 
 def check_cache_path_writable(cache_path):
@@ -325,8 +269,11 @@ def parse_requirements(args):
         for line in f:
             char_list = line.split("==")
             if len(char_list) == 2:
-                # Install package according to its name and version
-                install(char_list[0].strip(), char_list[1].strip(), cache_path)
+                # Parse PyPi and Piwheels pages to install package according to
+                # its name and version
+                parse_pypi_and_piwheels(
+                    char_list[0].strip(), char_list[1].strip(), cache_path
+                )
             else:
                 sys.exit(
                     "\nName format in cext.txt is incorrect. Should be 'packageName==packageVersion'.\n"
