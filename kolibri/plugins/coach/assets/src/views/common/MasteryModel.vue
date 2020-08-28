6073ff284ba3059743ae2f96372a752cb09e2251
Jonathan Boiser <jboiser@gmail.com>
2020-08-28 12:03:54 -0700

    Move mastery model constants to core/constants

--- kolibri/plugins/coach/assets/src/views/common/MasteryModel.vue
+++ kolibri/plugins/coach/assets/src/views/common/MasteryModel.vue
@@ -7,31 +7,16 @@
 
 <script>
 
-  // from le-utils/le_utils/constants/exercises.py
-  const DO_ALL = 'do_all';
-  const NUM_CORRECT_IN_A_ROW_10 = 'num_correct_in_a_row_10';
-  const NUM_CORRECT_IN_A_ROW_2 = 'num_correct_in_a_row_2';
-  const NUM_CORRECT_IN_A_ROW_3 = 'num_correct_in_a_row_3';
-  const NUM_CORRECT_IN_A_ROW_5 = 'num_correct_in_a_row_5';
-  const M_OF_N = 'm_of_n';
-
-  const models = [
-    DO_ALL,
-    NUM_CORRECT_IN_A_ROW_10,
-    NUM_CORRECT_IN_A_ROW_2,
-    NUM_CORRECT_IN_A_ROW_3,
-    NUM_CORRECT_IN_A_ROW_5,
-    M_OF_N,
-  ];
+  import { MasteryModelTypes } from 'kolibri.coreVue.vuex.constants';
 
   export default {
     name: 'MasteryModel',
     props: {
       model: {
         type: String,
-        default: M_OF_N,
+        default: MasteryModelTypes.m_of_n,
         validator(value) {
-          return models.includes(value);
+          return Object.values(MasteryModelTypes).includes(value);
         },
       },
       m: {
@@ -45,7 +30,7 @@
     },
     computed: {
       text() {
-        if (this.model === M_OF_N) {
+        if (this.model === MasteryModelTypes.m_of_n) {
           if (this.m === this.n) {
             if (this.m === 1) {
               return this.$tr('one');
@@ -53,15 +38,15 @@
             return this.$tr('streak', { m: this.m });
           }
           return this.$tr('mOfN', { M: this.m, N: this.n });
-        } else if (this.model === NUM_CORRECT_IN_A_ROW_10) {
+        } else if (this.model === MasteryModelTypes.num_correct_in_a_row_10) {
           return this.$tr('streak', { count: 10 });
-        } else if (this.model === NUM_CORRECT_IN_A_ROW_2) {
+        } else if (this.model === MasteryModelTypes.num_correct_in_a_row_2) {
           return this.$tr('streak', { count: 2 });
-        } else if (this.model === NUM_CORRECT_IN_A_ROW_3) {
+        } else if (this.model === MasteryModelTypes.num_correct_in_a_row_3) {
           return this.$tr('streak', { count: 3 });
-        } else if (this.model === NUM_CORRECT_IN_A_ROW_5) {
+        } else if (this.model === MasteryModelTypes.num_correct_in_a_row_5) {
           return this.$tr('streak', { count: 5 });
-        } else if (this.model === DO_ALL) {
+        } else if (this.model === MasteryModelTypes.do_all) {
           return this.$tr('doAll');
         } else {
           return this.$tr('unknown');
@@ -70,6 +55,7 @@
     },
     $trs: {
       one: 'Get one question correct',
+      // TODO add pluralized versions of 'streak', and 'mofN'
       streak: 'Get {count, number, integer} questions in a row correct',
       mOfN: 'Get {M, number, integer} of the last {N, number, integer} questions correct',
       doAll: 'Get every question correct',
