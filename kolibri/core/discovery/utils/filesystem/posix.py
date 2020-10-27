839172d058416d7fa111d68168acf90e24b420d3
Dylan McCall <dylan@endlessos.org>
2020-10-27 13:43:29 -0700

    Ignore drives where shutil.disk_usage fails

--- kolibri/core/discovery/utils/filesystem/posix.py
+++ kolibri/core/discovery/utils/filesystem/posix.py
@@ -116,7 +116,12 @@ def get_drive_list():
             continue
 
         # attempt to get some additional metadata about the drive
-        usage = _get_drive_usage(path)
+        try:
+            usage = _get_drive_usage(path)
+        except OSError:
+            # skip if we don't have access to get drive usage
+            continue
+
         dbus_drive_info = _try_to_get_drive_info_from_dbus(drive["device"])
         diskutil_info = _try_to_get_drive_info_from_diskutil(drive["device"])
 
