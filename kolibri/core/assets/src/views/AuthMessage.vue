53e1736a2e915828a4b01d48db3563cbc6872f95
Richard Tibbles <richard@learningequality.org>
2020-12-11 13:45:22 -0800

    Make AuthMessage add queryparam after hash. Fix tests.

--- kolibri/core/assets/src/views/AuthMessage.vue
+++ kolibri/core/assets/src/views/AuthMessage.vue
@@ -84,7 +84,7 @@
           return '/';
         } else {
           const currentURL = window.encodeURIComponent(window.location.href);
-          return `${this.userPluginUrl()}?next=${currentURL}`;
+          return `${this.userPluginUrl()}#/signin?next=${currentURL}`;
         }
       },
     },
