215bfe737c4193fd319bb7bc535bc16da277b498
Richard Tibbles <richard@learningequality.org>
2021-04-02 11:46:20 -0700

    Add test of unicode behaviour for log export.

--- kolibri/core/logger/test/test_api.py
+++ kolibri/core/logger/test/test_api.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Tests that ensure the correct items are returned from api calls.
 Also tests whether the users with permissions can create logs.
@@ -556,6 +557,32 @@ class ContentSummaryLogCSVExportTestCase(APITestCase):
             self.assertEqual(len(results[0]), len(row))
         self.assertEqual(len(results[1:]), expected_count)
 
+    def test_csv_download_unicode_username(self):
+        user = FacilityUserFactory.create(
+            facility=self.facility, username="كوليبري", full_name="كوليبري"
+        )
+        for _ in range(3):
+            ContentSummaryLogFactory.create(
+                user=user,
+                content_id=uuid.uuid4().hex,
+                channel_id="6199dde695db4ee4ab392222d5af1e5c",
+            )
+
+        expected_count = ContentSummaryLog.objects.count()
+        _, filepath = tempfile.mkstemp(suffix=".csv")
+        call_command(
+            "exportlogs", log_type="summary", output_file=filepath, overwrite=True
+        )
+        if sys.version_info[0] < 3:
+            csv_file = open(filepath, "rb")
+        else:
+            csv_file = open(filepath, "r", newline="")
+        with csv_file as f:
+            results = list(csv.reader(f))
+        for row in results[1:]:
+            self.assertEqual(len(results[0]), len(row))
+        self.assertEqual(len(results[1:]), expected_count)
+
 
 class ContentSessionLogCSVExportTestCase(APITestCase):
 
@@ -598,6 +625,32 @@ class ContentSessionLogCSVExportTestCase(APITestCase):
         expected_count = ContentSessionLog.objects.count()
         ContentNode.objects.all().delete()
         ChannelMetadata.objects.all().delete()
+        _, filepath = tempfile.mkstemp(suffix=".csv")
+        call_command(
+            "exportlogs", log_type="session", output_file=filepath, overwrite=True
+        )
+        if sys.version_info[0] < 3:
+            csv_file = open(filepath, "rb")
+        else:
+            csv_file = open(filepath, "r", newline="")
+        with csv_file as f:
+            results = list(csv.reader(f))
+        for row in results[1:]:
+            self.assertEqual(len(results[0]), len(row))
+        self.assertEqual(len(results[1:]), expected_count)
+
+    def test_csv_download_unicode_username(self):
+        user = FacilityUserFactory.create(
+            facility=self.facility, username="كوليبري", full_name="كوليبري"
+        )
+        for _ in range(3):
+            ContentSessionLogFactory.create(
+                user=user,
+                content_id=uuid.uuid4().hex,
+                channel_id="6199dde695db4ee4ab392222d5af1e5c",
+            )
+
+        expected_count = ContentSessionLog.objects.count()
         _, filepath = tempfile.mkstemp(suffix=".csv")
         call_command(
             "exportlogs", log_type="session", output_file=filepath, overwrite=True
