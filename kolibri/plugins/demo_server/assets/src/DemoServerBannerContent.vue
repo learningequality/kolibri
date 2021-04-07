4d45a1eeafe47bfe3bfd2be5c0cd375d77c59ac2
Jonathan Boiser <jboiser@gmail.com>
2021-04-07 14:29:41 -0700

    Remove admin credentials from demo banner

--- kolibri/plugins/demo_server/assets/src/DemoServerBannerContent.vue
+++ kolibri/plugins/demo_server/assets/src/DemoServerBannerContent.vue
@@ -9,7 +9,6 @@
     <ul>
       <li>{{ $tr('demoServerL1', { user: 'learnerdemo', pass: 'pass' } ) }}</li>
       <li>{{ $tr('demoServerL2', { user: 'coachdemo', pass: 'pass' } ) }}</li>
-      <li>{{ $tr('demoServerL3', { user: 'admindemo', pass: 'pass' } ) }}</li>
     </ul>
     <p>{{ $tr('demoServerP2') }}</p>
     <p>{{ $tr('demoServerP3') }}</p>
@@ -50,7 +49,6 @@
       demoServerP1: 'Explore any of the three primary user types:',
       demoServerL1: 'Learner ({user}/{pass} or access as a guest)',
       demoServerL2: 'Coach ({user}/{pass})',
-      demoServerL3: 'Admin ({user}/{pass})',
       demoServerP2:
         'This online version of Kolibri is intended for demonstration purposes only. Users and data will be periodically deleted. The demo shows features of the latest Kolibri version, and all resources found are samples.',
       demoServerP3:
