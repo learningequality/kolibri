ac89d38a348af9c8abccbc4b4dcdafe8c8e557fa
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2021-04-26 00:01:07 +0530

    tests: ensure jobs persist on server restart

--- kolibri/utils/tests/test_server.py
+++ kolibri/utils/tests/test_server.py
@@ -5,14 +5,23 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from unittest import TestCase
+import os
+import tempfile
 
+import pytest
 from mock import patch
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
+from kolibri.core.tasks.job import Job
+from kolibri.core.tasks.queue import Queue
+from kolibri.core.tasks.storage import Storage
+from kolibri.core.tasks.scheduler import Scheduler
+from kolibri.core.tasks.worker import Worker
 from kolibri.utils import server
 
 
-class TestServerInstallation(TestCase):
+class TestServerInstallation(object):
     @patch("sys.argv", ["kolibri-0.9.3.pex", "start"])
     def test_pex(self):
         install_type = server.installation_type()
@@ -53,3 +62,121 @@ class TestServerInstallation(TestCase):
     def test_whl(self):
         install_type = server.installation_type()
         assert install_type == "whl"
+
+
+@pytest.fixture
+def dbconnection():
+    fd, filepath = tempfile.mkstemp()
+    connection = create_engine(
+        "sqlite:///{path}".format(path=filepath),
+        connect_args={"check_same_thread": False},
+        poolclass=NullPool,
+    )
+    yield connection
+    os.close(fd)
+    os.remove(filepath)
+
+
+@pytest.fixture
+def storage(dbconnection):
+    s = Storage(dbconnection)
+    yield s
+    s.clear()
+
+
+QUEUE = "pytest"
+
+
+class MockServices(object):
+    """
+    Mocks the kolibri services plugin.
+    """
+
+    def __init__(self, connection):
+        self.workers = None
+        self.scheduler = None
+        self.connection = connection
+
+    def start(self):
+        # Initialize workers
+        self.workers = [Worker(QUEUE, connection=self.connection)]
+
+        # Start the scheduler
+        q = Queue(queue=QUEUE, connection=self.connection)
+        self.scheduler = Scheduler(queue=q)
+        self.scheduler.start_scheduler()
+
+    def stop(self):
+        # Shutdowns scheduler
+        if self.scheduler is not None:
+            self.scheduler.shutdown_scheduler()
+
+        # Shutdowns the workers
+        if self.workers is not None:
+            for worker in self.workers:
+                worker.shutdown()
+
+
+def test_nonscheduled_jobs_persist_on_server_restart(dbconnection, storage):
+    # Start server services
+    mock_services = MockServices(dbconnection)
+    mock_services.start()
+
+    # Enqueue three jobs
+    job = Job(id)
+    storage.enqueue_job(job, QUEUE)
+    job = Job(id)
+    storage.enqueue_job(job, QUEUE)
+    job = Job(id)
+    storage.enqueue_job(job, QUEUE)
+
+    # Did we enqueue all three jobs correctly?
+    assert storage.count_all_jobs(QUEUE) == 3
+
+    # Ok, let us stop the services
+    mock_services.stop()
+
+    # Do jobs persist on db?
+    assert storage.count_all_jobs(QUEUE) == 3
+
+    # Start services again
+    mock_services.start()
+
+    # Do jobs still persist?
+    assert storage.count_all_jobs(QUEUE) == 3
+
+    # Finally, stop the services
+    mock_services.stop()
+
+
+def test_scheduled_jobs_persist_on_server_restart(dbconnection):
+    # Start server services
+    mock_services = MockServices(dbconnection)
+    mock_services.start()
+
+    # Enqueue three scheduled jobs
+    from kolibri.utils.time_utils import local_now
+    from datetime import timedelta
+
+    schedule_time = local_now() + timedelta(hours=1)
+    mock_services.scheduler.schedule(schedule_time, id)
+    mock_services.scheduler.schedule(schedule_time, id)
+    mock_services.scheduler.schedule(schedule_time, id)
+
+    # Did we enqueue all three scheduled jobs?
+    assert mock_services.scheduler.count() == 3
+
+    # Ok, let us stop the services
+    mock_services.stop()
+
+    # Do scheduled jobs persist on db?
+    assert mock_services.scheduler.count() == 3
+
+    # Start services again
+    mock_services.start()
+
+    # Do scheduled jobs still persist?
+    assert mock_services.scheduler.count() == 3
+
+    # Finally, stop the services
+    mock_services.stop()
