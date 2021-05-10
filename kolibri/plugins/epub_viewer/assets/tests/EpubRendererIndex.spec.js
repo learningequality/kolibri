e8ab6b2453aff9b7ba849c8308ea2a836161cf10
sairina <sairina.merinotsui@gmail.com>
2021-05-10 13:50:27 -0700

    Add tests for pdfrenderer and epubrenderer

---
+++ kolibri/plugins/epub_viewer/assets/tests/EpubRendererIndex.spec.js
@@ -0,0 +1,40 @@
+import EpubRendererIndex from '../src/views/EpubRendererIndex';
+
+const { methods } = EpubRendererIndex;
+
+jest.mock('kolibri.urls');
+
+describe('updateProgress', () => {
+  let context = {};
+
+  beforeEach(() => {
+    context = {
+      forceTimeBasedProgress: null,
+      $emit: jest.fn(),
+      durationBasedProgress: 0.1,
+      visitedPages: [1, 2, 3],
+      locations: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    };
+  });
+
+  it('should be able to calculate progress using "pages visited/total" by default', () => {
+    methods.updateProgress.call(context);
+
+    expect(context.$emit.mock.calls[0][0]).toBe('updateProgress');
+    expect(context.$emit.mock.calls[0][1]).toEqual(
+      context.visitedPages.length / context.locations.length
+    );
+    expect(context.$emit.mock.calls[0][1]).not.toBe(context.durationBasedProgress);
+  });
+
+  it('should have option of using time-based tracking for progress calculation when forceTimeBasedProgress is true', () => {
+    context.forceTimeBasedProgress = true;
+    methods.updateProgress.call(context);
+
+    expect(context.$emit.mock.calls[0][0]).toBe('updateProgress');
+    expect(context.$emit.mock.calls[0][1]).toBe(0.1);
+    expect(context.$emit.mock.calls[0][1]).not.toEqual(
+      context.visitedPages.length / context.locations.length
+    );
+  });
+});
