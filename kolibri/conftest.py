d56d6b6e3851ec37c277822b69e0a46828680baf
Richard Tibbles <richard@learningequality.org>
2021-05-12 12:33:27 -0700

    Dispose of any SQLAlchemy connections before test teardown.

--- kolibri/conftest.py
+++ kolibri/conftest.py
@@ -14,6 +14,9 @@ def global_fixture():
     if not os.path.exists(os.path.join(TEMP_KOLIBRI_HOME, "content")):
         os.mkdir(os.path.join(TEMP_KOLIBRI_HOME, "content"))
     yield  # wait until the test ended
+    from kolibri.core.tasks.main import connection
+
+    connection.dispose()
     if os.path.exists(TEMP_KOLIBRI_HOME):
         try:
             shutil.rmtree(TEMP_KOLIBRI_HOME)
