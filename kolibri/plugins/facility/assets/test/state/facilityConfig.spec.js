32fb24a0c4073b860134313c5f5f238372c16476
Jonathan Boiser <jboiser@gmail.com>
2021-05-11 19:22:00 -0700

    Use kolibri.resource mock in tests

--- kolibri/plugins/facility/assets/test/state/facilityConfig.spec.js
+++ kolibri/plugins/facility/assets/test/state/facilityConfig.spec.js
@@ -1,16 +1,13 @@
 import { FacilityResource, FacilityDatasetResource } from 'kolibri.resources';
 import client from 'kolibri.client';
 import { showFacilityConfigPage } from '../../src/modules/facilityConfig/handlers';
-import { jestMockResource } from 'testUtils'; // eslint-disable-line
 import makeStore from '../makeStore';
 
 jest.mock('kolibri.client', () => jest.fn());
 jest.mock('kolibri.urls', () => ({
   'kolibri:core:facilitydataset-resetsettings': () => {},
 }));
-
-const FacilityStub = jestMockResource(FacilityResource);
-const DatasetStub = jestMockResource(FacilityDatasetResource);
+jest.mock('kolibri.resources');
 
 const fakeFacility = {
   name: 'Nalanda Maths',
@@ -40,23 +37,28 @@ describe('facility config page actions', () => {
 
   beforeEach(() => {
     store = makeStore();
-    store.state.route = { params: {} };
     commitStub = jest.spyOn(store, 'commit');
+    store.state.route = { params: {} };
     Object.assign(store.state.core, {
       pageId: '123',
       session: {
         facility_id: 1,
       },
     });
-    FacilityResource.__resetMocks();
-    FacilityDatasetResource.__resetMocks();
+  });
+
+  afterEach(() => {
+    FacilityResource.fetchModel.mockReset();
+    FacilityResource.fetchCollection.mockReset();
+    FacilityDatasetResource.fetchCollection.mockReset();
+    FacilityDatasetResource.saveModel.mockReset();
   });
 
   describe('showFacilityConfigPage action', () => {
     it('when resources load successfully', () => {
-      FacilityStub.__getModelFetchReturns(fakeFacility);
-      DatasetStub.__getCollectionFetchReturns(fakeDatasets);
-      FacilityStub.__getCollectionFetchReturns(fakeFacilities);
+      FacilityResource.fetchModel.mockResolvedValue(fakeFacility);
+      FacilityResource.fetchCollection.mockResolvedValue(fakeFacilities);
+      FacilityDatasetResource.fetchCollection.mockResolvedValue(fakeDatasets);
       const expectedState = {
         facilityDatasetId: 'dataset_2',
         facilityName: 'Nalanda Maths',
@@ -77,7 +79,9 @@ describe('facility config page actions', () => {
       };
 
       return showFacilityConfigPage(store, toRoute).then(() => {
-        expect(DatasetStub.getCollection).toHaveBeenCalledWith({ facility_id: 1 });
+        expect(FacilityDatasetResource.fetchCollection).toHaveBeenCalledWith({
+          getParams: { facility_id: 1 },
+        });
         expect(commitStub).toHaveBeenCalledWith(
           'facilityConfig/SET_STATE',
           expect.objectContaining(expectedState)
@@ -91,9 +95,9 @@ describe('facility config page actions', () => {
         settings: null,
       };
       it('when fetching Facility fails', () => {
-        FacilityStub.__getModelFetchReturns('incomprehensible error', true);
-        DatasetStub.__getCollectionFetchReturns(fakeDatasets);
-        FacilityStub.__getCollectionFetchReturns(fakeFacilities);
+        FacilityResource.fetchModel.mockRejectedValue('incomprehensible error');
+        FacilityResource.fetchCollection.mockResolvedValue(fakeFacilities);
+        FacilityDatasetResource.fetchCollection.mockResolvedValue(fakeDatasets);
         return showFacilityConfigPage(store, toRoute).then(() => {
           expect(commitStub).toHaveBeenCalledWith(
             'facilityConfig/SET_STATE',
@@ -102,16 +106,15 @@ describe('facility config page actions', () => {
         });
       });
 
-      it('when fetching FacilityDataset fails', () => {
-        FacilityStub.__getModelFetchReturns(fakeFacility);
-        DatasetStub.__getCollectionFetchReturns('incomprehensible error', true);
-        FacilityStub.__getCollectionFetchReturns(fakeFacilities);
-        return showFacilityConfigPage(store, toRoute).then(() => {
-          expect(commitStub).toHaveBeenCalledWith(
-            'facilityConfig/SET_STATE',
-            expect.objectContaining(expectedState)
-          );
-        });
+      it('when fetching FacilityDataset fails', async () => {
+        FacilityResource.fetchModel.mockResolvedValue(fakeFacility);
+        FacilityResource.fetchCollection.mockResolvedValue(fakeFacilities);
+        FacilityDatasetResource.fetchCollection.mockRejectedValue('incoprehensible error');
+        await showFacilityConfigPage(store, toRoute);
+        expect(commitStub).toHaveBeenCalledWith(
+          'facilityConfig/SET_STATE',
+          expect.objectContaining(expectedState)
+        );
       });
     });
   });
@@ -139,16 +142,17 @@ describe('facility config page actions', () => {
         learner_can_sign_up: false,
       };
       // IRL returns the updated Model
-      const saveStub = DatasetStub.__getModelSaveReturns('ok');
+      const saveStub = FacilityDatasetResource.saveModel.mockResolvedValue('ok');
 
       return store.dispatch('facilityConfig/saveFacilityConfig').then(() => {
-        expect(DatasetStub.getModel).toHaveBeenCalledWith(1000, {});
-        expect(saveStub).toHaveBeenCalledWith(expect.objectContaining(expectedRequest), false);
+        expect(saveStub).toHaveBeenCalledWith(
+          expect.objectContaining({ id: 1000, data: expectedRequest })
+        );
       });
     });
 
     it('when save fails', () => {
-      const saveStub = DatasetStub.__getModelSaveReturns('heck no', true);
+      const saveStub = FacilityDatasetResource.saveModel.mockRejectedValue('heck no', true);
       return store.dispatch('facilityConfig/saveFacilityConfig').catch(() => {
         expect(saveStub).toHaveBeenCalled();
         expect(store.state.facilityConfig.settings).toEqual({
