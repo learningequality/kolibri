7354d302af5677b5d5c0b7f3c764f2a68d489d20
Richard Tibbles <richard@learningequality.org>
2021-05-17 15:48:17 -0700

    Allow one user to be merged onto another.

--- kolibri/core/auth/test/test_utils.py
+++ kolibri/core/auth/test/test_utils.py
@@ -2,14 +2,22 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import datetime
+import random
 import uuid
 
+import factory
 import mock
 from django.core.management.base import CommandError
 from django.test import TestCase
 
 from ..models import Facility
+from ..models import FacilityUser
 from kolibri.core.auth.management import utils
+from kolibri.core.auth.test.test_api import FacilityFactory
+from kolibri.core.auth.test.test_api import FacilityUserFactory
+from kolibri.core.auth.utils import merge_users
+from kolibri.core.logger import models as log_models
 
 
 class GetFacilityTestCase(TestCase):
@@ -50,3 +58,182 @@ class GetFacilityFailureTestCase(TestCase):
     def test_get_facility_no_facilities(self):
         with self.assertRaisesRegexp(CommandError, "no facilities"):
             utils.get_facility()
+
+
+class ContentSessionLogFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = log_models.ContentSessionLog
+
+    user = factory.SubFactory(FacilityUserFactory)
+    start_timestamp = datetime.datetime.now()
+    content_id = factory.LazyFunction(lambda: uuid.uuid4().hex)
+    channel_id = factory.LazyFunction(lambda: uuid.uuid4().hex)
+    progress = factory.LazyFunction(random.random)
+
+
+class ContentSummaryLogFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = log_models.ContentSummaryLog
+
+    user = factory.SubFactory(FacilityUserFactory)
+    start_timestamp = datetime.datetime.now()
+    content_id = factory.LazyFunction(lambda: uuid.uuid4().hex)
+    channel_id = factory.LazyFunction(lambda: uuid.uuid4().hex)
+    progress = factory.LazyFunction(random.random)
+
+
+class UserSessionLogFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = log_models.UserSessionLog
+
+    user = factory.SubFactory(FacilityUserFactory)
+
+
+class MasteryLogFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = log_models.MasteryLog
+
+    user = factory.SubFactory(FacilityUserFactory)
+    summarylog = factory.SubFactory(ContentSummaryLogFactory)
+    start_timestamp = datetime.datetime.now()
+    mastery_level = factory.LazyFunction(lambda: random.randint(1, 10))
+
+
+class AttemptLogFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = log_models.AttemptLog
+
+    user = factory.SubFactory(FacilityUserFactory)
+    masterylog = factory.SubFactory(MasteryLogFactory)
+    sessionlog = factory.SubFactory(ContentSessionLogFactory)
+    start_timestamp = datetime.datetime.now()
+    end_timestamp = datetime.datetime.now()
+    correct = False
+    time_spent = factory.LazyFunction(random.random)
+
+
+class MergeUserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.facility = FacilityFactory.create()
+        for i in range(0, 5):
+            user = FacilityUserFactory.create(facility=cls.facility)
+
+            sess_logs = []
+            summ_logs = []
+
+            for _ in range(3):
+                sess_logs.append(
+                    ContentSessionLogFactory.create(
+                        user=user,
+                    )
+                )
+
+                summ_logs.append(
+                    ContentSummaryLogFactory.create(
+                        user=user,
+                    )
+                )
+            ex_csessl = ContentSessionLogFactory.create(
+                user=user,
+            )
+
+            ex_csmlog = ContentSummaryLogFactory.create(
+                user=user,
+            )
+            masterylog = MasteryLogFactory.create(user=user, summarylog=ex_csmlog)
+            attemptlog = AttemptLogFactory.create(
+                user=user, masterylog=masterylog, sessionlog=ex_csessl
+            )
+            usersessionlog = UserSessionLogFactory.create(user=user)
+
+            sess_logs.append(ex_csessl)
+            summ_logs.append(ex_csmlog)
+
+            setattr(cls, "user_{}".format(str(i)), user)
+            setattr(cls, "user_{}_sess_logs".format(str(i)), sess_logs)
+            setattr(cls, "user_{}_summ_logs".format(str(i)), summ_logs)
+            setattr(cls, "user_{}_masterylog".format(str(i)), masterylog)
+            setattr(cls, "user_{}_attemptlog".format(str(i)), attemptlog)
+            setattr(cls, "user_{}_usersessionlog".format(str(i)), usersessionlog)
+
+        cls.user_1_id = cls.user_1.id
+        cls.user_1.birth_year = "1984"
+        cls.user_1.id_number = "101"
+        cls.user_1.save()
+        cls.user_2.id_number = "13"
+        cls.user_2.save()
+        merge_users(cls.user_1, cls.user_2)
+
+    def test_no_user_after_merge(self):
+        self.assertFalse(FacilityUser.objects.filter(id=self.user_1_id).exists())
+
+    def test_user_data_after_merge(self):
+        self.user_2.refresh_from_db()
+        self.assertEqual(self.user_2.birth_year, "1984")
+        self.assertEqual(self.user_2.id_number, "13")
+
+    def test_masterylogs(self):
+        self.assertEqual(
+            log_models.MasteryLog.objects.filter(user=self.user_2).count(), 2
+        )
+        log = self.user_1_masterylog
+        self.assertTrue(
+            log_models.MasteryLog.objects.filter(
+                summarylog__progress=log.summarylog.progress,
+                user=self.user_2,
+                mastery_level=log.mastery_level,
+                summarylog__channel_id=log.summarylog.channel_id,
+                summarylog__content_id=log.summarylog.content_id,
+            ).exists()
+        )
+
+    def test_usersessionlogs(self):
+        self.assertEqual(
+            log_models.UserSessionLog.objects.filter(user=self.user_2).count(), 2
+        )
+
+    def test_attemptlogs(self):
+        self.assertEqual(
+            log_models.AttemptLog.objects.filter(user=self.user_2).count(), 2
+        )
+        log = self.user_1_attemptlog
+        self.assertTrue(
+            log_models.AttemptLog.objects.filter(
+                masterylog__summarylog__progress=log.masterylog.summarylog.progress,
+                user=self.user_2,
+                time_spent=log.time_spent,
+                masterylog__summarylog__channel_id=log.masterylog.summarylog.channel_id,
+                masterylog__summarylog__content_id=log.masterylog.summarylog.content_id,
+            ).exists()
+        )
+
+    def test_contentsessionlogs(self):
+        self.assertEqual(
+            log_models.ContentSessionLog.objects.filter(user=self.user_2).count(),
+            len(self.user_1_sess_logs) + len(self.user_2_sess_logs),
+        )
+        for log in self.user_1_sess_logs:
+            self.assertTrue(
+                log_models.ContentSessionLog.objects.filter(
+                    progress=log.progress,
+                    user=self.user_2,
+                    content_id=log.content_id,
+                    channel_id=log.channel_id,
+                ).exists()
+            )
+
+    def test_contentsummarylogs(self):
+        self.assertEqual(
+            log_models.ContentSummaryLog.objects.filter(user=self.user_2).count(),
+            len(self.user_1_summ_logs) + len(self.user_2_summ_logs),
+        )
+        for log in self.user_1_summ_logs:
+            self.assertTrue(
+                log_models.ContentSummaryLog.objects.filter(
+                    progress=log.progress,
+                    user=self.user_2,
+                    content_id=log.content_id,
+                    channel_id=log.channel_id,
+                ).exists()
+            )
--- kolibri/core/auth/utils.py
+++ kolibri/core/auth/utils.py
@@ -4,6 +4,11 @@ from django.utils.six.moves import input
 
 from kolibri.core.auth.models import AdHocGroup
 from kolibri.core.auth.models import Membership
+from kolibri.core.logger.models import AttemptLog
+from kolibri.core.logger.models import ContentSessionLog
+from kolibri.core.logger.models import ContentSummaryLog
+from kolibri.core.logger.models import MasteryLog
+from kolibri.core.logger.models import UserSessionLog
 
 
 def confirm_or_exit(message):
@@ -20,3 +25,124 @@ def create_adhoc_group_for_learners(classroom, learners):
     for learner in learners:
         Membership.objects.create(user=learner, collection=adhoc_group)
     return adhoc_group
+
+
+models_to_copy = [
+    ContentSessionLog,
+    ContentSummaryLog,
+    MasteryLog,
+    AttemptLog,
+    UserSessionLog,
+]
+
+user_fields = [
+    "gender",
+    "birth_year",
+    "id_number",
+]
+
+log_fields = {
+    ContentSessionLog: [
+        "content_id",
+        "visitor_id",
+        "channel_id",
+        "start_timestamp",
+        "end_timestamp",
+        "time_spent",
+        "progress",
+        "kind",
+        "extra_fields",
+    ],
+    ContentSummaryLog: [
+        "content_id",
+        "channel_id",
+        "start_timestamp",
+        "end_timestamp",
+        "completion_timestamp",
+        "time_spent",
+        "progress",
+        "kind",
+        "extra_fields",
+    ],
+    UserSessionLog: [
+        "channels",
+        "start_timestamp",
+        "last_interaction_timestamp",
+        "pages",
+        "device_info",
+    ],
+    MasteryLog: [
+        "mastery_criterion",
+        "start_timestamp",
+        "end_timestamp",
+        "completion_timestamp",
+        "mastery_level",
+        "complete",
+        ("summarylog_id", ContentSummaryLog),
+    ],
+    AttemptLog: [
+        "item",
+        "start_timestamp",
+        "end_timestamp",
+        "completion_timestamp",
+        "time_spent",
+        "complete",
+        "correct",
+        "hinted",
+        "answer",
+        "simple_answer",
+        "interaction_history",
+        "error",
+        ("masterylog_id", MasteryLog),
+        ("sessionlog_id", ContentSessionLog),
+    ],
+}
+
+
+def _merge_user_models(source_user, target_user):
+    for f in user_fields:
+        source_value = getattr(source_user, f, None)
+        target_value = getattr(target_user, f, None)
+        if not target_value and source_value:
+            setattr(target_user, f, source_value)
+    target_user.save()
+
+
+def merge_users(source_user, target_user):
+    """
+    Utility to merge two users. It makes no assumptions about whether
+    the users are in the same facility and does raw copies of all
+    associated user data, rather than try to do anything clever.
+    """
+    if source_user.id == target_user.id:
+        raise ValueError("Cannot merge a user with themselves")
+
+    _merge_user_models(source_user, target_user)
+
+    id_map = {}
+
+    def _merge_log_data(source_user, target_user, LogModel):
+        log_map = {}
+        id_map[LogModel] = log_map
+        for log in LogModel.objects.filter(user=source_user):
+            data = {}
+            for f in log_fields[LogModel]:
+                if isinstance(f, tuple):
+                    field_name, model = f
+                    data[field_name] = id_map[model][getattr(log, field_name)]
+                else:
+                    data[f] = getattr(log, f)
+            new_log = LogModel.objects.create(user=target_user, **data)
+            log_map[log.id] = new_log.id
+
+    _merge_log_data(source_user, target_user, ContentSessionLog)
+
+    _merge_log_data(source_user, target_user, ContentSummaryLog)
+
+    _merge_log_data(source_user, target_user, UserSessionLog)
+
+    _merge_log_data(source_user, target_user, MasteryLog)
+
+    _merge_log_data(source_user, target_user, AttemptLog)
+
+    source_user.delete()
