ab313ee0df6d0f3f7945545e2c3f06301aa053a3
sairina <sairina.merinotsui@gmail.com>
2021-06-10 23:41:47 -0700

    Add OverallStatus component

--- kolibri/plugins/learn/assets/src/views/classes/LessonMasteryBar.vue
+++ kolibri/plugins/learn/assets/src/views/classes/LessonMasteryBar.vue
@@ -6,20 +6,7 @@
     :style="{ backgroundColor: $themeTokens.surface }"
   >
     <div class="inner-header" :style="innerStyle">
-      <div class="overall-status" :style="{ color: $themeTokens.text }">
-        <KIcon
-          icon="mastered"
-          :color="success ? $themeTokens.mastered : $themePalette.grey.v_200"
-        />
-        <div class="overall-status-text">
-          <span v-if="success" class="completed" :style="{ color: $themeTokens.annotation }">
-            {{ coreString('completedLabel') }}
-          </span>
-          <span>
-            {{ $tr('goal', { count: totalCorrectRequiredM }) }}
-          </span>
-        </div>
-      </div>
+      <OverallStatus />
       <slot></slot>
     </div>
   </div>
@@ -30,9 +17,13 @@
 <script>
 
   import responsiveWindowMixin from 'kolibri.coreVue.mixins.responsiveWindowMixin';
+  import OverallStatus from './OverallStatus';
 
   export default {
     name: 'LessonMasteryBar',
+    components: {
+      OverallStatus,
+    },
     mixins: [responsiveWindowMixin],
     props: {
       maxWidth: {
@@ -73,14 +64,6 @@
       });
     },
     methods: {},
-
-    $trs: {
-      goal: {
-        message: 'Get {count, number, integer} {count, plural, other {correct}}',
-        context:
-          '\nMessage that indicates to the learner how many correct answers they need to give in order to master the given topic, and for the exercise to be considered completed.',
-      },
-    },
   };
 
 </script>
