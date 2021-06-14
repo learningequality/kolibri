c9809e3cfdde0d2dfaa7e01c0bb90820244c4c40
Michaela <michaela@learningequality.org>
2021-06-14 09:13:08 +0200

    Add learning activity icon component

---
+++ kolibri/plugins/learn/assets/src/views/LearningActivityIcon.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <KIcon :icon="icon" />
+
+</template>
+
+
+<script>
+
+  import { LearningActivityKinds } from 'kolibri.coreVue.vuex.constants';
+
+  const LearningActivityKindToIconMap = {
+    [LearningActivityKinds.CREATE]: 'create',
+    [LearningActivityKinds.LISTEN]: 'listen',
+    [LearningActivityKinds.REFLECT]: 'reflect',
+    [LearningActivityKinds.PRACTICE]: 'practice',
+    [LearningActivityKinds.READ]: 'read',
+    [LearningActivityKinds.WATCH]: 'watch',
+    [LearningActivityKinds.EXPLORE]: 'interact',
+  };
+
+  export default {
+    name: 'LearningActivityIcon',
+    props: {
+      /**
+       * Learning activity constant
+       */
+      kind: {
+        type: String,
+        required: true,
+        validator(value) {
+          return Object.values(LearningActivityKinds).includes(value);
+        },
+      },
+      /**
+       * Icon is solid by default.
+       * Set to `true` to make it shaded.
+       */
+      shaded: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+    },
+    computed: {
+      icon() {
+        const icon = LearningActivityKindToIconMap[this.kind];
+        if (this.shaded) {
+          return icon + 'Shaded';
+        }
+        return icon + 'Solid';
+      },
+    },
+  };
+
+</script>
