10e1f57a60742108122b800df1332810d2d288ec
Richard Tibbles <richard@learningequality.org>
2021-06-17 06:36:31 -0700

    Update browserlist to be more explicit and portable. Remove vestigial babelrc.js

---
+++ packages/browserslist-config-kolibri/index.js
@@ -0,0 +1,21 @@
+module.exports = [
+  // Exclude the following because their market share is no > 0.1%
+  // Baidu for Baidu Browser.
+  // BlackBerry or bb for Blackberry browser.
+  // kaios for KaiOS Browser.
+  // OperaMobile or op_mob for Opera Mobile.
+  // We exclude OperaMini explicitly as it does not support basic functionality
+  'Android > 4.4.3',
+  'Chrome >= 49',
+  'ChromeAndroid >= 49',
+  'Edge >= 18',
+  'IE >= 11',
+  'Firefox >= 52',
+  'FirefoxAndroid >= 68',
+  'iOS >= 9.3',
+  'Opera >= 67',
+  'QQAndroid >= 10.4',
+  'Safari >= 11.1',
+  'Samsung >= 4',
+  'UCAndroid >= 12.12',
+];
--- packages/kolibri-tools/jest.conf/babel-jest-transform.js
+++
@@ -1,4 +0,0 @@
-const babelJest = require('babel-jest');
-const babelConfig = require('../.babelrc.js');
-
-module.exports = babelJest.createTransformer(babelConfig);
