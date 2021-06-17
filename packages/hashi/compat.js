3b3d49db53769a864381f67e4dd8265d79a8dc6b
Richard Tibbles <richard@learningequality.org>
2021-06-17 06:37:12 -0700

    Add polyfills for hashi.

---
+++ packages/hashi/compat.js
@@ -0,0 +1,13 @@
+// Eslint configuration to check browser compatibility
+module.exports = {
+  env: {
+    browser: true,
+  },
+  plugins: ['compat'],
+  rules: {
+    'compat/compat': 'error',
+  },
+  settings: {
+    polyfills: ['Object.values', 'Object.assign', 'Object.entries', 'array-includes', 'Promise'],
+  },
+};
