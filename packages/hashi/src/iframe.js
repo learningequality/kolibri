5973d7ae47ea9a36477652c83c4d95d7f67a3036
Richard Tibbles <richard@learningequality.org>
2021-06-21 15:28:14 -0700

    Polyfill startsWith.

--- packages/hashi/src/iframe.js
+++ packages/hashi/src/iframe.js
@@ -3,6 +3,7 @@ import 'core-js/es/object/assign';
 import 'core-js/es/object/entries';
 import 'core-js/es/object/values';
 import 'core-js/es/promise';
+import 'core-js/es/string/starts-with';
 import 'core-js/web/url';
 import Hashi from './iframeClient';
 
