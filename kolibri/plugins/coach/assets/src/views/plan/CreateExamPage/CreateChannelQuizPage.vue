209b0b46327292554c3a47a16f17f8c509785785
sairina <sairina.merinotsui@gmail.com>
2021-06-29 14:56:09 -0700

    Complete UI on immersive toolbar and bottom app bar

--- kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/CreateChannelQuizPage.vue
+++ kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/CreateChannelQuizPage.vue
@@ -1,12 +1,41 @@
 <template>
 
-  <CoreBase>
+  <CoreBase
+    :immersivePage="true"
+    immersivePageIcon="close"
+    :immersivePagePrimary="true"
+    :immersivePageRoute="toolbarRoute"
+    :appBarTitle="$tr('createNewExamLabel')"
+    :authorized="userIsAuthorized"
+    authorizedRole="adminOrCoach"
+    :pageTitle="$tr('selectChannelQuizLabel')"
+    :marginBottom="72"
+  >
 
     <KPageContainer>
+      <h1>{{ $tr('selectChannelQuizLabel') }}</h1>
+      <ContentCardList
+        :contentList="filteredContentList"
+        :viewMoreButtonState="viewMoreButtonState"
+        :contentCardMessage="selectionMetadata"
+        :contentCardLink="contentLink"
+        @moreresults="handleMoreResults"
+      />
 
-      <h1>page 1</h1>
-
-
+      <BottomAppBar>
+        <KButtonGroup>
+          <KRouterLink
+            appearance="flat-button"
+            :text="coreString('cancelAction')"
+            :to="toolbarRoute"
+          />
+          <KButton
+            :text="coreString('finishAction')"
+            primary
+            @click="finishProcess"
+          />
+        </KButtonGroup>
+      </BottomAppBar>
     </KPageContainer>
 
   </CoreBase>
@@ -16,6 +45,50 @@
 
 <script>
 
-  undefined;
+  import { mapState } from 'vuex';
+  import responsiveWindowMixin from 'kolibri.coreVue.mixins.responsiveWindowMixin';
+  import BottomAppBar from 'kolibri.coreVue.components.BottomAppBar';
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import ContentCardList from '../../plan/LessonResourceSelectionPage/ContentCardList';
+  import commonCoach from '../../common';
+
+  export default {
+    name: 'CreateChannelQuizPage',
+    components: {
+      BottomAppBar,
+      ContentCardList,
+    },
+    mixins: [commonCoreStrings, commonCoach, responsiveWindowMixin],
+    data() {
+      return {
+        // showError: false,
+      };
+    },
+    computed: {
+      ...mapState(['toolbarRoute']),
+    },
+    methods: {
+      finishProcess() {
+        return;
+
+        // this.$router.push({ name: PageNames.EXAM_CREATION_QUESTION_SELECTION });
+      },
+    },
+    $trs: {
+      createNewExamLabel: 'Create new quiz',
+      selectChannelQuizLabel: 'Select a channel quiz',
+      // noneSelected: 'No exercises are selected',
+    },
+  };
 
 </script>
+
+
+<style lang="scss" scoped>
+
+  .search-box {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+</style>
