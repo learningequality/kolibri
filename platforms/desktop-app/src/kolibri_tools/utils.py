874b1db974030183fbc83a2387877547fa8122d4
Jamie Alexandre <jamalex@gmail.com>
2021-08-01 23:17:13 -0700

    Add zip_port to start call

--- platforms/desktop-app/src/kolibri_tools/utils.py
+++ platforms/desktop-app/src/kolibri_tools/utils.py
@@ -16,7 +16,7 @@ def start_kolibri_server():
     setup_logging(debug=False)
     initialize()
     automatic_provisiondevice()
-    start.callback(KOLIBRI_PORT, background=False)
+    start.callback(KOLIBRI_PORT, zip_port=None, background=False)
 
 
 def get_initialize_url(next_url=None):
