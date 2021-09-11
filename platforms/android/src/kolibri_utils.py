66cb3925847877b19911b42b5a6718328e6e3129
Richard Tibbles <richard@learningequality.org>
2021-09-11 10:16:02 -0700

    Cleanup unused function.

--- platforms/android/src/kolibri_utils.py
+++ platforms/android/src/kolibri_utils.py
@@ -1,6 +1,4 @@
 import os
-import pew.ui
-import sys
 
 import android_utils
 
@@ -20,8 +18,3 @@ def start_kolibri_server():
     print("Home folder: {}".format(os.environ.get("KOLIBRI_HOME", "(default)")))
     print("Timezone: {}".format(os.environ.get("TZ", "(default)")))
     main(["start", "--foreground"])
-
-
-def get_content_file_path(filename):
-    from kolibri.core.content.utils.paths import get_content_storage_file_path
-    return get_content_storage_file_path(filename)
