81448fd8e3d251dcfad987934b204a7238a2b1ee
Richard Tibbles <richard@learningequality.org>
2021-09-12 18:15:05 -0700

    Simplify main.py code and rely on Java webview updates.

--- platforms/android/src/main.py
+++ platforms/android/src/main.py
@@ -3,9 +3,9 @@ import initialization  # keep this first, to ensure we're set up for other impor
 import logging
 
 from config import KOLIBRI_PORT
+from jnius import autoclass
 
 from android_utils import start_service
-from webview import AndroidWebView
 
 # we need to initialize Kolibri to allow us to access the app key
 from kolibri.utils.cli import initialize
@@ -14,56 +14,19 @@ from kolibri.utils.server import wait_for_status
 from kolibri.utils.server import STATUS_RUNNING
 
 
-class Application(object):
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
-    def run(self):
-        """
-        Start your UI and app run loop here.
-        """
+logging.info("Initializing Kolibri and running any upgrade routines")
+initialize()
 
-        self.kolibri_loaded = False
-        self.kolibri_origin = "http://127.0.0.1:{port}".format(port=KOLIBRI_PORT)
-        self.view = AndroidWebView()
+# start kolibri server
+logging.info("Starting kolibri server via Android service...")
+start_service("server")
+start_service("remoteshell")
 
-        logging.info("Initializing Kolibri and running any upgrade routines")
-        initialize()
+# Tie up this thread until the server is running
+while not wait_for_status(STATUS_RUNNING):
+    logging.info("Kolibri server not yet started")
 
-        # start kolibri server
-        logging.info("Starting kolibri server via Android service...")
-        start_service("server")
-        start_service("remoteshell")
-
-        self.wait_for_server()
-
-        return 0
-    
-    def wait_for_server(self):
-        # Tie up this thread until the server is running
-        while not wait_for_status(STATUS_RUNNING):
-            logging.info("Kolibri server not yet started")
-
-        # Check for saved URL, which exists when the app was put to sleep last time it ran
-        saved_state = self.view.get_view_state()
-        logging.debug("Persisted View State: {}".format(self.view.get_view_state()))
-
-        next_url = None
-        if "URL" in saved_state and saved_state["URL"].startswith(self.kolibri_origin):
-            next_url = saved_state["URL"].replace(self.kolibri_origin, '')
-
-        start_url = self.kolibri_origin + interface.get_initialize_url(next_url=next_url)
-        self.view.load_url(start_url)
-
-    def should_load_url(self, url):
-        if (
-            url is not None
-            and url.startswith("http")
-            and not url.startswith(self.kolibri_origin)
-        ):
-            return False
-
-        return True
-
-
-if __name__ == "__main__":
-    app = Application()
-    app.run()
+start_url = "http://127.0.0.1:{port}".format(port=KOLIBRI_PORT) + interface.get_initialize_url()
+PythonActivity.mWebView.loadUrl(start_url)
