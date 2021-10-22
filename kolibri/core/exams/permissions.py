6567066c5e3a267ad6995e1ab4b90a254c10d3cc
Richard Tibbles <richard@learningequality.org>
2021-10-21 17:36:35 -0700

    Migrate exam API to user masterylog.

--- kolibri/core/exams/permissions.py
+++ kolibri/core/exams/permissions.py
@@ -10,18 +10,26 @@ class UserCanReadExamAssignmentData(DenyAll):
         if isinstance(user, AnonymousUser):
             return False
         # Import here to avoid circular import.
-        from kolibri.core.logger.models import ExamLog
+        from kolibri.core.logger.models import MasteryLog
 
         # If they are not a member of the assignment's collection, don't bother with any other checks
         return user.is_member_of(obj.collection) and (
-            obj.exam.active or ExamLog.objects.filter(exam=obj.exam, user=user).exists()
+            obj.exam.active
+            or MasteryLog.objects.filter(
+                summarylog__content_id=obj.exam_id, user=user
+            ).exists()
         )
 
     def readable_by_user_filter(self, user):
         if isinstance(user, AnonymousUser):
             return q_none
+        from kolibri.core.logger.models import MasteryLog
+
+        user_masterylog_content_ids = MasteryLog.objects.filter(user=user).values(
+            "content_id"
+        )
         return Q(collection_id__in=user.memberships.all().values("collection_id")) & Q(
-            Q(exam__active=True) | Q(exam__examlogs__user=user)
+            Q(exam__active=True) | Q(exam__id__in=user_masterylog_content_ids)
         )
 
 
@@ -30,23 +38,31 @@ class UserCanReadExamData(DenyAll):
         if isinstance(user, AnonymousUser):
             return False
         # Import here to avoid circular import.
-        from kolibri.core.logger.models import ExamLog
+        from kolibri.core.logger.models import MasteryLog
 
         # If they are not a member of the assignment's collection, don't bother with any other checks
         return obj.assignments.objects.filter(
             collection_id__in=user.memberships.all().values("collection_id")
         ).exists() and (
-            obj.active or ExamLog.objects.filter(exam=obj, user=user).exists()
+            obj.active
+            or MasteryLog.objects.filter(
+                summarylog__content_id=obj.id, user=user
+            ).exists()
         )
 
     def readable_by_user_filter(self, user):
         if isinstance(user, AnonymousUser):
             return q_none
         from kolibri.core.exams.models import ExamAssignment
+        from kolibri.core.logger.models import MasteryLog
+
+        user_masterylog_content_ids = MasteryLog.objects.filter(user=user).values(
+            "summarylog__content_id"
+        )
 
         assignments = ExamAssignment.objects.filter(
             collection_id__in=user.memberships.all().values("collection_id")
         )
         return Q(assignments__in=assignments) & Q(
-            Q(active=True) | Q(examlogs__user=user)
+            Q(active=True) | Q(id__in=user_masterylog_content_ids)
         )
