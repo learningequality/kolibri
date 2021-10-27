cec3289925d30657ba681815fe3df54d2675fc04
Richard Tibbles <richard@learningequality.org>
2021-10-27 15:16:14 -0700

    Add test for setting zip content origin. Add '' around zip content port when injecting into the page.

--- kolibri/core/content/test/test_paths.py
+++ kolibri/core/content/test/test_paths.py
@@ -3,6 +3,7 @@ import hashlib
 from django.test import TestCase
 
 from kolibri.core.content.models import LocalFile
+from kolibri.core.content.utils.paths import get_zip_content_config
 from kolibri.utils.tests.helpers import override_option
 
 
@@ -29,3 +30,11 @@ class LocalFilePathsTest(TestCase):
 @override_option("Deployment", "URL_PATH_PREFIX", "prefix_test/")
 class PrefixedLocalFilesPathsTest(LocalFilePathsTest):
     pass
+
+
+class ZipContentConfigTest(TestCase):
+    @override_option("Deployment", "ZIP_CONTENT_ORIGIN", "https://kolibri.example.com")
+    def test_zip_content_origin_set(self):
+        zip_content_origin, zip_content_port = get_zip_content_config()
+        self.assertEqual("https://kolibri.example.com", zip_content_origin)
+        self.assertEqual(zip_content_port, "")
