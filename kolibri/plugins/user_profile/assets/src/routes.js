5fe36a7d095393598b797203c308b45c40fc8cd6
Richard Tibbles <richard@learningequality.org>
2021-10-30 16:42:29 -0700

    Redirect browswer rather than trying to navigate to undefined route.

--- kolibri/plugins/user_profile/assets/src/routes.js
+++ kolibri/plugins/user_profile/assets/src/routes.js
@@ -1,6 +1,5 @@
 import store from 'kolibri.coreVue.vuex.store';
-import router from 'kolibri.coreVue.router';
-import { ComponentMap } from './constants';
+import { redirectBrowser } from 'kolibri.utils.redirectBrowser';
 import ProfilePage from './views/ProfilePage';
 import ProfileEditPage from './views/ProfileEditPage';
 
@@ -11,7 +10,7 @@ export default [
     beforeEnter(to, from, next) {
       store.commit('CORE_SET_PAGE_LOADING', false);
       if (!store.getters.isUserLoggedIn) {
-        next(router.getRoute(ComponentMap.SIGN_IN));
+        redirectBrowser();
       } else {
         next();
       }
@@ -23,7 +22,7 @@ export default [
     beforeEnter(to, from, next) {
       store.commit('CORE_SET_PAGE_LOADING', false);
       if (!store.getters.isUserLoggedIn) {
-        next(router.getRoute(ComponentMap.SIGN_IN));
+        redirectBrowser();
       } else {
         next();
       }
