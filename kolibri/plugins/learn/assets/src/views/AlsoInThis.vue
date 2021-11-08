3faa1e917af779773d96d81bc5e0ad88b5eeb3a7
Jacob Pierce <w.jacobpierce@gmail.com>
2021-11-08 13:13:25 -0800

    styles for side panel

--- kolibri/plugins/learn/assets/src/views/AlsoInThis.vue
+++ kolibri/plugins/learn/assets/src/views/AlsoInThis.vue
@@ -6,20 +6,40 @@
     </div>
 
     <div class="content-list">
-      <KRouterLink 
-        v-for="content in contentNodes" 
-        :key="content.id" 
+      <KRouterLink
+        v-for="content in contentNodes"
+        :key="content.id"
         :to="genContentLink(content.id, content.is_leaf)"
         class="item"
+        :style="linkStyles"
       >
-        <LearningActivityIcon class="activity-icon" :kind="content.learning_activities" />
-        <div class="content-meta">
-          <div>{{ content.title }}</div>
-          <TimeDuration :seconds="content.duration" />
+        <LearningActivityIcon
+          v-if="content.is_leaf"
+          class="activity-icon"
+          :kind="content.learning_activities"
+        />
+        <KIcon v-else class="topic-icon" icon="topic" />
+        <div class="content-meta" :style="{ top: (content.duration ? '0px' : '8px') }">
+          <TextTruncator
+            :text="content.title"
+            :maxHeight="24"
+            :style="{ marginTop: (content.duration ? '8px' : '0px') }"
+          />
+          <TimeDuration
+            v-if="content.duration"
+            class="time-duration"
+            :style="{ color: $themeTokens.annotation }"
+            :seconds="content.duration"
+          />
         </div>
         <div class="progress">
-          <KIcon v-if="content.progress === 1" icon="mastered" />
-          <ProgressBar :progress="content.progress" />
+          <KIcon
+            v-if="content.progress === 1"
+            icon="star"
+            class="mastered-icon"
+            :style="{ fill: $themeTokens.mastered }"
+          />
+          <ProgressBar v-else :progress="content.progress" class="bar" />
         </div>
       </KRouterLink>
     </div>
@@ -27,8 +47,10 @@
 
 </template>
 
+
 <script>
 
+  import TextTruncator from 'kolibri.coreVue.components.TextTruncator';
   import TimeDuration from 'kolibri.coreVue.components.TimeDuration';
   import genContentLink from '../utils/genContentLink';
   import LearningActivityIcon from './LearningActivityIcon.vue';
@@ -39,6 +61,7 @@
     components: {
       LearningActivityIcon,
       ProgressBar,
+      TextTruncator,
       TimeDuration,
     },
     props: {
@@ -58,33 +81,97 @@
         required: true,
       },
     },
+    computed: {
+      /** Overrides some styles in KRouterLink */
+      linkStyles() {
+        return {
+          color: this.$themeTokens.text + '!important',
+          fontSize: '14px',
+        };
+      },
+    },
     methods: { genContentLink },
   };
 
 </script>
 
-<style scoped>
 
-.wrapper {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
+<style scoped lang="scss">
 
-.item {
-  height: 80px;
-  width: 100%;
-  display: block;
-}
+  @import '~kolibri-design-system/lib/styles/definitions';
 
-.top-bar {
-  position: relative;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 40px;
-  /*line-height: 40px;*/
-  background-color: #fff;
+  $icon-size: 32px;
+  $progress-width: 48px;
+  $item-padding-x: 8px;
 
-}
+  .wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .item {
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 56px;
+    padding: 16px $item-padding-x;
+    margin-top: 8px;
+  }
+
+  .activity-icon,
+  .topic-icon {
+    position: absolute;
+    top: 0;
+    left: $item-padding-x;
+    display: inline-block;
+    width: $icon-size;
+    height: $icon-size;
+  }
+
+  .content-meta {
+    position: absolute;
+    left: calc(#{$icon-size} + #{$item-padding-x});
+    display: inline-block;
+    width: calc(100% - #{$icon-size} - #{$progress-width} - #{$item-padding-x * 2});
+    height: 56px;
+    padding: 0 16px;
+  }
+
+  .progress {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    .bar {
+      width: $progress-width;
+      margin-top: 16px;
+    }
+  }
+
+  .mastered-icon {
+    top: 0;
+    right: 0;
+    width: 24px;
+    height: 24px;
+  }
+
+  .top-bar {
+    position: relative;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 40px;
+
+    /* line-height: 40px; */
+    background-color: #ffffff;
+  }
+
+  /** Just ensures a newline precedes this. So it is always
+  positioned under the title at the start of the line */
+  .time-duration::before {
+    white-space: pre;
+    content: '\a';
+  }
+
 </style>
