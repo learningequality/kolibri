17ad36769589b186ff26fd45e97d864376243ab8
Marcella Maki <marcellamaki@gmail.com>
2021-11-11 16:12:46 -0500

    Update genContentLink to have default values to prevent regressions

--- kolibri/plugins/learn/assets/src/utils/genContentLink.js
+++ kolibri/plugins/learn/assets/src/utils/genContentLink.js
@@ -3,7 +3,13 @@ import { PageNames } from '../constants';
 // previous context allows to pass in ids or params from the last route
 // topic id is passed to account for variability in topic search
 // and parent/ancestor id navigation
-export default function genContentLink(id, topicId, isLeaf, last, prevContext = {}) {
+export default function genContentLink(
+  id,
+  topicId = null,
+  isLeaf = false,
+  last = null,
+  prevContext = {}
+) {
   const query = { ...prevContext };
   if (last) {
     query.last = last;
