0893b3211e53ad2793e2b3ccb4079611d556cfb9
Blaine Jester <blainesworld@gmail.com>
2021-11-18 14:04:09 -0800

    Add frontend resource methods for new endpoints

--- kolibri/core/assets/src/api-resources/attemptLog.js
+++ kolibri/core/assets/src/api-resources/attemptLog.js
@@ -2,4 +2,36 @@ import { Resource } from 'kolibri.lib.apiResource';
 
 export default new Resource({
   name: 'attemptlog',
+
+  /**
+   * Return attempt logs with previous attempt diff annotated on each attempt. If there wasn't a
+   * prior attempt, the diff will be null, otherwise an object with key `correct`
+   *
+   * @param {String|null} masterylog_id
+   * @param {String|null} content_id
+   * @param {String|null} item
+   * @param {String|null} user_id
+   * @return {Promise}
+   */
+  fetchCollectionWithDiff(masterylog_id = null, content_id = null, item = null, user_id = null) {
+    // requires masterylog or content_id with item or user_id
+    if (!masterylog_id && !(content_id && (item || user_id))) {
+      return Promise.reject(Error('Missing required parameters'));
+    }
+
+    const getParams = {};
+    if (masterylog_id) {
+      getParams.masterylog = masterylog_id;
+    }
+    if (content_id) {
+      getParams.content = content_id;
+    }
+    if (user_id) {
+      getParams.user = user_id;
+    }
+    if (item) {
+      getParams.item = item;
+    }
+    return this.fetchListCollection('diff', getParams);
+  },
 });
--- kolibri/core/assets/src/api-resources/masteryLog.js
+++ kolibri/core/assets/src/api-resources/masteryLog.js
@@ -2,4 +2,28 @@ import { Resource } from 'kolibri.lib.apiResource';
 
 export default new Resource({
   name: 'masterylog',
+
+  /**
+   * Gets the list of mastery logs (tries) for a particular content and user combo, with:
+   *  - `correct`: total correct for the try
+   *
+   * @param {String} content_id
+   * @param {String} user_id
+   * @return {Promise}
+   */
+  fetchSummary(content_id, user_id) {
+    return this.fetchListCollection('summary', { content: content_id, user: user_id });
+  },
+
+  /**
+   * Get's the diff of this mastery log compared with the previous attempt, with annotations:
+   *  - `diff`: object or null, with `correct` and `time_spent` diffs
+   *  - `attemptlogs`: list of attempt logs with annotated diff with previous try's attempt
+   *
+   * @param {String} id masterylog_id
+   * @return {Promise}
+   */
+  fetchDiff(id) {
+    return this.fetchDetailModel('diff', id);
+  },
 });
