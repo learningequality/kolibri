c0237b343eb4825150677f335d144b3308c3eba0
Blaine Jester <blainesworld@gmail.com>
2021-11-18 17:29:42 -0800

    Check for detail attr for list filtering

--- kolibri/core/auth/api.py
+++ kolibri/core/auth/api.py
@@ -85,14 +85,13 @@ class KolibriAuthPermissionsFilter(filters.BaseFilterBackend):
     to the ``filter_readable`` method on ``KolibriAbstractBaseUser``.
     """
 
-    def __init__(self, endpoints=None):
-        self.endpoints = endpoints or ["-list"]
-
     def filter_queryset(self, request, queryset, view):
-        if request.method == "GET" and any(
-            request.resolver_match.url_name.endswith(endpoint)
-            for endpoint in self.endpoints
-        ):
+        # if the url name ends with "-list" or the endpoint is explicitly declared a list endpoint
+        # with .detail=False
+        is_list = request.resolver_match.url_name.endswith("-list") or not getattr(
+            request.resolver_match.func, "detail", True
+        )
+        if request.method == "GET" and is_list:
             # only filter down the queryset in the case of the list view being requested
             return request.user.filter_readable(queryset)
         # otherwise, return the full queryset, as permission checks will happen object-by-object
