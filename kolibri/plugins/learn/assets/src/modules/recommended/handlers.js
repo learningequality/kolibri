a25c68d56f24cc25228b197b6597649339856650
Richard Tibbles <richard@learningequality.org>
2021-11-20 05:55:02 -0800

    Prevent unnecessary fetches on the library page.

--- kolibri/plugins/learn/assets/src/modules/recommended/handlers.js
+++ kolibri/plugins/learn/assets/src/modules/recommended/handlers.js
@@ -4,17 +4,15 @@ import samePageCheckGenerator from 'kolibri.utils.samePageCheckGenerator';
 import ConditionalPromise from 'kolibri.lib.conditionalPromise';
 import { PageNames } from '../../constants';
 import useChannels from '../../composables/useChannels';
-import useContentNodeProgress from '../../composables/useContentNodeProgress';
 import useLearnerResources from '../../composables/useLearnerResources';
+import { searchKeys } from '../../composables/useSearch';
 import { _collectionState } from '../coreLearn/utils';
 
 const { channels } = useChannels();
 
-const { fetchContentNodeProgress } = useContentNodeProgress();
-
 const { fetchResumableContentNodes } = useLearnerResources();
 
-export function showLibrary(store) {
+export function showLibrary(store, query) {
   if (!get(channels).length) {
     return;
   }
@@ -24,41 +22,45 @@ export function showLibrary(store) {
     store.commit('CORE_SET_PAGE_LOADING', true);
   }
 
-  const promises = [
-    ContentNodeResource.fetchCollection({
-      getParams: {
-        parent__isnull: true,
-        include_coach_content:
-          store.getters.isAdmin || store.getters.isCoach || store.getters.isSuperuser,
-      },
-    }),
-  ];
+  const promises = [];
 
-  if (store.getters.isUserLoggedIn) {
-    fetchContentNodeProgress({ resume: true });
-    promises.push(fetchResumableContentNodes());
+  if (!searchKeys.some(key => query[key])) {
+    promises.push(
+      ContentNodeResource.fetchCollection({
+        getParams: {
+          parent__isnull: true,
+          include_coach_content:
+            store.getters.isAdmin || store.getters.isCoach || store.getters.isSuperuser,
+        },
+      })
+    );
+    if (store.getters.isUserLoggedIn) {
+      promises.push(fetchResumableContentNodes());
+    }
   }
 
   return ConditionalPromise.all(promises).only(
     samePageCheckGenerator(store),
     ([channelCollection]) => {
-      // we want them to be in the same order as the channels list
-      const rootNodes = get(channels)
-        .map(channel => {
-          const node = _collectionState(channelCollection).find(n => n.channel_id === channel.id);
-          if (node) {
-            // The `channel` comes with additional data that is
-            // not returned from the ContentNodeResource.
-            // Namely thumbnail, description and tagline (so far)
-            node.title = channel.name || node.title;
-            node.thumbnail = channel.thumbnail;
-            node.description = channel.tagline || channel.description;
-            return node;
-          }
-        })
-        .filter(Boolean);
+      if (channelCollection && channelCollection.length) {
+        // we want them to be in the same order as the channels list
+        const rootNodes = get(channels)
+          .map(channel => {
+            const node = _collectionState(channelCollection).find(n => n.channel_id === channel.id);
+            if (node) {
+              // The `channel` comes with additional data that is
+              // not returned from the ContentNodeResource.
+              // Namely thumbnail, description and tagline (so far)
+              node.title = channel.name || node.title;
+              node.thumbnail = channel.thumbnail;
+              node.description = channel.tagline || channel.description;
+              return node;
+            }
+          })
+          .filter(Boolean);
 
-      store.commit('SET_ROOT_NODES', rootNodes);
+        store.commit('SET_ROOT_NODES', rootNodes);
+      }
 
       store.commit('CORE_SET_PAGE_LOADING', false);
       store.commit('CORE_SET_ERROR', null);
