a16dd78fe351bb7f09deb5612864979c24dea093
Devon Rueckner <devon.rueckner@gmail.com>
2021-12-11 19:57:18 -0500

    replace python constants with strings

--- kolibri/core/theme_hook.py
+++ kolibri/core/theme_hook.py
@@ -10,71 +10,43 @@ from kolibri.plugins import hooks
 logger = logging.getLogger(__name__)
 
 
-# These constants are used by theme.js and the $theme mixin on the front-end
-TOKEN_MAPPING = "tokenMapping"
-BRAND_COLORS = "brandColors"
-PRIMARY = "primary"
-SECONDARY = "secondary"
-COLOR_V50 = "v_50"
-COLOR_V100 = "v_100"
-COLOR_V200 = "v_200"
-COLOR_V300 = "v_300"
-COLOR_V400 = "v_400"
-COLOR_V500 = "v_500"
-COLOR_V600 = "v_600"
-COLOR_V700 = "v_700"
-COLOR_V800 = "v_800"
-COLOR_V900 = "v_900"
-SIGN_IN = "signIn"
-SIDE_NAV = "sideNav"
-APP_BAR = "appBar"
-BACKGROUND = "background"
-BACKGROUND_IMG_CREDIT = "backgroundImgCredit"
-SCRIM_OPACITY = "scrimOpacity"
-TITLE = "title"
-TITLE_STYLE = "titleStyle"
-TOP_LOGO = "topLogo"
-LOGO = "logo"
-BRANDED_FOOTER = "brandedFooter"
-PARAGRAPH_ARRAY = "paragraphArray"
-IMG_SRC = "src"
-IMG_STYLE = "style"
-IMG_ALT = "alt"
-SHOW_TITLE = "showTitle"
-SHOW_K_FOOTER_LOGO = "showKolibriFooterLogo"
-SHOW_POWERED_BY = "showPoweredBy"
-POWERED_BY_STYLE = "poweredByStyle"
+_TOKEN_MAPPING = "tokenMapping"
+_BRAND_COLORS = "brandColors"
+_SIGN_IN = "signIn"
+_SIDE_NAV = "sideNav"
+_APP_BAR = "appBar"
 
 
 def _validateBrandColors(theme):
-    if BRAND_COLORS not in theme:
+    if _BRAND_COLORS not in theme:
         logger.error("brand colors not defined by theme")
         return
 
-    required_colors = [PRIMARY, SECONDARY]
+    required_colors = ["primary", "secondary"]
     color_names = [
-        COLOR_V50,
-        COLOR_V100,
-        COLOR_V200,
-        COLOR_V300,
-        COLOR_V400,
-        COLOR_V500,
-        COLOR_V600,
-        COLOR_V700,
-        COLOR_V800,
-        COLOR_V900,
+        "v_50",
+        "v_100",
+        "v_200",
+        "v_300",
+        "v_400",
+        "v_500",
+        "v_600",
+        "v_700",
+        "v_800",
+        "v_900",
     ]
     for color in required_colors:
-        if color not in theme[BRAND_COLORS]:
+        if color not in theme[_BRAND_COLORS]:
             logger.error("'{}' not defined by theme".format(color))
         for name in color_names:
-            if name not in theme[BRAND_COLORS][color]:
+            if name not in theme[_BRAND_COLORS][color]:
                 logger.error("{} '{}' not defined by theme".format(color, name))
 
 
 def _validateScrimOpacity(theme):
-    if SCRIM_OPACITY in theme[SIGN_IN]:
-        opacity = theme[SIGN_IN][SCRIM_OPACITY]
+    SCRIM_OPACITY = "scrimOpacity"
+    if SCRIM_OPACITY in theme[_SIGN_IN]:
+        opacity = theme[_SIGN_IN][SCRIM_OPACITY]
         if opacity is None or opacity < 0 or opacity > 1:
             logger.error("scrim opacity should be a value in the closed interval [0,1]")
             return
@@ -84,14 +56,14 @@ def _initFields(theme):
     """
     set up top-level dicts if they don't exist
     """
-    if SIGN_IN not in theme:
-        theme[SIGN_IN] = {}
-    if TOKEN_MAPPING not in theme:
-        theme[TOKEN_MAPPING] = {}
-    if SIDE_NAV not in theme:
-        theme[SIDE_NAV] = {}
-    if APP_BAR not in theme:
-        theme[APP_BAR] = {}
+    if _SIGN_IN not in theme:
+        theme[_SIGN_IN] = {}
+    if _TOKEN_MAPPING not in theme:
+        theme[_TOKEN_MAPPING] = {}
+    if _SIDE_NAV not in theme:
+        theme[_SIDE_NAV] = {}
+    if _APP_BAR not in theme:
+        theme[_APP_BAR] = {}
 
 
 @hooks.define_hook(only_one_registered=True)
