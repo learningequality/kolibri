351d95116141a25905b48c9d19a3d1c47d0c6d88
Devon Rueckner <devon.rueckner@gmail.com>
2021-12-12 14:10:27 -0500

    work on theme validation

--- kolibri/core/assets/src/styles/initializeTheme.js
+++ kolibri/core/assets/src/styles/initializeTheme.js
@@ -4,6 +4,7 @@ import generateGlobalStyles from 'kolibri-design-system/lib/styles/generateGloba
 import Vue from 'vue';
 import trackInputModality from 'kolibri-design-system/lib/styles/trackInputModality';
 import trackMediaType from 'kolibri-design-system/lib/styles/trackMediaType';
+import validateTheme from './validateTheme';
 import themeConfig from './themeConfig';
 import plugin_data from 'plugin_data';
 
@@ -14,6 +15,7 @@ export function setThemeConfig(theme) {
 }
 
 export default function initializeTheme() {
+  validateTheme(plugin_data.kolibriTheme);
   setBrandColors(plugin_data.kolibriTheme.brandColors);
   setTokenMapping(plugin_data.kolibriTheme.tokenMapping);
   setThemeConfig(plugin_data.kolibriTheme);
--- kolibri/core/theme_hook.py
+++ kolibri/core/theme_hook.py
@@ -11,45 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 _TOKEN_MAPPING = "tokenMapping"
-_BRAND_COLORS = "brandColors"
 _SIGN_IN = "signIn"
 _SIDE_NAV = "sideNav"
 _APP_BAR = "appBar"
-
-
-def _validateBrandColors(theme):
-    if _BRAND_COLORS not in theme:
-        logger.error("brand colors not defined by theme")
-        return
-
-    required_colors = ["primary", "secondary"]
-    color_names = [
-        "v_50",
-        "v_100",
-        "v_200",
-        "v_300",
-        "v_400",
-        "v_500",
-        "v_600",
-        "v_700",
-        "v_800",
-        "v_900",
-    ]
-    for color in required_colors:
-        if color not in theme[_BRAND_COLORS]:
-            logger.error("'{}' not defined by theme".format(color))
-        for name in color_names:
-            if name not in theme[_BRAND_COLORS][color]:
-                logger.error("{} '{}' not defined by theme".format(color, name))
-
-
-def _validateScrimOpacity(theme):
-    SCRIM_OPACITY = "scrimOpacity"
-    if SCRIM_OPACITY in theme[_SIGN_IN]:
-        opacity = theme[_SIGN_IN][SCRIM_OPACITY]
-        if opacity is None or opacity < 0 or opacity > 1:
-            logger.error("scrim opacity should be a value in the closed interval [0,1]")
-            return
 
 
 def _initFields(theme):
@@ -77,8 +41,6 @@ class ThemeHook(hooks.KolibriHook):
     def get_theme(cls):
         theme = list(cls.registered_hooks)[0].theme
         _initFields(theme)
-        _validateBrandColors(theme)
-        _validateScrimOpacity(theme)
         return theme
 
     @abstractproperty
