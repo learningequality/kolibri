c203d7aaac0fc46ef53cab08da7b043712926d53
José Redrejo <jose@learningequality.org>
2021-12-13 18:14:50 +0100

    Save all the device info & filter when not in content

--- kolibri/core/discovery/serializers.py
+++ kolibri/core/discovery/serializers.py
@@ -41,12 +41,16 @@ class NetworkLocationSerializer(serializers.ModelSerializer):
             "subset_of_users_device",
         )
 
-    def validate_base_url(self, value):
+    def validate(self, data):
         try:
-            client = NetworkClient(address=value)
+            client = NetworkClient(address=data["base_url"])
         except errors.NetworkError as e:
             raise ValidationError(
-                "Error with address {} ({})".format(value, e.__class__.__name__),
+                "Error with address {} ({})".format(
+                    data["base_url"], e.__class__.__name__
+                ),
                 code=e.code,
             )
-        return client.base_url
+        info = {k: v for (k, v) in client.info.items() if v is not None}
+        data.update(info)
+        return super(NetworkLocationSerializer, self).validate(data)
