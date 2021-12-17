2012dbca8671439b57a4ed42c79542ef6b524d43
Richard Tibbles <richard@learningequality.org>
2021-12-17 10:24:15 -0800

    Add regression tests for bug fixes and fix behaviour.

--- kolibri/core/tasks/job.py
+++ kolibri/core/tasks/job.py
@@ -250,7 +250,7 @@ class Job(object):
                     "storage is not defined on this job, cannot update progress"
                 )
             self.progress = progress
-            self.total_progress = progress
+            self.total_progress = total_progress
             self.storage.update_job_progress(self.job_id, progress, total_progress)
 
     def check_for_cancel(self):
@@ -270,7 +270,7 @@ class Job(object):
             raise ReferenceError(
                 "storage is not defined on this job, cannot save as cancellable"
             )
-        self.job.cancellable = cancellable
+        self.cancellable = cancellable
         self.storage.save_job_as_cancellable(self.job_id, cancellable=cancellable)
 
     @property
