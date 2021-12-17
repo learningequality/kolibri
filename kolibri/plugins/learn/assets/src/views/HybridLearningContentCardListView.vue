2bd5f0ab6902863f8851833ba95974651ed53259
Devon Rueckner <devon.rueckner@gmail.com>
2021-12-16 19:42:43 -0500

    refactor

--- kolibri/plugins/learn/assets/src/views/HybridLearningContentCardListView.vue
+++ kolibri/plugins/learn/assets/src/views/HybridLearningContentCardListView.vue
@@ -6,84 +6,65 @@
       :class="{ 'mobile-card': isMobile }"
       :style="{ backgroundColor: $themeTokens.surface }"
     >
-      <div
-        v-if="!isMobile"
-        class="folder-header"
-        :style="{ backgroundColor: (!content.is_leaf ? $themeTokens.text : null ) }"
-      ></div>
-      <div class="thumbnail">
-        <CardThumbnail
-          :kind="content.kind"
-          v-bind="{ thumbnail, isMobile }"
-          :activityLength="content.duration"
-        />
-      </div>
-      <span class="details" :style="{ color: $themeTokens.text }">
-        <div
-          class="metadata-info"
-          :style="{ color: $themePalette.grey.v_700 }"
-        >
-          <LearningActivityLabel
-            :contentNode="content"
-            labelAfter
-            condensed
+      <KFixedGrid numCols="4">
+        <KFixedGridItem :span="isMobile ? 4 : 1" class="thumb-area">
+          <CardThumbnail
+            :kind="content.kind"
+            v-bind="{ thumbnail, isMobile }"
+            :activityLength="content.duration"
           />
-        </div>
-        <h3 class="title">
-          <TextTruncator
-            :text="content.title"
-            :maxHeight="maxTitleHeight"
-          />
-        </h3>
-        <p class="text">
-          <TextTruncator
-            :text="content.description"
-            :maxHeight="maxDescriptionHeight"
-          />
-        </p>
-        <div v-if="displayCategoryAndLevelMetadata && !isMobile" class="metadata-info">
-          <p> {{ coreString(displayCategoryAndLevelMetadata) }}</p>
-        </div>
-        <img
-          v-if="!isMobile"
-          :src="content.channel_thumbnail"
-          :alt="learnString('logo', { channelTitle: content.channel_title })"
-          class="channel-logo"
-        >
-        <KButton
-          v-if="!isMobile && isLibraryPage && content.copies"
-          appearance="basic-link"
-          class="copies"
-          :style="{ color: $themeTokens.text }"
-          :text="coreString('copies', { num: content.copies.length })"
-          @click.prevent="$emit('openCopiesModal', content.copies)"
-        />
-      </span>
-      <div class="footer">
-        <div class="footer-elements">
-          <div class="footer-progress">
-            <p
-              v-if="isBookmarksPage"
-              class="metadata-info-footer"
-              :style="{ color: $themePalette.grey.v_700 }"
-            >
-              {{ bookmarkCreated }}
+          <p
+            v-if="isBookmarksPage"
+            class="created-info"
+            :style="{ color: $themePalette.grey.v_700 }"
+          >
+            {{ bookmarkCreated }}
+          </p>
+          <ProgressBar :contentNode="content" />
+        </KFixedGridItem>
+
+        <KFixedGridItem :span="isMobile ? 4 : 3" class="text-area">
+          <span :style="{ color: $themeTokens.text }">
+            <div class="metadata-info" :style="{ color: $themePalette.grey.v_700 }">
+              <LearningActivityLabel :contentNode="content" labelAfter condensed />
+            </div>
+            <h3>
+              <TextTruncatorCss :text="content.title" :maxLines="1" />
+            </h3>
+            <p v-if="content.description" style="font-size: 14px;">
+              <TextTruncatorCss :text="content.description" :maxLines="4" />
             </p>
-            <ProgressBar :contentNode="content" />
-          </div>
-        </div>
-        <div class="footer-elements footer-icons">
-          <KIconButton
-            v-for="(value, key) in footerIcons"
-            :key="key"
-            :icon="key"
-            size="mini"
-            :color="$themePalette.grey.v_400"
-            :ariaLabel="coreString(value)"
-            :tooltip="coreString(value)"
-            @click.prevent="$emit(value)"
-          />
-        </div>
+            <div v-if="!isMobile" class="bottom-items">
+              <img
+                :src="content.channel_thumbnail"
+                :alt="learnString('logo', { channelTitle: content.channel_title })"
+                class="channel-logo"
+              >
+              <KButton
+                v-if="isLibraryPage && content.copies"
+                appearance="basic-link"
+                class="copies"
+                :style="{ color: $themeTokens.text }"
+                :text="coreString('copies', { num: content.copies.length })"
+                @click.prevent="$emit('openCopiesModal', content.copies)"
+              />
+            </div>
+          </span>
+        </KFixedGridItem>
+      </KFixedGrid>
+
+      <div class="footer">
+        <KIconButton
+          v-for="(value, key) in footerIcons"
+          :key="key"
+          :icon="key"
+          size="mini"
+          :color="$themePalette.grey.v_400"
+          :ariaLabel="coreString(value)"
+          :tooltip="coreString(value)"
+          class="icon-fix"
+          @click.prevent="$emit(value)"
+        />
       </div>
     </div>
   </router-link>
@@ -94,7 +75,7 @@
 <script>
 
   import { validateLinkObject } from 'kolibri.utils.validators';
-  import TextTruncator from 'kolibri.coreVue.components.TextTruncator';
+  import TextTruncatorCss from 'kolibri.coreVue.components.TextTruncatorCss';
   import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
   import { now } from 'kolibri.utils.serverClock';
   import { PageNames } from '../constants';
@@ -107,7 +88,7 @@
     name: 'HybridLearningContentCardListView',
     components: {
       CardThumbnail,
-      TextTruncator,
+      TextTruncatorCss,
       LearningActivityLabel,
       ProgressBar,
     },
@@ -147,23 +128,6 @@
       now: now(),
     }),
     computed: {
-      maxTitleHeight() {
-        return 40;
-      },
-      maxDescriptionHeight() {
-        return 64;
-      },
-      displayCategoryAndLevelMetadata() {
-        if (this.category && this.level) {
-          return `${this.category}| ${this.level} `;
-        } else if (this.category) {
-          return this.category;
-        } else if (this.level) {
-          return this.level;
-        } else {
-          return null;
-        }
-      },
       isLibraryPage() {
         return this.currentPage === PageNames.LIBRARY;
       },
@@ -189,7 +153,10 @@
 <style lang="scss" scoped>
 
   @import '~kolibri-design-system/lib/styles/definitions';
-  @import './ContentCard/card';
+
+  $footer-height: 36px;
+  $h-padding: 24px;
+  $v-padding: 16px;
 
   .card {
     @extend %dropshadow-1dp;
@@ -197,7 +164,8 @@
     position: relative;
     display: inline-block;
     width: 100%;
-    height: 246px;
+    min-height: 246px;
+    padding: $v-padding $h-padding;
     text-decoration: none;
     vertical-align: top;
     border-radius: 8px;
@@ -208,32 +176,18 @@
     }
   }
 
-  .details {
-    display: inline-block;
-    max-width: calc(100% - 350px);
-    margin: 24px;
-    margin-top: 8px;
-    vertical-align: top;
-  }
-
-  .title {
-    margin: 0;
-  }
-
-  .text {
-    font-size: 14px;
+  .mobile-card {
+    min-height: 490px;
   }
 
   .metadata-info {
-    margin-top: 8px;
-    margin-bottom: 8px;
     font-size: 14px;
   }
 
   .channel-logo {
     display: inline-block;
     height: 24px;
-    margin: 4px;
+    margin-right: 8px;
   }
 
   .copies {
@@ -244,78 +198,40 @@
     vertical-align: top;
   }
 
-  .folder-header {
-    width: 100%;
-    height: 15px;
-    border-radius: 8px 8px 0 0;
-  }
-
   .footer {
     position: absolute;
+    right: 0;
     bottom: 0;
-    width: 100%;
-    padding: 16px 24px;
+    height: $footer-height;
+    margin-right: $h-padding;
+    margin-bottom: $v-padding;
   }
 
-  .metadata-info-footer {
-    margin: 0;
+  .created-info {
     font-size: 13px;
   }
 
-  .footer-elements {
-    position: relative;
-    display: block;
-  }
-
-  .footer-progress {
-    width: 240px;
-  }
-
-  .footer-icons {
+  .icon-fix {
     // this override fixes an existing KDS bug with
     // the hover state circle being squished
     // and can be removed upon that hover state fix
-    float: right;
-    .button {
-      width: 32px !important;
-      height: 32px !important;
-      /deep/ svg {
-        top: 4px !important;
-      }
+    width: 32px !important;
+    height: 32px !important;
+    /deep/ svg {
+      top: 4px !important;
     }
   }
 
-  .k-linear-loader {
-    display: block;
-    width: 240px;
-    margin-top: -4px;
-  }
-
-  .thumbnail {
-    display: inline-block;
-    width: 240px;
+  .bottom-items {
     margin-top: 8px;
-    margin-right: 24px;
-    margin-left: 24px;
   }
 
-  .mobile-card.card {
-    width: 100%;
-    height: 490px;
+  .thumb-area {
+    margin-bottom: 16px;
   }
 
-  .mobile-card {
-    .thumbnail {
-      position: absolute;
-      top: 24px;
-      width: calc(100% - 48px);
-      height: calc(100% - 24px);
-      margin-left: 24px;
-    }
-    .details {
-      max-width: 100%;
-      margin-top: 224px;
-    }
+  .text-area {
+    margin-bottom: $footer-height;
   }
 
 </style>
