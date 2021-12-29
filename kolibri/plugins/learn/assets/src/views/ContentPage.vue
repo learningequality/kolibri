3363add1be07b8e924e46a7fef20272f7581d3e4
sairina <sairina.merinotsui@gmail.com>
2021-12-29 14:15:19 -0800

    Remove hideSubmitModal hack

--- kolibri/plugins/learn/assets/src/views/ContentPage.vue
+++ kolibri/plugins/learn/assets/src/views/ContentPage.vue
@@ -38,7 +38,6 @@
         :mastered="complete"
         :masteryLevel="masteryLevel"
         :updateContentSession="wrappedUpdateContentSession"
-        :hideSubmitModal="progress >= 1 && wasIncomplete"
         @startTracking="startTracking"
         @stopTracking="stopTracking"
         @updateInteraction="updateInteraction"
