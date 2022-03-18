5f1eda4ef710550b170391d13c651eec8061cdf8
Richard Tibbles <richard@learningequality.org>
2022-03-17 19:11:12 -0700

    Vendor runnable. Defer to server for running port.

--- platforms/android/src/main.py
+++ platforms/android/src/main.py
@@ -1,22 +1,30 @@
 import initialization  # keep this first, to ensure we're set up for other imports
 
 import logging
+import time
 
-from config import KOLIBRI_PORT
 from jnius import autoclass
 
 from android_utils import start_service
+from runnable import Runnable
 
 # we need to initialize Kolibri to allow us to access the app key
 from kolibri.utils.cli import initialize
 from kolibri.plugins.app.utils import interface
-from kolibri.utils.server import wait_for_status
 from kolibri.utils.server import STATUS_RUNNING
+from kolibri.utils.server import wait_for_status
+from kolibri.utils.server import _read_pid_file
+from kolibri.utils.server import PID_FILE
 
 
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
+loadUrl = Runnable(PythonActivity.mWebView.loadUrl)
+
 logging.info("Initializing Kolibri and running any upgrade routines")
+
+loadUrl("file:///android_asset/_load.html")
+
 initialize()
 
 # start kolibri server
@@ -25,8 +33,12 @@ start_service("server")
 start_service("remoteshell")
 
 # Tie up this thread until the server is running
-while not wait_for_status(STATUS_RUNNING):
-    logging.info("Kolibri server not yet started")
+wait_for_status(STATUS_RUNNING, timeout=120)
 
-start_url = "http://127.0.0.1:{port}".format(port=KOLIBRI_PORT) + interface.get_initialize_url()
-PythonActivity.mWebView.loadUrl(start_url)
+_, port, _, _ = _read_pid_file(PID_FILE)
+
+start_url = "http://127.0.0.1:{port}".format(port=port) + interface.get_initialize_url()
+loadUrl(start_url)
+
+while True:
+    time.sleep(0.05)
---
+++ platforms/android/src/runnable.py
@@ -0,0 +1,39 @@
+'''
+Runnable
+========
+
+'''
+
+from jnius import PythonJavaClass, java_method, autoclass
+
+# reference to the activity
+_PythonActivity = autoclass('org.kivy.android.PythonActivity')
+
+
+class Runnable(PythonJavaClass):
+    '''Wrapper around Java Runnable class. This class can be used to schedule a
+    call of a Python function into the PythonActivity thread.
+    '''
+
+    __javainterfaces__ = ['java/lang/Runnable']
+    __runnables__ = []
+
+    def __init__(self, func):
+        super(Runnable, self).__init__()
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        Runnable.__runnables__.append(self)
+        _PythonActivity.mActivity.runOnUiThread(self)
+
+    @java_method('()V')
+    def run(self):
+        try:
+            self.func(*self.args, **self.kwargs)
+        except:
+            import traceback
+            traceback.print_exc()
+
+        Runnable.__runnables__.remove(self)
