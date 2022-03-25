8c8faa4a16a572f4fb47c0ee5ccb45da30c24275
Marcella Maki <marcellamaki@gmail.com>
2022-03-25 16:17:48 -0400

    Remove Corebase related code from LearnIndex

--- kolibri/plugins/learn/assets/src/views/LearnIndex.vue
+++ kolibri/plugins/learn/assets/src/views/LearnIndex.vue
@@ -8,29 +8,16 @@
     :content="content"
   />
 
-  <CoreBase
+  <NotificationsRoot
     v-else
-    :marginBottom="bottomSpaceReserved"
-    :showSubNav="topNavIsVisible"
     :authorized="userIsAuthorized"
     authorizedRole="registeredUser"
-    v-bind="immersivePageProps"
   >
-
-    <template #sub-nav>
-      <LearnTopNav />
-    </template>
-
-    <template #totalPointsMenuItem>
-      <TotalPoints />
-    </template>
-
     <div>
-      <component :is="currentPage" v-if="currentPage" />
       <router-view />
     </div>
 
-  </CoreBase>
+  </NotificationsRoot>
 
 </template>
 
@@ -38,62 +25,24 @@
 <script>
 
   import { mapGetters, mapState } from 'vuex';
-  import lastItem from 'lodash/last';
   import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
   import responsiveWindowMixin from 'kolibri.coreVue.mixins.responsiveWindowMixin';
-  import CoreBase from 'kolibri.coreVue.components.CoreBase';
-  import { PageNames, ClassesPageNames } from '../constants';
-  import useChannels from '../composables/useChannels';
+  import NotificationsRoot from 'kolibri.coreVue.components.NotificationsRoot';
+  import { PageNames } from '../constants';
   import commonLearnStrings from './commonLearnStrings';
-  import TopicsPage from './TopicsPage';
-  import ContentUnavailablePage from './ContentUnavailablePage';
   import LearnImmersiveLayout from './LearnImmersiveLayout';
-  import ExamPage from './ExamPage';
-  import ExamReportViewer from './LearnExamReportViewer';
-  import TotalPoints from './TotalPoints';
-  import AllClassesPage from './classes/AllClassesPage';
-  import ClassAssignmentsPage from './classes/ClassAssignmentsPage';
-  import LessonPlaylistPage from './classes/LessonPlaylistPage';
-  import LearnTopNav from './LearnTopNav';
-  import { ASSESSMENT_FOOTER, QUIZ_FOOTER } from './footers.js';
-  import BookmarkPage from './BookmarkPage.vue';
   import plugin_data from 'plugin_data';
-
-  const pageNameToComponentMap = {
-    [PageNames.TOPICS_TOPIC]: TopicsPage,
-    [PageNames.TOPICS_TOPIC_SEARCH]: TopicsPage,
-    [PageNames.CONTENT_UNAVAILABLE]: ContentUnavailablePage,
-    [PageNames.BOOKMARKS]: BookmarkPage,
-    [ClassesPageNames.EXAM_VIEWER]: ExamPage,
-    [ClassesPageNames.EXAM_REPORT_VIEWER]: ExamReportViewer,
-    [ClassesPageNames.ALL_CLASSES]: AllClassesPage,
-    [ClassesPageNames.CLASS_ASSIGNMENTS]: ClassAssignmentsPage,
-    [ClassesPageNames.LESSON_PLAYLIST]: LessonPlaylistPage,
-  };
 
   export default {
     name: 'LearnIndex',
     components: {
-      CoreBase,
-      LearnTopNav,
-      TotalPoints,
+      NotificationsRoot,
       LearnImmersiveLayout,
     },
     mixins: [commonCoreStrings, commonLearnStrings, responsiveWindowMixin],
-    setup() {
-      const { channelsMap } = useChannels();
-      return {
-        channelsMap,
-      };
-    },
     computed: {
       ...mapGetters(['isUserLoggedIn']),
-      ...mapState('classAssignments', {
-        classroomName: state => state.currentClassroom.name,
-      }),
-      ...mapState('topicsTree', ['content', 'topic']),
-      ...mapState('examReportViewer', ['exam']),
-      ...mapState(['pageName']),
+      ...mapState('topicsTree', ['content']),
       userIsAuthorized() {
         if (this.pageName === PageNames.BOOKMARKS) {
           return this.isUserLoggedIn;
@@ -102,131 +51,14 @@
           (plugin_data.allowGuestAccess && this.$store.getters.allowAccess) || this.isUserLoggedIn
         );
       },
-      currentPage() {
-        return pageNameToComponentMap[this.pageName] || null;
-      },
       currentPageIsContent() {
-        return this.pageName === PageNames.TOPICS_CONTENT;
+        return this.$route.name === PageNames.TOPICS_CONTENT;
       },
-      currentTopicIsCustom() {
-        return (
-          this.topic && this.topic.options && this.topic.options.modality === 'CUSTOM_NAVIGATION'
-        );
-      },
-      immersivePageProps() {
-        if (this.pageName === ClassesPageNames.EXAM_VIEWER) {
-          return {
-            appBarTitle: this.classroomName || '',
-            immersivePage: true,
-            immersivePageRoute: this.$router.getRoute(ClassesPageNames.CLASS_ASSIGNMENTS),
-            immersivePagePrimary: true,
-            immersivePageIcon: 'close',
-          };
-        }
-        if (this.pageName === ClassesPageNames.EXAM_REPORT_VIEWER) {
-          if (this.exam) {
-            return {
-              appBarTitle: this.$tr('examReportTitle', {
-                examTitle: this.exam.title,
-              }),
-              immersivePage: true,
-              immersivePageRoute: this.$router.getRoute(ClassesPageNames.CLASS_ASSIGNMENTS),
-              immersivePagePrimary: false,
-              immersivePageIcon: 'close',
-            };
-          }
-        }
-        if (this.pageName === PageNames.TOPICS_TOPIC && this.currentTopicIsCustom) {
-          return {
-            appBarTitle: this.channelsMap[this.topic.channel_id].title || '',
-            immersivePage: true,
-            immersivePageRoute: this.$router.getRoute(PageNames.LIBRARY),
-            immersivePagePrimary: false,
-            immersivePageIcon: 'back',
-          };
-        }
-        if (this.pageName === PageNames.TOPICS_CONTENT) {
-          let immersivePageRoute = {};
-          let appBarTitle;
-          const { last } = this.$route.query;
-          if (last) {
-            // 'last' should only be route names for Recommended Page and its subpages
-            immersivePageRoute = this.$router.getRoute(last);
-            appBarTitle = {
-              [PageNames.RECOMMENDED_POPULAR]: this.learnString('popularLabel'),
-              [PageNames.RECOMMENDED_RESUME]: this.learnString('resumeLabel'),
-              [PageNames.RECOMMENDED_NEXT_STEPS]: this.learnString('nextStepsLabel'),
-              [PageNames.LIBRARY]: this.learnString('libraryLabel'),
-            }[last];
-          } else if (this.content.parent) {
-            // Need to guard for parent being non-empty to avoid console errors
-            immersivePageRoute = this.$router.getRoute(PageNames.TOPICS_TOPIC, {
-              id: this.content.parent,
-            });
-
-            if (this.content.ancestors.length > 1) {
-              appBarTitle = lastItem(this.content.ancestors).title;
-            } else {
-              // `ancestors` only has one entry if the direct parent is the channel
-              appBarTitle = this.channelsMap[this.content.channel_id].title;
-            }
-          }
-          return {
-            appBarTitle,
-            immersivePage: true,
-            immersivePageRoute,
-            immersivePagePrimary: false,
-            immersivePageIcon: 'close',
-          };
-        }
-        if (this.pageName === PageNames.LIBRARY) {
-          return {
-            appBarTitle: this.learnString('learnLabel'),
-            immersivePage: false,
-            hasSidebar: true,
-          };
-        }
-        if (
-          this.pageName === PageNames.TOPICS_TOPIC ||
-          this.pageName === PageNames.TOPICS_TOPIC_SEARCH
-        ) {
-          let immersivePageRoute;
-          if (this.$route.query.last == PageNames.HOME) {
-            immersivePageRoute = this.$router.getRoute(PageNames.HOME);
-          } else {
-            immersivePageRoute = this.$router.getRoute(PageNames.LIBRARY);
-          }
-          return {
-            appBarTitle: this.coreString('browseChannel'),
-            immersivePage: true,
-            immersivePageRoute,
-            immersivePagePrimary: true,
-            immersivePageIcon: 'close',
-            hasSidebar: true,
-          };
-        }
-        return {
-          appBarTitle: this.learnString('learnLabel'),
-          immersivePage: false,
-        };
-      },
-      topNavIsVisible() {
-        return (
-          this.pageName !== PageNames.CONTENT_UNAVAILABLE && !this.immersivePageProps.immersivePage
-        );
-      },
-      bottomSpaceReserved() {
-        if (this.pageName === ClassesPageNames.EXAM_VIEWER) {
-          return QUIZ_FOOTER;
-        }
-        let content;
-        if (this.pageName === PageNames.RECOMMENDED_CONTENT) {
-          content = this.content;
-        }
-        const isAssessment = content && content.assessment;
-        // height of .attempts-container in AssessmentWrapper
-        return isAssessment ? ASSESSMENT_FOOTER : 0;
-      },
+      // currentTopicIsCustom() {
+      //   return (
+      //     this.topic && this.topic.options && this.topic.options.modality === 'CUSTOM_NAVIGATION'
+      //   );
+      // },
       learnBackPageRoute() {
         // extract the key pieces of routing from immersive page props, but since we don't need
         // them all, just create two alternative route paths for return/'back' navigation
@@ -262,12 +94,6 @@
         return route;
       },
     },
-    $trs: {
-      examReportTitle: {
-        message: 'Report for { examTitle }',
-        context: 'Indicates the title of the quiz that the report corresponds to.',
-      },
-    },
   };
 
 </script>
@@ -278,6 +104,7 @@
   @import './learn';
 
   .content {
+    height: 100%;
     margin: auto;
   }
 
