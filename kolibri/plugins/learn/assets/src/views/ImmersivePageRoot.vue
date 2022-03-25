7320b3b3685363608fc7a647892fe6de02cc5914
Marcella Maki <marcellamaki@gmail.com>
2022-03-25 15:29:58 -0400

    Add new Learn components for standard and immersive pages, following FacilityIndex model

---
+++ kolibri/plugins/learn/assets/src/views/ImmersivePageRoot.vue
@@ -0,0 +1,66 @@
+<template>
+
+  <div class="main-wrapper" :style="wrapperStyles">
+    <ImmersiveToolbar
+      v-if="!loading"
+      ref="appBar"
+      :appBarTitle="appBarTitle"
+      :route="route"
+    />
+    <slot></slot>
+  </div>
+
+</template>
+
+
+<script>
+
+  import ImmersiveToolbar from 'kolibri.coreVue.components.ImmersiveToolbar';
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'ImmersivePageRoot',
+    components: { ImmersiveToolbar },
+    props: {
+      appBarTitle: {
+        type: String,
+        default: '',
+      },
+      route: {
+        type: Object,
+        default: null,
+      },
+      applyStandardLayout: {
+        type: Boolean,
+        default: true,
+      },
+    },
+    data() {
+      return {
+        appBarHeight: 0,
+      };
+    },
+    computed: {
+      ...mapState({
+        loading: state => state.core.loading,
+      }),
+      wrapperStyles() {
+        return this.applyStandardLayout
+          ? {
+              width: '100%',
+              display: 'inline-block',
+              backgroundColor: this.$themePalette.grey.v_100,
+              paddingLeft: '32px',
+              paddingRight: '32px',
+              paddingBottom: '72px',
+              paddingTop: this.appBarHeight + 16 + 'px',
+            }
+          : '';
+      },
+    },
+    mounted() {
+      this.appBarHeight = this.$refs.appBar.$el.clientHeight;
+    },
+  };
+
+</script>
---
+++ kolibri/plugins/learn/assets/src/views/LearnAppBarPage.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <AppBarCorePage :title="appBarTitle" :applyStandardLayout="applyStandardLayout">
+
+    <template #subNav>
+      <LearnTopNav ref="topNav" />
+    </template>
+
+    <div :style="styles">
+      <slot></slot>
+    </div>
+
+  </AppBarCorePage>
+
+</template>
+
+
+<script>
+
+  import AppBarCorePage from 'kolibri.coreVue.components.AppBarCorePage';
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import LearnTopNav from './LearnTopNav';
+
+  export default {
+    name: 'LearnAppBarPage',
+    components: { AppBarCorePage, LearnTopNav },
+    mixins: [commonCoreStrings],
+    props: {
+      appBarTitle: {
+        type: String,
+        default: null,
+      },
+      applyStandardLayout: {
+        type: Boolean,
+        default: true,
+      },
+    },
+    computed: {
+      styles() {
+        return this.applyStandardLayout ? 'max-width: 1000px; margin: 0 auto;' : '';
+      },
+    },
+  };
+
+</script>
