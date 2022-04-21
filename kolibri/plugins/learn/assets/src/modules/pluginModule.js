9bb3588fc95c9973c80acd0eb681b15d816f0733
Marcella Maki <marcellamaki@gmail.com>
2022-04-21 10:56:06 -0400

    Remove empty/unreferenced param objects

--- kolibri/plugins/learn/assets/src/modules/pluginModule.js
+++ kolibri/plugins/learn/assets/src/modules/pluginModule.js
@@ -1,3 +1,4 @@
+// import { LastPages } from './../constants/lastPagesConstants';
 import { PageNames, ClassesPageNames } from './../constants';
 import mutations from './coreLearn/mutations';
 import * as getters from './coreLearn/getters';
@@ -24,53 +25,42 @@ export default {
   getters: {
     ...getters,
     learnPageLinks() {
-      const params = {};
       return {
         HomePage: {
           name: PageNames.HOME,
-          params,
         },
         LibraryPage: {
           name: PageNames.LIBRARY,
-          params,
         },
         TopicsPage: {
           name: PageNames.TOPICS_TOPIC,
-          params,
         },
         TopicsSearchPage: {
           name: PageNames.TOPICS_TOPIC_SEARCH,
-          params,
         },
         ContentUnavailablePage: {
           name: PageNames.CONTENT_UNAVAILABLE,
-          params,
         },
         BookmarksPage: {
           name: PageNames.BOOKMARKS,
-          params,
         },
         ExamPage: id => {
           return {
             name: ClassesPageNames.EXAM_VIWER,
-            params: { params, quizId: id },
+            params: { quizId: id },
           };
         },
         ExamReportViewer: {
           name: ClassesPageNames.EXAM_REPORT_VIEWER,
-          params,
         },
         AllClassesPage: {
           name: ClassesPageNames.ALL_CLASSES,
-          params,
         },
         ClassAssignmentsPage: {
           name: ClassesPageNames.CLASS_ASSIGNMENTS,
-          params,
         },
         LessonPlaylistPage: {
           name: ClassesPageNames.LESSON_PLAYLIST,
-          params,
         },
       };
     },
