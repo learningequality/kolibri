6c47c1cc4d7debda4bf001b2e45a05216d84f556
Jacob Pierce <w.jacobpierce@gmail.com>
2022-04-22 09:55:12 -0700

    add router for topic-subsection test

--- kolibri/plugins/learn/assets/test/views/topic-row.spec.js
+++ kolibri/plugins/learn/assets/test/views/topic-row.spec.js
@@ -1,5 +1,18 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import VueRouter from 'vue-router';
 import TopicRow from '../../src/views/TopicsPage/TopicRow';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'TOPICS_TOPIC',
+    },
+  ],
+});
 
 describe('TopicRow', () => {
   it('displays a "more" button when the topic has more to show', () => {
@@ -12,6 +25,8 @@ describe('TopicRow', () => {
           showMore: true,
         },
       },
+      router,
+      localVue,
     });
     expect(wrapper.find('[data-test="more-button"]').exists()).toBe(true);
   });
