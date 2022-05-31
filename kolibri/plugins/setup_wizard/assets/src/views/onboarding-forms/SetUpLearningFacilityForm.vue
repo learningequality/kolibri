c5a93df3ce3ae6fcd624cfd2e9143c92680e805b
Marcella Maki <marcellamaki@gmail.com>
2022-05-31 15:14:43 -0400

    Create SetUpLearningFacilityForm

---
+++ kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/SetUpLearningFacilityForm.vue
@@ -0,0 +1,71 @@
+<template>
+
+  <OnboardingStepBase
+    :title="$tr('setUpFacilityTitle')"
+    :description="$tr('setUpFacilityDescription')"
+    @submit="handleSubmit"
+  >
+    <KRadioButton
+      v-model="selected"
+      :label="$tr('createFacilityLabel')"
+      value="CREATE"
+      class="radio-button"
+    />
+    <KRadioButton
+      v-model="selected"
+      :label="$tr('importFacilityLabel')"
+      value="IMPORT"
+      class="radio-button"
+    />
+  </OnboardingStepBase>
+
+</template>
+
+
+<script>
+
+  import OnboardingStepBase from '../OnboardingStepBase';
+
+  export default {
+    name: 'SetUpLearningFacilityForm',
+    components: {
+      OnboardingStepBase,
+    },
+
+    data() {
+      return {
+        selected: '',
+      };
+    },
+    $trs: {
+      setUpFacilityTitle: {
+        message: 'Set up learning facility for this full device',
+        context: '',
+      },
+      setUpFacilityDescription: {
+        message:
+          'Learning facility represents the location where you are installing Kolibri, such as a school, training center, or a home',
+        context: '',
+      },
+      createFacilityLabel: {
+        message: 'Create a new learning facility',
+        context: '',
+      },
+      importFacilityLabel: {
+        message: 'Import all data from an existing learning facility',
+        context: '',
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  .radio-button {
+    padding-bottom: 8px;
+    font-size: 0.875em;
+  }
+
+</style>
