8c8dff966d4deb7f159aba5043af0cc2f5f4acbc
Jacob Pierce <w.jacobpierce@gmail.com>
2022-06-01 15:30:38 -0700

    Clean up DeviceIndex

--- kolibri/plugins/device/assets/src/views/DeviceIndex.vue
+++ kolibri/plugins/device/assets/src/views/DeviceIndex.vue
@@ -1,15 +1,6 @@
 <template>
 
-  <!-- TODO Remove this stuff before making PR -->
-  <div
-    :appBarTitle="currentPageAppBarTitle"
-    :immersivePage="currentPageIsImmersive"
-    :immersivePagePrimary="immersivePagePrimary"
-    :immersivePageRoute="immersivePageRoute"
-    :immersivePageIcon="immersivePageIcon"
-    :toolbarTitle="currentPageAppBarTitle"
-    :showSubNav="canManageContent && !currentPageIsImmersive"
-  >
+  <div>
     <transition name="delay-entry">
       <PostSetupModalGroup
         v-if="welcomeModalVisible"
@@ -25,9 +16,8 @@
 
 <script>
 
-  import omit from 'lodash/omit';
-  import { mapState, mapGetters } from 'vuex';
-  import { ContentWizardPages, PageNames } from '../constants';
+  import { mapState } from 'vuex';
+  import { PageNames } from '../constants';
   import PostSetupModalGroup from './PostSetupModalGroup';
 
   const welcomeDimissalKey = 'DEVICE_WELCOME_MODAL_DISMISSED';
@@ -38,9 +28,7 @@
       PostSetupModalGroup,
     },
     computed: {
-      ...mapGetters(['canManageContent', 'isSuperuser']),
       ...mapState({ welcomeModalVisibleState: 'welcomeModalVisible' }),
-      ...mapState('coreBase', ['appBarTitle']),
       welcomeModalVisible() {
         return (
           this.welcomeModalVisibleState &&
@@ -49,93 +37,6 @@
       },
       pageName() {
         return this.$route.name;
-      },
-      currentPageAppBarTitle() {
-        if (this.pageName === PageNames.USER_PERMISSIONS_PAGE) {
-          return this.$tr('permissionsLabel');
-        } else {
-          return this.appBarTitle || this.$tr('deviceManagementTitle');
-        }
-      },
-      inMultipleImportPage() {
-        return this.pageName === 'AVAILABLE_CHANNELS' && this.$route.query.multiple;
-      },
-      immersivePageRoute() {
-        if (this.pageName === PageNames.MANAGE_TASKS) {
-          if (this.$route.query.last) {
-            const route = this.$router.getRoute(this.$route.query.last, {
-              channel_id: this.$route.query.channel_id,
-            });
-            return route;
-          } else {
-            return { name: PageNames.MANAGE_CONTENT_PAGE };
-          }
-        }
-        if (this.$route.query.last) {
-          return {
-            name: this.$route.query.last,
-            // TODO need to make query.last more sophisticated
-            // to handle longer breadcrumb trails
-            query: omit(this.$route.query, ['last']),
-          };
-        }
-        if (this.pageName === PageNames.MANAGE_CHANNEL) {
-          return { name: PageNames.MANAGE_CONTENT_PAGE };
-        }
-        // In all Import/Export pages, go back to ManageContentPage
-        if (this.inContentManagementPage) {
-          // If a user is selecting content, they should return to the content
-          // source that they're importing from using the query string.
-          if (this.inMultipleImportPage) {
-            return { query: omit(this.$route.query, ['multiple']) };
-          }
-          if (this.pageName === ContentWizardPages.SELECT_CONTENT) {
-            return {
-              name: ContentWizardPages.AVAILABLE_CHANNELS,
-              query: this.$store.state.coreBase.query,
-            };
-          } else {
-            return {
-              name: PageNames.MANAGE_CONTENT_PAGE,
-            };
-          }
-        } else if (this.pageName === PageNames.USER_PERMISSIONS_PAGE) {
-          // If Admin, goes back to ManagePermissionsPage
-          if (this.isSuperuser) {
-            return { name: PageNames.MANAGE_PERMISSIONS_PAGE };
-          } else {
-            // If Non-Admin, go to ManageContentPAge
-            return { name: PageNames.MANAGE_CONTENT_PAGE };
-          }
-        } else {
-          return {};
-        }
-      },
-      immersivePagePrimary() {
-        if (
-          this.pageName === PageNames.MANAGE_TASKS &&
-          this.$route.query &&
-          this.$route.query.last
-        ) {
-          return true;
-        } else if (
-          this.pageName === PageNames.MANAGE_TASKS &&
-          (!this.$route.query || !this.$route.query.last)
-        ) {
-          return false;
-        }
-        return this.inContentManagementPage;
-      },
-      immersivePageIcon() {
-        if (
-          this.pageName === PageNames.USER_PERMISSIONS_PAGE ||
-          this.pageName === ContentWizardPages.SELECT_CONTENT ||
-          this.inMultipleImportPage ||
-          this.pageName === PageNames.NEW_CHANNEL_VERSION_PAGE
-        ) {
-          return 'back';
-        }
-        return 'close';
       },
       currentPageIsImmersive() {
         if (this.pageName == PageNames.MANAGE_CONTENT_PAGE) {
@@ -161,18 +62,6 @@
       hideWelcomeModal() {
         window.sessionStorage.setItem(welcomeDimissalKey, true);
         this.$store.commit('SET_WELCOME_MODAL_VISIBLE', false);
-      },
-    },
-    $trs: {
-      deviceManagementTitle: {
-        message: 'Device',
-        context:
-          'The device is the physical or virtual machine that has the Kolibri server installed on it.',
-      },
-      permissionsLabel: {
-        message: 'Permissions',
-        context:
-          'Indicates the Device > Permissions tab. Permissions refer to what users can manage on the device.',
       },
     },
   };
