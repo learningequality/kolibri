5981d3c7cae2cda88188ab0818029a72fa15156b
José Redrejo <jose@learningequality.org>
2022-07-26 10:14:50 +0200

    Use facility on_my_own_setup config

--- kolibri/plugins/user_profile/viewsets.py
+++ kolibri/plugins/user_profile/viewsets.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from six.moves.urllib.parse import urljoin
 
-from kolibri.core.auth.models import FacilityUser
 from kolibri.core.device.utils import get_device_setting
 
 
@@ -17,14 +16,12 @@ class UserIndividualViewset(APIView):
     def get(self, request, format=None):
         if request.user.is_anonymous:
             self.permission_denied(request)
-
         subset_of_users_device = get_device_setting(
             "subset_of_users_device", default=False
         )
-        users_device = FacilityUser.objects.count()
-
+        user_facility = self.request.user.facility
         return Response(
-            {"individual": users_device == 1, "lod": subset_of_users_device}
+            {"individual": user_facility.on_my_own_setup, "lod": subset_of_users_device}
         )
 
 
