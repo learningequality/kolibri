d1956c32627d1c9949c1d9c0f18eaf319d3c2303
Marcella Maki <marcellamaki@gmail.com>
2022-08-02 10:47:05 -0400

    Add resuable overflow menu component to core app API spc

---
+++ kolibri/core/assets/src/views/HorizontalNavBarWithOverflowMenu.vue
@@ -0,0 +1,163 @@
+<template>
+
+  <Navbar>
+    <div v-for="(link, index) in navigationLinks" :key="index">
+      <NavbarLink
+        :vIf="link.condition"
+        :title="link.title"
+        :link="link.link"
+      >
+        <KIcon
+          :icon="link.icon"
+          :color="link.color"
+        />
+      </NavbarLink>
+    </div>
+    <span v-if="overflowMenuLinks && overflowMenuLinks.length > 0 && !windowIsLarge">
+      <KIconButton
+        class="menu-icon"
+        :tooltip="coreString('moreOptions')"
+        icon="optionsHorizontal"
+        appearance="flat-button"
+        :color="color"
+        :primary="false"
+      >
+        <template #menu>
+          <KDropdownMenu
+            :primary="false"
+            :disabled="false"
+            :hasIcons="true"
+            :options="overflowMenuLinks"
+            @select="handleSelect"
+          />
+        </template>
+      </KIconButton>
+    </span>
+  </Navbar>
+
+</template>
+
+
+<script>
+
+  import Navbar from 'kolibri.coreVue.components.Navbar';
+  import NavbarLink from 'kolibri.coreVue.components.NavbarLink';
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import responsiveWindowMixin from 'kolibri.coreVue.mixins.responsiveWindowMixin';
+  import debounce from 'lodash/debounce';
+
+  export default {
+    name: 'HorizontalNavBarWithOverflowMenu',
+    components: {
+      Navbar,
+      NavbarLink,
+    },
+    mixins: [commonCoreStrings, responsiveWindowMixin],
+    props: {
+      /**
+       * An array of options objects, with one object per dropdown item
+       */
+      navigationLinks: {
+        type: Array,
+        default: () => [],
+        required: true,
+      },
+    },
+    data() {
+      return {
+        numberOfNavigationTabsToDisplay: 0,
+        overflowMenuLinks: [],
+      };
+    },
+    computed: {
+      color() {
+        return this.$themeTokens.textInverted;
+      },
+    },
+    mounted() {
+      this.updateNavigationTabDisplay();
+      window.addEventListener('resize', this.updateNavigationTabDisplay);
+      // document.addEventListener('resize', console.log('hello world'));
+    },
+    beforeDestroy() {
+      document.removeEventListener('resize', this.debouncedUpdateNavigation);
+    },
+    methods: {
+      debouncedUpdateNavigation() {
+        return debounce(this.updateNavigationTabDisplay, 1000);
+      },
+      handleSelect(option) {
+        this.$router.push(this.$router.getRoute(option.value.name));
+      },
+      generateOverflowMenu() {
+        const limitedList = this.navigationLinks.slice(
+          this.numberOfNavigationTabsToDisplay,
+          this.navigationLinks.length
+        );
+        let options = [];
+        limitedList.forEach(o => options.push({ label: o.title, value: o.link, icon: o.icon }));
+        this.overflowMenuLinks = options;
+      },
+      updateNavigationTabDisplay() {
+        // turn HTML collection into an array
+        const navItems = [].slice.call(document.getElementsByClassName('list-item-navigation'));
+        let index = 0;
+        let viewportWidthTakenUp = 0;
+        let numberOfTabLinks = 0;
+        if (navItems && navItems.length > 0) {
+          while (index < navItems.length) {
+            viewportWidthTakenUp = viewportWidthTakenUp + navItems[index].offsetWidth;
+            if (viewportWidthTakenUp < window.innerWidth - 60) {
+              navItems[index].classList.add('visible');
+              numberOfTabLinks = index + 1;
+            } else {
+              navItems[index].classList.remove('visible');
+            }
+            index = index + 1;
+          }
+        }
+        this.numberOfNavigationTabsToDisplay = numberOfTabLinks;
+        this.generateOverflowMenu();
+      },
+    },
+    // $trs: {
+    //   permissionsLabel: {
+    //     message: 'Permissions',
+    //     context: 'Refers to the Device > Permissions tab.',
+    //   },
+    //   infoLabel: {
+    //     message: 'Info',
+    //     context: 'Refers to the Device > Info tab.',
+    //   },
+    //   settingsLabel: {
+    //     message: 'Settings',
+    //     context: 'Refers to the Device > Settings tab.\n',
+    //   },
+    // },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  @import '~kolibri-design-system/lib/styles/definitions';
+
+  .menu-icon {
+    position: absolute;
+    right: 4px;
+  }
+
+  .menu-popover {
+    @extend %dropshadow-4dp;
+
+    position: absolute;
+    right: 4px;
+    bottom: 50px;
+    z-index: 24;
+    font-size: 12px;
+    background-color: white;
+    border-radius: 8px;
+  }
+
+</style>
