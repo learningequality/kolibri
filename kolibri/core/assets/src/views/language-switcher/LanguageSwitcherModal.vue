41bcadae528617f610829d288f5399f7699f0585
Jacob Pierce <w.jacobpierce@gmail.com>
2022-08-03 16:38:50 -0700

    trap focus in language switcher modal

--- kolibri/core/assets/src/views/language-switcher/LanguageSwitcherModal.vue
+++ kolibri/core/assets/src/views/language-switcher/LanguageSwitcherModal.vue
@@ -1,34 +1,42 @@
 <template>
 
-  <KModal
-    :title="$tr('changeLanguageModalHeader')"
-    :submitText="coreString('confirmAction')"
-    :cancelText="coreString('cancelAction')"
-    :size="600"
-    @cancel="cancel"
-    @submit="setLang"
+  <FocusTrap
+    @shouldFocusFirstEl="focusFirstEl"
+    @shouldFocusLastEl="focusLastEl"
   >
-    <KGrid>
-      <KGridItem
-        v-for="(languageCol, index) in splitLanguageOptions"
-        :key="index"
-        :class="{ 'offset-col': windowIsSmall && index === 1 }"
-        :layout8="{ span: 4 }"
-        :layout12="{ span: 6 }"
-      >
-        <KRadioButton
-          v-for="language in languageCol"
-          :key="language.id"
-          v-model="selectedLanguage"
-          :value="language.id"
-          :label="language.lang_name"
-          :title="language.english_name"
-          class="language-name"
-        />
-      </KGridItem>
-    </KGrid>
 
-  </KModal>
+    <KModal
+      :title="$tr('changeLanguageModalHeader')"
+      :submitText="coreString('confirmAction')"
+      :cancelText="coreString('cancelAction')"
+      :size="600"
+      @cancel="cancel"
+      @submit="setLang"
+    >
+      <KGrid>
+        <KGridItem
+          v-for="(languageCol, index) in splitLanguageOptions"
+          :key="index"
+          :class="{ 'offset-col': windowIsSmall && index === 1 }"
+          :layout8="{ span: 4 }"
+          :layout12="{ span: 6 }"
+        >
+          <KRadioButton
+            v-for="language in languageCol"
+            :key="language.id"
+            ref="languageItem"
+            v-model="selectedLanguage"
+            :value="language.id"
+            :label="language.lang_name"
+            :title="language.english_name"
+            class="language-name"
+          />
+        </KGridItem>
+      </KGrid>
+
+    </KModal>
+
+  </FocusTrap>
 
 </template>
 
@@ -38,10 +46,12 @@
   import { currentLanguage } from 'kolibri.utils.i18n';
   import responsiveWindowMixin from 'kolibri.coreVue.mixins.responsiveWindowMixin';
   import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import FocusTrap from 'kolibri.coreVue.components.FocusTrap';
   import languageSwitcherMixin from './mixin';
 
   export default {
     name: 'LanguageSwitcherModal',
+    components: { FocusTrap },
     mixins: [commonCoreStrings, languageSwitcherMixin, responsiveWindowMixin],
     data() {
       return {
@@ -57,6 +67,12 @@
       },
     },
     methods: {
+      focusFirstEl() {
+        this.$refs.languageItem[0].focus();
+      },
+      focusLastEl() {
+        this.$refs.languageItem[this.$refs.languageItem.length - 1].focus();
+      },
       setLang() {
         if (currentLanguage === this.selectedLanguage) {
           this.cancel();
