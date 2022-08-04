aa98387c889d6521c50e03e32546fd6c044c5946
Jacob Pierce <w.jacobpierce@gmail.com>
2022-08-04 11:12:00 -0700

    export commonCoreStrings coreString getter itself along with the mixin

--- kolibri/core/assets/src/mixins/commonCoreStrings.js
+++ kolibri/core/assets/src/mixins/commonCoreStrings.js
@@ -1128,41 +1128,42 @@ const MetadataLookup = invert(
     METADATA.ResourcesNeededTypes
   )
 );
+/**
+ * Return translated string for key defined in the coreStrings translator. Will map
+ * ID keys generated in the kolibri-constants library to their appropriate translations
+ * if available.
+ *
+ * @param {string} key - A key as defined in the coreStrings translator; also accepts keys
+ * for the object MetadataLookup.
+ * @param {object} args - An object with keys matching ICU syntax arguments for the translation
+ * string mapping to the values to be passed for those arguments.
+ */
+export function coreString(key, args) {
+  if (key === 'None of the above' || key === METADATA.NoCategories) {
+    return noneOfTheAboveTranslator.$tr('None of the above', args);
+  }
+
+  if (key === 'overCertainNumberOfSearchResults') {
+    return overResultsTranslator.$tr(key, args);
+  }
+
+  const metadataKey = get(MetadataLookup, key, null);
+  key = metadataKey ? camelCase(metadataKey) : key;
+
+  if (nonconformingKeys[key]) {
+    return coreStrings.$tr(nonconformingKeys[key], args);
+  }
+
+  if (nonconformingKeys[metadataKey]) {
+    return coreStrings.$tr(nonconformingKeys[metadataKey], args);
+  }
+
+  return coreStrings.$tr(key, args);
+}
 
 export default {
   methods: {
-    /**
-     * Return translated string for key defined in the coreStrings translator. Will map
-     * ID keys generated in the kolibri-constants library to their appropriate translations
-     * if available.
-     *
-     * @param {string} key - A key as defined in the coreStrings translator; also accepts keys
-     * for the object MetadataLookup.
-     * @param {object} args - An object with keys matching ICU syntax arguments for the translation
-     * string mapping to the values to be passed for those arguments.
-     */
-    coreString(key, args) {
-      if (key === 'None of the above' || key === METADATA.NoCategories) {
-        return noneOfTheAboveTranslator.$tr('None of the above', args);
-      }
-
-      if (key === 'overCertainNumberOfSearchResults') {
-        return overResultsTranslator.$tr(key, args);
-      }
-
-      const metadataKey = get(MetadataLookup, key, null);
-      key = metadataKey ? camelCase(metadataKey) : key;
-
-      if (nonconformingKeys[key]) {
-        return coreStrings.$tr(nonconformingKeys[key], args);
-      }
-
-      if (nonconformingKeys[metadataKey]) {
-        return coreStrings.$tr(nonconformingKeys[metadataKey], args);
-      }
-
-      return coreStrings.$tr(key, args);
-    },
+    coreString,
     /**
      * Shows a specific snackbar notification from our notificationStrings translator.
      *
