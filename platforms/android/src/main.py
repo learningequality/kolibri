14261472038a02fb7ebe2bdca8fdfd0fb4ad226e
Richard Tibbles <richard@learningequality.org>
2022-08-25 06:12:14 -0700

    Remove use of kolibri workers.

--- platforms/android/src/main.py
+++ platforms/android/src/main.py
@@ -10,6 +10,7 @@ from kolibri.utils.server import BaseKolibriProcessBus
 from kolibri.utils.server import KolibriServerPlugin
 from kolibri.utils.server import ZeroConfPlugin
 from kolibri.utils.server import ZipContentServerPlugin
+from kolibri_tasks import start_default_tasks
 from magicbus.plugins import SimplePlugin
 from runnable import Runnable
 
@@ -42,6 +43,8 @@ enable_plugin("kolibri.plugins.app")
 
 # we need to initialize Kolibri to allow us to access the app key
 initialize()
+
+start_default_tasks()
 
 interface.register(share_file=share_by_intent)
 
