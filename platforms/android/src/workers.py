63f3d2b07d721d3df5fc5394ecd0c5dc07260e45
Richard Tibbles <richard@learningequality.org>
2022-08-31 15:14:13 -0700

    Update default tasks for latest develop.

--- platforms/android/src/workers.py
+++ platforms/android/src/workers.py
@@ -12,21 +12,12 @@ make_service_foreground("Kolibri service", "Running tasks.")
 initialize(skip_update=True)
 
 from kolibri.core.tasks.main import initialize_workers  # noqa: E402
-from kolibri.core.tasks.main import job_storage  # noqa: E402
-from kolibri.core.analytics.utils import DEFAULT_PING_JOB_ID  # noqa: E402
-from kolibri.core.deviceadmin.tasks import SCH_VACUUM_JOB_ID  # noqa: E402
+from kolibri.core.analytics.tasks import schedule_ping  # noqa: E402
+from kolibri.core.deviceadmin.tasks import schedule_vacuum  # noqa: E402
 
-# schedule the pingback job if not already scheduled
-if DEFAULT_PING_JOB_ID not in job_storage:
-    from kolibri.core.analytics.utils import schedule_ping
+schedule_ping()
 
-    schedule_ping()
-
-# schedule the vacuum job if not already scheduled
-if SCH_VACUUM_JOB_ID not in job_storage:
-    from kolibri.core.deviceadmin.tasks import schedule_vacuum
-
-    schedule_vacuum()
+schedule_vacuum()
 
 # Initialize the iceqube engine to handle queued tasks
 worker = initialize_workers()
