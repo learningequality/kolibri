0b0239a78a9afbcd535f9972b8e9a03a8dd84649
MisRob <robosovam@gmail.com>
2022-09-13 11:46:02 +0200

    Move states to a separate object
    This will be needed for history/back navigation implementation.
    This commit shouldn't contain any updates of states themselves.

--- kolibri/plugins/user_profile/assets/src/machines/changeFacilityMachine.js
+++ kolibri/plugins/user_profile/assets/src/machines/changeFacilityMachine.js
@@ -103,260 +103,262 @@ const generateMachineContext = () => {
   };
 };
 
-export const changeFacilityMachine = createMachine({
-  id: 'machine',
-  initial: 'selectFacility',
-  predictableActionArguments: true,
-  context: generateMachineContext(),
-  states: {
-    error: {
-      on: {
-        RESET: {
-          target: 'selectFacility',
-          actions: [resetMachineContext],
-        },
+const states = {
+  error: {
+    on: {
+      RESET: {
+        target: 'selectFacility',
+        actions: [resetMachineContext],
       },
     },
-    profile: {
-      meta: { route: 'PROFILE', path: '/' },
-      on: {
-        CONTINUE: {
-          target: 'selectFacility',
-          cond: context => !!context.sourceFacility && !!context.username,
-        },
-        SETCONTEXT: { actions: setInitialContext },
+  },
+  profile: {
+    meta: { route: 'PROFILE', path: '/' },
+    on: {
+      CONTINUE: {
+        target: 'selectFacility',
+        cond: context => !!context.sourceFacility && !!context.username,
+      },
+      SETCONTEXT: { actions: setInitialContext },
+    },
+  },
+  selectFacility: {
+    meta: { route: 'SELECT_FACILITY', path: '/change_facility' },
+    on: {
+      CONTINUE: {
+        target: 'changeFacility',
+        cond: context => Object.keys(context.targetFacility).length > 0,
+      },
+      BACK: 'profile',
+      SETCONTEXT: { actions: setInitialContext },
+      SELECTFACILITY: {
+        actions: assign({
+          targetFacility: (_, event) => {
+            return event.value;
+          },
+        }),
+        target: 'getFacilityUsernames',
       },
     },
-    selectFacility: {
-      meta: { route: 'SELECT_FACILITY', path: '/change_facility' },
-      on: {
-        CONTINUE: {
-          target: 'changeFacility',
-          cond: context => Object.keys(context.targetFacility).length > 0,
-        },
-        BACK: 'profile',
-        SETCONTEXT: { actions: setInitialContext },
-        SELECTFACILITY: {
-          actions: assign({
-            targetFacility: (_, event) => {
-              return event.value;
-            },
-          }),
-          target: 'getFacilityUsernames',
-        },
+  },
+  getFacilityUsernames: {
+    invoke: {
+      id: 'getUserNames',
+      src: (context, event) => connectToTargetKolibri(context, event),
+      onDone: {
+        target: 'selectFacility',
+        actions: checkExists,
+      },
+      onError: {
+        target: 'selectFacility',
       },
     },
-    getFacilityUsernames: {
-      invoke: {
-        id: 'getUserNames',
-        src: (context, event) => connectToTargetKolibri(context, event),
-        onDone: {
-          target: 'selectFacility',
-          actions: checkExists,
-        },
-        onError: {
-          target: 'selectFacility',
-        },
+  },
+  changeFacility: {
+    meta: { route: 'CONFIRM_CHANGE_FACILITY', path: '/change_facility' },
+    on: {
+      MERGE: {
+        target: 'mergeAccounts',
+        actions: setMerging,
+      },
+      CONTINUE: 'checkUsernameExists',
+      BACK: 'selectFacility',
+    },
+  },
+  checkUsernameExists: {
+    always: [
+      {
+        cond: context => !!context.accountExists,
+        target: 'usernameExists',
+      },
+      {
+        target: 'confirmAccountUsername',
+      },
+    ],
+  },
+  confirmAccountUsername: {
+    meta: { route: 'CONFIRM_ACCOUNT_USERNAME', path: '/change_facility' },
+    on: {
+      NEW: {
+        cond: context => context.targetFacility && context.targetFacility.learner_can_sign_up,
+        target: 'createAccount',
+      },
+      CONTINUE: 'isAdmin',
+      BACK: 'changeFacility',
+    },
+  },
+  isAdmin: {
+    always: [
+      {
+        cond: context => context.role === 'superuser',
+        target: 'fetchSourceFacilityUsers',
+      },
+      {
+        target: 'checkIsMerging',
+      },
+    ],
+  },
+  fetchSourceFacilityUsers: {
+    invoke: {
+      src: () => {
+        return FacilityUserResource.fetchCollection().then(users => {
+          return users;
+        });
+      },
+      onDone: {
+        target: 'checkNeedsNewSuperAdmin',
+        actions: [setSourceFacilityUsers],
+      },
+      onError: {
+        target: 'error',
       },
     },
-    changeFacility: {
-      meta: { route: 'CONFIRM_CHANGE_FACILITY', path: '/change_facility' },
-      on: {
-        MERGE: {
-          target: 'mergeAccounts',
-          actions: setMerging,
+  },
+  checkNeedsNewSuperAdmin: {
+    always: [
+      {
+        cond: context => {
+          const facilityHasAnotherSuperUser =
+            context.sourceFacilityUsers.length > 0 &&
+            context.sourceFacilityUsers.find(u => u.id !== context.userId && u.is_superuser);
+          return context.role === 'superuser' && !facilityHasAnotherSuperUser;
         },
-        CONTINUE: 'checkUsernameExists',
-        BACK: 'selectFacility',
+        target: 'chooseAdmin',
       },
+      {
+        target: 'checkIsMerging',
+      },
+    ],
+  },
+  chooseAdmin: {
+    meta: { route: 'CHOOSE_ADMIN', path: '/change_facility' },
+    on: {
+      CONTINUE: {
+        target: 'checkIsMerging',
+        cond: context => !!context.newSuperAdminId,
+      },
+      SELECTNEWSUPERADMIN: { actions: setNewSuperAdminId },
     },
-    checkUsernameExists: {
-      always: [
+  },
+  checkIsMerging: {
+    always: [
+      {
+        cond: context => context.isMerging,
+        target: 'confirmMerge',
+      },
+      {
+        target: 'syncChangeFacility',
+      },
+    ],
+  },
+  confirmMerge: {
+    meta: { route: 'CONFIRM_MERGE', path: '/change_facility' },
+    on: {
+      CONTINUE: 'syncChangeFacility',
+      BACK: [
         {
-          cond: context => !!context.accountExists,
-          target: 'usernameExists',
+          cond: context => context.role === 'superuser',
+          target: 'chooseAdmin',
+        },
+        {
+          target: 'confirmAccountDetails',
+        },
+      ],
+    },
+  },
+  syncChangeFacility: {
+    meta: { route: 'SYNCING_CHANGE_FACILITY', path: '/change_facility' },
+    type: 'final',
+  },
+  createAccount: {
+    meta: { route: 'CREATE_ACCOUNT', path: '/change_facility' },
+    on: {
+      CONTINUE: {
+        target: 'isAdmin',
+        actions: setTargetAccount,
+      },
+      BACK: [
+        {
+          cond: context => context.accountExists,
+          target: 'changeFacility',
         },
         {
           target: 'confirmAccountUsername',
         },
       ],
     },
-    confirmAccountUsername: {
-      meta: { route: 'CONFIRM_ACCOUNT_USERNAME', path: '/change_facility' },
-      on: {
-        NEW: {
-          cond: context => context.targetFacility && context.targetFacility.learner_can_sign_up,
-          target: 'createAccount',
-        },
-        CONTINUE: 'isAdmin',
-        BACK: 'changeFacility',
-      },
+  },
+  usernameExists: {
+    meta: { route: 'USERNAME_EXISTS', path: '/change_facility' },
+    on: {
+      MERGE: 'requireAccountCreds',
+      NEW: 'createAccount',
+      BACK: 'selectFacility',
     },
-    isAdmin: {
-      always: [
+  },
+  requireAccountCreds: {
+    meta: { route: 'REQUIRE_ACCOUNT_CREDENTIALS', path: '/change_facility' },
+    on: {
+      CONTINUE: { actions: setTargetAccount, target: 'confirmAccountDetails' },
+      USEADMIN: 'useAdminPassword',
+      BACK: 'confirmMerge',
+    },
+  },
+  useAdminPassword: {
+    meta: { route: 'ADMIN_PASSWORD', path: '/change_facility' },
+    on: {
+      CONTINUE: { actions: setTargetAccount, target: 'confirmAccountDetails' },
+      BACK: 'requireAccountCreds',
+    },
+  },
+  confirmAccountDetails: {
+    meta: { route: 'CONFIRM_DETAILS', path: '/change_facility' },
+    on: {
+      CONTINUE: 'isAdmin',
+      BACK: 'mergeAccounts',
+    },
+  },
+  mergeAccounts: {
+    meta: { route: 'MERGE_ACCOUNTS', path: '/change_facility' },
+    on: {
+      CONTINUE: [
         {
-          cond: context => context.role === 'superuser',
-          target: 'fetchSourceFacilityUsers',
+          cond: context =>
+            context.accountExists && !context.targetFacility.learner_can_login_with_no_password,
+          target: 'requireAccountCreds',
         },
         {
-          target: 'checkIsMerging',
+          cond: context =>
+            context.accountExists && context.targetFacility.learner_can_login_with_no_password,
+          target: 'getUserWithoutPasswordInfo',
         },
       ],
+      BACK: 'selectFacility',
     },
-    fetchSourceFacilityUsers: {
-      invoke: {
-        src: () => {
-          return FacilityUserResource.fetchCollection().then(users => {
-            return users;
-          });
-        },
-        onDone: {
-          target: 'checkNeedsNewSuperAdmin',
-          actions: [setSourceFacilityUsers],
-        },
-        onError: {
-          target: 'error',
-        },
-      },
-    },
-    checkNeedsNewSuperAdmin: {
-      always: [
-        {
-          cond: context => {
-            const facilityHasAnotherSuperUser =
-              context.sourceFacilityUsers.length > 0 &&
-              context.sourceFacilityUsers.find(u => u.id !== context.userId && u.is_superuser);
-            return context.role === 'superuser' && !facilityHasAnotherSuperUser;
+  },
+  getUserWithoutPasswordInfo: {
+    invoke: {
+      id: 'getPasswordlessUserInfo',
+      src: (context, event) => getUserWPasswordInfo(context, event),
+      onDone: {
+        target: 'confirmAccountDetails',
+        actions: assign({
+          targetAccount: (_, event) => {
+            return event.data;
           },
-          target: 'chooseAdmin',
-        },
-        {
-          target: 'checkIsMerging',
-        },
-      ],
-    },
-    chooseAdmin: {
-      meta: { route: 'CHOOSE_ADMIN', path: '/change_facility' },
-      on: {
-        CONTINUE: {
-          target: 'checkIsMerging',
-          cond: context => !!context.newSuperAdminId,
-        },
-        SELECTNEWSUPERADMIN: { actions: setNewSuperAdminId },
+        }),
       },
-    },
-    checkIsMerging: {
-      always: [
-        {
-          cond: context => context.isMerging,
-          target: 'confirmMerge',
-        },
-        {
-          target: 'syncChangeFacility',
-        },
-      ],
-    },
-    confirmMerge: {
-      meta: { route: 'CONFIRM_MERGE', path: '/change_facility' },
-      on: {
-        CONTINUE: 'syncChangeFacility',
-        BACK: [
-          {
-            cond: context => context.role === 'superuser',
-            target: 'chooseAdmin',
-          },
-          {
-            target: 'confirmAccountDetails',
-          },
-        ],
-      },
-    },
-    syncChangeFacility: {
-      meta: { route: 'SYNCING_CHANGE_FACILITY', path: '/change_facility' },
-      type: 'final',
-    },
-    createAccount: {
-      meta: { route: 'CREATE_ACCOUNT', path: '/change_facility' },
-      on: {
-        CONTINUE: {
-          target: 'isAdmin',
-          actions: setTargetAccount,
-        },
-        BACK: [
-          {
-            cond: context => context.accountExists,
-            target: 'changeFacility',
-          },
-          {
-            target: 'confirmAccountUsername',
-          },
-        ],
-      },
-    },
-    usernameExists: {
-      meta: { route: 'USERNAME_EXISTS', path: '/change_facility' },
-      on: {
-        MERGE: 'requireAccountCreds',
-        NEW: 'createAccount',
-        BACK: 'selectFacility',
-      },
-    },
-    requireAccountCreds: {
-      meta: { route: 'REQUIRE_ACCOUNT_CREDENTIALS', path: '/change_facility' },
-      on: {
-        CONTINUE: { actions: setTargetAccount, target: 'confirmAccountDetails' },
-        USEADMIN: 'useAdminPassword',
-        BACK: 'confirmMerge',
-      },
-    },
-    useAdminPassword: {
-      meta: { route: 'ADMIN_PASSWORD', path: '/change_facility' },
-      on: {
-        CONTINUE: { actions: setTargetAccount, target: 'confirmAccountDetails' },
-        BACK: 'requireAccountCreds',
-      },
-    },
-    confirmAccountDetails: {
-      meta: { route: 'CONFIRM_DETAILS', path: '/change_facility' },
-      on: {
-        CONTINUE: 'isAdmin',
-        BACK: 'mergeAccounts',
-      },
-    },
-    mergeAccounts: {
-      meta: { route: 'MERGE_ACCOUNTS', path: '/change_facility' },
-      on: {
-        CONTINUE: [
-          {
-            cond: context =>
-              context.accountExists && !context.targetFacility.learner_can_login_with_no_password,
-            target: 'requireAccountCreds',
-          },
-          {
-            cond: context =>
-              context.accountExists && context.targetFacility.learner_can_login_with_no_password,
-            target: 'getUserWithoutPasswordInfo',
-          },
-        ],
-        BACK: 'selectFacility',
-      },
-    },
-    getUserWithoutPasswordInfo: {
-      invoke: {
-        id: 'getPasswordlessUserInfo',
-        src: (context, event) => getUserWPasswordInfo(context, event),
-        onDone: {
-          target: 'confirmAccountDetails',
-          actions: assign({
-            targetAccount: (_, event) => {
-              return event.data;
-            },
-          }),
-        },
-        onError: {
-          target: 'mergeAccounts',
-        },
+      onError: {
+        target: 'mergeAccounts',
       },
     },
   },
+};
+
+export const changeFacilityMachine = createMachine({
+  id: 'machine',
+  initial: 'selectFacility',
+  predictableActionArguments: true,
+  context: generateMachineContext(),
+  states,
 });
