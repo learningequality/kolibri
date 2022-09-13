ce90df1760d0e160652fd275fb8ce550a6129851
Alex Velez <alanvele@espol.edu.ec>
2022-09-12 22:33:33 -0500

    Bookmarks support

---
+++ kolibri/plugins/pdf_viewer/assets/src/views/Bookmarks/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul
+    class="bookmarks-wrapper"
+    :style="{
+      backgroundColor: this.$themePalette.grey.v_100,
+      borderRight: `solid 1px ${this.$themeTokens.fineLine}`,
+    }"
+  >
+    <BookmarkItem
+      v-for="(item, index) in outline"
+      :item="item"
+      :key="index"
+      :goToDestination="goToDestination"
+    />
+  </ul>
+</template>
+<script>
+  import BookmarkItem from './item';
+
+  export default {
+    name: 'Bookmarks',
+    components: {
+      BookmarkItem,
+    },
+    props: {
+      outline: {
+        type: Array,
+        required: true,
+      },
+      pdfDocument: {
+        type: Object,
+        required: true,
+      },
+      goToDestination: {
+        type: Function,
+        required: true,
+      },
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .bookmarks-wrapper {
+    height: 100%;
+    z-index: 2;
+    position: relative;
+    top: 40px;
+    padding: 16px;
+    margin: 0;
+  }
+</style>
---
+++ kolibri/plugins/pdf_viewer/assets/src/views/Bookmarks/item.vue
@@ -0,0 +1,101 @@
+<template>
+  <li class="bookmark-item">
+    <div class="bookmark-item-title-container">
+      <span
+        v-if="item.items && item.items.length > 0"
+        tabindex="0"
+        class="dropdown-icon-container"
+        role="button"
+        @click="toggleExpanded"
+        @keydown.enter="toggleExpanded"
+      >
+        <KIcon
+          aria-controls="pdf-container"
+          icon="dropdown"
+          class="dropdown-icon"
+          :class="{ 'expanded': expanded }"
+        />
+      </span>
+      <span
+        tabindex="0"
+        class="bookmark-item-title"
+        role="button"
+        @click="goToDestination(item.dest)"
+        @keydown.enter="goToDestination(item.dest)"
+      >
+        {{ item.title }}
+      </span>
+    </div>
+    <ul v-if="expanded" class="bookmark-children">
+      <BookmarkItem
+        v-for="(child, index) in item.items"
+        :item="child"
+        :key="index"
+        :goToDestination="goToDestination"
+      />
+    </ul>
+  </li>
+</template>
+<script>
+  export default {
+    name: 'BookmarkItem',
+    props: {
+      item: {
+        type: Object,
+        required: true,
+      },
+      goToDestination: {
+        type: Function,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        expanded: false,
+      };
+    },
+    methods: {
+      toggleExpanded() {
+        this.expanded = !this.expanded;
+      },
+    },
+  }
+</script>
+<style scoped lang="scss">
+  .bookmark-item {
+    font-size: 12px;
+    list-style: none;
+    position: relative;
+    // &:hover {
+    //   .bookmark-item-title::before {
+    //     content: '';
+    //     position: absolute;
+    //     top: 0;
+    //     left: 0;
+    //     width: 100%;
+    //     height: 100%;
+    //     border-radius: 4px;
+    //     background-color: rgba(0, 0, 0, 0.1);
+    //     user-select: none;
+    //   }
+    // }
+    .bookmark-item-title-container {
+      cursor: pointer;
+    }
+    .bookmark-item-title:focus-visible,
+    .dropdown-icon-container:focus-visible {
+      outline-width: medium;
+      outline-style: solid;
+    }
+    .dropdown-icon {
+      transform: rotate(-90deg);
+      transition: transform 0.1s ease-in-out;
+      &.expanded {
+        transform: rotate(0deg);
+      }
+      .bookmark-children {
+        padding-left: 32px;
+      }
+    }
+  }
+</style>
