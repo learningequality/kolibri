4d01e1ef5a155c42fb3ec419b43d250c5d6dfb68
Richard Tibbles <richard@learningequality.org>
2022-09-16 14:41:32 -0700

    Update multiple worker test to ensure that connection is shared across workers.

--- kolibri/core/tasks/test/taskrunner/test_worker.py
+++ kolibri/core/tasks/test/taskrunner/test_worker.py
@@ -49,16 +49,6 @@ def worker():
         b.shutdown()
 
 
-@pytest.fixture
-def worker2():
-    with connection() as c:
-        b = Worker(c, regular_workers=1, high_workers=1)
-        b.storage.clear(force=True)
-        yield b
-        b.storage.clear(force=True)
-        b.shutdown()
-
-
 @pytest.mark.django_db
 class TestWorker:
     def test_enqueue_job_runs_job(self, worker):
@@ -68,34 +58,21 @@ class TestWorker:
         while job.state != State.COMPLETED:
             job = worker.storage.get_job(job.job_id)
             time.sleep(0.5)
-        try:
-            # Get the future, or pass if it has already been cleaned up.
-            future = worker.future_job_mapping[job.job_id]
-
-            future.result()
-        except KeyError:
-            pass
 
         assert job.state == State.COMPLETED
 
-    def test_enqueue_job_runs_job_once(self, worker, worker2, flag):
-
-        job = Job(toggle_flag, flag.event_id)
+    def test_enqueue_job_runs_job_once(self, worker, flag):
+        b = Worker(worker.storage.engine, regular_workers=1, high_workers=1)
+        job = Job(toggle_flag, args=(flag.event_id,))
         worker.storage.enqueue_job(job, QUEUE)
 
         while job.state != State.COMPLETED:
             job = worker.storage.get_job(job.job_id)
             time.sleep(0.5)
-        try:
-            # Get the future, or pass if it has already been cleaned up.
-            future = worker.future_job_mapping[job.job_id]
-
-            future.result()
-        except KeyError:
-            pass
 
         assert job.state == State.COMPLETED
         assert flag.is_set()
+        b.shutdown()
 
     def test_can_handle_unicode_exceptions(self, worker):
         # Make sure task exception info is not an object, but is either a string or None.
