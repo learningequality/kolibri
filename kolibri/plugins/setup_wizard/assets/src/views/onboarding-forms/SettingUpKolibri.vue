bed697659f7ccc951739cb748c1bc7a968fc0ef1
Jacob Pierce <w.jacobpierce@gmail.com>
2022-10-29 13:30:49 -0700

    animated kolibri vue component, styles for final onbaording page

--- kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/SettingUpKolibri.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/SettingUpKolibri.vue
@@ -1,13 +1,15 @@
 <template>
 
   <div class="full-page">
-    <CoreLogo class="logo" />
-    <h1 class="page-title">
-      {{ $tr('pageTitle') }}
-    </h1>
-    <p class="message">
-      {{ $tr('pleaseWaitMessage') }}
-    </p>
+    <main class="content">
+      <KolibriLoadingSnippet />
+      <h1 class="page-title">
+        {{ $tr('pageTitle') }}
+      </h1>
+      <p class="message">
+        {{ $tr('pleaseWaitMessage') }}
+      </p>
+    </main>
   </div>
 
 </template>
@@ -15,36 +17,63 @@
 
 <script>
 
-  import CoreLogo from 'kolibri.coreVue.components.CoreLogo';
-  import { FacilityImportResource } from '../../api';
+  import KolibriLoadingSnippet from 'kolibri.coreVue.components.KolibriLoadingSnippet';
+  import urls from 'kolibri.urls';
+  import { FacilityImportResource, OnMyOwnResource, SetupWizardResource } from '../../api';
   import { UsePresets } from '../../constants';
 
   export default {
     name: 'SettingUpKolibri',
-    components: { CoreLogo },
+    components: { KolibriLoadingSnippet },
     inject: ['wizardService'],
+    data() {
+      return {
+        user: {},
+      };
+    },
     mounted() {
-      // If we're in app mode
-      if (this.wizardService.state.context.canGetOsUser) {
-        const facilityUserData = {
-          facility_name: this.$store.state.onboardingData.facility.name,
-          extra_fields: {
-            on_my_own_setup: this.isOnMyOwnSetup(),
-            os_user: true,
-          },
-        };
-        FacilityImportResource.createsuperuser(facilityUserData)
-          .then(
-            () => (window.location = window.urls['kolibri:kolibri.plugins.user_auth:user_auth']())
-          )
-          .catch(err => console.log(err));
-      } else {
-        window.location = window.urls['kolibri:kolibri.plugins.user_auth:user_auth']();
-      }
+      this.user = this.$store.state.onboardingData.user;
+      // TODO figure out w/ richard how to properly get the auth_token passed so we can get_or_create_os_user
+      let { full_name, username, password } = this.user;
+      const facilityUserData = {
+        full_name,
+        username,
+        password,
+        facility_name: this.$store.state.onboardingData.facility.name,
+        extra_fields: {
+          on_my_own_setup: this.isOnMyOwnSetup(),
+          os_user: this.canGetOsUser(),
+        },
+      };
+      OnMyOwnResource.createonmyownuser(facilityUserData)
+        .then(() => {
+          const deviceProvisioningData = {
+            device_name: this.wizardService.state.context.deviceName,
+            language_id: this.$store.state.onboardingData.language_id,
+            is_provisioned: true,
+          };
+          SetupWizardResource.provisiondevice(deviceProvisioningData)
+            .then(() => {
+              // FIXME In dev mode, we'll wait 5 seconds before moving along so we can see the page... maybe we keep this?
+              const timeout = process.NODE_ENV === 'production' ? 1 : 5000;
+              setTimeout(
+                () =>
+                  (window.location.pathname = urls[
+                    'kolibri:kolibri.plugins.user_auth:user_auth'
+                  ]()),
+                timeout
+              );
+            })
+            .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
     },
     methods: {
       isOnMyOwnSetup() {
         return this.wizardService.state.context.onMyOwnOrGroup == UsePresets.ON_MY_OWN;
+      },
+      canGetOsUser() {
+        return this.wizardService.state.context.canGetOsUser;
       },
     },
     $trs: {
@@ -65,33 +94,18 @@
 <style scoped lang="scss">
 
   .full-page {
+    /* Fill the screen, no scroll bars */
     position: relative;
     width: 100vw;
     height: 100vh;
     overflow: hidden;
   }
 
-  .logo {
-    display: block;
-    max-width: 400px;
-    padding: 0 2em;
-    margin: 12em auto 0;
-
-    /* TODO Replace this with animated logo */
-    animation-name: spin;
-    animation-duration: 400ms;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-  }
-
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-
-    to {
-      transform: rotate(360deg);
-    }
+  .content {
+    /* Vertically centered */
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
   }
 
   .page-title,
