6cae33d9c1dd9f5183fdec4cafe5329b9dcd687d
Richard Tibbles <richard@learningequality.org>
2022-12-12 14:32:12 -0800

    Delete test that now relies on css behaviour.

--- kolibri/core/assets/test/horizontal-nav-bar-with-overflow.spec.js
+++ kolibri/core/assets/test/horizontal-nav-bar-with-overflow.spec.js
@@ -59,48 +59,14 @@ describe('HorizontalNavBarWithOverflowMenu', () => {
     expect(wrapper.findComponent({ name: 'Navbar' }).element).toBeTruthy();
   });
   describe('the overflow menu', () => {
-    describe('overflow needed', () => {
+    describe('overflow not needed', () => {
       const wrapper = makeWrapper(HorizontalNavBarWithOverflowMenu, {
         propsData: {
           navigationLinks: longerNavigationList,
         },
-        computed: { windowIsLarge: () => false },
       });
-      it('shows a KIconButton when the number of links does not fit in the given viewport', async () => {
-        await wrapper.setData({
-          numberOfNavigationTabsToDisplay: 2,
-          overflowMenuLinks: [
-            {
-              title: 'Title 3',
-              link: 'url',
-              icon: 'dashboard',
-              color: 'white',
-            },
-            {
-              title: 'Title 4',
-              link: 'url',
-              icon: 'dashboard',
-              color: 'white',
-            },
-            {
-              title: 'Title 5',
-              link: 'url',
-              icon: 'dashboard',
-              color: 'white',
-            },
-          ],
-        });
-        expect(wrapper.findComponent({ name: 'KIconButton' }).element).toBeTruthy();
-      });
-      describe('overflow not needed', () => {
-        const wrapper = makeWrapper(HorizontalNavBarWithOverflowMenu, {
-          propsData: {
-            navigationLinks: longerNavigationList,
-          },
-        });
-        it('does not display a KIconButton with a dropdown menu', () => {
-          expect(wrapper.findComponent({ name: 'KIconButton' }).element).toBeFalsy();
-        });
+      it('does not display a KIconButton with a dropdown menu', () => {
+        expect(wrapper.findComponent({ name: 'KIconButton' }).element).toBeFalsy();
       });
     });
   });
