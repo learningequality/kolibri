ac49987594d11a91ef38b4a7ed14969c9660aca6
Blaine Jester <blainesworld@gmail.com>
2023-02-02 13:29:48 -0800

    Fix sync progress tracking reference to progressbar

--- kolibri/core/auth/management/utils.py
+++ kolibri/core/auth/management/utils.py
@@ -691,7 +691,7 @@ class MorangoSyncCommand(AsyncCommand):
         :type sync_state: str
         :type noninteractive: bool
         """
-        tracker = self.start_progress(total=2)
+        tracker = self.start_progress(total=2).progresstracker
 
         def started(transfer_session):
             dataset_cache.clear()
@@ -705,7 +705,7 @@ class MorangoSyncCommand(AsyncCommand):
                     logger.info("No records transferred")
 
         def handler(transfer_session):
-            tracker.update_progress(
+            self.update_progress(
                 message=message, extra_data=dict(sync_state=sync_state)
             )
 
