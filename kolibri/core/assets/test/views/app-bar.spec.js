e7069dc61621e733675668c91bdbeacff2ff86ac
Marcella Maki <marcellamaki@gmail.com>
2023-02-28 18:11:03 -0500

    fix current app bar test (no new tests added)

--- kolibri/core/assets/test/views/app-bar.spec.js
+++ kolibri/core/assets/test/views/app-bar.spec.js
@@ -1,19 +1,33 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
 import AppBar from '../../src/views/AppBar';
 
 jest.mock('kolibri.urls');
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
+const store = new Vuex.Store({
+  state: {},
+  getters: {
+    isAppContext: () => false,
+  },
+});
+
 function createWrapper({ propsData } = {}) {
   const node = document.createElement('div');
   document.body.appendChild(node);
-  return shallowMount(AppBar, { propsData, attachTo: node });
+  return shallowMount(AppBar, {
+    store,
+    propsData,
+    attachTo: node,
+  });
 }
 
 describe('app bar component', () => {
   describe('smoke test', () => {
     it('should render', () => {
       const wrapper = createWrapper({ loading: false });
-      expect(wrapper.findComponent(AppBar).element).toBeVisible();
+      expect(wrapper.find({ name: 'AppBar' }).element).toBeVisible();
     });
   });
 });
