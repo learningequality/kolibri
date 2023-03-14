152283d97c32be3dc6149d8ea5afb23b91a4547a
Marcella Maki <marcellamaki@gmail.com>
2023-03-14 14:11:17 -0400

    Fix bug where lesson active state was not properly capture in conditional check

--- kolibri/plugins/coach/assets/src/views/common/LessonStatus.vue
+++ kolibri/plugins/coach/assets/src/views/common/LessonStatus.vue
@@ -198,7 +198,7 @@
         const hideModalConfirmation = Lockr.get(LESSON_VISIBILITY_MODAL_DISMISSED);
         this.activeLesson = lesson;
         if (!hideModalConfirmation) {
-          if (lesson.active) {
+          if (lesson.active || lesson.is_active) {
             this.showLessonIsVisibleModal = false;
             this.showLessonIsNotVisibleModal = true;
           } else {
