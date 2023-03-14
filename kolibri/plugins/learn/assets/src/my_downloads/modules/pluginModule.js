f3f6c7701b287494baa67785c8c9f23071b6f79b
Alex Velez <alanvele@espol.edu.ec>
2023-03-14 04:02:40 -0500

    Removing unused code

--- kolibri/plugins/learn/assets/src/my_downloads/modules/pluginModule.js
+++ kolibri/plugins/learn/assets/src/my_downloads/modules/pluginModule.js
@@ -1,21 +1,7 @@
 export default {
   state() {
-    return {
-      test: '',
-    };
+    return {};
   },
-  actions: {
-    reset(store) {
-      store.commit('CORE_SET_PAGE_LOADING', false);
-      store.commit('CORE_SET_ERROR', null);
-    },
-    resetModuleState(store) {
-      store.commit('profile/RESET_STATE');
-    },
-  },
-  mutations: {
-    SET_Test(state, test) {
-      state.test = test;
-    },
-  },
+  actions: {},
+  mutations: {},
 };
