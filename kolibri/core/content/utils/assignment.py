b0db76e63eda7d8db0366aa15b2adf477b252f66
ozer550 <prathameshd419@gmail.com>
2023-04-18 22:50:01 +0530

    apply linting

--- kolibri/core/content/utils/assignment.py
+++ kolibri/core/content/utils/assignment.py
@@ -79,13 +79,15 @@ class ContentAssignmentManager(object):
         CONTENT_ASSIGNMENT_MANAGER_REGISTRY.update({model.__class__.__name__: self})
 
     @classmethod
-    def find_all_downloadable_assignments(cls, dataset_id=None, transfer_session_id=None):
+    def find_all_downloadable_assignments(
+        cls, dataset_id=None, transfer_session_id=None
+    ):
         """
         :param dataset_id: optional argument to filter assignments by dataset
         :param transfer_session_id:
         :rtype: list of ContentAssignment
         """
-        
+
         if dataset_id is None and transfer_session_id is None:
             raise ValueError("Either dataset_id or transfer_session_id is required")
 
@@ -105,9 +107,11 @@ class ContentAssignmentManager(object):
 
         if dataset_id is None and transfer_session_id is None:
             raise ValueError("Either dataset_id or transfer_session_id is required")
-        
+
         for manager in CONTENT_ASSIGNMENT_MANAGER_REGISTRY.values():
-            for assignment in manager.find_removable_assignments(dataset_id, transfer_session_id):
+            for assignment in manager.find_removable_assignments(
+                dataset_id, transfer_session_id
+            ):
                 yield assignment
 
     def _get_modified_store(self, transfer_session_id):
@@ -153,7 +157,6 @@ class ContentAssignmentManager(object):
                     )
                     contentnode_ids.update([contentnode_id])
 
-
     def find_downloadable_assignments(self, dataset_id=None, transfer_session_id=None):
         """
         :param dataset_id: optional dataset_id to filter records by
@@ -168,17 +171,18 @@ class ContentAssignmentManager(object):
                 Q(deleted=True) | Q(hard_deleted=True) | ~Q(deserialization_error="")
             )
             if modified_store.exists():
-                model_qs = model_qs.filter(pk__in=modified_store.values_list("id", flat=True))
+                model_qs = model_qs.filter(
+                    pk__in=modified_store.values_list("id", flat=True)
+                )
         elif dataset_id:
             model_qs = model_qs.filter(dataset_id=dataset_id)
         if self.filters:
             model_qs = model_qs.filter(**self.filters)
-        
+
         for assignment in self._get_assignments(model_qs):
             yield assignment
-    
-    
-    def find_removable_assignments(self, dataset_id, transfer_session_id):
+
+    def find_removable_assignments(self, dataset_id=None, transfer_session_id=None):
         """
         :param dataset_id: the ID of the dataset to filter records by dataset_id
         :param transfer_session_id: the ID of the transfer session to filter records by transfer_session_id
