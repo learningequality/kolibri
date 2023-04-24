6a3c6b1e0751e309d0ee0ef9d22c59532ad4c4c8
thanksameeelian <ameliabreault@gmail.com>
2023-04-24 10:28:39 -0500

    initial fix for side nav routes with one facility + test

--- kolibri/core/assets/src/utils/generateNavRoutes.js
+++ kolibri/core/assets/src/utils/generateNavRoutes.js
@@ -6,28 +6,31 @@ export function generateNavRoute(rootUrl, pathReference, params = {}) {
   // when there is a direct path
   compiledRoute = `${rootUrl}#${pathReference}`;
 
-  // if the path requires params but none exist, send to root
   const pathParams = [];
   pathToRegexp(compiledRoute, pathParams);
 
-  if (pathParams.length > 0 && Object.keys(params).length < 1) {
+  const countPathParams = pathParams.length;
+  const paramName = !!countPathParams && pathParams[0].name;
+  const paramValue = paramName && params[paramName];
+
+  const onlyOptionalParams = countPathParams === 1 && pathParams[0].optional === true;
+  const missingOptionalParams = onlyOptionalParams && !paramValue;
+
+  // if path requires params but none exist (or they're the wrong params), send to root
+  if (!!countPathParams && !paramValue && !onlyOptionalParams) {
     compiledRoute = rootUrl;
   }
 
-  // if there are params, but it's the wrong params
-  if (
-    pathParams.length > 0 &&
-    pathParams[0] &&
-    pathParams[0].name &&
-    Object.keys(params)[0] !== pathParams[0].name
-  ) {
-    compiledRoute = rootUrl;
-  }
-
-  // Are there params being passes
-  if (Object.keys(params).length > 0) {
+  // if there are params with defined values being passed, include in route
+  if (Object.keys(params).length > 0 && !!Object.values(params)[0]) {
     const makeParamsRoute = pathToRegexp.compile(compiledRoute);
     compiledRoute = makeParamsRoute(params);
+  }
+
+  // if there are only optional params & they're not provided, build route without them
+  if (onlyOptionalParams && missingOptionalParams) {
+    const optionalPathReference = pathReference.slice(pathReference.indexOf('?') + 1);
+    compiledRoute = `${rootUrl}#${optionalPathReference}`
   }
 
   return compiledRoute;
