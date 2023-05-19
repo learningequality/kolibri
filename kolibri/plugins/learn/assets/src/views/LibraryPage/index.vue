56620d23ca02ae3efbd9dc8926e2e4fa02d570d0
Otodi Allan <otodi.allan1@gmail.com>
2023-05-19 18:30:17 +0300

    restored a message reminding the user that there is no libraries on his device

--- kolibri/plugins/learn/assets/src/views/LibraryPage/index.vue
+++ kolibri/plugins/learn/assets/src/views/LibraryPage/index.vue
@@ -37,24 +37,23 @@
         v-else-if="!displayingSearchResults"
         data-test="channels"
       >
-        <div v-if="!isLocalLibraryEmpty">
-          <h1 class="channels-label">
-            {{ channelsLabel }}
-          </h1>
-          <p
-            v-if="isLocalLibraryEmpty"
-            data-test="nothing-in-lib-label"
-            class="nothing-in-lib-label"
-          >
-            {{ coreString('nothingInLibraryLearner') }}
-          </p>
-          <ChannelCardGroupGrid
-            data-test="channel-cards"
-            class="grid"
-            :contents="rootNodes"
-            :deviceId="deviceId"
-          />
-        </div>
+        <h1 class="channels-label">
+          {{ channelsLabel }}
+        </h1>
+        <p
+          v-if="isLocalLibraryEmpty"
+          data-test="nothing-in-lib-label"
+          class="nothing-in-lib-label"
+        >
+          {{ coreString('nothingInLibraryLearner') }}
+        </p>
+        <ChannelCardGroupGrid
+          v-if="!isLocalLibraryEmpty"
+          data-test="channel-cards"
+          class="grid"
+          :contents="rootNodes"
+          :deviceId="deviceId"
+        />
         <!-- ResumableContentGrid mostly handles whether it renders or not internally !-->
         <!-- but we conditionalize it based on whether we are on another device's library page !-->
         <ResumableContentGrid
