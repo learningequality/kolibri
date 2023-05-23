65808522e95362296c3df82bb732ffe29684f629
Jacob Pierce <w.jacobpierce@gmail.com>
2023-05-23 15:26:12 -0700

    basic DeviceAppBarPage incl. DeprecationWarningBanner

---
+++ kolibri/plugins/device/assets/src/views/DeviceAppBarPage.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <AppBarPage :title="title">
+
+    <template #subNav>
+      <DeviceTopNav />
+    </template>
+
+    <DeprecationWarningBanner />
+
+    <div style="margin-top: 1em">
+      <slot></slot>
+    </div>
+
+  </AppBarPage>
+
+</template>
+
+
+<script>
+
+  import AppBarPage from 'kolibri.coreVue.components.AppBarPage';
+  import DeviceTopNav from './DeviceTopNav';
+  import DeprecationWarningBanner from './DeprecationWarningBanner';
+
+  export default {
+    name: 'DeviceAppBarPage',
+    components: { AppBarPage, DeprecationWarningBanner, DeviceTopNav },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+
+</script>
