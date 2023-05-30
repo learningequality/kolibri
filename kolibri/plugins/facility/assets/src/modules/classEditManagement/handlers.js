ce235f39133c11051b842b9a50ec7ac3508e65c9
Jacob Pierce <w.jacobpierce@gmail.com>
2023-05-30 15:43:39 -0700

    remove samePageCheck business from classEditManagement handler

--- kolibri/plugins/facility/assets/src/modules/classEditManagement/handlers.js
+++ kolibri/plugins/facility/assets/src/modules/classEditManagement/handlers.js
@@ -1,4 +1,3 @@
-import samePageCheckGenerator from 'kolibri.utils.samePageCheckGenerator';
 import { ClassroomResource, FacilityUserResource } from 'kolibri.resources';
 import { localeCompare } from 'kolibri.utils.i18n';
 import { _userState } from '../mappers';
@@ -17,24 +16,20 @@ export function showClassEditPage(store, classId) {
     ClassroomResource.fetchModel({ id: classId, force: true }),
     ClassroomResource.fetchCollection({ getParams: { parent: facilityId }, force: true }),
   ];
-  const shouldResolve = samePageCheckGenerator(store);
   store.commit('classEditManagement/SET_DATA_LOADING', true);
   Promise.all(promises).then(
     ([facilityUsers, classroom, classrooms]) => {
       store.commit('classEditManagement/SET_DATA_LOADING', false);
-
-      if (shouldResolve()) {
-        store.commit('classEditManagement/SET_STATE', {
-          modalShown: false,
-          currentClass: classroom,
-          classes: classrooms,
-          classLearners: sortUsersByFullName(facilityUsers).map(_userState),
-          classCoaches: sortUsersByFullName(classroom.coaches).map(_userState),
-        });
-      }
+      store.commit('classEditManagement/SET_STATE', {
+        modalShown: false,
+        currentClass: classroom,
+        classes: classrooms,
+        classLearners: sortUsersByFullName(facilityUsers).map(_userState),
+        classCoaches: sortUsersByFullName(classroom.coaches).map(_userState),
+      });
     },
     error => {
-      shouldResolve() ? store.dispatch('handleError', error) : null;
+      store.dispatch('handleError', error);
     }
   );
 }
