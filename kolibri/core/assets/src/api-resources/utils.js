01bb2541f547bd5e5c46b0f018d82bb092c417f8
Richard Tibbles <richard@learningequality.org>
2023-06-07 19:31:11 -0700

    Don't use content cache key for remote requests.

--- kolibri/core/assets/src/api-resources/utils.js
+++ kolibri/core/assets/src/api-resources/utils.js
@@ -5,7 +5,9 @@ const contentCacheKey = plugin_data.contentCacheKey;
 
 export function contentCacheClient(options) {
   // Add in content cache parameter if relevant
-  if (!options.data) {
+  // don't add it if we are fetching using a baseurl
+  // as the content cache key only applies to locally served content
+  if ((!options.data && !options.params) || !options.params.baseurl) {
     options.params = options.params || {};
     options.params['contentCacheKey'] = contentCacheKey;
   }
