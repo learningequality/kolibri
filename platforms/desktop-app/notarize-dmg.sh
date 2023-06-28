c838f46668c11d2ec1dcb2e36a4dd2c28c0db3db
Richard Tibbles <richard@learningequality.org>
2023-06-27 22:05:33 -0700

    Update notarization to use notarytool.

--- platforms/desktop-app/notarize-dmg.sh
+++ platforms/desktop-app/notarize-dmg.sh
@@ -5,12 +5,13 @@ primary_bundle_identifier=$( "${PLIST_BUDDY}" -c "Print CFBundleIdentifier" "dis
 
 DISK_IMAGE_PATH="$1"
 
-xcrun altool --notarize-app \
+xcrun notarytool submit \
+"${DISK_IMAGE_PATH}" \
 --type osx \
 --primary-bundle-id "org.learningequality.Kolibri" \
---username "${MAC_NOTARIZE_USERNAME}" \
+--apple-id "${MAC_NOTARIZE_USERNAME}" \
 --password "${MAC_NOTARIZE_PASSWORD}" \
---file "${DISK_IMAGE_PATH}" \
+--wait \
 --output-format xml | tee "build/notarize_result.plist"
 
 notarize_exit=$?
@@ -24,35 +25,6 @@ fi
 
 request_uuid="$("${PLIST_BUDDY}" -c "Print notarization-upload:RequestUUID"  "build/notarize_result.plist")"
 echo "Notarization UUID: ${request_uuid} result: $("${PLIST_BUDDY}" -c "Print success-message"  "build/notarize_result.plist")"
-
-
-# -------- Wait for notarization result
-
-for (( ; ; ))
-do
-	xcrun altool --notarization-info "${request_uuid}" \
-	-u "${MAC_NOTARIZE_USERNAME}" \
-	-p "${MAC_NOTARIZE_PASSWORD}" \
-	--output-format xml \
-	| tee "build/notarize_status.plist"
-
-	notarize_exit=$?
-	if [ "${notarize_exit}" != "0" ]
-	then
-		echo "Notarization failed: ${notarize_exit}"
-		cat "build/notarize_status.plist"
-		exit 1
-	fi
-	notarize_status="$("${PLIST_BUDDY}" -c "Print notarization-info:Status"  "build/notarize_status.plist")"
-	if [ "${notarize_status}" == "in progress" ]
-	then
-        echo "Waiting for notarization to complete"
-        sleep 10
-    else
-    	echo "Notarization status: ${notarize_status}"
-    	break
-	fi
-done
 
 # -------- Staple DMG
 
