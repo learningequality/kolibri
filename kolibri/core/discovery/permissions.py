8da0abe8fc5f3b7d147e9e6abb60793cd0a4ad84
José Redrejo <jose@learningequality.org>
2023-07-14 20:21:50 +0200

    Relax discovery permissions in SOuD devices

--- kolibri/core/discovery/permissions.py
+++ kolibri/core/discovery/permissions.py
@@ -1,36 +1,13 @@
 from rest_framework.permissions import BasePermission
 
-from kolibri.core.auth.permissions.general import _user_is_admin_for_own_facility
-from kolibri.core.device.utils import get_device_setting
-
 
 class NetworkLocationPermissions(BasePermission):
     """
-    A user can access NetworkLocation objects if:
-    1. User can manage content (to get import/export peers)
-    2. User is a facility admin (to be able to sync facility with peer)
-    In not Learner Only Devices, for users to be able to change to another facility,
-    any facility user must be able to discover other facilities
+    A user can access NetworkLocation objects if is a facility user:
     """
 
     def has_permission(self, request, view):
-        subset_of_users_device = get_device_setting(
-            "subset_of_users_device", default=False
-        )
-        if subset_of_users_device:
-            return request.user.can_manage_content or _user_is_admin_for_own_facility(
-                request.user
-            )
-        else:
-            return request.user.is_facility_user
+        return request.user.is_facility_user
 
     def has_object_permission(self, request, view, obj):
-        subset_of_users_device = get_device_setting(
-            "subset_of_users_device", default=False
-        )
-        if subset_of_users_device:
-            return request.user.can_manage_content or _user_is_admin_for_own_facility(
-                request.user
-            )
-        else:
-            return request.user.is_facility_user
+        return request.user.is_facility_user
