d5fcbd62d935563687b88c29dc0ac41d71cb50f3
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2023-08-10 23:38:44 +0300

    aligned the add section button border and section bottom border  to the  divider border

--- kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/CreateQuizSection.vue
+++ kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/CreateQuizSection.vue
@@ -1,164 +1,183 @@
 <template>
-    <div>
-        <KGrid>
-          <KGridItem 
-            :layout4="{ span : 1}"
-            :layout8="{ span: 1 }"
-            :layout12="{ span: 1 }">
-              <KIcon 
-                icon="quiz"
-                class="style_icon"
-              />
-          </KGridItem>
 
-          <KGridItem
-            :layout4="{ span : 3}"
-            :layout8="{ span: 7 }"
-            :layout12="{ span: 11 }">
-              <KTextbox
-                ref="title"
-                v-model.trim="examTitle"
-                :label="coachString('titleLabel')"
-                :autofocus="true"
-                :maxlength="100"
-              />
-          </KGridItem>
-        </KGrid>
+  <div>
+    <KGrid
+      class="add-padding"
+    >
+      <KGridItem
+        :layout4="{ span: 1 }"
+        :layout8="{ span: 1 }"
+        :layout12="{ span: 1 }"
+      >
+        <KIcon
+          icon="quiz"
+          class="style-icon"
+        />
+      </KGridItem>
 
-        <p>{{ $tr('addSectionsDescription') }}</p>
+      <KGridItem
+        :layout4="{ span: 3 }"
+        :layout8="{ span: 7 }"
+        :layout12="{ span: 11 }"
+      >
+        <KTextbox
+          ref="title"
+          v-model.trim="examTitle"
+          :label="coachString('titleLabel')"
+          :autofocus="true"
+          :maxlength="100"
+        />
+      </KGridItem>
+    </KGrid>
 
-        <hr id="bottom-border"/>
-        <br>
+    <p>{{ $tr('addSectionsDescription') }}</p>
 
-        <KGrid style="margin:0;padding:0">
-            <KGridItem 
-            :layout12="{ span: 8 }"
-            >
-            <span class="active-section">
-                {{ ($tr('sectionLabel')).toUpperCase() }}
-            </span>
-            </KGridItem>
+    <hr class="bottom-border">
+    <br>
 
-            <KGridItem 
-            :layout12="{ span : 4}"
-            >
-            <KButton
-                class="add-section-button"
-                appearance="flat-button"
-                icon="plus">
-                {{ ($tr('addSection')).toUpperCase() }}
-            </KButton>
-            </KGridItem>
-        </KGrid>
+    <KGrid style="margin:0;padding:0">
+      <KGridItem
+        :layout12="{ span: 8 }"
+      >
+        <span class="active-section">
+          {{ ($tr('sectionLabel')).toUpperCase() }}
+        </span>
+      </KGridItem>
 
-        <hr id="bottom-border"/>
-        
-        <div class="no-question-layout">
-            <div class="question-mark-layout">
-            <span id="help-icon-style">?</span>
-            </div>
+      <KGridItem
+        :layout12="{ span: 4 }"
+      >
+        <KButton
+          class="add-section-button"
+          appearance="flat-button"
+          icon="plus"
+        >
+          {{ ($tr('addSection')).toUpperCase() }}
+        </KButton>
+      </KGridItem>
+    </KGrid>
 
-            <p class="no-question-style">{{ $tr('noQuestionsLabel') }}</p>
+    <hr class="bottom-border">
 
-            <p>{{ $tr('selectResourceGuide') }}</p>
+    <div class="no-question-layout">
+      <div class="question-mark-layout">
+        <span class="help-icon-style">?</span>
+      </div>
 
-            <KButton 
-              primary
-              icon="plus"> 
-              {{  $tr('addQuestion') }}
-            </KButton>
+      <p class="no-question-style">
+        {{ $tr('noQuestionsLabel') }}
+      </p>
 
-        </div>
+      <p>{{ $tr('selectResourceGuide') }}</p>
+
+      <KButton
+        primary
+        icon="plus"
+      >
+        {{ $tr('addQuestion') }}
+      </KButton>
+
     </div>
-   
+  </div>
+
 </template>
 
+
 <script>
-import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
-import commonCoach from '../../common';
-export default {
-  name:"CreateQuizSection",
-  mixins: [commonCoreStrings, commonCoach],
-  $trs: {
-    sectionLabel:{
-      message:"section 1",
-      context:"Indicates the section number created",
+
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import commonCoach from '../../common';
+
+  export default {
+    name: 'CreateQuizSection',
+    mixins: [commonCoreStrings, commonCoach],
+    $trs: {
+      sectionLabel: {
+        message: 'section 1',
+        context: 'Indicates the section number created',
+      },
+      addSection: {
+        message: 'add section',
+        context: 'Label for adding the number of quiz sections',
+      },
+      noQuestionsLabel: {
+        message: 'There are no questions in this section',
+        context: 'Indicates that there is no question in the particular section',
+      },
+      selectResourceGuide: {
+        message: 'To add questions, select resources from the available channels.',
+        context: 'Explains a way of adding a question',
+      },
+      addQuestion: {
+        message: 'Add Questions',
+        context: 'Button label for adding a new question',
+      },
+      addSectionsDescription: {
+        message: 'Add one or more sections to your quiz, according to your needs',
+        context:
+          'This message indicates that more than one section can be added when creating a quiz.',
+      },
     },
-    addSection:{
-      message:"add section",
-      context:"Label for adding the number of quiz sections",
-    },
-    noQuestionsLabel:{
-      message:"There are no questions in this section",
-      context:"Indicates that there is no question in the particular section",
-    },
-    selectResourceGuide:{
-      message:"To add questions, select resources from the available channels.",
-      context:"Explains a way of adding a question",
-    },
-    addQuestion:{
-      message:"Add Questions",
-      context:"Button label for adding a new question",
-    },
-    addSectionsDescription:{
-      message:"Add one or more sections to your quiz, according to your needs",
-      context:"This message indicates that more than one section can be added when creating a quiz."
-    }
-  }
-}
+  };
+
 </script>
+
 
 <style lang="scss"  scoped>
 
-.style_icon{
-    width:40px;
-    height:40px;
-    margin:20px;
-}
-
- /deep/ .ui-textbox-label{
-    width:902px;
-  }
-  
-  #bottom-border{
-    line-height: 1.2;
-    color: #DEDEDE;
-    margin-bottom:10px;
-  }
-
-  .no-question-layout{
-    width: Fill (952px);
-    height: Fill (265px);
-    padding: 40px;
-    border-radius: 10px;
-    border: 1px;
-    text-align: center;
-    gap: 40px;
-    background-color:#FAFAFA;
-  }
-
-  .question-mark-layout{
-    margin:auto;
+  .style-icon {
     width: 40px;
     height: 40px;
-    background-color:#DBC3D4;
-    align-items: center;
+    margin: 20px;
   }
 
-  #help-icon-style{
+  /deep/ .ui-textbox-label {
+    width: 1220px;
+  }
+
+  .no-question-layout {
+    gap: 40px;
+    width: 952px;
+    height: 265px;
+    padding: 40px;
+    text-align: center;
+    background-color: #fafafa;
+    border: 1px;
+    border-radius: 10px;
+  }
+
+  .question-mark-layout {
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    margin: auto;
+    background-color: #dbc3d4;
+  }
+
+  .help-icon-style {
     font-size: 24px;
     font-weight: 700;
-    line-height: 34px;
-    color:#996189;
+    color: #996189;
   }
 
-  .active-section{
-    color:#996189;
-    border-bottom:1px solid #996189;
+  .active-section {
+    padding: 14px;
+    color: #996189;
+    border-bottom: 2px solid #996189;
   }
 
-  .add-section-button{
+  .add-section-button {
     float: right;
-    background-color:#F5F5F5;
+    margin-bottom: -8px;
+    background-color: #f5f5f5;
   }
+
+  .add-padding {
+    padding-top: 15px;
+  }
+
+  .no-question-style {
+    font-weight: bold;
+  }
+
 </style>
