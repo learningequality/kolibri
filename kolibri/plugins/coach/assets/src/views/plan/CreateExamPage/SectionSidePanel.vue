6bfd9dc756b0e7df0040801f963f0ebaf80070e6
Jacob Pierce <w.jacobpierce@gmail.com>
2023-08-21 21:35:13 -0700

    new routes, side panel basics

---
+++ kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/SectionSidePanel.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <SidePanelModal
+    v-if="$route.params.section_id"
+    ref="resourcePanel"
+    alignment="right"
+    :closeButtonIconType="closeIcon"
+    @closePanel="$router.back()"
+    @shouldFocusFirstEl="findFirstEl()"
+  >
+    <component :is="panel" :ref="$route.name" />
+  </SidePanelModal>
+
+</template>
+
+
+<script>
+
+  import SidePanelModal from 'kolibri-common/components/SidePanelModal';
+  import { PageNames } from '../../../constants';
+  import SectionEditor from './SectionEditor';
+  import ReplaceQuestions from './ReplaceQuestions';
+  import ResourceSelection from './ResourceSelection';
+
+  const pageNameComponentMap = {
+    [PageNames.QUIZ_SECTION_EDITOR]: SectionEditor,
+    [PageNames.QUIZ_REPLACE_QUESTIONS]: ReplaceQuestions,
+    [PageNames.QUIZ_SELECT_RESOURCES]: ResourceSelection,
+  };
+
+  export default {
+    name: 'SectionSidePanel',
+    components: { SidePanelModal, SectionEditor, ReplaceQuestions, ResourceSelection },
+    computed: {
+      panel() {
+        return pageNameComponentMap[this.$route.name];
+      },
+      closeIcon() {
+        return this.$route.name === PageNames.QUIZ_SELECT_RESOURCES ? 'back' : 'close';
+      },
+    },
+    methods: {
+      /**
+       * Calls the currently displayed ref's focusFirstEl method.
+       */
+      findFirstEl() {
+        this.$refs[this.$route.name].focusFirstEl();
+      },
+    },
+  };
+
+</script>
