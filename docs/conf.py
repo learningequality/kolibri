89b596e6301f86d317e64799e70bc9e37407ccf5
Richard Tibbles <richard@learningequality.org>
2023-09-15 11:58:45 -0700

    Remove commented out code.

--- docs/conf.py
+++ docs/conf.py
@@ -136,8 +136,6 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "default"
-# on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [".", sphinx_rtd_theme.get_html_theme_path()]
