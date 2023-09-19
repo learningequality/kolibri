8d61beeb9e8b1b550106e6fdab08a4b8b673aa9e
Dylan McCall <dylan@endlessos.org>
2023-09-19 15:56:33 -0700

    Remove Kolibri home template
    This is no longer necessary, because Kolibri 0.16 includes its own home
    template. Kolibri will need to run some additional database migrations
    for plugins that are specific to the Endless Key app, but these have a
    negligible performance impact.

--- platforms/flatpak-app/src/kolibri_daemon/kolibri_utils.py
+++ platforms/flatpak-app/src/kolibri_daemon/kolibri_utils.py
@@ -1,18 +1,13 @@
 from __future__ import annotations
 
-import filecmp
 import importlib.util
 import json
 import logging
 import os
 import platform
-import shutil
 import tempfile
 from gettext import gettext as _
 from pathlib import Path
-
-from kolibri_app.config import KOLIBRI_HOME_TEMPLATE_DIR
-from kolibri_app.globals import KOLIBRI_HOME_PATH
 
 from .content_extensions_manager import ContentExtensionsManager
 
@@ -35,8 +30,6 @@ OPTIONAL_PLUGINS = [
 
 
 def init_kolibri(**kwargs):
-    _kolibri_update_from_home_template()
-
     _init_kolibri_env()
 
     from kolibri.utils.main import initialize
@@ -116,42 +109,3 @@ def _get_automatic_provision_data() -> dict:
             "password": None,
         },
     }
-
-
-def _kolibri_update_from_home_template():
-    """
-    Construct a Kolibri home directory based on the Kolibri home template, if
-    necessary.
-    """
-
-    # TODO: This code should probably be in Kolibri itself
-
-    kolibri_home_template_dir = Path(KOLIBRI_HOME_TEMPLATE_DIR)
-
-    if not kolibri_home_template_dir.is_dir():
-        return
-
-    if not KOLIBRI_HOME_PATH.is_dir():
-        KOLIBRI_HOME_PATH.mkdir(parents=True, exist_ok=True)
-
-    compare = filecmp.dircmp(
-        kolibri_home_template_dir,
-        KOLIBRI_HOME_PATH,
-        ignore=["logs", "job_storage.sqlite3"],
-    )
-
-    if len(compare.common) > 0:
-        return
-
-    # If Kolibri home was not already initialized, copy files from the
-    # template directory to the new home directory.
-
-    logger.info(f"Copying KOLIBRI_HOME template to '{KOLIBRI_HOME_PATH.as_posix()}'")
-
-    for filename in compare.left_only:
-        left_file = Path(compare.left, filename)
-        right_file = Path(compare.right, filename)
-        if left_file.is_dir():
-            shutil.copytree(left_file, right_file)
-        else:
-            shutil.copy2(left_file, right_file)
