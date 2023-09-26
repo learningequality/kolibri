a03c8d5437df17bdcc6f31e299eec2e3882dbb16
Richard Tibbles <richard@learningequality.org>
2023-09-26 16:37:23 -0700

    Remove unneeded default task starting.

--- platforms/android/src/kolibri_tasks.py
+++
@@ -1,6 +0,0 @@
-def start_default_tasks():
-    from kolibri.core.analytics.tasks import schedule_ping
-    from kolibri.core.deviceadmin.tasks import schedule_vacuum
-
-    schedule_ping()
-    schedule_vacuum()
