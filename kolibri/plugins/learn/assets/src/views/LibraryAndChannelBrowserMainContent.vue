b7f93091688588506261de6336080d55176b6c40
thesujai <b210031@nitsikkim.ac.in>
2023-10-07 17:27:02 +0530

    regression fixed

--- kolibri/plugins/learn/assets/src/views/LibraryAndChannelBrowserMainContent.vue
+++ kolibri/plugins/learn/assets/src/views/LibraryAndChannelBrowserMainContent.vue
@@ -90,7 +90,7 @@
     },
     computed: {
       componentType() {
-        if (!this.windowIsSmall) {
+        if (this.windowIsSmall) {
           return 'ResourceCard';
         }
         if (this.currentCardViewStyle === 'card') {
