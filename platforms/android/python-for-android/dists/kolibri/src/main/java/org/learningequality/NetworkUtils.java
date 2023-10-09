757af63cebdc9714aca2fc01662521a54cf8f781
Richard Tibbles <richard@learningequality.org>
2023-10-09 16:25:14 -0700

    Restore zeroconf monkey patching to filter interfaces to reduce noise.

---
+++ platforms/android/python-for-android/dists/kolibri/src/main/java/org/learningequality/NetworkUtils.java
@@ -0,0 +1,54 @@
+package org.learningequality;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class NetworkUtils {
+
+    public static List<String> getActiveIPv4Addresses() {
+        List<String> ipAddresses = new ArrayList<>();
+
+        List<NetworkInterface> networkInterfaces;
+        try {
+            networkInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+        } catch (SocketException e) {
+            e.printStackTrace();
+            return ipAddresses; // Return empty list if there's a problem fetching network interfaces
+        }
+
+        for (NetworkInterface networkInterface : networkInterfaces) {
+            try {
+                // Check if the network interface is up (active)
+                if (!networkInterface.isUp() || networkInterface.isLoopback()) {
+                    continue;  // Skip inactive interfaces, and loopback ones
+                }
+
+                // Check if the network interface supports multicast
+                if (!networkInterface.supportsMulticast()) {
+                    continue;  // Skip interfaces that don't support multicast
+                }
+
+                // Get all IP addresses associated with the interface
+                List<InetAddress> inetAddresses = Collections.list(networkInterface.getInetAddresses());
+
+                for (InetAddress inetAddress : inetAddresses) {
+                    // Ensure this is an IPv4 address
+                    if (inetAddress instanceof Inet4Address) {
+                        ipAddresses.add(inetAddress.getHostAddress());
+                    }
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();  // Handle or log the error for this particular network interface
+                // Continue with the next interface
+            }
+        }
+
+        return ipAddresses;
+    }
+}
