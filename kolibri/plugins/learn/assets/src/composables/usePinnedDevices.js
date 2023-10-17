d77e76bd7b0af839172788d2636336b8266a5725
Richard Tibbles <richard@learningequality.org>
2023-10-17 16:14:43 -0700

    simplify pinned devices composable to match backend updates.

--- kolibri/plugins/learn/assets/src/composables/usePinnedDevices.js
+++ kolibri/plugins/learn/assets/src/composables/usePinnedDevices.js
@@ -2,23 +2,19 @@
  * A composable function containing logic related to pinned devices
  */
 
-import { getCurrentInstance } from 'kolibri.lib.vueCompositionApi';
 import { PinnedDeviceResource } from 'kolibri.resources';
 
 export default function usePinnedDevices() {
-  const $store = getCurrentInstance().proxy.$store;
-  const user = $store.state.core.session.user_id;
-
   function fetchPinsForUser() {
     return PinnedDeviceResource.fetchCollection({ force: true });
   }
 
   function createPinForUser(instance_id) {
-    return PinnedDeviceResource.create({ instance_id, user });
+    return PinnedDeviceResource.create({ instance_id });
   }
 
   function deletePinForUser(id) {
-    return PinnedDeviceResource.deleteModel(id, { user });
+    return PinnedDeviceResource.deleteModel(id);
   }
   return {
     createPinForUser,
