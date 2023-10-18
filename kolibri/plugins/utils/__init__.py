8450b145b68fce43669e96addb1c418331ddc4c5
Aryan Sharma <adusharma22@gmail.com>
2023-10-19 01:11:48 +0530

    updated use of entry_points according to importlib version >3.6.0 (#11417)
    * updated use of entry_points according to importlib version >3.6.0
    
    * added backwards compatibility for importlib
    
    * Update __init__.py
    
    Co-authored-by: Richard Tibbles <richard@learningequality.org>
    
    * use isInstance() for type
    
    ---------
    
    Co-authored-by: Richard Tibbles <richard@learningequality.org>

--- kolibri/plugins/utils/__init__.py
+++ kolibri/plugins/utils/__init__.py
@@ -481,7 +481,13 @@ def check_plugin_config_file_location(version):
 def iterate_plugins():
     # Use to dedupe plugins
     plugin_ids = set()
-    for entry_point in entry_points().get("kolibri.plugins", []):
+    all_entry_points = entry_points()
+    eps = (
+        all_entry_points.get("kolibri.plugins", [])
+        if isinstance(all_entry_points, dict)
+        else all_entry_points.select(group="kolibri.plugins")
+    )
+    for entry_point in eps:
         name = entry_point.name
         if name not in plugin_ids:
             plugin_ids.add(name)
