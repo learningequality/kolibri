87064a31f857c340a8e33771709fc3b062ae2456
Jacob Pierce <w.jacobpierce@gmail.com>
2023-10-23 20:36:59 -0700

    enhanced quiz messages updates

--- packages/kolibri-common/strings/enhancedQuizManagementStrings.js
+++ packages/kolibri-common/strings/enhancedQuizManagementStrings.js
@@ -1,6 +1,9 @@
 import { createTranslator } from 'kolibri.utils.i18n';
 
 export const enhancedQuizManagementStrings = createTranslator('EnhancedQuizManagementStrings', {
+  selectAllLabel: {
+    message: 'Select all',
+  },
   sectionLabel: {
     message: 'Section',
   },
@@ -70,7 +73,7 @@ export const enhancedQuizManagementStrings = createTranslator('EnhancedQuizManag
   numberOfSelectedQuestions: {
     message: '{count, number} {count, plural, one {question selected} other {questions selected}}',
   },
-  replqceQuestions: {
+  replaceQuestions: {
     message: 'Replace questions',
   },
   changeResources: {
@@ -88,11 +91,8 @@ export const enhancedQuizManagementStrings = createTranslator('EnhancedQuizManag
   expandAll: {
     message: 'Expand all',
   },
-  replace: {
+  replaceAction: {
     message: 'Replace',
-  },
-  replaceQuestions: {
-    message: 'Replace questions',
   },
   replaceQuestionsExplaination: {
     message: 'The new questions you selected will replace the current ones.',
