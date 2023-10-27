76d2100b17bb15e39c59b48f671c2624895c3149
marcellamaki <marcella@learningequality.org>
2023-10-27 13:24:37 -0400

    Revert "Disabling additional user import until multiple user syncing is working"

--- kolibri/plugins/setup_wizard/assets/src/views/ImportMultipleUsers.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/ImportMultipleUsers.vue
@@ -32,7 +32,6 @@
               v-if="!isImported(userRow.user) && !isImporting(userRow.user)"
               :text="coreString('importAction')"
               appearance="flat-button"
-              :disabled="isImportingUserDisabled"
               @click="startImport(userRow.user)"
             />
             <KCircularLoader
@@ -92,8 +91,6 @@
         isPolling: false,
         // array of user/learner ids
         learnersBeingImported: [],
-        // To be removed
-        isImportingUserDisabled: false,
       };
     },
     inject: ['wizardService'],
@@ -143,20 +140,12 @@
     beforeMount() {
       this.isPolling = true;
       this.pollImportTask();
-      this.checkImportedUser();
     },
     methods: {
       importedLearners() {
         return this.wizardService.state.context.importedUsers;
       },
       pollImportTask() {
-        // TODO :: This has to be removed once the multiple user syncing has been fixed.
-        if (
-          this.learnersBeingImported.length > 0 ||
-          this.wizardService.state.context.importedUsers.length > 0
-        ) {
-          this.wizardService.send('LOADING');
-        }
         TaskResource.list({ queue: SoudQueue }).then(tasks => {
           if (tasks.length) {
             tasks.forEach(task => {
@@ -197,7 +186,6 @@
         }
       },
       startImport(learner) {
-        this.isImportingUserDisabled = true;
         // Push the learner into being imported, we'll remove it if we get an error later on
         this.learnersBeingImported.push(learner.id);
 
@@ -219,20 +207,13 @@
         }
         TaskResource.startTask(params).catch(() => {
           this.learnersBeingImported = this.learnersBeingImported.filter(id => id != learner.id);
-          this.isImportingUserDisabled = false;
         });
       },
       isImported(learner) {
         return this.importedLearners().find(u => u === learner.username);
       },
       isImporting(learner) {
-        this.checkImportedUser(learner);
         return this.learnersBeingImported.includes(learner.id);
-      },
-      checkImportedUser(learner) {
-        if (this.learnersBeingImported.length > 0 || this.isImported(learner)) {
-          this.isImportingUserDisabled = true;
-        }
       },
     },
     $trs: {
