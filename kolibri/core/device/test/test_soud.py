b3e79b40aa598afc782d86d7b30460ed62ec4b75
Blaine Jester <blainesworld@gmail.com>
2023-11-03 12:33:10 -0700

    Better defensiveness against discovery hooks interference

--- kolibri/core/device/test/test_soud.py
+++ kolibri/core/device/test/test_soud.py
@@ -1,12 +1,18 @@
 """
 Subset of Users Device (SOUD) tests
 """
+import time
 import uuid
 
+import mock
 from django.test import TestCase
 
 from ..soud import Context
+from ..soud import request_sync_hook
+from kolibri.core.device.models import SyncQueue
+from kolibri.core.device.models import SyncQueueStatus
 from kolibri.core.discovery.models import ConnectionStatus
+from kolibri.core.discovery.models import DynamicNetworkLocation
 from kolibri.core.discovery.models import StaticNetworkLocation
 
 
@@ -41,3 +47,100 @@ class SoudContextTestCase(TestCase):
             instance_id=self.context.instance_id,
         )
         self.assertIsNone(self.context.network_location)
+
+
+class SoudRequestSyncHookHandlerTestCase(TestCase):
+    multi_db = True
+
+    def setUp(self):
+        super(SoudRequestSyncHookHandlerTestCase, self).setUp()
+        self.instance_id = uuid.uuid4().hex
+        self.user_ids = [
+            uuid.uuid4().hex,
+        ]
+
+        self.mock_network_location = mock.Mock(
+            spec=DynamicNetworkLocation,
+            instance_id=self.instance_id,
+        )
+
+        get_all_user_ids = mock.patch("kolibri.core.device.soud.get_all_user_ids")
+        self.mock_get_all_user_ids = get_all_user_ids.start()
+        self.mock_get_all_user_ids.return_value = self.user_ids
+        self.addCleanup(get_all_user_ids.stop)
+
+        request_sync = mock.patch("kolibri.core.device.soud.request_sync")
+        self.mock_request_sync = request_sync.start()
+        self.addCleanup(request_sync.stop)
+
+    def assertRequestSyncCalled(self, user_id, call_index=0):
+        calls = self.mock_request_sync.mock_calls
+        _, args, kwargs = calls[call_index]
+        self.assertIsInstance(args[0], Context)
+        self.assertEqual(args[0].user_id, user_id)
+        self.assertEqual(args[0].instance_id, self.instance_id)
+        self.assertEqual(kwargs, {"network_location": self.mock_network_location})
+
+    def assertPendingQueue(self, user_id):
+        queue = SyncQueue.objects.get(user_id=user_id)
+        self.assertEqual(queue.instance_id, self.instance_id)
+        self.assertEqual(queue.status, SyncQueueStatus.Pending)
+        self.assertGreaterEqual(queue.updated, time.time() - 1)
+
+    def test_single_user__no_queue(self):
+        request_sync_hook(self.mock_network_location)
+        self.assertRequestSyncCalled(self.user_ids[0])
+        self.assertPendingQueue(self.user_ids[0])
+
+    def test_single_user__existing_queue__active(self):
+        queue = SyncQueue.objects.create(
+            user_id=self.user_ids[0],
+            instance_id=self.instance_id,
+            status=SyncQueueStatus.Syncing,
+        )
+        request_sync_hook(self.mock_network_location)
+        self.mock_request_sync.assert_not_called()
+        updated = queue.updated
+        queue.refresh_from_db()
+        self.assertEqual(queue.status, SyncQueueStatus.Syncing)
+        self.assertEqual(queue.updated, updated)
+
+    def test_single_user__existing_queue__inactive(self):
+        SyncQueue.objects.create(
+            user_id=self.user_ids[0],
+            instance_id=self.instance_id,
+            status=SyncQueueStatus.Syncing,
+            updated=time.time() - 100,
+        )
+        request_sync_hook(self.mock_network_location)
+        self.assertRequestSyncCalled(self.user_ids[0])
+        self.assertPendingQueue(self.user_ids[0])
+
+    def test_multiple_users(self):
+        self.user_ids.extend([uuid.uuid4().hex, uuid.uuid4().hex])
+        user_a, user_b, user_c = self.user_ids
+
+        # user_a stale
+        SyncQueue.objects.create(
+            user_id=user_a,
+            instance_id=self.instance_id,
+            status=SyncQueueStatus.Pending,
+            updated=time.time() - 100,
+        )
+        # user_b active
+        queue = SyncQueue.objects.create(
+            user_id=user_b,
+            instance_id=self.instance_id,
+            status=SyncQueueStatus.Syncing,
+        )
+        # user_c no queue
+        request_sync_hook(self.mock_network_location)
+        self.assertEqual(self.mock_request_sync.call_count, 2)
+        self.assertRequestSyncCalled(user_a, call_index=0)
+        self.assertRequestSyncCalled(user_c, call_index=1)
+        self.assertPendingQueue(user_a)
+        self.assertPendingQueue(user_c)
+        updated = queue.updated
+        queue.refresh_from_db()
+        self.assertEqual(queue.status, SyncQueueStatus.Syncing)
+        self.assertEqual(queue.updated, updated)
