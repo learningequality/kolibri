a2fc18c3bc97d3badb15d86eeb39709ea2c95901
José Redrejo <jose@learningequality.org>
2023-11-14 20:14:32 +0100

    Hardcode user model to avoid conflict with other kolibri usages

--- kolibri/core/device/migrations/0019_syncqueue_and_status.py
+++ kolibri/core/device/migrations/0019_syncqueue_and_status.py
@@ -79,7 +79,7 @@ class Migration(migrations.Migration):
             name="user",
             field=models.ForeignKey(
                 on_delete=django.db.models.deletion.CASCADE,
-                to=settings.AUTH_USER_MODEL,
+                to="kolibriauth.FacilityUser",
                 unique=True,
             ),
         ),
