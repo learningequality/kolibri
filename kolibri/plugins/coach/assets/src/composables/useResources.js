fd7bf8eb134ab16d7a4af229a4f1d336d31d3a0c
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2023-11-22 19:36:48 +0300

    displays boomarked resources

--- kolibri/plugins/coach/assets/src/composables/useResources.js
+++ kolibri/plugins/coach/assets/src/composables/useResources.js
@@ -1,7 +1,7 @@
 import { ref, onMounted } from 'kolibri.lib.vueCompositionApi';
-import { ChannelResource,ContentNodeResource} from 'kolibri.resources';
-import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
-import { getContentNodeThumbnail } from 'kolibri.utils.contentNode';
+import { ChannelResource, ContentNodeResource } from 'kolibri.resources';
+// import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
+// import { getContentNodeThumbnail } from 'kolibri.utils.contentNode';
 
 export function useResources() {
   const resources = ref(null);
@@ -22,79 +22,80 @@ export function useResources() {
     });
   }
 
-  function _getTopicsWithExerciseDescendants(topicIds = []) {
-    return new Promise(resolve => {
-      if (!topicIds.length) {
-        resolve([]);
-        return;
-      }
-      const topicsNumAssessmentDescendantsPromise = ContentNodeResource.fetchDescendantsAssessments(
-        topicIds
-      );
-  
-      topicsNumAssessmentDescendantsPromise.then(response => {
-        const topicsWithExerciseDescendants = [];
-        response.data.forEach(descendantAssessments => {
-          if (descendantAssessments.num_assessments > 0) {
-            topicsWithExerciseDescendants.push({
-              id: descendantAssessments.id,
-              numAssessments: descendantAssessments.num_assessments,
-              exercises: [],
-            });
-          }
-        });
-  
-        ContentNodeResource.fetchDescendants(
-          topicsWithExerciseDescendants.map(topic => topic.id),
-          {
-            descendant_kind: ContentNodeKinds.EXERCISE,
-          }
-        ).then(response => {
-          response.data.forEach(exercise => {
-            const topic = topicsWithExerciseDescendants.find(t => t.id === exercise.ancestor_id);
-            topic.exercises.push(exercise);
-          });
-          resolve(topicsWithExerciseDescendants);
-        });
-      });
-    });
-  }
-  
-  function filterAndAnnotateContentList(childNodes) {
-    return new Promise(resolve => {
-      const childTopics = childNodes.filter(({ kind }) => kind === ContentNodeKinds.TOPIC);
-      const topicIds = childTopics.map(({ id }) => id);
-      const topicsThatHaveExerciseDescendants = _getTopicsWithExerciseDescendants(topicIds);
-  
-      topicsThatHaveExerciseDescendants.then(topics => {
-        const childNodesWithExerciseDescendants = childNodes
-          .map(childNode => {
-            const index = topics.findIndex(topic => topic.id === childNode.id);
-            if (index !== -1) {
-              return { ...childNode, ...topics[index] };
-            }
-            return childNode;
-          })
-          .filter(childNode => {
-            if (childNode.kind === ContentNodeKinds.TOPIC && (childNode.numAssessments || 0) < 1) {
-              return false;
-            }
-            return true;
-          });
-  
-        const contentList = childNodesWithExerciseDescendants.map(node => ({
-          ...node,
-          thumbnail: getContentNodeThumbnail(node),
-        }));
-        resolve(contentList);
-      });
-    });
+  //   function _getTopicsWithExerciseDescendants(topicIds = []) {
+  //     return new Promise(resolve => {
+  //       if (!topicIds.length) {
+  //         resolve([]);
+  //         return;
+  //       }
+  //       const topicsNumAssessmentDescendantsPromise
+  //   = ContentNodeResource.fetchDescendantsAssessments(
+  //         topicIds
+  //       );
+
+  //       topicsNumAssessmentDescendantsPromise.then(response => {
+  //         const topicsWithExerciseDescendants = [];
+  //         response.data.forEach(descendantAssessments => {
+  //           if (descendantAssessments.num_assessments > 0) {
+  //             topicsWithExerciseDescendants.push({
+  //               id: descendantAssessments.id,
+  //               numAssessments: descendantAssessments.num_assessments,
+  //               exercises: [],
+  //             });
+  //           }
+  //         });
+
+  //         ContentNodeResource.fetchDescendants(
+  //           topicsWithExerciseDescendants.map(topic => topic.id),
+  //           {
+  //             descendant_kind: ContentNodeKinds.EXERCISE,
+  //           }
+  //         ).then(response => {
+  //           response.data.forEach(exercise => {
+  //             const topic = topicsWithExerciseDescendants.find(t =>
+  //   t.id === exercise.ancestor_id);
+  //             topic.exercises.push(exercise);
+  //           });
+  //           resolve(topicsWithExerciseDescendants);
+  //         });
+  //       });
+  //     });
+  //   }
+
+  function filterAndAnnotateContentList(/*childNodes*/) {
+    // return new Promise(resolve => {
+    // const childTopics = childNodes.filter(({ kind }) => kind === ContentNodeKinds.TOPIC);
+    // const topicIds = childTopics.map(({ id }) => id);
+    // const topicsThatHaveExerciseDescendants = _getTopicsWithExerciseDescendants(topicIds);
+    //   topicsThatHaveExerciseDescendants.then(topics => {
+    //     const childNodesWithExerciseDescendants = childNodes
+    //       .map(childNode => {
+    //         const index = topics.findIndex(topic => topic.id === childNode.id);
+    //         if (index !== -1) {
+    //           return { ...childNode, ...topics[index] };
+    //         }
+    //         return childNode;
+    //       })
+    //       .filter(childNode => {
+    //         if (childNode.kind === ContentNodeKinds.TOPIC &&
+    //    (childNode.numAssessments || 0) < 1) {
+    //           return false;
+    //         }
+    //         return true;
+    //       });
+    //     const contentList = childNodesWithExerciseDescendants.map(node => ({
+    //       ...node,
+    //       thumbnail: getContentNodeThumbnail(node),
+    //     }));
+    //     resolve(contentList);
+    //   });
+    // } );
   }
 
   onMounted(() => {
     fetchChannelResource();
     fetchBookMarkedResource();
-    filterAndAnnotateContentList()
+    filterAndAnnotateContentList();
   });
 
   return {
--- kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/ShowBookMarkedResources.vue
+++ kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/ShowBookMarkedResources.vue
@@ -1,102 +1,127 @@
-  <template>
-    <div class="">
+<template>
 
-        <ContentCardList
-            :contentList="bookmarks"
-            :showSelectAll="selectAllIsVisible"
-            :viewMoreButtonState="viewMoreButtonState"
-            :selectAllChecked="bookmarks.length === 0"
-            :contentIsChecked="contentIsInLesson"
-            :contentHasCheckbox="c => !contentIsDirectoryKind(c)"
-            :contentCardMessage="() =>selectionMetadata"
-            :contentCardLink="contentLink"
-            @changeselectall="toggleTopicInWorkingResources"
-            @change_content_card="toggleSelected"
-            @moreresults="handleMoreResults"
-        />
-    </div>
+  <div class="">
+    <ContentCardList
+      :contentList="bookmarks"
+      :showSelectAll="selectAllIsVisible"
+      :viewMoreButtonState="viewMoreButtonState"
+      :selectAllChecked="bookmarks.length === 0"
+      :contentIsChecked="contentIsInLesson"
+      :contentHasCheckbox="c => !contentIsDirectoryKind(c)"
+      :contentCardMessage="() =>selectionMetadata"
+      :contentCardLink="contentLink"
+      @changeselectall="toggleTopicInWorkingResources"
+      @change_content_card="toggleSelected"
+      @moreresults="handleMoreResults"
+    />
+  </div>
+
 </template>
 
+
 <script>
- import { useResources } from '../../../composables/useResources';
- import ContentCardList from './../LessonResourceSelectionPage/ContentCardList.vue';
- import { PageNames } from '../../../constants';
-export default {
-    name:"ShowBookMarkedResources",
-    components:{
-        ContentCardList,
-    },
-    setup(){
-        const {
-         bookmarks
-        } = useResources();
 
-        return {
-            bookmarks,
-        }
+  import pickBy from 'lodash/pickBy';
+  import { useResources } from '../../../composables/useResources';
+  import { PageNames } from '../../../constants';
+  import ContentCardList from './../LessonResourceSelectionPage/ContentCardList.vue';
+
+  export default {
+    name: 'ShowBookMarkedResources',
+    components: {
+      ContentCardList,
     },
-    computed:{
-        selectAllIsVisible(){
-            return true;
-        },
-        addableContent() {
+    setup() {
+      const { bookmarks } = useResources();
+
+      return {
+        bookmarks,
+      };
+    },
+    data() {
+      return {
+        viewMoreButtonState: 'no_more_results',
+      };
+    },
+    computed: {
+      selectAllIsVisible() {
+        return true;
+      },
+      addableContent() {
         // Content in the topic that can be added if 'Select All' is clicked
-            const list = bookmarksList;
-            return list.filter(
-                content => !this.contentIsDirectoryKind(content) && !this.contentIsInLesson(content)
-            );
-        },
-        contentIsInLesson() {
-            return ({ id }) =>
-                Boolean(this.workingResources.find(resource => resource.contentnode_id === id));
-        },
-        selectionMetadata(){
-            return '';
-        },
+        const list = this.bookmarks;
+        return list.filter(
+          content => !this.contentIsDirectoryKind(content) && !this.contentIsInLesson(content)
+        );
+      },
+      contentIsInLesson() {
+        return ({ id }) => Boolean(this.bookmarks.find(resource => resource.contentnode_id === id));
+      },
+      selectionMetadata() {
+        return '';
+      },
     },
-    data(){
-        return {
-            viewMoreButtonState: 'no_more_results',
+    watch: {
+      workingResources(newVal, oldVal) {
+        this.showResourcesDifferenceMessage(newVal.length - oldVal.length);
+        this.debouncedSaveResources();
+      },
+      filters(newVal) {
+        const newQuery = {
+          ...this.$route.query,
+          ...newVal,
+        };
+        this.$router.push({
+          query: pickBy(newQuery),
+        });
+      },
+    },
+    methods: {
+      contentLink(content) {
+        if (!content.is_leaf) {
+          return {
+            name: PageNames.SELECT_FROM_RESOURCE,
+            params: {
+              topic_id: content.id,
+            },
+          };
+        } else {
+          return {};
         }
-    },
-    methods:{
-        contentLink(){
-            if (!content.is_leaf) {
-                return {
-                    name: PageNames.SELECT_FROM_RESOURCE,
-                    params: {
-                    topic_id: content.id,
-                    },
-                };
-            }
-        },
-        toggleTopicInWorkingResources(){
-            if (isChecked) {
-            this.addableContent.forEach(resource => {
-                this.addToResourceCache({
-                node: { ...resource },
-                });
+      },
+      toggleTopicInWorkingResources(isChecked) {
+        if (isChecked) {
+          this.addableContent.forEach(resource => {
+            this.addToResourceCache({
+              node: { ...resource },
             });
-            this.addToWorkingResources(this.addableContent);
-            } else {
-            this.removeFromSelectedResources(this.quizForge.channels.value);
-            }
-        },
-        toggleSelected(){
-            if (checked) {
-                this.addToSelectedResources(content);
-            } else {
-                this.removeFromSelectedResources([content]);
-            }
-        },
-        handleMoreResults(){
+          });
+          this.addToWorkingResources(this.addableContent);
+        } else {
+          this.removeFromSelectedResources(this.quizForge.channels.value);
+        }
+      },
+      toggleSelected({ content, checked }) {
+        if (checked) {
+          this.addToSelectedResources(content);
+        } else {
+          this.removeFromSelectedResources([content]);
+        }
+      },
+      addToSelectedResources(content) {
+        const list =
+          this.contentList && this.contentList.length ? this.contentList : this.bookmarksList;
+        this.addToResourceCache({
+          node: list.find(n => n.id === content.id),
+        });
+        this.addToWorkingResources([content]);
+      },
+      handleMoreResults() {},
 
-        },
-       
-        contentIsDirectoryKind({ is_leaf }) {
-            return !is_leaf;
-        },
-        
-    }
-}
+      contentIsDirectoryKind({ is_leaf }) {
+        return !is_leaf;
+      },
+    },
+  };
+
 </script>
