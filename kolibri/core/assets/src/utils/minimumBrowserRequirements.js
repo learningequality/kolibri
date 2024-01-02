269a04b5df12fad456db365530ec18eb32f919f6
Richard Tibbles <richard@learningequality.org>
2024-01-02 14:05:12 -0800

    Use browserslist configuration for kolibri as single source of truth for supported browsers.

--- kolibri/core/assets/src/utils/minimumBrowserRequirements.js
+++ kolibri/core/assets/src/utils/minimumBrowserRequirements.js
@@ -1,16 +1,30 @@
+import isUndefined from 'lodash/isUndefined';
+import browsers from 'browserslist-config-kolibri';
 import { browser, passesRequirements } from './browserInfo';
 import plugin_data from 'plugin_data';
 
-const minimumBrowserRequirements = {
-  IE: {
-    major: 11,
-  },
-  Android: {
-    major: 4,
-    minor: 0,
-    patch: 2,
-  },
-};
+const minimumBrowserRequirements = {};
+
+const browserRegex = /^([a-zA-Z]+) ([><=]+) (\d+)(?:\.(\d+))?(?:\.(\d+))?$/;
+
+for (const browser of browsers) {
+  const [name, sign, major, minor, patch] = browserRegex.exec(browser).slice(1);
+  // This only supports > and >=, but that's all we need.
+  const addOne = sign === '>' ? 1 : 0;
+  const entry = {
+    major: isUndefined(minor) && isUndefined(patch) ? Number(major) + addOne : Number(major),
+  };
+  if (!isUndefined(minor)) {
+    entry.minor = Number(minor);
+    if (isUndefined(patch)) {
+      entry.minor += addOne;
+    }
+  }
+  if (!isUndefined(patch)) {
+    entry.patch = Number(patch) + addOne;
+  }
+  minimumBrowserRequirements[name] = entry;
+}
 
 if (!passesRequirements(browser, minimumBrowserRequirements)) {
   window.location.href = plugin_data.unsupportedUrl;
