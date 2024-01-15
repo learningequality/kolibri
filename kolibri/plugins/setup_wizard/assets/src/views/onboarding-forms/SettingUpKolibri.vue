a14933c998894d9294e57ff33e0f27ac18dcff0e
Marcella Maki <marcellamaki@gmail.com>
2024-01-15 17:32:35 -0500

    Add computed prop to guard against invalid presets

--- kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/SettingUpKolibri.vue
+++ kolibri/plugins/setup_wizard/assets/src/views/onboarding-forms/SettingUpKolibri.vue
@@ -143,7 +143,7 @@
           ...this.facilityData,
           superuser,
           settings: omitBy(settings, v => v === null),
-          preset: this.wizardContext('formalOrNonformal') || 'nonformal',
+          preset: this.presetValue,
           language_id: currentLanguage,
           device_name:
             this.wizardContext('deviceName') ||
@@ -161,6 +161,16 @@
       /** Introspecting the machine via it's `state.context` properties */
       isOnMyOwnSetup() {
         return this.wizardContext('onMyOwnOrGroup') == Presets.PERSONAL;
+      },
+      presetValue() {
+        // this computed prop was to guard against a strange edge case where a mismatched
+        // preset was inadvertently being sent to the backend, where the values
+        // were not valid, including an incorrect fallback, and 'on my own' being sent as a value
+        if (this.isOnMyOwnSetup) {
+          return Presets.PERSONAL;
+        } else {
+          return this.wizardContext('formalOrNonformal');
+        }
       },
     },
     created() {
