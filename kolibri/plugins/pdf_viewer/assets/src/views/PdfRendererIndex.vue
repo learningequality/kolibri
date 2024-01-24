fa6100e7d3e15e18991f8ad72c1d84b1b4057d3f
theditor <theditormcpe@gmail.com>
2024-01-24 17:51:06 +0530

    fix: extra space at pdf end

--- kolibri/plugins/pdf_viewer/assets/src/views/PdfRendererIndex.vue
+++ kolibri/plugins/pdf_viewer/assets/src/views/PdfRendererIndex.vue
@@ -736,4 +736,19 @@
     height: 100%;
   }
 
+  /deep/ .resize-observer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: transparent;
+    pointer-events: none;
+    display: block;
+    overflow: hidden;
+    opacity: 0;
+  }
+
 </style>
