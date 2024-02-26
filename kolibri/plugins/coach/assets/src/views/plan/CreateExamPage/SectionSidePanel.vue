7dd656f9e06a5f37086587c0f688a5b8533e142c
Jacob Pierce <w.jacobpierce@gmail.com>
2024-02-25 22:58:28 -0800

    side panel doesn't open if the section_id in the URL is bad

--- kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/SectionSidePanel.vue
+++ kolibri/plugins/coach/assets/src/views/plan/CreateExamPage/SectionSidePanel.vue
@@ -1,6 +1,7 @@
 <template>
 
   <SidePanelModal
+    v-if="showSidePanel"
     ref="resourcePanel"
     alignment="right"
     sidePanelWidth="700px"
@@ -17,12 +18,47 @@
 <script>
 
   import SidePanelModal from 'kolibri-common/components/SidePanelModal';
+  import { computed, getCurrentInstance } from 'kolibri.lib.vueCompositionApi';
+  import logging from 'kolibri.lib.logging';
+  import { injectQuizCreation } from '../../../composables/useQuizCreation';
   import { PageNames } from '../../../constants';
+
+  const logger = logging.getLogger(__filename);
 
   export default {
     name: 'SectionSidePanel',
     components: {
       SidePanelModal,
+    },
+    setup() {
+      const { activeSection } = injectQuizCreation();
+      const store = getCurrentInstance().proxy.$store;
+      const router = getCurrentInstance().proxy.$router;
+      const route = computed(() => store.state.route);
+      const section_id = computed(() => route.value.params.section_id);
+
+      function handleClosePanel() {
+        router.push({
+          name: PageNames.EXAM_CREATION_ROOT,
+          params: { section_id: null },
+        });
+      }
+
+      /**
+       * Avoids flashing the side panel when we're going to just hide it anyway.
+       */
+      let showSidePanel = false;
+
+      if (section_id.value !== activeSection.value.section_id) {
+        logger.warn("Section ID doesn't match active section ID, forcing close of side panel.");
+        handleClosePanel();
+      } else {
+        showSidePanel = true;
+      }
+      return {
+        showSidePanel,
+        handleClosePanel,
+      };
     },
     data() {
       return {
@@ -50,9 +86,6 @@
       },
     },
     methods: {
-      handleClosePanel() {
-        this.$router.push({ name: PageNames.EXAM_CREATION_ROOT });
-      },
       /**
        * Calls the currently displayed ref's focusFirstEl method.
        */
