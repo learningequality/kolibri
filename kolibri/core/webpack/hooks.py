83a4a1d8c762c43fe9e1eb1807a4e65dad288d21
Richard Tibbles <richard@learningequality.org>
2024-02-28 13:33:26 -0800

    Update for new staticlib API.

--- kolibri/core/webpack/hooks.py
+++ kolibri/core/webpack/hooks.py
@@ -124,9 +124,9 @@ class WebpackBundleHook(hooks.KolibriHook):
                 try:
                     f["url"] = f["publicPath"]
                 except KeyError:
-                    f["url"] = staticfiles_storage.url(relpath)
+                    f["url"] = staticfiles_storage.path(relpath)
             else:
-                f["url"] = staticfiles_storage.url(relpath)
+                f["url"] = staticfiles_storage.path(relpath)
             yield f
 
     @property
