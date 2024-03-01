f1f71f2307ac4300c930ccd816fcb81f73728729
iskipu <rvamsi599+github@gmail.com>
2024-03-01 22:39:13 +0530

    fix syntax

--- kolibri/plugins/learn/assets/src/my_downloads/views/DownloadsList/index.vue
+++ kolibri/plugins/learn/assets/src/my_downloads/views/DownloadsList/index.vue
@@ -66,7 +66,7 @@
                   <div v-if="download.status === 'IN_PROGRESS'" class="inline-loader">
                     <KCircularLoader
                       :size="20"
-                      disableDefaultTransition="true"
+                      :disableDefaultTransition="true"
                       class="icon"
                     />
                   </div>
