abdfe36e224ece737dd33eec07eed2704a2af649
Richard Tibbles <richard@learningequality.org>
2024-04-30 11:57:34 -0700

    Update version.py to 0.1.2

--- platforms/android/scripts/version.py
+++ platforms/android/scripts/version.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from play_store_api import get_latest_version_code
 
 
-android_installer_version = "0.1.1"
+android_installer_version = "0.1.2"
 
 
 BUILD_TYPE_DEBUG = "debug"
