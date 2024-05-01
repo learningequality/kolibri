e5bb8ea145902ab1fa5963ea1e6ad9ba6243ffaf
Richard Tibbles <richard@learningequality.org>
2024-04-30 17:08:44 -0700

    Ensure we use options.ini configuration for port selection.

--- platforms/desktop-app/src/kolibri_app/__init__.py
+++ platforms/desktop-app/src/kolibri_app/__init__.py
@@ -13,3 +13,4 @@ if MAC:
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "kolibri_app.django_app_settings"
 enable_plugin("kolibri.plugins.app")
+enable_plugin("kolibri_app")
---
+++ platforms/desktop-app/src/kolibri_app/kolibri_plugin.py
@@ -0,0 +1,5 @@
+from kolibri.plugins import KolibriPluginBase
+
+
+class KolibriApp(KolibriPluginBase):
+    kolibri_option_defaults = "options_defaults"
