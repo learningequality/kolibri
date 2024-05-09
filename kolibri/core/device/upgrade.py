79df69adceb87898121a58920b61bc3598b9423b
Richard Tibbles <richard@learningequality.org>
2024-05-09 11:57:25 -0700

    Fix incorrect language code for Chewa. Use ny instead of nyn. Add intl locale data for ny language code.

--- kolibri/core/device/upgrade.py
+++ kolibri/core/device/upgrade.py
@@ -13,3 +13,12 @@ from kolibri.core.upgrade import version_upgrade
 @version_upgrade(old_version="<0.15.0")
 def clear_static_dir():
     rmtree(settings.STATIC_ROOT, ignore_errors=True)
+
+
+@version_upgrade(old_version="<0.16.2")
+def migrate_nyn_to_ny():
+    from kolibri.core.device.utils import get_device_setting
+    from kolibri.core.device.utils import set_device_settings
+
+    if get_device_setting("language_id") == "nyn":
+        set_device_settings(language_id="ny")
