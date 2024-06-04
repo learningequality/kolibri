b7a2fa9013b95b0bc9a1b5a52edafa6efacffc09
Nathanael Gutierrez <nathanaelg16@users.noreply.github.com>
2024-06-04 11:52:45 -0400

    Fixing typo in useUser composable getter (again) (#12235)

--- kolibri/core/assets/src/composables/useUser.js
+++ kolibri/core/assets/src/composables/useUser.js
@@ -15,7 +15,7 @@ export default function useUser() {
   const isFacilityCoach = computed(() => store.getters.isFacilityCoach);
   const isLearner = computed(() => store.getters.isLearner);
   const isFacilityAdmin = computed(() => store.getters.isFacilityAdmin);
-  const userIsMultipleFacilityAdmin = computed(() => store.getters.userIsMultipleFacilityAdmin);
+  const userIsMultiFacilityAdmin = computed(() => store.getters.userIsMultiFacilityAdmin);
   const getUserPermissions = computed(() => store.getters.getUserPermissions);
   const userFacilityId = computed(() => store.getters.userFacilityId);
   const getUserKind = computed(() => store.getters.getUserKind);
@@ -46,7 +46,7 @@ export default function useUser() {
     isFacilityCoach,
     isLearner,
     isFacilityAdmin,
-    userIsMultipleFacilityAdmin,
+    userIsMultiFacilityAdmin,
     getUserPermissions,
     userFacilityId,
     getUserKind,
