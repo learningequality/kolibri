5e10f5309cd6610db09b3d2127810339077189af
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-06-06 02:35:07 +0530

    create new errorreports app

---
+++ kolibri/core/errorreports/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class KolibriErrorConfig(AppConfig):
+    name = "kolibri.core.errorreports"
+    label = "errorreports"
+    verbose_name = "Kolibri ErrorReports"
+
+    def ready(self):
+        ...
