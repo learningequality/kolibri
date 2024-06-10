5f435e73f9ec3653b5928361b293599b063a558e
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-06-10 21:46:27 +0530

    simplify API: remove conditioning before calling insert_or_update_error()

--- kolibri/core/errorreports/api.py
+++ kolibri/core/errorreports/api.py
@@ -1,6 +1,5 @@
 import logging
 
-from django.conf import settings
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 @api_view(["POST"])
-def frontendreport(request):
+def report(request):
     """
     test with:
-    curl -X POST http://localhost:8000/api/errorreports/frontendreport/      -H "Content-Type: application/json"      -d '{
+    curl -X POST http://localhost:8000/api/errorreports/report/      -H "Content-Type: application/json"      -d '{
            "error_message": "An example error occurred",
            "traceback": "Traceback (most recent call last): ..."
          }'
@@ -23,28 +22,14 @@ def frontendreport(request):
     serializer = ErrorReportSerializer(data=request.data)
     if serializer.is_valid():
         data = serializer.validated_data
-        if not settings.DEVELOPER_MODE:
-            logger.error("Saving error report in the database.")
-            error = ErrorReports.insert_or_update_error(
-                error_from=ErrorReports.FRONTEND,
-                error_message=data["error_message"],
-                traceback=data["traceback"],
-            )
-            logger.error(
-                "Error report saved successfully. Error ID: {}".format(error.id)
-            )
-            return Response(
-                {"message": "Error report saved successfully.", "error_id": error.id},
-                status=status.HTTP_201_CREATED,
-            )
-        logger.error(
-            "Developer mode is enabled. Error report is not saved in the database."
+        error = ErrorReports.insert_or_update_error(
+            error_from=ErrorReports.FRONTEND,
+            error_message=data["error_message"],
+            traceback=data["traceback"],
         )
+
         return Response(
-            {
-                "message": "Error report not saved in the database. As we are in developer mode"
-            },
-            status=status.HTTP_200_OK,
+            {"error_id": error.id if error else None}, status=status.HTTP_200_OK
         )
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
