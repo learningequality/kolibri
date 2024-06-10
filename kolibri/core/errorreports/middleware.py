4c62c272960a6e9dac7cfb94a3f93ee1f2997029
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-06-10 21:02:54 +0530

    Simplify calling insert_or_update_error and tests

--- kolibri/core/errorreports/middleware.py
+++ kolibri/core/errorreports/middleware.py
@@ -1,7 +1,6 @@
 import logging
 import traceback
 
-from django.conf import settings
 from django.db import IntegrityError
 
 from .models import ErrorReports
@@ -24,20 +23,15 @@ class ErrorReportingMiddleware:
     def process_exception(self, request, exception):
         error_message = str(exception)
         traceback_info = traceback.format_exc()
-        self.logger.error("Unexpected Error %s", error_message)
-        # do not write to the database if dev's mode, development is a market for errors
-        if not settings.DEVELOPER_MODE:
-            try:
-                self.logger.error("Saving error report to the database.")
-                ErrorReports.insert_or_update_error(
-                    "Backend", error_message, traceback_info
-                )
-                self.logger.info("Error report saved to the database.")
-            except IntegrityError:
-                self.logger.error(
-                    "Error occurred while saving error report to the database."
-                )
-        else:
+        self.logger.error("Unexpected Error: %s", error_message)
+
+        try:
+            self.logger.error("Saving error report to the database.")
+            ErrorReports.insert_or_update_error(
+                "Backend", error_message, traceback_info
+            )
+            self.logger.info("Error report saved to the database.")
+        except IntegrityError:
             self.logger.error(
-                "Developer mode is enabled. Error report will not be saved to the database."
+                "Error occurred while saving error report to the database."
             )
