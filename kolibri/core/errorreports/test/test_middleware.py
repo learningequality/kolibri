bbf15e9de2508a7637c3f81e0cd8fb6b84551f90
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-06-10 21:02:54 +0530

    Simplify calling insert_or_update_error and tests

--- kolibri/core/errorreports/test/test_middleware.py
+++ kolibri/core/errorreports/test/test_middleware.py
@@ -1,4 +1,5 @@
-from django.test import override_settings
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from ..middleware import ErrorReportingMiddleware
@@ -6,24 +7,10 @@ from ..models import ErrorReports
 
 
 class ErrorReportingMiddlewareTestCase(TestCase):
-    databases = "__all__"
-
-    @override_settings(DEVELOPER_MODE=False)
-    def test_process_exception(self):
+    @patch.object(ErrorReports, "insert_or_update_error")
+    def test_process_exception(self, mock_insert_or_update_error):
         middleware = ErrorReportingMiddleware(lambda r: None)
         request = self.client.request()
         exception = Exception("Test Exception")
         middleware.process_exception(request, exception=exception)
-        self.assertTrue(
-            ErrorReports.objects.filter(error_message="Test Exception").exists()
-        )
-
-    @override_settings(DEVELOPER_MODE=True)
-    def test_process_exception_developer_mode_enabled(self):
-        middleware = ErrorReportingMiddleware(lambda r: None)
-        request = self.client.request()
-        exception = Exception("Test Exception")
-        middleware.process_exception(request, exception=exception)
-        self.assertFalse(
-            ErrorReports.objects.filter(error_message="Test Exception").exists()
-        )
+        mock_insert_or_update_error.assert_called_once()
