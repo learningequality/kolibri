d103ff8e6866bc072b5173d298e504fa08d39ca8
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-06-12 20:37:02 +0530

    expect (AttributeError, Exception) while calling insert_or_update

--- kolibri/core/errorreports/test/test_api.py
+++ kolibri/core/errorreports/test/test_api.py
@@ -1,7 +1,10 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.test import APIClient
 
 
@@ -28,3 +31,33 @@ class FrontendReportTestCase(TestCase):
         }
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+
+    @patch(
+        "kolibri.core.errorreports.models.ErrorReports.insert_or_update_error",
+        side_effect=AttributeError("Mocked AttributeError"),
+    )
+    def test_frontend_report_server_error_attribute_error(
+        self, mock_insert_or_update_error
+    ):
+        url = reverse("kolibri:core:report")
+        data = {
+            "error_message": "Something went wrong",
+            "traceback": "Traceback information",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertIn("error", response.data)
+
+    @patch(
+        "kolibri.core.errorreports.models.ErrorReports.insert_or_update_error",
+        side_effect=Exception("Mocked exception"),
+    )
+    def test_frontend_report_server_error(self, mock_insert_or_update_error):
+        url = reverse("kolibri:core:report")
+        data = {
+            "error_message": "Something went wrong",
+            "traceback": "Traceback information",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertIn("error", response.data)
