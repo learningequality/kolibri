0da59b476adf6398b706dc72f587f8cc825a9c0b
Alex Velez <velezllaquealex@gmail.com>
2024-06-17 19:06:22 -0500

    Remove user

--- kolibri/plugins/device/assets/src/users/composables/useUsers.js
+++ kolibri/plugins/device/assets/src/users/composables/useUsers.js
@@ -1,92 +1,21 @@
 import UserType from 'kolibri.utils.UserType';
 import { UserKinds } from 'kolibri.coreVue.vuex.constants';
-import { getCurrentInstance, ref } from 'kolibri.lib.vueCompositionApi';
-
-const mockUsers = [
-  {
-    id: '12',
-    full_name: 'Alex Velez',
-    username: 'alexvelezll',
-    is_superuser: true,
-    roles: [
-      {
-        collection: 'a5aa9e5e7ebdcd24564ac7e4915bc781',
-        kind: 'admin',
-        id: 'edc5abad7ab9c65732f6d77ff0105534',
-      },
-      {
-        collection: 'b4c0288bf4ca86cc88b46cdfc19ae347',
-        kind: 'coach',
-        id: 'b71439af334b3642c45c1b9047856243',
-      },
-    ],
-  },
-  {
-    id: '123',
-    full_name: 'John Doe',
-    username: 'johndoe',
-    is_superuser: false,
-    roles: [
-      {
-        collection: 'b4c0288bf4ca86cc88b46cdfc19ae347',
-        kind: 'coach',
-        id: 'b71439af334b3642c45c1b9047856243',
-      },
-    ],
-  },
-  {
-    id: '1234',
-    full_name: 'Alan Brito',
-    username: 'alanbrito',
-    is_superuser: false,
-    roles: [
-      {
-        collection: 'b4c0288bf4ca86cc88b46cdfc19ae347',
-        kind: 'coach',
-        id: 'b71439af334b3642c45c1b9047856243',
-      },
-    ],
-  },
-  {
-    id: '12345',
-    full_name: 'Armando Paredes',
-    username: 'armandoparedes',
-    is_superuser: false,
-    roles: [
-      {
-        collection: 'b4c0288bf4ca86cc88b46cdfc19ae347',
-        kind: 'coach',
-        id: 'b71439af334b3642c45c1b9047856243',
-      },
-    ],
-  },
-  {
-    id: '123456',
-    full_name: 'Lasom Brita',
-    username: 'lasombrita',
-    is_superuser: false,
-    roles: [
-      {
-        collection: 'b4c0288bf4ca86cc88b46cdfc19ae347',
-        kind: 'coach',
-        id: 'b71439af334b3642c45c1b9047856243',
-      },
-    ],
-  },
-];
+import { getCurrentInstance, ref, inject } from 'kolibri.lib.vueCompositionApi';
+import { FacilityUserResource } from 'kolibri.resources';
 
 export default function useUsers(store) {
   store = store || getCurrentInstance().proxy.$store;
-
+  const service = inject('importUserService');
+  service;
   const users = ref([]);
   const loading = ref(true);
   const showCannotRemoveUser = ref(false);
 
   async function fetchUsers() {
     loading.value = true;
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    const response = await FacilityUserResource.fetchCollection();
+    users.value = response;
     loading.value = false;
-    users.value = mockUsers;
     users.value.forEach(user => {
       user.kind = UserType(user);
     });
@@ -95,8 +24,8 @@ export default function useUsers(store) {
   }
 
   async function removeUser(userId) {
-    const index = mockUsers.findIndex(user => user.id === userId);
-    const user = users.value[index];
+    const user = users.value.find(user => user.id === userId);
+    if (!user) return;
     if (
       user.kind === UserKinds.SUPERUSER &&
       users.value.filter(user => user.kind === UserKinds.SUPERUSER).length === 1
@@ -105,7 +34,7 @@ export default function useUsers(store) {
       throw new Error('Cannot remove the last super admin');
     }
 
-    users.value.splice(index, 1);
+    await FacilityUserResource.deleteModel({ id: userId });
   }
 
   return {
