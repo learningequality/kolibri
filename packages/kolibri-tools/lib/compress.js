5e441bd715d48a19c61a086cbfe1ffd37e97258d
Richard Tibbles <richard@learningequality.org>
2024-06-25 15:39:59 -0700

    Stop compressing ico, woff, and woff2 files that do not compress significantly. Add more verbose logging to compress command.

--- packages/kolibri-tools/lib/compress.js
+++ packages/kolibri-tools/lib/compress.js
@@ -1,6 +1,6 @@
 const { constants, createGzip } = require('node:zlib');
 const { pipeline } = require('node:stream');
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream, statSync } = require('node:fs');
 const logger = require('./logging');
 
 const logging = logger.getLogger('Kolibri Compressor');
@@ -17,7 +17,18 @@ function compressFile(input) {
         logging.error('An error occurred compressing file: ', input);
         logging.error(err);
       } else {
-        logging.info('Successfully compressed: ', input);
+        const sourceStats = statSync(input);
+        const destStats = statSync(input + '.gz');
+        logging.info(
+          'Successfully compressed:',
+          input,
+          'by',
+          (1 - destStats.size / sourceStats.size) * 100,
+          '%',
+        );
+        if (destStats.size / sourceStats.size > 0.75) {
+          logging.warn('Compressed size is more than 75% of original size');
+        }
       }
       resolve();
     });
