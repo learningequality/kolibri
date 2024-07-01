61ba3b5a9b59dd9db4031be156c902e91cce3e3b
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-07-01 14:01:40 +0530

    add context field in errorreports/report/

--- kolibri/core/errorreports/serializers.py
+++ kolibri/core/errorreports/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
+from .schemas import context_frontend_schema
+from kolibri.core.utils.validators import JSON_Schema_Validator
+
 
 class ErrorReportSerializer(serializers.Serializer):
     error_message = serializers.CharField(max_length=255)
     traceback = serializers.CharField()
+    context = serializers.JSONField(
+        validators=[JSON_Schema_Validator(context_frontend_schema)]
+    )
