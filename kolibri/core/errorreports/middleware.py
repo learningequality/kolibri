0726d38dafd56696fddd7677720252b665e49d25
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-07-01 14:02:40 +0530

    update middleware to capture more fields

--- kolibri/core/errorreports/middleware.py
+++ kolibri/core/errorreports/middleware.py
@@ -1,10 +1,33 @@
 import logging
 import traceback
+from sys import version
 
+import pkg_resources
 from django.db import IntegrityError
 
 from .constants import BACKEND
 from .models import ErrorReports
+
+
+def get_request_info(request):
+    return {
+        "url": request.build_absolute_uri(),
+        "method": request.method,
+        "headers": dict(request.headers),
+        "body": request.body.decode("utf-8"),
+    }
+
+
+def get_server_info(request):
+    return {"host": request.get_host(), "port": request.get_port()}
+
+
+def get_packages():
+    return {dist.project_name: dist.version for dist in pkg_resources.working_set}
+
+
+def get_python_version():
+    return version.split()[0]
 
 
 class ErrorReportingMiddleware:
@@ -23,11 +46,19 @@ class ErrorReportingMiddleware:
     def process_exception(self, request, exception):
         error_message = str(exception)
         traceback_info = traceback.format_exc()
+        context = {
+            "request_info": get_request_info(request),
+            "server": get_server_info(request),
+            "packages": get_packages(),
+            "python_version": get_python_version(),
+        }
         self.logger.error("Unexpected Error: %s", error_message)
 
         try:
             self.logger.error("Saving error report to the database.")
-            ErrorReports.insert_or_update_error(BACKEND, error_message, traceback_info)
+            ErrorReports.insert_or_update_error(
+                BACKEND, error_message, traceback_info, context_backend=context
+            )
             self.logger.info("Error report saved to the database.")
         except IntegrityError as e:
             self.logger.error(
