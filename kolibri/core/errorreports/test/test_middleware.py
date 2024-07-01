44ce4e1b4bc2ae93d22023d3803517cbd5b44732
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-07-01 14:02:40 +0530

    update middleware to capture more fields

--- kolibri/core/errorreports/test/test_middleware.py
+++ kolibri/core/errorreports/test/test_middleware.py
@@ -3,6 +3,7 @@ import traceback
 from unittest.mock import patch
 
 from django.db import IntegrityError
+from django.test import RequestFactory
 from django.test import TestCase
 
 from ..constants import BACKEND
@@ -11,11 +12,27 @@ from ..models import ErrorReports
 
 
 class ErrorReportingMiddlewareTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @patch(
+        "kolibri.core.errorreports.middleware.get_python_version", return_value="3.9.9"
+    )
+    @patch(
+        "kolibri.core.errorreports.middleware.get_packages",
+        return_value={"Django": "3.2.25"},
+    )
     @patch.object(ErrorReports, "insert_or_update_error")
     @patch.object(logging.Logger, "error")
-    def test_process_exception(self, mock_logger_error, mock_insert_or_update_error):
+    def test_process_exception(
+        self,
+        mock_logger_error,
+        mock_insert_or_update_error,
+        mock_get_packages,
+        mock_get_python_version,
+    ):
         middleware = ErrorReportingMiddleware(lambda r: None)
-        request = self.client.request()
+        request = self.factory.get("/")
         exception = Exception("Test Exception")
         try:
             raise exception
@@ -27,8 +44,22 @@ class ErrorReportingMiddlewareTestCase(TestCase):
                 type(exception), exception, exception.__traceback__
             )
         )
+
         mock_insert_or_update_error.assert_called_once_with(
-            BACKEND, str(exception), expected_traceback_info
+            BACKEND,
+            str(exception),
+            expected_traceback_info,
+            context_backend={
+                "request_info": {
+                    "url": "http://testserver/",
+                    "method": "GET",
+                    "headers": dict(request.headers),
+                    "body": "",
+                },
+                "server": {"host": "testserver", "port": "80"},
+                "packages": {"Django": 1.1},
+                "python_version": "1.1.1",
+            },
         )
 
     @patch.object(ErrorReports, "insert_or_update_error")
@@ -37,7 +68,7 @@ class ErrorReportingMiddlewareTestCase(TestCase):
         self, mock_logger_error, mock_insert_or_update_error
     ):
         middleware = ErrorReportingMiddleware(lambda r: None)
-        request = self.client.request()
+        request = self.factory.get("/")
         exception = Exception("Test Exception")
         mock_insert_or_update_error.side_effect = IntegrityError("Some Integrity Error")
         middleware.process_exception(request, exception)
