2798cdfdb70dd0ba8de2af3913466022091e3dcd
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-07-01 14:04:50 +0530

    update frontend erroreports to capture more fields

--- kolibri/core/assets/src/api-resources/__tests__/errorReport.test.js
+++ kolibri/core/assets/src/api-resources/__tests__/errorReport.test.js
@@ -23,15 +23,20 @@ describe('Error Report', () => {
   it('should call api/core/report with VueErrorReport data', () => {
     const vueError = new Error('Vue error');
     vueError.stack = 'My stack trace';
-    const errorReport = new VueErrorReport(vueError);
+    const vm = { $options: { name: 'TestComponent' } };
+    const errorReport = new VueErrorReport(vueError, vm);
 
     const expectedData = {
       error_message: 'Vue error',
       traceback: 'My stack trace',
+      context: {
+        component: 'TestComponent',
+        browser: errorReport.getBrowserInfo(),
+        device: errorReport.getDeviceInfo(),
+      },
     };
 
     Resource.client = jest.fn();
-
     Resource.report(errorReport);
 
     expect(Resource.client).toHaveBeenCalledWith({
@@ -52,10 +57,13 @@ describe('Error Report', () => {
     const expectedData = {
       error_message: 'Javascript error',
       traceback: 'My stack trace',
+      context: {
+        browser: errorReport.getBrowserInfo(),
+        device: errorReport.getDeviceInfo(),
+      },
     };
 
     Resource.client = jest.fn();
-
     Resource.report(errorReport);
 
     expect(Resource.client).toHaveBeenCalledWith({
@@ -76,10 +84,13 @@ describe('Error Report', () => {
     const expectedData = {
       error_message: 'Unhandled rejection',
       traceback: 'My stack trace',
+      context: {
+        browser: errorReport.getBrowserInfo(),
+        device: errorReport.getDeviceInfo(),
+      },
     };
 
     Resource.client = jest.fn();
-
     Resource.report(errorReport);
 
     expect(Resource.client).toHaveBeenCalledWith({
--- kolibri/core/assets/src/utils/errorReportUtils.js
+++ kolibri/core/assets/src/utils/errorReportUtils.js
@@ -6,13 +6,35 @@ class ErrorReport {
   getErrorReport() {
     throw new Error('getErrorReport() method must be implemented.');
   }
+  getDeviceInfo() {
+    return {
+      type: /Mobi|Android/i.test(navigator.userAgent) ? 'Mobile' : 'Desktop',
+      platform: navigator.platform,
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    };
+  }
+  getBrowserInfo() {
+    return navigator.userAgent;
+  }
 }
 
 export class VueErrorReport extends ErrorReport {
+  constructor(e, vm) {
+    super(e);
+    this.vm = vm;
+  }
   getErrorReport() {
     return {
       error_message: this.e.message,
       traceback: this.e.stack,
+      context: {
+        component: this.vm.$options.name || this.vm.$options._componentTag || 'Unknown Component',
+        browser: this.getBrowserInfo(),
+        device: this.getDeviceInfo(),
+      },
     };
   }
 }
@@ -22,6 +44,10 @@ export class JavascriptErrorReport extends ErrorReport {
     return {
       error_message: this.e.error.message,
       traceback: this.e.error.stack,
+      context: {
+        browser: this.getBrowserInfo(),
+        device: this.getDeviceInfo(),
+      },
     };
   }
 }
@@ -29,8 +55,12 @@ export class JavascriptErrorReport extends ErrorReport {
 export class UnhandledRejectionErrorReport extends ErrorReport {
   getErrorReport() {
     return {
-      error_message: this.e.reason.message,
-      traceback: this.e.reason.stack,
+      error_message: this.e.reason ? this.e.reason.message : 'Unhandled Rejection',
+      traceback: this.e.reason ? this.e.reason.stack : 'No stack trace available',
+      context: {
+        browser: this.getBrowserInfo(),
+        device: this.getDeviceInfo(),
+      },
     };
   }
 }
