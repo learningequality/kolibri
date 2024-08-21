85ee42d55feb6669845abf8d58ffec2b873f638b
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-08-21 19:46:08 -0400

    cleanup

--- kolibri/core/assets/src/composables/useConnection.js
+++ kolibri/core/assets/src/composables/useConnection.js
@@ -1,8 +1,4 @@
 import { ref } from 'kolibri.lib.vueCompositionApi';
-import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
-
-Vue.use(VueCompositionApi);
 
 const connected = ref(true);
 const reconnectTime = ref(null);
