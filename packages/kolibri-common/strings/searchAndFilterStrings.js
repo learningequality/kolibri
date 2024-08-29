381c25e54c0722537bb682eb274f749357e62dfc
Alex Velez <velezllaquealex@gmail.com>
2024-08-29 15:56:31 -0500

    Add searchAndFilterStrings translator into kolibri-common

---
+++ packages/kolibri-common/strings/searchAndFilterStrings.js
@@ -0,0 +1,10 @@
+import { createTranslator } from 'kolibri.utils.i18n';
+
+export const searchAndFilterStrings = createTranslator('SearchAndFilterStrings', {
+  // Labels
+  filterAndSearchLabel: {
+    message: 'Filter and search',
+    context:
+      'Label for a section of the page that contains options for searching and filtering content',
+  },
+});
