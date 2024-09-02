301abaa0341ffe70636b15924c938574d9c4e780
Alex Velez <velezllaquealex@gmail.com>
2024-09-02 14:10:40 -0500

    Add All learners column to lesson and quiz list report

--- kolibri/plugins/coach/assets/src/views/reports/ReportsQuizListPage.vue
+++ kolibri/plugins/coach/assets/src/views/reports/ReportsQuizListPage.vue
@@ -308,6 +308,7 @@
           ...csvFields.avgScore(),
           ...csvFields.recipients(this.className),
           ...csvFields.tally(),
+          ...csvFields.allLearners('totalLearners'),
         ];
 
         const fileName = this.$tr('printLabel', { className: this.className });
