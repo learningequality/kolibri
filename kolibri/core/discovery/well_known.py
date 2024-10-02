9704b4a43e33c86cc99c18e19502197a955aa28d
Liana Harris <46411498+LianaHarris360@users.noreply.github.com>
2024-10-02 14:02:39 -0500

    Use hardcoded ID to create reserved network locations for KDP

--- kolibri/core/discovery/well_known.py
+++ kolibri/core/discovery/well_known.py
@@ -1,5 +1,6 @@
 from le_utils.uuidv5 import generate_ecosystem_namespaced_uuid
 
+from kolibri.core.serializers import HexOnlyUUIDField
 from kolibri.utils import conf
 
 # AKA Kolibri Studio
@@ -7,3 +8,7 @@ CENTRAL_CONTENT_BASE_URL = conf.OPTIONS["Urls"]["CENTRAL_CONTENT_BASE_URL"]
 CENTRAL_CONTENT_BASE_INSTANCE_ID = generate_ecosystem_namespaced_uuid(
     CENTRAL_CONTENT_BASE_URL
 ).hex
+
+# AKA Kolibri Data Portal
+DATA_PORTAL_SYNCING_BASE_URL = conf.OPTIONS["Urls"]["DATA_PORTAL_SYNCING_BASE_URL"]
+DATA_PORTAL_INSTANCE_ID = HexOnlyUUIDField()
