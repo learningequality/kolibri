ae88eec13d2e6e5c252a9c6dd742dc00a0f6caeb
Jacob Pierce <w.jacobpierce@gmail.com>
2024-10-02 13:51:49 -0700

    add rename group and enroll learners to common coach strings

--- kolibri/plugins/coach/assets/src/views/common/commonCoachStrings.js
+++ kolibri/plugins/coach/assets/src/views/common/commonCoachStrings.js
@@ -40,6 +40,15 @@ const coachStrings = createTranslator('CommonCoachStrings', {
     message: 'Print report',
     context: "Option to print a hard copy of a report in the 'Reports' tab.",
   },
+  renameGroupAction: {
+    message: 'Rename group',
+    context: 'Label for dropdown menu button that allows user to rename a group',
+  },
+  enrollLearnersAction: {
+    message: 'Enroll learners',
+    context:
+      'Label for dropdown menu button that navigates the coach to edit the enrollment for a group',
+  },
   renameAction: {
     message: 'Rename',
     context: 'Generic option to change the name of some element such as a class name. ',
