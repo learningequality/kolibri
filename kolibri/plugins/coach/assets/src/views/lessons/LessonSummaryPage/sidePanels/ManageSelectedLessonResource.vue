60f63531a854a86816d90fd4d15cd137824b8b87
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2024-11-22 21:03:09 +0300

    clean ups importations

--- kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/sidePanels/ManageSelectedLessonResource.vue
+++ kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/sidePanels/ManageSelectedLessonResource.vue
@@ -7,83 +7,19 @@
     @shouldFocusFirstEl="() => null"
   >
     <template #header>
-      <h4 class="side-panel-title"> {{ $tr('numberOfSelectedResource',{ count : resourcesTable.length })}}</h4>
-      <router-view @closePanel="() => $router.go(-1)" />
+      <KIconButton
+        v-if="true"
+        icon="back"
+        @click="$router.go(-1)"
+      />
+      <span :style="{ fontWeight: '600' }" class="side-panel-title"> {{ $tr('numberOfSelectedResource',{ count : fetchResources.length })}}</span>
     </template>
 
-    <div v-if="resourcesTable.length === 0">
-      <KCircularLoader />
-    </div>
-
-    <div v-else>
-      <p>{{ coreString('lessonsLabel') }} : {{ currentLesson.title }} </p>
-      <p>{{ $tr('sizeLabel') }} : {{ bytesForHumans(currentLesson.size) }}</p>
-
-      <div
-        v-for="(lesson , index) in resourcesTable"
-        :key="index"
-      >
-        <KGrid :style="lessonOrderListButtonBorder">
-          <KGridItem :layout12="{ span: 6 }">
-            <div style="display:flex;">
-              <div>
-                <DragSortWidget
-                  moveUpText="up"
-                  moveDownText="down"
-                  :noDrag="true"
-                  :isFirst="index === 0"
-                  :isLast="index === resourcesTable.length - 1"
-                  @moveUp="() => {}"
-                  @moveDown="() => {}"
-                />
-              </div>
-              <div style="padding:0px 10px">
-                <span
-                  style="flex: 1"
-                >
-                  <LearningActivityIcon
-                    :kind="lesson.learning_activities[0]"
-                    class="icon-style"
-                  />
-                </span>
-              </div>
-              <div>
-                <span
-                  class="arrange-item-block"
-                >
-                  <span>
-                    <KRouterLink
-                      v-if="lesson.link"
-                      :text="lesson.title"
-                      :to="lesson.link"
-                      style="font-size:14px"
-                    />
-                  </span>
-                  <p style="font-size:12px"> 4MB </p>
-                </span>
-              </div>
-            </div>
-          </KGridItem>
-
-          <KGridItem :layout12="{ span: 6 }">
-            <span
-              class="add-minus-button"
-            >
-              <KIconButton
-                icon="emptyTopic"
-                @click="()=>{ }"
-              />
-
-              <KIconButton
-                icon="minus"
-                @click="removeResource(lesson.id)"
-              />
-            </span>
-          </KGridItem>
-
-        </KGrid>
-      </div>
-    </div>
+    <SelectedResource
+      :resourceList="fetchResources"
+      :currentLesson="currentLesson"
+      :loading="fetchResources.length === 0"
+    />
 
 
     <template #bottomNavigation>
@@ -102,37 +38,26 @@
 
 <script>
 
-  import DragSortWidget from 'kolibri-common/components/sortable/DragSortWidget';
-  import commonCoreStrings from 'kolibri/uiText/commonCoreStrings';
-  import LearningActivityIcon from 'kolibri-common/components/ResourceDisplayAndSearch/LearningActivityIcon.vue';
-  import bytesForHumans from 'kolibri/uiText/bytesForHumans';
   import SidePanelModal from 'kolibri-common/components/SidePanelModal';
   import { mapState } from 'vuex';
+  import SelectedResource from './SelectedResource';
 
   export default {
     name:'ManageSelectedLessonResources',
     components: {
-      DragSortWidget,
-      LearningActivityIcon,
-      SidePanelModal
-    },
-    mixins:[commonCoreStrings],
-    data(){
-      return {
-        resources: [],
-        isLoading:false,
-      }
+      SidePanelModal,
+      SelectedResource
     },
     computed: {
       ...mapState('lessonSummary', ['currentLesson', 'workingResources','resourceCache']),
       lessonOrderListButtonBorder(){
         return {
-          borderBottom: `1px solid ${this.$themePalette.grey.v_200}`, 
+          borderBottom: `1px solid ${this.$themePalette.grey.v_200}`,
           height:`4em`,
           marginTop:`0.5em`
         };
       },
-      resourcesTable(){
+      fetchResources(){
         return this.workingResources.map(resource => {
           const content = this.resourceCache[resource.contentnode_id];
           if (!content) {
@@ -149,23 +74,21 @@
           }
 
           return tableRow;
-        })
+        });
       },
     },
+    mounted(){
+      setTimeout(() => {
+        
+        console.log(this.currentLesson);
+      }, 1000);
+    },
     methods:{
-      removeResource(id){
-        this.resources = this.resources.filter((lesson) => lesson.id !== id);
-      },
       closeSidePanel() {
         this.$router.go(-2);
       },
-      bytesForHumans,
     },
     $trs:{
-      sizeLabel:{
-        message: 'Size',
-        context: 'Size of the lesson'
-      },
       numberOfSelectedResource: {
         message: '{count, number, integer} {count, plural, one {resource selected} other {resources selected}}',
         context:'Indicates the number of resources selected'
@@ -178,15 +101,6 @@
 
 <style scoped>
 
-  .add-minus-button{
-    float:right;
-  }
-  .arrange-item-block{
-    display:block;
-  }
-  .icon-style{
-    font-size:24px;
-  }
   .bottom-buttons-style {
     position: absolute;
     right: 0;
---
+++ kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/sidePanels/SelectedResource.vue
@@ -0,0 +1,165 @@
+<template>
+
+  <div v-if="loading">
+    <KCircularLoader />
+  </div>
+  <div 
+    v-else
+    class="resource-list-style"
+  >
+    <p>{{ coreString('lessonsLabel') }} : {{ currentLesson.title }} </p>
+    <p>{{ $tr('sizeLabel') }} : {{ bytesForHumans(currentLesson.size) }}</p>
+
+    <div
+      v-for="(lesson , index) in resources"
+      :key="index"
+    >
+      <KGrid :style="lessonOrderListButtonBorder">
+        <KGridItem :layout12="{ span: 6 }">
+          <div style="display:flex;">
+            <div>
+              <DragSortWidget
+                moveUpText="up"
+                moveDownText="down"
+                :noDrag="true"
+                :isFirst="index === 0"
+                :isLast="index === resources.length - 1"
+                @moveUp="() => {}"
+                @moveDown="() => {}"
+              />
+            </div>
+            <div style="padding:0px 10px">
+              <span
+                style="flex: 1"
+              >
+                <LearningActivityIcon
+                  :kind="lesson.learning_activities[0]"
+                  class="icon-style"
+                />
+              </span>
+            </div>
+            <div>
+              <span
+                class="arrange-item-block"
+              >
+                <span>
+                  <KRouterLink
+                    v-if="lesson.link"
+                    :text="lesson.title"
+                    :to="lesson.link"
+                    style="font-size:14px"
+                  />
+                </span>
+                <p style="font-size:12px"> 4MB </p>
+              </span>
+            </div>
+          </div>
+        </KGridItem>
+
+        <KGridItem :layout12="{ span: 6 }">
+          <span
+            class="add-minus-button"
+          >
+            <KIconButton
+              icon="emptyTopic"
+              @click="()=>{ }"
+            />
+
+            <KIconButton
+              icon="minus"
+              @click="removeResource(lesson.id)"
+            />
+          </span>
+        </KGridItem>
+
+      </KGrid>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+
+  import DragSortWidget from 'kolibri-common/components/sortable/DragSortWidget';
+  import commonCoreStrings from 'kolibri/uiText/commonCoreStrings';
+  import LearningActivityIcon from 'kolibri-common/components/ResourceDisplayAndSearch/LearningActivityIcon.vue';
+  import bytesForHumans from 'kolibri/uiText/bytesForHumans';
+  
+  export default {
+    name:"SelectedResource",
+    components:{
+      DragSortWidget,
+      LearningActivityIcon
+    },
+    mixins:[commonCoreStrings],
+    props:{
+      resourceList:{
+        type: Array,
+        required:true
+      },
+      currentLesson:{
+        type:Object,
+        required:true
+      },
+      loading:{
+        type:Boolean,
+        required:true
+      }
+    },
+    data() {
+      return {
+        resources: []
+      }
+    },
+    computed:{
+      lessonOrderListButtonBorder(){
+        return {
+          borderBottom: `1px solid ${this.$themePalette.grey.v_200}`, 
+          height:`4em`,
+          marginTop:`0.5em`
+        };
+      },
+    },
+    mounted(){
+      setTimeout(() => {
+        this.resources = this.resourceList;
+      }, 1000);
+    },
+
+    methods:{
+      removeResource(id){
+        this.resources = this.resources.filter(lesson => lesson.id !== id);
+      },
+      bytesForHumans
+    },
+    $trs:{
+      sizeLabel:{
+        message: 'Size',
+        context: 'Size of the lesson'
+      },
+    }
+  }
+
+</script>
+
+
+<style scoped >
+
+ .add-minus-button{
+    float:right;
+  }
+
+  .arrange-item-block{
+    display:block;
+  }
+
+  .icon-style{
+    font-size:24px;
+  }
+
+  .resource-list-style{
+    margin-bottom: 3.5em;
+  }
+  
+</style>
