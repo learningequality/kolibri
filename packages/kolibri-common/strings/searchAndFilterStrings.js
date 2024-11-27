5264a6c872e7f909b0d2a7fc234bfbf7446a2f40
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2024-11-27 22:12:57 +0300

    rebase

--- packages/kolibri-common/strings/searchAndFilterStrings.js
+++ packages/kolibri-common/strings/searchAndFilterStrings.js
@@ -35,4 +35,8 @@ export const searchAndFilterStrings = createTranslator('SearchAndFilterStrings',
     message: 'Move this resource one position down in this lesson',
     context: 'Refers to changing the order of resources in a lesson.',
   },
+  saveLessonResources: {
+    message: 'save & finish',
+    context: 'Button to save resources in a lesson',
+  },
 });
