b05abc41e8dfc18a8916ba3a9ae8a6045e242a7b
Alex Velez <velezllaquealex@gmail.com>
2024-11-28 14:42:15 -0500

    Implement tree fetch and display

--- kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/sidePanels/LessonResourceSelection/subPages/SelectFromChannels.vue
+++ kolibri/plugins/coach/assets/src/views/lessons/LessonSummaryPage/sidePanels/LessonResourceSelection/subPages/SelectFromChannels.vue
@@ -1,14 +1,27 @@
 <template>
 
-  <div>Select from Channels</div>
+  <div>
+    <UpdatedResourceSelection :source="ResourceContentSource.TOPIC_TREE" />
+  </div>
 
 </template>
 
 
 <script>
 
+  import { ResourceContentSource } from '../constants';
+  import UpdatedResourceSelection from '../../../UpdatedResourceSelection.vue';
+
   export default {
     name: 'SelectFromChannels',
+    components: {
+      UpdatedResourceSelection,
+    },
+    data() {
+      return {
+        ResourceContentSource,
+      };
+    },
   };
 
 </script>
