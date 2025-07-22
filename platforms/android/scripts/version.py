974fe9ccf14faeff5a57286a83c6e7be83b59924
Richard Tibbles <richard@learningequality.org>
2025-07-22 14:27:47 -0700

    Update to 0.1.5

--- platforms/android/scripts/version.py
+++ platforms/android/scripts/version.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from play_store_api import get_latest_version_code
 
 
-android_installer_version = "0.1.4"
+android_installer_version = "0.1.5"
 
 
 BUILD_TYPE_DEBUG = "debug"
