e2d34214e65c360207ada0c5e16c9a7b1b393341
Richard Tibbles <richard@learningequality.org>
2025-09-26 11:05:15 -0700

    Clear up obselete buildkite pipeline.

--- .buildkite/build_whl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-make docker-whl
-buildkite-agent artifact upload 'dist/*.whl'
-buildkite-agent artifact upload 'dist/*.tar.gz'
-buildkite-agent artifact upload 'dist/*.pex'
-cd kolibri && buildkite-agent artifact upload 'VERSION' && cd -
