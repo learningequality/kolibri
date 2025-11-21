66a435af31f85a4ed808dc88eae677303626ecce
Aadarsh Mahesh K <aadarshmahk@gmail.com>
2025-11-22 03:00:20 +0530

    Remove Python 2.7 : Replace mkdirp() with os.makedirs()  (#13927)
    * remove: mkdir function from filesystem
    
    * remove: mkdirp utility and update call sites to os.makedirs
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- kolibri/utils/tests/test_file_transfer.py
+++ kolibri/utils/tests/test_file_transfer.py
@@ -22,7 +22,6 @@ from kolibri.utils.file_transfer import retry_import
 from kolibri.utils.file_transfer import RETRY_STATUS_CODE
 from kolibri.utils.file_transfer import SSLERROR
 from kolibri.utils.file_transfer import TransferFailed
-from kolibri.utils.filesystem import mkdirp
 
 
 class BaseTestTransfer(unittest.TestCase):
@@ -38,7 +37,7 @@ class BaseTestTransfer(unittest.TestCase):
             math.ceil(float(self.file_size) / float(ChunkedFile.chunk_size))
         )
 
-        mkdirp(self.dest + ".chunks", exist_ok=True)
+        os.makedirs(self.dest + ".chunks", exist_ok=True)
 
         hash = hashlib.md5()
 
