9efb32ba0dccec6857d44ddfa972e7376269dd99
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-09 00:37:54 +0530

    updated utils

--- kolibri/utils/sanity_checks.py
+++ kolibri/utils/sanity_checks.py
@@ -24,7 +24,7 @@ class SanityException(RuntimeError):
 class DatabaseNotMigrated(SanityException):
     def __init__(self, *args, **kwargs):
         self.db_exception = kwargs.get("db_exception")
-        super(DatabaseNotMigrated, self).__init__(
+        super().__init__(
             "An exception occurred for which it is assumed that the "
             "database is not fully migrated.\n\n"
             "Exception: {}".format(str(self.db_exception))
@@ -34,7 +34,7 @@ class DatabaseNotMigrated(SanityException):
 class DatabaseInaccessible(SanityException):
     def __init__(self, *args, **kwargs):
         self.db_exception = kwargs.get("db_exception")
-        super(DatabaseInaccessible, self).__init__(
+        super().__init__(
             "Not able to access the database while checking it.\n\n"
             "Exception: {}".format(str(self.db_exception))
         )
