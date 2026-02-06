2ab492f2e8c91a6ba882362cb889e0a9abeddf6d
Jacob Pierce <w.jacobpierce@gmail.com>
2026-02-05 21:52:23 -0800

    base fetch in CoursesSummaryPage

--- kolibri/plugins/coach/frontend/views/courses/CourseSummaryPage.vue
+++ kolibri/plugins/coach/frontend/views/courses/CourseSummaryPage.vue
@@ -2,7 +2,8 @@
 
   <CoachAppBarPage>
     <KPageContainer>
-      <!-- To be implemented -->
+      <KCircularLoader v-if="loading" />
+      <h1 v-else>Sup?</h1>
     </KPageContainer>
   </CoachAppBarPage>
 
@@ -11,12 +12,33 @@
 
 <script>
 
+  import { computed, ref } from 'vue';
+  import { useRoute, useRouter } from 'vue-router/composables';
+  import ContentNodeResource from 'kolibri-common/apiResources/ContentNodeResource';
   import CoachAppBarPage from '../CoachAppBarPage.vue';
 
   export default {
     name: 'CourseSummaryPage',
     components: {
       CoachAppBarPage,
+    },
+    setup() {
+      const route = useRoute();
+      const router = useRouter();
+      const course = ref(null);
+      const loading = ref(true);
+      ContentNodeResource.fetchTree({ id: route.params.courseId })
+        .then(results => {
+          course.value = results;
+          loading.value = false;
+        })
+        .catch(() => {
+          loading.value = false;
+        });
+      return {
+        loading,
+        course,
+      };
     },
   };
 
