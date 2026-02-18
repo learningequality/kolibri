f0654e8da3b245ba9a9b710067201ef304997612
Samson Akol <akolson2010@gmail.com>
2026-02-18 15:52:15 +0300

    Fixes lint errors
    # Conflicts:
    #	kolibri/plugins/coach/frontend/views/common/QuizLessonDetailsHeader.vue

--- kolibri/plugins/coach/frontend/views/common/QuizLessonDetailsHeader.vue
+++ kolibri/plugins/coach/frontend/views/common/QuizLessonDetailsHeader.vue
@@ -48,8 +48,7 @@
   import { mapState } from 'vuex';
   import UiAlert from 'kolibri-design-system/lib/keen/UiAlert';
   import MissingResourceAlert from 'kolibri-common/components/MissingResourceAlert';
-  import NoResourceAlert from 'kolibri-common/components/NoResourceAlert.vue';
-  import { enhancedQuizManagementStrings } from 'kolibri-common/strings/enhancedQuizManagementStrings';
+  import NoResourceAlert from "kolibri-common/components/NoResourceAlert.vue";
   import { searchAndFilterStrings } from 'kolibri-common/strings/searchAndFilterStrings';
   import useChannels from 'kolibri-common/composables/useChannels';
   import BackLink from './BackLink';
@@ -65,12 +64,10 @@
     setup() {
       const { fetchChannels } = useChannels();
       const { warningForQuizFromOldKolibri$ } = searchAndFilterStrings;
-      const { noResourcesAvailable$ } = enhancedQuizManagementStrings;
       return {
         fetchChannels,
 
         warningForQuizFromOldKolibri$,
-        noResourcesAvailable$,
       };
     },
     props: {
