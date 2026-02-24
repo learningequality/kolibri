a0c68fe2e2e108a42afc2b726ebee74d34471611
rohitt-pandey <rohit.raj.code@gmail.com>
2026-02-24 05:46:15 +0000

    test: add explanatory comment for manual focus workaround in JSDOM

--- kolibri/plugins/setup_wizard/frontend/views/__tests__/FacilityPermissionsForm.spec.js
+++ kolibri/plugins/setup_wizard/frontend/views/__tests__/FacilityPermissionsForm.spec.js
@@ -38,6 +38,9 @@ describe('FacilityPermissionsForm', () => {
     const facilityInput = screen.getByRole('textbox', { name: /facility name/i });
 
     await waitFor(() => {
+      // Hack for VTL/Vue 2: The component does  auto-focus on mount.
+      // However, VTL mounts things detached initially, so JSDOM drops the focus event.
+      // Firing .focus() manually here to bypass the timing issue so we can actually test it.
       facilityInput.focus();
       expect(facilityInput).toHaveFocus();
     });
