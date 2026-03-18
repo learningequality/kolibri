c1e7f368abbf925ed5a0cd0d5862b0613acdc618
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2026-03-18 11:04:19 +0300

    migrations

--- kolibri/core/auth/migrations/0033_facilitydataset_picture_password.py
+++ kolibri/core/auth/migrations/0033_facilitydataset_picture_password.py
@@ -2,6 +2,7 @@ from django.db import migrations
 from django.db import models
 
 import kolibri.core.fields
+import kolibri.core.utils.validators
 
 
 class Migration(migrations.Migration):
@@ -19,6 +20,25 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="facilitydataset",
             name="picture_password_settings",
-            field=kolibri.core.fields.JSONField(blank=True, default=None, null=True),
+            field=kolibri.core.fields.JSONField(
+                blank=True,
+                default=None,
+                null=True,
+                validators=[
+                    kolibri.core.utils.validators.JSON_Schema_Validator(
+                        {
+                            "type": "object",
+                            "properties": {
+                                "icon_style": {
+                                    "type": "string",
+                                    "enum": ["standard", "colorful"],
+                                },
+                                "show_icon_text": {"type": "boolean"},
+                            },
+                            "required": ["icon_style", "show_icon_text"],
+                        }
+                    )
+                ],
+            ),
         ),
     ]
