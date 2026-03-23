493f6cbcb1f1b498fb4c5b18efb932776e16747f
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2026-03-23 13:39:43 +0300

    rename synthetic content ID namespace for clarity

--- kolibri/core/logger/utils/pre_post_test.py
+++ kolibri/core/logger/utils/pre_post_test.py
@@ -5,9 +5,11 @@ report API (kolibri.plugins.coach.unit_report_api).
 """
 import uuid
 
-_SYNTHETIC_CONTENT_ID_NAMESPACE = uuid.UUID("7c9e4b1a-3d5f-4a8e-9c2b-6d0e1f2a3b4c")
+_PRE_POST_TEST_SYNTHETIC_CONTENT_ID_NAMESPACE = uuid.UUID(
+    "7c9e4b1a-3d5f-4a8e-9c2b-6d0e1f2a3b4c"
+)
 
 
 def get_synthetic_content_id(course_session_id, unit_id, test_type):
     key = "{}:{}:{}".format(course_session_id, unit_id, test_type)
-    return uuid.uuid5(_SYNTHETIC_CONTENT_ID_NAMESPACE, key).hex
+    return uuid.uuid5(_PRE_POST_TEST_SYNTHETIC_CONTENT_ID_NAMESPACE, key).hex
