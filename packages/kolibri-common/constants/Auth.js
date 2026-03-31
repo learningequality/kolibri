f5837b0037129c935d514a28575c4f4ed8d80f4d
Blaine Jester <blainesworld@gmail.com>
2026-03-31 12:41:13 -0700

    Add constants for picture passwords feature

---
+++ packages/kolibri-common/constants/Auth.js
@@ -0,0 +1,10 @@
+export const PicturePasswordIconStyle = {
+  COLORFUL: 'colorful',
+  STANDARD: 'standard',
+};
+
+export const OptionsForSignIn = {
+  USERNAME_PASSWORD: 'username_password',
+  USERNAME_ONLY: 'username_only',
+  PICTURE_PASSWORD: 'picture_password',
+};
