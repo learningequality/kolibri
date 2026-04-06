bcf84c3b614693dcc9f20c2360b881b26427f2f5
Aadarsh Mahesh K <aadarshmahk@gmail.com>
2026-04-06 12:29:28 +0530

    feat: migrate setup-wizard tests into vue testing library

--- kolibri/plugins/setup_wizard/frontend/views/__tests__/UserCredentialsForm.spec.js
+++ kolibri/plugins/setup_wizard/frontend/views/__tests__/UserCredentialsForm.spec.js
@@ -1,43 +1,49 @@
-import { mount } from '@vue/test-utils';
+import { render, screen, waitFor } from '@testing-library/vue';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import makeStore from '../../__tests__/utils/makeStore';
 import UserCredentialsForm from '../onboarding-forms/UserCredentialsForm';
 
-function makeWrapper() {
+function renderComponent() {
   const store = makeStore();
-  const wrapper = mount(UserCredentialsForm, {
+  const send = jest.fn();
+
+  render(UserCredentialsForm, {
     store,
-    // These need to be stubbed because of some mysterious errors
-    stubs: ['FullNameTextbox', 'UsernameTextbox'],
+    provide: {
+      wizardService: {
+        send,
+        state: {
+          context: {},
+        },
+      },
+    },
   });
-  jest.spyOn(wrapper.vm, '$emit');
-  const actions = {
-    simulateSubmit: () => wrapper.findComponent({ name: 'OnboardingForm' }).vm.$emit('submit'),
-  };
-  return { store, wrapper, actions };
+
+  return { send, store };
 }
 
-describe.skip('UserCredentialsForm', () => {
-  it('clicking submit updates vuex with correct data', async () => {
-    const { store, wrapper, actions } = makeWrapper();
-    wrapper.setData({
-      fullName: 'Schoolhouse Rock',
-      fullNameValid: true,
-      username: 'schoolhouse_rock',
-      usernameValid: true,
-      password: 'password',
-      passwordValid: true,
-    });
-    actions.simulateSubmit();
-    await wrapper.vm.$nextTick();
+describe('UserCredentialsForm', () => {
+  it('saves the entered super admin details when the user continues', async () => {
+    const { send, store } = renderComponent();
+
+    await userEvent.type(screen.getByLabelText(/full name/i), 'Schoolhouse Rock');
+    await userEvent.type(screen.getByLabelText(/^username$/i), 'schoolhouse_rock');
+    await userEvent.type(screen.getByLabelText(/^password$/i), 'password');
+    await userEvent.type(screen.getByLabelText(/re-enter password/i), 'password');
+
+    await userEvent.click(screen.getByRole('button', { name: /continue/i }));
+
     expect(store.state.onboardingData.user).toEqual({
       full_name: 'Schoolhouse Rock',
       username: 'schoolhouse_rock',
       password: 'password',
     });
-    expect(wrapper.vm.$emit).toHaveBeenCalledWith('click_next', {
-      full_name: 'Schoolhouse Rock',
-      username: 'schoolhouse_rock',
-      password: 'password',
+    await waitFor(() => {
+      expect(send).toHaveBeenCalledWith({
+        type: 'CONTINUE',
+        value: store.state.onboardingData.user,
+      });
     });
   });
 });
