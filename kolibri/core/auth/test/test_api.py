f8590bb7c3805d8d071f9e99fe2a8b1bcadfe94a
rtibblesbot <richard+githubagent@learningequality.org>
2026-07-02 08:49:37 -0700

    Merge pull request #14940 from rtibblesbot/issue-14927-ee85c3
    Remove factory-boy and fake-factory test dependencies

--- kolibri/core/auth/test/test_api.py
+++ kolibri/core/auth/test/test_api.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from datetime import timedelta
 from importlib import import_module
 
-import factory
 from django.conf import settings
 from django.db import connection
 from django.db.models.signals import pre_delete
@@ -44,6 +43,8 @@ from kolibri.core.discovery.utils.network.errors import NetworkLocationConnectio
 from kolibri.core.discovery.utils.network.errors import NetworkLocationResponseFailure
 from kolibri.core.discovery.utils.network.errors import NetworkLocationResponseTimeout
 from kolibri.core.tasks.job import Job
+from kolibri.core.test.model_factory import ModelFactory
+from kolibri.core.test.model_factory import sequence
 
 from .. import models
 from ..constants import role_kinds
@@ -60,34 +61,51 @@ from .helpers import provision_device
 from .helpers import setup_device
 
 
-class FacilityFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = models.Facility
+class FacilityFactory(ModelFactory):
+    model = models.Facility
+    _name = sequence("Rock N' Roll High School #%d")
 
-    name = factory.Sequence(lambda n: "Rock N' Roll High School #%d" % n)
-
-
-class ClassroomFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = models.Classroom
-
-    name = factory.Sequence(lambda n: "Basic Rock Theory #%d" % n)
+    @classmethod
+    def field_defaults(cls):
+        return {"name": cls._name}
 
 
-class LearnerGroupFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = models.LearnerGroup
+class ClassroomFactory(ModelFactory):
+    model = models.Classroom
+    _name = sequence("Basic Rock Theory #%d")
 
-    name = factory.Sequence(lambda n: "Group #%d" % n)
+    @classmethod
+    def field_defaults(cls):
+        return {"name": cls._name}
 
 
-class FacilityUserFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = models.FacilityUser
+class LearnerGroupFactory(ModelFactory):
+    model = models.LearnerGroup
+    _name = sequence("Group #%d")
 
-    facility = factory.SubFactory(FacilityFactory)
-    username = factory.Sequence(lambda n: "user%d" % n)
-    password = factory.PostGenerationMethodCall("set_password", DUMMY_PASSWORD)
+    @classmethod
+    def field_defaults(cls):
+        return {"name": cls._name}
+
+
+class FacilityUserFactory(ModelFactory):
+    model = models.FacilityUser
+    _username = sequence("user%d")
+
+    @classmethod
+    def field_defaults(cls):
+        return {
+            "facility": FacilityFactory.create,
+            "username": cls._username,
+        }
+
+    @classmethod
+    def create(cls, **kwargs):
+        password = kwargs.pop("password", DUMMY_PASSWORD)
+        user = super().create(**kwargs)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class LearnerGroupAPITestCase(APITestCase):
@@ -1139,9 +1157,9 @@ class UserUpdateTestCase(APITestCase):
         provision_device()
         cls.facility = FacilityFactory.create()
         cls.superuser = create_superuser(cls.facility)
+        cls.user = FacilityUserFactory.create(facility=cls.facility)
 
     def setUp(self):
-        self.user = FacilityUserFactory.create(facility=self.facility)
         self.client.login(
             username=self.superuser.username,
             password=DUMMY_PASSWORD,
@@ -1268,9 +1286,9 @@ class UserDeleteTestCase(APITestCase):
         provision_device()
         cls.facility = FacilityFactory.create()
         cls.superuser = create_superuser(cls.facility)
+        cls.user = FacilityUserFactory.create(facility=cls.facility)
 
     def setUp(self):
-        self.user = FacilityUserFactory.create(facility=self.facility)
         self.client.login(
             username=self.superuser.username,
             password=DUMMY_PASSWORD,
@@ -3364,12 +3382,13 @@ class CSRFProtectedAuthTestCase(APITestCase):
 
 
 class SetNonSpecifiedPasswordViewTestCase(APITestCase):
-    def setUp(self):
-        self.url = reverse("kolibri:core:setnonspecifiedpassword")
-        self.facility = FacilityFactory.create()
-        self.user = models.FacilityUser.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse("kolibri:core:setnonspecifiedpassword")
+        cls.facility = FacilityFactory.create()
+        cls.user = models.FacilityUser.objects.create(
             username="testuser",
-            facility=self.facility,
+            facility=cls.facility,
             password=demographics.NOT_SPECIFIED,
         )
 
@@ -3663,8 +3682,11 @@ class RemoteAccessSessionTestCase(APITestCase):
 class KolibriDataPortalViewSetTestCase(APITestCase):
     databases = "__all__"
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.facility, cls.superuser = setup_device()
+
     def setUp(self):
-        self.facility, self.superuser = setup_device()
         self.client.login(username=self.superuser.username, password=DUMMY_PASSWORD)
 
     @patch("kolibri.core.api.enqueue_automatic_kdp_sync")
