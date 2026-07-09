5cf796c91eb5b272cb98f0628c05f9258e05e10f
Shoaib Ahmad <saibuhasan22@gmail.com>
2026-07-09 10:17:22 +0000

    Add skeleton VTL tests for RearrangeChannelsPage

--- kolibri/plugins/device/frontend/views/__tests__/RearrangeChannelsPage.spec.js
+++ kolibri/plugins/device/frontend/views/__tests__/RearrangeChannelsPage.spec.js
@@ -1,92 +1,76 @@
-import { shallowMount } from '@vue/test-utils';
-import useUser, { useUserMock } from 'kolibri/composables/useUser'; // eslint-disable-line
-import useSnackbar, { useSnackbarMock } from 'kolibri/composables/useSnackbar'; // eslint-disable-line
-import makeStore from '../../__tests__/utils/makeStore';
+import { render, screen, waitFor } from '@testing-library/vue';
+import VueRouter from 'vue-router';
+import useUser, { useUserMock } from 'kolibri/composables/useUser'; // eslint-disable-line import-x/named
+import useSnackbar, { useSnackbarMock } from 'kolibri/composables/useSnackbar'; // eslint-disable-line import-x/named
+import { createTranslator } from 'kolibri/utils/i18n';
 import RearrangeChannelsPage from '../RearrangeChannelsPage';
+import makeStore from '../../__tests__/utils/makeStore';
+import { PageNames } from '../../constants';
+
+const { instructions$, noChannels$ } = createTranslator(
+  RearrangeChannelsPage.name,
+  RearrangeChannelsPage.$trs,
+);
 
 jest.mock('../../composables/useContentTasks');
 jest.mock('kolibri/composables/useUser');
 jest.mock('kolibri/composables/useSnackbar');
 
-RearrangeChannelsPage.methods.postNewOrder = () => Promise.resolve();
-RearrangeChannelsPage.methods.fetchChannels = () => {
-  return Promise.resolve([
-    { id: '1', name: 'Channel 1' },
-    { id: '2', name: 'Channel 2' },
-  ]);
-};
-async function makeWrapper() {
-  const store = makeStore();
-  useUser.mockImplementation(() => useUserMock({ canManageContent: true }));
-  const wrapper = shallowMount(RearrangeChannelsPage, {
-    store,
+function createRouter() {
+  return new VueRouter({
+    routes: [
+      { name: PageNames.REARRANGE_CHANNELS, path: '/content/reorder_channels' },
+      { name: PageNames.MANAGE_CONTENT_PAGE, path: '/content' },
+    ],
   });
-  // Have to wait to let the channels data load
-  await global.flushPromises();
-  return { wrapper };
 }
 
-describe('RearrangeChannelsPage', () => {
-  const createSnackbar = jest.fn();
-  beforeAll(() => {
-    useSnackbar.mockImplementation(() => useSnackbarMock({ createSnackbar }));
-  });
+const MOCK_CHANNELS = [
+  { id: '1', name: 'Channel 1' },
+  { id: '2', name: 'Channel 2' },
+];
 
-  async function simulateSort(wrapper) {
-    const region = wrapper.findComponent({ name: 'DraggableRegion' });
-    region.vm.$emit('update:items', [wrapper.vm.channels[1], wrapper.vm.channels[0]]);
-    expect(wrapper.vm.postNewOrder).toHaveBeenCalledWith(['2', '1']);
-    await global.flushPromises();
-  }
+RearrangeChannelsPage.methods.postNewOrder = () => Promise.resolve();
+RearrangeChannelsPage.methods.fetchChannels = () => Promise.resolve(MOCK_CHANNELS);
+
+describe('RearrangeChannelsPage', () => {
+  let createSnackbar;
+
+  const renderComponent = async () => {
+    createSnackbar = jest.fn();
+    useUser.mockImplementation(() => useUserMock({ canManageContent: true }));
+    useSnackbar.mockImplementation(() => useSnackbarMock({ createSnackbar }));
+
+    const store = makeStore();
+    const router = createRouter();
+    await router.push({ name: PageNames.REARRANGE_CHANNELS });
+    return render(RearrangeChannelsPage, { store, router });
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('loads the data on mount', async () => {
-    const { wrapper } = await makeWrapper();
-    expect(wrapper.vm.loading).toBe(false);
-    expect(wrapper.vm.channels).toHaveLength(2);
+    await renderComponent();
+    await waitFor(() => {
+      expect(screen.getByText(MOCK_CHANNELS[0].name)).toBeInTheDocument();
+      expect(screen.getByText(MOCK_CHANNELS[1].name)).toBeInTheDocument();
+    });
   });
 
-  it('handles a successful @sort event properly', async () => {
-    const { wrapper } = await makeWrapper();
-    wrapper.vm.postNewOrder = jest.fn().mockResolvedValue();
-    wrapper.vm.$store.dispatch = jest.fn();
-    await simulateSort(wrapper);
-    expect(createSnackbar).toHaveBeenCalledWith('Channel order saved');
-    expect(wrapper.vm.channels[0].id).toEqual('2');
-    expect(wrapper.vm.channels[1].id).toEqual('1');
+  it('shows the instructions text', async () => {
+    await renderComponent();
+    await waitFor(() => {
+      expect(screen.getByText(instructions$())).toBeInTheDocument();
+    });
   });
 
-  it('handles a failed @sort event properly', async () => {
-    const { wrapper } = await makeWrapper();
-    wrapper.vm.postNewOrder = jest.fn().mockRejectedValue();
-    wrapper.vm.$store.dispatch = jest.fn();
-    await simulateSort(wrapper);
-    expect(createSnackbar).toHaveBeenCalledWith('There was a problem reordering the channels');
-    // Channels array is reset after an error
-    expect(wrapper.vm.channels[0].id).toEqual('1');
-    expect(wrapper.vm.channels[1].id).toEqual('2');
-  });
-
-  // Will mock the handleOrderChange method to test these cases synchronousy,
-  // since that method should be tested by the previous tests.
-  it('handles a @moveUp event properly', async () => {
-    const { wrapper } = await makeWrapper();
-    const spy = (wrapper.vm.handleOrderChange = jest.fn());
-    const dragSortWidget = wrapper.findAllComponents({ name: 'DragSortWidget' }).at(1);
-    dragSortWidget.vm.$emit('moveUp');
-    expect(spy).toHaveBeenCalledWith([
-      { id: '2', name: 'Channel 2' },
-      { id: '1', name: 'Channel 1' },
-    ]);
-  });
-
-  it('handles a @moveDown event properly', async () => {
-    const { wrapper } = await makeWrapper();
-    const spy = (wrapper.vm.handleOrderChange = jest.fn());
-    const dragSortWidget = wrapper.findAllComponents({ name: 'DragSortWidget' }).at(0);
-    dragSortWidget.vm.$emit('moveDown');
-    expect(spy).toHaveBeenCalledWith([
-      { id: '2', name: 'Channel 2' },
-      { id: '1', name: 'Channel 1' },
-    ]);
+  it('shows a message when there are no channels', async () => {
+    RearrangeChannelsPage.methods.fetchChannels = () => Promise.resolve([]);
+    await renderComponent();
+    await waitFor(() => {
+      expect(screen.getByText(noChannels$())).toBeInTheDocument();
+    });
   });
 });
