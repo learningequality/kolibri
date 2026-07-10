26888803fcfa5849fa0b2c9a130255b80f774239
habibayman <habibaayman2004@outlook.com>
2026-07-11 02:07:58 +0300

    feat: add qti textentry numeric example

--- kolibri/plugins/qti_viewer/frontend/components/__fixtures__/items.js
+++ kolibri/plugins/qti_viewer/frontend/components/__fixtures__/items.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:581072841c36d499fa9401c73cb6a9ef87b298d9e86cf76be420c7101eeb80ba
-size 864016
+oid sha256:b1e8ed3139406e75848801ca80ca046d4b8449abd0a5e199762ddf03747b857a
+size 862145
